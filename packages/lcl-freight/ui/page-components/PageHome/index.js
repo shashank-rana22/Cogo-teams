a4dc624cc9d7c3a01efb7d428d29fe43b23b116c
Rajatuba <rajat.uba@cogoport.com>
2022-10-28 17:27:45 +0530

    lcl-freight

---
+++ packages/lcl-freight/ui/page-components/PageHome/index.js
@@ -0,0 +1,12 @@
+import { FluidContainer } from '@cogoport/components';
+import React from 'react';
+
+function PageHome() {
+	return (
+		<FluidContainer>
+			<p>Home Page</p>
+		</FluidContainer>
+	);
+}
+
+export default PageHome;
