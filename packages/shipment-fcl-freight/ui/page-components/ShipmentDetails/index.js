4b94afc3dcc890c4f5ac02b6dc3549ca6a77ff86
Rajatuba-Cogoport <rajat.uba@cogoport.com>
2022-11-02 20:12:56 +0530

    redux for partnerID

--- packages/shipment-fcl-freight/ui/page-components/ShipmentDetails/index.js
+++ packages/shipment-fcl-freight/ui/page-components/ShipmentDetails/index.js
@@ -2,11 +2,19 @@ import React from "react";
 
 import {useRouter} from "next/router"
 
+import { useSelector } from "react-redux";
+
+
 const ShipmentDetails=()=>{
 
-    const {id}=useRouter()
+    const partnerId=useSelector(state=>state.partnerIdStore.partnerId)
 
-    return <div>Shipment Detail id = {id} </div>
+    const {query}=useRouter()
+
+    return <div>
+        <div>Shipment Detail id = {query.id} </div>
+   <div> Current partnerId = {partnerId}</div>
+    </div>
 }
 
 export default ShipmentDetails
