fb5855c5c8614b5937894f861e7db4e63cb240de
Rajatuba <rajat.uba@cogoport.com>
2022-12-15 13:52:56 +0530

    removed form

--- common/form/index.js
+++
@@ -1,9 +0,0 @@
-import { useForm } from 'react-hook-form';
-
-import DatePickerController from './Controlled/DatePickerController';
-import PillsController from './Controlled/PillsController';
-import SelectController from './Controlled/SelectController';
-
-export {
-	useForm, DatePickerController, PillsController, SelectController,
-};
