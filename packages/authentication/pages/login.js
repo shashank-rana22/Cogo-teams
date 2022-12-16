6440929adea3a9d5b8f95cf0c5f0fda38f003277
Rishi Agarwal <rishi@cogoport.com>
2022-12-16 21:05:05 +0530

    moving pages to packages

---
+++ packages/authentication/pages/login.js
@@ -0,0 +1,7 @@
+import LoginComponent from '@cogoport/authentication/page-components/Login';
+
+LoginComponent.getInitialProps = () => ({
+	layout: 'hidden',
+});
+
+export default LoginComponent;
---
+++ packages/product-code-mappings/pages/product-code-mappings.js
@@ -0,0 +1,1 @@
+export { default } from '@cogoport/product-code-mappings/page-components/ProductCodeMappings';
