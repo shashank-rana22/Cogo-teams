19ad4d551d7ae173476fc8711db41952ad330308
prashantp01 <prashant.paddune@cogoport.com>
2022-12-19 12:51:14 +0530

    aadmin: fix controller
    Signed-off-by: prashantp01 <prashant.paddune@cogoport.com>

--- common/forms/Controlled/PillsController.tsx
+++ common/forms/Controlled/PillsController.tsx
@@ -3,14 +3,16 @@ import React from 'react';
 import { Controller } from 'react-hook-form';
 
 function PillsController(props: any) {
-	const { name, control, ...rest } = props;
+	const {
+		name, control, rules, ...rest
+	} = props;
 
 	return (
 		<Controller
 			key={rest.id}
 			control={control}
 			name={name}
-			rules={rest.rules}
+			rules={rules}
 			render={({ field: { onChange, onBlur, value } }) => (
 				<Pills
 					{...rest}
