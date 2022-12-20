19cdad6482dfe042b3316db8d656845a7da28d7d
cogomanish <manish.kumar@cogoport.com>
2022-12-20 19:32:53 +0530

    PR changes

--- packages/business-finance/page-components/AccountReceivables/index.tsx
+++ packages/business-finance/page-components/AccountReceivables/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import { useSelector } from '@cogoport/store';
 function AccountRecievables() {
-	const profileData = useSelector(({ profile }: any) => profile);
-	console.log('AccountRecievables', profileData);
+
 	return (
 		<div>
 			<h1>Account Recievables</h1>
