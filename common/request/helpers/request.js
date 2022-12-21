bcd644b41f3ec6e9d6e6160bd8626a3e611cdecf
Rajatuba <rajat.uba@cogoport.com>
2022-12-21 18:10:15 +0530

    changes

--- common/request/helpers/request.js
+++ common/request/helpers/request.js
@@ -15,13 +15,11 @@ const customSerializer = (params) => {
 const request = Axios.create({ baseURL: process.env.NEXT_PUBLIC_REST_BASE_API_URL });
 
 request.interceptors.request.use((oldConfig) => {
-	console.log("old",oldConfig)
 	const token = getCookieFromCtx(process.env.NEXT_PUBLIC_AUTH_TOKEN_NAME, oldConfig.ctx);
 	const newConfig = oldConfig;
 	newConfig.paramsSerializer = customSerializer;
-	// newConfig.headers = { authorizationscope: 'partner' , authorizationparameters:
-	// 'coe-fcl_revenue_desk:across_all'};
-	// newConfig.headers.authorization = `Bearer: 3598c948-7334-4eb5-a308-aee81af6cd93`;
+	newConfig.headers = { authorizationscope: 'partner' };
+	newConfig.headers.authorization = `Bearer: ${token}`;
 
 	return newConfig;
 });
