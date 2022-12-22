be62865cb6b6f5c0fac5afee8746d7d9d064917d
prashantp01 <prashant.paddune@cogoport.com>
2022-12-22 19:00:36 +0530

    admin: packages update
    Signed-off-by: prashantp01 <prashant.paddune@cogoport.com>

--- common/core/components/_app/index.js
+++ common/core/components/_app/index.js
@@ -1,4 +1,4 @@
-import '@cogoport/components/dist/themes/supernova.css';
+import '@cogoport/components/dist/themes/dawn.css';
 import handleAuthentication from '@cogoport/authentication/utils/handleAuthentication';
 import { Router, RoutesProvider } from '@cogoport/next';
 import store, { Provider } from '@cogoport/store';
