4a966a879e3faa061720e0afd61c27b94fa15de3
cogomanish <manish.kumar@cogoport.com>
2022-12-24 16:58:10 +0530

    supplier details

--- packages/business-finance/page-components/COEFinance/All_Invoices/ViewInvoices/SupplierDetails/index.tsx
+++ packages/business-finance/page-components/COEFinance/All_Invoices/ViewInvoices/SupplierDetails/index.tsx
@@ -5,8 +5,11 @@ import styles from './styles.module.css';
 const SupplierDetails =()=>{
     return(
         <div className={styles.container}> 
+
             <h3>Supplier Details</h3>
+
             <div className={styles.smallHr} />
+
             <div className={styles.card}>
                 <div className={styles.orgNameAndVerified}>
                     <div>Name - Evergreen Private Limited</div>
@@ -16,7 +19,20 @@ const SupplierDetails =()=>{
                         <div className={styles.kycVerified}><IcCFtick/><div>kyc verified</div></div>
                     </div>      
                 </div>
+
                 <div className={styles.verticalSmallHr}/>
+
+                <div className={styles.accountDetails}>
+                    <div className={styles.accounts}>Amount Payables : <div>INR 20,02,302</div></div>  
+                    <div className={styles.accounts}>Amount Receivables : <div>INR 20,02,302</div></div>  
+                </div>
+
+                <div className={styles.verticalSmallHr}/>
+
+                <div className={styles.supplierDetails}>
+                    <div>Supplier History</div>
+                    <div>Documents</div> 
+                </div>
             </div>
           
 
