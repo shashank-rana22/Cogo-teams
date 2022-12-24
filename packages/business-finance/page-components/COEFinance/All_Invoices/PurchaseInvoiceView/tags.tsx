7d5fc87e27a63b4df5ac1dcd43922c3431fb95df
mona singh <mona.singh@cogoport.com>
2022-12-24 20:13:50 +0530

    create Purchase view List

---
+++ packages/business-finance/page-components/COEFinance/All_Invoices/PurchaseInvoiceView/tags.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface TagProps {
+	itemData: any;
+}
+function TagsData({itemData}:TagProps) {
+
+  return (
+    <div>hy</div>
+  )
+}
+
+export default TagsData;
--- packages/business-finance/page-components/COEFinance/All_Invoices/index.tsx
+++ packages/business-finance/page-components/COEFinance/All_Invoices/index.tsx
@@ -1,4 +1,5 @@
 import React,{useState} from "react"
+import PurchaseInvoice from './PurchaseInvoiceView';
 import styles from './styles.module.css'
 const AllInvoices =()=>{
     const [classNamePurchase, setClassNamePurchase] = useState(true)
@@ -17,11 +18,15 @@ const AllInvoices =()=>{
         {id:2, name:'shipment-id-view', label:'SHIPMENT ID VIEW'}
     ]
 return(
+    <div>
     <div className={styles.container}>
 
                 <div className={classNamePurchase ? styles.subContainerClick :styles.subContainer  } onClick={getPurchase}>PURCHASE INVOICE VIEW</div>
                 <div className={classNameSID ? styles.subContainerClick :styles.subContainer } onClick={getSID}>SHIPMENT ID VIEW</div>
 
+
+    </div>
+           {classNamePurchase && <PurchaseInvoice/>}
     </div>
 )
       
