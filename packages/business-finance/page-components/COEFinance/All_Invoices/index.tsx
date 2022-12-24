cac242db178b6360d4b331983199d1e380a5e9bc
cogomanish <manish.kumar@cogoport.com>
2022-12-24 13:47:19 +0530

    view

--- packages/business-finance/page-components/COEFinance/All_Invoices/index.tsx
+++ packages/business-finance/page-components/COEFinance/All_Invoices/index.tsx
@@ -1,9 +1,13 @@
 import React,{useState} from "react"
 import styles from './styles.module.css'
+import { Button  } from "@cogoport/components"
+import { Link, useRouter } from '@cogoport/next';
 import ViewInvoices from "./ViewInvoices/index"
 const AllInvoices =()=>{
     const [classNamePurchase, setClassNamePurchase] = useState(true)
     const [classNameSID, setClassNameSID] = useState(false)
+    const router = useRouter();
+
     const getPurchase= () => { 
         setClassNamePurchase(true)
         setClassNameSID(false)
@@ -19,7 +23,7 @@ return(
 
                 <div  onClick={getPurchase}>
                     <div className={classNamePurchase ? styles.subContainerClick :styles.subContainer }> PURCHASE INVOICE VIEW </div> 
-                    <ViewInvoices/>
+                    <Button size="md" themeType="secondary" onClick={() => router.push('/business-finance/coe-finance/view-invoices')}>rgjndkjk</Button>
                 </div>
                 <div onClick={getSID}>
                     <div className={classNameSID ? styles.subContainerClick :styles.subContainer } >SHIPMENT ID VIEW</div>
