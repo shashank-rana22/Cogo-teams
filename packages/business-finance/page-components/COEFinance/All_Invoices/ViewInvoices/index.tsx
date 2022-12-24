1123e7492208957f09bd6e644c5aec083b745b87
cogomanish <manish.kumar@cogoport.com>
2022-12-24 13:18:01 +0530

    changes

---
+++ packages/business-finance/page-components/COEFinance/All_Invoices/ViewInvoices/index.tsx
@@ -0,0 +1,4 @@
+const ViewInvoices =()=>{
+    return(<div>brkhjb</div>)
+}
+export default ViewInvoices
--- packages/business-finance/page-components/COEFinance/All_Invoices/index.tsx
+++ packages/business-finance/page-components/COEFinance/All_Invoices/index.tsx
@@ -1,5 +1,6 @@
 import React,{useState} from "react"
 import styles from './styles.module.css'
+import ViewInvoices from "./ViewInvoices/index"
 const AllInvoices =()=>{
     const [classNamePurchase, setClassNamePurchase] = useState(true)
     const [classNameSID, setClassNameSID] = useState(false)
@@ -12,15 +13,18 @@ const AllInvoices =()=>{
         setClassNamePurchase(false)
 
     }
-    const commonCard =[
-        {id:1, name:'purchase-invoice-view', label:'PURCHASE INVOICE VIEW'},
-        {id:2, name:'shipment-id-view', label:'SHIPMENT ID VIEW'}
-    ]
+
 return(
     <div className={styles.container}>
 
-                <div className={classNamePurchase ? styles.subContainerClick :styles.subContainer  } onClick={getPurchase}>PURCHASE INVOICE VIEW</div>
-                <div className={classNameSID ? styles.subContainerClick :styles.subContainer } onClick={getSID}>SHIPMENT ID VIEW</div>
+                <div  onClick={getPurchase}>
+                    <div className={classNamePurchase ? styles.subContainerClick :styles.subContainer }> PURCHASE INVOICE VIEW </div> 
+                    <ViewInvoices/>
+                </div>
+                <div onClick={getSID}>
+                    <div className={classNameSID ? styles.subContainerClick :styles.subContainer } >SHIPMENT ID VIEW</div>
+                </div>
+                
 
     </div>
 )
