507abb27079096360cde7ad81f722d7f4ee1aa1f
cogomanish <manish.kumar@cogoport.com>
2022-12-24 18:20:09 +0530

    shipment details

--- packages/business-finance/page-components/COEFinance/All_Invoices/ViewInvoices/index.tsx
+++ packages/business-finance/page-components/COEFinance/All_Invoices/ViewInvoices/index.tsx
@@ -1,11 +1,13 @@
 import React from "react"
 import Header from "./Header/index"
 import SupplierDetails from "./SupplierDetails/index"
+import ShipmentDetails from "./ShipmentDetails/index"
 const ViewInvoices =()=>{
     return(
     <div>
         <Header/>
         <SupplierDetails/>
+        <ShipmentDetails/>
     </div>
     )
 }
