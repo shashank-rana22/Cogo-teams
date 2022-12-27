0a88ae698690f31e4fe7e98979390521b9f4d35f
mona singh <mona.singh@cogoport.com>
2022-12-27 14:39:23 +0530

    changes

--- packages/business-finance/page-components/COEFinance/All_Invoices/PurchaseInvoiceView/index.tsx
+++ packages/business-finance/page-components/COEFinance/All_Invoices/PurchaseInvoiceView/index.tsx
@@ -1,24 +1,41 @@
 import React from 'react'
 import List from '../../../commons/List/index';
 import useGetPurchaseViewList from '../../hook/usePurchaseViewList';
-import TagsData from './tags'
-
-
+import ShowRemarkData from './ShowRemark/index'
+import RenderStatus from './RenderStatus/index'
+import FieldPair from './FiledPair/index';
+import RenderCustomer from './RenderCustomer/index'
+import FormatedDate from './FormatedDate/index';
 
 
 
 
 function PurchaseInvoice() {
 
-const {data,loading,config}=useGetPurchaseViewList();
+  const {data,loading,config}=useGetPurchaseViewList();
+
+    const functions = {
+        renderRemark:(itemData:any,field:any)=>(
+          <ShowRemarkData itemData={itemData} field={field}/>
+        ),
+        renderStatus: (itemData:any,field:any) => (
+          <RenderStatus item={itemData} field={field} />
+      ),
+      renderFieldPair: (itemData:any, field:any) => (
+        <FieldPair item={itemData} field={field} />
+      ),
+      renderCustomer: (itemData:any, field:any) => (
+        <RenderCustomer item={itemData} field={field} />
+      ),
+      rendeFormate: (itemData:any, field:any) => (
+        <FormatedDate item={itemData} field={field} />
+      ),
+    };
 
 
-
-   
-    
   return (
     <div>
-        <List config={config}  itemData={data?.list}  />
+        <List config={config}  itemData={data?.list} functions={functions} />
     </div>
   )
 }
--- packages/business-finance/page-components/COEFinance/configurations/PURCHASE_VIEW_LIST.ts
+++ packages/business-finance/page-components/COEFinance/configurations/PURCHASE_VIEW_LIST.ts
@@ -1,13 +1,14 @@
 export const PURCHASE_VIEW_CONFIG = {
 	showHeader         : true,
 	headerStyles       : { marginBottom: '16px', borderRadius: '8px', background: '#333',marginTop:'20px' },
-    bodyStyles          :{border: '1px solid #C7C7C7',color:' #333333',fontWeight: '400',fontSize: '12px',lineHeight: '14px'},
+    bodyStyles          :{border: '1px solid #C7C7C7',color:' #333333',fontWeight: '400',fontSize: '12px',lineHeight: '14px',fontFamily: "Roboto",fontStyle: 'normal',},
 	fields             : [
 		
         {
 			label: 'Invoice No.',
 			topKey: {
 				key: 'billNumber',
+				type: 'href',
 			},
 			lowerKey: {
 				key: 'billType',
@@ -18,13 +19,22 @@ export const PURCHASE_VIEW_CONFIG = {
 			span: 1.2,
 		},
 		{
-			label : 'SID',
-			key   : 'jobNumber',
-			span  : 1,
+			label: 'SID',
+			topKey: {
+				key: 'jobNumber',
+				type: 'href',
+                redirectKey: 'shipmentId',
+			},
+			lowerKey: {
+				key: 'serviceType',
+			},
+			func: 'renderFieldPair',
+			span: 1,
 		},
 		{
             label   : 'Supplier Name',
             key     : 'organizationName',
+			func 	:'renderCustomer',
 			span    : 1.5,
 			
 		},
@@ -32,20 +42,24 @@ export const PURCHASE_VIEW_CONFIG = {
 			label : 'Invoice Amount',
 			key   : 'grandTotal',
 			span  : 1.5,
+			func  :'rendeFormate',
 		},
 		{
             label : 'Last Modified Date',
 			key   : 'createdDate',
 			span  : 1.5,
+			func 	:'rendeFormate',
 		},
 		{
 			label : 'Invoice Date',
             key   : 'billDate',
+			func 	:'rendeFormate',
 			span  : 1.5,
 		},
 		{
 			label : 'Payment Due Date',
             key   : 'dueDate',
+			func 	:'rendeFormate',
 			span  : 1.7,
 		},
 		{
@@ -58,6 +72,7 @@ export const PURCHASE_VIEW_CONFIG = {
 		{
 			label : 'Remarks ',
             key   : 'remarks',
+            func :  'renderRemark',
 			span  : 1.2,
 		},
 		{
