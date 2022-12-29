2586bd753cdc95a66828de97b60b684064f8de91
Manish <manishd.kumar@cogoport.com>
2022-12-29 15:12:08 +0530

    fix: readUrl

--- common/forms/page-components/Business/FileUploader/index.tsx
+++ common/forms/page-components/Business/FileUploader/index.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable no-await-in-loop */
 import { Upload, Toast } from '@cogoport/components';
 import { IcMDocument, IcMUpload } from '@cogoport/icons-react';
-import { useRequest, usePublicRequest } from '@cogoport/request';
-import React, { useState, useRef, useEffect } from 'react';
+import { useRequest, publicRequest, request } from '@cogoport/request';
+import React, { useState, useEffect } from 'react';
 
 import styles from './styles.module.css';
 
@@ -23,7 +23,6 @@ function FileUploader(props: any) {
 	const [fileName, setFileName] = useState(null);
 	const [loading, setLoading] = useState(true);
 	const [urlStore, setUrlStore] = useState([]);
-	const previousValue = useRef(null);
 
 	useEffect(() => {
 		if (multiple) {
@@ -33,53 +32,79 @@ function FileUploader(props: any) {
 		}
 	}, [urlStore]);
 
-	const [{ loading: addLoading }, triggerAdd] = useRequest({
-		method : 'GET',
-		url    : '/get_media_upload_url',
-	}, { manual: true });
-	const [{ loading: uploadLoading }, triggerUpload] = usePublicRequest({
-		method: 'PUT',
-	}, { manual: true });
-
+	// const [{ loading: uploadLoading }, triggerUpload] = usePublicRequest({
+	// 	method: 'PUT',
+	// }, { manual: true });
 	let i = 0;
+	const uploadFile = async (file:any) => {
+		console.log('file', file);
+		const { data } = await request({
+			method : 'GET',
+			url    : '/get_media_upload_url',
+			params : {
+				file_name: new Date().getTime(),
+			},
+		});
+		const { url, headers } = data;
+		await publicRequest({
+			url,
+			data    : file,
+			method  : 'PUT',
+			headers : {
+				...headers,
+				'Content-Type': file.type,
+			},
+		});
+		return url.split('?')[0];
+	};
+
 	const done = 10;
 	const handleChange = async (values: any) => {
 		setPercent(done);
 		const promises = [];
 		while (i < values.length) {
 			try {
-				const { data } = await triggerAdd({
-					params: { file_name: new Date().getTime() },
-				});
-				const { url, headers } = data;
-				try {
-					promises.push(await triggerUpload({
-						url,
-						data    : values[i],
-						headers : {
-							'Content-Type': values[i].type,
-						},
-					}));
-					setPercent(done + (50 * i));
-				} catch (err) {
-					console.log({ err });
-				}
-				setFileName(values);
-				setUrlStore((previousState) => [...previousState, url]);
-				i++;
-			} catch (err:any) {
-				return Toast.error('File Upload failed.......');
+				promises.push(uploadFile(values[i]));
+			} catch (err) {
+				Toast.error('File Upload failed.......');
 			}
+			i += 1;
 		}
-		console.log('cververvterb', urlStore);
-		Promise.all(promises).then((promise) => {
-			promise.forEach((item) => {
-				console.log('verbgerthtrege', item);
-				setPercent(100);
-				setLoading(false);
-			});
-		});
+		const allUrls = await Promise.all(promises);
+		console.log('urlssss', { allUrls });
+		setPercent(100);
 	};
+	// try {
+	// 	const { data } = await request({
+	// 		method : 'GET',
+	// 		url    : '/get_media_upload_url',
+	// 		params : {
+	// 			file_name: new Date().getTime(),
+	// 		},
+	// 	});
+	// 	const { url } = data;
+	// 	try {
+	// 		promises.push(
+	// 			publicRequest({
+	// 				url,
+	// 				data    : values[i],
+	// 				method  : 'PUT',
+	// 				headers : {
+	// 					'Content-Type': values[i].type,
+	// 				},
+	// 			})
+	// 				.then(() => url.split('?')[0]),
+	// 		);
+	// 		setPercent(done + (50 * i));
+	// 	} catch (err) {
+	// 		console.log({ err });
+	// 	}
+	// 	setFileName(values);
+	// 	// setUrlStore((previousState) => [...previousState, url]);
+	// 	i += 1;
+	// } catch (err:any) {
+	// 	Toast.error('File Upload failed.......');
+	// }
 
 	return (
 		<>
