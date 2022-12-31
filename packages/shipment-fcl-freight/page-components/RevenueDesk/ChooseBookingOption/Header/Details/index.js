7e055984f17dba646402654a784c9717217334a7
Naveen-1401 <naveen.verma@cogoport.com>
2022-12-31 16:09:57 +0530

    css-changes

--- packages/shipment-fcl-freight/page-components/RevenueDesk/ChooseBookingOption/Header/Details/index.js
+++ packages/shipment-fcl-freight/page-components/RevenueDesk/ChooseBookingOption/Header/Details/index.js
@@ -1,8 +1,6 @@
-import { Button } from '@cogoport/components';
 import getFormattedPrice from '@cogoport/forms/utils/get-formatted-price';
-import { IcMArrowRotateDown, IcMArrowRotateUp } from '@cogoport/icons-react';
 import { format } from '@cogoport/utils';
-import React, { useState } from 'react';
+import React from 'react';
 
 import BuyServiceQuotation from './BuyServiceQuotation';
 import SellServiceQuotation from './SellServiceQuotation';
@@ -13,8 +11,6 @@ function Details({ data = {} }) {
 	const cargoStuffingLocation =		data.fcl_freight_services[0].cargo_stuffing_location;
 	const shippingLine = data.shipping_line.business_name;
 	const preferredShippingLine = data.preferred_shipping_line?.business_name;
-
-	const [showQuotation, setShowQuotation] = useState(false);
 
 	return (
 		<>
@@ -89,44 +85,13 @@ function Details({ data = {} }) {
 				</div>
 			</div>
 
-			{!showQuotation ? (
-				<Button
-					themeType="terinary"
-					className={styles.service_quotation}
-					onClick={() => {
-						setShowQuotation(!showQuotation);
-					}}
-				>
-					<div className={styles.text}>
-						View Quotation
-						{' '}
-						<IcMArrowRotateDown />
-					</div>
-				</Button>
-			) : null}
-
-			{showQuotation ? (
-				<div>
-					<div className={styles.quotation_details}>
-						<SellServiceQuotation shipmentData={data} />
-						<BuyServiceQuotation shipmentData={data} />
-					</div>
-
-					<Button
-						themeType="terniary"
-						className={styles.service_quotation}
-						onClick={() => {
-							setShowQuotation(!showQuotation);
-						}}
-					>
-						<div className={styles.text}>
-							Hide Quotation
-							{' '}
-							<IcMArrowRotateUp />
-						</div>
-					</Button>
+			<div>
+				<div className={styles.quotation_details}>
+					<SellServiceQuotation shipmentData={data} />
+					<BuyServiceQuotation shipmentData={data} />
 				</div>
-			) : null}
+			</div>
+
 		</>
 	);
 }
