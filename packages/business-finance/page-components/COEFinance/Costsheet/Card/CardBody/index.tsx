70e8aa333cff4b670d5375ddb997222047ffdbcd
sivalaxman7 <sivalaxman.mylapilli@cogoport.com>
2023-01-03 12:52:04 +0530

    fix: cards

--- packages/business-finance/page-components/COEFinance/Costsheet/Card/CardBody/index.tsx
+++ packages/business-finance/page-components/COEFinance/Costsheet/Card/CardBody/index.tsx
@@ -1,26 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './styles.module.css'
 import { startCase } from '@cogoport/utils'
 import { GenericObject } from '../../../../commons/Interfaces'
 import getFormattedPrice from '../../../../commons/utils/getFormattedPrice'
-import { IcMArrowNext } from '@cogoport/icons-react'
+import { IcMArrowNext, IcMArrowDown } from '@cogoport/icons-react'
 
 interface Props{
-  charge:GenericObject
+  charge:GenericObject;
 }
 
 export const CardBody = ({charge}:Props) => {
+  const [showFullDetails, setShowFullDetails] =useState(false);
+
+  const handleWidth=()=>{
+    setShowFullDetails(!showFullDetails)
+  }
 
   const {service_type,line_items=[]}=charge||{}
+  const hidden=line_items.length<2?styles.hidden:''
   const borderColor='#333333'
-  return (
-    <div className={styles.card} style={{ '--bordercolor':borderColor } as React.CSSProperties}>
+  return (charge?(
+    <div className={`${showFullDetails?styles.card:styles.custompadding} ${line_items.length<2?styles.padding:""}`} style={{ '--bordercolor':borderColor} as React.CSSProperties}>
     <div className={styles.layout}>
         <div className={styles.heading} style={{ '--span': 2 } as React.CSSProperties}>{startCase(service_type) || 'Platform Fees'}</div>
         <div className={styles.flex} style={{ '--span': 1 } as React.CSSProperties}>Expected</div>
         <div className={styles.flex} style={{ '--span': 1 } as React.CSSProperties}>Actual</div>
     </div>
-    {line_items.map((lineItem:GenericObject)=>{
+    {(showFullDetails?line_items:line_items.slice(0,2)).map((lineItem:GenericObject)=>{
       const value=(lineItem?.actual_price)-(lineItem?.tax_total_price_discounted || lineItem?.tax_total_price)
       let className=styles.neutral
       let iconClassName=styles.neutralIcon
@@ -38,15 +44,16 @@ export const CardBody = ({charge}:Props) => {
 											lineItem?.tax_total_price,
                       lineItem?.currency || 'INR')}</div>
       <div className={styles.flex} style={{ '--span': 1 } as React.CSSProperties}>{getFormattedPrice(lineItem?.actual_price,
-										lineItem?.actual_price_currency || 'INR',)}<span className={iconClassName}><IcMArrowNext height={20} width={20}/></span></div>
-   </div>
+										lineItem?.actual_price_currency || 'INR',)}<span className={iconClassName}><IcMArrowNext height={15} width={15}/></span></div>
+  </div>
     )
     })}
-    <div className={styles.total}>
+    {(showFullDetails||line_items.length<2)&&<div className={styles.total}>
         <div className={styles.heading} style={{ '--span': 2 } as React.CSSProperties}></div>
         <div className={`${styles.flex} ${styles.totalamount}`} style={{ '--span': 1 } as React.CSSProperties}>INR 70,000</div>
-        <div className={`${styles.flex} ${styles.totalamount}`} style={{ '--span': 1 } as React.CSSProperties}>{getFormattedPrice(charge.tax_total_price||charge.total_price,charge.currency||'INR')}</div>
-    </div>
-    </div>
+        <div className={`${styles.flex} ${styles.totalamount}`} style={{ '--span': 1 } as React.CSSProperties}>{getFormattedPrice(charge?.tax_total_price||charge?.total_price,charge?.currency||'INR')}</div>
+    </div>}
+    <div className={`${styles.viewmore} ${hidden}`} role='presentation' onClick={handleWidth}>{showFullDetails?"View Less":"View More"}<span className={showFullDetails?styles.arrowicon:styles.bottomicon}><IcMArrowDown/></span></div>
+    </div>):null
   )
 }
--- packages/business-finance/page-components/COEFinance/hook/useGetShipmentCostSheet.ts
+++ packages/business-finance/page-components/COEFinance/hook/useGetShipmentCostSheet.ts
@@ -1,23 +1,35 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useRequestBf } from '@cogoport/request';
 import { Toast } from '@cogoport/components';
+import getFormattedData from '../utils/getFormattedData';
 
 interface Params{
     shipment_id?: string|string[]|undefined;
 }
 
 const useGetShipmentCostSheet = ({shipment_id}:Params) => {
-        const [{ data,loading}, trigger] = useRequestBf(
+    const[loadingState,setLoadingState]=useState(true)
+    const[selldata,setSelldata]=useState<any[]>([])
+    const[buydata,setBuydata]=useState<any[]>([])
+    const [{ data}, trigger] = useRequestBf(
             {
                 url     : `/get_shipment_cost_sheet`,
                 method  : 'get',
             },
             { manual: true },
         );
+
+        // useEffect(()=>{
+        //     const {formattedBuyData,sellQuotationData}=getFormattedData(data,setLoadingState )
+        //    },[data])
     
         const getdataFromApi = async () => {
             try {
                 const res = await trigger({ params: { shipment_id } });
+                const {formattedBuyData,sellQuotationData}=getFormattedData(res.data )
+                setSelldata(sellQuotationData);
+                setBuydata(formattedBuyData);
+                setLoadingState(false)
                 if (res.status!==200) {
                     Toast.error('Something went wrong!');
                 }
@@ -30,7 +42,7 @@ const useGetShipmentCostSheet = ({shipment_id}:Params) => {
                 getdataFromApi();
         }, [shipment_id]);
 
-        return { data,loading};
+        return {data, selldata,buydata,apiloading:(loadingState)};
 }
 
 export default useGetShipmentCostSheet
