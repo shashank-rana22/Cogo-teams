6b47f2c9b50a7a8a1fd5f9dd9b8ce5b3918b871c
Rajatuba <rajat.uba@cogoport.com>
2023-01-03 15:24:40 +0530

    removed unwanted

--- packages/shipment-fcl-freight/utils/revenueDeskUtils/getMinRate.js
+++ packages/shipment-fcl-freight/utils/revenueDeskUtils/getMinRate.js
@@ -1,11 +1,17 @@
 const getMinRate = (rates) => {
 	let minimumRate = null;
+
 	let currency = null;
+
 	let is_rate_expired = null;
+
 	if (rates.length) {
 		let min = rates[0]?.price;
+
 		currency = rates[0]?.currency;
+
 		is_rate_expired = rates[0]?.is_rate_expired;
+
 		rates.forEach((rate) => {
 			if (rate?.price < min) {
 				min = rate?.price;
@@ -13,6 +19,7 @@ const getMinRate = (rates) => {
 				is_rate_expired = rate?.is_rate_expired;
 			}
 		});
+
 		minimumRate = min;
 	}
 	return { minimumRate, currency, is_rate_expired };
