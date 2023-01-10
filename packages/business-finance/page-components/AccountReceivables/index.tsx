2be84d3f45ed3f1d0c4b194dac175b7ef60553df
cogomanish <manish.kumar@cogoport.com>
2023-01-10 21:33:57 +0530

    build changes

--- packages/business-finance/page-components/AccountReceivables/index.tsx
+++ packages/business-finance/page-components/AccountReceivables/index.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-
 import { PAYRUN_PAID_CONFIG } from '../account-payables/configurations/PAID_TABLE';
 import List from '../commons/List/index';
 import SegmentedControl from "../commons/SegmentedControl";
@@ -66,7 +65,7 @@ function AccountRecievables() {
 	return (
 		<div>
 			<h1>Account Recievables</h1>
-			<List config={PAYRUN_PAID_CONFIG} sort={sort} setSort={setSort} />
+			{/* <List config={PAYRUN_PAID_CONFIG} sort={sort} setSort={setSort} /> */}
 			<SegmentedControl
 						options={OPTIONS}
 						activeTab={currentTab}
