104d023546d5cfe576a8d7fb54462d0a021854b9
cogomanish <manish.kumar@cogoport.com>
2023-01-11 03:42:00 +0530

    changes

--- packages/business-finance/page-components/COEFinance/All_Invoices/PurchaseInvoiceView/RenderData/RenderRemarks/index.tsx
+++ packages/business-finance/page-components/COEFinance/All_Invoices/PurchaseInvoiceView/RenderData/RenderRemarks/index.tsx
@@ -21,8 +21,8 @@ const RenderRemarks = ({ item }) => {
             return (
                 <div className={styles.timeline_wrapper}>
                     <div className={styles.left_content}>
-                        {formatDate(item?.createdAt, "dd-MMM-yy")}
-                        <div>{formatDate(item?.createdAt, " hh:mm a")}</div>
+                        {formatDate(item?.createdAt, "dd-MMM-yy",{},true)}
+                        <div>{formatDate(item?.createdAt, " hh:mm a",{},true)}</div>
                     </div>
                     <div className={styles.path}>
                         <div className={styles.circle} />
