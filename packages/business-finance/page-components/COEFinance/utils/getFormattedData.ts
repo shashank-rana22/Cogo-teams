ec18e1b89d2750ae9cb0a2a976c94080fbf428f8
cogomanish <manish.kumar@cogoport.com>
2023-01-12 22:38:31 +0530

    changes

--- packages/business-finance/page-components/COEFinance/utils/getFormattedData.ts
+++ packages/business-finance/page-components/COEFinance/utils/getFormattedData.ts
@@ -3,17 +3,17 @@ const getFormattedData = (data) => {
     const buyQuotationData=buy_quotation.serviceCharges?.map((item)=>{
         return {...item,serviceType:item.serviceType||'Platform Fees'}
     })
-    const sellSevices=sell_quotation.serviceCharges?.map((item)=>(item.serviceType||'Platform Fees'))
-    const buySevices=buy_quotation.serviceCharges?.map((item)=>(item.serviceType ||'Platform Fees'))
+    const sellSevices=sell_quotation.serviceCharges?.map((item)=>(item.serviceType||'Platform Fees'))||[]
+    const buySevices=buy_quotation.serviceCharges?.map((item)=>(item.serviceType ||'Platform Fees'))||[]
     const commonServices = sellSevices.filter(value => buySevices?.includes(value));
-    const commonBuyData=buy_quotation.serviceCharges?.filter((item)=>(commonServices?.includes(item?.serviceType)))
-    const remainingBuyData=buy_quotation.serviceCharges?.filter((item)=>(!commonServices?.includes(item?.serviceType)))
-    const remainingSellData=sell_quotation.serviceCharges?.filter((item)=>(!commonServices?.includes(item?.serviceType)))
+    const commonBuyData=buy_quotation.serviceCharges?.filter((item)=>(commonServices?.includes(item?.serviceType)))||[]
+    const remainingBuyData=buy_quotation.serviceCharges?.filter((item)=>(!commonServices?.includes(item?.serviceType)))||[]
+    const remainingSellData=sell_quotation.serviceCharges?.filter((item)=>(!commonServices?.includes(item?.serviceType)))||[]
     const formattedSellData=[...commonBuyData,...remainingBuyData]?.map((items)=>{
         const serviceType=items.serviceType;
-        const filterData=buyQuotationData?.filter((item)=>(item.serviceType==serviceType));
+        const filterData=buyQuotationData?.filter((item)=>(item.serviceType==serviceType))||[];
         return filterData[0];
     })
-    return {formattedBuyData:[...commonBuyData,...remainingBuyData],sellQuotationData:[...formattedSellData,...remainingSellData]}
+    return {formattedBuyData:[...commonBuyData,...remainingBuyData],sellQuotationData:[...formattedSellData,...remainingSellData]}||[]
 }
 export default getFormattedData
