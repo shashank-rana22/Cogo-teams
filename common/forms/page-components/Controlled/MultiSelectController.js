0d6b7bdc26a2d20e57fc14e6837fea31cdc988c0
swapnil-cogoport <swapnil.vaidya@cogoport.com>
2023-01-14 17:39:54 +0530

    fix: fixing things

--- common/forms/page-components/Controlled/MultiSelectController.js
+++ common/forms/page-components/Controlled/MultiSelectController.js
@@ -1,5 +1,5 @@
 // import { MultiSelect } from '@cogoport/components';
-import { Multiselect } from '@cogoport/components';
+import { MultiSelect } from '@cogoport/components';
 import React from 'react';
 import { Controller } from 'react-hook-form';
 
@@ -7,24 +7,20 @@ function MultiSelectController(props) {
 	const {
 		name, control, rules, ...rest
 	} = props;
-	console.log('am i working>>??', { name, rest });
 	return (
 		<Controller
 			control={control}
 			name={name}
 			rules={rules}
-			render={({ field: { onChange, onBlur, value } }) => {
-				console.log('render', { name, value });
-				return (
-					<Multiselect
-						{...rest}
-						name={name}
-						onChange={onChange}
-						value={value}
-						onBlur={onBlur}
-					/>
-				);
-			}}
+			render={({ field: { onChange, onBlur, value } }) => (
+				<MultiSelect
+					{...rest}
+					name={name}
+					onChange={onChange}
+					value={value}
+					onBlur={onBlur}
+				/>
+			)}
 		/>
 	);
 }
