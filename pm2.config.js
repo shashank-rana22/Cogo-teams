422bc033e6b7cd84fdaefbe65ff747db850ba994
Rudraprasad Das <brayn003@gmail.com>
2023-01-14 19:52:35 +0530

    fix: remove next from pm2

--- pm2.config.js
+++ pm2.config.js
@@ -11,7 +11,6 @@ module.exports = {
 	apps: [
 		{
 			name      : 'project-admin',
-			script    : 'cogo-control/node_modules/.bin/next',
 			args      : ['start', '-p', '4073'],
 			instances : ifProd(2, 1),
 			exec_mode : ifProd('cluster', 'fork'),
