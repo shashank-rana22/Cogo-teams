a434ac2f528b39b7632d76fe2f1fc91c5f6a4eac
Shashank Drolia <shashank.drolia@cogoport.com>
2023-01-17 22:04:04 +0530

    fix

--- packages/supply-dashboards/page-components/RfqEnquiries/Layout/Item/index.js
+++ packages/supply-dashboards/page-components/RfqEnquiries/Layout/Item/index.js
@@ -27,14 +27,12 @@ function Item(props) {
 
 	return (
 		<div className={styles.element} style={{ width: `${flex}%`, padding: '4px' }}>
-			{heading && (
-				<div style={{
-					height: '16px', marginBottom: '6px', fontWeight: '600', fontSize: '13px',
-				}}
-				>
-					{heading}
-				</div>
-			)}
+			<div style={{
+				height: '16px', marginBottom: '6px', fontWeight: '600', fontSize: '13px',
+			}}
+			>
+				{heading}
+			</div>
 			<h4 style={{
 				height: '16px', marginBottom: '6px', fontWeight: '400', fontSize: '12px',
 			}}
