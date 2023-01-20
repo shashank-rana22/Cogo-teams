f99d0f51a61fcca5b47d7f5a6866fec9c9851f69
KUNAL BHOSALE <kunal.bhosale@cogoport.com>
2023-01-20 13:20:21 +0530

    changes and merge

--- packages/authentication/hooks/useLoginAuthenticate.js
+++ packages/authentication/hooks/useLoginAuthenticate.js
@@ -61,6 +61,7 @@ const useLoginAuthenticate = () => {
 		if (source !== 'add_account') {
 			getUserSessionMappings();
 		}
+	// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
 	useEffect(() => {
