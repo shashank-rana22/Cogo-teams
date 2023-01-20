7084d1077d1b879f77c6a885f60ce3e3555e7cf7
WorkCOGO <pranjal.somkuwar@cogoport.com>
2023-01-20 13:04:43 +0530

    partner-apis

--- common/navigation-configs/apis/partner-apis.js
+++ common/navigation-configs/apis/partner-apis.js
@@ -4047,6 +4047,11 @@ const partner = {
 			access_type  : 'private',
 			service_name : 'contract',
 		},
+		{
+			api          : 'get_contract_plan_stats',
+			access_type  : 'private',
+			service_name : 'contract',
+		},
 	],
 	discount: [
 		{
