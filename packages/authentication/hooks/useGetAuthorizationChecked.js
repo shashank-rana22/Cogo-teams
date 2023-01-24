772d9f036919984c26dd9969e777b7b146048c50
KUNAL BHOSALE <kunal.bhosale@cogoport.com>
2023-01-24 21:19:48 +0530

    change

--- packages/authentication/hooks/useGetAuthorizationChecked.js
+++ packages/authentication/hooks/useGetAuthorizationChecked.js
@@ -56,7 +56,7 @@ const useGetAuthorizationChecked = () => {
 							// eslint-disable-next-line no-undef
 							window.location.href = `/${profile?.partner?.id}${configs.as || configs.href}`;
 						} else {
-							await push(configs.href, configs.as);
+							await push('/', '/');
 						}
 					}
 				} else if (!isProfilePresent && (!isUnauthenticatedPath || route === '/')) {
