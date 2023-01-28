74e408faeb9585ccab3fc4ca076989ca26fe0d78
Harshitcogo <harshit.soni@cogoport.com>
2023-01-28 16:25:01 +0530

    fixes

---
+++ packages/allocation/common/Heading/index.js
@@ -0,0 +1,12 @@
+import styles from './styles.module.css';
+
+function Heading({ title = '', subTitle = '' }) {
+	return (
+		<div className="heading-container">
+			<h1 className={styles.title}>{title}</h1>
+			<span className={styles.subtitle}>{subTitle}</span>
+		</div>
+	);
+}
+
+export default Heading;
--- packages/allocation/page-components/CoreAllocationEngine/index.js
+++ packages/allocation/page-components/CoreAllocationEngine/index.js
@@ -1,5 +1,50 @@
+import { Tabs, TabPanel } from '@cogoport/components';
+import { useState } from 'react';
+
+import Heading from '../../common/Heading';
+
+import styles from './styles.module.css';
+
 function CoreAllocationEngine() {
-	return 'CoreAllocationEngine';
+	const [activeTab, setActiveTab] = useState('configurations');
+
+	return (
+		<section className={styles.container} id="core_engine_container">
+			<section className={styles.heading_container}>
+				<Heading
+					title="Core Engine"
+				/>
+
+			</section>
+
+			<div style={{ margin: '16px 0px' }}>
+				{/* // Todo make it a mapping  with title,tab Component,key */}
+
+				<Tabs
+					activeTab={activeTab}
+					fullWidth
+					themeType="primary"
+					onChange={setActiveTab}
+				>
+					<TabPanel name="configurations" title="Configurations">
+						<div>Config</div>
+					</TabPanel>
+
+					<TabPanel name="relations" title="Relations">
+						<div>Relations</div>
+					</TabPanel>
+
+					<TabPanel name="requests" title="Requests">
+						<div>Requests</div>
+					</TabPanel>
+
+					<TabPanel name="quotas" title="Quotas">
+						<div>Quotas</div>
+					</TabPanel>
+				</Tabs>
+			</div>
+		</section>
+	);
 }
 
 export default CoreAllocationEngine;
