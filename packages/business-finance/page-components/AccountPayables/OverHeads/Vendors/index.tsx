5b434be0a2651001b6251abed1e70a305671444b
Nagendra Singh <nagendra.singh@cogoport.com>
2023-01-31 16:03:00 +0530

    changes

--- packages/business-finance/page-components/AccountPayables/OverHeads/Vendors/index.tsx
+++ packages/business-finance/page-components/AccountPayables/OverHeads/Vendors/index.tsx
@@ -1,195 +1,208 @@
-import React, {useState, useEffect} from "react";
-import { Select, Button, Input, Tooltip } from "@cogoport/components"
-import { GenericObject } from "../../commons/Interfaces";
-import List from "../../commons/List";
-import VENDOR_CONFIG from "./utils/config";
-import {IcMSearchlight} from '@cogoport/icons-react';
-import Controls from "./utils/controls";
-import styles from "./styles.module.css";
-import dummyData from "./utils/data";
-import CreateVendorModal from "./CreateVendorModal";
-import useListVendors from "./hooks/useListVendors";
-import { IcMFtick,  IcMInfo } from "@cogoport/icons-react"
+import { Select, Button, Input, Tooltip } from '@cogoport/components';
+import { IcMSearchlight, IcMFtick, IcMInfo } from '@cogoport/icons-react';
+import React, { useState, useEffect } from 'react';
+
+import { GenericObject } from '../../commons/Interfaces';
+import List from '../../commons/List';
+
+import CreateVendorModal from './CreateVendorModal';
+import useListVendors from './hooks/useListVendors';
+import styles from './styles.module.css';
+import VENDOR_CONFIG from './utils/config';
+import Controls from './utils/controls';
+import dummyData from './utils/data';
 
 interface itemProps {
-    createdDate:String,
-    venderSerialId: Number, 
-    kycStatus:  String,
-    name: String,
-    pan: String,
-    category: String,
-    payments: Number,
-    openInvoices: Number,
-  }
-interface Props{
-filters:GenericObject;
-setFilters: (p: object) => void;
-subActiveTab:string
+	createdDate:String,
+	venderSerialId: Number,
+	kycStatus: String,
+	name: String,
+	pan: String,
+	category: String,
+	payments: Number,
+	openInvoices: Number,
+}
+interface Props {
+	filters:GenericObject;
+	setFilters: (p: object) => void;
+	subActiveTab:string
 }
 
-function VenderComponent () {
-    const [filters, setFilters] = useState({
-        KYC_STATUS: '', 
-        CATEGORY: '',
-        searchValue: '',
-        pageIndex: 1,
-        pageLimit: 10
-    });
-    const [sort, setSort] = useState({});
-    const [showModal, setShowModal] = useState(false);
-    const [dropdownId, setDropdownId] = useState(null);
-    const {loading = false, data, listApi = () => {}} = useListVendors();
-  
-    const handleChange = (e, value) => {
-        setFilters((previousState) => ({
+function VenderComponent() {
+	const [filters, setFilters] = useState({
+		KYC_STATUS  : '',
+		CATEGORY    : '',
+		searchValue : '',
+		pageIndex   : 1,
+		pageLimit   : 10,
+	});
+	const [sort, setSort] = useState({});
+	const [showModal, setShowModal] = useState(false);
+	const [dropdownId, setDropdownId] = useState(null);
+	const { loading = false, data, listApi = () => {} } = useListVendors();
+
+	const handleChange = (e, value) => {
+		setFilters((previousState) => ({
 			...previousState,
-			...{[value]: e}
-		})); 
-    }
+			...{ [value]: e },
+		}));
+	};
 
-    useEffect(() => {
-        listApi(filters)
-    }, [filters])
+	useEffect(() => {
+		listApi(filters);
+	}, [filters]);
 
-    const renderHeaders = () => {
-        return (
-            <div className ={styles.headerContainer}>
-                <div className={styles.leftContainer} >
-                    {
-                        Object.keys(Controls).map((key)=> {
-                            const  {options = [], placeholder = '', value =''} = Controls[key];
-                            return ( 
-                                <Select 
-                                   value={filters?.[key]} 
-                                   onChange={(e) => handleChange(e, value)} 
-                                   placeholder={placeholder} 
-                                   options={options} 
-                                   className ={styles.select}
-                                   isClearable
-                                   />
-                            )
+	const renderHeaders = () => (
+		<div className={styles.headerContainer}>
+			<div className={styles.leftContainer}>
+				{
+                        Object.keys(Controls).map((key) => {
+                        	const { options = [], placeholder = '', value = '' } = Controls[key];
+                        	return (
+	<Select
+		value={filters?.[key]}
+		onChange={(e) => handleChange(e, value)}
+		placeholder={placeholder}
+		options={options}
+		className={styles.select}
+		isClearable
+	/>
+                        	);
                         })
                     }
 
-                </div>
-                <div className={styles.rightContainer}>
-                    <Button 
-                       size="lg" 
-                       themeType="secondary" 
-                       onClick={ () => setShowModal(true)} >Create Vendor</Button>
-                    <Input 
-                        size="md" 
-                        placeholder="Search by Vendor Name/PAN/Organization ID/Sage ID" 
-                        prefix={<IcMSearchlight/>}
-                        value={filters.searchValue} 
-                        onChange = {(e)=> handleChange(e, 'searchValue')}  
-                        className = {styles.search}
-                    />
-                </div>
-            </div>
-        )
-    }
+			</div>
+			<div className={styles.rightContainer}>
+				<Button
+					size="lg"
+					themeType="secondary"
+					onClick={() => setShowModal(true)}
+				>
+					Create Vendor
+				</Button>
+				<Input
+					size="md"
+					placeholder="Search by Vendor Name/PAN/Organization ID/Sage ID"
+					prefix={<IcMSearchlight />}
+					value={filters.searchValue}
+					onChange={(e) => handleChange(e, 'searchValue')}
+					className={styles.search}
+				/>
+			</div>
+		</div>
+	);
 
-    const RenderKYCStatus = (item) => {
-        const {item: itemData = {}}=  item
-        const {kycStatus = ''} = itemData
-        return (
-            <div style={{display: 'flex', alignItems: 'center'}}>
-                { kycStatus === 'verified' ? <div className={styles.verified}>
-                        <div> <IcMFtick color ='#67C676'/> </div>
-                         <div>&nbsp;Verified </div>
-                          </div>
-                          : <div className={styles.pending}>
-                            <div><IcMInfo color ='#e10d1f' style={{rotate:'180deg', fontSize:'12px'}}/></div> 
-                            <div>&nbsp;Pending</div>
-                            </div>}
-            </div>
-        )
-    }
-    
-    const RenderPayments = (item) => {
-        const {item: itemData = {}}=  item
-        const {payments = ''} = itemData
-        return (
-            <div style={{display: 'flex', alignItems: 'center', gap: '4px'}}>
-                {payments} <Tooltip content="Current Month: " placement="top">
-                    <IcMInfo />
-                </Tooltip>
-            </div>
-        )
-    }
-    
-    const RenderInvoice = ({item}) => {
-        const {item: itemData = {}}=  item;
-        const {openInvoices = ''} = itemData;
-        return (
-            <div style={{display: 'flex', alignItems: 'center', gap: '4px'}}>
-                {openInvoices} <div> (INR 12000) </div>
-            </div>
-        )
-    }
-    
-    const RenderViewMoreButton = ({item}) =>{
-        return (
-            <Button 
-               themeType="secondary" 
-               size="md" 
-               onClick={() => {
-                setDropdownId(item?.vendorSerialId);
-            }}
-               >
-                View More
-            </Button>)
-        }
+	function RenderKYCStatus(item) {
+		const { item: itemData = {} } = item;
+		const { kycStatus = '' } = itemData;
+		return (
+			<div style={{ display: 'flex', alignItems: 'center' }}>
+				{ kycStatus === 'verified' ? (
+					<div className={styles.verified}>
+						<div>
+							{' '}
+							<IcMFtick color="#67C676" />
+							{' '}
+						</div>
+						<div>&nbsp;Verified </div>
+					</div>
+				)
+                	: (
+	<div className={styles.pending}>
+		<div><IcMInfo color="#e10d1f" style={{ rotate: '180deg', fontSize: '12px' }} /></div>
+		<div>&nbsp;Pending</div>
+	</div>
+					)}
+			</div>
+		);
+	}
 
-        const renderDropdown = (id:string|number='')=>{
-             if(id===dropdownId){
-                return <h1>hey there</h1>
-             }
-             return null
-        }
+	function RenderPayments(item) {
+		const { item: itemData = {} } = item;
+		const { payments = '' } = itemData;
+		return (
+			<div style={{ display: 'flex', alignItems: 'center', gap: '4px' }}>
+				{payments}
+				{' '}
+				<Tooltip content="Current Month: " placement="top">
+					<IcMInfo />
+				</Tooltip>
+			</div>
+		);
+	}
 
-    const functions:any = {
-        renderKYCStatus: (itemData:itemProps) => (
-            <RenderKYCStatus item={itemData}/>
-        ),
-        renderPayments: (itemData:itemProps) => (
-            <RenderPayments item={itemData}/>
-        ),
-        renderInvoice: (itemData:itemProps) => (
-            <RenderInvoice item={itemData}/> 
-        ),
-        renderViewMoreButton: (itemData:itemProps) => (
-            <RenderViewMoreButton item={itemData} />
-        )
-    }
+	function RenderInvoice({ item }) {
+		const { item: itemData = {} } = item;
+		const { openInvoices = '' } = itemData;
+		return (
+			<div style={{ display: 'flex', alignItems: 'center', gap: '4px' }}>
+				{openInvoices}
+				{' '}
+				<div> (INR 12000) </div>
+			</div>
+		);
+	}
 
-    return (
-        <div>
-            {renderHeaders()}
+	function RenderViewMoreButton({ item }) {
+		return (
+			<Button
+				themeType="secondary"
+				size="md"
+				onClick={() => {
+               	setDropdownId(item?.vendorSerialId);
+				}}
+			>
+				View More
+			</Button>
+		);
+	}
 
-            <List
-                config={VENDOR_CONFIG}  
-                itemData={dummyData}
-                loading={false}
-                sort={sort}
-                setSort={setSort}
-                functions = {functions}
-                page={filters.pageIndex||1}
-                handlePageChange={(pageValue:number)=>{
-                    setFilters((p) => ({...p, pageIndex: pageValue}))
-                }}
-                showPagination = {true}
-                renderDropdown={(id)=>renderDropdown(id)}
-            />
+	const renderDropdown = (id:string | number = '') => {
+		if (id === dropdownId) {
+			return <h1>hey there</h1>;
+		}
+		return null;
+	};
 
-            {
-                showModal && <CreateVendorModal showModal = {showModal} setShowModal = {setShowModal}/>
+	const functions:any = {
+		renderKYCStatus: (itemData:itemProps) => (
+			<RenderKYCStatus item={itemData} />
+		),
+		renderPayments: (itemData:itemProps) => (
+			<RenderPayments item={itemData} />
+		),
+		renderInvoice: (itemData:itemProps) => (
+			<RenderInvoice item={itemData} />
+		),
+		renderViewMoreButton: (itemData:itemProps) => (
+			<RenderViewMoreButton item={itemData} />
+		),
+	};
+
+	return (
+		<div>
+			{renderHeaders()}
+
+			<List
+				config={VENDOR_CONFIG}
+				itemData={dummyData}
+				loading={false}
+				sort={sort}
+				setSort={setSort}
+				functions={functions}
+				page={filters.pageIndex || 1}
+				handlePageChange={(pageValue:number) => {
+                	setFilters((p) => ({ ...p, pageIndex: pageValue }));
+				}}
+				showPagination
+				renderDropdown={(id) => renderDropdown(id)}
+			/>
+
+			{
+                showModal && <CreateVendorModal showModal={showModal} setShowModal={setShowModal} />
             }
-        </div>
-    )
+		</div>
+	);
 }
 
-export default VenderComponent
-
-
+export default VenderComponent;
--- packages/business-finance/page-components/AccountPayables/OverHeads/index.tsx
+++ packages/business-finance/page-components/AccountPayables/OverHeads/index.tsx
@@ -1,74 +1,76 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
+
+import Expenses from './Expenses/index';
 import styles from './styles.module.css';
 import Vendors from './Vendors/index';
-import Expenses from './Expenses/index';
 
-const Overheads = () =>{
-    const [activeTab, setActiveTab] = useState('vendors');
-    return (
-      <div>
-        <div className={styles.container}>
-          <div className={styles.flex}>
-            <div
-              onClick={() => {
-                setActiveTab("vendors");
-              }}
-            >
-              <div 
-                className={
-                  activeTab==='vendors' ? styles.sub_container_click : styles.sub_container
+function Overheads() {
+	const [activeTab, setActiveTab] = useState('vendors');
+	return (
+		<div>
+			<div className={styles.container}>
+				<div className={styles.flex}>
+					<div
+						onClick={() => {
+              	setActiveTab('vendors');
+						}}
+					>
+						<div
+							className={
+                  activeTab === 'vendors' ? styles.sub_container_click : styles.sub_container
                 }
-              >
-               VENDORS
-              </div>
-            </div>
+						>
+							VENDORS
+						</div>
+					</div>
 
-            <div
-              onClick={() => {
-                setActiveTab("expenses");
-              }}
-            >
-              <div
-                className={
-                    activeTab==='expenses' ? styles.sub_container_click : styles.sub_container
+					<div
+						onClick={() => {
+              	setActiveTab('expenses');
+						}}
+					>
+						<div
+							className={
+                    activeTab === 'expenses' ? styles.sub_container_click : styles.sub_container
                 }
-              >
-              EXPENSES
-              </div>
-              </div>
+						>
+							EXPENSES
+						</div>
+					</div>
 
-              <div
-                onClick={() => {
-                    setActiveTab("reports");
-                }}
-                >
-              <div
-                className={
-                    activeTab==='reports' ? styles.sub_container_click : styles.sub_container
+					<div
+						onClick={() => {
+                	setActiveTab('reports');
+						}}
+					>
+						<div
+							className={
+                    activeTab === 'reports' ? styles.sub_container_click : styles.sub_container
                 }
-              >
-              REPORTS
-              </div>
-              </div>
+						>
+							REPORTS
+						</div>
+					</div>
 
-            </div>
-          </div>
+				</div>
+			</div>
 
+			<div className={styles.section}>
+				{activeTab === 'vendors' && (
+					<div>
+						<Vendors />
+					</div>
+				)}
 
-          <div className={styles.section}>
-           {activeTab==='vendors' && <div>
-                  <Vendors/>
-              </div>}
+				{activeTab === 'expenses' && (
+					<div>
+						<Expenses />
+					</div>
+				)}
+			</div>
 
-            {activeTab==='expenses' && <div>
-                 <Expenses/>
-              </div>}
-          </div>
-
-
-        </div>
-    )
+		</div>
+	);
 }
-
 
 export default Overheads;
