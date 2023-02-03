186babb54cf81ba835b8123cf189c8a5e09b179b
cogomanish <manish.kumar@cogoport.com>
2023-02-03 11:48:26 +0530

    PR changes

--- packages/business-finance/page-components/COEFinance/All_Invoices/PurchaseInvoiceView/RenderData/RenderUrgencyTag/index.tsx
+++ packages/business-finance/page-components/COEFinance/All_Invoices/PurchaseInvoiceView/RenderData/RenderUrgencyTag/index.tsx
@@ -1,4 +1,7 @@
+import { Tooltip } from '@cogoport/components';
 import React from 'react';
+
+import showOverflowingNumber from '../../../../../commons/showOverflowingNumber';
 
 import styled from './styles.module.css';
 
@@ -12,11 +15,19 @@ function RenderUrgencyTag({ item }:Props) {
 	return (
 		<div>
 			{item?.urgencyTag ? (
-				<div className={styled.urgency}>
-					{' '}
-					{item?.urgencyTag[0]}
-					{' '}
-				</div>
+				<Tooltip
+					placement="top"
+					content={(
+						<div className={styled.urgency}>
+							{item?.urgencyTag[0]}
+						</div>
+					)}
+				>
+					<div className={styled.urgency}>
+						{showOverflowingNumber(item?.urgencyTag[0], 8)}
+					</div>
+				</Tooltip>
+
 			) : '-'}
 		</div>
 	);
