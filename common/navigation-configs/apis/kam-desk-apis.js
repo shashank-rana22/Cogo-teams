4f3f641d21a051e55d2ebef1b76c6b69ee5eb51f
SahilMazumder-Cogoport <sahil.mazumder@cogoport.com>
2023-02-07 15:29:38 +0530

    changes

--- common/navigation-configs/apis/kam-desk-apis.js
+++ common/navigation-configs/apis/kam-desk-apis.js
@@ -4,6 +4,191 @@ const apis = [
 		access_type  : 'private',
 		service_name : 'shipment',
 	},
+	{
+		api          : 'list_shipment_services',
+		access_type  : 'private',
+		service_name : 'shipment',
+	},
+	{
+		api          : 'list_shipment_pending_tasks',
+		access_type  : 'private',
+		service_name : 'shipment',
+	},
+	{
+		api           : 'get_shipment_invoice_preference',
+		access_type   : 'private',
+		project_scope : ['partner'],
+		service_name  : 'shipment',
+	},
+	{
+		api          : 'update_shipment',
+		access_type  : 'private',
+		service_name : 'shipment',
+	},
+	{
+		api          : 'get_shipment',
+		access_type  : 'private',
+		service_name : 'shipment',
+	},
+	{
+		api          : 'get_shipment_timeline',
+		access_type  : 'private',
+		service_name : 'shipment',
+	},
+	{
+		api          : 'create_shipment_document',
+		access_type  : 'private',
+		service_name : 'shipment',
+	},
+	{
+		api          : 'update_shipment_additional_service',
+		access_type  : 'private',
+		service_name : 'shipment',
+	},
+	{
+		api          : 'update_shipment_container_details',
+		access_type  : 'private',
+		service_name : 'shipment',
+	},
+	{
+		api          : 'create_shipment_trade_document',
+		access_type  : 'private',
+		service_name : 'trade',
+	},
+	{
+		api          : 'list_shipment_trade_documents',
+		access_type  : 'private',
+		service_name : 'trade',
+	},
+	{
+		api          : 'update_trade_document',
+		access_type  : 'private',
+		service_name : 'trade',
+	},
+	{
+		api          : 'update_shipment_stakeholders',
+		access_type  : 'private',
+		service_name : 'shipment',
+	},
+	{
+		api          : 'list_shipment_bl_details',
+		access_type  : 'private',
+		service_name : 'shipment',
+	},
+	{
+		api          : 'list_sage_invoices_v2',
+		access_type  : 'private',
+		service_name : 'sage',
+	},
+	{
+		api           : 'generate_freight_certificate',
+		access_type   : 'private',
+		project_scope : ['partner'],
+		service_name  : 'shipment',
+	},
+	{
+		api          : 'update_shipment_sell_quotations',
+		access_type  : 'private',
+		service_name : 'shipment',
+	},
+	{
+		api          : 'get_cost_sheet_quotations',
+		access_type  : 'private',
+		service_name : 'shipment',
+	},
+	{
+		api          : 'get_shipment_services_quotation',
+		access_type  : 'private',
+		service_name : 'shipment',
+	},
+	{
+		api          : 'reallocate_shipment_service_provider',
+		access_type  : 'private',
+		service_name : 'shipment',
+	},
+	{
+		api          : 'reallocate_shipment_dependent_service_provider',
+		access_type  : 'private',
+		service_name : 'shipment',
+	},
+	{
+		api          : 'list_organization_billing_addresses',
+		access_type  : 'private',
+		service_name : 'organization',
+	},
+	{
+		api          : 'list_cogo_entities',
+		access_type  : 'private',
+		service_name : 'partner',
+	},
+	{
+		api         : 'create_chat_message',
+		access_type : 'private',
+	},
+	{
+		api         : 'list_chat_channels',
+		access_type : 'private',
+	},
+	{
+		api         : 'delete_chat_message',
+		access_type : 'private',
+	},
+	{
+		api         : 'update_chat_message',
+		access_type : 'private',
+	},
+	{
+		api         : 'get_chat_channel',
+		access_type : 'private',
+	},
+	{
+		api         : 'create_chat_channel',
+		access_type : 'private',
+	},
+	{
+		api         : 'update_chat_channel_seen',
+		access_type : 'private',
+	},
+	{
+		api          : 'list_shipment_credit_notes',
+		access_type  : 'private',
+		service_name : 'shipment',
+	},
+	{
+		api          : 'update_shipment_credit_note',
+		access_type  : 'private',
+		service_name : 'shipment',
+	},
+	{
+		api          : 'create_shipment_credit_note',
+		access_type  : 'private',
+		service_name : 'shipment',
+	},
+	{
+		api          : 'get_shipment_credit_note',
+		access_type  : 'private',
+		service_name : 'shipment',
+	},
+	{
+		api          : 'update_shipment_invoice_currency',
+		access_type  : 'private',
+		service_name : 'shipment',
+	},
+	{
+		api          : 'update_shipment_invoice_status',
+		access_type  : 'private',
+		service_name : 'shipment',
+	},
+	{
+		api          : 'create_shipment_revoked_invoice',
+		access_type  : 'private',
+		service_name : 'shipment',
+	},
+	{
+		api          : 'bulk_update_shipment_services',
+		access_type  : 'private',
+		service_name : 'shipment',
+	},
 ];
 
 export default apis;
