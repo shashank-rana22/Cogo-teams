363c2c4e531a0fe3223f8157c0bd33c11cb7f58b
SahilMazumder-Cogoport <sahil.mazumder@cogoport.com>
2023-02-07 12:19:24 +0530

    changes

---
+++ common/navigation-configs/apis/document-desk-apis.js
@@ -0,0 +1,8 @@
+const apis = [
+	{
+		api          : 'list_document_desk_shipments',
+		access_type  : 'private',
+		service_name : 'shipment',
+	},
+];
+export default apis;
---
+++ common/navigation-configs/apis/kam-desk-apis.js
@@ -0,0 +1,9 @@
+const apis = [
+	{
+		api          : 'list_kam_desk_shipments',
+		access_type  : 'private',
+		service_name : 'shipment',
+	},
+];
+
+export default apis;
