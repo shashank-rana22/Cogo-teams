e8fbf5536b6b2d8a15cc68086a010be3772367ed
ShivaniBairoju <Shivani.Bairoju@cogoport.com>
2023-02-09 17:41:33 +0530

    api permission

--- common/navigation-configs/apis/saas.js
+++ common/navigation-configs/apis/saas.js
@@ -1807,6 +1807,12 @@ const saas = {
 			module      : 'trader-eligibility-check',
 			feature     : 'trader-eligibility-check',
 		},
+		{
+			api         : 'get_saas_bill_product_codes',
+			access_type : 'private',
+			module      : 'trader-eligibility-check',
+			feature     : 'trader-eligibility-check',
+		},
 	],
 	app_saas_order_history: [
 		{
