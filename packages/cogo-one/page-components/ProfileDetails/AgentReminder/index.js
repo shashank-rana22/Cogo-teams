046632fd202ef889923eb58beefeda9b65c227a6
Bishal <Bishal.Saha@cogoport.com>
2023-02-14 12:35:44 +0530

    activities

--- packages/cogo-one/page-components/ProfileDetails/AgentReminder/index.js
+++ packages/cogo-one/page-components/ProfileDetails/AgentReminder/index.js
@@ -1,9 +1,48 @@
+import { InputGroup, Timepicker, Input, SingleDateRange } from '@cogoport/components';
+// import { IcMArrowRotateDown } from '@cogoport/icons-react';
+import { useState } from 'react';
+
+import styles from './styles.module.css';
+
 function AgentReminder() {
+	const [inputValue, setInputValue] = useState('');
+	const [time, setTime] = useState({
+		start_time : '',
+		end_time   : '',
+	});
 	return (
-		<>
-			<h4>djfhdf</h4>
-			<h5>kdjffdjhjfdk</h5>
-		</>
+		<div className={styles.container}>
+			<div className={styles.title}>Set Reminder</div>
+			<div className={styles.wrapper}>
+				<div className={styles.label}>Title</div>
+				<Input
+					size="md"
+					placeholder="Type here..."
+					required
+					value={inputValue}
+					onChange={(e) => setInputValue(e.target?.value)}
+				/>
+			</div>
+			<div className={styles.date_wrapper}>
+				<div className={styles.label}>Select a date</div>
+				<SingleDateRange
+					placeholder="Enter Date"
+					dateFormat="MM/dd/yyyy"
+					name="date"
+					// onChange={setDate}
+					// value={date}
+				/>
+				<InputGroup>
+					<div className={styles.label}>Start Time</div>
+					<Timepicker
+						name="date"
+						timeIntervals={1}
+						value={time?.start_time}
+						onChange={(a) => setTime((p) => ({ ...p, start_time: a }))}
+					/>
+				</InputGroup>
+			</div>
+		</div>
 	);
 }
 export default AgentReminder;
