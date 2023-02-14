03564269337cde3fb4c3b3fc78e477f75fecf6cd
shatrunjaysrivastava <shatrunjay.srivastava@cogoport.com>
2023-02-14 19:30:31 +0530

    first-changes

---
+++ packages/cogo-verse-analytics/page-components/AnalyticsDashboard/Stats/dummy.js
@@ -0,0 +1,17 @@
+export const dummy = [
+    {
+        src: "https://cdn.cogoport.io/cms-prod/cogo_admin/vault/original/happy_user.svg",
+        name: "Happy users",
+        number:20
+    },
+    {
+        src: "https://cdn.cogoport.io/cms-prod/cogo_admin/vault/original/neutral_user.svg",
+        name: "Neutral users",
+        number:18
+    },
+    {
+        src: "https://cdn.cogoport.io/cms-prod/cogo_admin/vault/original/angry_user.svg",
+        name: "Angry users",
+        number:23
+    },
+];
