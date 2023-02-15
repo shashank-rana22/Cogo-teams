931a9e73568bc979f6cf93cbbebb6115dd83e5cf
MahakCogoport <mahak.bharatkar@cogoport.com>
2023-02-15 17:50:07 +0530

    spaces

--- packages/allocation/index.js
+++ packages/allocation/index.js
@@ -1,9 +1,1 @@
 export { default as CoreAllocationEngine } from './page-components/CoreAllocationEngine';
-
-// export { default as AllocationQuotas } from './components/AllocationQuotas';
-
-// export { default as AllocationRelations } from './components/AllocationRelations';
-
-// export { default as AllocationRequests } from './components/AllocationRequests';
-
-// export { default as AllocationDetails } from './components/AllocationDetails';
