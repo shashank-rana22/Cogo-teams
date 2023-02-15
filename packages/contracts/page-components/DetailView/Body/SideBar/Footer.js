3e5b3ca7727874c31926a7b6d5508ff5cfb6945b
Abhi Barbiyal <abhi.barbiyal@cogoport.com>
2023-02-15 21:52:26 +0530

    changes

--- packages/contracts/page-components/DetailView/Body/SideBar/Footer.js
+++ packages/contracts/page-components/DetailView/Body/SideBar/Footer.js
@@ -28,15 +28,15 @@ function Footer({ statsData, index, portPair }) {
 				<div className={styles.label}>Rate</div>
 				<div className={styles.value}>
 					{statsData?.port_pairs_data?.[index]?.price
-						? `${statsData?.port_pairs_data?.[index]?.price.toFixed(2)}/ 
-						${serviceToUnit[portPair?.service_type]}` : '-'}
+						? `$ ${statsData?.port_pairs_data?.[index]?.price.toFixed(2)}/ 
+						${serviceToUnit[portPair?.service_type]} ` : '-'}
 				</div>
 			</div>
 			<div className={styles.sub_container}>
 				<div className={styles.label}>Profitability</div>
 				<div className={styles.value}>
 					{statsData?.port_pairs_data?.[index]?.profitability
-						? statsData?.port_pairs_data?.[index]?.profitability.toFixed(2) : '-'}
+						? `${statsData?.port_pairs_data?.[index]?.profitability.toFixed(2)} %` : '-'}
 				</div>
 			</div>
 		</div>
