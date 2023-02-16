18a87d739768013494541acde443eefbef2122a4
Akshay Sawant <akshay.sawant@cogoport.com>
2023-02-16 13:32:52 +0530

    unified nav setuo

--- common/navigation-configs/route-config.js
+++ common/navigation-configs/route-config.js
@@ -2,12 +2,14 @@ import businessFinance from './config/business-finance';
 import contracts from './config/contracts';
 import rolesAndPermissions from './config/roles-n-permission';
 import supplyDashboard from './config/supply-dashboards';
+import unifiedDashboard from './config/unified-dashboard';
 
 const routeConfig = {
 	...rolesAndPermissions,
 	...supplyDashboard,
 	...contracts,
 	...businessFinance,
+	...unifiedDashboard,
 };
 
 export default routeConfig;
---
+++ packages/unified-dashboard/page-components/index.js
@@ -0,0 +1,10 @@
+/* eslint-disable import/no-unresolved */
+import React from 'react';
+
+function UnifiedDashboard() {
+	return (
+		<div>UnifiedDashboard</div>
+	);
+}
+
+export default UnifiedDashboard;
