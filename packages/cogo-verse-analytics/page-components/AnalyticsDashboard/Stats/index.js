5e76d6620baa95e066aeadb999e2d10858c9f2ac
SDineshKumarCogoport <dineshkumar.s@cogoport.com>
2023-02-16 12:09:10 +0530

    primary stat

--- packages/cogo-verse-analytics/page-components/AnalyticsDashboard/Stats/index.js
+++ packages/cogo-verse-analytics/page-components/AnalyticsDashboard/Stats/index.js
@@ -11,6 +11,11 @@ function Stats() {
 
 	return (
 		<div className={styles.main_container}>
+			<div className={styles.cogoverse_header}>
+				Welcome to the
+				{' '}
+				<span>CogoVerse!</span>
+			</div>
 
 			<div className={styles.primary_stats}>
 
@@ -41,15 +46,48 @@ function Stats() {
 							</div>
 						);
 					})}
-
 				</div>
 
 				<div className={styles.primary_right}>
 					<div className={styles.active_users}>
-						ki
+						<div className={styles.right_stat_title}>Users Active on</div>
+						<div className={styles.right_stat_content}>
+							<div className={styles.right_stat_label}>
+								CogoVerse AI
+							</div>
+							<div className={styles.right_stat_value}>
+								2K
+							</div>
+						</div>
+						<div className={styles.right_stat_content}>
+							<div className={styles.right_stat_label}>
+								Customer Support
+							</div>
+							<div className={styles.right_stat_value}>
+								20K
+							</div>
+						</div>
 					</div>
-					<div className={styles.ticket_details}>
-						ki
+					<div className={styles.ticket_container}>
+						<div className={styles.ticket_details}>
+
+							<div className={styles.ticket_value}>
+								20
+							</div>
+							<div className={styles.ticket_label}>
+								Tickets Raised
+							</div>
+						</div>
+						<div className={styles.vertical_line} />
+						<div className={styles.ticket_details}>
+
+							<div className={styles.ticket_value}>
+								15
+							</div>
+							<div className={styles.ticket_label}>
+								Tickets Resolved
+							</div>
+						</div>
 					</div>
 				</div>
 
