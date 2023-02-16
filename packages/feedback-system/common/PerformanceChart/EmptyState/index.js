94f792518592e3c7260c4923828eb5a3dc2f10da
AmaanAhmadCogo <amaan.ahmad@cogoport.com>
2023-02-16 20:25:01 +0530

    minute addition

--- packages/feedback-system/common/PerformanceChart/EmptyState/index.js
+++ packages/feedback-system/common/PerformanceChart/EmptyState/index.js
@@ -1,15 +1,3 @@
-// import styles from './styles.module.css';
-
-// function EmptyState() {
-// 	return (
-// 		<div className={styles.empty_state_container}>
-// 			<h4>Performance Stats Not Found</h4>
-// 		</div>
-// 	);
-// }
-
-// export default EmptyState;
-
 import styles from './styles.module.css';
 
 function EmptyState({
