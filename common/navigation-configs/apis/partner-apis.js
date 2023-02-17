06e728ccbb8726f2fbd694d1268435c1e63021e8
amandeep2508 <amandeep.chauhan@cogoport.com>
2023-02-18 04:29:35 +0530

    added spot search event 1

--- common/navigation-configs/apis/partner-apis.js
+++ common/navigation-configs/apis/partner-apis.js
@@ -881,6 +881,11 @@ const partner = {
 	],
 	demand_crm: [
 		{
+			api          : 'create_event',
+			access_type  : 'private',
+			service_name : 'event',
+		},
+		{
 			api          : 'get_tax_numbers_data',
 			access_type  : 'private',
 			service_name : 'credit',
@@ -1650,6 +1655,11 @@ const partner = {
 			api          : 'verify_otp',
 			access_type  : 'private',
 			service_name : 'user',
+		},
+		{
+			api          : 'create_event',
+			access_type  : 'private',
+			service_name : 'event',
 		},
 	],
 	supply_crm: [
@@ -9775,6 +9785,7 @@ const partner = {
 			access_type  : 'private',
 			service_name : 'communication',
 		},
+
 	],
 	omni_channel: [
 		{
