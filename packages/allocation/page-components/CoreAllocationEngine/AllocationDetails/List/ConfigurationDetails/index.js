3b13f668caec70e0947a667b25c31ddef74d9608
Harshitcogo <harshit.soni@cogoport.com>
2023-02-17 19:23:04 +0530

    final fixes

--- packages/allocation/page-components/CoreAllocationEngine/AllocationDetails/List/ConfigurationDetails/index.js
+++ packages/allocation/page-components/CoreAllocationEngine/AllocationDetails/List/ConfigurationDetails/index.js
@@ -1,117 +1,117 @@
 import { Pill, Tooltip } from '@cogoport/components';
-import { format, isEmpty, startCase, getByKey } from '@cogoport/utils';
+import { format, isEmpty, startCase } from '@cogoport/utils';
 
 import styles from './styles.module.css';
 
-const CONFIGURATION_DETAILS_MAPPING = [
-	{
-		key      : 'schedule_type',
-		label    : 'Schedule Type',
-		getValue : (data) => startCase(getByKey(data, 'schedule_type', '___')),
-		// flex   : '1 1 20%'
-	},
-	{
-		key      : 'days',
-		label    : 'Repeat On (day)',
-		getValue : (data) => getByKey(data, 'days', '___'),
-	},
-	{
-		key      : 'stakeholder_type',
-		label    : 'Stakeholder Type',
-		getValue : (data) => startCase(getByKey(data, 'stakeholder_type', '___')),
-	},
-	{
-		key      : 'roles',
-		label    : 'Roles',
-		getValue : (data) => {
-			const { role } = data;
+// const CONFIGURATION_DETAILS_MAPPING = [
+// 	{
+// 		key      : 'schedule_type',
+// 		label    : 'Schedule Type',
+// 		getValue : (data) => startCase(getByKey(data, 'schedule_type', '___')),
+// 		// flex   : '1 1 20%'
+// 	},
+// 	{
+// 		key      : 'days',
+// 		label    : 'Repeat On (day)',
+// 		getValue : (data) => getByKey(data, 'days', '___'),
+// 	},
+// 	{
+// 		key      : 'stakeholder_type',
+// 		label    : 'Stakeholder Type',
+// 		getValue : (data) => startCase(getByKey(data, 'stakeholder_type', '___')),
+// 	},
+// 	{
+// 		key      : 'roles',
+// 		label    : 'Roles',
+// 		getValue : (data) => {
+// 			const { role } = data;
 
-			return (
-				<div>
-					{!isEmpty(role) ? startCase(role[0]?.name) : '___'}
-					<Tooltip
-						placement="bottom"
-						content={(
-							<div>
-								{role.map((item) => (
-									<Pill size="md" color="orange">
-										{startCase(item.name)}
-									</Pill>
-								))}
-							</div>
-						)}
-					>
-						{role?.length > 1 && (
-							<strong>
-								+
-								{role.length - 1}
-								more
-							</strong>
-						)}
-					</Tooltip>
-				</div>
-			);
-		},
-	},
-	{
-		key      : 'segment_type',
-		label    : 'Segment',
-		getValue : (data) => startCase(data, 'segment_type', '___'),
-	},
-	{
-		key      : 'users',
-		label    : 'Users',
-		getValue : (data) => {
-			const { user } = data;
+// 			return (
+// 				<div>
+// 					{!isEmpty(role) ? startCase(role[0]?.name) : '___'}
+// 					<Tooltip
+// 						placement="bottom"
+// 						content={(
+// 							<div>
+// 								{role.map((item) => (
+// 									<Pill size="md" color="orange">
+// 										{startCase(item.name)}
+// 									</Pill>
+// 								))}
+// 							</div>
+// 						)}
+// 					>
+// 						{role?.length > 1 && (
+// 							<strong>
+// 								+
+// 								{role.length - 1}
+// 								more
+// 							</strong>
+// 						)}
+// 					</Tooltip>
+// 				</div>
+// 			);
+// 		},
+// 	},
+// 	{
+// 		key      : 'segment_type',
+// 		label    : 'Segment',
+// 		getValue : (data) => startCase(data, 'segment_type', '___'),
+// 	},
+// 	{
+// 		key      : 'users',
+// 		label    : 'Users',
+// 		getValue : (data) => {
+// 			const { user } = data;
 
-			return (
-				<div>
-					{!isEmpty(user) ? startCase(user[0]?.name) : '___'}
-					<Tooltip
-						placement="bottom"
-						content={(
-							<div>
-								{user.map((item) => (
-									<Pill size="md" color="orange">
-										{startCase(item.name)}
-									</Pill>
-								))}
-							</div>
-						)}
-					>
-						{user?.length > 1 && (
-							<strong>
-								+
-								{user.length - 1}
-								more
-							</strong>
-						)}
-					</Tooltip>
-				</div>
-			);
-		},
-	},
-	{
-		key      : 'locking_criterion',
-		label    : 'Locking Criterion',
-		getValue : (data) => startCase(getByKey(data, 'locking_criterion', '___')),
-	},
-	{
-		key      : 'expiry_date',
-		label    : 'Expire Date',
-		getValue : (data) => (data.end_date ? format(data.end_date, 'dd MMM yyyy') : '___'),
-	},
-	{
-		key      : 'locking_period',
-		label    : 'Locking Period (Days)',
-		getValue : (data) => getByKey(data, 'locking_period', '___'),
-	},
-	{
-		key      : 'cooling_period',
-		label    : 'Cooling Period (Days)',
-		getValue : (data) => getByKey(data, 'cooling_period', '___'),
-	},
-];
+// 			return (
+// 				<div>
+// 					{!isEmpty(user) ? startCase(user[0]?.name) : '___'}
+// 					<Tooltip
+// 						placement="bottom"
+// 						content={(
+// 							<div>
+// 								{user.map((item) => (
+// 									<Pill size="md" color="orange">
+// 										{startCase(item.name)}
+// 									</Pill>
+// 								))}
+// 							</div>
+// 						)}
+// 					>
+// 						{user?.length > 1 && (
+// 							<strong>
+// 								+
+// 								{user.length - 1}
+// 								more
+// 							</strong>
+// 						)}
+// 					</Tooltip>
+// 				</div>
+// 			);
+// 		},
+// 	},
+// 	{
+// 		key      : 'locking_criterion',
+// 		label    : 'Locking Criterion',
+// 		getValue : (data) => startCase(getByKey(data, 'locking_criterion', '___')),
+// 	},
+// 	{
+// 		key      : 'expiry_date',
+// 		label    : 'Expire Date',
+// 		getValue : (data) => (data.end_date ? format(data.end_date, 'dd MMM yyyy') : '___'),
+// 	},
+// 	{
+// 		key      : 'locking_period',
+// 		label    : 'Locking Period (Days)',
+// 		getValue : (data) => getByKey(data, 'locking_period', '___'),
+// 	},
+// 	{
+// 		key      : 'cooling_period',
+// 		label    : 'Cooling Period (Days)',
+// 		getValue : (data) => getByKey(data, 'cooling_period', '___'),
+// 	},
+// ];
 
 function ConfigurationDetails({ configurationDetails = {} }) {
 	return (
