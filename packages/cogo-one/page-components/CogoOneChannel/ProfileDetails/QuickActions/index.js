da8aa1ea7608c464a62255f0db01d09ed0e8beed
Sandeep Nalabolu <sandeep.nalabolu@cogoport.com>
2023-02-19 21:30:07 +0530

    fixes

--- packages/cogo-one/page-components/CogoOneChannel/ProfileDetails/QuickActions/index.js
+++ packages/cogo-one/page-components/CogoOneChannel/ProfileDetails/QuickActions/index.js
@@ -1,15 +1,23 @@
 import { IcAWhitePapers } from '@cogoport/icons-react';
+import { useSelector } from '@cogoport/store';
 
 import ACTION_ICON_MAPPING from '../../../../constants/ACTION_ICON_MAPPING';
 
 import styles from './styles.module.css';
 
 function QuickActions() {
+	const partnerId = useSelector((s) => s?.profile?.partner?.id);
+
 	return (
 		<div className={styles.container}>
 			<div className={styles.title}>Recently used Quick actions</div>
 			<div className={styles.header}>
-				<div className={styles.recent_icon}>
+				<div
+					className={styles.recent_icon}
+					role="presentation"
+					// eslint-disable-next-line no-undef
+					onClick={() => { window.location.href = `/${partnerId}/invoice-approvals`; }}
+				>
 					<IcAWhitePapers width={50} height={50} />
 					<div className={styles.name}>Invoicing</div>
 				</div>
@@ -18,7 +26,13 @@ function QuickActions() {
 				<div className={styles.title}>Quick Actions</div>
 				<div className={styles.row}>
 					{(ACTION_ICON_MAPPING || []).map((item) => (
-						<div className={styles.recent_icon}>
+						<div
+							key={item.name}
+							className={styles.recent_icon}
+							role="presentation"
+							// eslint-disable-next-line no-undef
+							onClick={() => { window.location.href = `/${partnerId}/${item?.href}`; }}
+						>
 							<div className={styles.icon}>{item?.icon}</div>
 							<div className={styles.name}>{item?.title}</div>
 						</div>
