0033f0588ac34c26478e5b81c2c7f1f9909c37b8
Sandeep Nalabolu <sandeep.nalabolu@cogoport.com>
2023-02-20 01:54:49 +0530

    fix message view

--- packages/cogo-one/page-components/CogoOneChannel/ProfileDetails/RightSideNav/index.js
+++ packages/cogo-one/page-components/CogoOneChannel/ProfileDetails/RightSideNav/index.js
@@ -1,5 +1,5 @@
 import { cl } from '@cogoport/components';
-import { useRouter } from '@cogoport/next';
+import { useSelector } from '@cogoport/store';
 import { snakeCase } from '@cogoport/utils';
 
 import IconMapping from './IconMapping';
@@ -11,20 +11,17 @@ function RightSideNav({
 	activeMessageCard,
 	activeVoiceCard,
 }) {
+	const partnerId = useSelector((s) => s?.profile?.partner?.id);
 	const { organization_id } = activeVoiceCard || {};
 
 	const { organization_id: org_id } = activeMessageCard || {};
 
 	const ORG_ID = organization_id || org_id || '272a2072-7009-4df9-b852-185bfa49a541';
 
-	const { push } = useRouter();
-
 	const handleClick = (val) => {
 		if (val === 'spot_search') {
-			push(
-				'/details/demand/[organization_id]?source=communication',
-				`/details/demand/${ORG_ID}?source=communication`,
-			);
+			// eslint-disable-next-line no-undef
+			window.location.href = `/${partnerId}/details/demand/${ORG_ID}?source=communication`;
 		} else {
 			setActiveSelect(val);
 		}
