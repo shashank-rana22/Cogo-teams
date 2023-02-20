05e94c3f301cd534bdb3f972102dc054463fa27f
jatinChoubisaCp <jatin.choubisa@cogoport.com>
2023-02-20 11:38:33 +0530

    admin dashboard work

--- packages/cogo-one/page-components/CogoOneDashboard/constants/index.js
+++ packages/cogo-one/page-components/CogoOneDashboard/constants/index.js
@@ -14,3 +14,4 @@ export const platformChatIcon = 'https://cdn.cogoport.io/cms-prod/cogo_admin/vau
 export const missedcallIcon = 'https://cdn.cogoport.io/cms-prod/cogo_admin/vault/original/misscall.svg';
 export const incomingcallIcon = 'https://cdn.cogoport.io/cms-prod/cogo_admin/vault/original/incomingcall.svg';
 export const outgoingcallIcon = 'https://cdn.cogoport.io/cms-prod/cogo_admin/vault/original/outgoingcall.svg';
+export const agentAvatar = 'https://cdn.cogoport.io/cms-prod/cogo_admin/vault/original/userAvatar.svg';
--- packages/cogo-one/page-components/CogoOneDashboard/index.js
+++ packages/cogo-one/page-components/CogoOneDashboard/index.js
@@ -1,11 +1,15 @@
+import { useSelector } from '@cogoport/store';
 import { useState } from 'react';
 
-import Calender from './Calender';
+// import AgentActivity from './AgentActivity';
+// import Calender from './Calender';
 import ChatStatistics from './ChatStatistics';
 import Header from './Header';
 import Intelligence from './Intelligence';
 import IntentServed from './IntentServed';
 import LineChart from './LineChart';
+// import PerformanceTab from './PerformanceTabs';
+// import RedFlags from './RedFlags';
 import Statisfaction from './Statisfaction';
 import Statistics from './Statistics';
 import styles from './styles.module.css';
@@ -13,14 +17,16 @@ import TimeSpent from './TimeSpent';
 
 function CogoOneDashboard() {
 	const [activeTab, setActiveTab] = useState('day');
-
+	const { user } = useSelector(({ profile }) => profile);
+	console.log(user.id);
 	return (
 		<div className={styles.prime_container}>
 			<Header activeTab={activeTab} setActiveTab={setActiveTab} />
 			<div className={styles.sub_container}>
 				<div className={styles.left_sub_container}>
-					<Calender />
+					{/* <Calender /> */}
 					<LineChart />
+					{/* <AgentActivity /> */}
 					<div className={styles.statistics}>
 						<Statistics />
 					</div>
@@ -29,6 +35,9 @@ function CogoOneDashboard() {
 				<div className={styles.right_sub_container}>
 					<TimeSpent />
 					<Intelligence />
+					{/* <RedFlags />
+					<PerformanceTab /> */}
+
 					<div className={styles.satisfaction_intent_served_box}>
 						<Statisfaction />
 						<IntentServed />
