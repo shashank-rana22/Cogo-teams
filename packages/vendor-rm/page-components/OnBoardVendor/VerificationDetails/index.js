e068aeee11ed290cd368120c2c201706f84b2eb9
MrutyunjayaB <mrutyunjaya.behera@cogoport.com>
2023-02-20 14:04:28 +0530

    verification-success page done

--- packages/vendor-rm/page-components/OnBoardVendor/VerificationDetails/index.js
+++ packages/vendor-rm/page-components/OnBoardVendor/VerificationDetails/index.js
@@ -1,15 +1,35 @@
 // eslint-disable-next-line import/no-cycle
 import ButtonLayout from '../../../commons/components/ButtonLayout/ButtonLayout';
 
+// import DisplayDetails from './DisplayDetails';
+import Header from './Header';
+import styles from './styles.module.css';
+import Success from './Success';
+
 function VerificationDetails({
+	// vendorInformation = {},
 	activeStepper,
 	setActiveStepper = () => {},
 }) {
 	return (
-		<ButtonLayout
-			activeStepper={activeStepper}
-			setActiveStepper={setActiveStepper}
-		/>
+		<>
+			<Header />
+
+			<div className={styles.button_container}>
+				<div className={styles.text_container}>
+					Submit the following details for verification?
+				</div>
+
+				<ButtonLayout
+					activeStepper={activeStepper}
+					setActiveStepper={setActiveStepper}
+				/>
+			</div>
+
+			{/* <DisplayDetails vendorInformation={vendorInformation} /> */}
+
+			<Success />
+		</>
 	);
 }
 
--- packages/vendor-rm/page-components/OnBoardVendor/index.js
+++ packages/vendor-rm/page-components/OnBoardVendor/index.js
@@ -22,7 +22,7 @@ function OnBoardVendor() {
 		method : 'GET',
 	}, { manual: true });
 
-	const [activeStepper, setActiveStepper] = useState('payment_details');
+	const [activeStepper, setActiveStepper] = useState('verification');
 
 	const [vendorInformation, setVendorInformation] = useState({});
 
