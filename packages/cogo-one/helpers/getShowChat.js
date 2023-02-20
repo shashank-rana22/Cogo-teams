864c9242babe863ce22064000567e6ded33cc077
Sandeep Nalabolu <sandeep.nalabolu@cogoport.com>
2023-02-20 17:37:44 +0530

    add null handling

--- packages/cogo-one/helpers/getShowChat.js
+++ packages/cogo-one/helpers/getShowChat.js
@@ -4,14 +4,14 @@ function getShowChat({
 	searchValue = '',
 }) {
 	const { new_message_count = 0, user_name = '' } = item;
-	const searchName = user_name.toLowerCase();
+	const searchName = user_name?.toLowerCase();
 
 	const { status = 'all' } = appliedFilters;
 
 	if (status === 'unread' && Number(new_message_count) === 0) {
 		return false;
 	}
-	return searchName.includes(searchValue);
+	return searchName?.includes(searchValue);
 }
 
 export default getShowChat;
