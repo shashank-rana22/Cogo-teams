f83514b6704ead5e684db0dd4b578001efe7e324
Ankit Kaushal <ankit.kaushal@cogoport.com>
2023-02-21 19:25:48 +0530

    layout

--- packages/ground-ops/page-components/GenerateMAWB/index.tsx
+++ packages/ground-ops/page-components/GenerateMAWB/index.tsx
@@ -3,6 +3,8 @@ import { Button, Modal } from '@cogoport/components';
 import { useForm } from '@cogoport/forms';
 import React, { useState } from 'react';
 
+import Layout from '../Air/commons/Layout';
+
 import FieldArray from './FieldArray';
 import GenerateMawbDoc from './GenerateMawbDoc';
 import getElementController from './getController';
@@ -45,48 +47,18 @@ function GenerateMAWB({ 	shipment_id = '', task = {}, viewDoc = false }) {
 		<div>
 			<div className={styles.heading}>Generate MAWB</div>
 			<div className={styles.form_container}>
-				<div className={styles.flex}>
-					<form onSubmit={handleSubmit(onSubmit)}>
-						<div className={styles.content}>
-							{fields.map((field) => {
-								const { ...rest } = field;
-
-								if (rest.type === 'fieldArray') {
-									return (
-										<div className={styles.list}>
-											<h4>{field.label}</h4>
-											<FieldArray {...rest} control={control} />
-										</div>
-									);
-								}
-								const Element = getElementController(rest.type);
-								return (
-									<div className={styles.list}>
-										<h4>{field.label}</h4>
-										<Element
-											width="100%"
-											control={control}
-											{...rest}
-										/>
-										<div className={styles.error}>{errors[field.name]?.message}</div>
-									</div>
-								);
-							})}
-						</div>
-						<div className={styles.button_div}>
-							{!back ? (
-								<Button
-									onClick={handleSubmit(onSubmit)}
-									disabled={documentLoading || generateLoading}
-
-								>
-									{documentLoading || generateLoading
-										? 'Generating'
-										: 'Generate Master Airway Bill'}
-								</Button>
-							) : null}
-						</div>
-					</form>
+				<Layout fields={fields} control={control} errors={errors} />
+				<div className={styles.button_div}>
+					{!back ? (
+						<Button
+							onClick={handleSubmit(onSubmit)}
+							disabled={documentLoading || generateLoading}
+						>
+							{documentLoading || generateLoading
+								? 'Generating'
+								: 'Generate Master Airway Bill'}
+						</Button>
+					) : null}
 				</div>
 
 			</div>
