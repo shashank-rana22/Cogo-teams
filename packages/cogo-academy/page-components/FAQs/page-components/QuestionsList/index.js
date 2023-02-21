0d2d11f7d011ce88909d9a6b85a15bde8264415f
Namanagarwal015 <124123426+Namanagarwal015@users.noreply.github.com>
2023-02-21 18:51:29 +0530

    Topic_List_Edit

--- packages/cogo-academy/page-components/FAQs/page-components/QuestionsList/index.js
+++ packages/cogo-academy/page-components/FAQs/page-components/QuestionsList/index.js
@@ -3,11 +3,19 @@ import React from 'react';
 import Questions from '../Questions';
 
 import styles from './styles.module.css';
+import useQuestionList from './useQuestionList';
 
 function QuestionsList(Tab) {
+	const {
+		questionList, setQuestionList, searchInput,
+		setSearchInput,
+	} = useQuestionList();
 	return (
 		<div>
-			<h1 className={styles.title}>{Tab.Tabtitle}</h1>
+			<h1 className={styles.title}>
+
+				{Tab.Tabtitle}
+			</h1>
 			<br />
 			<div className={styles.border}><Questions /></div>
 
--- packages/cogo-academy/page-components/FAQs/page-components/TopicList/index.js
+++ packages/cogo-academy/page-components/FAQs/page-components/TopicList/index.js
@@ -1,4 +1,4 @@
-import { ListGroup } from '@cogoport/components';
+import { Tabs, TabPanel } from '@cogoport/components';
 import { React, useState } from 'react';
 
 import QuestionsList from '../QuestionsList';
@@ -6,52 +6,81 @@ import QuestionsList from '../QuestionsList';
 import styles from './styles.module.css';
 
 function TopicList(Tab) {
-	const DEFAULT_LIST = [
+	const topics = ['discover_rates', 'shipment', 'checkout', 'discover_rates2',
+		'shipment2', 'checkout2', 'discover_rates3', 'shipment3', 'checkout3'];
+	const data = [
 		{
-
-			label: 'Sales 101: An introduction to sal...',
-
-		},	{
-			label: 'Sales 101: An introduction to sal...',
-
-		}, {
-			label: 'Sales 101: An introduction to sal...',
-		}, {
-			label: 'Incoterms',
-
-		}, {
-			label: 'The art of persuading',
+			question : 'How long will it take to receive B/L after payment?',
+			topics   : ['discover_rates', 'shipment', 'checkout'],
+			tags     : ['spot_search', 'trade_party'],
 		},
 		{
-			label: 'How to negotiate the non-nego...',
-
+			question : 'What are the payment terms?',
+			topics   : ['discover_rates', 'shipment', 'checkout'],
+			tags     : ['spot_search', 'trade_party'],
 		},
 		{
-
-			label: 'Sales 101: An introduction to sal...',
-
-		},	{
-			label: 'Sales 101: An introduction to sal...',
-
-		}, {
-			label: 'Sales 101: An introduction to sal...',
-		}, {
-			label: 'Incoterms',
-
-		}, {
-			label: 'The art of persuading',
+			question : 'How long will it take to receive B/L after payment?',
+			topics   : ['discover_rates', 'shipment', 'checkout'],
+			tags     : ['spot_search', 'trade_party'],
 		},
 		{
-			label: 'How to negotiate the non-nego...',
-
+			question : 'When will I receive Booking Note?',
+			topics   : ['discover_rates', 'shipment', 'checkout'],
+			tags     : ['spot_search', 'trade_party'],
+		},
+		{
+			question : 'How long will it take to receive B/L after payment?',
+			topics   : ['discover_rates', 'shipment', 'checkout', 'discover_rates',
+				'shipment', 'checkout', 'discover_rates', 'shipment', 'checkout'],
+			tags: ['spot_search', 'trade_party', 'trade_party', 'spot_search', 'trade_party'],
+		},
+		{
+			question : 'What is the process of shipment booking?',
+			topics   : ['discover_rates', 'shipment', 'checkout'],
+			tags     : ['spot_search', 'trade_party'],
+		},
+		{
+			question : 'What are Incoterms?',
+			topics   : ['discover_rates', 'shipment', 'checkout'],
+			tags     : ['spot_search', 'trade_party'],
+		},
+		{
+			question : 'How long will it take to receive B/L after payment?',
+			topics   : ['discover_rates', 'shipment', 'checkout'],
+			tags     : ['spot_search', 'trade_party'],
+		},
+		{
+			question : 'How long will it take to receive B/L after payment?',
+			topics   : ['discover_rates', 'shipment', 'checkout'],
+			tags     : ['spot_search', 'trade_party'],
+		},
+		{
+			question : 'When will I receive Booking Note?',
+			topics   : ['discover_rates', 'shipment', 'checkout'],
+			tags     : ['spot_search', 'trade_party'],
 		},
 	];
-
+	const [activeTab, setActiveTab] = useState('');
 	return (
 
 		<div className={styles.gridcontainer}>
 
-			<div><ListGroup options={DEFAULT_LIST} /></div>
+			<div style={{ margin: 20 }}>
+				<Tabs
+					activeTab={activeTab}
+					themeType="primary-vertical"
+					onChange={setActiveTab}
+				>
+					{topics?.map((singleOption) => (
+						<TabPanel name={singleOption} title={singleOption}>
+							{/* <div>This is local search</div> */}
+						</TabPanel>
+
+					))}
+
+				</Tabs>
+			</div>
 
 			<div><QuestionsList Tabtitle={Tab.Tabtitle} /></div>
 
