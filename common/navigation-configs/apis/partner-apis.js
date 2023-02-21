5adae9fa08df5e043e2c7da0d9346a4972932ffd
Akshay Sawant <akshay.sawant@cogoport.com>
2023-02-21 14:34:31 +0530

    api name change

--- common/navigation-configs/apis/partner-apis.js
+++ common/navigation-configs/apis/partner-apis.js
@@ -5985,7 +5985,7 @@ const partner = {
 			service_name : 'unified_dashboard',
 		},
 		{
-			api          : 'get_outstanding_invoices',
+			api          : 'get_outstanding_amount',
 			access_type  : 'private',
 			service_name : 'unified_dashboard',
 		},
