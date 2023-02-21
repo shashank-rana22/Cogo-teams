7bf33c394c3962df58d2422bae60e8934483f297
mohit-cogo <mohit.nagar@cogoport.com>
2023-02-21 16:16:19 +0530

    styles

--- packages/allocation/page-components/CoreAllocationEngine/AllocationConfigurations/Actions/Instances/ListInstances/index.js
+++ packages/allocation/page-components/CoreAllocationEngine/AllocationConfigurations/Actions/Instances/ListInstances/index.js
@@ -1,23 +1,10 @@
 import {
-	Button,
-	DateRangepicker, Legend, Pill, Table, Pagination,
+	DateRangepicker, Table, Pagination,
 } from '@cogoport/components';
-import { useRouter } from '@cogoport/next';
-import { format, getByKey, startCase } from '@cogoport/utils';
 
-import INSTANCES_STATUS_COLOR_MAPPING from '../../../../../../constants/instances-status-color-mapping';
 import useListAllocationInstances from '../../../../../../hooks/useListAllocationInstances';
 
 import styles from './styles.module.css';
-
-const LIST_COLUMNS_MAPPING = {
-	serial_id    : 'SERIAL ID',
-	created_at   : 'CREATED AT',
-	updated_at   : 'UPDATED AT',
-	execution_at : 'EXECUTION AT',
-	status       : 'STATUS',
-	action       : 'ACTION',
-};
 
 function ListInstances({ item }) {
 	const {
@@ -27,93 +14,13 @@ function ListInstances({ item }) {
 		getNextPage,
 		dateRange,
 		setDateRange,
+		columns,
 	} =	 useListAllocationInstances({ item });
-
-	const { push } = useRouter();
-
-	const onRowClick = (listItem) => {
-		const { status, id } = listItem;
-
-		if (['pending_approval', 'completed'].includes(status)) {
-			push(
-				'/allocation/core-engine/details/[instance_id]',
-				`/allocation/core-engine/details/${id}`,
-			);
-		}
-	};
 
 	const { page = 0, page_limit = 0, total_count = 0 } = paginationData || {};
 
-	const columns = Object.entries(LIST_COLUMNS_MAPPING).map(([key, value]) => ({
-		Header   : <div key={key}>{value}</div>,
-		accessor : key,
-		id       : key,
-	}));
-
-	const data = list.map((listItem) => {
-		const formattedData = {
-			serial_id: (
-				<Pill id={listItem.id} color="blue" size="lg">{getByKey(listItem, 'serial_id', '___')}</Pill>
-			),
-			created_at: (
-				<div>
-					{getByKey(listItem, 'created_at', '___')
-						? format(getByKey(listItem, 'created_at', '___'), 'dd MMM yyyy')
-						: '___'}
-				</div>
-			),
-			updated_at: (
-				<div>
-					{getByKey(listItem, 'updated_at', '___')
-						? format(getByKey(listItem, 'updated_at', '___'), 'dd MMM yyyy')
-						: '___'}
-				</div>
-			),
-			execution_at: (
-				<div>
-					{getByKey(listItem, 'execution_at', '___')
-						? format(getByKey(listItem, 'execution_at', '___'), 'dd MMM yyyy')
-						: '___'}
-				</div>
-			),
-			status: (
-				<Legend
-					className={styles.legend}
-					hasBackground={false}
-					direction="horizontal"
-					size="md"
-					items={[
-						{
-							label : startCase(getByKey(listItem, 'status', '___')),
-							color : INSTANCES_STATUS_COLOR_MAPPING[getByKey(listItem, 'status', '___')],
-							key   : getByKey(listItem, 'status', '___'),
-						},
-					]}
-				/>
-			),
-			action: (
-				<Button
-					size="sm"
-					themeType="secondary"
-					onClick={() => onRowClick(listItem)}
-					disabled={!['pending_approval', 'completed'].includes(listItem.status)}
-				>
-					View Details
-				</Button>
-			),
-		};
-
-		const dataToPush = {};
-
-		Object.keys(LIST_COLUMNS_MAPPING).forEach((dataKey) => {
-			dataToPush[dataKey] = formattedData[dataKey] || listItem[dataKey] || '___';
-		});
-
-		return dataToPush;
-	});
-
 	return (
-		<section>
+		<section className={styles.container}>
 			<div className={styles.daterange_container}>
 				<DateRangepicker
 					value={dateRange}
@@ -126,7 +33,7 @@ function ListInstances({ item }) {
 			<Table
 				className={styles.table}
 				columns={columns}
-				data={data}
+				data={list}
 				loading={listLoading}
 			/>
 
