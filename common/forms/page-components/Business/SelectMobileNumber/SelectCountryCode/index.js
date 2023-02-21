40cbf93c9cc9bc663e3f18d60476797e451f1e62
Prachi Dewangan <prachi.dewangan@cogoport.com>
2023-02-21 20:05:09 +0530

    mobile country code edited

--- common/forms/page-components/Business/SelectMobileNumber/SelectCountryCode/index.js
+++ common/forms/page-components/Business/SelectMobileNumber/SelectCountryCode/index.js
@@ -1,13 +1,15 @@
+/* eslint-disable import/no-relative-packages */
 import { Select } from '@cogoport/components';
 import React from 'react';
 
-import countryCode from '../../constants/country-codes.json';
+import countries from '../../../../../../.data-store/constants/countries.json';
 
 function SelectCountryCode(props) {
 	const { value } = props;
-	const formattedList = countryCode.map((code) => ({
-		value : code.value,
-		label : `${code.value}: ${code.label}`,
+
+	const formattedList = countries.map((code) => ({
+		value : code.mobile_country_code,
+		label : `${code.mobile_country_code}: ${code.name}`,
 	}));
 	return (
 		<Select {...props} options={formattedList} value={value} readOnly={false} />
