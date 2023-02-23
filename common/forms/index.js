c69c90cd2e6ee8aaf21a452f809e0ec722e32d47
Harsh <harsh.bhagat@cogoport.com>
2023-02-23 17:03:07 +0530

    minor

--- common/forms/index.js
+++ common/forms/index.js
@@ -8,7 +8,6 @@ export { default as DatepickerController } from './page-components/Controlled/Da
 export { default as InputController } from './page-components/Controlled/InputController';
 export { default as MobileNumberController } from './page-components/Controlled/MobileNumberController';
 export { default as UploadController } from './page-components/Controlled/UploadController';
-export { default as MobileNumberController } from './page-components/Controlled/MobileNumberController';
 export { default as PillsController } from './page-components/Controlled/PillsController';
 export { default as RadioGroupController } from './page-components/Controlled/RadioGroupController';
 export { default as DateRangePickerController } from './page-components/Controlled/DateRangePickerController';
