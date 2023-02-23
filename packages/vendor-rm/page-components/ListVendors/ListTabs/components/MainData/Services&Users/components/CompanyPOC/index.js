bf4dc5969714a222d2d6b54868c43e3f89238941
MrutyunjayaB <mrutyunjaya.behera@cogoport.com>
2023-02-23 19:31:21 +0530

    company poc and finalUrl null handling

--- packages/vendor-rm/page-components/ListVendors/ListTabs/components/MainData/Services&Users/components/CompanyPOC/index.js
+++ packages/vendor-rm/page-components/ListVendors/ListTabs/components/MainData/Services&Users/components/CompanyPOC/index.js
@@ -1,35 +1,57 @@
 /* eslint-disable jsx-a11y/control-has-associated-label */
 /* eslint-disable max-len */
-import { IcMEdit } from '@cogoport/icons-react';
+// import { IcMEdit } from '@cogoport/icons-react';
 import React from 'react';
+
+import workScope from '../../../../../../../../commons/utils/work-scopes.json';
 
 import styles from './styles.module.css';
 
-function CompanyPOC() {
-	const obj = 	[
-		{ label: 'Name', value: 'Chandrashekhar Na...' },
-		{ label: 'Email ID', value: 'chandrashekhar@ab...' },
-		{ label: 'Mobile Number', value: '+91 8383929900' },
-		{ label: 'Role in the Company', value: 'Finance Head' },
-	];
+const labelMapping = {
+	name          : 'Name',
+	email         : 'Email ID',
+	mobile_number : 'Mobile Number',
+	poc_role      : 'Role in the Company',
+};
+
+function CompanyPOC({
+	data,
+}) {
+	const details = (data?.pocs || []).map((poc) => {
+		const obj = {
+			name          : poc?.name,
+			email         : poc?.email,
+			mobile_number : `${poc?.mobile_country_code} ${poc?.mobile_number}`,
+			poc_role      : poc?.poc_role[0],
+		};
+		return obj;
+	});
+
+	const getRoleLabel = (val) => {
+		const scopeObj = workScope.find((scope) => scope.value === val);
+		return scopeObj.label;
+	};
 
 	return (
 		<div className={styles.main}>
-			<span className={styles.heading}>Company POC </span>
+			<span className={styles.heading}>
+				Company POC
+			</span>
 			<div className={styles.cont}>
-				{obj.map((item) => (
+				{details.map((item) => (
 					<div className={styles.box_info}>
-						<div>
-							<div className={styles.top}>
-								{item.label}
-							</div>
-							<div className={styles.bottom}>
-								{item.value}
-							</div>
-						</div>
-
-						{item.label === 'Role in the Company' ? <button className={styles.btn}><IcMEdit /></button> : ''}
-
+						{
+							Object.keys(item).map((poc) => (
+								<div>
+									<div className={styles.top}>
+										{labelMapping[poc]}
+									</div>
+									<div className={styles.bottom}>
+										{poc === 'poc_role' ? getRoleLabel(item[poc]) : item[poc]}
+									</div>
+								</div>
+							))
+						}
 					</div>
 				)) }
 
