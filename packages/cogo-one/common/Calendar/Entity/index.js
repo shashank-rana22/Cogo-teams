0effba4aff02b460247c25853139b6ac6704dd9d
Likhilesh Balpande <likhilesh@Likhileshs-MacBook-Air.local>
2023-02-23 12:29:13 +0530

    calendar code optimization v1

--- packages/cogo-one/common/Calendar/Entity/index.js
+++ packages/cogo-one/common/Calendar/Entity/index.js
@@ -1,72 +1,20 @@
-import { format } from '@cogoport/utils';
-import React from 'react';
-
 import styles from './styles.module.css';
 
-export function DateEntity({ dates }) {
+export function CalendarEntity({ calendarData }) {
 	return (
 		<div className={styles.calendar}>
 			{
-				dates?.map((date) => (
+				calendarData?.map(({ head, text }) => (
 					<div className={styles.dateContainer}>
 						<div className={styles.dayH1}>
-							{format(date, 'dd')}
+							{head}
 						</div>
 						<div className={styles.dayH2}>
-							{format(date, 'MMM yy')}
+							{text}
 						</div>
 					</div>
 				))
 			}
-		</div>
-	);
-}
-
-export function MonthEntity({ months }) {
-	return (
-		<div className={styles.calendar}>
-			{
-				months?.map((month) => (
-					<div className={styles.dateContainer}>
-						<div className={styles.dayH1}>
-							{format(month, 'MMM')}
-						</div>
-						<div className={styles.dayH2}>
-							{format(month, 'yyyy')}
-						</div>
-					</div>
-				))
-			}
-		</div>
-	);
-}
-
-export function WeekEntity({ weeks }) {
-	const todayDate = new Date();
-	return (
-		<div className={styles.calendar}>
-			{
-				weeks?.map((week) => (
-					todayDate.getTime() >= week.date.getTime()
-						&& (
-							<div className={styles.dateContainer}>
-								<div className={styles.dayH1}>
-									Week
-									{' '}
-									{week.iterator}
-								</div>
-								<div className={styles.dayH2}>
-									{format(week.date, 'MMM')}
-									{' '}
-									{week.start}
-									{' to '}
-									{week.end}
-								</div>
-							</div>
-						)
-
-				))
-}
 		</div>
 	);
 }
--- packages/cogo-one/common/Calendar/index.js
+++ packages/cogo-one/common/Calendar/index.js
@@ -1,45 +1,21 @@
-// import { format } from '@cogoport/utils';
 import { IcMArrowDoubleLeft, IcMArrowDoubleRight } from '@cogoport/icons-react';
+import { format } from '@cogoport/utils';
 import React, { useState, useEffect } from 'react';
 
-import { DateEntity, MonthEntity, WeekEntity } from './Entity';
+import { CalendarEntity } from './Entity';
 import styles from './styles.module.css';
 
-function Calendar() {
+function Calendar({ calendarType = 'month' }) {
 	const [pagination, setPagination] = useState(0);
-	const [dates, setDates] = useState([]);
-	const [months, setMonths] = useState([]);
-	const [weeks, setWeeks] = useState([]);
 	const [calendarData, setCalendarData] = useState([]);
 
 	const numberOfDays = 14;
 	const numberOfMonthsForWeeks = 2;
+
 	const calcDate = (subtractDays) => {
 		const d = new Date();
 		d.setDate(d.getDate() - subtractDays);
 		return d;
-	};
-
-	const loadDates = () => {
-		const newDates = [];
-		for (let i = pagination * numberOfDays; i < (pagination + 1) * numberOfDays; i += 1) {
-			newDates.push(calcDate(i));
-		}
-		setDates(newDates.reverse());
-	};
-
-	const calcMonth = (subtractMonths) => {
-		const d = new Date();
-		d.setMonth(d.getMonth() - subtractMonths);
-		return d;
-	};
-
-	const loadMonths = () => {
-		const newMonths = [];
-		for (let i = pagination * numberOfDays; i < (pagination + 1) * numberOfDays; i += 1) {
-			newMonths.push(calcMonth(i));
-		}
-		setMonths(newMonths.reverse());
 	};
 
 	function getWeeksOfMonth(month, year) {
@@ -50,13 +26,9 @@ function Calendar() {
 
 		let iterator = 1;
 		let start = 1;
-		let end = 7 - firstDate.getDay();
-		if (start === 'monday') {
-			if (firstDate.getDay() === 0) {
-				end = 1;
-			} else {
-				end = 7 - firstDate.getDay() + 1;
-			}
+		let end = 7 - firstDate.getDay() + 1;
+		if (firstDate.getDay() === 0) {
+			end = 1;
 		}
 		while (start <= numDays) {
 			newWeeks.push({ start, end, date: new Date(year, month, start), iterator });
@@ -71,64 +43,83 @@ function Calendar() {
 		return newWeeks.reverse();
 	}
 
+	const calcMonth = (subtractMonths) => {
+		const d = new Date();
+		d.setMonth(d.getMonth() - subtractMonths);
+		return d;
+	};
+
+	const FORMAT_TYPE = {
+		day: {
+			label    : 'dd',
+			subLabel : 'MMM yy',
+		},
+		week: {
+			label    : '',
+			subLabel : 'MMM',
+		},
+		month: {
+			label    : 'MMM',
+			subLabel : 'yyyy',
+		},
+	};
+
+	const processData = (func) => {
+		const newData = [];
+		for (let i = pagination * numberOfDays; i < (pagination + 1) * numberOfDays; i += 1) {
+			newData.push(func(i));
+		}
+		const data = [];
+		newData.reverse().forEach((item) => {
+			data.push({
+				head : format(item, FORMAT_TYPE[calendarType].label),
+				text : format(item, FORMAT_TYPE[calendarType].subLabel),
+			});
+		});
+		setCalendarData(data);
+	};
+
 	const loadWeeks = () => {
 		let newWeeks = [];
 		for (let i = pagination * numberOfMonthsForWeeks; i < (pagination + 1) * numberOfMonthsForWeeks; i += 1) {
 			const newDate = calcMonth(i);
 			newWeeks = [...newWeeks, ...getWeeksOfMonth(newDate.getMonth(), newDate.getFullYear(), newDate)];
 		}
-		console.log(newWeeks);
-		setWeeks(newWeeks.reverse());
+		const todayDate = new Date();
+		const data = [];
+		newWeeks.reverse().forEach((week) => {
+			if (todayDate.getTime() >= week.date.getTime()) {
+				data.push(
+					{
+						head : `Week ${week.iterator}`,
+						text : `${format(week.date, 'MMM')} ${week.start} to ${week.end}`,
+					},
+				);
+			}
+		});
+		setCalendarData(data);
 	};
 
 	useEffect(() => {
-		loadDates();
-		loadMonths();
-		loadWeeks();
-	}, [pagination]);
+		if (calendarType === 'day') processData(calcDate);
+		else if (calendarType === 'month') processData(calcMonth);
+		else loadWeeks();
+	}, [calendarType, pagination]);
 
 	return (
-		<>
-			<div className={styles.calendar}>
-				<button onClick={() => setPagination(pagination + 1)} className={styles.navBtn}>
-					<IcMArrowDoubleLeft />
-				</button>
-				<DateEntity dates={dates} />
-				<button
-					disabled={pagination === 0}
-					onClick={() => setPagination(pagination - 1)}
-					className={`${styles.navBtn} ${(pagination === 0) ? styles.inactive : ''}`}
-				>
-					<IcMArrowDoubleRight />
-				</button>
-			</div>
-			{/* <div className={styles.calendar}>
-				<button onClick={() => setPagination(pagination + 1)} className={styles.navBtn}>
-					<IcMArrowDoubleLeft />
-				</button>
-				<MonthEntity months={months} />
-				<button
-					disabled={pagination === 0}
-					onClick={() => setPagination(pagination - 1)}
-					className={`${styles.navBtn} ${(pagination === 0) ? styles.inactive : ''}`}
-				>
-					<IcMArrowDoubleRight />
-				</button>
-			</div>
-			<div className={styles.calendar}>
-				<button onClick={() => setPagination(pagination + 1)} className={styles.navBtn}>
-					<IcMArrowDoubleLeft />
-				</button>
-				<WeekEntity weeks={weeks} />
-				<button
-					disabled={pagination === 0}
-					onClick={() => setPagination(pagination - 1)}
-					className={`${styles.navBtn} ${(pagination === 0) ? styles.inactive : ''}`}
-				>
-					<IcMArrowDoubleRight />
-				</button>
-			</div> */}
-		</>
+		<div className={styles.calendar}>
+			<button onClick={() => setPagination(pagination + 1)} className={styles.navBtn}>
+				<IcMArrowDoubleLeft />
+			</button>
+			<CalendarEntity calendarData={calendarData} />
+			<button
+				disabled={pagination === 0}
+				onClick={() => setPagination(pagination - 1)}
+				className={`${styles.navBtn} ${(pagination === 0) ? styles.inactive : ''}`}
+			>
+				<IcMArrowDoubleRight />
+			</button>
+		</div>
 	);
 }
 
