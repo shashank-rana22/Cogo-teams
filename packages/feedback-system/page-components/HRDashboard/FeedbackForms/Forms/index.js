38a40b2b1d54a35fb167ed510eba62e1070ebcb3
shridharDB <shridhar.kulkarni@cogoport.com>
2023-02-23 19:59:56 +0530

    small fixes

--- packages/feedback-system/page-components/HRDashboard/FeedbackForms/Forms/index.js
+++ packages/feedback-system/page-components/HRDashboard/FeedbackForms/Forms/index.js
@@ -27,7 +27,7 @@ function Forms({
 
 	const { total_count = '' } = paginationData;
 
-	const dummyArray = ['ok', 'ok1', 'harry', 'potter', 'lorem', 'ipsum'];
+	const arr = [1, 2, 3, 4, 5, 6];
 
 	if (isEmpty(Object.values(formsParams).filter((i) => i))) {
 		return <div className={styles.no_role}>Please select designation to show forms...</div>;
@@ -38,83 +38,81 @@ function Forms({
 		setFormStage('add_questions');
 	};
 
-	return (isEmpty(formList) ? <EmptyState setFormStage={setFormStage} setOpenCreateForm={setOpenCreateForm} /> : (
-		<div className={styles.container}>
-			<div className={styles.form_header}>
-				<div className={styles.form_cat}>{`${startCase(department)}: ${startCase(designation)}`}</div>
-				{!openCreateForm && !!designation && (
-					<Button themeType="accent" onClick={() => showForm()}>Create New</Button>
-				)}
-			</div>
+	return (isEmpty(formList) && !loading
+		? <EmptyState setFormStage={setFormStage} setOpenCreateForm={setOpenCreateForm} /> : (
+			<div className={styles.container}>
+				<div className={styles.form_header}>
+					<div className={styles.form_cat}>{`${startCase(department)}: ${startCase(designation)}`}</div>
+					{!openCreateForm && !!designation && (
+						<Button themeType="accent" onClick={() => showForm()}>Create New</Button>
+					)}
+				</div>
 
-			<div className={styles.forms}>
-				{ loading ? (
-					<div className={styles.right_form_div}>
-						{dummyArray.map(() => (
-							<Placeholder
-								width="100%"
-								height="80px"
-								className={styles.loading_right_form}
-							/>
-						))}
-					</div>
-				) : formList.map((form) => {
-					const {
-						id = '', status = '', created_at = '', 'Questions Count': question_count = '',
-					} = form;
+				<div className={styles.forms}>
+					{ loading ? (
+						<div className={styles.right_form_div}>
+							{arr.map((i) => (
+								<Placeholder
+									key={i}
+									width="100%"
+									height="80px"
+									className={styles.loading_right_form}
+								/>
+							))}
+						</div>
+					) : formList.map((form) => {
+						const {
+							id = '', status = '', created_at = '', 'Questions Count': question_count = '',
+						} = form;
 
-					return (
-						<div className={styles.form} key={id}>
-							<div className={styles.published}>
-								<p className={styles.label}>Published On</p>
-								<div className={styles.value}>
-									{format(created_at, 'MMM yyyy')}
+						return (
+							<div className={styles.form} key={id}>
+								<div className={styles.published}>
+									<p className={styles.label}>Published On</p>
+									<div className={styles.value}>
+										{format(created_at, 'dd MMM yyyy')}
+										{' '}
+										<span>
+											{' '}
+											{format(created_at, 'hh:mm a')}
+										</span>
+									</div>
 								</div>
-							</div>
 
-							<div className={styles.form_name}>
-								<p className={styles.label}>Questions</p>
-								<div className={styles.value}>{question_count || '---'}</div>
-							</div>
-
-							{/* <div className={styles.last_used}>
-								<p className={styles.label}>Last Used</p>
-								<div className={styles.value}>
-									{format(last_used, 'MMM yyyy')}
+								<div className={styles.form_name}>
+									<p className={styles.label}>Questions</p>
+									<div className={styles.value}>{question_count || '---'}</div>
 								</div>
-							</div> */}
 
-							<div className={styles.status}>
-								<p className={styles.label}>Status</p>
-								<div className={styles.value}>
-									<Pill color={status === 'active' ? 'green' : 'red'}>
-										{status}
-									</Pill>
+								<div className={styles.status}>
+									<p className={styles.label}>Status</p>
+									<div className={styles.value}>
+										<Pill color={status === 'active' ? 'green' : 'red'}>
+											{startCase(status)}
+										</Pill>
+									</div>
 								</div>
-							</div>
 
-							<div className={styles.action}>
-								<div className={styles.value}>
+								<div className={styles.action}>
 									<Button themeType="secondary" onClick={() => openOldForm(id)}>Use</Button>
 								</div>
 							</div>
-						</div>
-					);
-				}) }
+						);
+					}) }
 
-			</div>
+				</div>
 
-			<div className={styles.pagination_container}>
-				<Pagination
-					type="number"
-					currentPage={pagination}
-					totalItems={total_count}
-					pageSize={10}
-					onPageChange={setPagination}
-				/>
+				<div className={styles.pagination_container}>
+					<Pagination
+						type="number"
+						currentPage={pagination}
+						totalItems={total_count}
+						pageSize={10}
+						onPageChange={setPagination}
+					/>
+				</div>
 			</div>
-		</div>
-	)
+		)
 
 	);
 }
