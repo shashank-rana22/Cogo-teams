444668a5437cf6c45f68de23ab2ba2124b3dc0ad
MahakCogoport <mahak.bharatkar@cogoport.com>
2023-02-23 17:42:28 +0530

    heading

--- packages/allocation/page-components/KamExpertise/index.js
+++ packages/allocation/page-components/KamExpertise/index.js
@@ -1,16 +1,24 @@
 import { Button } from '@cogoport/components';
 import { useRouter } from '@cogoport/next';
 
+import styles from './styles.module.css';
+
 function KamExpertise() {
 	const router = useRouter();
 
 	return (
-		<Button
-			onClick={() => router.push('/allocation/kam-expertise/view-badges')}
-			themeType="primary"
-		>
-			View Badges
-		</Button>
+		<section className={styles.container} id="kam_expertise_container">
+			<section className={styles.heading_container}>
+				KAM Expertise
+			</section>
+			<Button
+				onClick={() => router.push('/allocation/kam-expertise/view-badges')}
+				themeType="primary"
+			>
+				View Badges
+			</Button>
+		</section>
+
 	);
 }
 
