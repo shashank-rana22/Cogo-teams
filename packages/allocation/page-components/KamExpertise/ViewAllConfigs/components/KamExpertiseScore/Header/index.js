12a5162f0b230916332b951b8702bd7619ae4a18
MahakCogoport <mahak.bharatkar@cogoport.com>
2023-02-23 22:29:42 +0530

    header

--- packages/allocation/page-components/KamExpertise/ViewAllConfigs/components/KamExpertiseScore/Header/index.js
+++ packages/allocation/page-components/KamExpertise/ViewAllConfigs/components/KamExpertiseScore/Header/index.js
@@ -7,9 +7,26 @@ function Header() {
 	return (
 		<div className={styles.container}>
 			<div className={styles.config_basic_details}>
-				Currently Editing:
-				{' '}
-				<b>saved draft</b>
+				<div className={styles.draft_name}>
+					Currently Editing :
+					{' '}
+				&nbsp;
+					<b>Saved Draft</b>
+				</div>
+
+				<div className={styles.lower_details}>
+					<div>
+						Last Modified: 31-02-23
+					</div>
+
+					<div>
+						Last Edit By:
+						{' '}
+						<b>Cogoparth</b>
+					</div>
+
+				</div>
+
 			</div>
 
 			<div className={styles.button_container}>
