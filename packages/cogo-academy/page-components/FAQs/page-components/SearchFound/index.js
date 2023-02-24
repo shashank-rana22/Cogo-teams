dd914a489f30c1d0f6883605f9ce48b0f29dd443
Namanagarwal015 <124123426+Namanagarwal015@users.noreply.github.com>
2023-02-24 13:48:10 +0530

    searchfound component

---
+++ packages/cogo-academy/page-components/FAQs/page-components/SearchFound/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import styles from './styles.module.css';
+
+function SearchFound() {
+	return (
+		<div className="heading-container">
+			<h1 className={styles.title}>Do you Mean?</h1>
+			<p>dgvzd</p>
+
+		</div>
+	);
+}
+
+export default SearchFound;
