b367ee8d4774212d6a462537ee28fe6ab75ff808
Vikas-Cogo <vikas.patidar@cogoport.com>
2023-02-24 22:09:03 +0530

    add api

--- common/navigation-configs/apis/saas.js
+++ common/navigation-configs/apis/saas.js
@@ -446,6 +446,11 @@ const saas = {
 			access_type  : 'public',
 			service_name : 'saas_subscriptions_v2',
 		},
+		{
+			api          : 'list_organization_addresses',
+			access_type  : 'private',
+			service_name : 'organization',
+		},
 	],
 	app_saas_schedules: [
 		{
