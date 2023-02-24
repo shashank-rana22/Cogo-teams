e9832e7611f570bbc075cdfdf5623afd91d25534
Ritvik98349 <124043414+Ritvik98349@users.noreply.github.com>
2023-02-24 18:20:03 +0530

    added loading and empty state in list questions

---
+++ packages/cogo-academy/commons/EmptyQuestionListState/index.js
@@ -0,0 +1,62 @@
+import { Input, Modal, Button } from '@cogoport/components';
+import { useState, useEffect } from 'react';
+
+import useCreateQuestionSet
+	from '../../page-components/FAQs/page-components/QuestionsList/hooks/useCreateQuestionRequest';
+
+import styles from './styles.module.css';
+
+function EmptyQuestionListState({
+	searchquestion = '',
+	setSearchquestion = () => {},
+	searchState = '',
+}) {
+	const [show, setShow] = useState(false);
+
+	const { createQuestionSet, createQuestionLoading } = useCreateQuestionSet();
+
+	useEffect(() => {
+		setSearchquestion(searchState);
+	}, [searchState, setSearchquestion]);
+
+	return (
+		<div className={styles.nullstate}>
+			<div className={styles.nullstate_heading}>
+				OOPS! No Question Available
+			</div>
+
+			<div className={styles.nullstate_btn}>
+				<Button onClick={() => { setShow(true); }} size="lg">
+					Request your Question
+				</Button>
+			</div>
+			<div style={{ padding: '20px' }}>
+
+				<Modal size="md" show={show} onClose={() => setShow(false)} placement="top">
+					<Modal.Header title="Request your question here" />
+					<Modal.Body>
+						<div style={{ display: 'flex', alignItems: 'center' }}>
+							<Input
+								value={searchquestion}
+								size="lg"
+								style={{ marginRight: '8px', width: '100%' }}
+								onChange={(value) => { setSearchquestion(value); }}
+							/>
+						</div>
+
+					</Modal.Body>
+					<Modal.Footer>
+						<Button
+							loading={createQuestionLoading}
+							onClick={() => { createQuestionSet({ searchState: searchquestion, setShow }); }}
+						>
+							Submit
+						</Button>
+					</Modal.Footer>
+				</Modal>
+			</div>
+
+		</div>
+	);
+}
+export default EmptyQuestionListState;
--- packages/cogo-academy/page-components/FAQs/page-components/QuestionsList/index.js
+++ packages/cogo-academy/page-components/FAQs/page-components/QuestionsList/index.js
@@ -1,16 +1,14 @@
-import { Input, Modal, Button } from '@cogoport/components';
-import { startCase } from '@cogoport/utils';
-import React, { useState, useEffect } from 'react';
+import { isEmpty, startCase } from '@cogoport/utils';
+import React, { useState } from 'react';
 
+import EmptyQuestionListState from '../../../../commons/EmptyQuestionListState';
+import Spinner from '../../../../commons/Spinner';
 import useListFaqQuestions from '../../hooks/useListFaqQuestion';
 import Questions from '../Questions';
 
-import useCreateQuestionSet from './hooks/useCreateQuestionRequest';
 import styles from './styles.module.css';
 
 function QuestionsList({ tabTitle, searchState = '' }) {
-	const [show, setShow] = useState(false);
-
 	const [searchquestion, setSearchquestion] = useState(searchState);
 	const {
 		refetchQuestions = () => {},
@@ -20,11 +18,30 @@ function QuestionsList({ tabTitle, searchState = '' }) {
 		setActiveTab,
 	} = useListFaqQuestions();
 
-	const { createQuestionSet, createQuestionLoading } = useCreateQuestionSet();
+	if (loading) {
+		return (
+			<div className={styles.spinner}>
+				<Spinner
+					height={60}
+					width={60}
+					outerBorderColor="#FFF"
+					spinBorderColor="#000"
+					borderWidth="7px"
+				/>
+			</div>
+		);
+	}
 
-	useEffect(() => {
-		setSearchquestion(searchState);
-	}, [searchState]);
+	if (isEmpty(data)) {
+		return (
+			<EmptyQuestionListState
+				searchquestion={searchquestion}
+				setSearchquestion={setSearchquestion}
+				searchState={searchState}
+
+			/>
+		);
+	}
 
 	return (
 		<div>
@@ -32,46 +49,9 @@ function QuestionsList({ tabTitle, searchState = '' }) {
 				{startCase(tabTitle)}
 			</h1>
 			<div style={{ margin: '5px 0', width: '100%', height: '462px' }} className={styles.scrollable}>
-				{(!data) ? (
-					<div className={styles.nullstate}>
-						<div className={styles.nullstate_heading}>
-							OOPS! No Question Available
-						</div>
 
-						<div className={styles.nullstate_btn}>
-							<Button onClick={() => { setShow(true); }} size="lg">
-								Ask your Question
-							</Button>
-						</div>
-					</div>
-				) : data?.list.map((question) => (
-					<div className={styles.border}><Questions questions={question} /></div>
-				))}
-			</div>
+				{data?.list.map((question) => <div className={styles.border}><Questions questions={question} /></div>)}
 
-			<div style={{ padding: '20px' }}>
-				<Modal size="md" show={show} onClose={() => setShow(false)} placement="top">
-					<Modal.Header title="Request your question here" />
-					<Modal.Body>
-						<div style={{ display: 'flex', alignItems: 'center' }}>
-							<Input
-								value={searchquestion}
-								size="lg"
-								style={{ marginRight: '8px', width: '100%' }}
-								onChange={(value) => { setSearchquestion(value); }}
-							/>
-						</div>
-
-					</Modal.Body>
-					<Modal.Footer>
-						<Button
-							loading={createQuestionLoading}
-							onClick={() => { createQuestionSet({ searchState: searchquestion, setShow }); }}
-						>
-							Submit
-						</Button>
-					</Modal.Footer>
-				</Modal>
 			</div>
 		</div>
 	);
