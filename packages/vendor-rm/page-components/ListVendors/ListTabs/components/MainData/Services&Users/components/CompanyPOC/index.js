619d8d045c6b7896086d063809ce6d2900dd8c49
Harsh <harsh.bhagat@cogoport.com>
2023-02-24 19:23:03 +0530

    edit poc added

--- packages/vendor-rm/page-components/ListVendors/ListTabs/components/MainData/Services&Users/components/CompanyPOC/index.js
+++ packages/vendor-rm/page-components/ListVendors/ListTabs/components/MainData/Services&Users/components/CompanyPOC/index.js
@@ -1,11 +1,11 @@
-/* eslint-disable jsx-a11y/control-has-associated-label */
-/* eslint-disable max-len */
-// import { IcMEdit } from '@cogoport/icons-react';
-import { isEmpty } from '@cogoport/utils';
-import React from 'react';
+import { Modal, Button } from '@cogoport/components';
+import { IcMEdit } from '@cogoport/icons-react';
+import { useState } from 'react';
 
-import getPocRole from '../../utils/getPocRole';
+import FormLayout from '../../../../../../../../commons/components/FormLayout/FormLayout';
+import workScope from '../../../../../../../../utils/work-scopes.json';
 
+import useEditPoc from './hooks/useEditPoc';
 import styles from './styles.module.css';
 
 const labelMapping = {
@@ -49,31 +49,84 @@ function CompanyPOC({
 		return obj;
 	})[0];
 
-	if (isEmpty(details)) {
-		return null;
-	}
+	const [showEditPocModal, setShowEditPocModal] = useState(false);
+
+	const { control, controls, handleSubmit, errors, onSubmit } = useEditPoc({ data, setShowEditPocModal });
+
+	const getRoleLabel = (val) => {
+		const scopeObj = workScope.find((scope) => scope.value === val);
+		return scopeObj.label;
+	};
 
 	return (
 		<div className={styles.main}>
 			<span className={styles.heading}>
 				Company POC
 			</span>
-
-			<div className={styles.content}>
-				<div className={styles.box_info}>
-					{Object.keys(details || []).map((poc) => (
-						<div className={styles.label_value_container}>
-							<div className={styles.top}>
-								{labelMapping[poc]}
-							</div>
-
-							<div className={styles.bottom}>
-								{poc === 'poc_role' ? getPocRole(details[poc]) : details[poc]}
-							</div>
+			<div className={styles.cont}>
+				{details.map((item) => (
+					<>
+						<div className={styles.box_info}>
+							{
+							Object.keys(item).map((poc) => (
+								<div>
+									<div className={styles.top}>
+										{labelMapping[poc]}
+									</div>
+									<div className={styles.bottom}>
+										{poc === 'poc_role' ? getRoleLabel(item[poc]) : item[poc]}
+									</div>
+								</div>
+							))
+						}
 						</div>
-					))}
-				</div>
+						<Button size="md" themeType="secondary" onClick={() => setShowEditPocModal(!showEditPocModal)}>
+							<IcMEdit style={{ marginRight: 5 }} />
+						</Button>
+					</>
+				)) }
+				<Button size="md" themeType="secondary" onClick={() => setShowEditPocModal(!showEditPocModal)}>
+					<IcMEdit style={{ marginRight: 5 }} />
+				</Button>
 			</div>
+			<Modal
+				show={showEditPocModal}
+				size="lg"
+				onClose={() => setShowEditPocModal(false)}
+				// className={styles.modal_container}
+			>
+				<Modal.Header title="Edit" />
+				<Modal.Body>
+					<section
+						className={styles.bodyStyle}
+					>
+						<FormLayout
+							control={control}
+							fields={controls}
+							errors={errors}
+						/>
+					</section>
+				</Modal.Body>
+				<Modal.Footer>
+					<Button
+						size="md"
+						style={{ marginRight: 10 }}
+						themeType="secondary"
+						onClick={() => {
+							setShowEditPocModal(false);
+						}}
+					>
+						Cancel
+					</Button>
+					<Button
+						size="md"
+						onClick={handleSubmit(onSubmit)}
+						// loading={loading}
+					>
+						submit
+					</Button>
+				</Modal.Footer>
+			</Modal>
 
 			<hr className={styles.dis} />
 
