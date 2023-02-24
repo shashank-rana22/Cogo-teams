e0849c5a63df93f69c2ffa62477af5cd82f9af9d
Rahul Danampally <Danampally.Rahul@cogoport.com>
2023-02-24 17:06:19 +0530

    added constants

--- packages/cogo-one/constants/IDS_CONSTANTS.js
+++ packages/cogo-one/constants/IDS_CONSTANTS.js
@@ -1,6 +1,8 @@
+import ROLE_IDS from '@cogoport/constants/role_ids';
+
 const hasPermission = [
-	'5de782f6-f59d-41fc-84f4-8425dc1fa670', // SUPERADMIN_ID
-	'381b9d1d-e005-4cc0-9844-a5f1636e71b1', // 	TECH_SUPERADMIN_ID
+	ROLE_IDS.SUPERADMIN_ID,
+	ROLE_IDS.TECH_SUPERADMIN_ID,
 ];
 
 export default hasPermission;
