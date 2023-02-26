989d615552f435a6db19aaa8594bc1a542145aa9
Rahul Danampally <Danampally.Rahul@cogoport.com>
2023-02-26 23:53:44 +0530

    added bot message filter

--- packages/cogo-one/helpers/getFireStoreQuery.js
+++ packages/cogo-one/helpers/getFireStoreQuery.js
@@ -1,22 +1,43 @@
-import {
-	query,
-	orderBy,
-	where,
-} from 'firebase/firestore';
+import { query, orderBy, where } from 'firebase/firestore';
 
-function getFireStoreQuery({ omniChannelCollection, userId, appliedFilters, isomniChannelAdmin = false }) {
+function getFireStoreQuery({
+	omniChannelCollection,
+	userId,
+	appliedFilters,
+	isomniChannelAdmin = false,
+	showBotMessages = false,
+}) {
 	let firestoreQuery;
 	let queryFilters = [];
-
+	if (showBotMessages) {
+		return query(
+			omniChannelCollection,
+			where('session_type', '==', 'bot'),
+			orderBy('new_message_sent_at', 'desc'),
+		);
+	}
 	Object.keys(appliedFilters).forEach((item) => {
 		if (item === 'tags') {
-			queryFilters = [...queryFilters, where('chat_tags', 'array-contains', appliedFilters[item])];
+			queryFilters = [
+				...queryFilters,
+				where('chat_tags', 'array-contains', appliedFilters[item]),
+			];
 		} else if (item === 'channels') {
-			queryFilters = [...queryFilters, where('channel_type', 'in', appliedFilters[item])];
+			queryFilters = [
+				...queryFilters,
+				where('channel_type', 'in', appliedFilters[item]),
+			];
 		} else if (item === 'status' && appliedFilters[item] === 'unread') {
-			queryFilters = [...queryFilters, where('new_message_count', '>', 0), orderBy('new_message_count', 'desc')];
+			queryFilters = [
+				...queryFilters,
+				where('new_message_count', '>', 0),
+				orderBy('new_message_count', 'desc'),
+			];
 		} else if (item === 'escalation') {
-			queryFilters = [...queryFilters, where('chat_status', '==', appliedFilters[item])];
+			queryFilters = [
+				...queryFilters,
+				where('chat_status', '==', appliedFilters[item]),
+			];
 		}
 	});
 
@@ -37,9 +58,7 @@ function getFireStoreQuery({ omniChannelCollection, userId, appliedFilters, isom
 		);
 	}
 
-	return (
-		firestoreQuery
-	);
+	return firestoreQuery;
 }
 
 export default getFireStoreQuery;
