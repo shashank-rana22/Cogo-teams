592488c4319639050d654e2decd505a1bf8e6713
Kunal Singh <kunalsingh@Kunals-MacBook-Air.local>
2023-02-27 12:58:48 +0530

    badge list api calling structure

---
+++ packages/allocation/page-components/KamExpertise/components/Badges/useBadgesList.js
@@ -0,0 +1,35 @@
+import { useAllocationRequest } from '@cogoport/request';
+import { useState } from 'react';
+
+const useBadgesList = () => {
+	const [params, setParams] = useState({
+		sort_type  : 'desc',
+		sort_by    : 'expiry_date',
+		page_limit : 10,
+		page       : 1,
+		// filters    : {
+		// 	status : 'active',
+		// 	q      : searchQuery || undefined,
+		// },
+		// data_required: true,
+	});
+
+	const [{ loading, data }, refetch] = useAllocationRequest({
+		url     : '/relations',
+		method  : 'get',
+		authkey : 'get_allocation_relations',
+		params,
+	}, { manual: false });
+
+	const { list = [], ...paginationData } = data || {};
+
+	return ({
+		list,
+		paginationData,
+		refetch,
+		loading,
+		setParams,
+	});
+};
+
+export default useBadgesList;
