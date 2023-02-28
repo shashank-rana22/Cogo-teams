0f7952b668656279f4f405bbfa0d50d62654ed2c
MrutyunjayaB <mrutyunjaya.behera@cogoport.com>
2023-02-28 20:43:52 +0530

    removed bank doc from payload

---
+++ packages/vendor-rm/page-components/ListVendors/ListTabs/components/MainData/Profile/utils/vendorFieldMapping.js
@@ -0,0 +1,20 @@
+const VENDOR_FIELDS_MAPPING = [
+	{
+		key   : 'invalid_country',
+		value : 'country_id',
+	},
+	{
+		key   : 'invalid_pan_or_gst',
+		value : 'registration_number',
+	},
+	{
+		key   : 'invalid_business_name',
+		value : 'business_name',
+	},
+	{
+		key   : 'invalid_company_type',
+		value : 'company_type',
+	},
+];
+
+export default VENDOR_FIELDS_MAPPING;
