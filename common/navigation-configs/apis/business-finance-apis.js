49d98a3a5ffb8652dd293f4414537f76d6066590
cogomanish <manish.kumar@cogoport.com>
2023-03-01 11:56:17 +0530

    changes

--- common/navigation-configs/apis/business-finance-apis.js
+++ common/navigation-configs/apis/business-finance-apis.js
@@ -6,16 +6,6 @@ const business_finance = {
 			feature     : 'coe_finance',
 		},
 		{
-			api         : 'put_purchase_bills_by_id',
-			access_type : 'private',
-			feature     : 'coe_finance',
-		},
-		{
-			api          : 'update_shipment_collection_party',
-			access_type  : 'private',
-			service_name : 'shipment',
-		},
-		{
 			api          : 'list_shipment_documents',
 			access_type  : 'private',
 			service_name : 'shipment',
@@ -62,17 +52,7 @@ const business_finance = {
 			feature     : 'coe_finance',
 		},
 		{
-			api         : 'put_purchase_bills_status_v2',
-			access_type : 'private',
-			feature     : 'coe_finance',
-		},
-		{
 			api         : 'get_purchase_bills_stats',
-			access_type : 'private',
-			feature     : 'coe_finance',
-		},
-		{
-			api         : 'put_purchase_bills_by_id_urgeny_tag',
 			access_type : 'private',
 			feature     : 'coe_finance',
 		},
@@ -84,77 +64,6 @@ const business_finance = {
 		},
 		{
 			api         : 'get_payments_accounts_org_stats',
-			access_type : 'private',
-			feature     : 'coe_finance',
-		},
-		{
-			api         : 'get_sales_report',
-			access_type : 'private',
-			feature     : 'coe_finance',
-		},
-		{
-			api         : 'get_purchase_report',
-			access_type : 'private',
-			feature     : 'coe_finance',
-		},
-		{
-			api         : 'get_sales_report_profitability',
-			access_type : 'private',
-			feature     : 'coe_finance',
-		},
-		{
-			api         : 'list_sales_report',
-			access_type : 'private',
-			feature     : 'coe_finance',
-		},
-		{
-			api         : 'list_purchase_report',
-			access_type : 'private',
-			feature     : 'coe_finance',
-		},
-		{
-			api         : 'create_chat_message',
-			access_type : 'private',
-			feature     : 'coe_finance',
-		},
-		{
-			api         : 'list_chat_channels',
-			access_type : 'private',
-			feature     : 'coe_finance',
-		},
-		{
-			api         : 'delete_chat_message',
-			access_type : 'private',
-			feature     : 'coe_finance',
-		},
-		{
-			api         : 'update_chat_message',
-			access_type : 'private',
-			feature     : 'coe_finance',
-		},
-		{
-			api         : 'get_chat_channel',
-			access_type : 'private',
-			feature     : 'coe_finance',
-		},
-		{
-			api         : 'create_chat_channel',
-			access_type : 'private',
-			feature     : 'coe_finance',
-		},
-		{
-			api         : 'update_chat_channel_seen',
-			access_type : 'private',
-			feature     : 'coe_finance',
-		},
-		{
-			api          : 'update_shipment_manifest',
-			access_type  : 'private',
-			feature      : 'coe_finance',
-			service_name : 'shipment',
-		},
-		{
-			api         : 'get_sales_invoice_shipment_list',
 			access_type : 'private',
 			feature     : 'coe_finance',
 		},
