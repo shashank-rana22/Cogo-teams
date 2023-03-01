c91af50849daef53abf676e8f47579190f36839c
Shivam Singh <shivam.singh@cogoport.com>
2023-03-01 22:29:39 +0530

    answer page comment

--- packages/cogo-academy/page-components/FAQs/page-components/AnswerPage/index.js
+++ packages/cogo-academy/page-components/FAQs/page-components/AnswerPage/index.js
@@ -20,297 +20,298 @@ const FEEDBACK_MAPPING_ISLIKED = {
 };
 
 function AnswerPage() {
-	const {
-		general,
-	} = useSelector((state) => state);
-	const router = useRouter();
+	return (<div>shivam</div>);
+	// const {
+	// 	general,
+	// } = useSelector((state) => state);
+	// const router = useRouter();
 
-	const { query } = general || {};
+	// const { query } = general || {};
 
-	const { id = '', topicId = '' } = query || {};
+	// const { id = '', topicId = '' } = query || {};
 
-	const [show, setShow] = useState(false);
-	const [load, setload] = useState(true);
-	const { refetchQuestions, data: answerData, loading } = useGetQuestions({ id });
+	// const [show, setShow] = useState(false);
+	// const [load, setload] = useState(true);
+	// const { refetchQuestions, data: answerData, loading } = useGetQuestions({ id });
 
-	const is_positive = answerData?.answers?.[0]?.faq_feedbacks?.[0]?.is_positive;
+	// const is_positive = answerData?.answers?.[0]?.faq_feedbacks?.[0]?.is_positive;
 
-	const [isLiked, setIsLiked] = useState(FEEDBACK_MAPPING_ISLIKED[is_positive] || '');
+	// const [isLiked, setIsLiked] = useState(FEEDBACK_MAPPING_ISLIKED[is_positive] || '');
 
-	useEffect(() => {
-		setIsLiked(FEEDBACK_MAPPING_ISLIKED[is_positive]);
-	// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [loading]);
+	// useEffect(() => {
+	// 	setIsLiked(FEEDBACK_MAPPING_ISLIKED[is_positive]);
+	// // eslint-disable-next-line react-hooks/exhaustive-deps
+	// }, [loading]);
 
-	const { handleSubmit, formState: { errors }, control } = useForm();
+	// const { handleSubmit, formState: { errors }, control } = useForm();
 
-	const apiName = answerData?.answers?.[0]?.faq_feedbacks?.[0]?.id
-		? '/update_faq_feedback'
-		: '/create_faq_feedback';
+	// const apiName = answerData?.answers?.[0]?.faq_feedbacks?.[0]?.id
+	// 	? '/update_faq_feedback'
+	// 	: '/create_faq_feedback';
 
-	const [{ loading: feedbackLoading }, trigger] = useRequest({
-		url    : apiName,
-		method : 'POST',
-	});
+	// const [{ loading: feedbackLoading }, trigger] = useRequest({
+	// 	url    : apiName,
+	// 	method : 'POST',
+	// });
 
-	const onClose = () => {
-		setIsLiked(FEEDBACK_MAPPING_ISLIKED[is_positive] || '');
-		setShow(false);
-	};
+	// const onClose = () => {
+	// 	setIsLiked(FEEDBACK_MAPPING_ISLIKED[is_positive] || '');
+	// 	setShow(false);
+	// };
 
-	const onClickLikeButton = async ({ _id }) => {
-		setload(false);
+	// const onClickLikeButton = async ({ _id }) => {
+	// 	setload(false);
 
-		try {
-			let payload = {
-				faq_answer_id : _id,
-				is_positive   : true,
-				status        : 'active',
-			};
-			if (isLiked === 'liked') {
-				payload = {
-					id     : answerData?.answers?.[0]?.faq_feedbacks?.[0]?.id,
-					status : 'inactive',
-				};
-			} else if (isLiked === 'disliked') {
-				payload = {
-					id          : answerData?.answers?.[0]?.faq_feedbacks?.[0]?.id,
-					is_positive : true,
-					status      : 'active',
-				};
-			}
+	// 	try {
+	// 		let payload = {
+	// 			faq_answer_id : _id,
+	// 			is_positive   : true,
+	// 			status        : 'active',
+	// 		};
+	// 		if (isLiked === 'liked') {
+	// 			payload = {
+	// 				id     : answerData?.answers?.[0]?.faq_feedbacks?.[0]?.id,
+	// 				status : 'inactive',
+	// 			};
+	// 		} else if (isLiked === 'disliked') {
+	// 			payload = {
+	// 				id          : answerData?.answers?.[0]?.faq_feedbacks?.[0]?.id,
+	// 				is_positive : true,
+	// 				status      : 'active',
+	// 			};
+	// 		}
 
-			await trigger({
-				data: payload,
-			});
+	// 		await trigger({
+	// 			data: payload,
+	// 		});
 
-			setIsLiked(isLiked === 'liked' ? '' : 'liked');
-			refetchQuestions();
-		} catch (error) {
-			console.log('error :: ', error);
-		}
-	};
+	// 		setIsLiked(isLiked === 'liked' ? '' : 'liked');
+	// 		refetchQuestions();
+	// 	} catch (error) {
+	// 		console.log('error :: ', error);
+	// 	}
+	// };
 
-	const onClickRemoveDisLike = async () => {
-		setload(false);
+	// const onClickRemoveDisLike = async () => {
+	// 	setload(false);
 
-		try {
-			await trigger({
-				data: {
-					id     : answerData?.answers?.[0]?.faq_feedbacks?.[0]?.id,
-					status : 'inactive',
-				},
-			});
+	// 	try {
+	// 		await trigger({
+	// 			data: {
+	// 				id     : answerData?.answers?.[0]?.faq_feedbacks?.[0]?.id,
+	// 				status : 'inactive',
+	// 			},
+	// 		});
 
-			setIsLiked('');
-			refetchQuestions();
-		} catch (error) {
-			console.log('error :: ', error);
-		}
-	};
+	// 		setIsLiked('');
+	// 		refetchQuestions();
+	// 	} catch (error) {
+	// 		console.log('error :: ', error);
+	// 	}
+	// };
 
-	const onSubmit = async (values) => {
-		setload(false);
-		let remark = values?.remark;
+	// const onSubmit = async (values) => {
+	// 	setload(false);
+	// 	let remark = values?.remark;
 
-		if (values?.answer_checkbox) {
-			remark = `Answer not satisfactory. ${remark}`;
-		}
-		if (values?.question_checkbox) {
-			remark = `Question not satisfactory. ${remark}`;
-		}
+	// 	if (values?.answer_checkbox) {
+	// 		remark = `Answer not satisfactory. ${remark}`;
+	// 	}
+	// 	if (values?.question_checkbox) {
+	// 		remark = `Question not satisfactory. ${remark}`;
+	// 	}
 
-		let payload = {
-			faq_answer_id : answerData?.answers[0]?.id,
-			is_positive   : false,
-			remark,
-			status        : 'active',
-		};
-		if (answerData?.answers?.[0]?.faq_feedbacks?.[0]?.is_positive) {
-			payload = {
-				id            : answerData?.answers?.[0]?.faq_feedbacks?.[0]?.id,
-				faq_answer_id : answerData?.answers[0]?.id,
-				is_positive   : false,
-				remark,
-				status        : 'active',
-			};
-		}
+	// 	let payload = {
+	// 		faq_answer_id : answerData?.answers[0]?.id,
+	// 		is_positive   : false,
+	// 		remark,
+	// 		status        : 'active',
+	// 	};
+	// 	if (answerData?.answers?.[0]?.faq_feedbacks?.[0]?.is_positive) {
+	// 		payload = {
+	// 			id            : answerData?.answers?.[0]?.faq_feedbacks?.[0]?.id,
+	// 			faq_answer_id : answerData?.answers[0]?.id,
+	// 			is_positive   : false,
+	// 			remark,
+	// 			status        : 'active',
+	// 		};
+	// 	}
 
-		try {
-			await trigger({
-				data: payload,
-			});
+	// 	try {
+	// 		await trigger({
+	// 			data: payload,
+	// 		});
 
-			setIsLiked('disliked');
-			setShow(false);
-			refetchQuestions();
-		} catch (error) {
-			console.log('error :: ', error);
-		}
-	};
+	// 		setIsLiked('disliked');
+	// 		setShow(false);
+	// 		refetchQuestions();
+	// 	} catch (error) {
+	// 		console.log('error :: ', error);
+	// 	}
+	// };
 
-	const onClickBackIcon = () => {
-		const href = `/learning/faq?topicId=${topicId}`;
-		router.push(href, href);
-	};
+	// const onClickBackIcon = () => {
+	// 	const href = `/learning/faq?topicId=${topicId}`;
+	// 	router.push(href, href);
+	// };
 
-	if (loading && load) {
-		return (
-			<div className={styles.spinner}>
-				<Spinner
-					borderWidth="4px"
-					outerBorderColor="#FBD69F"
-					spinBorderColor="red"
-					width="36px"
-					height="36px"
-				/>
-			</div>
-		);
-	}
+	// if (loading && load) {
+	// 	return (
+	// 		<div className={styles.spinner}>
+	// 			<Spinner
+	// 				borderWidth="4px"
+	// 				outerBorderColor="#FBD69F"
+	// 				spinBorderColor="red"
+	// 				width="36px"
+	// 				height="36px"
+	// 			/>
+	// 		</div>
+	// 	);
+	// }
 
-	return (
-		<div>
-			<div
-				style={{ display: 'flex' }}
-				role="presentation"
-				className={styles.back}
-				onClick={onClickBackIcon}
-			>
-				<div className={styles.arrow}><IcMArrowBack /></div>
-				Go Back
-			</div>
+	// return (
+	// 	<div>
+	// 		<div
+	// 			style={{ display: 'flex' }}
+	// 			role="presentation"
+	// 			className={styles.back}
+	// 			onClick={onClickBackIcon}
+	// 		>
+	// 			<div className={styles.arrow}><IcMArrowBack /></div>
+	// 			Go Back
+	// 		</div>
 
-			<div className={styles.questionheading}>Question</div>
+	// 		<div className={styles.questionheading}>Question</div>
 
-			<div className={styles.questionabstract}>
-				{answerData?.question_abstract}
-			</div>
+	// 		<div className={styles.questionabstract}>
+	// 			{answerData?.question_abstract}
+	// 		</div>
 
-			<div className={styles.answer}>Answer:</div>
+	// 		<div className={styles.answer}>Answer:</div>
 
-			<div className={styles.heading_container}>
-				<div dangerouslySetInnerHTML={{ __html: answerData?.answers[0]?.answer }} />
-			</div>
+	// 		<div className={styles.heading_container}>
+	// 			<div dangerouslySetInnerHTML={{ __html: answerData?.answers[0]?.answer }} />
+	// 		</div>
 
-			<div className={styles.answer}>Tags</div>
+	// 		<div className={styles.answer}>Tags</div>
 
-			<div className={styles.pills}>
-				{(answerData?.faq_tags || []).map((item) => (
-					<Pill
-						className={styles.questions_tag}
-						key={item.display_name}
-						size="sm"
-						color="white"
-					>
-						{startCase(item.display_name)}
-					</Pill>
-				))}
-			</div>
+	// 		<div className={styles.pills}>
+	// 			{(answerData?.faq_tags || []).map((item) => (
+	// 				<Pill
+	// 					className={styles.questions_tag}
+	// 					key={item.display_name}
+	// 					size="sm"
+	// 					color="white"
+	// 				>
+	// 					{startCase(item.display_name)}
+	// 				</Pill>
+	// 			))}
+	// 		</div>
 
-			<div className={styles.flex_items}>
-				<span className={styles.subtitle}>Did you find this information helpful?</span>
-				<div
-					role="presentation"
-					className={styles.like_container}
-					onClick={() => {
-						onClickLikeButton({ _id: answerData?.answers[0]?.id });
-					}}
-				>
-					{answerData?.answers[0]?.upvote_count >= 0 ? (
-						<Badge
-							placement="left"
-							color="green"
-							size="md"
-							// eslint-disable-next-line no-unsafe-optional-chaining
-							text={answerData?.answers[0]?.upvote_count || 0}
-						>
-							<IcCLike fill={isLiked === 'liked' ? 'black' : '#f8f5ec'} />
-						</Badge>
-					) : (
-						<IcCLike fill={isLiked === 'liked' ? 'black' : '#f8f5ec'} />
-					) }
-				</div>
+	// 		<div className={styles.flex_items}>
+	// 			<span className={styles.subtitle}>Did you find this information helpful?</span>
+	// 			<div
+	// 				role="presentation"
+	// 				className={styles.like_container}
+	// 				onClick={() => {
+	// 					onClickLikeButton({ _id: answerData?.answers[0]?.id });
+	// 				}}
+	// 			>
+	// 				{answerData?.answers[0]?.upvote_count >= 0 ? (
+	// 					<Badge
+	// 						placement="left"
+	// 						color="green"
+	// 						size="md"
+	// 						// eslint-disable-next-line no-unsafe-optional-chaining
+	// 						text={answerData?.answers[0]?.upvote_count || 0}
+	// 					>
+	// 						<IcCLike fill={isLiked === 'liked' ? 'black' : '#f8f5ec'} />
+	// 					</Badge>
+	// 				) : (
+	// 					<IcCLike fill={isLiked === 'liked' ? 'black' : '#f8f5ec'} />
+	// 				) }
+	// 			</div>
 
-				<div
-					role="presentation"
-					className={styles.dislike_container}
-					onClick={() => {
-						if (isLiked !== 'disliked') {
-							setShow(true);
-							setIsLiked('disliked');
-						} else {
-							onClickRemoveDisLike();
-						}
-					}}
-				>
-					<IcCDislike fill={isLiked === 'disliked' ? 'black' : '#f8f5ec'} />
-				</div>
+	// 			<div
+	// 				role="presentation"
+	// 				className={styles.dislike_container}
+	// 				onClick={() => {
+	// 					if (isLiked !== 'disliked') {
+	// 						setShow(true);
+	// 						setIsLiked('disliked');
+	// 					} else {
+	// 						onClickRemoveDisLike();
+	// 					}
+	// 				}}
+	// 			>
+	// 				<IcCDislike fill={isLiked === 'disliked' ? 'black' : '#f8f5ec'} />
+	// 			</div>
 
-				<Modal
-					size="md"
-					show={show}
-					onClose={onClose}
-					placement="right"
-				>
-					<Modal.Header title="Reason for dislike" />
-					<Modal.Body>
-						<form
-							className={styles.form_container}
-							onSubmit={handleSubmit(onSubmit)}
-						>
-							<div>
-								<CheckboxController
-									control={control}
-									name="question_checkbox"
-									type="checkbox"
-									label="Question not satisfactory"
-								/>
-								<CheckboxController
-									control={control}
-									name="answer_checkbox"
-									type="checkbox"
-									label="Answer not satisfactory"
-								/>
-							</div>
+	// 			<Modal
+	// 				size="md"
+	// 				show={show}
+	// 				onClose={onClose}
+	// 				placement="right"
+	// 			>
+	// 				<Modal.Header title="Reason for dislike" />
+	// 				<Modal.Body>
+	// 					<form
+	// 						className={styles.form_container}
+	// 						onSubmit={handleSubmit(onSubmit)}
+	// 					>
+	// 						<div>
+	// 							<CheckboxController
+	// 								control={control}
+	// 								name="question_checkbox"
+	// 								type="checkbox"
+	// 								label="Question not satisfactory"
+	// 							/>
+	// 							<CheckboxController
+	// 								control={control}
+	// 								name="answer_checkbox"
+	// 								type="checkbox"
+	// 								label="Answer not satisfactory"
+	// 							/>
+	// 						</div>
 
-							<div className={styles.remark}>
-								<div className={styles.aftercheckbox}>Remarks</div>
-								<InputController
-									control={control}
-									name="remark"
-									type="text"
-									placeholder="Enter remark here"
-									rules={{ required: 'Remark is required' }}
-								/>
-								{errors.remark && (
-									<span className={styles.errors}>
-										{errors.remark.message}
-									</span>
-								)}
-							</div>
-							<Button type="submit" loading={feedbackLoading}>Submit</Button>
-						</form>
-					</Modal.Body>
-				</Modal>
-			</div>
+	// 						<div className={styles.remark}>
+	// 							<div className={styles.aftercheckbox}>Remarks</div>
+	// 							<InputController
+	// 								control={control}
+	// 								name="remark"
+	// 								type="text"
+	// 								placeholder="Enter remark here"
+	// 								rules={{ required: 'Remark is required' }}
+	// 							/>
+	// 							{errors.remark && (
+	// 								<span className={styles.errors}>
+	// 									{errors.remark.message}
+	// 								</span>
+	// 							)}
+	// 						</div>
+	// 						<Button type="submit" loading={feedbackLoading}>Submit</Button>
+	// 					</form>
+	// 				</Modal.Body>
+	// 			</Modal>
+	// 		</div>
 
-			<div>
-				<span className={styles.sidetext}>
-					{answerData?.answers[0]?.upvote_count}
-					{' '}
-					people found it useful.
-				</span>
-				{'    '}
-				<span className={styles.sidetext}>
-					Last updated on:
-					{' '}
-					{format(answerData?.updated_at, 'dd MMM yyyy')}
-				</span>
-			</div>
+	// 		<div>
+	// 			<span className={styles.sidetext}>
+	// 				{answerData?.answers[0]?.upvote_count}
+	// 				{' '}
+	// 				people found it useful.
+	// 			</span>
+	// 			{'    '}
+	// 			<span className={styles.sidetext}>
+	// 				Last updated on:
+	// 				{' '}
+	// 				{format(answerData?.updated_at, 'dd MMM yyyy')}
+	// 			</span>
+	// 		</div>
 
-			<RelatedQuestion tags={answerData?.faq_tags[0]} question_abstract={answerData?.question_abstract} />
-		</div>
-	);
+	// 		<RelatedQuestion tags={answerData?.faq_tags[0]} question_abstract={answerData?.question_abstract} />
+	// 	</div>
+	// );
 }
 
 export default AnswerPage;
