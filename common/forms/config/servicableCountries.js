8ac222e63634c76b909b358b7f2056ead9d6c04a
shivanshi-cogoport <shivanshi.gupta@cogoport.com>
2023-03-01 15:35:16 +0530

    file removed

--- common/forms/config/servicableCountries.js
+++
@@ -1,8 +0,0 @@
-// import { getCountryId } from '../utils/getCountryId';
-
-// const SERVICABLE_COUNTRY_IDS = {
-// 	IN : getCountryId('IN'),
-// 	VN : getCountryId('VN'),
-// };
-
-// export default SERVICABLE_COUNTRY_IDS;
