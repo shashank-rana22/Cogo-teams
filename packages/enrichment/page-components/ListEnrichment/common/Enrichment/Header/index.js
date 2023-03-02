61bb5df0ceb8b5245fab6ef9dea5c79bc1eb4015
mohit-cogo <mohit.nagar@cogoport.com>
2023-03-02 15:15:50 +0530

    download

--- packages/enrichment/page-components/ListEnrichment/common/Enrichment/Header/index.js
+++ packages/enrichment/page-components/ListEnrichment/common/Enrichment/Header/index.js
@@ -1,11 +1,13 @@
 import { Button } from '@cogoport/components';
 import { IcMCloudUpload, IcMDownload } from '@cogoport/icons-react';
 
+import useDownloadEnrichmentCsv from '../../../hooks/useDownloadEnrichmentCsv';
+
 import Filters from './Filters';
 import styles from './styles.module.css';
 
 // eslint-disable-next-line max-len
-const URL = 'https://cogoport-testing.sgp1.digitaloceanspaces.com/bd4627da374e4fe2c49784f166f7f29b/feedback_response_sample.csv';
+// const URL = 'https://cogoport-testing.sgp1.digitaloceanspaces.com/bd4627da374e4fe2c49784f166f7f29b/feedback_response_sample.csv';
 
 function Header(props) {
 	const {
@@ -17,6 +19,10 @@ function Header(props) {
 		setSearchValue,
 		setShowUpload,
 	} = props;
+
+	const { loading, onDownload } = useDownloadEnrichmentCsv();
+
+	// console.log('csv_url :: ', csv_url);
 
 	return (
 		<div className={styles.header}>
@@ -31,13 +37,14 @@ function Header(props) {
 
 			{activeTab === 'enrichment_requests' && (
 				<div className={styles.actions}>
-
 					<Button
 						size="lg"
 						themeType="secondary"
 						style={{ marginRight: '12px' }}
 						// eslint-disable-next-line no-undef
-						onClick={() => window.open(URL, '_blank')}
+						// onClick={() => window.open(URL, '_blank')}
+						disabled={loading}
+						onClick={onDownload}
 					>
 						<IcMDownload width={16} height={16} style={{ marginRight: '4px' }} />
 						Download
