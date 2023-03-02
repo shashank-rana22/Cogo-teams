eb1e586a787aead5a9ffb6859c6e7ad3be3c258b
Mayank Shahria <mayank.shahria@cogoport.com>
2023-03-02 14:52:38 +0530

    admin side changes of rate density

--- packages/supply-dashboards/page-components/RfqEnquiries/index.js
+++ packages/supply-dashboards/page-components/RfqEnquiries/index.js
@@ -4,7 +4,7 @@ import { useSelector } from '@cogoport/store';
 import RfqEnquiries from './Content';
 
 function RfqEnquiriesView() {
-	const partnerId = useSelector((s) => s?.profile?.partner?.id);
+	const partnerId = useSelector((state) => state?.profile?.partner?.id);
 	const activeTab = 'rfq_enquiries';
 	const handleTabChange = (tab) => {
 		if (tab !== 'rfq_enquiries') {
@@ -20,7 +20,8 @@ function RfqEnquiriesView() {
 				<TabPanel name="live_bookings" title="Live Bookings">--</TabPanel>
 				<TabPanel name="trade_enquiry" title="Missing Rates">--</TabPanel>
 				<TabPanel name="disliked_rates" title="Disliked Rates">--</TabPanel>
-				<TabPanel name="rate_density" title="Rate Density & Coverage">--</TabPanel>
+				{partnerId !== 'b67d40b1-616c-4471-b77b-de52b4c9f2ff' && (
+				<TabPanel name="rate_density" title="Rate Density & Coverage">--</TabPanel>)}
 				<TabPanel name="manage_forecast" title="Manage Forcast">--</TabPanel>
 				<TabPanel name="rfq_enquiries" title="RFQ Enquiries">
 					<RfqEnquiries />
