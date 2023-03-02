f21aabaf1c7ed25078b33070e57b6651c3d2aa23
ShivaniBairoju <Shivani.Bairoju@cogoport.com>
2023-03-02 11:42:45 +0530

    faq permission added

--- common/navigation-configs/apis/saas.js
+++ common/navigation-configs/apis/saas.js
@@ -1554,6 +1554,12 @@ const saas = {
 			access_type  : 'private',
 			service_name : 'organization',
 		},
+		{
+			api         : 'get_saas_faq',
+			access_type : 'private',
+			module      : 'insurance',
+			feature     : 'insurance',
+		},
 	],
 	app_saas_trade_partner: [
 		{
