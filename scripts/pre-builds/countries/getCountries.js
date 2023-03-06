bf884228be379e89665bc0fe164738e912cefe8d
samir-dupare <samir.dupare@cogoport.com>
2023-03-06 13:19:19 +0530

    added currency code param in filters

--- scripts/pre-builds/countries/getCountries.js
+++ scripts/pre-builds/countries/getCountries.js
@@ -3,7 +3,7 @@ const https = require('https');
 
 const getCountries = async (callBack, path, baseUrl) => {
 	const actualUrl = `${baseUrl}
-		location/list_locations?filters=%7B"type"%3A"country"%7D&includes=%7B"default_params_required"%3A1%2C"mobile_country_code"%3A1%2C"flag_icon_url"%3A1%7D&page=1&page_limit=500`;
+		location/list_locations?filters=%7B"type"%3A"country"%7D&includes=%7B"default_params_required"%3A1%2C"mobile_country_code"%3A1%2C"currency_code"%3A1%2C"flag_icon_url"%3A1%7D&page=1&page_limit=500`;
 
 	https
 		.get(actualUrl, (resp) => {
