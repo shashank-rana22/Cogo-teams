621dda6e9bc059485d868f378694030610e0c714
Shivam Singh <shivam.singh@cogoport.com>
2023-03-06 18:47:56 +0530

    service name reverted

--- common/navigation-configs/apis/faq-apis.js
+++ common/navigation-configs/apis/faq-apis.js
@@ -3,37 +3,37 @@ const faq = {
 		{
 			api          : 'list_faq_questions',
 			access_type  : 'private',
-			service_name : 'cogo_academy',
+			service_name : 'faq',
 		},
 		{
 			api          : 'create_faq_topic',
 			access_type  : 'private',
-			service_name : 'cogo_academy',
+			service_name : 'faq',
 		},
 		{
 			api          : 'get_faq_topic',
 			access_type  : 'private',
-			service_name : 'cogo_academy',
+			service_name : 'faq',
 		},
 		{
 			api          : 'update_faq_topic',
 			access_type  : 'private',
-			service_name : 'cogo_academy',
+			service_name : 'faq',
 		},
 		{
 			api          : 'list_faq_topics',
 			access_type  : 'private',
-			service_name : 'cogo_academy',
+			service_name : 'faq',
 		},
 		{
 			api          : 'list_faq_tags',
 			access_type  : 'private',
-			service_name : 'cogo_academy',
+			service_name : 'faq',
 		},
 		{
 			api          : 'update_question_answer_set',
 			access_type  : 'private',
-			service_name : 'cogo_academy',
+			service_name : 'faq',
 		},
 		{
 			api          : 'list_cogo_entities',
@@ -43,52 +43,52 @@ const faq = {
 		{
 			api          : 'create_question_answer_set',
 			access_type  : 'private',
-			service_name : 'cogo_academy',
+			service_name : 'faq',
 		},
 		{
 			api          : 'get_question',
 			access_type  : 'private',
-			service_name : 'cogo_academy',
+			service_name : 'faq',
 		},
 		{
 			api          : 'bulk_create_question_answer_set',
 			access_type  : 'private',
-			service_name : 'cogo_academy',
+			service_name : 'faq',
 		},
 		{
 			api          : 'create_faq_tag',
 			access_type  : 'private',
-			service_name : 'cogo_academy',
+			service_name : 'faq',
 		},
 		{
 			api          : 'get_faq_tag',
 			access_type  : 'private',
-			service_name : 'cogo_academy',
+			service_name : 'faq',
 		},
 		{
 			api          : 'update_faq_tag',
 			access_type  : 'private',
-			service_name : 'cogo_academy',
+			service_name : 'faq',
 		},
 		{
 			api          : 'create_faq_audience',
 			access_type  : 'private',
-			service_name : 'cogo_academy',
+			service_name : 'faq',
 		},
 		{
 			api          : 'list_faq_audiences',
 			access_type  : 'private',
-			service_name : 'cogo_academy',
+			service_name : 'faq',
 		},
 		{
 			api          : 'get_faq_audience',
 			access_type  : 'private',
-			service_name : 'cogo_academy',
+			service_name : 'faq',
 		},
 		{
 			api          : 'update_faq_audience',
 			access_type  : 'private',
-			service_name : 'cogo_academy',
+			service_name : 'faq',
 		},
 
 	],
@@ -96,37 +96,37 @@ const faq = {
 		{
 			api          : 'create_question_answer_set',
 			access_type  : 'private',
-			service_name : 'cogo_academy',
+			service_name : 'faq',
 		},
 		{
 			api          : 'list_faq_topics',
 			access_type  : 'private',
-			service_name : 'cogo_academy',
+			service_name : 'faq',
 		},
 		{
 			api          : 'list_faq_tags',
 			access_type  : 'private',
-			service_name : 'cogo_academy',
+			service_name : 'faq',
 		},
 		{
 			api          : 'list_faq_questions',
 			access_type  : 'private',
-			service_name : 'cogo_academy',
+			service_name : 'faq',
 		},
 		{
 			api          : 'get_question',
 			access_type  : 'private',
-			service_name : 'cogo_academy',
+			service_name : 'faq',
 		},
 		{
 			api          : 'create_faq_feedback',
 			access_type  : 'private',
-			service_name : 'cogo_academy',
+			service_name : 'faq',
 		},
 		{
 			api          : 'update_faq_feedback',
 			access_type  : 'private',
-			service_name : 'cogo_academy',
+			service_name : 'faq',
 		},
 	],
 };
