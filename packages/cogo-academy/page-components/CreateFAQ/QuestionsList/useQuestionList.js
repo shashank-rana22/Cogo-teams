ef721581c3681a11a027423cccad3f950cc2a4bb
Namanagarwal015 <124123426+Namanagarwal015@users.noreply.github.com>
2023-03-08 19:49:43 +0530

    author_changes

--- packages/cogo-academy/page-components/CreateFAQ/QuestionsList/useQuestionList.js
+++ packages/cogo-academy/page-components/CreateFAQ/QuestionsList/useQuestionList.js
@@ -113,7 +113,7 @@ const requestedQuestionsColumns = ({ deactivateQuestion, onClickEditButton }) =>
 		Header   : 'CREATED BY',
 		accessor : (items) => (
 			<div>
-				{items?.created_by || '-'}
+				{items?.author?.name || '-'}
 			</div>
 		),
 	},
@@ -189,6 +189,8 @@ const useQuestionList = () => {
 					is_admin_view            : true,
 					faq_tags_data_required   : true,
 					faq_topics_data_required : true,
+					author_data_required :true,
+
 
 				},
 			});
