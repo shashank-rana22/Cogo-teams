dc71a3b6b40d23dbfb4248be7d62bd3a61c1d2c6
Likhilesh Balpande <likhilesh@Likhileshs-MacBook-Air.local>
2023-03-08 13:48:32 +0530

    fix: week issue, todo: week pagintn & optimiztn

--- packages/cogo-one/page-components/CogoOneDashboard/common/Calendar/index.js
+++ packages/cogo-one/page-components/CogoOneDashboard/common/Calendar/index.js
@@ -1,4 +1,3 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import { IcMArrowDoubleLeft, IcMArrowDoubleRight } from '@cogoport/icons-react';
 import { format } from '@cogoport/utils';
 import React, { useState, useEffect, useRef } from 'react';
@@ -30,8 +29,8 @@ function Calendar({ props }) {
 			subLabel : 'MMM yy',
 		},
 		week: {
-			label    : '',
-			subLabel : 'MMM',
+			label    : 'MMM yyyy',
+			subLabel : 'dd',
 		},
 		month: {
 			label    : 'MMM',
@@ -45,40 +44,49 @@ function Calendar({ props }) {
 		return d;
 	};
 
-	function getWeeksOfMonth(month, year) {
-		const newWeeks = [];
-		const firstDate = new Date(year, month, 1);
-		const lastDate = new Date(year, month + 1, 0);
-		const numDays = lastDate.getDate();
+	// function getWeeksOfMonth(month, year) {
+	// 	const newWeeks = [];
+	// 	const firstDate = new Date(year, month, 1);
+	// 	const lastDate = new Date(year, month + 1, 0);
+	// 	const numDays = lastDate.getDate();
 
-		let iterator = 1;
-		let start = 1;
-		let end = 7 - firstDate.getDay() + 1;
-		if (firstDate.getDay() === 0) {
-			end = 1;
-		}
-		while (start <= numDays) {
-			newWeeks.push({
-				start,
-				end,
-				date: new Date(year, month, start),
-				iterator,
-			});
-			iterator += 1;
-			start = end + 1;
-			end += 7;
-			end = start === 1 && end === 8 ? 1 : end;
-			if (end > numDays) {
-				end = numDays;
-			}
-		}
-		return newWeeks.reverse();
-	}
+	// 	let iterator = 1;
+	// 	let start = 1;
+	// 	let end = 7 - firstDate.getDay() + 1;
+	// 	if (firstDate.getDay() === 0) {
+	// 		end = 1;
+	// 	}
+	// 	while (start <= numDays) {
+	// 		newWeeks.push({
+	// 			start,
+	// 			end,
+	// 			date: new Date(year, month, start),
+	// 			iterator,
+	// 		});
+	// 		iterator += 1;
+	// 		start = end + 1;
+	// 		end += 7;
+	// 		end = start === 1 && end === 8 ? 1 : end;
+	// 		if (end > numDays) {
+	// 			end = numDays;
+	// 		}
+	// 	}
+	// 	return newWeeks.reverse();
+	// }
 
 	const calcMonth = (subtractMonths) => {
 		const d = new Date();
 		d.setDate(1);
 		d.setMonth(d.getMonth() - subtractMonths);
+		return d;
+	};
+
+	const calcWeek = (subtractWeeks) => {
+		const d = new Date();
+		if (d.getDay() !== 1) {
+			d.setDate(d.getDate() - ((d.getDay() + 6) % 7));
+		}
+		d.setDate(d.getDate() - (subtractWeeks * 7));
 		return d;
 	};
 
@@ -118,37 +126,61 @@ function Calendar({ props }) {
 		setCalendarData([...data, ...calendarData]);
 	};
 
-	function getMonday(d) {
-		const newD = new Date(d);
-		const day = newD.getDay();
-		const diff = newD.getDate() - day + (day === 0 ? -6 : 1);
-		return new Date(newD.setDate(diff));
-	}
-
 	const loadWeeks = () => {
-		let newWeeks = [];
-		for (let i = 0;
-			i < (weekPagination * numberOfMonthsForWeeks + (3 * numberOfMonthsForWeeks));
+		const newData = [];
+		for (let i = numberOfDays + (shiftDays * (weekPagination - 1));
+			i < numberOfDays + (weekPagination * shiftDays);
 			i += 1) {
-			const newDate = calcMonth(i);
-			newWeeks = [...newWeeks, ...getWeeksOfMonth(newDate.getMonth(), newDate.getFullYear(), newDate)];
+			newData.push(calcWeek(i));
 		}
-		const todayDate = new Date();
+		// console.log('newData:', newData);
 		const data = [];
-		newWeeks.reverse().forEach((week, index) => {
-			if (todayDate.getTime() >= week.date.getTime()) {
-				data.push(
-					{
-						key      : `cal-week-${pagination}-${index}`,
-						label    : `Week ${week.iterator}`,
-						subLabel : `${format(week.date, 'MMM')} ${week.start} to ${week.end}`,
-						date     : getMonday(new Date(week.date)),
-					},
-				);
-			}
+		newData.reverse().forEach((item, iterator) => {
+			const startDate = new Date(item);
+			const endDate = startDate;
+			endDate.setDate(startDate.getDate() + 6);
+			data.push({
+				key      : `cal-${timeline}-${pagination}-${iterator}`,
+				label    : format(item, FORMAT_TYPE[timeline].label),
+				subLabel : `${format(item, FORMAT_TYPE[timeline].subLabel)} to ${format(endDate, FORMAT_TYPE[timeline].subLabel)}`,
+				date     : item,
+				endDate,
+			});
 		});
 		setCalendarData(data);
 	};
+
+	// function getMonday(d) {
+	// 	const newD = new Date(d);
+	// 	const day = newD.getDay();
+	// 	const diff = newD.getDate() - day + (day === 0 ? -6 : 1);
+	// 	return new Date(newD.setDate(diff));
+	// }
+
+	// const loadWeeks = () => {
+	// 	let newWeeks = [];
+	// 	for (let i = 0;
+	// 		i < (weekPagination * numberOfMonthsForWeeks + (3 * numberOfMonthsForWeeks));
+	// 		i += 1) {
+	// 		const newDate = calcMonth(i);
+	// 		newWeeks = [...newWeeks, ...getWeeksOfMonth(newDate.getMonth(), newDate.getFullYear(), newDate)];
+	// 	}
+	// 	const todayDate = new Date();
+	// 	const data = [];
+	// 	newWeeks.reverse().forEach((week, index) => {
+	// 		if (todayDate.getTime() >= week.date.getTime()) {
+	// 			data.push(
+	// 				{
+	// 					key      : `cal-week-${pagination}-${index}`,
+	// 					label    : `Week ${week.iterator}`,
+	// 					subLabel : `${format(week.date, 'MMM')} ${week.start} to ${week.end}`,
+	// 					date     : getMonday(new Date(week.date)),
+	// 				},
+	// 			);
+	// 		}
+	// 	});
+	// 	setCalendarData(data);
+	// };
 
 	useEffect(() => {
 		setCalendarData([]);
@@ -160,29 +192,26 @@ function Calendar({ props }) {
 	}, [timeline]);
 
 	function addPagination(x) {
-		console.log('___x___: ', x);
 		if (timeline === 'day' || timeline === 'month') {
 			setPagination(x);
 		} else {
-			// setWeekPagination(x);  Week Function Check!!!
+			console.log('___x___: ', x);
+			// setWeekPagination(x);
 		}
 	}
 
-	useEffect(() => {
+	function doPagination() {
 		if (pagination !== 0) {
 			if (timeline === 'day') addProcessData(calcDate);
 			else if (timeline === 'month') addProcessData(calcMonth);
-			else loadWeeks();
+			return;
 		}
-	}, [pagination]);
+		if (weekPagination !== 0) loadWeeks();
+	}
 
 	useEffect(() => {
-		if (weekPagination !== 0) {
-			if (timeline === 'day') addProcessData(calcDate);
-			else if (timeline === 'month') addProcessData(calcMonth);
-			else loadWeeks();
-		}
-	}, [weekPagination]);
+		doPagination();
+	}, [pagination, weekPagination]);
 
 	useEffect(() => {
 		console.log('calendarData', calendarData);
