b0a6c776e2c2318b4430fd5be0e3eafa7dfbd5c0
Chandrakant26 <chandrakant.vangule@cogoport.com>
2023-03-08 17:50:50 +0530

    revert scope

--- packages/cogo-one/page-components/CogoOneDashboard/common/Header/index.js
+++ packages/cogo-one/page-components/CogoOneDashboard/common/Header/index.js
@@ -1,6 +1,4 @@
 import { Tabs, TabPanel } from '@cogoport/components';
-// import ScopeSelector from '@cogoport/forms/page-components/Business/ScopeSelect';
-// import { IcMFilter } from '@cogoport/icons-react';
 import { IcMArrowBack } from '@cogoport/icons-react';
 import { useRouter } from '@cogoport/next';
 import React from 'react';
@@ -12,8 +10,6 @@ import styles from './styles.module.css';
 function Header({ timeline, setTimeline }) {
 	const { query, back } = useRouter();
 	const { id: agentId = '' } = query || {};
-	// const [filterVisible, setFilterVisible] = useState(false);
-	// const [filters, setFilters] = useState('');
 
 	return (
 		<div className={styles.container}>
@@ -31,27 +27,7 @@ function Header({ timeline, setTimeline }) {
 			</div>
 
 			<div className={styles.header_right_section}>
-
-				{/* <ScopeSelector /> */}
 				<div className={styles.filter_dot} />
-				{/* <div className={styles.filter_icon}>
-        <Popover
-            placement="left"
-            render={(
-                <Filter
-                    setFilterVisible={setFilterVisible}
-                    filters={filters}
-                    setFilters={setFilters}
-                />
-            )}
-            visible={filterVisible}
-            onClickOutside={() => setFilterVisible(false)}
-        >
-
-            <IcMFilter width={25} height={25} onClick={() => setFilterVisible(!filterVisible)} />
-        </Popover>
-    </div> */}
-
 				<Tabs
 					activeTab={timeline}
 					themeType="tertiary"
@@ -62,9 +38,7 @@ function Header({ timeline, setTimeline }) {
 					<TabPanel name="week" title="Week" />
 					<TabPanel name="month" title="Month" />
 				</Tabs>
-
 			</div>
-
 		</div>
 	);
 }
