fa5f1fdd9d1baaa0d8522e7c877bc0175a10a194
Rahul Danampally <Danampally.Rahul@cogoport.com>
2023-03-10 20:29:11 +0530

    added filter for agent

--- packages/cogo-one/configurations/filter-controls.js
+++ packages/cogo-one/configurations/filter-controls.js
@@ -1,51 +1,94 @@
-const controls = [
-	{
-		label     : '',
-		name      : 'status',
-		type      : 'radio',
-		className : 'status_field_controller',
-		value     : '',
-		options   : [
-			{
-				label : 'Unread',
-				value : 'unread',
-			},
-			{
-				label : 'All',
-				value : 'all',
-			},
-		],
-	},
-	{
-		label     : 'Channels',
-		name      : 'channels',
-		type      : 'checkboxgroup',
-		className : 'channels_field_controller',
-		multiple  : true,
-		value     : [],
-		options   : [
-			{ label: 'Whatsapp', value: 'whatsapp' },
-			{ label: 'Platform Chat', value: 'platform_chat' },
-		],
-	},
-	{
-		label     : 'Escalation',
-		name      : 'escalation',
-		type      : 'radio',
-		value     : '',
-		className : 'escalation_field_controller',
-		options   : [
-			{
-				label : 'Warning',
-				value : 'warning',
-			},
-			{
-				label : 'Escalated',
-				value : 'escalated',
-			},
+import useGetAsyncOptions from '@cogoport/forms/hooks/useGetAsyncOptions';
+import { asyncFieldsListAgents } from '@cogoport/forms/utils/getAsyncFields';
+import { merge } from '@cogoport/utils';
 
-		],
-	},
-];
+const useGetControls = () => {
+	const listAgentsOptions = useGetAsyncOptions(
+		merge(asyncFieldsListAgents()),
+	);
 
-export default controls;
+	const controls = [
+		{
+			label     : '',
+			name      : 'status',
+			type      : 'radio',
+			className : 'status_field_controller',
+			value     : '',
+			options   : [
+				{
+					label : 'Unread',
+					value : 'unread',
+				},
+				{
+					label : 'All',
+					value : 'all',
+				},
+			],
+		},
+		{
+			label     : 'Channels',
+			name      : 'channels',
+			type      : 'checkboxgroup',
+			className : 'channels_field_controller',
+			multiple  : true,
+			value     : [],
+			options   : [
+				{ label: 'Whatsapp', value: 'whatsapp' },
+				{ label: 'Platform Chat', value: 'platform_chat' },
+			],
+		},
+		{
+			label     : 'Escalation',
+			name      : 'escalation',
+			type      : 'radio',
+			value     : '',
+			className : 'escalation_field_controller',
+			options   : [
+				{
+					label : 'Warning',
+					value : 'warning',
+				},
+				{
+					label : 'Escalated',
+					value : 'escalated',
+				},
+
+			],
+		},
+		{
+			label        : 'Assigned To',
+			name         : 'assigned_to',
+			type         : 'radio',
+			value        : '',
+			className    : 'escalation_field_controller',
+			onlyForAdmin : true,
+			options      : [
+				{
+					label : 'Me',
+					value : 'me',
+				},
+				{
+					label : 'Agent',
+					value : 'agent',
+				},
+
+			],
+		},
+		{
+			label        : '',
+			name         : 'assigned_agent',
+			type         : 'select',
+			value        : '',
+			onlyForAdmin : true,
+			className    : 'escalation_field_controller',
+			placeholder  : 'Select Agent',
+			rules        : {
+				required: 'This is Requied',
+			},
+			...(listAgentsOptions || {}),
+		},
+	];
+	return controls;
+};
+
+export default useGetControls;
--- packages/cogo-one/page-components/CogoOneChannel/Customers/FilterComponents/index.js
+++ packages/cogo-one/page-components/CogoOneChannel/Customers/FilterComponents/index.js
@@ -3,12 +3,12 @@ import { useForm } from '@cogoport/forms';
 import { isEmpty } from '@cogoport/utils';
 import { useState, useEffect } from 'react';
 
-import filterControls from '../../../../configurations/filter-controls';
+import useGetControls from '../../../../configurations/filter-controls';
 
 import Item from './Item';
 import styles from './styles.module.css';
 
-function getDefaultValues({ filters }) {
+function getDefaultValues({ filters, filterControls }) {
 	let defaultValues = {};
 
 	filterControls.forEach((item) => {
@@ -32,13 +32,17 @@ function FilterComponents({
 }) {
 	const [botToggle, setBotToggle] = useState(false);
 
-	const defaultValues = getDefaultValues({ filters: appliedFilters });
+	const filterControls = useGetControls();
+
+	const defaultValues = getDefaultValues({ filters: appliedFilters, filterControls });
 
 	const {
-		control, formState: { errors }, watch, setValue,
+		control, formState: { errors }, watch, setValue, handleSubmit,
 	} = useForm({ defaultValues });
 
 	const formValues = watch();
+	const { assigned_to = '' } = formValues || {};
+	const showElements = { assigned_agent: assigned_to === 'agent' };
 
 	let filterValues = {};
 
@@ -54,7 +58,12 @@ function FilterComponents({
 		}
 	// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [botToggle]);
-
+	useEffect(() => {
+		if (assigned_to === 'me') {
+			setValue('assigned_agent', '');
+		}
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [assigned_to]);
 	useEffect(() => {
 		setBotToggle(showBotMessages);
 	}, [showBotMessages]);
@@ -73,9 +82,40 @@ function FilterComponents({
 		setFilterVisible(false);
 		setShowBotMessages(botToggle);
 	};
-
+	const renderComp = (singleField) => {
+		const show = !(singleField?.name in showElements) || showElements[singleField?.name];
+		if (singleField?.onlyForAdmin) {
+			if (isomniChannelAdmin) {
+				return (
+					show && (
+						<Item
+							{...singleField}
+							control={control}
+							value={formValues[singleField.name]}
+							setValue={setValue}
+							error={errors[singleField.name]}
+							botToggle={botToggle}
+						/>
+					)
+				);
+			}
+			return null;
+		}
+		return (
+			show && (
+				<Item
+					{...singleField}
+					control={control}
+					value={formValues[singleField.name]}
+					setValue={setValue}
+					error={errors[singleField.name]}
+					botToggle={botToggle}
+				/>
+			)
+		);
+	};
 	return (
-		<div className={styles.container}>
+		<form className={styles.container} onSubmit={handleSubmit(handleClick)}>
 			<div className={styles.header}>
 				<div className={styles.title}>
 					Filters
@@ -112,23 +152,15 @@ function FilterComponents({
 
 			{filterControls.map((field) => (
 				<div className={cl`${styles.filter_container} ${botToggle ? styles.disabled : ''}`} key={field.name}>
-					<Item
-						{...field}
-						control={control}
-						value={formValues[field.name]}
-						setValue={setValue}
-						error={errors[field.name]}
-						botToggle={botToggle}
-
-					/>
+					{renderComp(field)}
 				</div>
 			))}
 
 			<div className={styles.actions}>
 				<Button size="md" themeType="tertiary" onClick={() => setFilterVisible(false)}>Cancel</Button>
-				<Button size="md" themeType="accent" onClick={() => handleClick()}>Apply</Button>
+				<Button size="md" themeType="accent" type="submit">Apply</Button>
 			</div>
-		</div>
+		</form>
 	);
 }
 
