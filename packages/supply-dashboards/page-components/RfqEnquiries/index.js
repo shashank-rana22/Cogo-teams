dbad38a5d9385dd1073b01fad9be46aa12721b54
Mayank Shahria <mayank.shahria@cogoport.com>
2023-03-13 12:27:14 +0530

    update the id to constant

--- packages/supply-dashboards/page-components/RfqEnquiries/index.js
+++ packages/supply-dashboards/page-components/RfqEnquiries/index.js
@@ -3,6 +3,8 @@ import { useSelector } from '@cogoport/store';
 
 import RfqEnquiries from './Content';
 
+const VIETNAM_PARTNER_ID = 'b67d40b1-616c-4471-b77b-de52b4c9f2ff';
+
 function RfqEnquiriesView() {
 	const partnerId = useSelector((state) => state?.profile?.partner?.id);
 	const activeTab = 'rfq_enquiries';
@@ -20,9 +22,9 @@ function RfqEnquiriesView() {
 				<TabPanel name="live_bookings" title="Live Bookings">--</TabPanel>
 				<TabPanel name="trade_enquiry" title="Missing Rates">--</TabPanel>
 				<TabPanel name="disliked_rates" title="Disliked Rates">--</TabPanel>
-				{partnerId !== 'b67d40b1-616c-4471-b77b-de52b4c9f2ff' && (
-				<TabPanel name="rate_density" title="Rate Density & Coverage">--</TabPanel>)}
-				<TabPanel name="manage_forecast" title="Manage Forcast">--</TabPanel>
+				{partnerId !== VIETNAM_PARTNER_ID && (
+					<TabPanel name="rate_density" title="Rate Density & Coverage">--</TabPanel>)}
+				<TabPanel name="manage_forecast" title="Manage Forecast">--</TabPanel>
 				<TabPanel name="rfq_enquiries" title="RFQ Enquiries">
 					<RfqEnquiries />
 				</TabPanel>
