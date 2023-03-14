ef02dfa006580d7fd9b04650c17f2b3efaa5449b
mona singh <mona.singh@cogoport.com>
2023-03-14 19:23:15 +0530

    add bar chart

--- packages/business-finance/page-components/CFODashboard/Logistics/IncomeExpense/index.tsx
+++ packages/business-finance/page-components/CFODashboard/Logistics/IncomeExpense/index.tsx
@@ -1,9 +1,47 @@
 import { IcMInfo } from '@cogoport/icons-react';
 import React from 'react';
+import ResponsiveBarChart from './ResponsiveBarChart/index'
 
 import styles from './styles.module.css';
 
 function IncomeExpense() {
+	const barData=[
+		{
+		  "months": "Jan",
+		  "income": 79,
+		  "expense": 154,
+		},
+		{
+		  "months": "Feb",
+		  "income": 111,
+		  "expense": 185,
+		},
+		{
+		  "months": "Mar",
+		  "income": 164,
+		  "expense": 70,
+		},
+		{
+		  "months": "Apr",
+		  "income": 188,
+		  "expense": 165,
+		},
+		{
+		  "months": "Jun",
+		  "income": 166,
+		  "expense": 200,
+		},
+		{
+		  "months": "Jul",
+		  "income": 115,
+		  "expense": 68,
+		},
+		{
+		  "months": "Aug",
+		  "income": 115,
+		  "expense": 177,
+		}
+	  ]
 	return (
 		<div>
 			<div className={styles.card}>
@@ -11,6 +49,9 @@ function IncomeExpense() {
 					Income & Expense
 					<IcMInfo />
 					<div className={styles.border} />
+				</div>
+				<div className={styles.responsive}>
+					<ResponsiveBarChart barData={barData}/>
 				</div>
 			</div>
 		</div>
