e24617b2aa5b808b9c9677ed06a001be5cc1e155
Lalith Phanindra Patibanda <lalithphanindrapatibanda@pp.local>
2023-03-15 13:16:07 +0530

    createnewtestui

--- packages/test-module/page-components/HomePage/index.js
+++ packages/test-module/page-components/HomePage/index.js
@@ -1,8 +1,13 @@
 // import CreateQuestionSet from '../CreateModule/components/CreateQuestionSet';
-import CreateTest from '../CreateModule/components/CreateTest';
+// import CreateTest from '../CreateModule/components/CreateTest';
+
+// import BasicDetailsForm from '../CreateModule/components/CreateQuestionSet/components/BasicDetailsForm';
+import CreateNewTest from '../CreateModule/components/CreateNewTest';
 
 function HomePage() {
 	// return <CreateQuestionSet />;
-	return <CreateTest />;
+	// return <CreateTest />;
+	// return <BasicDetailsForm />;
+	return <CreateNewTest />;
 }
 export default HomePage;
