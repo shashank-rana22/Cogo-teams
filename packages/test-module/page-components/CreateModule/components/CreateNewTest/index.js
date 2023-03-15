07671a40e6b6fa42aac99d2f93a4a643358b183c
Lalith Phanindra Patibanda <lalithphanindrapatibanda@pp.local>
2023-03-15 13:41:34 +0530

    createtestui

--- packages/test-module/page-components/CreateModule/components/CreateNewTest/index.js
+++ packages/test-module/page-components/CreateModule/components/CreateNewTest/index.js
@@ -1,4 +1,5 @@
 import { useForm } from '@cogoport/forms';
+import { IcMArrowBack } from '@cogoport/icons-react';
 
 import getElementController from '../../../../configs/getElementController';
 
@@ -11,43 +12,49 @@ function CreateNewTest() {
 	const controls = getControls();
 
 	return (
-		<div className={styles.container}>
-			{controls.map((controlItem) => {
-				const { type, label, name, use = [] } = controlItem || {};
+		<div>
+			<div className={styles.header}>
+				<IcMArrowBack width={20} height={20} />
+				<div className={styles.title}>New Test</div>
+			</div>
+			<div className={styles.container}>
+				{controls.map((controlItem) => {
+					const { type, label, name, use = [] } = controlItem || {};
 
-				const Element = getElementController(type);
-				if (name === 'select_entity_usergroups') {
+					const Element = getElementController(type);
+					if (name === 'select_entity_usergroups') {
+						return (
+							<div className={styles.control_container}>
+								<div className={`${styles.label} ${styles.label_type}`}>
+									{label}
+								</div>
+								<div className={styles.control_type}>
+
+									{use.map((inp) => (
+										<div>
+											<Element control={control} {...inp} />
+											{errors[name] && <div className={styles.error_msg}>This is required</div>}
+										</div>
+									))}
+								</div>
+
+							</div>
+						);
+					}
 					return (
 						<div className={styles.control_container}>
-							<div className={`${styles.label} ${styles.label_type}`}>
+							<div className={styles.label}>
 								{label}
 							</div>
-							<div className={styles.control_type}>
 
-								{use.map((inp) => (
-									<div>
-										<Element control={control} {...inp} />
-										{errors[name] && <div className={styles.error_msg}>This is required</div>}
-									</div>
-								))}
+							<div className={styles.control}>
+								<Element control={control} {...controlItem} />
+								{errors[name] && <div className={styles.error_msg}>This is required</div>}
 							</div>
-
 						</div>
 					);
-				}
-				return (
-					<div className={styles.control_container}>
-						<div className={styles.label}>
-							{label}
-						</div>
-
-						<div className={styles.control}>
-							<Element control={control} {...controlItem} />
-							{errors[name] && <div className={styles.error_msg}>This is required</div>}
-						</div>
-					</div>
-				);
-			})}
+				})}
+			</div>
 		</div>
 	);
 }
