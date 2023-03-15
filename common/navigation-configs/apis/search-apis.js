f69c4bd96e2c12d769155d97a251caf4a90adf2e
niranjan1375 <potnuru.niranjan@cogoport.com>
2023-03-15 10:52:23 +0530

    get_exchange_rate

--- common/navigation-configs/apis/search-apis.js
+++ common/navigation-configs/apis/search-apis.js
@@ -426,5 +426,10 @@ const apis = [
 		access_type  : 'private',
 		service_name : 'user',
 	},
+	{
+		api          : 'get_exchange_rate',
+		access_type  : 'private',
+		service_name : 'exchange_rate',
+	},
 ];
 export default apis;
