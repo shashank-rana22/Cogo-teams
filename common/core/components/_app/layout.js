dd7485f9214bf5eb796e5520ebd8ec9ff09b8869
Prachi Dewangan <prachi.dewangan@cogoport.com>
2023-03-15 19:26:27 +0530

    permission for chat faq added

--- common/core/components/_app/layout.js
+++ common/core/components/_app/layout.js
@@ -1,11 +1,21 @@
 import navigationMappingAdmin from '@cogoport/navigation-configs/navigation-mapping-admin';
+import { useSelector } from '@cogoport/store';
 import React from 'react';
 
 import AdminLayout from '../AdminLayout';
 import ChatFAQs from '../AdminLayout/FAQs';
 
 function Layout({ children, layout }) {
+
 	const hideLayout = layout === 'hidden';
+
+	const { profile = {} } = useSelector((state) => state);
+
+	const { auth_role_data = []} = profile;
+
+	const { role_functions = []} = auth_role_data|| {};
+
+	console.log('role_functions',role_functions);
 
 	if (hideLayout) {
 		return <div>{children}</div>;
@@ -17,8 +27,9 @@ function Layout({ children, layout }) {
 			showTopbar
 			navbar={navigationMappingAdmin}
 		>
+			
 			<div style={{ margin: 0, padding: '24px 20px' }}>
-				<ChatFAQs />
+				{role_functions.includes('sales') &&  <ChatFAQs />}
 				{children}
 
 			</div>
