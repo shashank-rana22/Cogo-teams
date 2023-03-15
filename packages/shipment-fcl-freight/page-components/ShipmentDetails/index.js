50d207811c4e770ddd5499f14ff35b56a0536400
ankit-rajpoot <ankit.rajpoot@cogoport.com>
2023-03-15 21:10:04 +0530

    fix routes for chat

--- packages/shipment-fcl-freight/page-components/ShipmentDetails/index.js
+++ packages/shipment-fcl-freight/page-components/ShipmentDetails/index.js
@@ -3,10 +3,12 @@ import React, { useState } from 'react';
 import Header from './Header';
 import Tab from './Tabs';
 import Timeline from './TimeLine';
+import { ShipmentChat } from '@cogoport/shipment-chat';
 
 function ShipmentDetails() {
 	return (
 		<div>
+			<ShipmentChat />
 			<Header />
 			<Timeline />
 			<Tab />
