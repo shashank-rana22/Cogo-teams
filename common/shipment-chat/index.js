06eba86f1a2d825069f8e0bec15d8cc1a0b267c7
ankit-rajpoot <ankit.rajpoot@cogoport.com>
2023-03-15 20:33:15 +0530

    create chat package

---
+++ common/shipment-chat/index.js
@@ -0,0 +1,1 @@
+export { default as shipmentChat } from './page-components';
---
+++ common/shipment-chat/page-components/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function shipmentChat() {
+    return (
+        <div>
+            Hello world
+        </div>
+    )
+}
+
+export default shipmentChat;
