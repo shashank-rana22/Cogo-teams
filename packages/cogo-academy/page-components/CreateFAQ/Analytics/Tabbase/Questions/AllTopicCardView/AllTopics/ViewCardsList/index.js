7572619936fd58681da1775a3bc13cd352db5a4e
Namanagarwal015 <124123426+Namanagarwal015@users.noreply.github.com>
2023-03-16 16:42:07 +0530

    No Data Handelling solved

--- packages/cogo-academy/page-components/CreateFAQ/Analytics/Tabbase/Questions/AllTopicCardView/AllTopics/ViewCardsList/index.js
+++ packages/cogo-academy/page-components/CreateFAQ/Analytics/Tabbase/Questions/AllTopicCardView/AllTopics/ViewCardsList/index.js
@@ -4,6 +4,18 @@ import styles from './styles.module.css';
 
 function ViewCardsList({ cardHeading = '', contentQuestion = [{}] }) {
 	const truncate = (str) => (str?.length > 28 ? `${str.substring(0, 26)}...` : str);
+	if (!contentQuestion[0]) {
+		return (
+			<div className={styles.primary_right}>
+				<div className={styles.active_users}>
+					<div className={styles.right_stat_label}>
+						{cardHeading}
+					</div>
+					<div className={styles.sub_heading} style={{ textAlign: 'center' }}>No Data Available</div>
+				</div>
+			</div>
+		);
+	}
 	return (
 
 		<div className={styles.primary_right}>
@@ -11,55 +23,67 @@ function ViewCardsList({ cardHeading = '', contentQuestion = [{}] }) {
 				<div className={styles.right_stat_label}>
 					{cardHeading}
 				</div>
-
 				<div className={styles.sub_heading}>
-					<div style={{ display: 'flex', justifyContent: 'space-between' }}>
-						1.
-						<div style={{ marginLeft: '-9px' }}>
-							{truncate(contentQuestion[0]?.question_abstract)}
-						</div>
-						<div style={{ display: 'flex' }}>
-							<div style={{ marginRight: '0.25rem' }}>{contentQuestion[0]?.view_count}</div>
-							<IcMEyeopen style={{ marginTop: '0.15rem' }} />
-						</div>
+					{(contentQuestion[0])
+						? (
+							<div style={{ display: 'flex', justifyContent: 'space-between' }}>
+								1.
+								<div style={{ marginLeft: '-9px' }}>
+									{truncate(contentQuestion[0]?.question_abstract)}
+								</div>
+								<div style={{ display: 'flex' }}>
+									<div style={{ marginRight: '0.25rem' }}>{contentQuestion[0]?.view_count}</div>
+									<IcMEyeopen style={{ marginTop: '0.15rem' }} />
+								</div>
 
-					</div>
-					<div style={{ display: 'flex', justifyContent: 'space-between' }}>
-						2.
-						<div style={{ marginLeft: '-9px' }}>
-
-							{truncate(contentQuestion[1]?.question_abstract)}
-						</div>
-						<div style={{ display: 'flex' }}>
-							<div style={{ marginRight: '0.25rem' }}>{contentQuestion[1]?.view_count}</div>
-							<IcMEyeopen style={{ marginTop: '0.15rem' }} />
-						</div>
-
-					</div>
-					<div style={{ display: 'flex', justifyContent: 'space-between' }}>
-						3.
-						<div style={{ marginLeft: '-29px' }}>{truncate(contentQuestion[2]?.question_abstract)}</div>
-						<div style={{ display: 'flex' }}>
-							<div style={{ marginRight: '0.25rem' }}>
-
-								{contentQuestion[2]?.view_count}
 							</div>
-							<IcMEyeopen style={{ marginTop: '0.15rem' }} />
-						</div>
+						) : null}
 
-					</div>
-					<div style={{ display: 'flex', justifyContent: 'space-between' }}>
-						4.
-						<div style={{ marginLeft: '-5px' }}>{truncate(contentQuestion[3]?.question_abstract)}</div>
-						<div style={{ display: 'flex' }}>
-							<div style={{ marginRight: '0.25rem' }}>
+					{(contentQuestion[1])
+						? (
+							<div style={{ display: 'flex', justifyContent: 'space-between' }}>
+								2.
+								<div style={{ marginLeft: '-9px' }}>
 
-								{contentQuestion[3]?.view_count}
+									{truncate(contentQuestion[1]?.question_abstract)}
+								</div>
+								<div style={{ display: 'flex' }}>
+									<div style={{ marginRight: '0.25rem' }}>{contentQuestion[1]?.view_count}</div>
+									<IcMEyeopen style={{ marginTop: '0.15rem' }} />
+								</div>
+
 							</div>
-							<IcMEyeopen style={{ marginTop: '0.15rem' }} />
-						</div>
+						) : null}
+					{(contentQuestion[2])
+						? (
+							<div style={{ display: 'flex', justifyContent: 'space-between' }}>
+								2.
+								<div style={{ marginLeft: '-9px' }}>
 
-					</div>
+									{truncate(contentQuestion[2]?.question_abstract)}
+								</div>
+								<div style={{ display: 'flex' }}>
+									<div style={{ marginRight: '0.25rem' }}>{contentQuestion[2]?.view_count}</div>
+									<IcMEyeopen style={{ marginTop: '0.15rem' }} />
+								</div>
+
+							</div>
+						) : null}
+					{(contentQuestion[3])
+						? (
+							<div style={{ display: 'flex', justifyContent: 'space-between' }}>
+								2.
+								<div style={{ marginLeft: '-9px' }}>
+
+									{truncate(contentQuestion[3]?.question_abstract)}
+								</div>
+								<div style={{ display: 'flex' }}>
+									<div style={{ marginRight: '0.25rem' }}>{contentQuestion[3]?.view_count}</div>
+									<IcMEyeopen style={{ marginTop: '0.15rem' }} />
+								</div>
+
+							</div>
+						) : null}
 
 				</div>
 
