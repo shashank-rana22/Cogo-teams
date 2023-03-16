ada73d653d2131122543ca95e192360d37d73f08
Namanagarwal015 <124123426+Namanagarwal015@users.noreply.github.com>
2023-03-16 22:21:01 +0530

    pie-chart

--- packages/cogo-academy/page-components/CreateFAQ/Analytics/GraphUI/GraphData/index.js
+++ packages/cogo-academy/page-components/CreateFAQ/Analytics/GraphUI/GraphData/index.js
@@ -20,38 +20,61 @@ function GraphData() {
 		total_viewed_only_questions = 0,
 		total_viewed_only_questions_percentage = 0,
 	} = pie_chart;
+	const Like = (total_search_result_available_percentage / 100) * total_like_percentage;
+	const Dislike = (total_search_result_available_percentage / 100) * total_dislike_percentage;
+	const Viewed = (total_search_result_available_percentage / 100) * total_viewed_only_questions_percentage;
+	const Available_Remaining = total_search_result_available_percentage - (Like + Dislike + Viewed);
+	const Requested = (total_search_result_not_available_percentage / 100) * total_requested_questions_percentage;
 	const pie_data = [
 		{
 			id    : 'Search Available',
 			label : 'Search Available',
-			value : total_search_result_available,
+			value : total_search_result_available_percentage,
+			color : 'hsla(80, 52%, 84%, 1)',
 		},
 		{
 			id    : 'Search not Available',
 			label : 'Search not Available',
-			value : total_search_result_not_available,
+			value : total_search_result_not_available_percentage,
+			color : 'hsla(232, 44%, 96%, 1)',
 		},
 	];
 	const pie_outer_data = [
 		{
 			id    : 'Like',
 			label : 'Likes',
-			value : (total_like / total_search_result_available) * 100,
+			value : Like,
+			color : 'hsla(39, 55%, 94%, 1)',
 		},
 		{
 			id    : 'Dislikes',
 			label : 'Dislikes',
-			value : (total_dislike / total_search_result_available) * 100,
+			value : Dislike,
+			color : 'hsla(41, 64%, 86%, 1)',
 		},
 		{
 			id    : 'Viewed Only',
 			label : 'Viewed Only',
-			value : (total_viewed_only_questions / total_search_result_available) * 100,
+			value : Viewed,
+			color : 'hsl(202, 0%, 50%)',
+		},
+		{
+			id    : 'No Action',
+			label : 'N0 Action',
+			value : Available_Remaining,
+			color : 'hsl(255, 100%, 100%)',
+		},
+		{
+			id    : 'Not Requested',
+			label : 'Not Requested',
+			value : (total_search_result_not_available_percentage - Requested),
+			color : 'hsl(255, 100%, 100%)',
 		},
 		{
 			id    : 'Requested',
 			label : 'Requested',
-			value : total_requested_questions_percentage,
+			value : (total_search_result_not_available_percentage / 100) * total_requested_questions_percentage,
+			color : 'hsla(234, 46%, 87%, 1)',
 		},
 
 	];
--- packages/cogo-academy/page-components/CreateFAQ/Analytics/GraphUI/index.js
+++ packages/cogo-academy/page-components/CreateFAQ/Analytics/GraphUI/index.js
@@ -262,110 +262,24 @@ function GraphUI() {
 						data={pie_data}
 						margin={{ top: 50, right: 10, bottom: 80, left: 40 }}
 						innerRadius={0}
-						padAngle={0.7}
-						cornerRadius={3}
+						padAngle={0}
+						cornerRadius={0}
 						activeOuterRadiusOffset={8}
 						borderWidth={1}
+						colors={{ datum: 'data.color' }}
 						borderColor={{
                 from      : 'color',
                 modifiers : [['darker', 0.2]],
 						}}
-						enableArcLabels={false}
+						enableArcLabels
 						enableArcLinkLabels={false}
+						isInteractive
 						defs={[
                 {
                   id         : 'dots',
                   type       : 'patternDots',
                   background : 'inherit',
-                  color      : 'rgba(255, 255, 255, 0.3)',
-                  size       : 4,
-                  padding    : 1,
-                  stagger    : true,
-                },
-                {
-                  id         : 'lines',
-                  type       : 'patternLines',
-                  background : 'inherit',
-                  color      : 'rgba(255, 255, 255, 0.3)',
-                  rotation   : -45,
-                  lineWidth  : 6,
-                  spacing    : 10,
-                },
-						]}
-						fill={[
-                {
-                  match: {
-                    id: 'ruby',
-                  },
-                  id: 'dots',
-                },
-                {
-                  match: {
-                    id: 'c',
-                  },
-                  id: 'dots',
-                },
-                {
-                  match: {
-                    id: 'go',
-                  },
-                  id: 'dots',
-                },
-                {
-                  match: {
-                    id: 'python',
-                  },
-                  id: 'dots',
-                },
-                {
-                  match: {
-                    id: 'scala',
-                  },
-                  id: 'lines',
-                },
-                {
-                  match: {
-                    id: 'lisp',
-                  },
-                  id: 'lines',
-                },
-                {
-                  match: {
-                    id: 'elixir',
-                  },
-                  id: 'lines',
-                },
-                {
-                  match: {
-                    id: 'javascript',
-                  },
-                  id: 'lines',
-                },
-						]}
-					/>
-				</div>
-
-				<div className={styles.outer_pie}>
-					<ResponsivePie
-						data={pie_outer_data}
-						margin={{ top: 50, right: 10, bottom: 80, left: 40 }}
-						innerRadius={0.8}
-						padAngle={0.7}
-						cornerRadius={3}
-						activeOuterRadiusOffset={8}
-						borderWidth={1}
-						borderColor={{
-                from      : 'color',
-                modifiers : [['darker', 0.2]],
-						}}
-						// enableArcLabels={false}
-						// enableArcLinkLabels={false}
-						defs={[
-                {
-                  id         : 'dots',
-                  type       : 'patternDots',
-                  background : 'inherit',
-                  color      : 'rgba(255, 255, 255, 0.3)',
+                  color      : 'rgba(255, 255, 255, )',
                   size       : 4,
                   padding    : 1,
                   stagger    : true,
@@ -431,30 +345,86 @@ function GraphUI() {
                 },
 						]}
 						legends={[
-                {
-                  anchor        : 'bottom',
-                  direction     : 'row',
-                  justify       : false,
-                  translateX    : 0,
-                  translateY    : 56,
-                  itemsSpacing  : 0,
-                  itemWidth     : 80,
-                  itemHeight    : 18,
-                  itemTextColor : '#999',
-                  itemDirection : 'left-to-right',
-                  itemOpacity   : 1,
-                  symbolSize    : 18,
-                  symbolShape   : 'circle',
-                  effects       : [
-                    {
-                      on    : 'hover',
-                      style : {
-                        itemTextColor: '#000',
-                      },
+              {
+                anchor        : 'bottom',
+                direction     : 'row',
+                justify       : false,
+                translateX    : 0,
+                translateY    : 56,
+                itemsSpacing  : 40,
+                itemWidth     : 80,
+                itemHeight    : 18,
+                itemTextColor : '#999',
+                itemDirection : 'left-to-right',
+                itemOpacity   : 1,
+                symbolSize    : 20,
+                symbolShape   : 'circle',
+                effects       : [
+                  {
+                    on    : 'hover',
+                    style : {
+                      itemTextColor: '#000',
                     },
-                  ],
+                  },
+                ],
+              },
+						]}
+					/>
+				</div>
+
+				<div className={styles.outer_pie}>
+					<ResponsivePie
+						data={pie_outer_data}
+						margin={{ top: 50, right: 10, bottom: 80, left: 40 }}
+						innerRadius={0.8}
+						padAngle={0}
+						cornerRadius={0}
+						activeOuterRadiusOffset={8}
+						borderWidth={0}
+						borderColor={{
+                from      : 'color',
+                modifiers : [['darker', 0.2]],
+						}}
+						enableArcLabels={false}
+						enableArcLinkLabels={false}
+						isInteractive
+						animate
+						colors={{ datum: 'data.color' }}
+						defs={[
+                {
+                  id: 'dots',
+
+                  size    : 0,
+                  padding : 0,
+                  stagger : false,
+                },
+                {
+                  id         : 'lines',
+                  type       : 'patternLines',
+                  background : 'inherit',
+                  color      : 'rgba(255, 255, 255, 1)',
+                  rotation   : -45,
+                  lineWidth  : 6,
+                  spacing    : 10,
                 },
 						]}
+						fill={[
+                {
+                  match: {
+                    id: 'N/A',
+                  },
+                  id: 'dots',
+                },
+                {
+                  match: {
+                    id: 'N/A2',
+                  },
+                  id: 'dots',
+
+                },
+
+						]}
+
 					/>
 				</div>
 			</div>
