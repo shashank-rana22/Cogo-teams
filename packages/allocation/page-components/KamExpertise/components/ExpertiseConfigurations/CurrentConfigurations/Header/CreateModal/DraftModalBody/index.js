e0af73f4bf8c62c2b3ba5f06cacb36c560817b10
garlapadu harshith <68819636+harshithwebdesign4e@users.noreply.github.com>
2023-03-16 19:09:29 +0530

    modal for create and some fixes

---
+++ packages/allocation/page-components/KamExpertise/components/ExpertiseConfigurations/CurrentConfigurations/Header/CreateModal/DraftModalBody/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function DraftModalBody() {
+	return (
+		<div>DraftModaBody</div>
+	);
+}
+
+export default DraftModalBody;
---
+++ packages/allocation/page-components/KamExpertise/components/ExpertiseConfigurations/CurrentConfigurations/Header/CreateModal/PublishedModalBody/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function PublishedModalBody() {
+	return (
+		<div>PublishedModalBody</div>
+	);
+}
+
+export default PublishedModalBody;
