711d9744693dd88dc8b7b5b06dfe71d175f55974
Muchipalli Abhishikth <muchipal.abhishikth@cogoport.com>
2023-03-16 19:25:52 +0530

    changes

--- packages/cogo-one/page-components/CogoOneChannel/Customers/MailList/MailModal/index.js
+++ packages/cogo-one/page-components/CogoOneChannel/Customers/MailList/MailModal/index.js
@@ -1,10 +1,19 @@
-import { cl, Modal, Button, Tags } from '@cogoport/components';
+import { Toast, cl, Modal, Button, Tags, RTE, Input } from '@cogoport/components';
 import { IcMSend, IcMAttach, IcMCross } from '@cogoport/icons-react';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
+
+import CustomFileUploader from '../../../../../common/CustomFileUploader';
+import { TOOLBARCONFIG } from '../../../../../constants';
+import getFormatedEmailBody from '../../../../../helpers/getFormatedEmailBody';
+import getFileAttributes from '../../../../../utils/getFileAttributes';
+//  import hideDetails from '../../../../../utils/hideDetails';
 
 import styles from './styles.module.css';
 
-function MailModal({ showMailModal, setShowMailModal }) {
+function MailModal({
+	showMailModal, setShowMailModal, userData = {}, sendQuickCommuncation = () => {},
+	loading,
+}) {
 	const [showControl, setShowControl] = useState(false);
 	const [type, setType] = useState('');
 	const [recipientValue, setRecipientValue] = useState('');
@@ -12,16 +21,71 @@ function MailModal({ showMailModal, setShowMailModal }) {
 	const [error, setError] = useState(false);
 	const [recipientArray, setRecipientArray] = useState([]);
 	const [bccArray, setBccArray] = useState([]);
+	const [uploading, setUploading] = useState(false);
+
+	const [attachments, setAttachments] = useState([]);
+	const [emailState, setEmailState] = useState({
+		subject : '',
+		body    : '',
+	});
+
+	const uploaderRef = useRef(null);
+
+	const handleProgress = (val) => {
+		setUploading(val);
+	};
+
+	const handleSend = () => {
+		const isEmptyMail = getFormatedEmailBody({ emailState });
+		if (isEmptyMail || !emailState?.subject) {
+			Toast.error('Both Subject and Body are Requied');
+		} else {
+			sendQuickCommuncation({
+				template_name         : 'send_email_template',
+				otherChannelRecipient : userData?.email,
+				variables             : { ...emailState },
+				type                  : 'email',
+				attachment_urls       : attachments || [],
+			});
+		}
+	};
 
 	const renderHeader = () => (
 		<>
 			<div className={styles.send_icon}>
-				<IcMSend />
+				<IcMSend
+					onClick={handleSend}
+					loading={loading}
+				/>
 			</div>
 			<div className={styles.title}>New Message</div>
 			<div className={styles.right_header}>
-				<IcMAttach />
-				<Button size="md" themeType="link" onClick={() => setShowMailModal(true)}>Cancel</Button>
+				<div className={styles.file_uploader_div}>
+					<CustomFileUploader
+						disabled={uploading}
+						handleProgress={handleProgress}
+						className="file_uploader"
+						accept=".png, .pdf, .jpg, .jpeg, .doc, .docx, .csv, .svg, .gif, .mp4, .xlsx"
+						multiple
+						uploadIcon={(
+							<IcMAttach
+								className={styles.upload_icon}
+								fill="#000000"
+								style={{
+									cursor: uploading
+										? 'not-allowed'
+										: 'pointer',
+								}}
+							/>
+						)}
+						onChange={(val) => {
+							setAttachments(val);
+						}}
+						showProgress={false}
+						ref={uploaderRef}
+					/>
+				</div>
+				<Button size="md" themeType="link" onClick={() => setShowMailModal(false)}>Cancel</Button>
 			</div>
 		</>
 	);
@@ -45,11 +109,8 @@ function MailModal({ showMailModal, setShowMailModal }) {
 	};
 
 	const handleKeyPress = (event) => {
-		console.log(recipientValue);
-		console.log(event.target.value);
 		if (event.key === 'Enter') {
 			event.preventDefault();
-			console.log(validateEmail(recipientValue));
 			if (!validateEmail(recipientValue)) {
 				setError(true);
 				return;
@@ -91,10 +152,23 @@ function MailModal({ showMailModal, setShowMailModal }) {
 		setValue(formattedValue);
 	};
 
-	const handleError = () => {
+	const handleAttachmentDelete = (url) => {
+		const filteredAttachments = attachments.filter((data) => data !== url);
+		setAttachments(filteredAttachments);
+		uploaderRef?.current?.externalHandleDelete(filteredAttachments);
+	};
+
+	const handleError = (s) => {
 		setError(false);
-		setRecipientValue('');
+		if (s === 'receipient') { setRecipientValue(''); } else { setCcBccValue(''); }
 		setShowControl(false);
+	};
+
+	const decode = (data = '') => {
+		const val = decodeURI(data).split('/');
+		const fileName = val[val.length - 1];
+		const { uploadedFileName, fileIcon } = getFileAttributes({ fileName, finalUrl: data });
+		return { uploadedFileName, fileIcon };
 	};
 	return (
 		<Modal
@@ -104,6 +178,7 @@ function MailModal({ showMailModal, setShowMailModal }) {
 			size="lg"
 			className={styles.styled_ui_modal_dialog}
 			placement="top"
+			scroll
 		>
 			<Modal.Header title={renderHeader()} />
 			<Modal.Body>
@@ -133,7 +208,7 @@ function MailModal({ showMailModal, setShowMailModal }) {
 									id="inputId"
 								/>
 								<div className={styles.cross_icon}>
-									<IcMCross onClick={() => handleError()} />
+									<IcMCross onClick={() => handleError('receipient')} />
 								</div>
 							</div>
 							{(error) && (
@@ -164,19 +239,26 @@ function MailModal({ showMailModal, setShowMailModal }) {
 						/>
 					</div>
 					{(showControl && type === 'cc_bcc') && (
-						<div className={styles.tag_container}>
-							<input
-								size="sm"
-								placeholder="Enter cc recipient"
-								type="text"
-								value={ccBccValue}
-								onChange={(e) => handleChange(e)}
-								onKeyPress={(e) => handleKeyPress(e)}
-								className={styles.input_container}
-							/>
-							<div className={styles.cross_icon}>
-								<IcMCross onClick={(e) => handleError(e)} />
+						<div className={styles.tag_and_errorcontainer}>
+							<div className={styles.tag_container}>
+								<input
+									size="sm"
+									placeholder="Enter cc recipient"
+									type="text"
+									value={ccBccValue}
+									onChange={(e) => handleChange(e)}
+									onKeyPress={(e) => handleKeyPress(e)}
+									className={cl`${error ? styles.error_input_container : styles.input_container}`}
+								/>
+								<div className={styles.cross_icon}>
+									<IcMCross onClick={() => handleError('ccBccValue')} />
+								</div>
 							</div>
+							{(error) && (
+								<div className={styles.error_content_container}>
+									Enter valid mail
+								</div>
+							)}
 						</div>
 					)}
 					<div
@@ -189,9 +271,47 @@ function MailModal({ showMailModal, setShowMailModal }) {
 				</div>
 				<div className={styles.type_to}>
 					<div className={styles.sub_text}>
-						Subject:
+						Sub:
 						{' '}
 					</div>
+					<Input
+						value={emailState?.subject}
+						onChange={(val) => setEmailState((p) => ({ ...p, subject: val }))}
+						size="xs"
+						placeholder="Enter your Subject"
+						className={styles.styled_input}
+					/>
+				</div>
+
+				<div className={styles.rte_container}>
+					<RTE
+						value={emailState?.body}
+						onChange={(val) => setEmailState((p) => ({ ...p, body: val }))}
+						toolbarConfig={TOOLBARCONFIG}
+						className={styles.styled_editor}
+					/>
+
+					<div className={styles.attachments_scroll}>
+						<div className={styles.uploading}>{uploading && 'Uploading...'}</div>
+						{(attachments || []).map((data) => (
+							<div className={styles.uploaded_files}>
+								<div className={styles.uploaded_files_content}>
+									{decode(data)?.fileIcon}
+									<div className={styles.content_div}>
+										{decode(data)?.uploadedFileName}
+									</div>
+								</div>
+								<IcMCross
+									className={styles.cross_svg}
+									onClick={(e) => {
+										e.stopPropagation();
+										handleAttachmentDelete(data);
+									}}
+								/>
+							</div>
+						))}
+					</div>
+
 				</div>
 			</Modal.Body>
 		</Modal>
