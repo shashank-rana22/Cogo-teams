944f5c5424babbb79d5bf2d04e9e9ff158557b69
Lalith Phanindra Patibanda <lalithphanindrapatibanda@pp.local>
2023-03-16 17:47:45 +0530

    review and set criteria

--- packages/cogo-academy/page-components/CreateModule/components/CreateNewTest/components/ReviewAndCriteria/index.js
+++ packages/cogo-academy/page-components/CreateModule/components/CreateNewTest/components/ReviewAndCriteria/index.js
@@ -1,13 +1,43 @@
+import { Pill } from '@cogoport/components';
 import { IcMArrowBack } from '@cogoport/icons-react';
 
 import styles from './styles.module.css';
 
 function ReviewAndCriteria() {
 	return (
-		<div>
+		<div className={styles.container}>
 			<div className={styles.header}>
 				<IcMArrowBack width={20} height={20} />
-				<div className={styles.title}>New Test</div>
+				<div className={styles.title}> Review and Set Criteria </div>
+			</div>
+			<div className={styles.subcontainer}>
+				<div className={styles.label}>Shipment and Enrichment Test</div>
+				<div className={styles.topic}>
+					<div className={styles.subtopic}>Topics </div>
+					<div>
+						<Pill size="md" color="blue" className={styles.names}>
+							<span className={styles.names}>Enrichments</span>
+						</Pill>
+						<Pill size="md" color="blue">
+							<span className={styles.names}>Shipment</span>
+						</Pill>
+					</div>
+				</div>
+				<div className={styles.entity}>
+					<div className={styles.label_entity}>Cogo Entity </div>
+					<div className={styles.entity_name}>Cogo India</div>
+				</div>
+				<div className={styles.topic}>
+					<div className={styles.subtopic}> Users </div>
+					<div>
+						<Pill size="md" color="#FEF3E9" className={styles.names}>
+							<span className={styles.names}>KAM 1</span>
+						</Pill>
+						<Pill size="md" color="#FEF3E9">
+							<span className={styles.names}>KAM 2</span>
+						</Pill>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
--- packages/cogo-academy/page-components/CreateModule/components/HomePage/index.js
+++ packages/cogo-academy/page-components/CreateModule/components/HomePage/index.js
@@ -1,17 +1,19 @@
-import CreateTest from '../CreateNewTest';
+// import CreateTest from '../CreateNewTest';
 // import CreateQuestionSet from '../CreateModule/components/CreateQuestionSet';
 // import Header from '../Header';
 // import TestsList from '../TestsList';
+import ReviewAndCriteria from '../CreateNewTest/components/ReviewAndCriteria';
 
 function HomePage() {
 	return (
-	// <div>
-	// 	<Header />
-	// 	{/* <Analytics /> */}
-	// 	<TestsList />
-	// </div>
-	// <CreateQuestionSet />
-		<CreateTest />
+		// <div>
+		// 	<Header />
+		// 	{/* <Analytics /> */}
+		// 	<TestsList />
+		// </div>
+		// <CreateQuestionSet />
+		//	<CreateTest />
+		<ReviewAndCriteria />
 	);
 }
 export default HomePage;
