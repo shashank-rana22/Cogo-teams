52f3afeabf62c417da5e03c30dab6c83ecec065e
niranjan1375 <potnuru.niranjan@cogoport.com>
2023-03-16 16:35:52 +0530

    fixes

--- packages/cogo-academy/page-components/CreateModule/components/Header/index.js
+++ packages/cogo-academy/page-components/CreateModule/components/Header/index.js
@@ -4,12 +4,24 @@ import React from 'react';
 
 import styles from './styles.module.css';
 
-function Header() {
+const BUTTON_TEXT_MAPPING = {
+	tests         : ' Test',
+	question_set  : 'Question Set',
+	all_questions : 'Not Sure',
+
+};
+
+function Header(props) {
+	const { activeTab } = props;
 	const router = useRouter();
 	return (
 		<div className={styles.flex_div}>
 			<div className={styles.title}>Test Module</div>
-			<Button onClick={() => router.push('/learning/faq/create/test-module/create')}>+ Create New Test</Button>
+			<Button onClick={() => router.push('/learning/faq/create/test-module/create')}>
+				+ Create New
+				{' '}
+				{BUTTON_TEXT_MAPPING[activeTab]}
+			</Button>
 		</div>
 	);
 }
--- packages/cogo-academy/page-components/CreateModule/components/HomePage/index.js
+++ packages/cogo-academy/page-components/CreateModule/components/HomePage/index.js
@@ -1,17 +1,21 @@
-import CreateTest from '../CreateNewTest';
+// import React, { useState } from 'react';
+
 // import CreateQuestionSet from '../CreateModule/components/CreateQuestionSet';
+import CreateTest from '../CreateNewTest';
 // import Header from '../Header';
 // import TestsList from '../TestsList';
 
 function HomePage() {
+	// const [activeTab, setActiveTab] = useState('tests');
+
 	return (
-	// <div>
-	// 	<Header />
-	// 	{/* <Analytics /> */}
-	// 	<TestsList />
-	// </div>
-	// <CreateQuestionSet />
-		<CreateTest />
+		<>
+			{/* <Header activeTab={activeTab} /> */}
+			{/* <TestsList activeTab={activeTab} setActiveTab={setActiveTab} /> */}
+			<CreateTest />
+			{/* <CreateQuestionSet /> */}
+			{/* <Analytics /> */}
+		</>
 	);
 }
 export default HomePage;
