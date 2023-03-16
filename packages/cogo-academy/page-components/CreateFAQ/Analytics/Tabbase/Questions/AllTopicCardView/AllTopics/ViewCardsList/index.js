6fb19cfced3e8d6bb173b8066627a3797734e46e
Shivam Singh <shivam.singh@cogoport.com>
2023-03-16 21:38:45 +0530

    PR changes

--- packages/cogo-academy/page-components/CreateFAQ/Analytics/Tabbase/Questions/AllTopicCardView/AllTopics/ViewCardsList/index.js
+++ packages/cogo-academy/page-components/CreateFAQ/Analytics/Tabbase/Questions/AllTopicCardView/AllTopics/ViewCardsList/index.js
@@ -8,106 +8,101 @@ function ViewCardsList({ cardHeading = '', contentQuestion = [{}] }) {
 	if (!contentQuestion[0]) {
 		return (
 			<div className={styles.primary_right}>
-				<div className={styles.active_users}>
-					<div className={styles.right_stat_label}>
-						{cardHeading}
-					</div>
-					<div className={styles.sub_heading} style={{ textAlign: 'center' }}>No Data Available</div>
-				</div>
-			</div>
-		);
-	}
-	return (
-
-		<div className={styles.primary_right}>
-			<div className={styles.active_users}>
 				<div className={styles.right_stat_label}>
 					{cardHeading}
 				</div>
-				<div className={styles.sub_heading}>
-					{(contentQuestion[0])
-						? (
-							<div style={{ display: 'flex', justifyContent: 'space-between' }}>
+				<div className={styles.sub_heading} style={{ textAlign: 'center' }}>No Data Available</div>
+			</div>
+		);
+	}
 
-								<div style={{ marginLeft: '-9px' }}>
-									<Tooltip content={contentQuestion[0]?.question_abstract} placement="right">
-										<div>
-											1.
-											{truncate(contentQuestion[0]?.question_abstract)}
-										</div>
-									</Tooltip>
-								</div>
-								<div style={{ display: 'flex' }}>
-									<div style={{ marginRight: '0.25rem' }}>{contentQuestion[0]?.view_count}</div>
-									<IcMEyeopen style={{ marginTop: '0.15rem' }} />
-								</div>
+	return (
+		<div className={styles.primary_right}>
+			<div className={styles.right_stat_label}>
+				{cardHeading}
+			</div>
+
+			<div className={styles.sub_heading}>
+				{(contentQuestion[0])
+					? (
+						<div style={{ display: 'flex', justifyContent: 'space-between' }}>
+
+							<div style={{ marginLeft: '-9px' }}>
+								<Tooltip content={contentQuestion[0]?.question_abstract} placement="right">
+									<div>
+										1.
+										{truncate(contentQuestion[0]?.question_abstract)}
+									</div>
+								</Tooltip>
+							</div>
+							<div style={{ display: 'flex' }}>
+								<div style={{ marginRight: '0.25rem' }}>{contentQuestion[0]?.view_count}</div>
+								<IcMEyeopen style={{ marginTop: '0.15rem' }} />
+							</div>
+
+						</div>
+					) : null}
+
+				{(contentQuestion[1])
+					? (
+						<div style={{ display: 'flex', justifyContent: 'space-between' }}>
+
+							<div style={{ marginLeft: '-9px' }}>
+								<Tooltip content={contentQuestion[1]?.question_abstract} placement="right">
+									<div>
+										2.
+										{truncate(contentQuestion[1]?.question_abstract)}
+									</div>
+								</Tooltip>
 
 							</div>
-						) : null}
-
-					{(contentQuestion[1])
-						? (
-							<div style={{ display: 'flex', justifyContent: 'space-between' }}>
-
-								<div style={{ marginLeft: '-9px' }}>
-									<Tooltip content={contentQuestion[1]?.question_abstract} placement="right">
-										<div>
-											2.
-											{truncate(contentQuestion[1]?.question_abstract)}
-										</div>
-									</Tooltip>
-
-								</div>
-								<div style={{ display: 'flex' }}>
-									<div style={{ marginRight: '0.25rem' }}>{contentQuestion[1]?.view_count}</div>
-									<IcMEyeopen style={{ marginTop: '0.15rem' }} />
-								</div>
-
+							<div style={{ display: 'flex' }}>
+								<div style={{ marginRight: '0.25rem' }}>{contentQuestion[1]?.view_count}</div>
+								<IcMEyeopen style={{ marginTop: '0.15rem' }} />
 							</div>
-						) : null}
-					{(contentQuestion[2])
-						? (
-							<div style={{ display: 'flex', justifyContent: 'space-between' }}>
 
-								<div style={{ marginLeft: '-9px' }}>
+						</div>
+					) : null}
+				{(contentQuestion[2])
+					? (
+						<div style={{ display: 'flex', justifyContent: 'space-between' }}>
 
-									<Tooltip content={contentQuestion[2]?.question_abstract} placement="right">
-										<div>
-											3.
-											{truncate(contentQuestion[2]?.question_abstract)}
-										</div>
-									</Tooltip>
-								</div>
-								<div style={{ display: 'flex' }}>
-									<div style={{ marginRight: '0.25rem' }}>{contentQuestion[2]?.view_count}</div>
-									<IcMEyeopen style={{ marginTop: '0.15rem' }} />
-								</div>
+							<div style={{ marginLeft: '-9px' }}>
 
+								<Tooltip content={contentQuestion[2]?.question_abstract} placement="right">
+									<div>
+										3.
+										{truncate(contentQuestion[2]?.question_abstract)}
+									</div>
+								</Tooltip>
 							</div>
-						) : null}
-					{(contentQuestion[3])
-						? (
-							<div style={{ display: 'flex', justifyContent: 'space-between' }}>
-
-								<div style={{ marginLeft: '-9px' }}>
-
-									<Tooltip content={contentQuestion[3]?.question_abstract} placement="right">
-										<div>
-											4.
-											{truncate(contentQuestion[3]?.question_abstract)}
-										</div>
-									</Tooltip>
-								</div>
-								<div style={{ display: 'flex' }}>
-									<div style={{ marginRight: '0.25rem' }}>{contentQuestion[3]?.view_count}</div>
-									<IcMEyeopen style={{ marginTop: '0.15rem' }} />
-								</div>
-
+							<div style={{ display: 'flex' }}>
+								<div style={{ marginRight: '0.25rem' }}>{contentQuestion[2]?.view_count}</div>
+								<IcMEyeopen style={{ marginTop: '0.15rem' }} />
 							</div>
-						) : null}
 
-				</div>
+						</div>
+					) : null}
+				{(contentQuestion[3])
+					? (
+						<div style={{ display: 'flex', justifyContent: 'space-between' }}>
 
+							<div style={{ marginLeft: '-9px' }}>
+
+								<Tooltip content={contentQuestion[3]?.question_abstract} placement="right">
+									<div>
+										4.
+										{truncate(contentQuestion[3]?.question_abstract)}
+									</div>
+								</Tooltip>
+							</div>
+							<div style={{ display: 'flex' }}>
+								<div style={{ marginRight: '0.25rem' }}>{contentQuestion[3]?.view_count}</div>
+								<IcMEyeopen style={{ marginTop: '0.15rem' }} />
+							</div>
+
+						</div>
+					) : null}
 			</div>
 		</div>
 
