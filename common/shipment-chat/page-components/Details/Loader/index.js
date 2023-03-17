609c5fe476c8d302b664cfd156c9f10e4f685ce4
ankit-rajpoot <ankit.rajpoot@cogoport.com>
2023-03-18 00:01:50 +0530

    fixes

--- common/shipment-chat/page-components/Details/Loader/index.js
+++ common/shipment-chat/page-components/Details/Loader/index.js
@@ -1,25 +1,14 @@
 import { Placeholder } from '@cogoport/components';
 import React from 'react';
 
-import styles from './styles.module.css';
-
-const customStyle = { height: '14px' };
-
 function Loader() {
 	return (
-		<div className={styles.container}>
-			<div className={styles.details}>
-				<Placeholder style={{ marginRight: '50px' }} />
-
-				<div className={styles.ports}>
-					<Placeholder style={{ ...customStyle, margin: '0 0 10px 0' }} />
-					<Placeholder style={customStyle} />
-				</div>
-
-				<div className={styles.ports} className="destination">
-					<Placeholder style={{ ...customStyle, margin: '0 0 10px 0' }} />
-					<Placeholder style={customStyle} />
-				</div>
+		<div style={{ width: '100%', display: 'flex', justifyContent: 'space-between' }}>
+			<div style={{ display: 'flex', justifyContent: 'flex-start' }}>
+				<Placeholder width="220px" height="50px" margin="16px 8px" />
+			</div>
+			<div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+				<Placeholder width="220px" height="50px" margin="16px 8px" />
 			</div>
 		</div>
 	);
--- common/shipment-chat/page-components/Details/MsgLoader/index.js
+++ common/shipment-chat/page-components/Details/MsgLoader/index.js
@@ -5,22 +5,16 @@ function MsgLoader() {
 	return (
 		<>
 			<div style={{ display: 'flex', justifyContent: 'flex-start' }}>
-				<Placeholder width="240px" height="50px" margin="16px 12px" />
+				<Placeholder width="220px" height="50px" margin="16px 8px" />
 			</div>
 			<div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-				<Placeholder width="240px" height="50px" margin="16px 12px" />
+				<Placeholder width="220px" height="50px" margin="16px 8px" />
 			</div>
 			<div style={{ display: 'flex', justifyContent: 'flex-start' }}>
-				<Placeholder width="240px" height="50px" margin="16px 12px" />
+				<Placeholder width="220px" height="40px" margin="16px 8px" />
 			</div>
 			<div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-				<Placeholder width="240px" height="50px" margin="16px 12px" />
-			</div>
-			<div style={{ display: 'flex', justifyContent: 'flex-start' }}>
-				<Placeholder width="240px" height="50px" margin="16px 12px" />
-			</div>
-			<div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-				<Placeholder width="240px" height="50px" margin="16px 12px" />
+				<Placeholder width="220px" height="40px" margin="16px 8px" />
 			</div>
 		</>
 	);
