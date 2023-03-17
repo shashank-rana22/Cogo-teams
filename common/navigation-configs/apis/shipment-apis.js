b0e2d5adcc33a1a1b8ef23ebbeff709220d569f3
piyushi-cogo <piyushi.garg@cogoport.com>
2023-03-17 11:23:58 +0530

    list services api

--- common/navigation-configs/apis/shipment-apis.js
+++ common/navigation-configs/apis/shipment-apis.js
@@ -1277,6 +1277,11 @@ const apis = [
 		access_type  : 'private',
 		service_name : 'shipment',
 	},
+	{
+		api          : 'list_services',
+		access_type  : 'private',
+		service_name : 'bookings',
+	},
 ];
 
 export default apis;
