a5b706f46fc34d0b66533cfdf03def2f240d4d70
garlapadu harshith <68819636+harshithwebdesign4e@users.noreply.github.com>
2023-03-17 16:07:31 +0530

    modals added

--- packages/allocation/page-components/KamExpertise/components/ExpertiseConfigurations/CurrentConfigurations/Header/CreateModal/DraftModalBody/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function DraftModalBody() {
-	return (
-		<div>DraftModaBody</div>
-	);
-}
-
-export default DraftModalBody;
---
+++ packages/allocation/page-components/KamExpertise/components/ExpertiseConfigurations/CurrentConfigurations/Header/CreateModal/NewVersion/index.js
@@ -0,0 +1,40 @@
+import { Toast, Button } from '@cogoport/components';
+import { IcMAlert } from '@cogoport/icons-react';
+import React from 'react';
+
+import styles from './styles.module.css';
+
+function NewVersion({ setMode, setShowModal }) {
+	return (
+
+		<div className={styles.container}>
+
+			<IcMAlert className={styles.alert_icon} />
+
+			<div style={{ color: '#bf291e' }} className={styles.supporting_text}>
+				Creating a new version will overwrite the existing saved draft.
+			</div>
+
+			<div className={styles.supporting_text}>
+				Do you wish to proceed?
+			</div>
+			<div className={styles.button_container}>
+				<Button themeType="teritiary" onClick={() => { setMode(''); }}>Back</Button>
+				<Button onClick={() => {
+					setShowModal(false);
+					setMode('');
+					Toast.success('New Draft Loaded');
+				}}
+				>
+					Proceed
+
+				</Button>
+
+			</div>
+
+		</div>
+
+	);
+}
+
+export default NewVersion;
