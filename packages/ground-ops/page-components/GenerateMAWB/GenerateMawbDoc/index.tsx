6a9e147622866bcd4293ba7b7891d9169ae643f5
Ankit Kaushal <ankit.kaushal@cogoport.com>
2023-03-19 13:15:01 +0530

    size fix

--- packages/ground-ops/page-components/GenerateMAWB/GenerateMawbDoc/index.tsx
+++ packages/ground-ops/page-components/GenerateMAWB/GenerateMawbDoc/index.tsx
@@ -150,12 +150,12 @@ function GenerateMawb({
 				const pdfWidth = pdf.internal.pageSize.getWidth();
 				const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
 				footerImages.forEach((item, i) => {
-					pdf.addImage(imgData, 'PNG', 0, -10, pdfWidth, pdfHeight);
+					pdf.addImage(imgData, 'PNG', 0, -5, pdfWidth, pdfHeight);
 					pdf.addImage(
 						item,
 						'PNG',
 						0,
-						pdfHeight - 40,
+						pdfHeight - 35,
 						pdfWidth,
 						10,
 					);
