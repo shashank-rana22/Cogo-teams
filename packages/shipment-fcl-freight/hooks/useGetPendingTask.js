a701d4504c784f7f1bb865377627de97ca908c25
Shivani Gupta <109941648+shivani-gupta19@users.noreply.github.com>
2023-03-20 17:38:36 +0530

    fixes

--- packages/shipment-fcl-freight/hooks/useGetPendingTask.js
+++ packages/shipment-fcl-freight/hooks/useGetPendingTask.js
@@ -24,7 +24,7 @@ function useGetPendingTasks({ shipment_data }) {
 
 					},
 				}); if (!res.hasError) {
-					Toast.error('dsfghj');
+					// Toast.error('dsfghj');
 				}
 			} catch (err) {
 				console.log(err);
