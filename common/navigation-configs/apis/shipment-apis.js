b67cc409d82ddfd9549e675308c01ae75e74936d
Kanchan Bhandari <kanchan.bhandari@cogoport.com>
2023-03-20 14:26:27 +0530

    saas apis added

--- common/navigation-configs/apis/shipment-apis.js
+++ common/navigation-configs/apis/shipment-apis.js
@@ -1281,6 +1281,51 @@ const apis = [
 		api         : 'get_exchange_rate',
 		access_type : 'private',
 	},
+	{
+		api          : 'get_spot_search_cargo_insurance_rate',
+		access_type  : 'private',
+		service_name : 'spot_search',
+	},
+	{
+		api          : 'create_checkout_service',
+		access_type  : 'private',
+		service_name : 'spot_search',
+	},
+	{
+		api          : 'get_insurance_country_supported',
+		access_type  : 'private',
+		service_name : 'saas',
+	},
+	{
+		api          : 'get_insurance_list_commodities',
+		access_type  : 'private',
+		service_name : 'saas',
+	},
+	{
+		api          : 'post_insurance',
+		access_type  : 'private',
+		service_name : 'saas',
+	},
+	{
+		api          : 'get_insurance_draft_details',
+		access_type  : 'private',
+		service_name : 'saas',
+	},
+	{
+		api          : 'post_insurance_draft',
+		access_type  : 'private',
+		service_name : 'saas',
+	},
+	{
+		api          : 'post_insurance_checkout',
+		access_type  : 'private',
+		service_name : 'saas',
+	},
+	{
+		api          : 'get_insurance_rate',
+		access_type  : 'private',
+		service_name : 'saas',
+	},
 ];
 
 export default apis;
