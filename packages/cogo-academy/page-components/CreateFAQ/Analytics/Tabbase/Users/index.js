f4dc3cf7f0192907705237433f998002ca5d2912
Namanagarwal015 <124123426+Namanagarwal015@users.noreply.github.com>
2023-03-20 14:05:51 +0530

    extra CSS Removed

--- packages/cogo-academy/page-components/CreateFAQ/Analytics/Tabbase/Users/index.js
+++ packages/cogo-academy/page-components/CreateFAQ/Analytics/Tabbase/Users/index.js
@@ -4,7 +4,7 @@ import AllUsers from './AllUsers';
 import Filter from './Filter';
 import UsersGroup from './UsersGroup';
 
-function Users({ props, date = '', setDate = () => {} }) {
+function Users({ props }) {
 	const [selectedItem, setSelectedItem] = useState('Users Group');
 
 	return (
