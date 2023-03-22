141a481029d7db296391cdf5c3be339b3391cee7
Ritvik98349 <124043414+Ritvik98349@users.noreply.github.com>
2023-03-22 14:27:53 +0530

    draggable implemented

--- common/core/components/AdminLayout/FAQs/index.js
+++ common/core/components/AdminLayout/FAQs/index.js
@@ -1,7 +1,6 @@
 import { Modal } from '@cogoport/components';
 import { useSelector } from '@cogoport/store';
 import React, { useState, useRef } from 'react';
-import Draggable from 'react-draggable';
 
 import styles from './styles.module.css';
 import TopicList from './TopicList';
@@ -10,34 +9,38 @@ function FAQs() {
 	const [show, setShow] = useState(false);
 	const { general:{ isMobile = false } } = useSelector((state) => state);
 
-	const handleShow = () => {
-		setShow(true);
-	};
+	const ballRef = useRef();
 
-	const isDraggingRef = useRef(false);
+	function onDragHandler(ev) {
+		const shiftX = ev.clientX;
 
-	const onDrag = () => {
-		isDraggingRef.current = true;
-	};
+		const shiftY = ev.clientY;
 
-	const onStop = () => {
-		if (!isDraggingRef.current) {
-			handleShow();
-		}
-		isDraggingRef.current = false;
-	};
+		if (shiftX && shiftY)ballRef.current.style = `top: ${shiftY - 30}px;left: ${shiftX - 30}px;`;
+	}
 
 	return (
-		<div className={styles.container}>
-			<Draggable onStop={onStop} onDrag={onDrag}>
-				<div size={400} className={styles.faq_icon}>
+		<div className={styles.container} draggable="false">
+			<div draggable="false">
+				<div
+					size={400}
+					className={styles.faq_icon}
+					role="presentation"
+					draggable="true"
+					onDrag={(ev) => onDragHandler(ev)}
+					ref={ballRef}
+					onClick={() => { setShow(!show); }}
+				>
 					<img
 						src="https://cdn.cogoport.io/cms-prod/cogo_admin/vault/original/faq-icon-final.svg"
 						style={{ background: '#ffffff' }}
 						alt="faq-icon"
+						draggable="false"
+						className={styles.FAQimage}
 					/>
 				</div>
-			</Draggable>
+			</div>
+			<div />
 			{show ? (
 				<Modal
 					className="primary lg"
