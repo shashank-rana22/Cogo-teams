8361f01577f22048a91a7bebbd9f0eedd66a8e27
Ritik Goyal <ritik.goyal@cogoport.com>
2023-03-22 11:14:27 +0530

    eslint fixes

--- packages/cogo-academy/pages/announcements/index.js
+++ packages/cogo-academy/pages/announcements/index.js
@@ -1,1 +1,2 @@
-export { default } from '../../page-components/Announcements';
+// eslint-disable-next-line import/no-unresolved
+export { default } from '@cogoport/cogo-academy/page-components/Announcements';
