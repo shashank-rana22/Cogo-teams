56be6cfb7bf52a521b13c93b73e48f5c52054ab0
Hritwik Tripathi <hritwik.tripathi@cogoport.com>
2023-03-22 17:33:00 +0530

    (fix): margin and style fix for eventlist in events

--- packages/allocation/page-components/KamExpertise/components/Events/EventDetails/EventList/EventLoadingState/index.js
+++ packages/allocation/page-components/KamExpertise/components/Events/EventDetails/EventList/EventLoadingState/index.js
@@ -9,28 +9,28 @@ function EventLoadingState() {
 			{[1, 2, 3].map((item) => (
 				<section className={styles.list_item_container} key={item}>
 					<div className={styles.top_div}>
-						<Placeholder width="20px" style={{ marginBottom: '4px' }} />
+						<Placeholder width="20px" margin="0 0 4px 0" />
 
-						<Placeholder width="20px" style={{ marginBottom: '4px' }} />
+						<Placeholder width="20px" margin="0 0 4px 0" />
 					</div>
 
 					<div>
 						<p className={styles.info_tag}>
-							<Placeholder width="200px" style={{ marginBottom: '4px' }} />
+							<Placeholder width="200px" margin="0 0 4px 0" />
 						</p>
 
 						<div className={styles.info_tag}>
-							<Placeholder width="200px" style={{ marginBottom: '12px' }} />
+							<Placeholder width="200px" margin="0 0 12px 0" />
 						</div>
 
 						<p className={styles.info_tag}>
-							<Placeholder width="200px" style={{ marginBottom: '4px' }} />
+							<Placeholder width="200px" margin="0 0 4px 0" />
 						</p>
 					</div>
 
 					<div className={styles.rule}>
 						<p className={styles.rule_head}>
-							<Placeholder width="200px" style={{ marginBottom: '12px' }} />
+							<Placeholder width="200px" margin="0 0 12px 0" />
 						</p>
 
 						{[11, 12].map(() => (
@@ -38,9 +38,8 @@ function EventLoadingState() {
 								{[21, 22, 23, 24, 25].map(() => (
 									<Placeholder
 										width="120px"
-										style={{
-											margin: '0 12px 4px 0',
-										}}
+										margin="0 12px 4px 0"
+
 									/>
 								))}
 							</div>
