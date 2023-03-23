73b93719b941fae449e14820659b75e0c3928528
piyushi-cogo <piyushi.garg@cogoport.com>
2023-03-23 19:53:43 +0530

    layout

---
+++ packages/shipment-fcl-freight/common/Layout/getController.js
@@ -0,0 +1,31 @@
+import {
+	InputController, PillsController, SelectController, DatepickerController,
+} from '@cogoport/forms';
+import UploadController from '@cogoport/forms/page-components/Controlled/UploadController';
+
+const getElementController = (type = 'text') => {
+	switch (type) {
+		case 'text':
+			return InputController;
+
+		case 'number':
+			return InputController;
+
+		case 'select':
+			return SelectController;
+
+		case 'file':
+			return UploadController;
+
+		case 'pills':
+			return PillsController;
+
+		case 'date_picker':
+			return DatepickerController;
+
+		default:
+			return null;
+	}
+};
+
+export default getElementController;
