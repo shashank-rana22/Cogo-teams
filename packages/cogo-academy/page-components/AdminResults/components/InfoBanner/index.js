f8d117ad1ea8b42015844f8defbbf6b091cf88af
shivanshi-cogoport <shivanshi.gupta@cogoport.com>
2023-03-23 15:57:06 +0530

    publish test

---
+++ packages/cogo-academy/page-components/AdminResults/components/InfoBanner/index.js
@@ -0,0 +1,52 @@
+import { IcMCrossInCircle } from '@cogoport/icons-react';
+
+import PublishNow from '../PublishNow';
+
+import styles from './styles.module.css';
+
+const TEXT_MAPPING = {
+	active: {
+		key             : 'active',
+		text            : 'Test Results have not been published yet.',
+		subText         : 'They will be auto-published when the validity ends',
+		iconColor       : '#D6B300',
+		backgroundColor : '#FDFBF6',
+		borderColor     : '#D6B300',
+	},
+	published: {
+		key             : 'published',
+		text            : 'Test Results have been Published.',
+		subText         : 'Your students can now see their scores.',
+		iconColor       : '#849E4C',
+		backgroundColor : '#F7FAEF',
+		borderColor     : '#849E4C',
+	},
+};
+
+function InfoBanner({ test_status, test_id, refetchTest }) {
+	if (!['published', 'active'].includes(test_status)) {
+		return null;
+	}
+
+	const content = TEXT_MAPPING[test_status];
+
+	const { key, backgroundColor, text, subText, iconColor, borderColor } = content;
+
+	return (
+		<div className={styles.container} style={{ border: `1px solid ${borderColor}`, background: backgroundColor }}>
+			<div className={styles.content}>
+				<IcMCrossInCircle style={{ color: iconColor, marginRight: 12, width: 24, height: 24 }} />
+
+				<b className={styles.margin_right}>{text}</b>
+
+				<span>{subText}</span>
+			</div>
+
+			<div>
+				{key !== 'published' ? <PublishNow test_id={test_id} refetchTest={refetchTest} /> : null}
+			</div>
+		</div>
+	);
+}
+
+export default InfoBanner;
---
+++ packages/cogo-academy/page-components/AdminResults/components/TestResults/index.js
@@ -0,0 +1,3 @@
+const TestResults = () => {};
+
+export default TestResults;
