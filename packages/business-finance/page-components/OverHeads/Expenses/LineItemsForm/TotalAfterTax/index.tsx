916cb7db22fbce241e217c9b9db9ae4ed099dc50
Nagendra Singh <nagendra.singh@cogoport.com>
2023-03-24 13:40:24 +0530

    showing amount in words

--- packages/business-finance/page-components/OverHeads/Expenses/LineItemsForm/TotalAfterTax/index.tsx
+++ packages/business-finance/page-components/OverHeads/Expenses/LineItemsForm/TotalAfterTax/index.tsx
@@ -1,6 +1,8 @@
+import { startCase } from '@cogoport/utils';
+import converter from 'number-to-words';
 import React from 'react';
 
-import styles from '../styles.module.css';
+import styles from './styles.module.css';
 
 function TotalAfterTax({ payableAmount, invoiceCurrency }) {
 	const getValue = (value) => {
@@ -10,13 +12,24 @@ function TotalAfterTax({ payableAmount, invoiceCurrency }) {
 		return parseFloat(value).toFixed(2);
 	};
 	return (
-		<div className={styles.tat}>
-			<span className={styles.label}>Total Payable :</span>
-			<span className={styles.value}>
-				{invoiceCurrency}
-				{' '}
-				{getValue(payableAmount)}
-			</span>
+		<div>
+			<div className={styles.tat}>
+				<div>
+					<span className={styles.label}>Total Payable :</span>
+				</div>
+				<span className={styles.value}>
+					{invoiceCurrency}
+					{' '}
+					{getValue(payableAmount)}
+				</span>
+			</div>
+			{payableAmount > 0 ? (
+				<div className={styles.words}>
+					{startCase(converter.toWords(payableAmount))}
+					{' '}
+					Rupees Only
+				</div>
+			) : null}
 		</div>
 	);
 }
