f2d10927ced86acb59fa9eabaddc26a5746b396e
shivanshi-cogoport <shivanshi.gupta@cogoport.com>
2023-03-24 15:37:34 +0530

    header fixes

--- packages/cogo-academy/page-components/UserDashboard/LeftSection/LastTestResults/Percentile/index.js
+++ packages/cogo-academy/page-components/UserDashboard/LeftSection/LastTestResults/Percentile/index.js
@@ -1,11 +1,11 @@
 import styles from './styles.module.css';
 
-function Percentile({ stats_data }) {
+function Percentile({ stats_data, hasPassed }) {
 	return (
 		<div className={styles.main_container}>
-			<div className={styles.container}>
+			<div className={styles.container} style={{ background: hasPassed ? '#f7faef' : '#fef3f1' }}>
 				Percentile
-				<div className={styles.percentile}>
+				<div className={styles.percentile} style={{ color: hasPassed ? '#849e4c' : '#bf2a1e' }}>
 					{stats_data.user_percentile?.toFixed(2)}
 					{' '}
 					%
