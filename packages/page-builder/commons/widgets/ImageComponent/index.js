371d52edf24db2768600c74f28aec4a4a9d92fdf
sushilr29 <sushil.rindhe@cogoport.com>
2023-03-25 19:56:07 +0530

    implemented image upload

--- packages/page-builder/commons/widgets/ImageComponent/index.js
+++ packages/page-builder/commons/widgets/ImageComponent/index.js
@@ -1,27 +1,14 @@
-import { useState, useRef } from 'react';
+import { useState, useEffect } from 'react';
+
+import FileUploader from '../FileUploader';
 
 function ImageComponent(props) {
-	const { src, alt, styles, components, setComponents, elementId } = props;
+	const { src, styles, components, setComponents, elementId } = props;
 
-	const [fileValue, setFileValue] = useState(src);
+	const [fileValue, setFileValue] = useState();
+	const [isFocused, setIsFocused] = useState(false);
 
-	const inputRef = useRef(null);
-
-	const handleClick = () => {
-		inputRef.current?.click();
-	};
-
-	const handleFileUpload = (event) => {
-		const file = event.target.files[0];
-
-		if (file) {
-			const reader = new FileReader();
-			reader.readAsDataURL(file);
-			reader.onload = () => {
-				setFileValue(reader.result);
-			};
-		}
-
+	useEffect(() => {
 		if (fileValue) {
 			// eslint-disable-next-line max-len, max-len, max-len
 			const selectedComponentIndex = (components || []).findIndex((component) => (component.id === elementId));
@@ -42,19 +29,35 @@ function ImageComponent(props) {
 				...prevComponents.slice(selectedComponentIndex + 1),
 			]);
 		}
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [fileValue]);
+
+	const editorStyle = {
+		border  : isFocused ? '1.5px solid #88cad1' : '1px solid #ccc',
+		padding : '10px',
+		margin  : '20px',
+
 	};
 
 	return (
-		<div>
-			<img
-				role="presentation"
-				src={fileValue}
-				style={styles}
-				alt={alt}
-				onClick={handleClick}
-			/>
+		<div style={editorStyle}>
 
-			<input type="file" onChange={handleFileUpload} ref={inputRef} style={{ display: 'none' }} />
+			{fileValue ? (
+				<img
+					role="presentation"
+					src={src}
+					style={styles}
+					alt="upload-img"
+					onClick={() => setIsFocused(!isFocused)}
+				/>
+			) : (
+				<FileUploader
+					value={fileValue}
+					onChange={setFileValue}
+					accept="png"
+					uploadDesc="Upload your image here"
+				/>
+			) }
 
 		</div>
 	);
