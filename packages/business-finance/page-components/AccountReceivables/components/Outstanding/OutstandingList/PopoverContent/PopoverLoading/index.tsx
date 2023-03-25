fbb72aede3d467c7dfd896832dd22615c6f543f6
simranjitsingh3720 <simranjit.singh@cogoport.com>
2023-03-25 16:09:07 +0530

    changes done

--- packages/business-finance/page-components/AccountReceivables/components/Outstanding/OutstandingList/PopoverContent/PopoverLoading/index.tsx
+++ packages/business-finance/page-components/AccountReceivables/components/Outstanding/OutstandingList/PopoverContent/PopoverLoading/index.tsx
@@ -1,7 +1,7 @@
 import styles from './styles.module.css';
 
 interface Props {
-	containerHeight: string,
+	containerHeight?: string,
 }
 function PopoverLoader({ containerHeight = '' }: Props) {
 	return (
