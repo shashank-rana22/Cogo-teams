a2eee3b439a7f15606d6f4171793c28d16c738d3
shivanshi-cogoport <shivanshi.gupta@cogoport.com>
2023-03-25 12:43:43 +0530

    search and select filter added in tests list

--- packages/cogo-academy/page-components/UserDashboard/TestsList/Header/index.js
+++ packages/cogo-academy/page-components/UserDashboard/TestsList/Header/index.js
@@ -1,10 +1,34 @@
-import { Input } from '@cogoport/components';
+import { Select, Input } from '@cogoport/components';
 import { IcMSearchlight } from '@cogoport/icons-react';
+import { useState } from 'react';
 
 import styles from './styles.module.css';
 
-function Header({ searchInput, debounceQuery }) {
+const testCategoryOptions = [
+	{
+		label : 'Active',
+		value : 'active_test',
+	},
+	{
+		label : 'Upcoming',
+		value : 'upcoming',
+	},
+	{
+		label : 'Completed',
+		value : 'completed',
+	},
+	{
+		label : 'Expired',
+		value : 'expired',
+	},
+];
+
+function Header({ debounceQuery, testCategory, setTestCategory }) {
+	const [searchInput, setSearchInput] = useState('');
+
 	const handleSearchQuery = (obj) => {
+		setSearchInput(obj);
+
 		debounceQuery(obj);
 	};
 
@@ -20,6 +44,16 @@ function Header({ searchInput, debounceQuery }) {
 					suffix={<div className={styles.icon_container}><IcMSearchlight /></div>}
 					onChange={handleSearchQuery}
 				/>
+
+				<div className={styles.select_container}>
+					<Select
+						size="sm"
+						value={testCategory}
+						placeholder="Select Day"
+						options={testCategoryOptions}
+						onChange={setTestCategory}
+					/>
+				</div>
 			</div>
 		</div>
 	);
--- packages/cogo-academy/page-components/UserDashboard/TestsList/index.js
+++ packages/cogo-academy/page-components/UserDashboard/TestsList/index.js
@@ -1,8 +1,10 @@
 import { useDebounceQuery } from '@cogoport/forms';
 import { useRequest } from '@cogoport/request';
 import { useSelector } from '@cogoport/store';
+import { useEffect, useState } from 'react';
 
 import Header from './Header';
+import LoadingState from './LoadingState';
 import styles from './styles.module.css';
 import TestCard from './TestCard';
 
@@ -14,24 +16,32 @@ function TestsList() {
 	const [{ data = {}, loading }, trigger] = useRequest({
 		method : 'GET',
 		url    : '/list_tests',
-		params : {
-			// page_limit : 5,
-			filters: {
-				user_id,
-				status: 'active',
-			},
-		},
-	}, { manual: false });
+	}, { manual: true });
 
-	if (loading) {
-		return 'loading';
-	}
+	const [testCategory, setTestCategory] = useState('active_test');
+
+	useEffect(() => {
+		try {
+			trigger({
+				params: {
+					filters: {
+						q              : searchQuery,
+						user_id,
+						status         : 'active',
+						current_status : testCategory,
+					},
+				},
+			});
+		} catch (err) {
+			console.log(err, 'err');
+		}
+	}, [searchQuery, trigger, user_id, testCategory]);
 
 	return (
 		<div className={styles.container}>
-			<Header debounceQuery={debounceQuery} />
+			<Header debounceQuery={debounceQuery} testCategory={testCategory} setTestCategory={setTestCategory} />
 
-			{(data.list || []).map((test_card) => (
+			{loading ? <LoadingState /> : (data.list || []).map((test_card) => (
 				<TestCard key={test_card} test_card={test_card} />
 			))}
 		</div>
