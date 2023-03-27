c7b26dd51b54c7c0e8f13a984e1d1bd7804781b6
Nagendra Singh <nagendra.singh@cogoport.com>
2023-03-27 15:08:15 +0530

    module declared for all BF modules

--- packages/business-finance/global.d.ts
+++ packages/business-finance/global.d.ts
@@ -8,10 +8,4 @@ declare module '@cogoport/utils';
 declare module '@cogoport/components';
 declare module '@cogoport/next';
 declare module '@cogoport/forms';
-declare module '@cogoport/business-finance/page-components/TranslateAr';
-declare module '@cogoport/business-finance/page-components/IncidentManagement'
-declare module '@cogoport/business-finance/page-components/COEFinance';
-declare module '@cogoport/business-finance/page-components/COEFinance/Costsheet';
-declare module '@cogoport/business-finance/page-components/COEFinance/All_Invoices/ViewInvoices';
-declare module '@cogoport/business-finance/page-components/Reports';
-declare module '@cogoport/business-finance/page-components/Overheads';
+declare module '@cogoport/business-finance/page-components/*';
