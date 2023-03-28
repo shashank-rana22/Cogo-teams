cd62bec3b8786f11071eeb10001b65a5737a455f
shivanshi-cogoport <shivanshi.gupta@cogoport.com>
2023-03-29 01:30:58 +0530

    mapping created

--- packages/cogo-academy/commons/TestResultMessage/index.js
+++ packages/cogo-academy/commons/TestResultMessage/index.js
@@ -2,28 +2,39 @@ import { format, startCase } from '@cogoport/utils';
 
 import styles from './styles.module.css';
 
+const RESULT_MAPPING = {
+	passed: {
+		image_url : 'https://cdn.cogoport.io/cms-prod/cogo_admin/vault/original/confetti.svg',
+		message   : 'Congratulations',
+		alt       : 'confetti',
+		color     : '#849e4c',
+	},
+	failed: {
+		image_url : 'https://cdn.cogoport.io/cms-prod/cogo_admin/vault/original/sad-face.svg',
+		message   : 'Unfortunately, you have not been able to clear the test',
+		alt       : 'sad-face',
+		color     : '#BF291E',
+	},
+};
+
 function TestResultMessage({ stats_data }) {
 	const { date, status, test_name } = stats_data || {};
 
-	const hasPassed = status === 'passed';
-
-	const image_url = hasPassed
-		? 'https://cdn.cogoport.io/cms-prod/cogo_admin/vault/original/confetti.svg'
-		: 'https://cdn.cogoport.io/cms-prod/cogo_admin/vault/original/sad-face.svg';
+	const { image_url, message, alt, color } = RESULT_MAPPING[status];
 
 	return (
 		<div className={styles.message_container}>
 			<img
 				src={image_url}
-				alt="confetti"
+				alt={alt}
 				style={{ marginRight: 20 }}
 				width={42}
 				height={42}
 			/>
 
 			<div className={styles.column}>
-				<div className={styles.message_content} style={{ color: hasPassed ? '#849e4c' : '#BF291E' }}>
-					{hasPassed ? 'Congratulations' : 'Unfortunately, you have not been able to clear the test'}
+				<div className={styles.message_content} style={{ color }}>
+					{message}
 				</div>
 
 				<div className={styles.sub_text}>
@@ -40,7 +51,7 @@ function TestResultMessage({ stats_data }) {
 					{format(date, 'dd MMM yyyy')}
 				</div>
 
-				{!hasPassed ? (
+				{status === 'failed' ? (
 					<div className={styles.sub_text}>
 						Prepare well and try again in a few days.
 					</div>
