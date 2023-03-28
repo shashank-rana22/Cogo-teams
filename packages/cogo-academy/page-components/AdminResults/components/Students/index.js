e04e444ddffbfe39d651fbc74fe0f3aaa25f1624
rupesh-mutyala <rupesh.mutyala@cogoport.com>
2023-03-28 18:37:22 +0530

    changes in admin dashboard

--- packages/cogo-academy/page-components/AdminResults/components/Students/index.js
+++ packages/cogo-academy/page-components/AdminResults/components/Students/index.js
@@ -1,13 +1,12 @@
 import { Pagination, Table, TabPanel, Tabs } from '@cogoport/components';
 import { useDebounceQuery } from '@cogoport/forms';
-import { IcMArrowRotateDown, IcMArrowRotateUp } from '@cogoport/icons-react';
 import { useRequest } from '@cogoport/request';
-import { format, startCase } from '@cogoport/utils';
 import { useState, useEffect } from 'react';
 
 import Filters from '../../commons/Filters';
 
 import styles from './styles.module.css';
+import tableColumns from './TableColumns';
 
 function StudentsComponent({ test_id }) {
 	const [activeTab, setActiveTab] = useState('');
@@ -19,11 +18,7 @@ function StudentsComponent({ test_id }) {
 	const [sortType, setSortType] = useState('');
 	const [searchValue, setSearchValue] = useState('');
 
-	useEffect(() => {
-		debounceQuery(searchValue);
-	}, [debounceQuery, searchValue]);
-
-	const [{ data, loading }] = useRequest({
+	const [{ data, loading }, refetch] = useRequest({
 		method : 'GET',
 		url    : '/list_admin_student_wise_test_result',
 		params : {
@@ -32,215 +27,21 @@ function StudentsComponent({ test_id }) {
 			sort_type   : sortType,
 			filters     : { final_result: filter },
 			search_term : query,
+			...params,
 		},
 	}, { manual: false });
 
 	const { page_limit = 0, total_count = 0, list } = data || {};
 
-	const applySorting = (val, typ) => {
-		setSortBy(val);
-		setSortType(typ);
-	};
-	const columns = [
-		{
-			Header:
-	<div style={{ display: 'flex', alignItems: 'baseline' }}>
-		<div style={{ marginRight: '12px' }}>NAME</div>
-		<div style={{ display: 'flex', flexDirection: 'column', marginTop: '-4px' }}>
-			{
-					(sortBy === 'user' && sortType === 'desc')
-				&& (
-					<div>
-						<IcMArrowRotateUp
-							width={12}
-							height={12}
-							onClick={() => { applySorting('user', 'asc'); }}
-						/>
-					</div>
-				)
-				}
-			{
-					!(sortBy === 'user' && sortType === 'desc')
-				&& (
-					<div>
-						<IcMArrowRotateDown
-							width={12}
-							height={12}
-							onClick={() => { applySorting('user', 'desc'); }}
-						/>
-					</div>
-				)
-	}
-		</div>
-	</div>,
-			id       : 'a',
-			accessor : ({ user = '' }) => (
-				<section>{user}</section>
-			),
-		},
+	const columns = tableColumns({ sortType, sortBy, setSortBy, setSortType });
 
-		{
-			Header   : <div>PASSED/FAILED</div>,
-			id       : 'ss',
-			accessor : ({ result = 0 }) => (
-				<section>{startCase(result) || '-'}</section>
-			),
-		},
-		{
-			Header:
-	<div style={{ display: 'flex', alignItems: 'baseline' }}>
-		<div style={{ marginRight: '12px' }}>SCORE</div>
-		<div style={{ display: 'flex', flexDirection: 'column', marginTop: '-4px' }}>
-			{
-				(sortBy === 'score_achieved' && sortType === 'desc')
-			&& (
-				<div>
-					<IcMArrowRotateUp
-						width={12}
-						height={12}
-						onClick={() => { applySorting('score_achieved', 'asc'); }}
-					/>
-				</div>
-			)
-			}
-			{
-				!(sortBy === 'score_achieved' && sortType === 'desc')
-			&& (
-				<div>
-					<IcMArrowRotateDown
-						width={12}
-						height={12}
-						onClick={() => { applySorting('score_achieved', 'desc'); }}
-					/>
-				</div>
-			)
-}
-		</div>
-	</div>,
-			id       : 'e',
-			accessor : ({ score_achieved = '', total_score }) => (
-				<section>
-					{score_achieved || '0'}
-					/
-					{total_score}
-				</section>
-			),
-		},
-		{
-			Header:
-	<div style={{ display: 'flex', alignItems: 'baseline' }}>
-		<div style={{ marginRight: '12px' }}>PERCENTILE</div>
-		<div style={{ display: 'flex', flexDirection: 'column', marginTop: '-4px' }}>
-			{
-				(sortBy === 'percentile' && sortType === 'desc')
-			&& (
-				<div>
-					<IcMArrowRotateUp
-						width={12}
-						height={12}
-						onClick={() => { applySorting('percentile', 'asc'); }}
-					/>
-				</div>
-			)
-			}
-			{
-				!(sortBy === 'percentile' && sortType === 'desc')
-			&& (
-				<div>
-					<IcMArrowRotateDown
-						width={12}
-						height={12}
-						onClick={() => { applySorting('percentile', 'desc'); }}
-					/>
-				</div>
-			)
-}
-		</div>
-	</div>,
-			id       : 'results',
-			accessor : ({ percentile = '' }) => (
-				<div>
-					{percentile !== null ? (percentile.toFixed(2)) : (' ')}
-				</div>
-			),
-		},
-		{
-			Header:
-	<div style={{ display: 'flex', alignItems: 'baseline' }}>
-		<div style={{ marginRight: '12px' }}>TIME TAKEN</div>
-		<div style={{ display: 'flex', flexDirection: 'column', marginTop: '-4px' }}>
-			{
-				(sortBy === 'time_taken' && sortType === 'desc')
-			&& (
-				<div>
-					<IcMArrowRotateUp
-						width={12}
-						height={12}
-						onClick={() => { applySorting('time_taken', 'asc'); }}
-					/>
-				</div>
-			)
-			}
-			{
-				!(sortBy === 'time_taken' && sortType === 'desc')
-			&& (
-				<div>
-					<IcMArrowRotateDown
-						width={12}
-						height={12}
-						onClick={() => { applySorting('time_taken', 'desc'); }}
-					/>
-				</div>
-			)
-}
-		</div>
-	</div>,
-			id       : 'time',
-			accessor : ({ time_taken = '' }) => (
-				<div>
-					{time_taken}
-				</div>
-			),
-		},
-		{
-			Header:
-	<div style={{ display: 'flex', alignItems: 'baseline' }}>
-		<div style={{ marginRight: '12px' }}>ATTEMPTED ON</div>
-		<div style={{ display: 'flex', flexDirection: 'column', marginTop: '-4px' }}>
-			{
-				(sortBy === 'attempted_on' && sortType === 'desc')
-			&& (
-				<div>
-					<IcMArrowRotateUp
-						width={12}
-						height={12}
-						onClick={() => { applySorting('attempted_on', 'asc'); }}
-					/>
-				</div>
-			)
-			}
-			{
-				!(sortBy === 'attempted_on' && sortType === 'desc')
-			&& (
-				<div>
-					<IcMArrowRotateDown
-						width={12}
-						height={12}
-						onClick={() => { applySorting('attempted_on', 'desc'); }}
-					/>
-				</div>
-			)
-}
-		</div>
-	</div>,
-			id       : 'updatedAt',
-			accessor : ({ attempted_on = '' }) => (
-				<section>
-					{format(attempted_on, 'dd MMM yyyy hh:mm a')}
-				</section>
-			),
-		},
-	];
+	useEffect(() => {
+		debounceQuery(searchValue);
+	}, [debounceQuery, searchValue]);
+
+	useEffect(() => {
+		refetch();
+	}, [params, refetch]);
 
 	return (
 		<div>
@@ -271,14 +72,14 @@ function StudentsComponent({ test_id }) {
 					loading={loading}
 				/>
 
-				{total_count > 10 ? (
+				{total_count > page_limit ? (
 					<div className={styles.pagination_container}>
 						<Pagination
 							type="table"
 							currentPage={params?.page}
 							totalItems={total_count}
 							pageSize={page_limit}
-							// onPageChange={(val) => setParams((prev) => ({ ...prev, page: val }))}
+							onPageChange={(val) => setParams((prev) => ({ ...prev, page: val }))}
 						/>
 					</div>
 				) : null}
