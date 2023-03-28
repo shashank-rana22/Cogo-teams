ba90bfefab0e99a098e454aebde689ffa54245aa
rupesh-mutyala <rupesh.mutyala@cogoport.com>
2023-03-28 13:31:45 +0530

    fixes

--- packages/cogo-academy/page-components/CreateModule/utils/checkErrors.js
+++ packages/cogo-academy/page-components/CreateModule/utils/checkErrors.js
@@ -1,6 +1,10 @@
 const checkErrors = ({ options, question_type }) => {
 	const correctOptions = options.filter((item) => item.is_correct === 'true');
 
+	const allOptions = options.map((item) => item?.answer_text);
+
+	const uniqueOptions = new Set(allOptions);
+
 	const correctOptionsLength = correctOptions.length;
 
 	if (!correctOptionsLength) {
@@ -9,6 +13,10 @@ const checkErrors = ({ options, question_type }) => {
 
 	if (question_type === 'single_correct' && correctOptionsLength > 1) {
 		return 'Only one option should be true';
+	}
+
+	if (allOptions.length !== [...uniqueOptions].length) {
+		return 'All the options should be unique';
 	}
 
 	return 'noError';
