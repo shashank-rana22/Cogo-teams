7fd5b809d2a23f9d3672e3545a69a35f90118c27
Prachi Dewangan <prachi.dewangan@cogoport.com>
2023-03-28 20:00:56 +0530

    apis data removed

--- common/core/components/AdminLayout/FAQs/QuestionList/Answer/useAnswer.js
+++ common/core/components/AdminLayout/FAQs/QuestionList/Answer/useAnswer.js
@@ -1,33 +1,14 @@
 import { Toast } from '@cogoport/components';
 import { useRequest } from '@cogoport/request';
-import { useSelector } from '@cogoport/store';
-import { isEmpty } from '@cogoport/utils';
 import { useCallback, useMemo } from 'react';
 
 const useAnswer = ({ question }) => {
-	const { general = {}, profile = {} } = useSelector((state) => state);
-	const { auth_role_data = {}, partner = {} } = profile;
-	const { scope = '' } = general || {};
-	const { country_id = '', id : cogo_entity_id = '' } = partner;
-	const { role_functions = [], role_sub_functions = [] } = auth_role_data || {};
-
-	const roleFunction = !isEmpty(role_functions) ? role_functions : undefined;
-
-	const roleSubFunction = !isEmpty(role_sub_functions)
-		? role_sub_functions
-		: undefined;
-
 	const params = useMemo(
 		() => ({
-			id                : question?.id,
-			auth_function     : scope === 'partner' ? roleFunction : undefined,
-			auth_sub_function : scope === 'partner' ? roleSubFunction : undefined,
-			country_id,
-			cogo_entity_id,
-			persona           : scope === 'partner' ? 'admin_user' : 'importer_exporter',
-			platform          : scope === 'partner' ? 'admin' : 'app',
+			id: question?.id,
+
 		}),
-		[cogo_entity_id, country_id, question?.id, roleFunction, roleSubFunction, scope],
+		[question?.id],
 	);
 
 	const [{ data, loading }, trigger] = useRequest({
--- common/core/components/AdminLayout/FAQs/TopicList/NotificationContent/useClearFaqNotifications.js
+++ common/core/components/AdminLayout/FAQs/TopicList/NotificationContent/useClearFaqNotifications.js
@@ -1,24 +1,10 @@
 import { Toast } from '@cogoport/components';
 import { useRequest } from '@cogoport/request';
-import { useSelector } from '@cogoport/store';
-import { isEmpty } from '@cogoport/utils';
 
 const useClearFaqNotifications = ({
 	setShowNotificationContent,
 	faqNotificationData = {},
 }) => {
-	const { general = {}, profile = {} } = useSelector((state) => state);
-	const { auth_role_data = {}, partner = {} } = profile;
-	const { scope = '' } = general || {};
-	const { country_id = '', id : cogo_entity_id = '' } = partner;
-	const { role_functions = [], role_sub_functions = [] } = auth_role_data || {};
-
-	const roleFunction = !isEmpty(role_functions) ? role_functions : undefined;
-
-	const roleSubFunction = !isEmpty(role_sub_functions)
-		? role_sub_functions
-		: undefined;
-
 	const [{ loading = false }, trigger] = useRequest({
 		url    : 'clear_faq_notifications',
 		method : 'POST',
@@ -29,13 +15,8 @@ const useClearFaqNotifications = ({
 	);
 
 	const params = {
-		faq_question_ids  : questionIds,
-		auth_function     : scope === 'partner' ? roleFunction : undefined,
-		auth_sub_function : scope === 'partner' ? roleSubFunction : undefined,
-		country_id,
-		cogo_entity_id,
-		persona           : scope === 'partner' ? 'admin_user' : 'importer_exporter',
-		platform          : scope === 'partner' ? 'admin' : 'app',
+		faq_question_ids: questionIds,
+
 	};
 
 	const onClickClearNotifications = async () => {
