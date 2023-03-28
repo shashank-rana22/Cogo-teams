79c196b597dbd9d34a1ea1f6078bd041bfe9e44e
cogomanish <manish.kumar@cogoport.com>
2023-03-28 14:09:24 +0530

    changes

--- packages/business-finance/page-components/CogoBook/P&L/PLStatement/index.tsx
+++ packages/business-finance/page-components/CogoBook/P&L/PLStatement/index.tsx
@@ -1,11 +1,13 @@
 import Card from './Card';
 import ListProfit from './ListProfit';
+import styles from './styles.module.css';
 
 function PLStatement() {
 	return (
 		<div>
 			<Card />
 			<ListProfit />
+			<div className={styles.amount}>All amounts are in INR*</div>
 		</div>
 	);
 }
