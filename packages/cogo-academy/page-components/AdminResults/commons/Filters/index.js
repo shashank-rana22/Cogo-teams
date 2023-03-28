67adc5c8459cf9566a3067c52f7ad9fb3a000267
Ritvik98349 <124043414+Ritvik98349@users.noreply.github.com>
2023-03-28 13:03:39 +0530

    Filters and sorting working

--- packages/cogo-academy/page-components/AdminResults/commons/Filters/index.js
+++ packages/cogo-academy/page-components/AdminResults/commons/Filters/index.js
@@ -1,29 +1,42 @@
-import { Input } from '@cogoport/components';
-import { IcMSearchlight } from '@cogoport/icons-react';
-import { useState } from 'react';
+import { Select, Input } from '@cogoport/components';
+import { IcMFilter, IcMSearchlight } from '@cogoport/icons-react';
 
 import styles from './styles.module.css';
 
-function Filters() {
-	const [value, setValue] = useState();
-
-	const handleSearchValue = (values) => {
-		console.log(values, 'input search');
+function Filters({ filter = '', setFilter = () => {}, searchValue = '', setSearchValue = () => {} }) {
+	const handleSearchValue = (search) => {
+		setSearchValue(search);
 	};
+	const options = [
+		{ label: 'Passed', value: 'passed' },
+		{ label: 'Failed', value: 'failed' },
+	];
 
 	return (
 		<div className={styles.container}>
 			<Input
 				size="md"
 				placeholder="Search for Student name"
-				value={value}
+				value={searchValue}
 				onChange={handleSearchValue}
 				suffix={(
 					<div className={styles.icon_container}>
 						<IcMSearchlight />
 					</div>
 				)}
+				style={{ width: 300 }}
 			/>
+			<div className={styles.filters}>
+
+				<Select
+					prefix={(<IcMFilter />)}
+					value={filter}
+					onChange={(val) => setFilter(val)}
+					placeholder="Filter"
+					options={options}
+				/>
+
+			</div>
 		</div>
 	);
 }
--- packages/cogo-academy/page-components/AdminResults/components/Students/index.js
+++ packages/cogo-academy/page-components/AdminResults/components/Students/index.js
@@ -1,30 +1,78 @@
 import { Pagination, Table, TabPanel, Tabs } from '@cogoport/components';
+import { useDebounceQuery } from '@cogoport/forms';
+import { IcMArrowRotateDown, IcMArrowRotateUp } from '@cogoport/icons-react';
 import { useRequest } from '@cogoport/request';
 import { format, startCase } from '@cogoport/utils';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import Filters from '../../commons/Filters';
 
 import styles from './styles.module.css';
 
 function StudentsComponent({ test_id }) {
-	const [activeTab, setActiveTab] = useState();
+	const [activeTab, setActiveTab] = useState('');
+	const { debounceQuery, query } = useDebounceQuery();
 
 	const [params, setParams] = useState({});
+	const [filter, setFilter] = useState('');
+	const [sortBy, setSortBy] = useState('');
+	const [sortType, setSortType] = useState('');
+	const [searchValue, setSearchValue] = useState('');
+
+	useEffect(() => {
+		debounceQuery(searchValue);
+	}, [debounceQuery, searchValue]);
 
 	const [{ data, loading }] = useRequest({
 		method : 'GET',
 		url    : 'list_admin_student_wise_test_result',
 		params : {
 			test_id,
+			sort_by     : sortBy,
+			sort_type   : sortType,
+			filters     : { final_result: filter },
+			search_term : query,
 		},
 	}, { manual: false });
 
 	const { page_limit = 0, total_count = 0, list } = data || {};
 
+	const applySorting = (val, typ) => {
+		setSortBy(val);
+		setSortType(typ);
+	};
 	const columns = [
 		{
-			Header   : 'NAME',
+			Header:
+	<div style={{ display: 'flex', alignItems: 'baseline' }}>
+		<div style={{ marginRight: '12px' }}>NAME</div>
+		<div style={{ display: 'flex', flexDirection: 'column', marginTop: '-4px' }}>
+			{
+					(sortBy === 'user' && sortType === 'desc')
+				&& (
+					<div>
+						<IcMArrowRotateUp
+							width={12}
+							height={12}
+							onClick={() => { applySorting('user', 'asc'); }}
+						/>
+					</div>
+				)
+				}
+			{
+					!(sortBy === 'user' && sortType === 'desc')
+				&& (
+					<div>
+						<IcMArrowRotateDown
+							width={12}
+							height={12}
+							onClick={() => { applySorting('user', 'desc'); }}
+						/>
+					</div>
+				)
+	}
+		</div>
+	</div>,
 			id       : 'a',
 			accessor : ({ user = '' }) => (
 				<section>{user}</section>
@@ -32,14 +80,43 @@ function StudentsComponent({ test_id }) {
 		},
 
 		{
-			Header   : 'PASSED / FAILED',
+			Header   : <div>PASSED/FAILED</div>,
 			id       : 'ss',
 			accessor : ({ result = 0 }) => (
 				<section>{startCase(result) || '-'}</section>
 			),
 		},
 		{
-			Header   : 'SCORE',
+			Header:
+	<div style={{ display: 'flex', alignItems: 'baseline' }}>
+		<div style={{ marginRight: '12px' }}>SCORE</div>
+		<div style={{ display: 'flex', flexDirection: 'column', marginTop: '-4px' }}>
+			{
+				(sortBy === 'score_achieved' && sortType === 'desc')
+			&& (
+				<div>
+					<IcMArrowRotateUp
+						width={12}
+						height={12}
+						onClick={() => { applySorting('score_achieved', 'asc'); }}
+					/>
+				</div>
+			)
+			}
+			{
+				!(sortBy === 'score_achieved' && sortType === 'desc')
+			&& (
+				<div>
+					<IcMArrowRotateDown
+						width={12}
+						height={12}
+						onClick={() => { applySorting('score_achieved', 'desc'); }}
+					/>
+				</div>
+			)
+}
+		</div>
+	</div>,
 			id       : 'e',
 			accessor : ({ score_achieved = '', total_score }) => (
 				<section>
@@ -49,34 +126,113 @@ function StudentsComponent({ test_id }) {
 				</section>
 			),
 		},
-		// {
-		// 	Header   : 'TOPIC WISE SCORE',
-		// 	id       : 'ik',
-		// 	accessor : ({ audience_ids = [] }) => (
-		// 		<section>{audience_ids.length}</section>
-		// 	),
-		// },
-
 		{
-			Header   : 'PERCENTILE',
+			Header:
+	<div style={{ display: 'flex', alignItems: 'baseline' }}>
+		<div style={{ marginRight: '12px' }}>PERCENTILE</div>
+		<div style={{ display: 'flex', flexDirection: 'column', marginTop: '-4px' }}>
+			{
+				(sortBy === 'percentile' && sortType === 'desc')
+			&& (
+				<div>
+					<IcMArrowRotateUp
+						width={12}
+						height={12}
+						onClick={() => { applySorting('percentile', 'asc'); }}
+					/>
+				</div>
+			)
+			}
+			{
+				!(sortBy === 'percentile' && sortType === 'desc')
+			&& (
+				<div>
+					<IcMArrowRotateDown
+						width={12}
+						height={12}
+						onClick={() => { applySorting('percentile', 'desc'); }}
+					/>
+				</div>
+			)
+}
+		</div>
+	</div>,
 			id       : 'results',
 			accessor : ({ percentile = '' }) => (
 				<div>
-					{percentile}
+					{percentile !== null ? (percentile.toFixed(2)) : (' ')}
 				</div>
 			),
 		},
-		// {
-		// 	Header   : 'TIME TAKEN',
-		// 	id       : 'updatedAt',
-		// 	accessor : ({ updated_at = '' }) => (
-		// 		<section>
-		// 			{format(updated_at, 'dd MMM yyyy')}
-		// 		</section>
-		// 	),
-		// },
 		{
-			Header   : 'ATTEMPTED ON',
+			Header:
+	<div style={{ display: 'flex', alignItems: 'baseline' }}>
+		<div style={{ marginRight: '12px' }}>TIME TAKEN</div>
+		<div style={{ display: 'flex', flexDirection: 'column', marginTop: '-4px' }}>
+			{
+				(sortBy === 'time_taken' && sortType === 'desc')
+			&& (
+				<div>
+					<IcMArrowRotateUp
+						width={12}
+						height={12}
+						onClick={() => { applySorting('time_taken', 'asc'); }}
+					/>
+				</div>
+			)
+			}
+			{
+				!(sortBy === 'time_taken' && sortType === 'desc')
+			&& (
+				<div>
+					<IcMArrowRotateDown
+						width={12}
+						height={12}
+						onClick={() => { applySorting('time_taken', 'desc'); }}
+					/>
+				</div>
+			)
+}
+		</div>
+	</div>,
+			id       : 'time',
+			accessor : ({ time_taken = '' }) => (
+				<div>
+					{time_taken}
+				</div>
+			),
+		},
+		{
+			Header:
+	<div style={{ display: 'flex', alignItems: 'baseline' }}>
+		<div style={{ marginRight: '12px' }}>ATTEMPTED ON</div>
+		<div style={{ display: 'flex', flexDirection: 'column', marginTop: '-4px' }}>
+			{
+				(sortBy === 'attempted_on' && sortType === 'desc')
+			&& (
+				<div>
+					<IcMArrowRotateUp
+						width={12}
+						height={12}
+						onClick={() => { applySorting('attempted_on', 'asc'); }}
+					/>
+				</div>
+			)
+			}
+			{
+				!(sortBy === 'attempted_on' && sortType === 'desc')
+			&& (
+				<div>
+					<IcMArrowRotateDown
+						width={12}
+						height={12}
+						onClick={() => { applySorting('attempted_on', 'desc'); }}
+					/>
+				</div>
+			)
+}
+		</div>
+	</div>,
 			id       : 'updatedAt',
 			accessor : ({ attempted_on = '' }) => (
 				<section>
@@ -100,7 +256,12 @@ function StudentsComponent({ test_id }) {
 				</Tabs>
 			</div>
 
-			<Filters />
+			<Filters
+				filter={filter}
+				setFilter={setFilter}
+				searchValue={searchValue}
+				setSearchValue={setSearchValue}
+			/>
 
 			<div className={styles.table_container}>
 				<Table
