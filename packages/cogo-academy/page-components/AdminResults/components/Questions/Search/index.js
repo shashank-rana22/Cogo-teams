836d705cd5d10f4edd5914e9e7d17344318a8d36
Ritvik98349 <124043414+Ritvik98349@users.noreply.github.com>
2023-03-29 15:43:22 +0530

    search changes

--- packages/cogo-academy/page-components/AdminResults/components/Questions/Search/index.js
+++ packages/cogo-academy/page-components/AdminResults/components/Questions/Search/index.js
@@ -4,17 +4,13 @@ import { IcMSearchlight } from '@cogoport/icons-react';
 import styles from './styles.module.css';
 
 function Search({ searchQuestion = '', setSearchQuestion = () => {} }) {
-	const handleSearchValue = (search) => {
-		setSearchQuestion(search);
-	};
-
 	return (
 		<div className={styles.container}>
 			<Input
 				size="md"
 				placeholder="Search for a Question"
 				value={searchQuestion}
-				onChange={handleSearchValue}
+				onChange={(search) => setSearchQuestion(search)}
 				suffix={(
 					<div className={styles.icon_container}>
 						<IcMSearchlight />
