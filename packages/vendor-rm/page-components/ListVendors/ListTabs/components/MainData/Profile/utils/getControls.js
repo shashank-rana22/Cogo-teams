39ea3a3fe9ca0f704be0b4e8516d369df20a4793
shivanshi-cogoport <shivanshi.gupta@cogoport.com>
2023-03-31 13:12:11 +0530

    vendor id from query

--- packages/vendor-rm/page-components/ListVendors/ListTabs/components/MainData/Profile/utils/getControls.js
+++ packages/vendor-rm/page-components/ListVendors/ListTabs/components/MainData/Profile/utils/getControls.js
@@ -25,7 +25,6 @@ export const getControls = ({ countryOptions = {}, cityOptions = {} }) => [
 		type            : 'file',
 		themeType       : 'secondary',
 		draggable       : true,
-		// uploadIcon      : 'ic-upload',
 		onlyURLOnChange : true,
 		accept          : 'image/*',
 		uploadType      : 'aws',
