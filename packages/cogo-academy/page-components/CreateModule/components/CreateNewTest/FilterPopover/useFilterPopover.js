7c9e7523454b90925e4547ad04f209c1a264c499
Madhesh Medasani <Madhesh.Medasani@cogoport.com>
2023-03-31 11:29:29 +0530

    filter popover

--- packages/cogo-academy/page-components/CreateModule/components/CreateNewTest/FilterPopover/useFilterPopover.js
+++ packages/cogo-academy/page-components/CreateModule/components/CreateNewTest/FilterPopover/useFilterPopover.js
@@ -7,9 +7,10 @@ const useFilterPopover = ({ setFilters }) => {
 	const { control, handleSubmit, reset } = useForm();
 
 	const onSubmit = (values) => {
-		setFilters({
+		setFilters((prev) => ({
+			...prev,
 			cogo_entity_id: values?.cogo_entity_id,
-		});
+		}));
 
 		setShowFilter(false);
 	};
