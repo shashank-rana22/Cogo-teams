2a7ee27da846462d50c034225c75ffacb90827c2
Prachi Dewangan <prachi.dewangan@cogoport.com>
2023-03-31 17:12:00 +0530

    added request flow in admin faq

---
+++ packages/cogo-academy/commons/EmptyQuestionListState/RequestQAForm/index.js
@@ -0,0 +1,82 @@
+import { Button, Textarea, Input } from '@cogoport/components';
+
+import useCreateQuestionSet from '../useCreateQuestionRequest';
+
+import styles from './styles.module.css';
+
+function RequestForm({
+	searchquestion,
+	setSearchquestion,
+	answer,
+	setAnswer,
+	setShow,
+	setQuestionCreated,
+}) {
+	const { createQuestionSet, createQuestionLoading } = useCreateQuestionSet(
+		{ searchquestion, setShow, setQuestionCreated, answer },
+	);
+
+	return (
+		<div className={styles.container}>
+			<div className={styles.header}>
+				Request An Answer
+			</div>
+
+			<div className={styles.input_label}>
+				Name of the Question
+			</div>
+
+			<div className={styles.input_container}>
+				<Input
+					value={searchquestion}
+					size="md"
+					onChange={(value) => setSearchquestion(value)}
+				/>
+			</div>
+
+			<div className={styles.input_label}>
+				If you have an answer suggestion, enter below
+				{' '}
+				<span className={styles.span}>(Optional)</span>
+			</div>
+
+			<div className={styles.answer_container}>
+
+				<Textarea
+					value={answer}
+					onChange={(val) => setAnswer(val)}
+					size="md"
+					placeholder="Enter Answer"
+					width={400}
+					height={400}
+				/>
+
+			</div>
+
+			<div className={styles.button_container}>
+				<div style={{ paddingRight: '16px' }}>
+					<Button
+						size="md"
+						themeType="secondary"
+						onClick={() => setShow(false)}
+						disabled={createQuestionLoading}
+					>
+						Cancel
+					</Button>
+				</div>
+
+				<Button
+					size="md"
+					themeType="primary"
+					type="submit"
+					loading={createQuestionLoading}
+					onClick={createQuestionSet}
+				>
+					Submit
+				</Button>
+			</div>
+		</div>
+	);
+}
+
+export default RequestForm;
