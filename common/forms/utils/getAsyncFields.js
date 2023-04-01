9c37df7c66ab546dac9b09d593d8c55ef5402d80
simranjitsingh3720 <simranjit.singh@cogoport.com>
2023-04-01 13:55:02 +0530

    fixes

--- common/forms/utils/getAsyncFields.js
+++ common/forms/utils/getAsyncFields.js
@@ -211,6 +211,21 @@ function listVendors() {
 	};
 }
 
+function asyncListCogoEntity() {
+	return {
+		labelKey    : 'business_name',
+		valueKey    : 'entity_code',
+		endpoint    : 'list_cogo_entities',
+		initialCall : true,
+		params      : {
+			filters: {
+				status: 'active',
+			},
+			page_limit: 100,
+		},
+	};
+}
+
 export {
 	asyncFieldsLocations,
 	asyncFieldsLocations2,
@@ -228,4 +243,5 @@ export {
 	asyncFieldListRateChargeCodes,
 	asyncAllotBanks,
 	listVendors,
+	asyncListCogoEntity,
 };
