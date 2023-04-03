e0d7649243493d3e43635f5b184f1fdde30232a4
Ritvik98349 <124043414+Ritvik98349@users.noreply.github.com>
2023-04-03 18:27:01 +0530

    correction

--- packages/cogo-academy/page-components/AdminResults/components/TestResults/DifficultyAndTopicDistribution/index.js
+++ packages/cogo-academy/page-components/AdminResults/components/TestResults/DifficultyAndTopicDistribution/index.js
@@ -10,12 +10,15 @@ function DifficultyAndTopicDistribution({ data = {}, toggleState = false }) {
 
 	const getBarChartData = ({ chart_data = {} }) => (Object.keys(chart_data).map((key, index) => ({
 		label      : toggleState ? startCase(key) : startCase(key.split('_')[0]),
-		percentile : toFixed(data[key], 2),
+		percentile : toFixed(chart_data[key], 2),
 		color      : colorArray[index],
 	})));
 
-	const chart_data = toggleState
-		? getBarChartData({ chart_data: topic_wise_percent }) : getBarChartData({ chart_data: difficulty_wise_stats });
+	const topic_wise_data = getBarChartData({ chart_data: topic_wise_percent });
+	const difficulty_wise_data = getBarChartData({ chart_data: difficulty_wise_stats });
+
+	const chart_data = toggleState ? topic_wise_data : difficulty_wise_data;
+
 	return (
 		<div style={{ display: 'flex', marginRight: '12px' }}>
 			<BarChart chart_data={chart_data} yAxis="Correct Answer %" />
