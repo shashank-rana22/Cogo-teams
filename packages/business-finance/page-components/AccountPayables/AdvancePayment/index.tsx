eec8b9bdbc68d56c1a1853eec060d91f3c4b66e0
Jaiprakash kushwaha <jaiprakash.kushwaha@cogoport.com>
2023-04-04 19:27:07 +0530

    advance tab

--- packages/business-finance/page-components/AccountPayables/AdvancePayment/index.tsx
+++ packages/business-finance/page-components/AccountPayables/AdvancePayment/index.tsx
@@ -1,9 +1,25 @@
+import { Tabs, TabPanel } from '@cogoport/components';
+import { useState } from 'react';
+
 import styles from './styles.module.css';
 
 function AdvancePayment() {
+	const [activeTab, setActiveTab] = useState('pending');
 	return (
 		<div className={styles.container}>
-			<h3>Advance Payment</h3>
+			<Tabs
+				activeTab={activeTab}
+				themeType="tertiary"
+				onChange={setActiveTab}
+			>
+				<TabPanel name="pending" title="Pending" badge={3}>
+					<div>This is local search</div>
+				</TabPanel>
+
+				<TabPanel name="history" title="History" badge={5}>
+					<div>This is suggested</div>
+				</TabPanel>
+			</Tabs>
 		</div>
 	);
 }
