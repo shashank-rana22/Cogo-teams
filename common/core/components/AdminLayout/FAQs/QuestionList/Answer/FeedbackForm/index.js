243fa316bfb7661572449cc826da21f1b9ecf043
manoj-kumar-cogo <manoj.kumar@cogoport.com>
2023-04-04 11:59:49 +0530

    dislike form added

---
+++ common/core/components/AdminLayout/FAQs/QuestionList/Answer/FeedbackForm/index.js
@@ -0,0 +1,80 @@
+import { RTEditor } from '@cogoport/components';
+import { InputController, CheckboxController } from '@cogoport/forms';
+import React from 'react';
+
+import styles from './styles.module.css';
+
+function FeedbackForm({
+	answerData = {},
+	control,
+	answer,
+	setAnswer,
+	watch = () => {},
+}) {
+	const watchQuestionCheckbox = watch('question_checkbox');
+	const watchAnswerCheckbox = watch('answer_checkbox');
+
+	return (
+		<div className={styles.container}>
+			<section className={styles.section_container}>
+				<CheckboxController
+					control={control}
+					name="question_checkbox"
+					type="checkbox"
+					label="Question not satisfactory"
+					checked={watchQuestionCheckbox}
+					style={{ marginBottom: 12 }}
+				/>
+
+				{watchQuestionCheckbox ? (
+					<section className={styles.watch_checkbox}>
+						<h4>Rephrase the question (optional)</h4>
+
+						<InputController
+							control={control}
+							name="question"
+							type="text"
+							placeholder="Enter text here"
+							value={answerData?.question_abstract}
+						/>
+					</section>
+				) : null}
+			</section>
+
+			<section className={styles.section_container}>
+				<CheckboxController
+					control={control}
+					name="answer_checkbox"
+					type="checkbox"
+					label="Answer not satisfactory"
+					style={{ marginBottom: 12 }}
+					checked={watchAnswerCheckbox}
+				/>
+
+				{watchAnswerCheckbox ? (
+					<section>
+						<h4>Rephrase the answer (optional)</h4>
+
+						<RTEditor
+							value={answer}
+							onChange={setAnswer}
+						/>
+					</section>
+				) : null}
+			</section>
+
+			<div className={styles.remark}>
+				<div className={styles.aftercheckbox}>Remarks (optional)</div>
+
+				<InputController
+					control={control}
+					name="remark"
+					type="text"
+					placeholder="Enter remark here"
+				/>
+			</div>
+		</div>
+	);
+}
+
+export default FeedbackForm;
