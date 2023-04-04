3467f0c59938e1a51d732e8a685f87147a0f7bc1
AmaanAhmadCogo <124043454+AmaanAhmadCogo@users.noreply.github.com>
2023-04-04 15:20:56 +0530

    resolved nesting of components, moved ViewAllConfigurations parallel to ExpertiseConfigurations

--- packages/allocation/pages/allocation/kam-expertise/configurations/all-configurations/index.js
+++ packages/allocation/pages/allocation/kam-expertise/configurations/all-configurations/index.js
@@ -1,2 +1,2 @@
 // eslint-disable-next-line max-len, import/no-unresolved
-export { default } from '@cogoport/allocation/page-components/KamExpertise/components/ExpertiseConfigurations/ViewAllConfigurations';
+export { default } from '@cogoport/allocation/page-components/KamExpertise/components/ViewAllConfigurations';
