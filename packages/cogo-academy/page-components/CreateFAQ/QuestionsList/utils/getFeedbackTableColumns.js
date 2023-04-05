9e5cfb98f946f67a1dcf103e4b7b952daf12e6e9
Namanagarwal015 <124123426+Namanagarwal015@users.noreply.github.com>
2023-04-05 21:13:22 +0530

    FeedBack Column resolved

--- packages/cogo-academy/page-components/CreateFAQ/QuestionsList/utils/getFeedbackTableColumns.js
+++ packages/cogo-academy/page-components/CreateFAQ/QuestionsList/utils/getFeedbackTableColumns.js
@@ -1,87 +1,87 @@
-import { Pill, Button } from '@cogoport/components';
-import { IcMLiveChat } from '@cogoport/icons-react';
+// import { Pill, Button } from '@cogoport/components';
+// import { IcMLiveChat } from '@cogoport/icons-react';
 
 // import styles from '../styles.module.css';
 
-const tags = ['one', 'two', 'three', 'four'];
+// const tags = ['one', 'two', 'three', 'four'];
 
-const tagsToDisplay = tags.map((tag) => ({
-	label : tag,
-	size  : 'md',
-}));
+// const tagsToDisplay = tags.map((tag) => ({
+// 	label : tag,
+// 	size  : 'md',
+// }));
 
-const finalTagsToDisplay = tagsToDisplay.length > 3 ? [...tagsToDisplay.slice(0, 3), {
-	label : `+${tagsToDisplay.length - 3} more`,
-	size  : 'md',
-}] : tagsToDisplay;
+// const finalTagsToDisplay = tagsToDisplay.length > 3 ? [...tagsToDisplay.slice(0, 3), {
+// 	label : `+${tagsToDisplay.length - 3} more`,
+// 	size  : 'md',
+// }] : tagsToDisplay;
 
-const columns = ({
-	onClickEditButton = () => {},
-	onClickViewButton = () => {},
-}) => [
-	{
-		Header   : 'QUESTIONS',
-		accessor : () => (
-			<h3>
-				Questions
-			</h3>
-		),
-	},
-	{
-		Header   : 'TOPICS',
-		accessor : () => (
-			<div className={styles.pills}>
-				{(finalTagsToDisplay || []).map((item) => (
-					<Pill
-						className={styles.questions_tag}
-						key={item.label}
-						size="sm"
-						color="white"
-					>
-						{item.label}
-					</Pill>
-				))}
-			</div>
-		),
-	},
-	{
-		Header   : 'Feedbacks',
-		accessor : () => (
-			<div className={styles.feedback_count_space}>
-				<IcMLiveChat height={20} width={20} />
-				<b className={styles.feedback_count}>5</b>
-			</div>
-		),
-	},
-	{
-		Header   : 'ACTIONS',
-		accessor : (item) => (
-			<div className={styles.button_container}>
-				<Button
-					type="button"
-					themeType="secondary"
-					size="sm"
-					style={{ marginRight: 8 }}
-					onClick={() => onClickEditButton(item?.id)}
-				>
-					EDIT
-				</Button>
-				{/* {!['inactive', 'draft'].includes(activeList)
-					? ( */}
-				<Button
-					type="button"
-					themeType="primary"
-					size="sm"
-					style={{ marginRight: 8 }}
-					onClick={() => onClickViewButton(item?.id)}
-				>
-					VIEW
-				</Button>
-				{/* )
-					: null} */}
-			</div>
-		),
-	},
-];
+// const columns = ({
+// 	onClickEditButton = () => {},
+// 	onClickViewButton = () => {},
+// }) => [
+// 	{
+// 		Header   : 'QUESTIONS',
+// 		accessor : () => (
+// 			<h3>
+// 				Questions
+// 			</h3>
+// 		),
+// 	},
+// 	{
+// 		Header   : 'TOPICS',
+// 		accessor : () => (
+// 			<div className={styles.pills}>
+// 				{(finalTagsToDisplay || []).map((item) => (
+// 					<Pill
+// 						className={styles.questions_tag}
+// 						key={item.label}
+// 						size="sm"
+// 						color="white"
+// 					>
+// 						{item.label}
+// 					</Pill>
+// 				))}
+// 			</div>
+// 		),
+// 	},
+// 	{
+// 		Header   : 'Feedbacks',
+// 		accessor : () => (
+// 			<div className={styles.feedback_count_space}>
+// 				<IcMLiveChat height={20} width={20} />
+// 				<b className={styles.feedback_count}>5</b>
+// 			</div>
+// 		),
+// 	},
+// 	{
+// 		Header   : 'ACTIONS',
+// 		accessor : (item) => (
+// 			<div className={styles.button_container}>
+// 				<Button
+// 					type="button"
+// 					themeType="secondary"
+// 					size="sm"
+// 					style={{ marginRight: 8 }}
+// 					onClick={() => onClickEditButton(item?.id)}
+// 				>
+// 					EDIT
+// 				</Button>
+// 				{/* {!['inactive', 'draft'].includes(activeList)
+// 					? ( */}
+// 				<Button
+// 					type="button"
+// 					themeType="primary"
+// 					size="sm"
+// 					style={{ marginRight: 8 }}
+// 					onClick={() => onClickViewButton(item?.id)}
+// 				>
+// 					VIEW
+// 				</Button>
+// 				{/* )
+// 					: null} */}
+// 			</div>
+// 		),
+// 	},
+// ];
 
-export default columns;
+// export default columns;
