a810dc044d99d614b68ea17b3b37f6653934e9a3
piyushi-cogo <piyushi.garg@cogoport.com>
2023-04-05 17:36:49 +0530

    overview code fix for services and upselling

--- packages/shipment-fcl-freight/common/Overview/Services/ServiceDetails/Header/index.js
+++ packages/shipment-fcl-freight/common/Overview/Services/ServiceDetails/Header/index.js
@@ -1,50 +1,22 @@
 import { cl } from '@cogoport/components';
 import React from 'react';
 
-// import EditCancelService from './EditCancelService';
 import styles from './styles.module.css';
 
 function Header({
-	service_type = '',
-	id = '',
 	state = '',
 	heading = '',
-	supply_agent = {},
-	serviceData = [],
-	servicesList,
-	isSeller = false,
 	service_provider = {},
-	refetchServices = () => {},
-	refetchList = () => {},
-	shipmentData = {},
+
 }) {
 	return (
 		<div className={cl`${state} ${styles.container}`}>
-			<div className={styles.heading_wrap}>
-				<div>
-					<div className={cl`${styles.heading} ${state}`}>{heading}</div>
-					<div className={cl`${styles.sub_heading} ${state}`}>
-						{service_provider?.business_name}
-					</div>
-				</div>
-
-				{/* <EditCancelService
-					service_type={service_type}
-					id={id}
-					state={state}
-					isSeller={isSeller}
-					serviceData={serviceData}
-					servicesList={servicesList}
-					refetchServices={refetchServices}
-					refetchList={refetchList}
-					shipmentData={shipmentData}
-				/> */}
+			<div className={cl`${styles.heading} ${state}`}>{heading}</div>
+			<div className={cl`${styles.sub_heading} ${state}`}>
+				{service_provider?.business_name}
 			</div>
-			{/*
-			{supply_agent?.name ? (
-				<div className={cl`${styles.sub_heading} ${state}`}>{supply_agent?.name}</div>
-			) : null} */}
 		</div>
+
 	);
 }
 
