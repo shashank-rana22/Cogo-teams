2aeab1a51be010d4fee02835adcab5fa04eb1460
piyushi-cogo <piyushi.garg@cogoport.com>
2023-04-05 18:23:59 +0530

    fix

--- packages/shipment-fcl-freight/common/Overview/Services/configurations/get-configs.js
+++ packages/shipment-fcl-freight/common/Overview/Services/configurations/get-configs.js
@@ -5,26 +5,24 @@ import fcl_freight_local_service from './fcl-local.json';
 import ftl_freight from './ftl-freight.json';
 import haulage_freight from './haulage-freight.json';
 import ltl_freight from './ltl-freight.json';
-import rail_domestic_freight from './rail-domestic-freight.json';
 import trailer_freight from './trailer-freight.json';
 
 const getConfigs = (service_type) => {
 	const configs = {
-		'fcl-freight'                 : fcl_freight,
-		'fcl-customs'                 : fcl_customs,
-		'haulage-freight'             : haulage_freight,
-		'ftl-freight'                 : ftl_freight,
-		'trailer-freight'             : trailer_freight,
-		'ltl-freight'                 : ltl_freight,
-		fcl_freight_service           : fcl_freight,
-		fcl_customs_service           : fcl_customs,
-		haulage_freight_service       : haulage_freight,
-		trailer_freight_service       : haulage_freight,
-		ftl_freight_service           : ftl_freight,
-		ltl_freight_service           : ltl_freight,
+		'fcl-freight'           : fcl_freight,
+		'fcl-customs'           : fcl_customs,
+		'haulage-freight'       : haulage_freight,
+		'ftl-freight'           : ftl_freight,
+		'trailer-freight'       : trailer_freight,
+		'ltl-freight'           : ltl_freight,
+		fcl_freight_service     : fcl_freight,
+		fcl_customs_service     : fcl_customs,
+		haulage_freight_service : haulage_freight,
+		trailer_freight_service : haulage_freight,
+		ftl_freight_service     : ftl_freight,
+		ltl_freight_service     : ltl_freight,
 		fcl_freight_local_service,
-		fcl_cfs_service               : fcl_cfs,
-		rail_domestic_freight_service : rail_domestic_freight,
+		fcl_cfs_service         : fcl_cfs,
 	};
 
 	return configs[service_type] || fcl_freight;
