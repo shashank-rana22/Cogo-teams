48cf940de16e5ac39212c3f66b35b8e851afbb70
Aakash Dinkar <aakash.dinkar@cogoport.com>
2023-04-06 21:17:53 +0530

    controller handle with props

--- common/forms/page-components/Business/SelectCountryCode/index.js
+++ common/forms/page-components/Business/SelectCountryCode/index.js
@@ -5,10 +5,14 @@ import React from 'react';
 import countries from '../../../../../.data-store/constants/countries.json';
 
 function SelectCountryCode(props) {
+	const { mobile_country_code = true } = props;
 	
-	const formattedList = countries.map((code) => ({
+	const formattedList = countries.map((code) => mobile_country_code ? ({
 		value : code.mobile_country_code,
 		label : `${code.mobile_country_code}: ${code.name}`,
+	}) : ({
+		value: code.id,
+		label: code.name,
 	}));
 
 	return (
