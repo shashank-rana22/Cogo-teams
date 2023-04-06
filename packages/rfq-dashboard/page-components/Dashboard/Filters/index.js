68870f8b5114caef2a7ca6ee8b03a3dd056c1d65
amandeep2508 <amandeep.chauhan@cogoport.com>
2023-04-06 16:32:47 +0530

    fix

---
+++ packages/rfq-dashboard/page-components/Dashboard/Filters/index.js
@@ -0,0 +1,13 @@
+import styles from './styles.module.css';
+
+function Filters() {
+	return (
+		<div className={styles.container}>
+			<div className={styles.title}>
+				Filters
+			</div>
+		</div>
+	);
+}
+
+export default Filters;
