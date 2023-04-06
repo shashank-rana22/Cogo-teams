d41f2395432a88fa7efe1ff95ac7d76fb763b02e
ankit-rajpoot <ankit.rajpoot@cogoport.com>
2023-04-06 15:36:50 +0530

    fixes

--- common/globalization/constants/geo/index.js
+++ common/globalization/constants/geo/index.js
@@ -1,4 +1,5 @@
-/* eslint-disable valid-typeof */
+import { getCookie } from '@cogoport/utils';
+
 import GLOBAL_CONSTANTS from '../globals.json';
 
 import IN from './IN';
@@ -16,7 +17,8 @@ const getGeoConstants = () => {
 		return null;
 	}
 
-	const parent_entity_id = '6fd98605-9d5d-479d-9fac-cf905d292b88';
+	const parent_entity_id = getCookie('parent_entity_id');
+
 	return MAPPING[
 		parent_entity_id in MAPPING ? parent_entity_id : country_entity_ids.IN
 	];
