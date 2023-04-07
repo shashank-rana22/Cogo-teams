22326ac157ce177329b2b294ad7a70fe8d4a3789
Jaiprakash kushwaha <jaiprakash.kushwaha@cogoport.com>
2023-04-07 18:31:32 +0530

    breadcumb

--- packages/business-finance/page-components/AccountPayables/AdvancePayment/CreateNewPayRun/index.tsx
+++ packages/business-finance/page-components/AccountPayables/AdvancePayment/CreateNewPayRun/index.tsx
@@ -1,9 +1,18 @@
+import { Breadcrumb } from '@cogoport/components';
 import React from 'react';
 
 function CreateNewPayRun() {
 	return (
 		<div>
-			hii
+
+			<Breadcrumb>
+				<Breadcrumb.Item label="jai" />
+				<Breadcrumb.Item label="Page Name 2" />
+				<Breadcrumb.Item label="Page Name 3" />
+				<Breadcrumb.Item label="Page Name 4" />
+				<Breadcrumb.Item label="Page Name 5" />
+				<Breadcrumb.Item label="Page Name 6" />
+			</Breadcrumb>
 		</div>
 	);
 }
