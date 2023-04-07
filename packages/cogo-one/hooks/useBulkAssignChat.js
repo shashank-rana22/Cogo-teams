350635d964ffa40c509db23c0095e5c3fd496a12
Rahul Danampally <Danampally.Rahul@cogoport.com>
2023-04-07 15:03:43 +0530

    bulk assign payload

--- packages/cogo-one/hooks/useBulkAssignChat.js
+++ packages/cogo-one/hooks/useBulkAssignChat.js
@@ -11,36 +11,33 @@ function useBulkAssignChat({ setSelectedAutoAssign = () => {}, setAutoAssignChat
 	}, { manual: true, autoCancel: false });
 
 	const bulkAssignChat = async ({ selectedAutoAssign = {} }) => {
-		const chats = Object.values(selectedAutoAssign) || [];
+		const filteredChatsPayload = Object.keys(selectedAutoAssign || {})
+			.filter((key) => selectedAutoAssign[key]).map((key) => {
+				const eachChat = selectedAutoAssign[key] || {};
+				const {
+					channel_type = '',
+					id = '',
+					user_id = '',
+					mobile_no = '',
+					sender = '',
+					lead_user_id = '',
+					organization_id = '',
+				} = getActiveCardDetails(eachChat) || {};
+				return {
+					channel                 : channel_type,
+					channel_chat_id         : id,
+					user_id,
+					whatsapp_number_eformat : channel_type === 'whatsapp' ? mobile_no : undefined,
+					lead_user_id            : (!(user_id) && lead_user_id) ? lead_user_id : undefined,
+					organization_id,
+					sender,
 
-		const payload = chats.map((chat) => {
-			console.log('chat:', chat);
-
-			const {
-				channel_type = '',
-				id = '',
-				user_id = '',
-				mobile_no = '',
-				sender = '',
-				lead_user_id = '',
-				organization_id = '',
-			} = getActiveCardDetails(chat) || {};
-
-			return {
-				channel                 : channel_type,
-				channel_chat_id         : id,
-				user_id,
-				whatsapp_number_eformat : channel_type === 'whatsapp' ? mobile_no : undefined,
-				lead_user_id            : (!(user_id) && lead_user_id) ? lead_user_id : undefined,
-				organization_id,
-				sender,
-
-			};
-		});
+				};
+			});
 
 		try {
 			await trigger({
-				data: { chats: payload },
+				data: { chats: filteredChatsPayload },
 			});
 
 			Toast.success('Successfully Auto Assigned');
