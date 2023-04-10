3cc506d10c34e7d832521ea8259cff07d3c7644e
cogomanish <manish.kumar@cogoport.com>
2023-04-10 22:49:20 +0530

    fixes

--- packages/business-finance/page-components/commons/StyledTable/EmptyState/index.tsx
+++ packages/business-finance/page-components/commons/StyledTable/EmptyState/index.tsx
@@ -3,8 +3,9 @@ import React from 'react';
 import styles from './styles.module.css';
 
 const imageData = {
-	NoDataImage: 'https://cdn.cogoport.io/cms-prod/cogo_admin/vault/original/no-incident-data.svg',
-	FinanceDashboard: 'https://cogoport-production.sgp1.digitaloceanspaces.com/970a0d72c48e76c4da52963dfc852230/no-data.svg',
+	NoDataImage      : 'https://cdn.cogoport.io/cms-prod/cogo_admin/vault/original/no-incident-data.svg',
+	FinanceDashboard : 'https://cogoport-production.sgp1.digitaloceanspaces.com/'
+	+ '970a0d72c48e76c4da52963dfc852230/no-data.svg',
 };
 
 function EmptyState({ imageFind }) {
