238e6e7e29779e68dd9cae252153c754ac701f52
ankit-rajpoot <ankit.rajpoot@cogoport.com>
2023-04-10 15:03:29 +0530

    date fix

--- common/shipment-mails/page-components/ListRpa/EmailCard/index.js
+++ common/shipment-mails/page-components/ListRpa/EmailCard/index.js
@@ -11,12 +11,12 @@ import styles from './styles.module.css';
 
 function EmailCard({ data, onClick, activeMail }) {
 	const yesterday = subtractDays(new Date(), 1);
-	let received_time = addHours(data.received_time, 5);
+	let received_time = addHours(new Date(data?.received_time), 5);
 	received_time = addMinutes(received_time, 30);
 	const displayDate =	new Date(received_time) > yesterday
 		? formatDistanceToNow(received_time)
 		: formatDate({
-			date       : data.received_time,
+			date       : received_time,
 			dateFormat : GLOBAL_CONSTANTS.formats.date['eee, dd MMM, yyyy'],
 			formatType : 'date',
 		});
