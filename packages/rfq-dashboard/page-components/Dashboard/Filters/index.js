14eb10250e646f7531d046f3a1568b2206f4b8af
amandeep2508 <amandeep.chauhan@cogoport.com>
2023-04-10 18:30:20 +0530

    fix

--- packages/rfq-dashboard/page-components/Dashboard/Filters/index.js
+++ packages/rfq-dashboard/page-components/Dashboard/Filters/index.js
@@ -1,11 +1,43 @@
+import { useForm } from '@cogoport/forms';
+
+import controls from '../../../configurations/filter-controls';
+import { getFieldController } from '../../../utils/getFieldController';
+
 import styles from './styles.module.css';
 
 function Filters() {
+	const formProps = useForm();
+	const { control } = formProps;
+
 	return (
 		<div className={styles.container}>
 			<div className={styles.title}>
 				Filters
 			</div>
+
+			<form className={styles.form}>
+				{controls.map((controlItem) => {
+					const el = { ...controlItem };
+
+					const Element = getFieldController(el.type);
+
+					if (!Element) return null;
+
+					return (
+						<div className={styles.control_container}>
+							{el.label && <div className={styles.label}>{el.label}</div>}
+							<Element
+								{...el}
+								size="sm"
+								key={el.name}
+								control={control}
+								id={`${el.name}_input`}
+							/>
+						</div>
+					);
+				})}
+			</form>
+
 		</div>
 	);
 }
