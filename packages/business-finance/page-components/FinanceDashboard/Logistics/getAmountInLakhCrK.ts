f205f67ecfb26527f01b3d90494d32004b952fa5
mona singh <mona.singh@cogoport.com>
2023-04-10 12:14:14 +0530

    changes

--- packages/business-finance/page-components/FinanceDashboard/Logistics/getAmountInLakhCrK.ts
+++ packages/business-finance/page-components/FinanceDashboard/Logistics/getAmountInLakhCrK.ts
@@ -11,10 +11,12 @@ const getAmountInLakhCrK = (value:number, currency:string) => {
 		formatedAmount = `${(val / 1000).toFixed(2)} K`;
 	} else if (val === 0) {
 		formatedAmount = `${(val).toFixed(2)}`;
-	} else if (val <= (100000 * -1)) {
-		formatedAmount = `${(val / 100000).toFixed(2)}Lac`;
 	} else if (val <= (10000000 * -1)) {
 		formatedAmount = `${(val / 10000000).toFixed(2)} Cr`;
+	} else if (val <= (100000 * -1)) {
+		formatedAmount = `${(val / 100000).toFixed(2)}Lac`;
+	} else if (val <= (1000 * -1)) {
+		formatedAmount = `${(val / 1000).toFixed(2)} K`;
 	}
 
 	return `${currency || 'INR'} ${formatedAmount}`;
