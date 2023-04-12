0d141a83c0f9cd79bd0e6a8b9e3036aff50e814b
Ankit Kaushal <ankit.kaushal@cogoport.com>
2023-04-12 11:51:39 +0530

    generate mainfest doc

---
+++ packages/ground-ops/page-components/GenerateMAWB/GenerateManifestDoc/stylesTHC.ts
@@ -0,0 +1,64 @@
+export const stylesTHC = `
+.flex_column {
+    display: flex;
+    flex-direction: column;
+}
+
+.flex_row {
+    display: flex;
+}
+
+.manifest_heading {
+    border: 0.5px solid #333;
+    margin-left: auto;
+    margin-right: auto;
+    padding: 2px 8px;
+}
+
+.shipper {
+    flex:0.5;
+    font-weight: 600;
+    font-size: 13px;
+    margin-top: 40px;
+    line-height: 1.6;
+}
+
+.shipper_head {
+    text-decoration: underline;
+}
+
+.consignee {
+    flex:0.5;
+    margin-top: 40px;
+}
+
+.font_style {
+    font-weight: 600;
+    font-size: 12px;
+    line-height: 1.6;
+}
+
+.consignee_head {
+    text-decoration: underline;
+}
+
+.extra_space {
+    flex: 0.5;
+}
+
+table {
+    border-collapse: collapse;
+    margin-top: 40px;
+    width: 100%;
+}
+
+td {
+    border:1px solid #333;
+    padding: 4px 12px;
+}
+
+th {
+    border:1px solid #333;
+    padding: 4px 12px;
+}
+`;
--- packages/ground-ops/page-components/GenerateMAWB/index.tsx
+++ packages/ground-ops/page-components/GenerateMAWB/index.tsx
@@ -8,6 +8,7 @@ import useGetHawbList from '../Air/hooks/useGetHawbList';
 
 import FormContainer from './FormContainer';
 import GenerateHeader from './GenerateHeader';
+import GenerateManifestDoc from './GenerateManifestDoc';
 import GenerateMawbDoc from './GenerateMawbDoc';
 import { iataCodeMapping } from './Helpers/configurations/iataCodeMapping';
 import mawbControls from './Helpers/configurations/mawbControls';
@@ -73,6 +74,8 @@ function GenerateMAWB({
 
 	const [activeHawb, setActiveHawb] = useState(hawbDetails[0]);
 	const [activeKey, setActiveKey] = useState('basic');
+
+	const [triggerManifest, setTriggerManifest] = useState(false);
 
 	const fields = mawbControls(disableClass);
 
@@ -299,6 +302,7 @@ function GenerateMAWB({
 
 	return (
 		<div className={styles.container}>
+			<button onClick={() => setTriggerManifest(true)}>manifest</button>
 			{loading && <Loader themeType="primary" className={styles.loader} />}
 			{!viewDoc && (
 				<>
@@ -369,6 +373,24 @@ function GenerateMAWB({
 					</Modal>
 				)}
 			</div>
+
+			{triggerManifest && (
+				<Modal
+					show={triggerManifest}
+					onClose={() => { setTriggerManifest(false); }}
+					size="lg"
+					className={styles.modal_container}
+				>
+					<Modal.Body>
+						<GenerateManifestDoc
+							taskItem={taskItem}
+							formData={formData}
+							setTriggerManifest={setTriggerManifest}
+						/>
+					</Modal.Body>
+
+				</Modal>
+			)}
 		</div>
 	);
 }
