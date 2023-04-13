47994fff194d79fab17278ce88f62f483a3bef1a
Rella Panduvardhan Reddy <rella.reddy@cogoport.com>
2023-04-13 14:25:22 +0530

    password fix

--- packages/my-profile/page-components/MyProfile/PasswordValidator/index.js
+++ packages/my-profile/page-components/MyProfile/PasswordValidator/index.js
@@ -2,30 +2,43 @@ import { isEmpty } from '@cogoport/utils';
 
 import styles from './styles.module.css';
 
+function range(start, stop) {
+	const startCharCode = start.charCodeAt(0);
+	const endCharCode = stop.charCodeAt(0);
+
+	const result = [];
+
+	for (let charCode = startCharCode; charCode <= endCharCode; charCode += 1) {
+		result.push(String.fromCharCode(charCode));
+	}
+
+	return result;
+}
+
 const VALID_PASSWORD_MAPPINGS = {
 	lowercase: {
-		pattern : /[a-z]/gm,
-		message : 'at least one lowercase character.',
+		characters : range('a', 'z'),
+		message    : 'at least one lowercase character.',
 	},
 	uppercase: {
-		pattern : /[A-Z]/gm,
-		message : 'at least one uppercase character.',
+		characters : range('A', 'Z'),
+		message    : 'at least one uppercase character.',
 	},
 	digit: {
-		pattern : /[0-9]/gm,
-		message : 'at least one digit.',
+		characters : range('0', '9'),
+		message    : 'at least one digit.',
 	},
 	special: {
-		pattern : /[!@#$%^&*]/gm,
-		message : 'at least one special character.',
+		characters : '!@#$%^&*'.split(''),
+		message    : 'at least one special character.',
 	},
 	minLength: {
-		pattern : /[a-zA-Z0-9!@#$%^&*]{8,}/gm,
+		length  : 8,
 		message : 'minimum 8 characters.',
 	},
 };
 
-function PasswordValidator({ errorMessage = '' }) {
+function PasswordValidator({ password = '', errorMessage = '' }) {
 	return (
 		<div>
 			<div className={styles.error_message}>
@@ -36,33 +49,33 @@ function PasswordValidator({ errorMessage = '' }) {
 
 				<div className="list">
 					{Object.entries(VALID_PASSWORD_MAPPINGS)?.map(([key, value]) => {
-						const { message = '' } = value;
+						const { message = '', length = 0, characters = [] } = value;
 
 						// const regex = new RegExp(pattern);
 
 						// const isValid = regex.test(password);
 
+						let isValid = false;
+						if (key === 'minLength') {
+							isValid = password.length >= length;
+						} else {
+							isValid = password.split('').some((char) => characters.includes(char));
+						}
+
 						return (
 							<div className={`item item--${key}}`} style={{ display: 'flex', alignItems: 'center' }}>
-								{/* <div className={`icon icon--${isValid ? 'tick' : 'dot'}`}>
-								{isValid ? (
-									<img
-										alt="tick"
-										src="https://cdn.cogoport.io/cms-prod/cogo_admin/vault/original/tick.svg"
-									/>
-								) : (
-									<img
-										alt="dot"
-										src="https://cdn.cogoport.io/cms-prod/cogo_admin/vault/original/dot.svg"
-									/>
-								)}
-							</div> */}
-
-								<div className="icon icon--dot">
-									<img
-										alt="dot"
-										src="https://cdn.cogoport.io/cms-prod/cogo_admin/vault/original/dot.svg"
-									/>
+								<div className={`icon icon--${isValid ? 'tick' : 'dot'}`}>
+									{isValid ? (
+										<img
+											alt="tick"
+											src="https://cdn.cogoport.io/cms-prod/cogo_admin/vault/original/tick.svg"
+										/>
+									) : (
+										<img
+											alt="dot"
+											src="https://cdn.cogoport.io/cms-prod/cogo_admin/vault/original/dot.svg"
+										/>
+									)}
 								</div>
 
 								<div className="message">{message}</div>
