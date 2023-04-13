c39b85dd5098bf97ba5ffcdf7fc42ba0c352eb93
Aakash Dinkar <aakash.dinkar@cogoport.com>
2023-04-13 12:52:44 +0530

    pathnam fix booking -desk

--- packages/booking-desk/hooks/useCallApi.js
+++ packages/booking-desk/hooks/useCallApi.js
@@ -1,10 +1,14 @@
 import { useEffect, useRef } from 'react';
 
-export default function useCallApi({ listShipments, filters, authParams, activeTab, selected_agent_id }) {
-	const debounceQuery = useRef({ q: filters.q });
+export default function useCallApi({ listShipments, filters, authParams, activeTab, selected_agent_id, pathname }) {
+	const debounceQuery = useRef({ q: filters.q, pathname });
 
 	useEffect(() => {
 		const [, scope, view_type] = (authParams || '').split(':');
+
+		if (pathname !== debounceQuery.current.pathname) {
+			return;
+		}
 
 		if (debounceQuery.current.q !== filters.q) {
 			clearTimeout(debounceQuery.current.timerId);
@@ -19,5 +23,5 @@ export default function useCallApi({ listShipments, filters, authParams, activeT
 			'booking_desk_stored_values',
 			JSON.stringify({ filters, activeTab, scopeFilters: { scope, view_type, selected_agent_id } }),
 		);
-	}, [listShipments, activeTab, filters, authParams, selected_agent_id]);
+	}, [listShipments, activeTab, filters, authParams, selected_agent_id, pathname]);
 }
