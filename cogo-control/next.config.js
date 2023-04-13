f71f8a63cb1bdb6f7239ce606c269719740f3b3a
pranjal-somkuwar <pranjal.somkuwar@cogoport.com>
2023-04-13 13:35:11 +0530

    final Changes

--- cogo-control/next.config.js
+++ cogo-control/next.config.js
@@ -58,6 +58,7 @@ module.exports = withBundleAnalyzer({
 				net           : false,
 				tls           : false,
 				request       : false,
+				encoding   	  : false,
 			};
 		} else {
 			newConfig.resolve.fallback = { ...newConfig.resolve.fallback };
