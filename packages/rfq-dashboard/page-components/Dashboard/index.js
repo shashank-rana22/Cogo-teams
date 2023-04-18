1bd7806fdf878e4229f32a013cd662d7529d34ec
amandeep2508 <amandeep.chauhan@cogoport.com>
2023-04-18 15:47:31 +0530

    fix

--- packages/rfq-dashboard/page-components/Dashboard/index.js
+++ packages/rfq-dashboard/page-components/Dashboard/index.js
@@ -1,16 +1,30 @@
+import { useForm } from '@cogoport/forms';
+import { useState } from 'react';
+
 import Content from './Content';
 import Filters from './Filters';
 import styles from './styles.module.css';
 
 function Dashboard() {
+	const [activeTab, setActiveTab] = useState('local_rates');
+
+	const formProps = useForm();
+
+	const { watch } = formProps;
+
+	const watchFilters = watch();
+
+	const [filterData, setFilterData] = useState({});
+
+	console.log('filterData: ', filterData);
+
 	return (
 		<div>
-			<div className={styles.title}>
-				RFQ Dashboard
-			</div>
+			<div className={styles.title}>RFQ Dashboard</div>
+
 			<div className={styles.container}>
-				<Filters />
-				<Content />
+				<Filters formProps={formProps} />
+				<Content activeTab={activeTab} setActiveTab={setActiveTab} />
 			</div>
 		</div>
 	);
