ed37f061c3580bca2355c3f08f6c16bdc23c5d84
amandeep2508 <amandeep.chauhan@cogoport.com>
2023-04-18 15:48:08 +0530

    added list

---
+++ packages/rfq-dashboard/page-components/Dashboard/Content/List/index.js
@@ -0,0 +1,12 @@
+import Filter from './Filter';
+import styles from './styles.module.css';
+
+function List() {
+	return (
+		<div className={styles.container}>
+			<Filter />
+		</div>
+	);
+}
+
+export default List;
