f870a1af9538146f7b7a247c7a77b0945addc5fd
amandeep2508 <amandeep.chauhan@cogoport.com>
2023-04-18 15:47:57 +0530

    fix

--- packages/rfq-dashboard/page-components/Dashboard/Content/index.js
+++ packages/rfq-dashboard/page-components/Dashboard/Content/index.js
@@ -1,11 +1,24 @@
+import { Tabs, TabPanel } from '@cogoport/components';
+
+import List from './List';
 import styles from './styles.module.css';
 
-function Content() {
+function Content({ activeTab, setActiveTab }) {
 	return (
 		<div className={styles.container}>
-			<div className={styles.title}>
-				Content
-			</div>
+			<Tabs
+				activeTab={activeTab}
+				themeType="primary"
+				onChange={setActiveTab}
+			>
+				<TabPanel name="local_rates" title="Local Rates">
+					<List />
+				</TabPanel>
+
+				<TabPanel name="freight_bookings" title="Freight Bookings">
+					<div>bookings</div>
+				</TabPanel>
+			</Tabs>
 
 		</div>
 	);
