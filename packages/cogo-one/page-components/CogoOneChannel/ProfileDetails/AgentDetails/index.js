a1c48bacd053de140ed6fa8a30a38c00a5d3f148
Sandeep Nalabolu <sandeep.nalabolu@cogoport.com>
2023-04-18 17:00:25 +0530

    update

--- packages/cogo-one/page-components/CogoOneChannel/ProfileDetails/AgentDetails/index.js
+++ packages/cogo-one/page-components/CogoOneChannel/ProfileDetails/AgentDetails/index.js
@@ -37,7 +37,6 @@ function AgentDetails({
 		mobile_no,
 		organization_id,
 		sender,
-		channel_type,
 	} = formattedMessageData || {};
 
 	const [showAddNumber, setShowAddNumber] = useState(false);
