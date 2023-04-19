abbc6a62a593c2a66e6034b9a5505f3457547996
amandeep2508 <amandeep.chauhan@cogoport.com>
2023-04-19 16:51:17 +0530

    chekbox

--- packages/rfq-dashboard/page-components/Dashboard/Content/RfqDetails/index.js
+++ packages/rfq-dashboard/page-components/Dashboard/Content/RfqDetails/index.js
@@ -1,12 +1,41 @@
+import { useState, useEffect } from 'react';
+
 import Filter from './Filter';
 import List from './List';
 import styles from './styles.module.css';
 
 function RfqDetails(props) {
+	const [checkedItems, setCheckedItems] = useState([]);
+	const [selectAll, setSelectAll] = useState(false);
+
+	const data = [
+		{ id: '1', name: 'apple' },
+		{ id: '2', name: 'ball' },
+		{ id: '3', name: 'cat' },
+		{ id: '4', name: 'dog' },
+	];
+
+	useEffect(() => {
+		setSelectAll(checkedItems.length === data.length);
+	}, [checkedItems.length, data.length]);
+
 	return (
 		<div className={styles.container}>
-			<Filter {...props} />
-			<List />
+			<Filter
+				{...props}
+				data={data}
+				selectAll={selectAll}
+				checkedItems={checkedItems}
+				setSelectAll={setSelectAll}
+				setCheckedItems={setCheckedItems}
+			/>
+
+			<List
+				data={data}
+				checkedItems={checkedItems}
+				setCheckedItems={setCheckedItems}
+				setSelectAll={setSelectAll}
+			/>
 		</div>
 	);
 }
