aa5f2088cea43da3ad95fd98c67576ee1631beac
amandeep2508 <amandeep.chauhan@cogoport.com>
2023-04-19 16:51:44 +0530

    data map

--- packages/rfq-dashboard/page-components/Dashboard/Content/RfqDetails/List/index.js
+++ packages/rfq-dashboard/page-components/Dashboard/Content/RfqDetails/List/index.js
@@ -1,10 +1,30 @@
 import Card from './Card';
 import styles from './styles.module.css';
 
-function List() {
+function List({ checkedItems, setCheckedItems, setSelectAll, data }) {
+	const handleCheck = (event) => {
+		const { value, checked } = event.target;
+
+		if (checked) {
+			setCheckedItems((prevState) => [
+				...prevState,
+				data.find((item) => item.id === value),
+			]);
+		} else {
+			setCheckedItems((prevState) => prevState.filter((item) => item.id !== value));
+			setSelectAll(false);
+		}
+	};
+
 	return (
 		<div className={styles.container}>
-			{[...Array(4).map((item) => <Card />)]}
+			{data.map((item) => (
+				<Card
+					item={item}
+					handleCheck={handleCheck}
+					checkedItems={checkedItems}
+				/>
+			))}
 		</div>
 	);
 }
