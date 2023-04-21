559989ae924f926293ef4c1db58b84624a6187d5
ankit-rajpoot <ankit.rajpoot@cogoport.com>
2023-04-21 18:56:06 +0530

    add trade document

---
+++ common/ocean-modules/common/TradeDocTemplate/CargoArrivalNotice/Frontside/ExtraHardCodeData.js
@@ -0,0 +1,53 @@
+export function ExtraHardCodeData() {
+	return (
+		<>
+			<div style={{ marginTop: '40px', fontSize: '11px' }}>
+				Kindly submit the ORIGINAL BILL OF LADING with endorsements to release
+				the shipment
+			</div>
+			<div style={{ fontSize: '11px' }}>
+				the above-mentioned vessel is expected to arrive on or about: 26-JUN-21
+				at CHENNAI
+			</div>
+			<div style={{ fontSize: '11px', marginTop: '7px' }}>
+				Kindly arrange to present the original Bill of Lading & Freight
+				Certificate (if it is Collect Shipment) and obtain a delivery order
+				against payment of necessary charges by DEMAND DRAFT. Please note we
+				will not hold the FCL Container(s) in the Terminal beyond 3 days or as
+				specified by the Terminal and or Carrier from the day of vessel arrival.
+				We will move the container to off-Dock CFS after completion of free-days
+				under your cost and risk. Looking forward to your speedy clearance of
+				cargo.
+			</div>
+			<div style={{ fontSize: '11px', marginTop: '8px' }}>
+				PENALTY SLABS FOR LATE COLLECTION OF DELIVERY ORDER
+			</div>
+			<div style={{ fontSize: '11px' }}>
+				Upto14 days : No fine (starting from the devanning date).
+			</div>
+			<div style={{ fontSize: '11px' }}>Between 15-30 days: Rs.1500/BL</div>
+			<div style={{ fontSize: '11px' }}>Between 31-60 days: Rs.2500/BL</div>
+			<div style={{ fontSize: '11px' }}>Above 61 days: Rs.6000/BL</div>
+
+			<div style={{ fontSize: '11px', marginTop: '40px' }}>
+				For COGO FREIGHT PVT LTD
+			</div>
+
+			<div style={{ fontSize: '11px', marginTop: '30px' }}>
+				A/C Name: COGO FREIGHT PRIVATE LIMITED
+			</div>
+			<div style={{ fontSize: '11px' }}>
+				Bank Name: RBL BANK A/C No: 409000876343
+			</div>
+			<div style={{ fontSize: '11px' }}>
+				Address: ONE INDIA BULLS CENTRE,3RD FLOOR, MUMBAI -400013
+			</div>
+			<div style={{ fontSize: '11px' }}>IFSC Code – RATN0000088</div>
+			<div style={{ fontSize: '11px', marginTop: '30px' }}>
+				Please note that cargoes remaining undelivered for 30 days from the date
+				of arrival will be listed for auction and will be duly auctioned upon
+				completion of Customs & CFS Formalities
+			</div>
+		</>
+	);
+}
---
+++ common/ocean-modules/common/TradeDocTemplate/CargoArrivalNotice/Frontside/HeaderPart.js
@@ -0,0 +1,24 @@
+export function HeaderPart() {
+	return (
+		<>
+			<div className="company-details">
+				COGO FREIGHT PRIVATE LIMITED (COGOPORT)
+			</div>
+			<div style={{ textAlign: 'center', marginTop: '2px' }}>
+				Door no. T-6 Pinnacle Business Park Mahakali Caves Road
+			</div>
+			<div style={{ textAlign: 'center', marginTop: '2px' }}>
+				Shanti Nagar Andheri East, MUMBAI - 400093 India.
+			</div>
+			<div style={{ textAlign: 'center', marginTop: '2px' }}>
+				GST No :27AAGCC4470P1Z5
+			</div>
+			<div style={{ textAlign: 'center', marginTop: '2px' }}>
+				CIN No: U60222MH2016PTC28059
+			</div>
+			<div style={{ textAlign: 'center', marginTop: '25px', fontSize: '16px' }}>
+				CARGO ARRIVAL NOTICE
+			</div>
+		</>
+	);
+}
