cabec0dbcdf9972dd66e968ffb601dd17a56dbeb
Namanagarwal015 <124123426+Namanagarwal015@users.noreply.github.com>
2023-04-24 12:21:45 +0530

    getQuestion fix

--- packages/cogo-academy/page-components/ControlCenter/Question/hooks/useGetQuestion.js
+++ packages/cogo-academy/page-components/ControlCenter/Question/hooks/useGetQuestion.js
@@ -1,12 +1,20 @@
 import { useRequest } from '@cogoport/request';
 import { useSelector } from '@cogoport/store';
+import { isEmpty } from '@cogoport/utils';
 import { useCallback } from 'react';
 
 function useGetQuestion() {
-	const { general } = useSelector((state) => state);
+	const { general = {}, profile = {} } = useSelector((state) => state);
 	const { query = {} } = general;
 	const { id = '' } = query || {};
 	const { mode = '' } = query || {};
+	const { auth_role_data = [], partner = {} } = profile;
+	const { role_functions = {}, role_sub_functions = {} } = auth_role_data || {};
+
+	const { scope = '' } = general;
+	const { country_id = '' } = partner;
+	const roleFunction = !isEmpty(role_functions) ? role_functions : undefined;
+	const roleSubFunction = !isEmpty(role_sub_functions) ? role_sub_functions : undefined;
 
 	const [{ data, loading }, trigger] = useRequest({
 		method : 'get',
@@ -19,13 +27,21 @@ function useGetQuestion() {
 
 			try {
 				await trigger({
-					params: { id, is_admin_view: true },
+					params: {
+						auth_function     : scope === 'partner' ? roleFunction : undefined,
+						auth_sub_function : scope === 'partner' ? roleSubFunction : undefined,
+						country_id,
+						cogo_entity_id    : partner?.id || undefined,
+						persona           : scope === 'partner' ? 'admin_user' : 'importer_exporter',
+						id,
+						is_admin_view     : true,
+					},
 				});
 			} catch (err) {
 				console.log(err);
 			}
 		},
-		[id, trigger],
+		[country_id, id, partner?.id, roleFunction, roleSubFunction, scope, trigger],
 	);
 
 	return { fetchQuestion, query, data, loading, mode };
