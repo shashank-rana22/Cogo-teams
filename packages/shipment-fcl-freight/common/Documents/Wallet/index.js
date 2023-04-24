07e53bb7599b33f36fc72fb11a1ca30cec30aa9c
santkumar1799 <sant.kumar@cogoport.com>
2023-04-24 11:39:57 +0530

    fixes

--- packages/shipment-fcl-freight/common/Documents/Wallet/index.js
+++ packages/shipment-fcl-freight/common/Documents/Wallet/index.js
@@ -6,7 +6,7 @@ import styles from './styles.module.css';
 import TradeDocuments from './TradeDocuments';
 
 function Wallet({
-	showWalletDocs = () => {},
+	showWalletDocs,
 	forModal = false,
 	activeWallet = '',
 	searchDocsVal,
