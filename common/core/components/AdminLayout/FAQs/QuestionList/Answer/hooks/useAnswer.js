4306fc64efc74f3eaced56d6b844d2fc7dc57e6a
Namanagarwal015 <124123426+Namanagarwal015@users.noreply.github.com>
2023-04-24 12:16:56 +0530

    useAnswer changes

--- common/core/components/AdminLayout/FAQs/QuestionList/Answer/hooks/useAnswer.js
+++ common/core/components/AdminLayout/FAQs/QuestionList/Answer/hooks/useAnswer.js
@@ -1,20 +1,40 @@
 import { Toast } from '@cogoport/components';
 import getApiErrorString from '@cogoport/forms/utils/getApiError';
 import { useRequest } from '@cogoport/request';
+import { useSelector } from '@cogoport/store';
+import { isEmpty } from '@cogoport/utils';
 import { useCallback, useMemo, useEffect } from 'react';
 
 const useAnswer = ({ question, setIsLiked, FEEDBACK_MAPPING_ISLIKED }) => {
+	const { general = {}, profile = {} } = useSelector((state) => state);
+
+	const { auth_role_data = [], partner = {}, platform = '' } = profile;
+	const { role_functions = {}, role_sub_functions = {} } = auth_role_data || {};
+
+	const { scope = '' } = general;
+	const { country_id = '', id = '' } = partner;
+	const roleFunction = !isEmpty(role_functions) ? role_functions : undefined;
+	const roleSubFunction = !isEmpty(role_sub_functions) ? role_sub_functions : undefined;
 	const params = useMemo(
 		() => ({
+			auth_function     : scope === 'partner' ? roleFunction : undefined,
+			auth_sub_function : scope === 'partner' ? roleSubFunction : undefined,
+			platform,
+			country_id,
+			cogo_entity_id    : id,
+			persona           : scope === 'partner' ? 'admin_user' : 'importer_exporter',
+
 			id: question?.id,
+
 		}),
-		[question?.id],
+		[country_id, id, platform, question?.id, roleFunction, roleSubFunction, scope],
 	);
 
 	const [{ data, loading }, trigger] = useRequest({
 		url    : '/get_question',
 		method : 'get',
-	}, { manual: true });
+		params,
+	}, { manual: false });
 
 	const fetch = useCallback(async () => {
 		try {
