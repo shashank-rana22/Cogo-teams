cd07bc18cee75c8d27a57efbced9ac6967f6a36e
Rahul Kumar <rahulk@cogoport.com>
2023-04-24 19:53:00 +0530

    stylelint fix

--- packages/shipment-fcl-freight/common/Overview/Services/ServiceDetails/index.js
+++ packages/shipment-fcl-freight/common/Overview/Services/ServiceDetails/index.js
@@ -1,14 +1,10 @@
-import { cl } from '@cogoport/components';
 import React from 'react';
 
 import Header from './Header';
-import styles from './styles.module.css';
 
 function ServiceDetails({ servicesData = [] }) {
 	const addedServiceComponent = (
-		<div className={cl`${styles.container}`}>
-			<Header serviceData={servicesData} />
-		</div>
+		<Header serviceData={servicesData} />
 	);
 
 	return addedServiceComponent;
