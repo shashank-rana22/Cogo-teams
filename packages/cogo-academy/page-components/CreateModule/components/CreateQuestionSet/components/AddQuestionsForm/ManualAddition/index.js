b3c083b20931737ad8298656960addc51682af26
rupesh-mutyala <rupesh.mutyala@cogoport.com>
2023-04-25 17:14:51 +0530

    fixes

--- packages/cogo-academy/page-components/CreateModule/components/CreateQuestionSet/components/AddQuestionsForm/ManualAddition/index.js
+++ packages/cogo-academy/page-components/CreateModule/components/CreateQuestionSet/components/AddQuestionsForm/ManualAddition/index.js
@@ -26,8 +26,7 @@ function ManualAddition({
 					className={styles.breadcrumb_item}
 				/>
 				<Breadcrumb.Item
-					style={{ color: '#221F20' }}
-					className={styles.breadcrumb_item}
+					className={styles.breadcrumb_inner_item}
 					label="Manual Addition"
 				/>
 			</Breadcrumb>
