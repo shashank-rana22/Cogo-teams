77e3379191d0265068308ab4b361b098e527081c
Rajatuba-Cogoport <rajat.uba@cogoport.com>
2023-04-27 15:00:56 +0530

    changes

---
+++ packages/booking-desk/commons/Stepper/Child/index.js
@@ -0,0 +1,18 @@
+import { cl } from '@cogoport/components';
+
+import styles from './styles.module.css';
+
+function Step({ item = {}, value = '', onChange = () => {} }) {
+	const isActive = item?.value === value;
+	return (
+		<div
+			className={cl`${styles.container} ${isActive ? styles.active : styles.remaining} `}
+			role="button"
+			tabIndex={0}
+			onClick={() => onChange(item?.value)}
+		>
+			<div className={cl`${styles.text} ${isActive ? styles.active_text : ''}`}>{item?.label || ''}</div>
+		</div>
+	);
+}
+export default Step;
