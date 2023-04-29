a1dbbeaac7e9fe5a4808093b4308b95e3e999792
Harshitcogo <harshit.soni@cogoport.com>
2023-04-29 18:49:37 +0530

    fixes

--- packages/allocation/page-components/AccountScoring/components/AccountLeaderboard/ScoreDistributionGraph/index.js
+++ packages/allocation/page-components/AccountScoring/components/AccountLeaderboard/ScoreDistributionGraph/index.js
@@ -61,8 +61,9 @@ function ScoreDistributionGraph(props) {
 					legend         : 'Accounts',
 					legendPosition : 'middle',
 					legendOffset   : -80,
+					tickValues     : 1,
 				}}
-				enableGridY
+				enableGridX={false}
 				enableLabel={false}
 			/>
 		</div>
