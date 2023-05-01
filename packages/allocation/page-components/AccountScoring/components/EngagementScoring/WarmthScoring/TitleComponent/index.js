23bdf0e87918aacc988b44bc504250d6f5f8d6f2
MahakCogoport <mahak.bharatkar@cogoport.com>
2023-05-01 16:12:36 +0530

    removed unneccesary code in the title section

--- packages/allocation/page-components/AccountScoring/components/EngagementScoring/WarmthScoring/TitleComponent/index.js
+++ packages/allocation/page-components/AccountScoring/components/EngagementScoring/WarmthScoring/TitleComponent/index.js
@@ -16,28 +16,25 @@ function TitleComponent({
 	return (
 		<div className={styles.title_container}>
 			<div className={styles.engagement_type}>{startCase(engagement_type)}</div>
-			{activeCollapse === engagement_type
+
+			{activeCollapse === engagement_type && editMode === engagement_type
 				? (
-					<div>
-						{editMode === engagement_type ? (
-							<div className={styles.title_buttons}>
-								<Button
-									size="md"
-									themeType="tertiary"
-									style={{ marginRight: '20px' }}
-									onClick={(e) => {
-										e.stopPropagation();
-										onDelete(engagement_type);
-									}}
-								>
-									<IcMDelete style={{ marginRight: '8px' }} />
-									Delete
-								</Button>
-							</div>
-						) : (
-							null
-						)}
+
+					<div className={styles.title_buttons}>
+						<Button
+							size="md"
+							themeType="tertiary"
+							style={{ marginRight: '20px' }}
+							onClick={(e) => {
+								e.stopPropagation();
+								onDelete(engagement_type);
+							}}
+						>
+							<IcMDelete style={{ marginRight: '8px' }} />
+							Delete
+						</Button>
 					</div>
+
 				) : null }
 
 		</div>
