7e6cf484b5641ece6437d6b9e123d7645541a9cd
Rahul Kumar <rahulk@cogoport.com>
2023-05-04 14:48:16 +0530

    fixes

--- packages/shipment-fcl-freight/common/Tasks/TaskExecution/CustomTasks/ChooseServiceProvider/index.js
+++ packages/shipment-fcl-freight/common/Tasks/TaskExecution/CustomTasks/ChooseServiceProvider/index.js
@@ -1,4 +1,5 @@
 import { Loader } from '@cogoport/components';
+import EmptyState from '@cogoport/ocean-modules/common/EmptyState';
 import toastApiError from '@cogoport/ocean-modules/utils/toastApiError';
 
 import useListShipmentBookingConfirmationPreferences
@@ -96,15 +97,17 @@ function ChooseServiceProvider({
 	}
 
 	return (
-		<div>
-			{(data?.list || []).map((item) => (
+		data?.list?.length
+			? (data?.list || []).map((item) => (
 				<Card
 					item={item}
 					priority={item.priority}
 					handleUpdateTask={handleUpdateTask}
 				/>
-			))}
-		</div>
+			))
+			: (
+				<EmptyState subEmptyText="No Booking Preference Found" />
+			)
 	);
 }
 export default ChooseServiceProvider;
