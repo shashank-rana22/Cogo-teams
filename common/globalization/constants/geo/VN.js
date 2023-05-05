7990a5cbb93ac5e9737e235c86f0b60973575b3c
Rahul Kumar <rahulk@cogoport.com>
2023-05-05 12:27:41 +0530

    Update VN.js

--- common/globalization/constants/geo/VN.js
+++ common/globalization/constants/geo/VN.js
@@ -115,6 +115,8 @@ export default {
 			'95d62549-8ab6-4ee5-a655-9edd0ec46dac', // Purnendu Shekhar
 			'cd8dde11-678c-4467-85b2-2e2d6055bef4', // Amitabh Shankar
 		],
+		lastmile_ops_id         : 'b808aabb-2245-4369-aaa7-6ebd2d5de20b',
+		lastmile_ops_manager_id : '3965f742-f4e5-420a-911c-f3657be05028',
 		prod_kam_cp           : 'e0e2f83b-9e5b-41a3-948e-ab1ec9b0f3ad',
 		prod_es_sales         : 'c71dd2db-9c8d-4d0c-84c6-beece1b3af42',
 		entity_manager_id     : '9d1d10dd-06c0-489d-92f8-272c6a40e9bb',
