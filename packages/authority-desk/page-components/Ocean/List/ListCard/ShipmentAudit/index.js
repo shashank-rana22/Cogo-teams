2e28abec79045df7fa8bd3f937b915d899309b78
Aakash Dinkar <aakash.dinkar@cogoport.com>
2023-05-05 10:07:05 +0530

    style fix

--- packages/authority-desk/page-components/Ocean/List/ListCard/ShipmentAudit/index.js
+++ packages/authority-desk/page-components/Ocean/List/ListCard/ShipmentAudit/index.js
@@ -1,40 +1,41 @@
-import { Modal, cl, Button } from "@cogoport/components";
-import { startCase } from "@cogoport/utils";
-import React, { useState } from "react";
+import { Modal, cl } from '@cogoport/components';
+import { startCase } from '@cogoport/utils';
+import React, { useState } from 'react';
 
-import CargoDetails from "../../../../../commons/CargoDetails";
-import PortDetails from "../../../../../commons/PortDetails";
-import ShipmentBreif from "../../../../../commons/ShipmentBreif";
-import ReleaseCard from "./ReleaseCard";
-import BlDetails from "./BlDetails";
-import CustodyShipments from "./CustodyShipments";
-import Invoices from "./Invoices";
-import Organizations from "./Organizations";
-import styles from "./styles.module.css";
+import CargoDetails from '../../../../../commons/CargoDetails';
+import PortDetails from '../../../../../commons/PortDetails';
+import ShipmentBreif from '../../../../../commons/ShipmentBreif';
+
+import BlDetails from './BlDetails';
+import CustodyShipments from './CustodyShipments';
+import Invoices from './Invoices';
+import Organizations from './Organizations';
+import ReleaseCard from './ReleaseCard';
+import styles from './styles.module.css';
 
 function ShipmentAudit({
 	item = {},
 	closeModal = () => {},
-	bucket = "eligible",
-	role = "kam",
+	bucket = 'eligible',
+	role = 'kam',
 }) {
 	const { freight_service } = item;
 
 	const tabs = {
-		invoices: "Invoices",
-		payments: `Payments - Total Outstanding  ${item?.invoice_status?.outstanding_amount}`,
-		shipments: "Shipments In Custody",
+		invoices  : 'Invoices',
+		payments  : `Payments - Total Outstanding  ${item?.invoice_status?.outstanding_amount}`,
+		shipments : 'Shipments In Custody',
 	};
 
 	const isApprovalAllowed = [
-		"eligible",
-		"requested",
-		"hold",
-		"approved",
-		"released",
+		'eligible',
+		'requested',
+		'hold',
+		'approved',
+		'released',
 	].includes(bucket);
 
-	const [additionalTab, setAdditionalTab] = useState("invoices");
+	const [additionalTab, setAdditionalTab] = useState('invoices');
 
 	return (
 		<div className={styles.container}>
@@ -48,53 +49,44 @@ function ShipmentAudit({
 					title={`Shipments > Audits > ${startCase(bucket)}`}
 				/>
 				<Modal.Body className={styles.modal_body_content}>
-					<>
-						<div className={styles.shipment_content_container}>
-							<div className={styles.shipment_details}>
-								<ShipmentBreif item={item} />
-								<PortDetails
-									primary_service={freight_service}
-								/>
-								<CargoDetails
-									primary_service={freight_service}
-								/>
+					<div className={styles.shipment_content_container}>
+						<div className={styles.shipment_details}>
+							<ShipmentBreif item={item} />
+							<PortDetails primary_service={freight_service} />
+							<CargoDetails primary_service={freight_service} />
+						</div>
+
+						<div className={styles.bl_details}>
+							<BlDetails item={item} />
+						</div>
+					</div>
+
+					<div className={styles.tabs}>
+						{Object.keys(tabs).map((tab) => (
+							<div
+								className={cl`${styles.tab} ${
+									tab === additionalTab
+										? styles.active
+										: ''
+								}`}
+								role="button"
+								tabIndex={0}
+								onClick={() => setAdditionalTab(tab)}
+							>
+								{tabs[tab]}
 							</div>
+						))}
+					</div>
 
-							<div className={styles.bl_details}>
-								<BlDetails item={item} />
-							</div>
-						</div>
-
-						<div className={styles.tabs}>
-							{Object.keys(tabs).map((tab) => (
-								<div
-									className={cl`${styles.tab} ${
-										tab === additionalTab
-											? styles.active
-											: ""
-									}`}
-									role="button"
-									tabIndex={0}
-									onClick={() => setAdditionalTab(tab)}
-								>
-									{tabs[tab]}
-								</div>
-							))}
-						</div>
-
-						<div className={styles.more_info}>
-							{additionalTab === "invoices" ? <Invoices /> : null}
-							{additionalTab === "payments" ? (
-								<Organizations />
-							) : null}
-							{additionalTab === "shipments" ? (
-								<CustodyShipments />
-							) : null}
-						</div>
-					</>
+					<div className={styles.more_info}>
+						{additionalTab === 'invoices' ? <Invoices /> : null}
+						{additionalTab === 'payments' ? <Organizations /> : null}
+						{additionalTab === 'shipments' ? <CustodyShipments /> : null}
+					</div>
 				</Modal.Body>
+
 				<Modal.Footer>
-					{role === "credit_control" && isApprovalAllowed ? (
+					{role === 'credit_control' && isApprovalAllowed ? (
 						<ReleaseCard data={item} bucket={bucket} />
 					) : null}
 				</Modal.Footer>
