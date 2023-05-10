42f213fc4100e18415c42f395c0f2d49d4e9a591
Kanchan Bhandari <85485325+kanchan-13@users.noreply.github.com>
2023-05-10 19:08:23 +0530

    Update index.js

--- packages/bl_do-collection-release/page-components/StationaryManagement/Usage/TableView/index.js
+++ packages/bl_do-collection-release/page-components/StationaryManagement/Usage/TableView/index.js
@@ -3,22 +3,22 @@ import { format, upperCase } from '@cogoport/utils';
 
 import styles from './styles.module.css';
 
-function TableView({ data = {}, loading }) {
-	const { list = [] } = data || {};
+const columns = [
+	{
+		Header   : 'SID',
+		accessor : (row) => <span className={styles.serial_id}>{`#${row.serial_id}` }</span>,
+	},
+	{
+		Header   : 'Used On',
+		accessor : (row) => (row.used_at ? format(row.used_at, 'dd MMM yyyy') : '-'),
+	},
+	{ Header: 'BL Type', accessor: (row) => upperCase(row.bl_type || '-') },
+	{ Header: 'Copies', accessor: 'bls_count' },
+	{ Header: 'SO2', accessor: (row) => row?.service_ops2?.name || '-' },
+];
 
-	const columns = [
-		{
-			Header   : 'SID',
-			accessor : (row) => <span className={styles.serial_id}>{`#${row.serial_id}` }</span>,
-		},
-		{
-			Header   : 'Used On',
-			accessor : (row) => (row.used_at ? format(row.used_at, 'dd MMM yyyy') : '-'),
-		},
-		{ Header: 'BL Type', accessor: (row) => upperCase(row.bl_type || '-') },
-		{ Header: 'Copies', accessor: 'bls_count' },
-		{ Header: 'SO2', accessor: (row) => row?.service_ops2?.name || '-' },
-	];
+function TableView({ data = {}, loading = false }) {
+	const { list = [] } = data;
 
 	return <Table onRowClick={false} data={list} columns={columns} loading={loading} />;
 }
