0dc1135d2ff373ded73b8089ae6666baae438694
rajkamalCP <rajkamal.singh@cogoport.com>
2023-05-11 19:37:11 +0530

    changes

--- packages/bl_do-collection-release/commons/Accordion/PendingKnockOff/index.js
+++ packages/bl_do-collection-release/commons/Accordion/PendingKnockOff/index.js
@@ -1,10 +1,17 @@
-import { Tabs, TabPanel, Table } from '@cogoport/components';
+import { Table, cl } from '@cogoport/components';
+import { useState } from 'react';
 
 import getTableFormatedData from '../../../helpers/getTableFormatedData';
+import ClickableDiv from '../../ClickableDiv';
 import PendingTasks from '../../PendingTasks/TaskList';
 import { columns } from '../Invoices/tableColumn';
 
 import styles from './styles.module.css';
+
+const Tabs = [
+	{ label: 'Invoices', value: 'invoice' },
+	{ label: 'Tasks', value: 'tasks' },
+];
 
 function PendingKnockOff({
 	item = {},
@@ -14,6 +21,8 @@ function PendingKnockOff({
 	getShipmentPendingTask = () => {},
 	taskLoading = false,
 }) {
+	const [tab, setTab] = useState('invoice');
+	console.log('tab', tab);
 	const list_of_invoices = item?.invoice_data || [];
 
 	const tableData = getTableFormatedData(list_of_invoices);
@@ -23,34 +32,44 @@ function PendingKnockOff({
 		handleAccordionOpen();
 	};
 
-	const onTabChange = (val) => {
-		if (val === 'tasks') {
+	const onTabChange = (currentTab) => {
+		if (currentTab.value === 'tasks') {
 			getShipmentPendingTask();
 		}
+		setTab(currentTab.value);
 	};
 
 	return (
 		<div className={styles.container}>
-			<Tabs
-				defaultTab="invoice"
-				onChange={(val) => onTabChange(val)}
-			>
-				<TabPanel name="invoice" title="Invoices">
-					<div className={styles.list_container}>
-						<Table fields={columns} data={tableData} />
-					</div>
-				</TabPanel>
+			<div className={styles.service_tabs}>
+				{
+				Tabs.map((currentTab) => (
+					<ClickableDiv onClick={() => onTabChange(currentTab)}>
+						<div className={cl`${tab === currentTab.value ? styles.active : ''} 
+				${styles.service_tab}`}
+						>
+							{currentTab.label}
+						</div>
+					</ClickableDiv>
+				))
+			}
+			</div>
+			<div>
+				{
+				tab === 'invoice'
+					? <Table columns={columns} data={tableData} />
+					: (
+						<PendingTasks
+							taskList={tasks}
+							item={item}
+							handleAccordionOpen={handleAccordionOpen}
+							refetchForTask={refetchForTask}
+							tasksLoading={taskLoading}
+						/>
+					)
+			}
+			</div>
 
-				<TabPanel name="tasks" title="Tasks">
-					<PendingTasks
-						taskList={tasks}
-						item={item}
-						handleAccordionOpen={handleAccordionOpen}
-						refetchForTask={refetchForTask}
-						tasksLoading={taskLoading}
-					/>
-				</TabPanel>
-			</Tabs>
 		</div>
 	);
 }
