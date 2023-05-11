c7d3850a0ffd03bd2fef45b29da0cc98a6eac020
Nagendra Singh <nagendra.singh@cogoport.com>
2023-05-11 13:59:56 +0530

    fix: currency option

--- packages/business-finance/page-components/OverHeads/constants/CURRENCY_OPTIONS.ts
+++ packages/business-finance/page-components/OverHeads/constants/CURRENCY_OPTIONS.ts
@@ -2,5 +2,5 @@ import GLOBAL_CONSTANTS from '@cogoport/globalization/constants/globals.json';
 
 export const CURRENCY_OPTIONS = Object.keys(GLOBAL_CONSTANTS.currency_code).map((currency) => ({
 	label : currency,
-	value : currency,
+	value : GLOBAL_CONSTANTS.currency_code[currency],
 }));
