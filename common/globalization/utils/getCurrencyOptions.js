c6a813fa5623becefc266f2593fefc476f9d1516
Rkoshta79 <rahul.koshta@cogoport.com>
2023-05-12 19:01:44 +0530

    value fix

--- common/globalization/utils/getCurrencyOptions.js
+++ common/globalization/utils/getCurrencyOptions.js
@@ -2,7 +2,7 @@ import GLOBAL_CONSTANTS from '../constants/globals.json';
 
 const getCurrencyOptions = () => Object.keys(GLOBAL_CONSTANTS.currency_code).map((currency) => ({
 	label : currency,
-	value : GLOBAL_CONSTANTS.currency_code[currency],
+	value : currency,
 }));
 
 export default getCurrencyOptions;
