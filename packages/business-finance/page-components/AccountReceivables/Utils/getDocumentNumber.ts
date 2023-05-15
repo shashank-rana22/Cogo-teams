e292154ec698a5e05afa1faa624a396ef7c4f7c2
akashcogoport <akash.shukla@cogoport.com>
2023-05-15 16:03:58 +0530

    fixes

--- packages/business-finance/page-components/AccountReceivables/Utils/getDocumentNumber.ts
+++ packages/business-finance/page-components/AccountReceivables/Utils/getDocumentNumber.ts
@@ -3,19 +3,23 @@ import { isEmpty, getByKey } from '@cogoport/utils';
 import { Object } from '../commons/Interfaces';
 
 export const getDocumentNumber = ({ itemData }: Object) => {
-	const key = itemData?.eInvoicePdfUrl ? 'irnNumber' : 'invoiceNumber';
+	const isPresent = ['irnNumber', 'invoiceNumber'].some((item) => !isEmpty(getByKey(itemData, item)));
 
-	if (!isEmpty(getByKey(itemData, key))) {
-		return getByKey(itemData, key) as string;
+	let key = 'proformaNumber';
+
+	if (isPresent) {
+		key = itemData?.eInvoicePdfUrl ? 'irnNumber' : 'invoiceNumber';
 	}
-	return getByKey(itemData, 'proformaNumber') as string;
+	return getByKey(itemData, key) as string;
 };
 
 export const getDocumentUrl = ({ itemData }: Object) => {
-	const key = itemData?.eInvoicePdfUrl ? 'eInvoicePdfUrl' : 'invoicePdf';
+	const isPresent = ['eInvoicePdfUrl', 'invoicePdf'].some((item) => !isEmpty(getByKey(itemData, item)));
 
-	if (!isEmpty(getByKey(itemData, 'invoiceNumber')) || !isEmpty(getByKey(itemData, 'irnNumber'))) {
-		return getByKey(itemData, key);
+	let key = 'proformaPdfUrl';
+
+	if (isPresent) {
+		key = itemData?.eInvoicePdfUrl ? 'eInvoicePdfUrl' : 'invoicePdf';
 	}
-	return getByKey(itemData, 'proformaPdfUrl');
+	return getByKey(itemData, key);
 };
