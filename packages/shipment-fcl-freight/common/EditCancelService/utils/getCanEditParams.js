b4654d15ad591405ea49df4da94feeff59292db6
Aakash Dinkar <aakash.dinkar@cogoport.com>
2023-05-18 16:34:19 +0530

    cancelled background change

--- packages/shipment-fcl-freight/common/EditCancelService/utils/getCanEditParams.js
+++ packages/shipment-fcl-freight/common/EditCancelService/utils/getCanEditParams.js
@@ -29,9 +29,7 @@ function getShowCondition(shipment_data, conditions) {
 	return Object.entries(conditions).some(([key, value]) => value.includes(shipment_data[key]));
 }
 
-export default function getCanEditParams({ shipment_data, user_data, serviceData }) {
-	const activeStakeholder = 'booking_agent';
-
+export default function getCanEditParams({ shipment_data, user_data, serviceData, activeStakeholder }) {
 	if (`${shipment_data?.shipment_type}_service` !== serviceData?.service_type) {
 		return false;
 	}
