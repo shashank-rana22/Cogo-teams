c78e91fc9ea8ddbb164d79a6d3003608eec7e83d
Kanchan Bhandari <85485325+kanchan-13@users.noreply.github.com>
2023-05-19 10:48:40 +0530

    Update index.js

--- packages/purchase-invoicing/common/ConfirmationModal/index.js
+++ packages/purchase-invoicing/common/ConfirmationModal/index.js
@@ -13,9 +13,7 @@ function ConfirmationModal({
 			<Modal
 				size="sm"
 				show
-				onClose={() => {
-					setConfirmation(false);
-				}}
+				onClose={() => setConfirmation(false)}
 				closeOnOuterClick={false}
 				placement="center"
 				className={styles.modal_container}
@@ -24,15 +22,18 @@ function ConfirmationModal({
 					<div className={styles.infoicon}>
 						<IcMInformation height={20} width={20} color="red" />
 					</div>
+
 					<div className={styles.exchangeheading}>
 						Please check all the details carefully and validate.
 					</div>
 				</Modal.Body>
+
 				<Modal.Footer>
 					<div className={styles.flex}>
 						<Button themeType="secondary" onClick={() => setConfirmation(null)}>
 							Cancel
 						</Button>
+
 						<Button
 							className={styles.button}
 							onClick={handleFinalSubmit}
