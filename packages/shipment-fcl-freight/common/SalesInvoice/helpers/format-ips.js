b541d701c2c2d0e542ef78066caf2b80cf196f96
Rajatuba-Cogoport <rajat.uba@cogoport.com>
2023-05-20 21:11:30 +0530

    change formatIps format

--- packages/shipment-fcl-freight/common/SalesInvoice/helpers/format-ips.js
+++ packages/shipment-fcl-freight/common/SalesInvoice/helpers/format-ips.js
@@ -1,11 +1,15 @@
 const formatIps = (ips) => {
 	const newIps = ips?.map((ip) => {
-		const ba = ip?.billing_address;
+		const {
+			billing_address : ba, id, is_active, services = [], is_igst,
+			invoice_currency, invoicing_party_total_discounted, invoice_total_currency,
+			status, source:invoice_source,
+		} = ip || {};
 
 		return {
-			id              : ip?.id,
-			is_active       : ip?.is_active,
-			billing_address : {
+			id,
+			is_active,
+			billing_address: {
 				trade_party_type            : ba?.trade_party_type,
 				tax_number                  : ba?.tax_number,
 				organization_trade_party_id : ba?.organization_trade_party_id,
@@ -19,26 +23,22 @@ const formatIps = (ips) => {
 				business_name               : ba?.business_name,
 				address                     : ba?.address,
 			},
-			services: (ip?.services || []).map((service) => {
-				const serv = {
-					service_type : service?.service_type,
-					service_id   : service?.service_id,
-					trade_type   : service?.trade_type,
-					display_name:
-						service?.service_type === 'shipment'
-							? 'Convenience Fees'
-							: service?.display_name,
-					serviceKey : service?.serviceKey,
-					is_igst    : ip?.is_igst || null,
-				};
-				return serv;
-			}),
-			invoice_currency                 : ba?.invoice_currency || ip?.invoice_currency,
-			invoicing_party_total_discounted : ip?.invoicing_party_total_discounted,
-			invoice_total_currency           : ip?.invoice_total_currency,
-			status                           : ip?.status,
-			invoice_source                   : ip?.source,
-			is_igst                          : ip?.is_igst,
+			services: (services || []).map((service) => ({
+				service_type : service?.service_type,
+				service_id   : service?.service_id,
+				trade_type   : service?.trade_type,
+				display_name : service?.service_type === 'shipment'
+					? 'Convenience Fees'
+					: service?.display_name,
+				serviceKey : service?.serviceKey,
+				is_igst    : is_igst || null,
+			})),
+			invoice_currency: ba?.invoice_currency || invoice_currency,
+			invoicing_party_total_discounted,
+			invoice_total_currency,
+			status,
+			invoice_source,
+			is_igst,
 		};
 	});
 
