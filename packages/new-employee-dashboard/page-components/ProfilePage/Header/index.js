2ed9eae535dbfa330a99eec79db527e359ed6fc3
Shivam Singh <shivam.singh@cogoport.com>
2023-05-22 19:15:37 +0530

    avatar

--- packages/new-employee-dashboard/page-components/ProfilePage/Header/index.js
+++ packages/new-employee-dashboard/page-components/ProfilePage/Header/index.js
@@ -4,16 +4,14 @@ import React from 'react';
 
 import styles from './styles.module.css';
 
-const src = 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=800';
-
 function Header({ detail }) {
 	const { name, employee_code, designation, passport_size_photo_url } = detail || {};
-	console.log('detail', detail);
+
 	return (
 		<div className={styles.container}>
 			<div className={styles.profile}>
 				<Avatar
-					src={src}
+					src={passport_size_photo_url || null}
 					alt="img"
 					disabled={false}
 					size="160px"
