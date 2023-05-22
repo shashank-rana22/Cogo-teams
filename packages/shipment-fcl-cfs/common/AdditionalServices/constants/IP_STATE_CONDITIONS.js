59ca12e8ae690f55f51ece1a176f70dff2133145
santkumar1799 <sant.kumar@cogoport.com>
2023-05-22 11:50:28 +0530

    fcl cfs service

--- packages/shipment-fcl-cfs/common/AdditionalServices/constants/IP_STATE_CONDITIONS.js
+++
@@ -1,8 +0,0 @@
-// for these states ADD IP button will not show
-const IP_STATE_CONDITONS = [
-	'accepted_by_importer_exporter',
-	'cancelled',
-	'cancelled_by_supplier',
-];
-
-export default IP_STATE_CONDITONS;
