f7f43880f6e0d6a2b9f4dc3e3cbe9f8177297b92
amandeep2508 <amandeep.chauhan@cogoport.com>
2023-05-23 14:00:05 +0530

    added tabs

--- packages/ticket-management/page-components/SuperAdmin/index.js
+++ packages/ticket-management/page-components/SuperAdmin/index.js
@@ -1,6 +1,43 @@
+import { Tabs, TabPanel } from '@cogoport/components';
+import { useState } from 'react';
+
+import styles from './styles.module.css';
+
 function SuperAdmin() {
+	const [activeTab, setActiveTab] = useState('dashboard');
+
 	return (
-		<div>SuperAdmin</div>
+		<>
+			<div className={styles.title}>Ticket Management</div>
+			<Tabs
+				activeTab={activeTab}
+				themeType="primary"
+				onChange={setActiveTab}
+				className={styles.tab_panel}
+
+			>
+				<TabPanel
+					name="dashboard"
+					title="Dashboard"
+				>
+					<div>Dashboard</div>
+				</TabPanel>
+				<TabPanel
+					name="all_tickets"
+					title="All Tickets"
+				>
+					<div>All Tickets</div>
+				</TabPanel>
+				<TabPanel
+					name="configurations"
+					title="Configurations"
+				>
+					<div>configurations</div>
+				</TabPanel>
+
+			</Tabs>
+		</>
+
 	);
 }
 
