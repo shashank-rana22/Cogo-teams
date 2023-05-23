c919100cb24a75385ae32b320390a32b94ec9a42
amandeep2508 <amandeep.chauhan@cogoport.com>
2023-05-23 19:53:13 +0530

    fix

---
+++ packages/ticket-management/page-components/SuperAdmin/Dashboard/UserStatistics/index.js
@@ -0,0 +1,10 @@
+import styles from './styles.module.css';
+
+function UserStatistics() {
+	return (
+		<div className={styles.title}>UserStatistics</div>
+
+	);
+}
+
+export default UserStatistics;
