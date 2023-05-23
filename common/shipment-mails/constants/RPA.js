69e34c8a5af4039877c662a1b09fc4b9fc839db0
Arpit Gupta <arpitgutpa@Arpits-MacBook-Air.local>
2023-05-23 19:15:13 +0530

    lint fix

--- common/shipment-mails/constants/RPA.js
+++ common/shipment-mails/constants/RPA.js
@@ -1,8 +1,8 @@
 const RPA = {
 	BOOKINGS_DEFAULT_COMPOSE_EMAIL : 'operations@cogoport.com',
 	BOOKINGS_DEFAULT_RECIEVE_EMAIL : 'operations@cogoport.com',
-	AIR_BOOKINGS_COMPOSE_EMAIL: 'operations.air@cogoport.com',
-	AIR_BOOKINGS_RECEIVE_EMAIL: 'operations.air@cogoport.com',
+	AIR_BOOKINGS_COMPOSE_EMAIL : 'operations.air@cogoport.com',
+	AIR_BOOKINGS_RECEIVE_EMAIL : 'operations.air@cogoport.com',
 };
 
 export default RPA;
