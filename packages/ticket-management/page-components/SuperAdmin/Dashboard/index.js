90434cd9b4c61d39b67e6810c755f281622351d6
amandeep2508 <amandeep.chauhan@cogoport.com>
2023-05-24 13:03:32 +0530

    fix

--- packages/ticket-management/page-components/SuperAdmin/Dashboard/index.js
+++ packages/ticket-management/page-components/SuperAdmin/Dashboard/index.js
@@ -1,10 +1,12 @@
 import OverallStats from './OverallStats';
 import styles from './styles.module.css';
+import UserStatistics from './UserStatistics';
 
 function Dashboard() {
 	return (
 		<div className={styles.container}>
 			<OverallStats />
+			<UserStatistics />
 		</div>
 	);
 }
