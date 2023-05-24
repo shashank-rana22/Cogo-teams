b21da558ca7a4499bcc502af43d49f2fd7b1ccd3
hritwikt <hritwik.tripathi@cogoport.com>
2023-05-24 20:29:09 +0530

    (update): merge conflict changes done

--- packages/new-employee-dashboard/page-components/ProfilePage/index.js
+++ packages/new-employee-dashboard/page-components/ProfilePage/index.js
@@ -1,13 +1,6 @@
-import { Tabs, TabPanel } from '@cogoport/components';
-import React, { useState } from 'react';
+import { Tabs, TabPanel } from "@cogoport/components";
+import React, { useState } from "react";
 
-import AdditionalDetails from './AdditionalDetails';
-import Header from './Header';
-import CtcBreakupModal from './Header/CtcBreakupModal';
-import ProfileDetails from './ProfileDetails';
-import SignedDocuments from './SignedDocuments';
-import styles from './styles.module.css';
-import useProfileDetails from './useProfileDetails';
 import AdditionalDetails from "./AdditionalDetails";
 import Header from "./Header";
 import CtcBreakupModal from "./Header/CtcBreakupModal";
@@ -17,18 +10,16 @@ import styles from "./styles.module.css";
 import useProfileDetails from "./useProfileDetails";
 
 function ProfilePage() {
-	const [activeTab, setActiveTab] = useState('profile_info');
-	const {
-		data: profileData = {},
-		loading = false,
-		ctcStructure = {},
-		setCtcStructure = () => {},
-		initialQuestion = '',
-		setInitialQuestion = () => {},
-		formProps = {},
-	} = useProfileDetails();
-
-  console.log("profile", profileData);
+  const [activeTab, setActiveTab] = useState("profile_info");
+  const {
+    data: profileData = {},
+    loading = false,
+    ctcStructure = {},
+    setCtcStructure = () => {},
+    initialQuestion = "",
+    setInitialQuestion = () => {},
+    formProps = {},
+  } = useProfileDetails();
 
   const { detail = {} } = profileData || {};
 
@@ -36,10 +27,6 @@ function ProfilePage() {
   return (
     <div className={styles.container}>
       <Header detail={detail} setShowCtcBreakupModal={setShowCtcBreakupModal} />
-	const [showCtcBreakupModal, setShowCtcBreakupModal] = useState(false);
-	return (
-		<div className={styles.container}>
-			<Header detail={detail} setShowCtcBreakupModal={setShowCtcBreakupModal} />
 
       <div className={styles.tab_container}>
         <Tabs activeTab={activeTab} themeType="primary" onChange={setActiveTab}>
@@ -57,18 +44,6 @@ function ProfilePage() {
         </Tabs>
       </div>
 
-			{showCtcBreakupModal && (
-				<CtcBreakupModal
-					showCtcBreakupModal={showCtcBreakupModal}
-					setShowCtcBreakupModal={setShowCtcBreakupModal}
-					ctcStructure={ctcStructure}
-					initialQuestion={initialQuestion}
-					setInitialQuestion={setInitialQuestion}
-					formProps={formProps}
-				/>
-			)}
-		</div>
-	);
       {showCtcBreakupModal && (
         <CtcBreakupModal
           showCtcBreakupModal={showCtcBreakupModal}
