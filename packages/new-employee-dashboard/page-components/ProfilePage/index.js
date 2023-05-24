b0b18819d5c847987c11f7b8205136b8e47142bd
Prachi Dewangan <prachi.dewangan@cogoport.com>
2023-05-24 19:55:17 +0530

    added tabs

--- packages/new-employee-dashboard/page-components/ProfilePage/index.js
+++ packages/new-employee-dashboard/page-components/ProfilePage/index.js
@@ -1,29 +1,29 @@
-import { Tabs, TabPanel } from "@cogoport/components";
-import React, { useState } from "react";
+import { Tabs, TabPanel } from '@cogoport/components';
+import React, { useState } from 'react';
 
 import AdditionalDetails from './AdditionalDetails';
-import Header from "./Header";
-import CtcBreakupModal from "./Header/CtcBreakupModal";
-import ProfileDetails from "./ProfileDetails";
+import Header from './Header';
+import CtcBreakupModal from './Header/CtcBreakupModal';
+import ProfileDetails from './ProfileDetails';
 import SignedDocuments from './SignedDocuments';
-import styles from "./styles.module.css";
-import useProfileDetails from "./useProfileDetails";
+import styles from './styles.module.css';
+import useProfileDetails from './useProfileDetails';
 
 function ProfilePage() {
-  const [activeTab, setActiveTab] = useState("profile_info");
-  const {
-    data: profileData = {},
-    loading = false,
-    ctcStructure = {},
-    setCtcStructure = () => {},
-    initialQuestion = "",
-    setInitialQuestion = () => {},
-    formProps = {},
-  } = useProfileDetails();
+	const [activeTab, setActiveTab] = useState('profile_info');
+	const {
+		data: profileData = {},
+		loading = false,
+		ctcStructure = {},
+		setCtcStructure = () => {},
+		initialQuestion = '',
+		setInitialQuestion = () => {},
+		formProps = {},
+	} = useProfileDetails();
 
 	const { detail = {} } = profileData || {};
 
-  const [showCtcBreakupModal, setShowCtcBreakupModal] = useState(false);
+	const [showCtcBreakupModal, setShowCtcBreakupModal] = useState(false);
 	return (
 		<div className={styles.container}>
 			<Header detail={detail} setShowCtcBreakupModal={setShowCtcBreakupModal} />
@@ -48,16 +48,16 @@ function ProfilePage() {
 				</Tabs>
 			</div>
 
-      {showCtcBreakupModal && (
-        <CtcBreakupModal
-          showCtcBreakupModal={showCtcBreakupModal}
-          setShowCtcBreakupModal={setShowCtcBreakupModal}
-          ctcStructure={ctcStructure}
-          initialQuestion={initialQuestion}
-          setInitialQuestion={setInitialQuestion}
-          formProps={formProps}
-        />
-      )}
+			{showCtcBreakupModal && (
+				<CtcBreakupModal
+					showCtcBreakupModal={showCtcBreakupModal}
+					setShowCtcBreakupModal={setShowCtcBreakupModal}
+					ctcStructure={ctcStructure}
+					initialQuestion={initialQuestion}
+					setInitialQuestion={setInitialQuestion}
+					formProps={formProps}
+				/>
+			)}
 		</div>
 	);
 }
