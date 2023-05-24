95514843af0fb413fe03267e1d2b6e942ecca712
Prachi Dewangan <prachi.dewangan@cogoport.com>
2023-05-24 13:07:07 +0530

    prefilled resume

--- packages/employee-portal/page-components/TabComponents/index.js
+++ packages/employee-portal/page-components/TabComponents/index.js
@@ -25,9 +25,9 @@ function TabComponents({ data, informationPage, setInformationPage }) {
 
 	const MAPPING = {
 		new_hire_information : 'https://cdn.cogoport.io/cms-prod/cogo_admin/vault/original/image 180.png',
-		company_policies     : 'https://cdn.cogoport.io/cms-prod/cogo_admin/vault/original/image 180.png',
 		offer_letter         : 'https://cdn.cogoport.io/cms-prod/cogo_admin/vault/original/image 182.png',
 		sign_your_docs       : 'https://cdn.cogoport.io/cms-prod/cogo_admin/vault/original/image 183.png',
+		company_policies     : 'https://cdn.cogoport.io/cms-prod/cogo_admin/vault/original/image 180.png',
 		day_1                : 'https://cdn.cogoport.io/cms-prod/cogo_admin/vault/original/image 181.png',
 		maps                 : 'https://cdn.cogoport.io/cms-prod/cogo_admin/vault/original/image 183.png',
 
