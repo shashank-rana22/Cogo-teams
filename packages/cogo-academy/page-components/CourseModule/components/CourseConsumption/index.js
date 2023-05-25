cae53d9ce9dd874688bf5fad653b59a068e9280d
Harsh <harsh.bhagat@cogoport.com>
2023-05-25 15:22:17 +0530

    changes

--- packages/cogo-academy/page-components/CourseModule/components/CourseConsumption/index.js
+++ packages/cogo-academy/page-components/CourseModule/components/CourseConsumption/index.js
@@ -1,6 +1,49 @@
+import { IcMStarfull } from '@cogoport/icons-react';
+import { useRouter } from '@cogoport/next';
+
+import useGetCogoAcademyCourse from './hooks/useGetCogoAcademyCourse';
+import useGetCourseDetails from './hooks/useGetCourseDetails';
+import styles from './styles.module.css';
+
 function CourseConsumption() {
+	const router = useRouter();
+
+	const course_id = router?.query?.course_id;
+
+	const {
+		loading,
+		finalData,
+	} = useGetCourseDetails({ id: course_id });
+
+	const {
+		courseData,
+		courseLoading,
+	} = useGetCogoAcademyCourse({ id: course_id });
+
 	return (
-		<h1>hello this is madhesh</h1>
+		<>
+			<div className={styles.header}>
+				<div className={styles.name}>ffrcf</div>
+				<IcMStarfull />
+			</div>
+			<div>
+				About the course:
+				{' '}
+				{courseData.query_name}
+			</div>
+			<div>
+				About the course:
+				{' '}
+				{courseData.query_name}
+			</div>
+			<div>
+				<div>In this course, you will learn (Objectives)</div>
+				{(courseData?.course_objectives || []).map((item) => (
+					<div>hws</div>
+				))}
+			</div>
+		</>
+
 	);
 }
 
