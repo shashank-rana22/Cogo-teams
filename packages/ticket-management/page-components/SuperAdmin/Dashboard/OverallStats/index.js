6045d8e6a8de7d9ae5d669cc29892f094ecc7ab3
amandeep2508 <amandeep.chauhan@cogoport.com>
2023-05-25 19:20:37 +0530

    OverallStats added data

--- packages/ticket-management/page-components/SuperAdmin/Dashboard/OverallStats/index.js
+++ packages/ticket-management/page-components/SuperAdmin/Dashboard/OverallStats/index.js
@@ -5,81 +5,62 @@ import { statsIconsAndData } from '../../../../configurations/dashboard-stats';
 
 import styles from './styles.module.css';
 
-function OverallStats() {
-	const PieData = [
-		{
-			id       : 'open',
-			label    : 'Open',
-			value    : 25,
-			isMargin : true,
-		},
-		{
-			id       : 'escalated',
-			label    : 'Escalated',
-			value    : 30,
-			isMargin : true,
-
-		},
-		{
-			id    : 'pending',
-			label : 'Pending',
-			value : 25,
-		},
-		{
-			id    : 'closed',
-			label : 'Closed',
-			value : 20,
-		},
-	];
+function OverallStats({ data, ticketCount }) {
+	const pieData = Object.keys(ticketCount || {}).map((key) => ({
+		id    : key.toLocaleLowerCase(),
+		label : key,
+		value : ticketCount[key] || 0,
+	}));
 
 	return (
 		<div className={styles.container}>
 			<div className={cl`${styles.tile} ${styles.total_graph}`}>
-				<div className={styles.total}>
-					<div className={styles.graph}>
-						<ResponsivePie
-							data={PieData}
-							innerRadius={0.85}
-							enableArcLinkLabels={false}
-							enableArcLabels={false}
-							colors={['#F8AEA8', '#FCDC00', '#E0E0E0', '#CED1ED']}
-							colorBy="index"
-							margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
-							tooltip={({
-								datum: { label = '', value = '' },
-							}) => (
-								<div className={styles.pie_tooltip}>
-									<strong>
-										{label}
-										:
-										{' '}
-										{value}
-									</strong>
+				<div className={styles.graph}>
+					<ResponsivePie
+						data={pieData}
+						innerRadius={0.85}
+						enableArcLinkLabels={false}
+						enableArcLabels={false}
+						colors={['#F8AEA8', '#e0e0e0', '#FCDC00', '#7278AD', '#2AC85F']}
+						colorBy="index"
+						margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
+						tooltip={({
+							datum: { label = '', value = '', id = '' },
+						}) => (
+							<div className={styles.pie_tooltip}>
+								<div className={cl`${styles.tooltip_dot} ${styles[id]}`} />
+								<div>
+									{label}
+									:
+									{' '}
+									{value}
+									{' '}
+									Tickets
 								</div>
-							)}
-						/>
-					</div>
-					<div className={styles.graph_total}>
-						<span className={styles.graph_label}>No. of Tickets</span>
-						<span className={styles.graph_count}>8.4k</span>
-					</div>
-					<div className={styles.legends}>
-						{(PieData || []).map(({ id, label, value, isMargin }) => (
-							<div className={cl`${styles.legend} ${isMargin ? styles.margin_bottom : ''}`} key={id}>
-								<div className={styles.legend_count}>
-									<div className={cl`${styles.dot} ${styles[id]}`} />
-									<span className={styles.stats_count}>{value}</span>
-								</div>
-								<div className={styles.stats_label}>{label}</div>
 							</div>
-						))}
-					</div>
+						)}
+					/>
+				</div>
+				<div className={styles.graph_total}>
+					<span className={styles.graph_label}>No. of Tickets</span>
+					<span className={styles.graph_count}>8.4k</span>
+				</div>
+				<div className={styles.legends}>
+					{(pieData || []).map(({ id, label, value, isMargin }) => (
+						<div className={cl`${styles.legend} ${isMargin ? styles.margin_bottom : ''}`} key={id}>
+							<div className={styles.legend_count}>
+								<div className={cl`${styles.dot} ${styles[id]}`} />
+								<span className={styles.stats_count}>{value}</span>
+							</div>
+							<div className={styles.stats_label}>{label}</div>
+						</div>
+					))}
 				</div>
 			</div>
-			{(statsIconsAndData || []).map(({ key, icon, label, count }) => (
+			{(statsIconsAndData || []).map(({ key, icon, label }) => (
 				<div className={styles.tile} key={key}>
 					<div>{icon}</div>
-					<div className={styles.count}>{count}</div>
+					<div className={styles.count}>{data?.[key] || 0}</div>
 					<div className={styles.label}>{label}</div>
 				</div>
 			))}
