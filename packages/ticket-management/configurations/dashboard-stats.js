9cab4f1851bb14d4e349fb8ad4c242d53238bbf2
amandeep2508 <amandeep.chauhan@cogoport.com>
2023-05-26 12:24:09 +0530

    mapping fix

--- packages/ticket-management/configurations/dashboard-stats.js
+++ packages/ticket-management/configurations/dashboard-stats.js
@@ -2,23 +2,25 @@ import { IcMTicket, IcMFtick, IcMClock } from '@cogoport/icons-react';
 
 export const statsIconsAndData = [
 	{
-		key   : 'AverageResponseTime',
-		label : 'Avg First Response Time',
-		icon  : <IcMClock fill="#ABB0DE" width="18px" height="18px" />,
+		key    : 'AverageResponseTime',
+		label  : 'Avg First Response Time',
+		suffix : 'sec',
+		icon   : <IcMClock fill="#ABB0DE" width="22px" height="22px" />,
 	},
 	{
-		key   : 'AverageResolutionTime',
-		label : 'Avg Resolution Time',
-		icon  : <IcMFtick fill="#ABB0DE" width="25px" height="25px" />,
+		key    : 'AverageResolutionTime',
+		label  : 'Avg Resolution Time',
+		suffix : 'sec',
+		icon   : <IcMFtick fill="#ABB0DE" width="25px" height="25px" />,
 	},
 	{
 		key   : 'four',
 		label : 'Reopened Tickets',
-		icon  : <IcMTicket fill="#F8AEA8" width="22px" height="22px" />,
+		icon  : <IcMTicket fill="#F8AEA8" width="25px" height="25px" />,
 	},
 	{
 		key   : 'five',
 		label : 'Escalated Tickets',
-		icon  : <IcMTicket fill="#D6B300" width="22px" height="22px" />,
+		icon  : <IcMTicket fill="#D6B300" width="25px" height="25px" />,
 	},
 ];
