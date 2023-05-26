938b3e3228e4657ce440fe300412d24d3c1b5d07
amandeep2508 <amandeep.chauhan@cogoport.com>
2023-05-26 17:00:53 +0530

    reformatted

--- packages/ticket-management/page-components/SuperAdmin/Dashboard/UserStatistics/index.js
+++ packages/ticket-management/page-components/SuperAdmin/Dashboard/UserStatistics/index.js
@@ -1,11 +1,9 @@
 import styles from './styles.module.css';
 import Widget from './Widget';
 
-function UserStatistics({ topCustomer, topCategory, topAgents, loading }) {
+function UserStatistics({ topAgents, loading }) {
 	return (
 		<div className={styles.container}>
-			<Widget label="Users (based on issues)" data={topCustomer} type="Users" loading={loading} />
-			<Widget label="Top Categories" data={topCategory} type="Categories" loading={loading} />
 			<Widget
 				label="Top Agents"
 				subLabel="Performance Rating"
