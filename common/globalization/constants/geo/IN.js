347420f8ea48828b3e29ed7fb1e7641261720ea6
Kanchan Bhandari <85485325+kanchan-13@users.noreply.github.com>
2023-05-26 15:53:18 +0530

    Update IN.js

--- common/globalization/constants/geo/IN.js
+++ common/globalization/constants/geo/IN.js
@@ -187,8 +187,8 @@ export default {
 		prod_coe_finance_head : '7b1fc916-6984-4179-aee5-c23f9871085d',
 		finance_head_id       : 'a8a175c5-fec2-4c08-8e6b-0fb5c7951c86',
 		so_1_manager          : '17885538-e746-4650-a5bc-7d4d81247c7d',
-		so_2_manager    : '1665784f-6e58-4299-8a33-4b7487f61188',
-		so1_so2_role_id : '0285645b-0d06-42a2-9968-67d544626300', // SO1 and SO2 VN
+		so_2_manager	      : '1665784f-6e58-4299-8a33-4b7487f61188',
+		so1_so2_role_id       : '0285645b-0d06-42a2-9968-67d544626300', // SO1 and SO2 VN
 
 	},
 	options: {
