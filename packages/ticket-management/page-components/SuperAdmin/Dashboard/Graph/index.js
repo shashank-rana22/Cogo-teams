88f8a34a840bc289c07fa9857aae6bb951b6b866
amandeep2508 <amandeep.chauhan@cogoport.com>
2023-05-26 13:03:09 +0530

    graph fi

--- packages/ticket-management/page-components/SuperAdmin/Dashboard/Graph/index.js
+++ packages/ticket-management/page-components/SuperAdmin/Dashboard/Graph/index.js
@@ -1,12 +1,10 @@
 import FeedbackGraph from './FeedbackGraph';
 import styles from './styles.module.css';
-import TicketGraph from './TicketGraph';
 
 function Graph({ customerSatisfactionStats }) {
 	return (
 		<div className={styles.container}>
 			<FeedbackGraph customerSatisfactionStats={customerSatisfactionStats} />
-			<TicketGraph />
 		</div>
 
 	);
