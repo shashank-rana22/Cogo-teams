d19cf2d1384256f9df214dbbdd441e0b5bfe4d4b
amandeep2508 <amandeep.chauhan@cogoport.com>
2023-05-26 12:01:04 +0530

    fix

--- packages/ticket-management/page-components/SuperAdmin/Dashboard/index.js
+++ packages/ticket-management/page-components/SuperAdmin/Dashboard/index.js
@@ -1,14 +1,25 @@
+import useGetDashboard from '../../../hooks/useGetDashboard';
+
 import Graph from './Graph';
 import OverallStats from './OverallStats';
 import styles from './styles.module.css';
 import UserStatistics from './UserStatistics';
 
 function Dashboard() {
+	const { data } = useGetDashboard();
+
+	const {
+		CustomerSatisfactionStats, TicketCount, TopCategory, TopCustomer,
+	} = data || {};
+
 	return (
 		<div className={styles.container}>
-			<OverallStats />
-			<UserStatistics />
-			<Graph />
+			<OverallStats
+				ticketCount={TicketCount}
+				data={data}
+			/>
+			<UserStatistics topCustomer={TopCustomer} topCategory={TopCategory} />
+			<Graph customerSatisfactionStats={CustomerSatisfactionStats} />
 		</div>
 	);
 }
