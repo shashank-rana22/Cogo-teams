aa32248f7bed64ee42a154f664fb8ba970b3d0f2
amandeep2508 <amandeep.chauhan@cogoport.com>
2023-05-26 11:56:53 +0530

    Widget

--- packages/ticket-management/page-components/SuperAdmin/Dashboard/UserStatistics/Widget/index.js
+++ packages/ticket-management/page-components/SuperAdmin/Dashboard/UserStatistics/Widget/index.js
@@ -1,24 +1,49 @@
 import styles from './styles.module.css';
 
-function Widget({ label = 'Top Users', subLabel = 'No of issues', data }) {
+function UserCard({ data }) {
+	return (
+		<div className={styles.list}>
+			{(data || []).map(({ UserId, Name, OrganizationName, Tickets }) => (
+				<div className={styles.card} key={UserId}>
+					<div className={styles.agent_details}>
+						<div className={styles.agent_name}>{Name}</div>
+						<div className={styles.agent_org}>{OrganizationName}</div>
+					</div>
+					<div className={styles.count}>{Tickets}</div>
+				</div>
+			))}
+		</div>
+	);
+}
+
+function CategoriesCard({ data }) {
+	return (
+		<div className={styles.list}>
+			{(data || []).map(({ TopTicketType, Type }) => (
+				<div className={styles.card} key={Type}>
+					<div className={styles.type}>{Type}</div>
+					<div className={styles.count}>{TopTicketType}</div>
+				</div>
+			))}
+		</div>
+	);
+}
+
+function Widget({ label = 'Top Users', subLabel = 'No of issues', data, type }) {
+	const cardComponentMapping = {
+		Users      : <UserCard data={data} />,
+		Categories : <CategoriesCard data={data} />,
+	};
+
+	const CardComponent = cardComponentMapping[type];
+
 	return (
 		<div className={styles.container}>
 			<div className={styles.header}>
 				<div className={styles.title}>{label}</div>
 				<div className={styles.title}>{subLabel}</div>
 			</div>
-			<div className={styles.list}>
-				{(data || []).map(({ UserId, Name, OrganizationName, Tickets }) => (
-					<div className={styles.card} key={UserId}>
-						<div className={styles.agent_details}>
-							<div className={styles.agent_name}>{Name}</div>
-							<div className={styles.agent_org}>{OrganizationName}</div>
-						</div>
-						<div className={styles.count}>{Tickets}</div>
-					</div>
-				))}
-			</div>
-
+			{CardComponent}
 		</div>
 
 	);
