1b86fe9885e4d45d7613c276c59d6868b926dcf0
Ankit Kaushal <ankit.kaushal@cogoport.com>
2023-05-30 15:42:48 +0530

    awb inventory initial commit

---
+++ packages/awb-inventory/page-components/Header/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './styles.module.css';
+
+function Header() {
+	return (
+		<header>
+			<div className={styles.heading}>AWB Inventory</div>
+		</header>
+	);
+}
+
+export default Header;
---
+++ packages/awb-inventory/page-components/index.tsx
@@ -0,0 +1,12 @@
+import Header from './Header';
+import styles from './styles.module.css';
+
+function AwbInventory() {
+	return (
+		<div className={styles.container}>
+			<Header />
+		</div>
+	);
+}
+
+export default AwbInventory;
