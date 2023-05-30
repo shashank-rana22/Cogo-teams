fee0e13a4a7eacfafe2a6aec80d678005b9ee084
amandeep2508 <amandeep.chauhan@cogoport.com>
2023-05-30 14:06:25 +0530

    component fix

--- packages/ticket-management/page-components/SuperAdmin/Dashboard/Widget/index.js
+++ packages/ticket-management/page-components/SuperAdmin/Dashboard/Widget/index.js
@@ -1,66 +1,10 @@
 import { cl } from '@cogoport/components';
 import { isEmpty } from '@cogoport/utils';
 
-import EmptyTicket from '../../../../common/EmptyTicket';
-import UserCardLoader from '../../../../common/UserCardLoader';
-
+import CategoriesCard from './CategoriesCard';
+import PerformanceCard from './PerformanceCard';
 import styles from './styles.module.css';
-
-function UserCard({ data, loading }) {
-	if (loading) return <UserCardLoader />;
-
-	if (isEmpty(data)) return <EmptyTicket emptyText="No User Data" />;
-
-	return (
-		<div className={styles.list}>
-			{(data || []).map(({ UserId, Name, OrganizationName, Tickets }) => (
-				<div className={styles.card} key={UserId}>
-					<div className={styles.agent_details}>
-						<div className={styles.agent_name}>{Name}</div>
-						<div className={styles.agent_org}>{OrganizationName}</div>
-					</div>
-					<div className={styles.count}>{Tickets}</div>
-				</div>
-			))}
-		</div>
-	);
-}
-
-function CategoriesCard({ data, loading }) {
-	if (loading) return <UserCardLoader />;
-	if (isEmpty(data)) return <EmptyTicket emptyText="No Categories Found" />;
-
-	return (
-		<div className={styles.list}>
-			{(data || []).map(({ TopTicketType, Type }) => (
-				<div className={styles.card} key={Type}>
-					<div className={styles.type}>{Type}</div>
-					<div className={styles.count}>{TopTicketType}</div>
-				</div>
-			))}
-		</div>
-	);
-}
-
-function PerformanceCard({ data, loading }) {
-	if (loading) return <UserCardLoader />;
-
-	if (isEmpty(data)) return <EmptyTicket emptyText="No Agents Found" />;
-
-	return (
-		<div className={styles.list}>
-			{(data || []).map(({ AgentName, PerformanceRating }) => (
-				<div className={styles.card} key={PerformanceRating}>
-					<div className={styles.type}>{AgentName}</div>
-					<div className={styles.count}>
-						{PerformanceRating.toFixed(2)}
-						%
-					</div>
-				</div>
-			))}
-		</div>
-	);
-}
+import UserCard from './UserCard';
 
 function Widget({ label = 'Top Users', subLabel = 'No of issues', data, type, loading, isMargin }) {
 	const cardComponentMapping = {
