73a441316c99b1fc749a87a6f7161d5aeda50906
ankit-rajpoot <ankit.rajpoot@cogoport.com>
2023-05-30 16:30:13 +0530

    edit service done

--- packages/shipment-ftl/page-components/ShipmentDetails/index.js
+++ packages/shipment-ftl/page-components/ShipmentDetails/index.js
@@ -22,6 +22,7 @@ import useServiceList from '../../hooks/useServiceList';
 import styles from './styles.module.css';
 
 function ShipmentDetails() {
+	const activeStakeholder = 'superadmin';
 	const router = useRouter();
 	const { get } = useGetShipment();
 
@@ -41,7 +42,8 @@ function ShipmentDetails() {
 		...get,
 		...getTimeline,
 		...servicesGet,
-	}), [get, servicesGet, getTimeline]);
+		activeStakeholder,
+	}), [get, servicesGet, getTimeline, activeStakeholder]);
 
 	useEffect(() => {
 		router.prefetch(router.asPath);
