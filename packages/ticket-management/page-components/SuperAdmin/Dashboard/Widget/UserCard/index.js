570f236b14a7bca7610e35be821d9b03589ac528
amandeep2508 <amandeep.chauhan@cogoport.com>
2023-05-30 14:07:31 +0530

    added usercard component fix

---
+++ packages/ticket-management/page-components/SuperAdmin/Dashboard/Widget/UserCard/index.js
@@ -0,0 +1,28 @@
+import { isEmpty } from '@cogoport/utils';
+
+import EmptyTicket from '../../../../../common/EmptyTicket';
+import UserCardLoader from '../../../../../common/UserCardLoader';
+
+import styles from './styles.module.css';
+
+function UserCard({ data, loading }) {
+	if (loading) return <UserCardLoader />;
+
+	if (isEmpty(data)) return <EmptyTicket emptyText="No User Data" />;
+
+	return (
+		<div className={styles.list}>
+			{(data || []).map(({ UserId, Name, OrganizationName, Tickets }) => (
+				<div className={styles.card} key={UserId}>
+					<div>
+						<div className={styles.agent_name}>{Name}</div>
+						<div className={styles.agent_org}>{OrganizationName}</div>
+					</div>
+					<div className={styles.count}>{Tickets}</div>
+				</div>
+			))}
+		</div>
+	);
+}
+
+export default UserCard;
