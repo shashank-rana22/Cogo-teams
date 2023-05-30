03c824a9ed93f8675bcb399a6de032f8ca9deb9b
Kanchan Bhandari <85485325+kanchan-13@users.noreply.github.com>
2023-05-30 11:21:20 +0530

    Update index.js

--- packages/shipment-fcl-freight/common/SalesInvoice/Invoices/InvoiceItem/Item/Header/Actions/ChangePaymentMode/index.js
+++ packages/shipment-fcl-freight/common/SalesInvoice/Invoices/InvoiceItem/Item/Header/Actions/ChangePaymentMode/index.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 
 import useUpdateInvoicePaymentMode from '../../../../../../../../hooks/useUpdateInvoicePaymentMode';
 
-const optionsToShow = {
+const OPTIONS_TO_SHOW = {
 	credit : { label: 'Cash', value: 'cash' },
 	cash   : { label: 'Deferred Payment', value: 'credit' },
 };
@@ -17,9 +17,9 @@ function ChangePaymentMode({
 	const [check, setCheck] = useState(!invoice?.paymentMode);
 	const [value, setValue] = useState('');
 
-	const options = [optionsToShow.cash, optionsToShow.credit];
+	const options = [OPTIONS_TO_SHOW.cash, OPTIONS_TO_SHOW.credit];
 
-	const paymentMode = invoice?.payment_mode ? optionsToShow[invoice?.payment_mode]?.value : value;
+	const paymentMode = invoice?.payment_mode ? OPTIONS_TO_SHOW[invoice?.payment_mode]?.value : value;
 
 	const payload = { id: invoice?.id, payment_mode: paymentMode };
 
@@ -37,7 +37,7 @@ function ChangePaymentMode({
 			<Modal.Body>
 				{invoice?.payment_mode ? (
 					<Radio
-						label={optionsToShow[invoice?.payment_mode]?.label}
+						label={OPTIONS_TO_SHOW[invoice?.payment_mode]?.label}
 						checked={check}
 						onChange={() => setCheck(!check)}
 					/>
