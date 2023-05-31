af72663c5a2031dc80385e1209f1e4e4cfecf52c
CogoportNew <cogoportnew@Narendras-MacBook-Air.local>
2023-05-31 12:42:43 +0530

    made a hook

--- packages/rfq-dashboard/page-components/Details/Services/PortsCard/index.js
+++ packages/rfq-dashboard/page-components/Details/Services/PortsCard/index.js
@@ -1,11 +1,8 @@
-import { IcCFcl, IcMPortArrow, IcMArrowRotateDown, IcMArrowRotateUp, IcCLcl, IcCAir } from '@cogoport/icons-react';
+import { IcMPortArrow, IcMArrowRotateDown, IcMArrowRotateUp } from '@cogoport/icons-react';
 import { isEmpty } from '@cogoport/utils';
-import { useState, useEffect } from 'react';
 
 import ServiceStats from '../../../../common/ServiceStats';
-import { priceBreakupChildData } from '../../../../configurations/price-breakup-card-child-data';
-import useGetRfqRateCardDetails from '../../../../hooks/useGetRfqRateCardDetails';
-import useUpdateRfqRateMargin from '../../../../hooks/useUpdateRfqRateMargin';
+import useGetPortCard from '../../../../hooks/useGetPortCard';
 import BreakdownDetails from '../BreakdownDetails';
 
 import BreakdownLoading from './BreakdownLoading';
@@ -15,87 +12,17 @@ import LocationDetails from './LocationDetails';
 import PriceFreightCtr from './PriceFrieghtCtr';
 import styles from './styles.module.css';
 
-const COMMODITY_MAPPING = ['cargo_weight_per_container', 'commodity',
-	'container_size', 'container_type', 'containers_count'];
-
-const ICONMAPPING = {
-	fcl_freight : IcCFcl,
-	lcl_freight : IcCLcl,
-	air_freight : IcCAir,
-};
-
-const TEXTMAPPING = {
-	fcl_freight : 'FCL',
-	lcl_freight : 'LCL',
-	air_freight : 'AIR',
-};
-
 function PortsCard(props) {
-	const { data = {}, loading, refetchRateCards, getRfqsForApproval } = props;
-
-	const [showPrice, setShowPrice] = useState({});
 	const {
-		detail = {}, freight_price_currency = '', freight_price_discounted = '',
-		total_price_discounted = '', id = '', stats = {},
-	} = data;
+		loading, Component, origin_port, TEXTMAPPING,
+		service_type, destination_port, commodity_array,
+		stats, freight_price_currency, freight_price_discounted,
+		total_price_discounted, showPrice, setShowPrice, id, rfq_card_loading, rate_card_details_data,
+		rate, rate_card_details, currency_conversion, editedMargins, setEditedMargins, primaryService,
+		convenienceDetails, setConvenienceDetails, updateMargin, refetchRateCards, getRfqsForApproval,
+		margin_limit,
+	} = useGetPortCard({ props });
 
-	const {
-		origin_port = {}, destination_port = {}, service_type,
-	} = detail;
-
-	const commodity_array = [];
-
-	COMMODITY_MAPPING.forEach((commodity) => commodity_array.push({ [commodity]: detail[commodity] }));
-
-	const {
-		getRfqRateCardDetails, rfq_card_loading,
-		rate_card_details_data,
-	} = useGetRfqRateCardDetails();
-
-	useEffect(() => {
-		if (!isEmpty(showPrice)) {
-			getRfqRateCardDetails({ rfq_rate_card_id: showPrice?.rfq_rate_card_id });
-		}
-	}, [getRfqRateCardDetails, showPrice]);
-
-	const {
-		rate = {}, detail: rate_card_details = {}, currency_conversion = {}, margin_limit = {},
-	} = rate_card_details_data || {};
-
-	const convenience_line_item = rate?.booking_charges?.convenience_rate?.line_items[0];
-
-	const [editedMargins, setEditedMargins] = useState({});
-
-	const [convenienceDetails, setConvenienceDetails] = useState({
-		convenience_rate: {
-			price    : convenience_line_item?.price,
-			currency : convenience_line_item?.currency,
-			unit     : convenience_line_item?.unit,
-		},
-	});
-
-	const primary_service_id = rate_card_details?.primary_service_id;
-	const primaryService = {
-		...(rate_card_details?.service_details?.[primary_service_id] || {}),
-		rate: rate?.service_rates?.[primary_service_id] || {},
-	};
-
-	const { updateMargin } = useUpdateRfqRateMargin({
-		rate,
-		rate_card_details,
-	});
-
-	const prefilledValues = [];
-	priceBreakupChildData?.forEach((item) => {
-		item?.data.forEach((dataItem) => {
-			prefilledValues.push({
-				margin_type           : dataItem.margin_type,
-				margin_value_currency : dataItem.margin_value_currency,
-				margin_value          : dataItem.margin_value,
-			});
-		});
-	});
-	const Component = ICONMAPPING[service_type];
 	return (
 		<div className={styles.main_container}>
 			<div className={styles.port_container}>
