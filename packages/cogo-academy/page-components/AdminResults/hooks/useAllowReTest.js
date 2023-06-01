16af6ea364cb9700c77bdc6c996e86b77103bdb7
lalithphanindra <patibanda.phanindra@cogoport.com>
2023-06-01 15:13:56 +0530

    pr changes 3

--- packages/cogo-academy/page-components/AdminResults/hooks/useAllowReTest.js
+++ packages/cogo-academy/page-components/AdminResults/hooks/useAllowReTest.js
@@ -5,53 +5,54 @@ import { useRequest } from '@cogoport/request';
 
 function useAllowReTest({ setShowRetestModal = () => {}, test_id, refetchTest = () => {} }) {
 	const {
-		control, formState: { errors }, watch, setValue, handleSubmit, reset,
+		control,
+		formState: { errors },
+		watch,
+		setValue,
+		handleSubmit,
+		reset,
 	} = useForm();
 
-	const [{ loading = false }, trigger] = useRequest({
-		method : 'post',
-		url    : '/re_allow_test',
-	}, { manual: true });
+	const [{ loading = false }, trigger] = useRequest(
+		{
+			method : 'post',
+			url    : '/re_allow_test',
+		},
+		{ manual: true },
+	);
+
+	const getPayload = (values) => {
+		const payload = {
+			test_id,
+			validity_start         : values?.test_validity?.startDate,
+			validity_end           : values?.test_validity?.endDate,
+			is_percentile_editable : values?.is_percentile_editable === 'true',
+		};
+
+		if (values?.users_list === 'custom') {
+			payload.users_list = 'custom';
+			payload.percentile = values?.filtered_users.includes('percentile_checked') ? Number(values?.percentile)
+				: undefined;
+			payload.percentage = values?.filtered_users.includes('percentage_checked') ? Number(values?.percentage)
+				: undefined;
+			payload.not_appeared_included = values?.filtered_users.includes('not_appeared');
+		} else {
+			payload.users_list = 'all';
+		}
+
+		return payload;
+	};
 
 	const onSubmit = async (values) => {
-		if (values?.users_list === 'custom') {
-			try {
-				await trigger({
-					data: {
-						test_id,
-						users_list: 'custom',
-						percentile:
-						values?.filtered_users.includes('percentile_checked') ? Number(values?.percentile) : undefined,
-						percentage:
-						values?.filtered_users.includes('percentage_checked') ? Number(values?.percentage) : undefined,
-						validity_start         : values?.test_validity?.startDate,
-						validity_end           : values?.test_validity?.endDate,
-						is_percentile_editable : values?.is_percentile_editable === 'true',
-						not_appeared_included  : values?.filtered_users.includes('not_appeared'),
-					},
-
-				});
-			} catch (err) {
-				Toast.error(getApiErrorString(err.response?.data));
-			}
-		} else {
-			try {
-				await trigger({
-					data: {
-						test_id,
-						users_list             : 'all',
-						validity_start         : values?.test_validity?.startDate,
-						validity_end           : values?.test_validity?.endDate,
-						is_percentile_editable : values?.is_percentile_editable === 'true',
-					},
-				});
-			} catch (err) {
-				Toast.error(getApiErrorString(err.response?.data));
-			}
+		try {
+			const payload = getPayload(values);
+			await trigger({ data: payload });
+			refetchTest({ test_id });
+			setShowRetestModal(false);
+			reset();
+		} catch (err) {
+			Toast.error(getApiErrorString(err.response?.data));
 		}
-		refetchTest({ test_id });
-		setShowRetestModal(false);
-		reset();
 	};
 
 	return {
