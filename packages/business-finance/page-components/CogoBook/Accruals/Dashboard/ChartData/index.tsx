2a414ed6e28270bef2de5837be5a2723f05bb706
cogomanish <manish.kumar@cogoport.com>
2023-06-02 13:30:59 +0530

    add_dashboard

--- packages/business-finance/page-components/CogoBook/Accruals/Dashboard/ChartData/index.tsx
+++ packages/business-finance/page-components/CogoBook/Accruals/Dashboard/ChartData/index.tsx
@@ -1,6 +1,124 @@
+import { ResponsivePie } from '@cogoport/charts/pie';
+
 import styles from './styles.module.css';
 
+const colors = ['#57C6D1', '#ADCC6A'];
+
+const data = [
+	{
+		id    : 'Income Accrued',
+		label : 'Income Accrued',
+		value : 12,
+		color : '#57C6D1',
+	},
+	{
+		id    : 'Income Booked',
+		label : 'Income Booked',
+		value : 12,
+		color : '#ADCC6A',
+	},
+];
+
+const dataExpense = [
+	{
+		id    : 'Expense Accrued',
+		label : 'Expense Accrued',
+		value : 12,
+		color : '#57C6D1',
+	},
+	{
+		id    : 'Expense Booked',
+		label : 'Expense Booked',
+		value : 12,
+		color : '#ADCC6A',
+	},
+];
+
 function ChartData() {
-	return <div className={styles.container}>dfkl</div>;
+	function CenteredMetric({ centerX, centerY }) {
+		return (
+			<text
+				x={centerX}
+				y={centerY}
+				textAnchor="middle"
+				dominantBaseline="central"
+				style={{
+					fontSize   : '12px',
+					fontWeight : 600,
+
+				}}
+			>
+				Income
+			</text>
+
+		);
+	}
+	return (
+		<div className={styles.container}>
+			<div className={styles.income_pie_chart}>
+				<ResponsivePie
+					data={data}
+					margin={{ top: 0, right: 0, bottom: 25, left: 10 }}
+					innerRadius={0.7}
+					colors={colors}
+					padAngle={1}
+					enableArcLabels={false}
+					enableArcLinkLabels={false}
+					isInteractive
+					activeOuterRadiusOffset={4}
+					layers={['arcs', 'arcLabels', 'arcLinkLabels', 'legends', CenteredMetric]}
+				/>
+			</div>
+
+			<div className={styles.income_data}>
+				<div className={styles.booked_data}>
+					Booked Income
+					<div className={styles.amount_data}>
+						INR 60,30,00,000
+					</div>
+				</div>
+
+				<div>
+					Accrued Income
+					<div className={styles.amount_data}>
+						INR 60,30,00,000
+					</div>
+				</div>
+
+			</div>
+
+			<div className={styles.income_pie_chart}>
+				<ResponsivePie
+					data={dataExpense}
+					margin={{ top: 0, right: 0, bottom: 25, left: 10 }}
+					innerRadius={0.7}
+					colors={colors}
+					padAngle={1}
+					enableArcLabels={false}
+					enableArcLinkLabels={false}
+					isInteractive
+					activeOuterRadiusOffset={4}
+					layers={['arcs', 'arcLabels', 'arcLinkLabels', 'legends', CenteredMetric]}
+				/>
+			</div>
+
+			<div className={styles.income_data}>
+				<div className={styles.booked_data}>
+					Booked Expense
+					<div className={styles.amount_data}>
+						INR 60,30,00,000
+					</div>
+				</div>
+
+				<div>
+					Accrued Expense
+					<div className={styles.amount_data}>
+						INR 60,30,00,000
+					</div>
+				</div>
+
+			</div>
+		</div>
+	);
 }
 export default ChartData;
--- packages/business-finance/page-components/CogoBook/Accruals/Dashboard/index.tsx
+++ packages/business-finance/page-components/CogoBook/Accruals/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import { Tooltip } from '@cogoport/components';
+import { Popover, Tooltip } from '@cogoport/components';
 import { IcMInfo } from '@cogoport/icons-react';
 import { useState } from 'react';
 
@@ -9,34 +9,81 @@ import styles from './styles.module.css';
 
 function Dashboard() {
 	const [dashboardFilters, setDashboardFilters] = useState({});
+
+	const reportMonth = [
+		{ id: '1', days: '1 - 15 Days Left' },
+		{ id: '2', days: '15 - 30 Days Left' },
+		{ id: '3', days: '1 - 2 Month Left' },
+		{ id: '4', days: '2 - 3 Month Left' },
+	];
+
+	const renderDownloadReport = (
+		reportMonth.map((item) => (
+			<div key={item?.id} className={styles.days_show}>
+				{item?.days}
+			</div>
+		))
+	);
+
 	return (
 		<div>
 			<FilterHeader setDashboardFilters={setDashboardFilters} dashboardFilters={dashboardFilters} />
 			<div className={styles.statistics_card}>
 				<div className={styles.statistics}>
-					Shipment ID Statistics
-					<Tooltip
-						content={(
-							<div className={styles.font_size_tooltip}>
-								Please select the
-								<br />
-								accounting month
+					<div className={styles.sid_view}>
+						Shipment ID Statistics
+						<Tooltip
+							content={(
+								<div className={styles.font_size_tooltip}>
+									Please select the
+									<br />
+									accounting month
+								</div>
+							)}
+							placement="top"
+						>
+							<div className={styles.info_icon_container}>
+								<IcMInfo />
 							</div>
-						)}
-						placement="top"
-					>
-						<div className={styles.info_icon_container}>
-							<IcMInfo />
-						</div>
-					</Tooltip>
+						</Tooltip>
+					</div>
+
+					<div className={styles.report}> Download Report</div>
 				</div>
 				<div className={styles.hr_statistics} />
 
 				<div className={styles.stats_full_data}>
 					<StatsNumericData />
-					{' '}
 					<ChartData />
 				</div>
+
+			</div>
+
+			<div className={styles.sid_card}>
+				<div className={styles.statistics}>
+					<div className={styles.sid_view}>
+						3 Months SID View
+						<Tooltip
+							content={(
+								<div className={styles.font_size_tooltip}>
+									Please select the
+									<br />
+									accounting month
+								</div>
+							)}
+							placement="top"
+						>
+							<div className={styles.info_icon_container}>
+								<IcMInfo />
+							</div>
+						</Tooltip>
+					</div>
+					<Popover placement="bottom" caret={false} render={renderDownloadReport}>
+						<div className={styles.report}> Download Report</div>
+					</Popover>
+
+				</div>
+				<div className={styles.hr_statistics} />
 			</div>
 
 		</div>
