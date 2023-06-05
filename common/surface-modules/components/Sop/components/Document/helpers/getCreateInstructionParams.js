e370ae973a1e843114b7f8de49165b617d9b50f8
santkumar1799 <sant.kumar@cogoport.com>
2023-06-05 15:48:22 +0530

    add sop new

--- common/surface-modules/components/Sop/components/Document/helpers/getCreateInstructionParams.js
+++
@@ -1,6 +0,0 @@
-const getCreateInstructionParams = ({ formValues }) => {
-	const params = { instruction: 'document_handling_preference' };
-	params.sop_instructions = [formValues];
-	return params;
-};
-export default getCreateInstructionParams;
