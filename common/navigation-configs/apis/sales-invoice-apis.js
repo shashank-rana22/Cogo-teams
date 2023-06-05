fbc84331bbc17e8232bf3140dec1e5a48ff5c5cf
shivani-gupta19 <shivani.gupta@cogoport.com>
2023-06-05 12:00:49 +0530

    fixes

--- common/navigation-configs/apis/sales-invoice-apis.js
+++ common/navigation-configs/apis/sales-invoice-apis.js
@@ -1,11 +1,5 @@
 const apis = [
 	{
-		api          : 'get_invoices',
-		access_type  : 'private',
-		service_name : 'bookings',
-		feature      : 'sales_invoice',
-	},
-	{
 		api          : 'update_invoice_currency',
 		access_type  : 'private',
 		service_name : 'bookings',
