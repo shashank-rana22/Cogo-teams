83a792a0bdde4ecd15e0ef398b454292b6e34791
piyushi-cogo <piyushi.garg@cogoport.com>
2023-06-05 17:56:50 +0530

    mark confirm services

--- packages/shipment-fcl-custom/common/Tasks/TaskExecution/ExecuteTask.js
+++ packages/shipment-fcl-custom/common/Tasks/TaskExecution/ExecuteTask.js
@@ -1,5 +1,9 @@
+import { ShipmentDetailContext } from '@cogoport/context';
+import { useContext } from 'react';
+
 import useGetTaskConfig from '../../../hooks/useGetTaskConfig';
 
+import { MarkConfirmServices } from './CustomTasks';
 import ExecuteStep from './ExecuteStep';
 import useTaskExecution from './helpers/useTaskExecution';
 
@@ -18,12 +22,30 @@ function ExecuteTask({
 		serviceIdMapping = [],
 	} = useTaskExecution({ task, taskConfigData });
 
+	const { servicesList, shipment_data, primary_service } = useContext(ShipmentDetailContext);
+
 	const stepConfigValue = steps.length
 		? steps[currentStep] || steps[steps.length - 1]
 		: {};
 
 	if (loading) {
 		return <div>Loading...</div>;
+	}
+
+	if (
+		task.service_type
+		&& task.task === 'mark_confirmed'
+	) {
+		return (
+			<MarkConfirmServices
+				task={task}
+				onCancel={onCancel}
+				taskListRefetch={taskListRefetch}
+				primaryService={primary_service}
+				shipment_data={shipment_data}
+				servicesList={servicesList}
+			/>
+		);
 	}
 
 	return (
