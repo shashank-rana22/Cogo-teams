8cdfb8b40f01403bc18595deab63d03e7c733ea3
piyushi-cogo <piyushi.garg@cogoport.com>
2023-06-05 18:32:59 +0530

    fixes

--- packages/shipment-fcl-custom/common/EditCancelService/utils/getCanEditSupplier.js
+++ packages/shipment-fcl-custom/common/EditCancelService/utils/getCanEditSupplier.js
@@ -1,6 +1,5 @@
 const EDIT_SUPPLIER_SERVICE_STATES = ['init',
-	'awaiting_service_provider_confirmation',
-	'confirmed_by_service_provider'];
+	'awaiting_service_provider_confirmation', 'confirmed_by_service_provider'];
 
 const SERVICE_COMPLETED_OR_CANCELLED = ['completed', 'cancelled'];
 
@@ -9,12 +8,11 @@ export default function getCanEditSupplier({ shipment_data, user_data, state, st
 		return true;
 	}
 
-	const userCanCancel = !!stakeholderConfig.edit_supplier;
+	const isStakeholderAllowed = !!stakeholderConfig.edit_supplier;
 
 	const serviceInEditSupplierState = EDIT_SUPPLIER_SERVICE_STATES?.includes(state);
 
-	const oldShipmentCancellable = shipment_data?.serial_id <= 120347
- && !SERVICE_COMPLETED_OR_CANCELLED.includes(state);
+	const oldShipmentEditable = shipment_data?.serial_id <= 120347 && !SERVICE_COMPLETED_OR_CANCELLED.includes(state);
 
-	return userCanCancel && (serviceInEditSupplierState || oldShipmentCancellable);
+	return isStakeholderAllowed && (serviceInEditSupplierState || oldShipmentEditable);
 }
--- packages/shipment-fcl-freight-local/common/ShipmentHeader/utils/getCanCancelShipment.js
+++ packages/shipment-fcl-freight-local/common/ShipmentHeader/utils/getCanCancelShipment.js
@@ -4,24 +4,13 @@ const SHIPMENT_CANCELLATION_STATES = [
 	'in_progress',
 ];
 
-const SHIPMENT_CANCEL_STAKEHOLDERS = [
-	'superadmin',
-	'admin',
-	'booking_agent',
-	'sales_agent',
-	'booking_desk',
-	'booking_desk_manager',
-	'so1_so2_ops',
-];
-
-export default function getCanCancelShipment({ shipment_data, user_data, activeStakeholder }) {
+export default function getCanCancelShipment({ shipment_data, user_data, stakeholderConfig }) {
+	const { cancel_shipment : { can_cancel = false } = {} } = stakeholderConfig || {};
 	const { state } = shipment_data || {};
 
 	const isShipmentInCancellationState = SHIPMENT_CANCELLATION_STATES.includes(state);
 
-	const isStakeholderAllowed = SHIPMENT_CANCEL_STAKEHOLDERS.includes(activeStakeholder);
-
 	const allowedEmail = user_data?.email === 'ajeet@cogoport.com';
 
-	return isShipmentInCancellationState && (isStakeholderAllowed || allowedEmail);
+	return isShipmentInCancellationState && (can_cancel || allowedEmail);
 }
