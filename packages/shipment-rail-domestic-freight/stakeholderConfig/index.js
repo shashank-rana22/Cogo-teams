75f7b2a989254fb1e135aea4fc28ebf25f044fff
Rajatuba-Cogoport <rajat.uba@cogoport.com>
2023-06-06 18:48:12 +0530

    admin

--- packages/shipment-rail-domestic-freight/stakeholderConfig/index.js
+++ packages/shipment-rail-domestic-freight/stakeholderConfig/index.js
@@ -1,9 +1,11 @@
+import adminConfig from './admin.json';
 import bookingAgent from './bookingAgent.json';
 import superadminConfig from './superadmin.json';
 
 const CONFIG_MAPPING = {
 	superadmin    : superadminConfig,
 	booking_agent : bookingAgent,
+	admin         : adminConfig,
 };
 
 const stakeholderConfig = ({ stakeholder }) => CONFIG_MAPPING[stakeholder] || {};
