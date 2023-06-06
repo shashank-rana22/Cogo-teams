2c441300a7a83ff7ab3ac3a381a7d5544191742a
shivani-gupta19 <shivani.gupta@cogoport.com>
2023-06-06 16:31:54 +0530

    fixes

--- packages/shipment-fcl-freight-local/hooks/useGetCreditNotes.js
+++ packages/shipment-fcl-freight-local/hooks/useGetCreditNotes.js
@@ -1,30 +1,24 @@
 import { ShipmentDetailContext } from '@cogoport/context';
 import toastApiError from '@cogoport/ocean-modules/utils/toastApiError';
 import { useRequest } from '@cogoport/request';
-import { useEffect, useContext, useCallback, useState } from 'react';
+import { useEffect, useContext, useCallback } from 'react';
 
 const useGetCreditNotes = () => {
 	const { shipment_data } = useContext(ShipmentDetailContext);
-	const [apiData, setApiData] = useState({});
-
 	const { id: shipment_id = '' } = shipment_data || {};
 
-	const [{ loading }, trigger] = useRequest({
+	const [{ loading, data }, trigger] = useRequest({
 		url    : 'fcl_local/get_credit_notes',
 		method : 'GET',
 		params : {
-			filters: {
-				shipment_id,
-				is_active                 : true,
-				combination_data_required : true,
-			},
+			shipment_id,
+			additional_methods: ['services'],
 		},
 	}, { manual: true });
 
 	const getCreditNoteList = useCallback(async () => {
 		try {
-			const res = await trigger();
-			setApiData(res.data || {});
+			await trigger();
 		} catch (err) {
 			toastApiError(err);
 		}
@@ -36,7 +30,7 @@ const useGetCreditNotes = () => {
 
 	return {
 		loading,
-		list      : apiData?.list || [],
+		list      : data?.list || [],
 		cnRefetch : getCreditNoteList,
 	};
 };
