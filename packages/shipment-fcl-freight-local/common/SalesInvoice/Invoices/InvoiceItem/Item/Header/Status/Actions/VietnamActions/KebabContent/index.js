69535ecc3834bc4d003312d282008244ee40c199
shivani-gupta19 <shivani.gupta@cogoport.com>
2023-06-06 15:47:27 +0530

    checks for edit invoice

--- packages/shipment-fcl-freight-local/common/SalesInvoice/Invoices/InvoiceItem/Item/Header/Status/Actions/VietnamActions/KebabContent/index.js
+++ packages/shipment-fcl-freight-local/common/SalesInvoice/Invoices/InvoiceItem/Item/Header/Status/Actions/VietnamActions/KebabContent/index.js
@@ -4,6 +4,7 @@ import {
 	IcMOverflowDot,
 	IcMInfo,
 } from '@cogoport/icons-react';
+import { useSelector } from '@cogoport/store';
 import { isEmpty } from '@cogoport/utils';
 import React, { useState } from 'react';
 
@@ -19,6 +20,7 @@ function Actions({
 	setShowChangePaymentMode = () => {},
 	setIsEditInvoice = () => {},
 }) {
+	const user_data = useSelector(({ profile }) => profile || {});
 	const [show, setShow] = useState(false);
 	const showForOldShipments =	shipment_data.serial_id <= GLOBAL_CONSTANTS.invoice_check_id
 	&& invoice.status === 'pending';
@@ -48,19 +50,26 @@ function Actions({
 
 	const commonActions = invoice.status !== 'approved' && !disableAction;
 
+	const editInvoicesVisiblity = shipment_data?.is_cogo_assured !== true
+	|| user_data?.user?.id === GLOBAL_CONSTANTS.ajeet_singh_user_id;
+
 	const content = (
 		<div className={styles.dialog_box}>
 			{commonActions ? (
 				<>
-					<div
-						role="button"
-						tabIndex={0}
-						className={styles.text}
-						onClick={() => handleClick(setIsEditInvoice)}
-					>
-						Edit Invoice
-					</div>
-					<div className={styles.line} />
+					{editInvoicesVisiblity ? (
+						<div>
+							<div
+								role="button"
+								tabIndex={0}
+								className={styles.text}
+								onClick={() => handleClick(setIsEditInvoice)}
+							>
+								Edit Invoice
+							</div>
+							<div className={styles.line} />
+						</div>
+					) : null}
 					<div>
 						<div
 							role="button"
