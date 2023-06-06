83e9fdfd10efd91b6285ecff8f046419d08cc674
mayank-1546 <mayank.gangwar@cogoport.com>
2023-06-06 11:08:42 +0530

    eslint changes

--- packages/kam-desk/page-components/SURFACE/Card/index.js
+++ packages/kam-desk/page-components/SURFACE/Card/index.js
@@ -1,6 +1,8 @@
+import { Tooltip } from '@cogoport/components';
+import { IcMInfo } from '@cogoport/icons-react';
 import { useRouter } from '@cogoport/next';
 import { useContext } from 'react';
-import { Tooltip } from '@cogoport/components'
+
 import {
 	BasicDetails,
 	AssignedStakeholder,
@@ -11,7 +13,6 @@ import {
 	SingleLocation,
 	Accordian,
 } from '../../../common/ShipmentCard';
-import { IcMInfo } from '@cogoport/icons-react'
 import KamDeskContext from '../../../context/KamDeskContext';
 import isSingleLocation from '../../../utils/checkSingleLocation';
 
@@ -66,15 +67,24 @@ function Card({ data = {}, activeTab = '' }) {
 
 					<div className={styles.pill_container}>
 						<CargoPills data={data} />
-						{data?.fm_rejection_reason && <Tooltip content={<div className={styles.rejection_tooltip}>{data?.fm_rejection_reason}</div>} placement="top">
-						<div style={{ marginBlock: 'auto' }}>
-							<IcMInfo
-								width={15}
-								height={15}
-								style={{ marginBottom: '2px', color: 'red' }}
-							/>
-						</div>
-					</Tooltip>}
+						{data?.fm_rejection_reason && (
+							<Tooltip
+								content={(
+									<div className={styles.rejection_tooltip}>
+										{data?.fm_rejection_reason}
+									</div>
+								)}
+								placement="top"
+							>
+								<div style={{ marginBlock: 'auto' }}>
+									<IcMInfo
+										width={15}
+										height={15}
+										style={{ marginBottom: '2px', color: 'red' }}
+									/>
+								</div>
+							</Tooltip>
+						)}
 					</div>
 				</div>
 			</div>
