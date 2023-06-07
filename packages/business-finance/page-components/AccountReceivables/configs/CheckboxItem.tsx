d71abeb8e2ad68fc30cec3557db5e865c1e4fd97
nagendra.singh@cogoport.com <nagendra.singh@cogoport.com>
2023-06-07 14:08:45 +0530

    feat: handleChange

--- packages/business-finance/page-components/AccountReceivables/configs/CheckboxItem.tsx
+++ packages/business-finance/page-components/AccountReceivables/configs/CheckboxItem.tsx
@@ -7,23 +7,25 @@ function CheckboxItem({
 	row,
 }) {
 	const { id, invoiceStatus } = row || {};
+	const handleChange = () => {
+		if ((checkedRows || []).includes(id)) {
+			const index = (checkedRows || []).indexOf(id);
+			if (index > -1) { // only splice array when item is found
+				checkedRows.splice(index, 1);
+				setCheckedRows([...checkedRows]);
+			}
+		} else {
+			setCheckedRows((prev:string[]) => [...prev,
+				id]);
+		}
+	};
+
 	return (
 		<div>
 			<Checkbox
 				disabled={!IRN_GENERATEABLE_STATUSES.includes(invoiceStatus)}
 				checked={(checkedRows || []).includes(id)}
-				onChange={() => {
-					if ((checkedRows || []).includes(id)) {
-						const index = (checkedRows || []).indexOf(id);
-						if (index > -1) { // only splice array when item is found
-							checkedRows.splice(index, 1);
-							setCheckedRows([...checkedRows]);
-						}
-					} else {
-						setCheckedRows((prev:string[]) => [...prev,
-							id]);
-					}
-				}}
+				onChange={handleChange}
 			/>
 		</div>
 	);
--- packages/business-finance/page-components/AccountReceivables/configs/HeaderCheckbox.tsx
+++ packages/business-finance/page-components/AccountReceivables/configs/HeaderCheckbox.tsx
@@ -15,24 +15,26 @@ function HeaderCheckbox({
 	IRN_GENERATEABLE_STATUSES,
 	setCheckedRows,
 }:Props) {
+	const handleChange = (e) => {
+		if (e?.target?.checked) {
+			setIsHeaderChecked(true);
+			const totalIds = totalRows?.map((row:{ id?:string | number, invoiceStatus?:string }) => {
+				if (IRN_GENERATEABLE_STATUSES.includes(row?.invoiceStatus)) { return row?.id; }
+				return null;
+			});
+			const filteredIds = totalIds?.filter((id) => id !== null);
+			setCheckedRows([...filteredIds]);
+		} else {
+			setIsHeaderChecked(false);
+			setCheckedRows([]);
+		}
+	};
+
 	return (
 		<div>
 			<Checkbox
 				checked={isHeaderChecked}
-				onChange={(e) => {
-					if (e?.target?.checked) {
-						setIsHeaderChecked(true);
-						const totalIds = totalRows?.map((row:{ id?:string | number, invoiceStatus?:string }) => {
-							if (IRN_GENERATEABLE_STATUSES.includes(row?.invoiceStatus)) { return row?.id; }
-							return null;
-						});
-						const filteredIds = totalIds?.filter((id) => id !== null);
-						setCheckedRows([...filteredIds]);
-					} else {
-						setIsHeaderChecked(false);
-						setCheckedRows([]);
-					}
-				}}
+				onChange={handleChange}
 			/>
 		</div>
 	);
