c121de8f50704f2e7cf39a9e102c5c035a927a21
Lachiramnaik1330 <malothu.lachiramnaik@cogoport.com>
2023-06-08 15:09:11 +0530

    lint fixes

--- packages/referral/configurations/pie-chart-mapping.js
+++ packages/referral/configurations/pie-chart-mapping.js
@@ -1,38 +1,32 @@
-import getFormatedChartData from '../utils/getFormatedChartData';
-import { handleValues } from '../utils/handleValue';
+import getFormatedChartData from "../utils/getFormatedChartData";
+import { handleValues } from "../utils/handleValue";
 
-const pieChartMapping = (
-	alloted = {},
+const pieChartMapping = (alloted = {}) => {
+  const chartCount = () => {
+    let totalCount = 0;
+    Object.values(alloted).forEach((value) => {
+      totalCount += value;
+    });
+    return totalCount;
+  };
 
-) => {
-	const chartCount = () => {
-		let totalCount = 0;
-		Object.values(alloted).forEach((value) => {
-			totalCount += value;
-		});
-		return totalCount;
-	};
+  const totalAllotedCogoppoints = chartCount();
 
-	const totalAllotedCogoppoints = chartCount();
+  const { allottedData = [] } = getFormatedChartData({
+    alloted,
+  });
 
-	const {
-		allottedData = [],
-	} = getFormatedChartData({
-		alloted,
-});
+  const detailsPieChart = [
+    {
+      title: "Allotted",
+      data: allottedData,
+      total_count: handleValues(totalAllotedCogoppoints),
+    },
+  ];
 
-	const detailsPieChart = [
-		{
-			title       : 'Allotted',
-			data        : allottedData,
-			total_count : handleValues(totalAllotedCogoppoints),
-		},
-
-	];
-
-	return {
-		detailsPieChart,
-	};
+  return {
+    detailsPieChart,
+  };
 };
 
 export default pieChartMapping;
--- packages/referral/utils/getFormatedChartData.js
+++ packages/referral/utils/getFormatedChartData.js
@@ -1,65 +1,68 @@
-
 const DEFAULT_COUNT = 0;
 
-const getFormatedChartData = ({
-	userData = {},
-	alloted = {},
+const getFormatedChartData = ({ userData = {}, alloted = {} }) => {
+  const {
+    kyc_verified: allotedKyc = DEFAULT_COUNT,
+    shipment = DEFAULT_COUNT,
+    subscription = DEFAULT_COUNT,
+  } = alloted;
 
-}) => {
-	const { kyc_verified: allotedKyc = DEFAULT_COUNT, shipment = DEFAULT_COUNT, subscription = DEFAULT_COUNT } = alloted;
+  const {
+    affiliate = DEFAULT_COUNT,
+    invited = DEFAULT_COUNT,
+    kyc_verified = DEFAULT_COUNT,
+    signed_up = DEFAULT_COUNT,
+  } = userData;
 
-	const { affiliate = DEFAULT_COUNT, invited = DEFAULT_COUNT, kyc_verified = DEFAULT_COUNT, signed_up = DEFAULT_COUNT} = userData;
+  const allottedData = [
+    {
+      id: "subscription",
+      value: subscription,
+      color: "#FCDC00",
+    },
+    {
+      id: "kyc",
+      value: allotedKyc,
+      color: "#ABCD62",
+    },
+    {
+      id: "shipment",
+      value: shipment,
+      color: "#88CAD1",
+    },
+  ];
 
-	const allottedData = [
-		{
-			id    : 'subscription',
-			value : subscription,
-			color : '#FCDC00',
-		},
-		{
-			id    : 'kyc',
-			value : allotedKyc,
-			color : '#ABCD62',
-		},
-		{
-			id    : 'shipment',
-			value : shipment,
-			color : '#88CAD1',
-		},
-	];
+  const usersData = [
+    {
+      id: "invited_user",
+      label: "Invited User",
+      value: invited,
+      color: "#7278AD",
+    },
+    {
+      id: "signed_up_users",
+      label: "Signed Up Users",
+      value: signed_up,
+      color: "#888FD1",
+    },
+    {
+      id: "kyc_registered_users",
+      label: "KYC Registered Users",
+      value: kyc_verified,
+      color: "#ABB0DE",
+    },
+    {
+      id: "affiliate_users",
+      label: "Affiliate Users",
+      value: affiliate,
+      color: "#CED1ED",
+    },
+  ];
 
-	const usersData = [
-		{
-			id    : 'invited_user',
-			label : 'Invited User',
-			value : invited,
-			color : '#7278AD',
-		},
-		{
-			id    : 'signed_up_users',
-			label : 'Signed Up Users',
-			value : signed_up,
-			color : '#888FD1',
-		},
-		{
-			id    : 'kyc_registered_users',
-			label : 'KYC Registered Users',
-			value : kyc_verified,
-			color : '#ABB0DE',
-		},
-		{
-			id    : 'affiliate_users',
-			label : 'Affiliate Users',
-			value : affiliate,
-			color : '#CED1ED',
-		},
-
-	];
-
-	return {
-		allottedData,
-		usersData,
-	};
+  return {
+    allottedData,
+    usersData,
+  };
 };
 
 export default getFormatedChartData;
