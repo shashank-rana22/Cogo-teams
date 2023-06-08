e76b5017f9085360f37c8435cab966cb1ee191a7
Shivam Singh <shivam.singh@cogoport.com>
2023-06-08 16:48:02 +0530

    service name

--- common/navigation-configs/apis/chro.js
+++ common/navigation-configs/apis/chro.js
@@ -2,13 +2,13 @@ const chro = [
 	{
 		api          : 'list_employee_offer_letters',
 		access_type  : 'private',
-		service_name : 'harbour',
+		service_name : 'hrms',
 	},
 
 	{
 		api          : 'update_employee_offer_letter',
 		access_type  : 'private',
-		service_name : 'harbour',
+		service_name : 'hrms',
 	},
 ];
 
