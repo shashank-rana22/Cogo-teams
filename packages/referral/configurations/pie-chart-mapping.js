e9600c4e7cc481123f1590d2404adb2f4018b645
Lachiramnaik1330 <malothu.lachiramnaik@cogoport.com>
2023-06-08 15:26:40 +0530

    lint fixes

--- packages/referral/configurations/pie-chart-mapping.js
+++ packages/referral/configurations/pie-chart-mapping.js
@@ -2,31 +2,31 @@ import getFormatedChartData from "../utils/getFormatedChartData";
 import { handleValues } from "../utils/handleValue";
 
 const pieChartMapping = (alloted = {}) => {
-  const chartCount = () => {
-    let totalCount = 0;
-    Object.values(alloted).forEach((value) => {
-      totalCount += value;
-    });
-    return totalCount;
-  };
+	const chartCount = () => {
+		let totalCount = 0;
+		Object.values(alloted).forEach((value) => {
+			totalCount += value;
+		});
+		return totalCount;
+	};
 
-  const totalAllotedCogoppoints = chartCount();
+	const totalAllotedCogoppoints = chartCount();
 
-  const { allottedData = [] } = getFormatedChartData({
-    alloted,
-  });
+	const { allottedData = [] } = getFormatedChartData({
+		alloted,
+	});
 
-  const detailsPieChart = [
-    {
-      title: "Allotted",
-      data: allottedData,
-      total_count: handleValues(totalAllotedCogoppoints),
-    },
-  ];
+	const detailsPieChart = [
+		{
+			title: "Allotted",
+			data: allottedData,
+			total_count: handleValues(totalAllotedCogoppoints),
+		},
+	];
 
-  return {
-    detailsPieChart,
-  };
+	return {
+		detailsPieChart,
+	};
 };
 
 export default pieChartMapping;
--- packages/referral/page-components/Dashboard/Progress/Users/index.js
+++ packages/referral/page-components/Dashboard/Progress/Users/index.js
@@ -7,20 +7,20 @@ import StatusDetails from "./StatusDetails";
 import styles from "./styles.module.css";
 
 function Users({ userData = {} }) {
-  const { usersData = [] } = getFormatedChartData({ userData });
-  return (
-    <>
-      <div className={styles.title}>USERS</div>
-      <div className={styles.chart_container}>
-        <div className={styles.chart}>
-          <SemiPieChart usersData={usersData} />
-        </div>
-        <div className={styles.chart_status}>
-          <StatusDetails userData={userData} />
-        </div>
-      </div>
-    </>
-  );
+	const { usersData = [] } = getFormatedChartData({ userData });
+	return (
+		<>
+			<div className={styles.title}>USERS</div>
+			<div className={styles.chart_container}>
+				<div className={styles.chart}>
+					<SemiPieChart usersData={usersData} />
+				</div>
+				<div className={styles.chart_status}>
+					<StatusDetails userData={userData} />
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default Users;
