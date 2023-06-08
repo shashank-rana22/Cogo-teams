250f452a38ad16ef338647f30ac772fc43b9cc0a
mona singh <mona.singh@cogoport.com>
2023-06-08 12:02:24 +0530

    add tab

--- packages/business-finance/page-components/Dunnings/index.tsx
+++ packages/business-finance/page-components/Dunnings/index.tsx
@@ -1,8 +1,45 @@
-import React from 'react';
+import { TabPanel, Tabs } from '@cogoport/components';
+import { useRouter } from '@cogoport/next';
+import React, { useState } from 'react';
+import styles from './styles.module.css';
 
 function Dunnings() {
+	const { query, push } = useRouter();	
+
+	const [activeTab, setActiveTab] = useState(query?.active_tab|| 'campaign-management');
+	const handleTabChange = (tab:string) => {
+		setActiveTab(tab);
+		push(
+			'/business-finance/dunnings/[active_tab]',
+			`/business-finance/dunnings/${tab}`,
+		);
+	};
+	
 	return (
-		<div>Dunning</div>
+		<div>
+			<div className={styles.header}>
+			Dunning
+			</div>
+			<div className={styles.tabs_container}>
+				<Tabs
+					activeTab={activeTab}
+					onChange={(tab:string) => handleTabChange(tab)}
+					fullWidth
+					themeType="primary"
+				>
+					<TabPanel
+						className={styles.tab_panel_dashboard}
+						name="campaign-management"
+						title="Campaign Management"
+					>
+						Campaign Management
+					</TabPanel>
+					<TabPanel name="exceptions-management" title="Exceptions Management">
+					Exceptions Management
+					</TabPanel>
+				</Tabs>
+			</div>
+		</div>
 	);
 }
 
