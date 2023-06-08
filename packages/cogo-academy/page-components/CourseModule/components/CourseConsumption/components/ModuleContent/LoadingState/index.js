05373afe4e6231db8cb9e43e9a8afd196bb02a67
rupesh-mutyala <rupesh.mutyala@cogoport.com>
2023-06-08 19:32:24 +0530

    fixes

--- packages/cogo-academy/page-components/CourseModule/components/CourseConsumption/components/ModuleContent/LoadingState/index.js
+++ packages/cogo-academy/page-components/CourseModule/components/CourseConsumption/components/ModuleContent/LoadingState/index.js
@@ -5,8 +5,8 @@ function LoadingState() {
 	return (
 		<Placeholder height="400px" width="100%" margin="0px 0px 20px 80px">
 			<Image
-				width={20}
-				height={20}
+				width={60}
+				height={60}
 				src="https://cdn.cogoport.io/cms-prod/cogo_admin/vault/original/PlaceholderMedia.svg"
 				alt="loading_media.png"
 			/>
