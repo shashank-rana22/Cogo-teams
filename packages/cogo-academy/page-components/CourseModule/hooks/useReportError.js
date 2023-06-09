cb64355eb67b3361b8e9cb39a1bcac82c1b98bc0
rupesh-mutyala <rupesh.mutyala@cogoport.com>
2023-06-09 12:50:34 +0530

    changes

--- packages/cogo-academy/page-components/CourseModule/hooks/useReportError.js
+++ packages/cogo-academy/page-components/CourseModule/hooks/useReportError.js
@@ -2,72 +2,56 @@ import { Toast } from '@cogoport/components';
 import { useForm } from '@cogoport/forms';
 import getApiErrorString from '@cogoport/forms/utils/getApiError';
 import GLOBAL_CONSTANTS from '@cogoport/globalization/constants/globals';
-import { useRequest } from '@cogoport/request';
+import { request } from '@cogoport/request';
 
 const useReportError = ({ setShowErrorModal = () => {} }) => {
 	const { handleSubmit, control, formState: { errors } } = useForm();
-
-	const [{ loading : feedbackLoading }, trigger] = useRequest({
-		url    : '/create_communication',
-		method : 'POST',
-	}, { manual: true });
 
 	const onSubmit = async (values) => {
 		const { description, error_screenshot_url = {} } = values || {};
 
 		const { finalUrl = '' } = error_screenshot_url || {};
 
-		const payload = {
-			user_id       : GLOBAL_CONSTANTS.uuid.cogo_course_notification_id,
-			type          : 'platform_notification',
-			service       : 'user',
-			service_id    : GLOBAL_CONSTANTS.uuid.cogo_course_notification_id,
-			template_name : 'api_error_request_response',
-			provider_name : 'cogoport',
-			variables     : {
-				description,
-				image_url         : finalUrl,
-				status_code       : 1,
-				session_data      : '',
-				requested_payload : '',
-				api_response      : '',
-				api_curl          : '',
-			},
-		};
+		const { cogo_course_notification_ids } = GLOBAL_CONSTANTS.uuid;
 
-		try {
-			await trigger({
-				data: payload,
-			});
-
-			Toast.success('Your Report Was Submitted Successfully...');
-			setShowErrorModal(false);
-		} catch (error) {
-			if (error.response?.data) { Toast.error(getApiErrorString(error.response?.data)); }
-		}
-	};
-
-	const reportError = async () => {
-		try {
+		const promises = cogo_course_notification_ids.reduce((acc, id) => {
 			const payload = {
-				user_id       : GLOBAL_CONSTANTS.uuid.cogo_course_notification_id,
 				type          : 'platform_notification',
 				service       : 'user',
-				service_id    : GLOBAL_CONSTANTS.uuid.cogo_course_notification_id,
 				template_name : 'api_error_request_response',
 				provider_name : 'cogoport',
+				user_id       : id,
+				service_id    : id,
 				variables     : {
+					description,
+					image_url         : finalUrl,
+					status_code       : 1,
 					session_data      : '',
 					requested_payload : '',
 					api_response      : '',
-					status_code       : '',
+					api_curl          : '',
 				},
 			};
-			await trigger({ data: payload });
+
+			const promise = request({
+				method : 'POST',
+				url    : '/create_communication',
+				data   : payload,
+			});
+
+			acc.push(promise);
+
+			return acc;
+		}, []);
+
+		try {
+			await Promise.all(promises);
 
 			Toast.success(
-				'Error has been noted',
+				'Issue has been noted',
 			);
+
+			setShowErrorModal(false);
 		} catch (error) {
 			Toast.error(getApiErrorString(error.response?.data));
 		}
@@ -76,10 +60,8 @@ const useReportError = ({ setShowErrorModal = () => {} }) => {
 	return {
 		handleSubmit,
 		control,
-		feedbackLoading,
 		errors,
 		onSubmit,
-		reportError,
 	};
 };
 
