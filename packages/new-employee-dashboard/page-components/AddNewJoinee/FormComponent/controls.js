694234c519c1604aa5291f883280ed9b5969234f
RoshanOraonCogoport <roshan.oraon@cogoport.com>
2023-06-12 15:27:38 +0530

    date from bulk

--- packages/new-employee-dashboard/page-components/AddNewJoinee/FormComponent/controls.js
+++ packages/new-employee-dashboard/page-components/AddNewJoinee/FormComponent/controls.js
@@ -53,7 +53,7 @@ const controls = [
 		name                  : 'date_of_joining',
 		label                 : 'Date of joining',
 		type                  : 'SingleDateRange',
-		dateFormat            : 'dd-MMM-yyyy',
+		dateFormat            : GLOBAL_CONSTANTS.formats.date['dd MMM yyyy'],
 		isPreviousDaysAllowed : true,
 		isClearable           : true,
 	},
