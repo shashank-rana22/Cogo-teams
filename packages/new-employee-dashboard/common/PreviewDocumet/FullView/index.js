3ccd01441bb29c1f56b9042bcc015367c2a3d510
RoshanOraonCogoport <roshan.oraon@cogoport.com>
2023-06-12 15:52:10 +0530

    open-document-added

--- packages/new-employee-dashboard/common/PreviewDocumet/FullView/index.js
+++ packages/new-employee-dashboard/common/PreviewDocumet/FullView/index.js
@@ -2,22 +2,15 @@ import { Button } from '@cogoport/components';
 import { IcMEyeopen } from '@cogoport/icons-react';
 import React from 'react';
 
+import openDocument from '../../../utils/open-document';
+
 import styles from './styles.module.css';
 
 function FullView({ url, containerStyle = {} }) {
-	const openDocument = () => {
-		let modifiedUrl = `https://${url}`;
-		if (url?.includes('http://') || url?.includes('https://')) {
-			modifiedUrl = url;
-		}
-
-		window.open(modifiedUrl, '_blank');
-	};
-
 	return (
 		<div className={styles.container} style={{ ...containerStyle }}>
 			<Button
-				onClick={() => openDocument()}
+				onClick={() => openDocument(url)}
 				style={{ borderRadius: 4 }}
 			>
 				<div style={{ display: 'flex', alignItems: 'center' }}>
