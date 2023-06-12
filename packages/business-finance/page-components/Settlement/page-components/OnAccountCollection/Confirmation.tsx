74f431432bf60d60efb2a7d39e2d18e15bcc0ab1
sivalaxman7 <sivalaxman.mylapilli@cogoport.com>
2023-06-12 14:16:23 +0530

    eslint

--- packages/business-finance/page-components/Settlement/page-components/OnAccountCollection/Confirmation.tsx
+++ packages/business-finance/page-components/Settlement/page-components/OnAccountCollection/Confirmation.tsx
@@ -1,65 +1,64 @@
-import {Button, Modal} from '@cogoport/components';
-import {isEmpty} from '@cogoport/utils';
-import React, {Dispatch, SetStateAction} from 'react';
+import { Button, Modal } from '@cogoport/components';
+import { isEmpty } from '@cogoport/utils';
+import React, { Dispatch, SetStateAction } from 'react';
 
 import styles from './styles.module.css';
 
 interface Props {
-    bulkPost: () => void,
-    showConfirm: boolean;
-    setShowConfirm: Dispatch<SetStateAction<string | boolean>>;
-    checkedRows: string[];
-    loading: boolean;
+	bulkPost: () => void,
+	showConfirm: boolean;
+	setShowConfirm: Dispatch<SetStateAction<string | boolean>>;
+	checkedRows: string[];
+	loading: boolean;
 }
 
 function Confirmation(
-    {
-        showConfirm,
-        setShowConfirm,
-        bulkPost = () => {
-        },
-        checkedRows = [],
-        loading,
-    }: Props,
+	{
+		showConfirm,
+		setShowConfirm,
+		bulkPost = () => {
+		},
+		checkedRows = [],
+		loading,
+	}: Props,
 ) {
-
-    const unqIds = [...new Set(checkedRows)]
-    return (
-        <Modal
-            show={showConfirm}
-            placement="top"
-            closeOnOuterClick={false}
-            onClose={() => {
-                setShowConfirm(false);
-            }}
-        >
-            <div className={styles.heading}>
-                You have Selected
-                {' '}
-                {unqIds?.length || 0}
-                {' '}
-                Payments. Are You Sure you want to Post?
-            </div>
-            <div className={styles.buttons}>
-                <Button
-                    themeType="secondary"
-                    onClick={() => {
-                        setShowConfirm(false);
-                    }}
-                >
-                    Cancel
-                </Button>
-                <Button
-                    themeType="primary"
-                    onClick={bulkPost}
-                    className={styles.post}
-                    loading={loading}
-                >
-                    Post
-                </Button>
-            </div>
-        </Modal>
-    );
+	const unqIds = [...new Set(checkedRows)];
+	return (
+		<Modal
+			show={showConfirm}
+			placement="top"
+			closeOnOuterClick={false}
+			onClose={() => {
+            	setShowConfirm(false);
+			}}
+		>
+			<div className={styles.heading}>
+				You have Selected
+				{' '}
+				{unqIds?.length || 0}
+				{' '}
+				Payments. Are You Sure you want to Post?
+			</div>
+			<div className={styles.buttons}>
+				<Button
+					themeType="secondary"
+					onClick={() => {
+                    	setShowConfirm(false);
+					}}
+				>
+					Cancel
+				</Button>
+				<Button
+					themeType="primary"
+					onClick={bulkPost}
+					className={styles.post}
+					loading={loading}
+				>
+					Post
+				</Button>
+			</div>
+		</Modal>
+	);
 }
 
 export default Confirmation;
--- packages/business-finance/page-components/Settlement/page-components/OnAccountCollection/common/CustomTable/index.tsx
+++ packages/business-finance/page-components/Settlement/page-components/OnAccountCollection/common/CustomTable/index.tsx
@@ -1,7 +1,7 @@
-import {Checkbox, Pagination} from '@cogoport/components';
+import { Checkbox, Pagination } from '@cogoport/components';
 import GLOBAL_CONSTANTS from '@cogoport/globalization/constants/globals';
-import {isEmpty} from '@cogoport/utils';
-import React, {useState} from 'react';
+import { isEmpty } from '@cogoport/utils';
+import React, { useState } from 'react';
 
 import EmptyStateDocs from '../../../../../commons/EmptyStateDocs';
 import usePostToSageBulk from '../../../../hooks/usePostToSageBulk';
@@ -11,138 +11,137 @@ import List from './List';
 import styles from './styles.module.css';
 
 interface GlobalInterface {
-    page?: number;
-    pageLimit?: number;
-    accMode?: string;
-    search?: string;
-    date?: {
-        startDate?: Date;
-        endDate?: Date;
-    };
-    paymentDocumentStatus?: string;
-    docType?: string;
-    sortBy?: string;
-    sortType?: string;
+	page?: number;
+	pageLimit?: number;
+	accMode?: string;
+	search?: string;
+	date?: {
+		startDate?: Date;
+		endDate?: Date;
+	};
+	paymentDocumentStatus?: string;
+	docType?: string;
+	sortBy?: string;
+	sortType?: string;
 }
 
 interface CustomInterface {
-    data?: {
-        list?: Array<{ id: string }>;
-        page?: number;
-        totalRecords?: number;
-    };
-    onPageChange?: (val: number) => void;
-    refetch?: () => void;
-    loading?: boolean;
-    globalFilters?: GlobalInterface;
-    setGlobalFilters?: React.Dispatch<React.SetStateAction<GlobalInterface>>;
-    checkedRows?: string[];
-    setCheckedRows?: React.Dispatch<React.SetStateAction<object>>;
+	data?: {
+		list?: Array<{ id: string }>;
+		page?: number;
+		totalRecords?: number;
+	};
+	onPageChange?: (val: number) => void;
+	refetch?: () => void;
+	loading?: boolean;
+	globalFilters?: GlobalInterface;
+	setGlobalFilters?: React.Dispatch<React.SetStateAction<GlobalInterface>>;
+	checkedRows?: string[];
+	setCheckedRows?: React.Dispatch<React.SetStateAction<object>>;
 }
 
 function CustomTable(
-    {
-        data = {},
-        onPageChange,
-        refetch,
-        loading,
-        setGlobalFilters,
-        globalFilters,
-        checkedRows,
-        setCheckedRows,
-    }: CustomInterface,
+	{
+		data = {},
+		onPageChange,
+		refetch,
+		loading,
+		setGlobalFilters,
+		globalFilters,
+		checkedRows,
+		setCheckedRows,
+	}: CustomInterface,
 ) {
-    const GET_STATUS = ['POSTED', 'APPROVED', 'POSTING_FAILED'];
+	const GET_STATUS = ['POSTED', 'APPROVED', 'POSTING_FAILED'];
 
-    const GET_ENTITY = Object.keys(GLOBAL_CONSTANTS.cogoport_entities).filter(
-        (entity) => entity !== '501',
-    );
+	const GET_ENTITY = Object.keys(GLOBAL_CONSTANTS.cogoport_entities).filter(
+		(entity) => entity !== '501',
+	);
 
-    const isCheckBoxAllowed = (item) => GET_STATUS.includes(item?.paymentDocumentStatus)
+	const isCheckBoxAllowed = (item) => GET_STATUS.includes(item?.paymentDocumentStatus)
         && GET_ENTITY.includes(item?.entityType?.toString());
 
-    const {list = [], page = 1, totalRecords = 0} = data;
-    const onChangeTableHeaderCheckbox = (event) => {
-        event.stopPropagation();
+	const { list = [], page = 1, totalRecords = 0 } = data;
+	const onChangeTableHeaderCheckbox = (event) => {
+		event.stopPropagation();
 
-        const listIds = (list || []).map(({id}) => id)
+		const listIds = (list || []).map(({ id }) => id);
 
-        if (event.target.checked) {
-            setCheckedRows([
-                ...checkedRows,
-                ...((list || [])
-                    ?.filter((item) => isCheckBoxAllowed(item))
-                    .map(({id}) => id) || []),
-            ]);
-        } else {
-            setCheckedRows([
-                ...(checkedRows.filter((idChecked) => !(listIds || []).includes(idChecked))),
-            ]);
-        }
-    };
+		if (event.target.checked) {
+			setCheckedRows([
+				...checkedRows,
+				...((list || [])
+					?.filter((item) => isCheckBoxAllowed(item))
+					.map(({ id }) => id) || []),
+			]);
+		} else {
+			setCheckedRows([
+				...(checkedRows.filter((idChecked) => !(listIds || []).includes(idChecked))),
+			]);
+		}
+	};
 
-    const onChangeTableBodyCheckbox = (event, item) => {
-        event.stopPropagation();
-        if (event.target.checked) {
-            setCheckedRows([
-                    ...checkedRows,
-                    item?.id,
-                ]
-            );
-        } else {
-            setCheckedRows([
-                ...((checkedRows || []).filter(
-                    (Id) => Id !== item?.id,
-                ) || []),
-            ]);
-        }
-    };
+	const onChangeTableBodyCheckbox = (event, item) => {
+		event.stopPropagation();
+		if (event.target.checked) {
+			setCheckedRows([
+				...checkedRows,
+				item?.id,
+			]);
+		} else {
+			setCheckedRows([
+				...((checkedRows || []).filter(
+					(Id) => Id !== item?.id,
+				) || []),
+			]);
+		}
+	};
 
-    const getTableBodyCheckbox = (item) => {
-        const isChecked = (checkedRows || []).includes(
-            item?.id,
-        );
+	const getTableBodyCheckbox = (item) => {
+		const isChecked = (checkedRows || []).includes(
+			item?.id,
+		);
 
-        return isCheckBoxAllowed(item) ? (
-            <Checkbox
-                checked={isChecked}
-                disabled={loading}
-                onChange={(event) => onChangeTableBodyCheckbox(event, item)}
-            />
-        ) : null;
-    };
+		return isCheckBoxAllowed(item) ? (
+			<Checkbox
+				checked={isChecked}
+				disabled={loading}
+				onChange={(event) => onChangeTableBodyCheckbox(event, item)}
+			/>
+		) : null;
+	};
 
-    const IsAllChecked = (list || [])?.filter((item) => isCheckBoxAllowed(item) && !checkedRows.includes(item?.id))?.length === 0;
+	const IsAllChecked = (list || [])?.filter((item) => isCheckBoxAllowed(item) && !checkedRows.includes(item?.id))?.length === 0;
 
-    const showHeaderCheckbox = (list || [])?.filter((item) => isCheckBoxAllowed(item))?.length > 0;
+	const showHeaderCheckbox = (list || [])?.filter((item) => isCheckBoxAllowed(item))?.length > 0;
 
-    return isEmpty(list) ? (
-        <EmptyStateDocs/>
-    ) : (
-        <div className={styles.table}>
-            <Header
-                setGlobalFilters={setGlobalFilters}
-                globalFilters={globalFilters}
-                onChangeTableHeaderCheckbox={onChangeTableHeaderCheckbox}
-                IsAllChecked={IsAllChecked}
-                showHeaderCheckbox={showHeaderCheckbox}
-                loading={loading}
-            />
-            <List
-                list={list}
-                refetch={refetch}
-                loading={loading}
-                getTableBodyCheckbox={getTableBodyCheckbox}
-            />
-            <Pagination
-                className={styles.pagination}
-                currentPage={page}
-                totalItems={totalRecords}
-                pageSize={10}
-                onPageChange={onPageChange}
-            />
-        </div>
-    );
+	return isEmpty(list) ? (
+		<EmptyStateDocs />
+	) : (
+		<div className={styles.table}>
+			<Header
+				setGlobalFilters={setGlobalFilters}
+				globalFilters={globalFilters}
+				onChangeTableHeaderCheckbox={onChangeTableHeaderCheckbox}
+				IsAllChecked={IsAllChecked}
+				showHeaderCheckbox={showHeaderCheckbox}
+				loading={loading}
+			/>
+			<List
+				list={list}
+				refetch={refetch}
+				loading={loading}
+				getTableBodyCheckbox={getTableBodyCheckbox}
+			/>
+			<Pagination
+				className={styles.pagination}
+				currentPage={page}
+				totalItems={totalRecords}
+				pageSize={10}
+				onPageChange={onPageChange}
+			/>
+		</div>
+	);
 }
 
 export default CustomTable;
