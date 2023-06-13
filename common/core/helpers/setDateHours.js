cbdfb2cdf756aa4f18fceef39b825b93c801c78f
Ankit Agarwal <ankitagarwal@Ankits-MacBook-Air.local>
2023-06-13 17:35:22 +0530

    removing parse int

--- common/core/helpers/setDateHours.js
+++ common/core/helpers/setDateHours.js
@@ -7,19 +7,17 @@ const setDateHours = ({ time = '0:0:0', date }) => {
 		return null;
 	}
 
-	let [getHour = 0, getMinute = 0, getSecond = 0] = time.split(':');
+	let [hour = 0, minute = 0, second = 0] = time.split(':');
 
-	getHour = parseInt(getHour, DEFAULT_VALUE_FOR_RADIX_PARAMETER);
+	[hour, minute, second] = [hour, minute, second].map(
+		(item) => parseInt(item, DEFAULT_VALUE_FOR_RADIX_PARAMETER),
+	);
 
-	getMinute = parseInt(getMinute, DEFAULT_VALUE_FOR_RADIX_PARAMETER);
-
-	getSecond = parseInt(getSecond, DEFAULT_VALUE_FOR_RADIX_PARAMETER);
-
-	if (Number.isNaN(getHour) || Number.isNaN(getMinute) || Number.isNaN(getSecond)) {
+	if (Number.isNaN(hour) || Number.isNaN(minute) || Number.isNaN(second)) {
 		return null;
 	}
 
-	newDate.setHours(getHour, getMinute, getSecond);
+	newDate.setHours(hour, minute, second);
 	return newDate;
 };
 export default setDateHours;
