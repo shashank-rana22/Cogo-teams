819365cb24237c96c604300d0535637a3005a6fc
Nagendra Singh <nagendra.singh@cogoport.com>
2023-06-15 17:09:38 +0530

    feat: top header tabs created

--- packages/business-finance/page-components/Dunnings/components/CampaignManagement/RenderActions/index.tsx
+++ packages/business-finance/page-components/Dunnings/components/CampaignManagement/RenderActions/index.tsx
@@ -1,12 +1,26 @@
 import { Toggle } from '@cogoport/components';
 import { IcMDelete, IcMEdit } from '@cogoport/icons-react';
 
+import styles from './styles.module.css';
+
 function RenderActions() {
 	return (
 		<div style={{ display: 'flex' }}>
 			<Toggle name="isActive" size="md" showOnOff disabled={false} checked />
-			<button style={{ border: 'none', cursor: 'pointer' }} aria-label="edit"><IcMEdit /></button>
-			<button style={{ border: 'none', cursor: 'pointer' }} aria-label="delete"><IcMDelete /></button>
+			<button className={styles.btn} aria-label="edit">
+				<IcMEdit
+					height={15}
+					width={15}
+				/>
+
+			</button>
+			<button className={styles.btn} aria-label="delete">
+				<IcMDelete
+					height={15}
+					width={15}
+				/>
+
+			</button>
 		</div>
 	);
 }
