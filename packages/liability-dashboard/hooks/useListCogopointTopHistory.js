15b4dd4470b58194b21982e0a550894f1352b21d
Bishal <Bishal.Saha@cogoport.com>
2023-06-15 12:46:05 +0530

    add day

--- packages/liability-dashboard/hooks/useListCogopointTopHistory.js
+++ packages/liability-dashboard/hooks/useListCogopointTopHistory.js
@@ -1,9 +1,11 @@
 import { useRequest } from '@cogoport/request';
+import { addDays } from '@cogoport/utils';
 import { useEffect, useCallback, useState } from 'react';
 
 import { EVENT_MAPPING } from '../constants';
 
-const DEFAULT_PAGE_NUMBER = 1;
+const PAGE_NUMBER = 1;
+const ADD_DAY = 1;
 
 const useListCogopointTopHistory = ({
 	transactionType = '',
@@ -17,7 +19,7 @@ const useListCogopointTopHistory = ({
 		method : 'get',
 	}, { manual: true });
 
-	const [pagination, setPagination] = useState(DEFAULT_PAGE_NUMBER);
+	const [pagination, setPagination] = useState(PAGE_NUMBER);
 	const [selectOrganization, setSelectOrganization] = useState('');
 	const { startDate, endDate } = selectedDate || {};
 
@@ -31,7 +33,7 @@ const useListCogopointTopHistory = ({
 					organization_id   : selectOrganization,
 					transaction_type  : transactionType,
 					organization_type : activeHeaderTab === 'overall' ? undefined : activeHeaderTab,
-					from_date         : startDate,
+					from_date         : addDays(startDate, ADD_DAY),
 					to_date           : endDate,
 					event             : EVENT_MAPPING[activeStatsCard] || undefined,
 				},
