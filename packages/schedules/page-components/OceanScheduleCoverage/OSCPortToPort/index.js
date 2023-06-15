2cd8fedf14af13370af7537cd91dde375edcfa12
Abhik Kumar <abhik.kumar@cogoport.com>
2023-06-15 12:11:04 +0530

    lint

--- packages/schedules/page-components/OceanScheduleCoverage/OSCPortToPort/index.js
+++ packages/schedules/page-components/OceanScheduleCoverage/OSCPortToPort/index.js
@@ -1,19 +1,18 @@
 import { Button, Table } from '@cogoport/components';
-import {getColumns} from '../helpers/column'
-import styles from '../styles.module.css'
-import useGetSailingSchedulePortPairCoverage from '../hooks/useGetSailingSchedulePortPairCoverage';
-import ViewScheduleModal from '../ViewScheduleModal';
 import { IcMArrowBack } from '@cogoport/icons-react';
+import { getColumns } from '../helpers/column';
+import useGetSailingSchedulePortPairCoverage from '../hooks/useGetSailingSchedulePortPairCoverage';
+import styles from '../styles.module.css';
+import ViewScheduleModal from '../ViewScheduleModal';
 
-const OSCPortToPort = ( {originPort,destinationPort,setIsPortToPort,columnsForPattern,columnsForPortToPort,show,setShow}) => {
-	const {data,coverageTotalCount} = useGetSailingSchedulePortPairCoverage({originPort,destinationPort});
-    return (
-        <>
-            <ViewScheduleModal show={show} setShow={setShow} columnsForPattern={columnsForPattern}/>
-			<Button themeType="secondary" onClick={()=>setIsPortToPort(false)}><IcMArrowBack/></Button>
-            {columnsForPortToPort && data && <Table columns={columnsForPortToPort} data={data} className={styles.table}/>} 
-        </>
-
-    );
+function OSCPortToPort({ originPort, destinationPort, setIsPortToPort, columnsForPattern, columnsForPortToPort, show, setShow }) {
+	const { data, coverageTotalCount } = useGetSailingSchedulePortPairCoverage({ originPort, destinationPort });
+	return (
+		<>
+			<ViewScheduleModal show={show} setShow={setShow} columnsForPattern={columnsForPattern} />
+			<Button themeType="secondary" onClick={() => setIsPortToPort(false)}><IcMArrowBack /></Button>
+			{columnsForPortToPort && data && <Table columns={columnsForPortToPort} data={data} className={styles.table} />}
+		</>
+	);
 }
 export default OSCPortToPort;
