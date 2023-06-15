9dff33f1424235318e64dfd27af85ad56bc727bd
Abhik Kumar <abhik.kumar@cogoport.com>
2023-06-15 19:17:24 +0530

    day work

--- packages/schedules/page-components/OceanScheduleCoverage/ViewScheduleModal/index.js
+++ packages/schedules/page-components/OceanScheduleCoverage/ViewScheduleModal/index.js
@@ -1,13 +1,23 @@
 import { Button, Modal, Table } from "@cogoport/components";
 import styles from "./styles.module.css";
-import { getColumns } from "../helpers/column";
+import { useState } from "react";
+import UpdateScheduleModal from "./UpdateScheduleModal";
+import BulkUploadSchedule from "./BulkUploadSchedule";
 
 const ViewScheduleModal = ({ show, setShow, columnsForPattern }) => {
+    const [update, setUpdate] = useState(false);
+    const [bulkUpload, setBulkUpload] = useState(false);
+
     const modelTitle = (
         <div className={styles.heading}>
             Ocean Port Pair Schedule
             <div className={styles.button}>
-                <Button type="update" size="md" themeType="accent">
+                <Button
+                    type="update"
+                    size="md"
+                    themeType="accent"
+                    onClick={() => setUpdate(true)}
+                >
                     Update
                 </Button>
                 <Button
@@ -15,6 +25,7 @@ const ViewScheduleModal = ({ show, setShow, columnsForPattern }) => {
                     size="md"
                     themeType="accent"
                     style={{ marginLeft: "4px" }}
+                    onClick={() => setBulkUpload(true)}
                 >
                     Bulk Upload
                 </Button>
@@ -23,19 +34,35 @@ const ViewScheduleModal = ({ show, setShow, columnsForPattern }) => {
     );
 
     return (
-        <Modal
-            size="md"
-            show={show}
-            onClose={() => setShow(null)}
-            placement="center"
-        >
-            <Modal.Header title={modelTitle} />
-            <Modal.Body>
-                {columnsForPattern && show && (
-                    <Table columns={columnsForPattern} data={show?.patterns} />
-                )}
-            </Modal.Body>
-        </Modal>
+        <>
+            {!update && !bulkUpload && (
+                <Modal
+                    size="md"
+                    show={show}
+                    onClose={() => setShow(null)}
+                    placement="center"
+                >
+                    <Modal.Header title={modelTitle} />
+                    <Modal.Body>
+                        {columnsForPattern && show && (
+                            <Table
+                                columns={columnsForPattern}
+                                data={show?.patterns}
+                            />
+                        )}
+                    </Modal.Body>
+                </Modal>
+            )}
+            {update && !bulkUpload && (
+                <UpdateScheduleModal setUpdate={setUpdate} update={update} />
+            )}
+            {!update && bulkUpload && (
+                <BulkUploadSchedule
+                    setBulkUpload={setBulkUpload}
+                    bulkUpload={bulkUpload}
+                />
+            )}
+        </>
     );
 };
 export default ViewScheduleModal;
