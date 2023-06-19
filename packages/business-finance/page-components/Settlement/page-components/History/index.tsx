1bece5c7a4120ddb63a2deb70c047ed24712f8a4
simranjitsingh3720 <simranjit.singh@cogoport.com>
2023-06-19 18:09:39 +0530

    changes done

--- packages/business-finance/page-components/Settlement/page-components/History/index.tsx
+++ packages/business-finance/page-components/Settlement/page-components/History/index.tsx
@@ -1,6 +1,7 @@
-import { Checkbox, Input } from '@cogoport/components';
+import { Checkbox, Input, Toggle } from '@cogoport/components';
 import GLOBAL_CONSTANTS from '@cogoport/globalization/constants/globals';
 import { IcMSearchlight } from '@cogoport/icons-react';
+import { useRouter } from '@cogoport/next';
 import { isEmpty } from '@cogoport/utils';
 import React, { useState } from 'react';
 
@@ -34,11 +35,17 @@ interface DataInterface {
 }
 
 function History() {
+	const { query } = useRouter();
+
 	const [checkedRows, setCheckedRows] = useState({});
 
 	const { loading, filters, setFilters, apiData, refetch } = useHistorySettlemet();
 
 	const { list = [] } = (apiData as DataInterface) || {};
+
+	const handleVersionChange = () => {
+		window.location.href = `/${query.partner_id}/business-finance/settlement/history`;
+	};
 
 	const onPageChange = (val:number) => {
 		setFilters({ ...filters, page: val });
@@ -121,14 +128,23 @@ function History() {
 		<div>
 			<div className={styles.filter_container}>
 				<Filter controls={historyFilters(filters)} setFilters={setFilters} filters={filters} pageKey="page" />
-				<Input
-					name="query"
-					onChange={(val) => { setFilters({ ...filters, query: val, page: 1 }); }}
-					placeholder="Search by Document Number"
-					size="md"
-					suffix={<IcMSearchlight height="20px" width="20px" />}
-					className={styles.search_div}
-				/>
+				<div className={styles.toggle_Div}>
+					<Toggle
+						name="toggle"
+						size="md"
+						onLabel="Old"
+						offLabel="New"
+						onChange={handleVersionChange}
+					/>
+					<Input
+						name="query"
+						onChange={(val) => { setFilters({ ...filters, query: val, page: 1 }); }}
+						placeholder="Search by Document Number"
+						size="md"
+						suffix={<IcMSearchlight height="20px" width="20px" />}
+						className={styles.search_div}
+					/>
+				</div>
 			</div>
 			<CustomTable
 				data={apiData}
