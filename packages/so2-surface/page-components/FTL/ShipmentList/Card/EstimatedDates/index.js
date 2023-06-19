90709cede3c446ae2f54174fdf8e70184222910b
newMonk02 <ayushman.tiwari@cogoport.com>
2023-06-19 16:53:40 +0530

    added ATA and ATD

--- packages/so2-surface/page-components/FTL/ShipmentList/Card/EstimatedDates/index.js
+++ packages/so2-surface/page-components/FTL/ShipmentList/Card/EstimatedDates/index.js
@@ -7,42 +7,75 @@ import styles from './styles.module.css';
 const getDisplayDate = (date, dateFormat = 'dd MMM yyyy') => (date ? format(date, dateFormat, null, true) : null);
 
 function EstimatedDates({ data = {} }) {
-	const { estimated_arrival = '', estimated_departure = '' } = data.ftl_freight_services[0] || {};
+	const { estimated_arrival = '', estimated_departure = '', pickup_date = '', delivery_date = '' } = data.ftl_freight_services[0] || {};
 	return (
-		<div className={`${styles.container} core_ui_port_conatiner`}>
+		<div>
+			<div className={`${styles.container} core_ui_port_conatiner`}>
 
-			<div className={styles.date_detail}>
-				{estimated_departure ? (
-					<>
-						<div className={styles.heading}>
-							ETD:
-						</div>
-
+				<div className={styles.date_detail}>
+					<div className={styles.heading}>
+						ETD:
+					</div>
+					{estimated_departure ? (
 						<div className={styles.eta_etd}>
 							{' '}
 							{getDisplayDate(estimated_departure)}
 						</div>
-					</>
+
+					) : 'TBD'}
+				</div>
+
+				<div className={styles.icon_wrapper}>
+					<IcMPortArrow className="core_ui_icon" />
+				</div>
+				<div className={styles.heading}>
+					ETA:
+				</div>
+				{estimated_arrival ? (
+
+					<div className={styles.date_detail}>
+
+						<div className={styles.eta_etd}>
+							{'  '}
+							{getDisplayDate(estimated_arrival)}
+
+						</div>
+					</div>
+
 				) : 'TBD'}
 			</div>
-
-			<div className={styles.icon_wrapper}>
-				<IcMPortArrow className="core_ui_icon" />
-			</div>
-			{estimated_arrival ? (
+			<div className={`${styles.container} core_ui_port_conatiner`}>
 
 				<div className={styles.date_detail}>
 					<div className={styles.heading}>
-						ETA:
+						ATD:
 					</div>
-					<div className={styles.eta_etd}>
-						{'  '}
-						{getDisplayDate(estimated_arrival)}
-
-					</div>
+					{pickup_date ? (
+						<div className={styles.eta_etd}>
+							{getDisplayDate(pickup_date)}
+						</div>
+					) : 'TBD'}
 				</div>
 
-			) : 'TBD'}
+				<div className={styles.icon_wrapper}>
+					<IcMPortArrow className="core_ui_icon" />
+				</div>
+				<div className={styles.heading}>
+					ATA:
+				</div>
+				{delivery_date ? (
+
+					<div className={styles.date_detail}>
+
+						<div className={styles.eta_etd}>
+							{'  '}
+							{getDisplayDate(delivery_date)}
+
+						</div>
+					</div>
+
+				) : 'TBD'}
+			</div>
 		</div>
 	);
 }
