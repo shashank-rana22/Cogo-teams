69e679a37343ab167bcbfff51e0c0eefe8802ddf
Rajatuba-Cogoport <rajat.uba@cogoport.com>
2023-06-20 20:28:01 +0530

    api fix

--- packages/shipment-air/page-components/hooks/useListShipment.js
+++ packages/shipment-air/page-components/hooks/useListShipment.js
@@ -1,51 +1,50 @@
+import toastApiError from '@cogoport/air-modules/utils/toastApiError';
 import { useRequest } from '@cogoport/request';
-import { useEffect, useState } from 'react';
+import { useSelector } from '@cogoport/store';
+import { useEffect, useState, useCallback } from 'react';
 
 const SHIPMENT_STATE_MAPPINGS = {
 	ongoing   : ['shipment_received', 'confirmed_by_importer_exporter', 'in_progress'],
 	completed : ['completed'],
 	cancelled : ['cancelled'],
 };
+const DEFAULT_PAGE = 1;
+
 const useListShipment = ({ serviceActiveTab, shipmentStateTab, searchQuery }) => {
+	const { authParams, selected_agent_id } = useSelector(({ profile }) => profile) || {};
 	const [filters, setFilters] = useState({});
-	const [page, setPage] = useState(1);
+	const [page, setPage] = useState(DEFAULT_PAGE);
 	const [{ loading, data }, trigger] = useRequest({
 		url    : 'list_shipments',
 		method : 'GET',
 		params : {
 			page,
-			filters : {
+			filters: {
 				state         : SHIPMENT_STATE_MAPPINGS[shipmentStateTab],
-				shipment_type : serviceActiveTab,		
+				shipment_type : serviceActiveTab,
+				q             : searchQuery,
+				...filters,
 			},
 		},
 	}, { manual: true });
 
-	const apiTrigger = async () => {
-		const params = {
-			page,
-			
-			filters : {
-				q       : searchQuery,
-				state         : SHIPMENT_STATE_MAPPINGS[shipmentStateTab],
-				shipment_type : serviceActiveTab,
-				...filters,
-			},
-		};
-		try {
-			 await trigger({ params });
-		} catch (err) {
-			console.log(err)
-		}
-	};
+	const apiTrigger = useCallback(() => {
+		(async () => {
+			try {
+				await trigger();
+			} catch (err) {
+				toastApiError(err);
+			}
+		})();
+	}, [trigger]);
 
 	useEffect(() => {
 		apiTrigger();
-	}, [ shipmentStateTab,serviceActiveTab, searchQuery, filters, page]);
+	}, [shipmentStateTab, serviceActiveTab, searchQuery, filters, page, apiTrigger, authParams, selected_agent_id]);
 
-	useEffect(()=>{
-		setPage(1);
-	},[searchQuery,serviceActiveTab])
+	useEffect(() => {
+		setPage(DEFAULT_PAGE);
+	}, [searchQuery, serviceActiveTab]);
 
 	return {
 		loading,
