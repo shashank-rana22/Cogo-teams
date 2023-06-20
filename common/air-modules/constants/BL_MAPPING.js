be7552de886284b473f1309b2a792f192256ca45
Ankit Kaushal <ankit.kaushal@cogoport.com>
2023-06-20 13:17:16 +0530

    unwanted code removed

--- common/air-modules/constants/BL_MAPPING.js
+++
@@ -1,5 +0,0 @@
-const BL_CATEGORY_MAPPING = { hbl: 'HBL', mbl: 'MBL' };
-
-const BL_PREFERENCE_MAPPING = { rfs: 'RFS', sob: 'SOB', seaway: 'Seaway' };
-
-export { BL_CATEGORY_MAPPING, BL_PREFERENCE_MAPPING };
