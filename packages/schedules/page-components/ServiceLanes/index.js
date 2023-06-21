9cd98c2dbee542c19d2573aac4af8824dfcce041
Abhik Kumar <abhik.kumar@cogoport.com>
2023-06-21 14:38:52 +0530

    filters

--- packages/schedules/page-components/ServiceLanes/index.js
+++ packages/schedules/page-components/ServiceLanes/index.js
@@ -2,20 +2,27 @@ import { useState } from "react";
 import SailingSchedulesManagement from "../SailingSchedulesManagement";
 import { Button } from "@cogoport/components";
 import styles from "./styles.module.css";
+import Filters from "./Filters";
 
 function ServiceLane() {
     const [activeTab, setActiveTab] = useState("service_lanes");
     return (
-        <div className={styles.button}>
-            <SailingSchedulesManagement
-                activeTab={activeTab}
-                setActiveTab={setActiveTab}
-            />
+        <>
+            <div className={styles.button}>
+                <SailingSchedulesManagement
+                    activeTab={activeTab}
+                    setActiveTab={setActiveTab}
+                />
 
-            <Button size="md" themeType="primary">
-                + Create
-            </Button>
-        </div>
+                <Button size="md" themeType="primary">
+                    + Create
+                </Button>
+            </div>
+
+            <div>
+                <Filters></Filters>
+            </div>
+        </>
     );
 }
 export default ServiceLane;
