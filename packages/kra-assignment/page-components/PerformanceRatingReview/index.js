f449d9bc51550f46ed947eaf7841fe5ecec12e95
prachidewangan26 <prachi.dewangan@cogoport.com>
2023-06-22 12:07:47 +0530

    added add button

--- packages/kra-assignment/page-components/PerformanceRatingReview/index.js
+++ packages/kra-assignment/page-components/PerformanceRatingReview/index.js
@@ -1,7 +1,18 @@
+import { Button } from '@cogoport/components';
 import React from 'react';
 
 function PerformanceRatingReview() {
-	return <div>PerformanceRatingReview</div>;
+	return (
+		<div>
+			<div>
+				PerformanceRatingReview
+			</div>
+
+			<Button>
+				Add
+			</Button>
+		</div>
+	);
 }
 
 export default PerformanceRatingReview;
