12f5b975581f26f14f870bcc08cba35fd14629e1
Vishal <Vishal.Nagar@cogoport.com>
2023-06-26 16:59:12 +0530

    added cost booking navigation

---
+++ common/navigation-configs/apis/ff-cost-booking-apis.js
@@ -0,0 +1,69 @@
+const apis = [
+	{
+		api          : 'list_organizations',
+		access_type  : 'private',
+		service_name : 'organization',
+	},
+	{
+		api          : 'list_shipment_air_csr_sheets',
+		access_type  : 'private',
+		service_name : 'shipment',
+	},
+	{
+		api          : 'create_shipment_air_csr_sheet',
+		access_type  : 'private',
+		service_name : 'shipment',
+	},
+	{
+		api          : 'update_shipment_air_csr_ocr_data',
+		access_type  : 'private',
+		service_name : 'shipment',
+	},
+	{
+		api          : 'list_cogo_entities',
+		access_type  : 'private',
+		service_name : 'shipment',
+	},
+	{
+		api          : 'update_shipment_air_csr_sheet',
+		access_type  : 'private',
+		service_name : 'shipment',
+	},
+	{
+		api          : 'generate_variance_report',
+		access_type  : 'private',
+		service_name : 'shipment',
+	},
+	{
+		api          : 'create_shipment_air_freight_consolidated_invoice',
+		access_type  : 'private',
+		service_name : 'shipment',
+	},
+	{
+		api          : 'list_organization_trade_parties',
+		access_type  : 'private',
+		service_name : 'shipment',
+	},
+	{
+		api          : 'get_shipment_air_csr_ocr_data',
+		access_type  : 'private',
+		service_name : 'shipment',
+	},
+	{
+		api          : 'list_shipment_air_freight_consolidated_invoices',
+		access_type  : 'private',
+		service_name : 'shipment',
+	},
+	{
+		api          : 'update_shipment_air_freight_consolidated_invoice',
+		access_type  : 'private',
+		service_name : 'shipment',
+	},
+	{
+		api          : 'update_bulk_shipment_air_freight_consolidated_invoice',
+		access_type  : 'private',
+		service_name : 'shipment',
+	},
+];
+
+export default apis;
