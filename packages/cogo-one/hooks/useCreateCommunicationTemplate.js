e9559caf3ec674477952edec8e356f3d00e37677
danampallyrahul-cogo <danampally.rahul@cogoport.com>
2023-06-26 11:58:09 +0530

    added language key and agent type key

--- packages/cogo-one/hooks/useCreateCommunicationTemplate.js
+++ packages/cogo-one/hooks/useCreateCommunicationTemplate.js
@@ -1,7 +1,28 @@
-/* eslint-disable max-len */
 import { Toast } from '@cogoport/components';
 import getApiErrorString from '@cogoport/forms/utils/getApiError';
 import { useRequest } from '@cogoport/request';
+
+const getPayload = ({ data }) => {
+	const { content = '', title = '' } = data || {};
+	return {
+		name          : title.split(' ').join('_').toLowerCase(),
+		provider_name : 'meta',
+		type          : 'whatsapp',
+		content       : JSON.stringify({
+			name       : title.split(' ').join('_').toLowerCase(),
+			category   : 'marketing',
+			language   : 'en_US',
+			components : [{ text: content?.trim(), type: 'body' }],
+		}),
+		variables     : [],
+		description   : title,
+		tags          : ['quick_reply'],
+		category      : 'company_introduction',
+		// eslint-disable-next-line max-len
+		html_template : `<p>${content}</p>\n <div style="display: flex; justify-content: flex-end; color: rgba(0, 0, 0, .45); font-size: 10px;"><div>21:6</div></div>`,
+		language      : 'english',
+	};
+};
 
 function useCreateCommunicationTemplate({ reset = () => {}, refetch = () => {}, setOpenCreateReply = () => {} }) {
 	const [{ loading }, trigger] = useRequest({
@@ -9,25 +30,9 @@ function useCreateCommunicationTemplate({ reset = () => {}, refetch = () => {}, 
 		method : 'post',
 	}, { manual: true });
 	const createTemplate = async (data = {}) => {
-		const { content = '', title = '' } = data || {};
 		try {
 			await trigger({
-				data: {
-					name          : title.split(' ').join('_').toLowerCase(),
-					provider_name : 'meta',
-					type          : 'whatsapp',
-					content       : JSON.stringify({
-						name       : title.split(' ').join('_').toLowerCase(),
-						category   : 'marketing',
-						language   : 'en_US',
-						components : [{ text: content?.trim(), type: 'body' }],
-					}),
-					variables     : [],
-					description   : title,
-					tags          : ['quick_reply'],
-					category      : 'company_introduction',
-					html_template : `<p>${content}</p>\n <div style="display: flex; justify-content: flex-end; color: rgba(0, 0, 0, .45); font-size: 10px;"><div>21:6</div></div>`,
-				},
+				data: getPayload({ data }),
 			});
 			reset({});
 			refetch();
