9e061d726e9723abbf05c0202b9d872dbf8bb982
Akshay Sawant <akshay.sawant@cogoport.com>
2023-06-26 16:16:19 +0530

    pagination added

--- packages/byod-dashboards/hooks/useGetEmployees.js
+++ packages/byod-dashboards/hooks/useGetEmployees.js
@@ -1,30 +1,36 @@
 import { useHarbourRequest } from '@cogoport/request';
-import { useEffect, useCallback } from 'react';
+import { useEffect, useCallback, useState } from 'react';
 
 const useGetEmployees = () => {
+	const [filters, setFilters] = useState({
+		page: 1,
+	});
+
 	const [{ data, loading }, trigger] = useHarbourRequest({
 		url    : 'list_employee_device_details',
 		method : 'GET',
 	}, { manual: true });
 
 	const getEmployees = useCallback(() => {
+		const { page } = filters;
 		try {
 			trigger({
 				params: {
 					employee_details_required : true,
 					required_keys             : ['id', 'employee_detail_id', 'status'],
+					page,
 				},
 			});
 		} catch (error) {
 			console.log('error', error);
 		}
-	}, [trigger]);
+	}, [trigger, filters]);
 
 	useEffect(() => {
 		getEmployees();
 	}, [getEmployees]);
 
-	return { data, loading };
+	return { data, loading, filters, setFilters };
 };
 
 export default useGetEmployees;
--- packages/byod-dashboards/page-components/EmployeeList/index.js
+++ packages/byod-dashboards/page-components/EmployeeList/index.js
@@ -1,3 +1,4 @@
+import { Pagination } from '@cogoport/components';
 import React from 'react';
 
 import StyledTable from '../../common/StyledTable';
@@ -7,15 +8,24 @@ import styles from './styles.module.css';
 import getColumns from './useGetColumns';
 
 function EmployeeList() {
-	const { data, loading } = useGetEmployees();
+	const { data, loading, setFilters } = useGetEmployees();
 	const columns = getColumns();
-	const { list } = data || {};
+	const { list, page, page_limit, total_count } = data || {};
 	return (
 		<>
 			<div className={styles.title}>
 				Employee List
 			</div>
 			<StyledTable columns={columns} data={list} loading={loading} />
+			<div className={styles.pagination_container}>
+				<Pagination
+					type="table"
+					currentPage={page}
+					totalItems={total_count}
+					pageSize={page_limit}
+					onPageChange={(val) => setFilters((prev) => ({ ...prev, page: val }))}
+				/>
+			</div>
 		</>
 	);
 }
