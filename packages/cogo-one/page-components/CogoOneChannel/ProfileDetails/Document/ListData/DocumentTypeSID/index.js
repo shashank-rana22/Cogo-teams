ac6743352b33cfa18dfb4288b448f1fb2d2e72e4
shatrunjaysrivastava <shatrunjay.srivastava@cogoport.com>
2023-06-26 16:15:15 +0530

    onSubmit handled

--- packages/cogo-one/page-components/CogoOneChannel/ProfileDetails/Document/ListData/DocumentTypeSID/index.js
+++ packages/cogo-one/page-components/CogoOneChannel/ProfileDetails/Document/ListData/DocumentTypeSID/index.js
@@ -15,7 +15,7 @@ function DocumentTypeSID({
 	setDocumentTagUrl = () => {},
 }) {
 	const { account_type = '' } = formattedMessageData || {};
-	const { control, formState:{ errors = {} }, watch, handleSubmit, resetField } = useForm();
+	const { control, formState: { errors = {} }, watch, handleSubmit, resetField } = useForm();
 	const watchListShipment = watch('list_shipments');
 	const watchListShipmentPendingTasks = watch('list_shipment_pending_tasks');
 
@@ -31,11 +31,11 @@ function DocumentTypeSID({
 		postDocumentTag = () => {},
 	} = useSendShipmentDocumentationNotification();
 
-	const createDocumentTag = () => {
+	const createDocumentTag = (formValues) => {
 		const payload = {
 			document_id   : id,
-			document_type : watchListShipmentPendingTasks,
-			shipment_id   : watchListShipment,
+			document_type : formValues?.list_shipment_pending_tasks,
+			shipment_id   : formValues?.list_shipments,
 			document_link : documentTagUrl,
 		};
 		postDocumentTag({ payload, setDocumentTagUrl });
@@ -48,21 +48,19 @@ function DocumentTypeSID({
 					<div>Document Tag</div>
 					<IcMCross className={styles.cross} onClick={() => setDocumentTagUrl('')} />
 				</div>
-				<div key={watchListShipment}>
-					{controls.map((eachControl = {}) => {
-						const { label = '', name = '' } = eachControl || {};
+				{controls.map((eachControl = {}) => {
+					const { label = '', name = '' } = eachControl || {};
 
-						return (
-							<div className={styles.styled_element} key={name}>
-								<div>{label}</div>
-								<AsyncSelectController control={control} {...eachControl} />
-								<div className={styles.error_text}>
-									{errors?.[name] && (errors?.[name]?.message || 'This is Required')}
-								</div>
+					return (
+						<div className={styles.styled_element} key={name}>
+							<div>{label}</div>
+							<AsyncSelectController control={control} {...eachControl} />
+							<div className={styles.error_text}>
+								{errors?.[name] && (errors?.[name]?.message || 'This is Required')}
 							</div>
-						);
-					})}
-				</div>
+						</div>
+					);
+				})}
 				<div className={styles.button_styles}>
 					<Button size="sm" themeType="primary" onClick={handleSubmit(createDocumentTag)}>
 						OK
