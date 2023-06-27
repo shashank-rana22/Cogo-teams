642272e3cd6506293c0e146dcac1c19aa29b5d20
Mayank Shahria <mayank.shahria@cogoport.com>
2023-06-27 11:45:12 +0700

    PR changes

--- packages/business-finance/page-components/COEFinance/constants/constant.tsx
+++ packages/business-finance/page-components/COEFinance/constants/constant.tsx
@@ -1,25 +1,15 @@
-import {
-	IcMFrupee,
-	IcMFdollar,
-	IcMFeuro,
-	IcMFpound,
-	IcMFsingaporeDollar,
-	IcMFdong,
-} from '@cogoport/icons-react';
-import React from 'react';
+import CURRENCY_ICON_MAPPING from '@cogoport/globalization/constants/currencyIconMapping';
 
-const CURRENCY_DATA = [
-	{ id: '1', icon: <IcMFrupee width={25} height={25} />, text: 'INR' },
-	{ id: '2', icon: <IcMFdollar width={25} height={25} />, text: 'USD' },
-	{
-		id   : '3',
-		icon : <IcMFsingaporeDollar width={25} height={25} />,
-		text : 'SGD',
-	},
-	{ id: '4', icon: <IcMFpound width={25} height={25} />, text: 'GBP' },
-	{ id: '5', icon: <IcMFeuro width={25} height={25} />, text: 'EUR' },
-	{ id: '6', icon: <IcMFdong width={25} height={25} />, text: 'VND' },
-];
+const CURRENCY_DATA = Object.keys(CURRENCY_ICON_MAPPING).map((key, index) => {
+	const Icon = CURRENCY_ICON_MAPPING[key];
+
+	return ({
+		id   : index,
+		icon : <Icon width={25} height={25} />,
+		text : key,
+	});
+});
+
 const POC_DATA_MAPPING = [
 	{ id: '1', label: 'Customer Information' },
 	{ id: '2', label: 'Timeline' },
