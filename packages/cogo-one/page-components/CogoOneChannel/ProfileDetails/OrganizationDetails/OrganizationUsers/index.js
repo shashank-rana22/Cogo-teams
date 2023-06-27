fb97ee476a066d58ffa53af89883372e88244eb5
Lachiramnaik1330 <malothu.lachiramnaik@cogoport.com>
2023-06-27 19:28:31 +0530

    added workscope

--- packages/cogo-one/page-components/CogoOneChannel/ProfileDetails/OrganizationDetails/OrganizationUsers/index.js
+++ packages/cogo-one/page-components/CogoOneChannel/ProfileDetails/OrganizationDetails/OrganizationUsers/index.js
@@ -1,16 +1,40 @@
-import { cl } from '@cogoport/components';
+import { cl, Tooltip } from '@cogoport/components';
 import { IcMCall } from '@cogoport/icons-react';
 import { useDispatch } from '@cogoport/store';
 import { setProfileState } from '@cogoport/store/reducers/profile';
+import { startCase } from '@cogoport/utils';
 
 import styles from './styles.module.css';
 
+const MAX_SHOW_LENGTH = 2;
+const MIN_SHOW_LENGTH = 0;
 function OrganizationUsers({
 	user = {},
 	hasVoiceCallAccess,
 }) {
 	const dispatch = useDispatch();
-	const { user_id, email, mobile_country_code, mobile_number, name, organization_id } = user || {};
+	const { user_id, email, mobile_country_code, mobile_number, name, organization_id, work_scopes = [] } = user || {};
+
+	const lessList = (work_scopes || []).slice(MIN_SHOW_LENGTH, MAX_SHOW_LENGTH);
+	const moreList = (work_scopes || []).slice(MAX_SHOW_LENGTH);
+	const showMoreList = (work_scopes || []).length > MAX_SHOW_LENGTH;
+
+	const toolTipContent = (
+		<div>
+			{(moreList || []).map((item) => (
+				<div className={styles.scope_name} key={item}>{startCase(item)}</div>
+			))}
+		</div>
+	);
+
+	const toolTipComp = (
+		<Tooltip content={toolTipContent} theme="light" placement="bottom">
+			<div className={styles.more_tags}>
+				{moreList?.length}
+				+
+			</div>
+		</Tooltip>
+	);
 
 	const handleCall = () => {
 		if (mobile_number && hasVoiceCallAccess) {
@@ -56,6 +80,11 @@ function OrganizationUsers({
 					{mobile_country_code}
 					{mobile_number || '-'}
 				</div>
+			</div>
+
+			<div className={styles.user_work_scope}>
+				{(lessList || []).map((item) => <div className={styles.scope_name} key={item}>{startCase(item)}</div>)}
+				{showMoreList && toolTipComp}
 			</div>
 		</div>
 
