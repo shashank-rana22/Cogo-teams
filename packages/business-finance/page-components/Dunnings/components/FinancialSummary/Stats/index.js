f9ffdd6350112cf403700feedf9f5c1b20495acb
Nagendra Singh <nagendra.singh@cogoport.com>
2023-06-27 13:24:21 +0530

    feat: graph fixed and toggle added

--- packages/business-finance/page-components/Dunnings/components/FinancialSummary/Stats/index.js
+++ packages/business-finance/page-components/Dunnings/components/FinancialSummary/Stats/index.js
@@ -1,81 +1,99 @@
 import { Bar } from '@cogoport/charts/bar';
-import React from 'react';
+import { Toggle } from '@cogoport/components';
+import { IcMInfo } from '@cogoport/icons-react';
+import React, { useState } from 'react';
+
+import styles from './styles.module.css';
 
 function Stats() {
+	const [isGraphView, setIsGraphView] = useState(true);
 	const keys = ['collected', 'totalOutstanding'];
 	const myData = [
 		{
 			month            : 'Jan',
 			collected        : 100,
-			//   collectedColor        : 'hsl(217, 90%, 61%)',
 			totalOutstanding : 144,
-			// totalOutstandingColor : 'hsl(313, 70%, 50%)',
 		},
 		{
 			month            : 'Feb',
 			collected        : 100,
-			// collectedColor        : 'hsl(217, 90%, 61%)',
 			totalOutstanding : 144,
-			// totalOutstandingColor : 'hsl(313, 70%, 100%)',
 		},
 		{
 			month            : 'Mar',
 			collected        : 100,
-			// collectedColor        : 'hsl(217, 90%, 61%)',
 			totalOutstanding : 144,
-			// totalOutstandingColor : 'hsl(313, 70%, 50%)',
 		},
 	];
 
 	return (
 		<div>
-			bar chart here
-			<div>
-				<Bar
-					className="barGraph"
-					// ref={forwardRef}
-					width={900}
-					height={500}
-					margin={{ top: 60, right: 80, bottom: 60, left: 80 }}
-					data={myData}
-					indexBy="month"
-					keys={keys}
-					padding={0.2}
-					labelTextColor="inherit:darker(1.4)"
-					labelSkipWidth={16}
-					labelSkipHeight={16}
-					layout="vertical"
-					groupMode="stack"
-					enableGridY={false}
-					enableGridX
-					legends={[
-						{
-							anchor            : 'bottom-right',
-							direction         : 'column',
-							justify           : false,
-							translateX        : 80,
-							translateY        : 0,
-							itemsSpacing      : 0,
-							itemDirection     : 'left-to-right',
-							itemWidth         : 80,
-							itemHeight        : 20,
-							itemOpacity       : 0.75,
-							symbolSize        : 12,
-							symbolShape       : 'circle',
-							symbolBorderColor : 'rgba(0, 0, 0, .5)',
-							effects           : [
-								{
-									on    : 'hover',
-									style : {
-										itemBackground : 'rgba(0, 0, 0, .03)',
-										itemOpacity    : 1,
-									},
-								},
-							],
-						},
-					]}
-				/>
+			<div className={styles.header}>
+				<div className={styles.flex_align}>
+					<div className={styles.subject}>Statistics</div>
+					<div><IcMInfo /></div>
+				</div>
+				<div>
+					<Toggle
+						name="view"
+						size="md"
+						disabled={false}
+						onLabel="Linear View"
+						offLabel="Graph View"
+						onChange={() => setIsGraphView(!isGraphView)}
+					/>
+				</div>
 			</div>
+			{isGraphView ? (
+				<div>
+					<Bar
+						className="barGraph"
+						colors={['#DDEBC0', '#ACDADF']}
+						width={900}
+						height={500}
+						margin={{ top: 60, right: 80, bottom: 60, left: 80 }}
+						data={myData}
+						indexBy="month"
+						keys={keys}
+						padding={0.2}
+						labelTextColor="inherit:darker(1.4)"
+						labelSkipWidth={16}
+						labelSkipHeight={16}
+						layout="vertical"
+						groupMode="stack"
+						enableGridY={false}
+						enableGridX
+						legends={[
+							{
+								anchor            : 'bottom-right',
+								direction         : 'column',
+								justify           : false,
+								translateX        : 80,
+								translateY        : 0,
+								itemsSpacing      : 0,
+								itemDirection     : 'left-to-right',
+								itemWidth         : 80,
+								itemHeight        : 20,
+								itemOpacity       : 0.75,
+								symbolSize        : 12,
+								symbolShape       : 'circle',
+								symbolBorderColor : 'rgba(0, 0, 0, .5)',
+								effects           : [
+									{
+										on    : 'hover',
+										style : {
+											itemBackground : 'rgba(0, 0, 0, .03)',
+											itemOpacity    : 1,
+										},
+									},
+								],
+							},
+						]}
+					/>
+				</div>
+			)
+				: null}
+
 		</div>
 	);
 }
