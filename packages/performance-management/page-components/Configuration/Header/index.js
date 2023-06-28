19f3d6475592c0d0ccd76249ad7289b6983e470c
prachidewangan26 <prachi.dewangan@cogoport.com>
2023-06-28 13:05:05 +0530

    folder structure changed

--- packages/performance-management/page-components/Configuration/Header/index.js
+++
@@ -1,9 +0,0 @@
-import styles from './styles.module.css';
-
-function Header() {
-	return (
-		<div className={styles.container}>Performance Management Configuration</div>
-	);
-}
-
-export default Header;
--- packages/performance-management/pages/performance-management/configuration/index.js
+++ packages/performance-management/pages/performance-management/configuration/index.js
@@ -1,2 +1,2 @@
 // eslint-disable-next-line import/no-unresolved
-export { default } from '@cogoport/performance-management/page-components/Configuration/Dashboard';
+export { default } from '@cogoport/performance-management/page-components/Configuration/Tabomponent';
