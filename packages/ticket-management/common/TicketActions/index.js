c12de4649910b8e23a04baac9662067ddf039095
amandeep2508 <amandeep.chauhan@cogoport.com>
2023-06-30 22:23:13 +0530

    fix

--- packages/ticket-management/common/TicketActions/index.js
+++ packages/ticket-management/common/TicketActions/index.js
@@ -8,7 +8,7 @@ function getActionMapping({ status, isClosureAuthorizer }) {
 		if (isClosureAuthorizer) {
 			return ['resolve'];
 		}
-		return ['resolve_requested'];
+		return ['resolve_request'];
 	}
 
 	if ((['pending', 'resolve_requested'].includes(status) && isClosureAuthorizer)) {
@@ -26,22 +26,37 @@ function TicketActions({
 	status,
 	isModal,
 	handleTicket,
+	setShowReassign = () => {},
 	isClosureAuthorizer,
 }) {
 	const actionMappings = getActionMapping({ status, isClosureAuthorizer });
 
 	return (
-		<div className={styles.pending_actions}>
+		<div>
 			{actionMappings.map((item) => (
-				<Button
-					key={item}
-					size={isModal ? 'md' : 'sm'}
-					themeType={isModal ? 'primary' : 'linkUi'}
-					className={styles.reopen_resolve}
-					onClick={(e) => handleTicket(e, { actionType: item })}
-				>
-					{startCase(item)}
-				</Button>
+				<div className={styles.pending_actions} key={`button_${item}_container`}>
+					{(isModal && item !== 'reopen') && (
+						<Button
+							key="reassign_button"
+							size="sm"
+							themeType="secondary"
+							className={styles.reopen_resolve}
+							onClick={() => setShowReassign(true)}
+						>
+							Reassign
+						</Button>
+					)}
+
+					<Button
+						key={item}
+						size="sm"
+						themeType={isModal ? 'primary' : 'linkUi'}
+						className={styles.reopen_resolve}
+						onClick={(e) => handleTicket(e, { actionType: item })}
+					>
+						{startCase(item)}
+					</Button>
+				</div>
 			))}
 		</div>
 	);
