a5a312965679c22336254d47b778a0c44158a3e9
Aakash Dinkar <aakash.dinkar@cogoport.com>
2023-07-03 16:00:20 +0530

    eslint stylelint fix

--- packages/shipment-fcl-freight-local/common/TimeLine/Loader/index.js
+++ packages/shipment-fcl-freight-local/common/TimeLine/Loader/index.js
@@ -2,15 +2,19 @@ import { Placeholder } from '@cogoport/components';
 
 import { container, big_circle, small_circle } from './styles.module.css';
 
+const RANDOM_BOOLEAN_DECIDER = 0.5;
+
+const NUMBER_OF_LOADERS = 16;
+
 function rendomBoolean() {
-	return Math.random() >= 0.5;
+	return Math.random() >= RANDOM_BOOLEAN_DECIDER;
 }
 
 export default function Loader() {
 	return (
 		<div className={container}>
 			<Placeholder className={big_circle} />
-			{Array(16).fill(null).map(() => (
+			{Array(NUMBER_OF_LOADERS).fill(null).map(() => (
 				<>
 					<Placeholder height="2px" style={{ flex: 1 }} />
 					<Placeholder className={rendomBoolean() ? big_circle : small_circle} />
