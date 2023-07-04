48a0b9ce00e656fe051223982bd808db26cbdaf2
Nagendra Singh <nagendra.singh@cogoport.com>
2023-07-04 19:57:50 +0530

    feat: yearly filter added

--- packages/business-finance/page-components/Dunnings/components/FinancialSummary/Stats/index.js
+++ packages/business-finance/page-components/Dunnings/components/FinancialSummary/Stats/index.js
@@ -1,4 +1,4 @@
-import { Toggle } from '@cogoport/components';
+import { Input, Popover, Toggle } from '@cogoport/components';
 import { IcMInfo } from '@cogoport/icons-react';
 import React, { useState } from 'react';
 
@@ -9,8 +9,48 @@ import LinearGraphView from './LinearGraphView';
 import StatsLoader from './StatsLoader';
 import styles from './styles.module.css';
 
+const MONTH_DIVISION = 3;
+const DECREMENT = 1;
+const INCREMENT = 1;
+const PREV_LIMIT = 5;
+const START_MONTH_INDEX = 3;
+const START_DAY_INDEX = 1;
+const END_MONTH_INDEX = 2;
+const END_DAY_INDEX = 31;
+const YEAR_END_INCREMENT = 2;
+
+function getFinancialYear(date) {
+	const year = date.getFullYear();
+	const month = date.getMonth();
+	if (month < MONTH_DIVISION) {
+		return year - DECREMENT;
+	} return year;
+}
+
 function Stats() {
 	const [isGraphView, setIsGraphView] = useState(true);
+	const [inputValue, setInputValue] = useState('');
+	const [visible, setVisible] = useState(false);
+	const [yearHandle, setYearHandle] = useState(false);
+	const [yearFilters, setYearFilters] = useState([]);
+
+	const currentYear = new Date().getFullYear();
+	const today = new Date();
+
+	const FINANCIAL_YEARS = [];
+	for (let i = 1; i <= PREV_LIMIT; i += INCREMENT) {
+		const yearStart = new Date(today.getFullYear() - i, START_MONTH_INDEX, START_DAY_INDEX);
+		const yearEnd = new Date(today.getFullYear() - i + YEAR_END_INCREMENT, END_MONTH_INDEX, END_DAY_INDEX);
+		const financialYear = `${getFinancialYear(yearStart)}-${getFinancialYear(yearEnd)}`;
+		FINANCIAL_YEARS.push(financialYear);
+	}
+
+	const CALENDER_YEAR = [];
+	for (let i = 0; i < PREV_LIMIT; i += INCREMENT) {
+		const year = `${currentYear - i}`;
+		CALENDER_YEAR.push(year);
+	}
+
 	const { statsData, loading } = useGetMonthwiseStats({ statsFilter: {} });
 
 	const linearData = [
@@ -41,12 +81,91 @@ function Stats() {
 		);
 	});
 
+	const onClickFinancialYear = (year, type) => {
+		let yearType = 'CY';
+		if (type === FINANCIAL_YEARS) {
+			yearType = 'FY';
+		}
+		const years = year?.split('-');
+		setYearFilters(years);
+		setInputValue(`${yearType} - ${year}`);
+		setVisible(false);
+	};
+
+	const renderYearData = (years) => (
+		<div>
+			{years.map((year) => (
+				<div key={year} style={{ marginBottom: '10px', cursor: year === 'financialYears' && 'pointer' }}>
+					<div
+						key={year}
+						role="presentation"
+						onClick={() => onClickFinancialYear(year, years)}
+					>
+						{year}
+
+					</div>
+					<div className={styles.year_bottom_border} />
+				</div>
+			))}
+		</div>
+	);
+
+	const yearHandleChange = () => {
+		if (yearHandle) {
+			return renderYearData(CALENDER_YEAR);
+		}
+		return renderYearData(FINANCIAL_YEARS);
+	};
+
+	const content = () => (
+
+		<Popover
+			placement="right"
+			caret={false}
+			render={yearHandleChange()}
+			className={styles.years_styles}
+			visible={visible}
+		>
+			<>
+				<div
+					className={styles.data_styles}
+					onClick={() => { setYearHandle(true); setVisible(true); }}
+					role="presentation"
+				>
+					Calendar Year
+
+				</div>
+				<div className={styles.borders} />
+				<div
+					className={styles.data_styles}
+					onClick={() => { setYearHandle(false); setVisible(true); }}
+					role="presentation"
+				>
+					Financial Year
+
+				</div>
+			</>
+		</Popover>
+
+	);
+
 	return (
 		<div>
 			<div className={styles.header}>
 				<div className={styles.flex_align}>
 					<div className={styles.subject}>Statistics</div>
 					<div><IcMInfo /></div>
+					<div style={{ marginTop: '10px', marginLeft: '20px' }}>
+						<Popover render={content()} caret={false} placement="bottom">
+							<div className={styles.input_div}>
+								<Input
+									placeholder="Select Year Mode"
+									size="sm"
+									value={inputValue || `CY - ${currentYear}`}
+								/>
+							</div>
+						</Popover>
+					</div>
 				</div>
 				<div>
 					<Toggle
