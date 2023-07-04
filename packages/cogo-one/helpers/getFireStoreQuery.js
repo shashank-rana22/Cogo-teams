40798c34bf3f291794beb2443bda68b984fa3ccf
Sandeep Nalabolu <sandeep.nalabolu@cogoport.com>
2023-07-04 13:52:23 +0530

    lint

--- packages/cogo-one/helpers/getFireStoreQuery.js
+++ packages/cogo-one/helpers/getFireStoreQuery.js
@@ -39,30 +39,34 @@ function getFireStoreQuery({
 			let filtersToBeApplied = [];
 
 			const queryFilterMapping = {
-				channels   : [where('channel_type', 'in', appliedFilters[item])],
-				escalation : [where('chat_status', '==', appliedFilters[item])],
-				chat_tags  : [where('chat_tags', 'array-contains', appliedFilters?.chat_tags)],
-				mobile_no  : [where('mobile_no', '==', appliedFilters?.mobile_no)],
+				channels:
+					[where('channel_type', 'in', appliedFilters[item])],
+				escalation:
+					[where('chat_status', '==', appliedFilters[item])],
+				chat_tags:
+					[where('chat_tags', 'array-contains', appliedFilters?.chat_tags)],
+				mobile_no:
+					[where('mobile_no', '==', appliedFilters?.mobile_no)],
 				assigned_to:
-				isBotSession
-					? [where('spectators_ids', 'array-contains', filterId)]
-					: [where('support_agent_id', '==', filterId)],
+					isBotSession
+						? [where('spectators_ids', 'array-contains', filterId)]
+						: [where('support_agent_id', '==', filterId)],
 				status:
-				appliedFilters[item] === 'unread'
-					? [where('has_admin_unread_messages', '==', true)]
-					: [],
+					appliedFilters[item] === 'unread'
+						? [where('has_admin_unread_messages', '==', true)]
+						: [],
 				shipment_filters:
-				appliedFilters[item]?.includes('likely_to_book_shipment')
-					? [where('is_likely_to_book_shipment', '==', true)]
-					: [],
+					appliedFilters[item]?.includes('likely_to_book_shipment')
+						? [where('is_likely_to_book_shipment', '==', true)]
+						: [],
 				'15_min_filter':
-				appliedFilters[item]?.includes('seen_by_user')
-					? [
-						where('last_message_document.conversation_type', '==', 'received'),
-						where('last_message_document.message_type', '==', 'text'),
-						where('last_message_document.created_at', '<=', epochTimestamp),
-						orderBy('last_message_document.created_at', 'desc'),
-					] : [],
+					appliedFilters[item]?.includes('seen_by_user')
+						? [
+							where('last_message_document.conversation_type', '==', 'received'),
+							where('last_message_document.message_type', '==', 'text'),
+							where('last_message_document.created_at', '<=', epochTimestamp),
+							orderBy('last_message_document.created_at', 'desc'),
+						] : [],
 			};
 
 			filtersToBeApplied = queryFilterMapping?.[item] || [];
