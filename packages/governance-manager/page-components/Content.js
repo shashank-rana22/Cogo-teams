9c9f306727883e4a555ba69084f7385fe53ddb6f
mdmurtaza <murtaza-cogo@cogoport.com>
2023-07-04 12:19:23 +0530

    status-bar

--- packages/governance-manager/page-components/Content.js
+++ packages/governance-manager/page-components/Content.js
@@ -1,11 +1,13 @@
 import ListSupplier from './ListSupplier';
 import Stats from './Stats';
+import StatusBar from './StatusBar';
 
 function Content() {
 	return (
 		<>
 			Content
 			<Stats />
+			<StatusBar />
 			<ListSupplier />
 		</>
 	);
