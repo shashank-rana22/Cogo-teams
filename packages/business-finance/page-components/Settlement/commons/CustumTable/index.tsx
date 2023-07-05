89396a8592dfc432ff40d68ccbb3bac0502f04a9
man-ishh <manishd.kumar@cogoport.com>
2023-07-05 15:25:15 +0700

    ststic variable outside of componnent

--- packages/business-finance/page-components/Settlement/commons/CustumTable/index.tsx
+++ packages/business-finance/page-components/Settlement/commons/CustumTable/index.tsx
@@ -36,10 +36,10 @@ interface Props {
 	filters: { sortType?: string };
 }
 
+const PAGE_SIZE = 10;
+
 function CustumTable({ data = {}, onPageChange, loading, refetch, setFilters, filters }:Props) {
 	const { list = [], pageNo = 1, totalRecords = 0 } = (data as DataInterface || {});
-
-	const PAGE_SIZE = 10;
 
 	return (
 		<div className={styles.table}>
