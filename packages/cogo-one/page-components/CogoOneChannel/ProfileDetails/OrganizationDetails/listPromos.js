9dde92bc8694b95f32784ee9e2c2f32376735878
Lachiramnaik1330 <malothu.lachiramnaik@cogoport.com>
2023-07-05 18:22:34 +0530

    changes

--- packages/cogo-one/page-components/CogoOneChannel/ProfileDetails/OrganizationDetails/listPromos.js
+++ packages/cogo-one/page-components/CogoOneChannel/ProfileDetails/OrganizationDetails/listPromos.js
@@ -6,16 +6,20 @@ import PromocodeThumbnail from './PromocodeThumbnail';
 import styles from './styles.module.css';
 
 function ListPromos({ list = [] }) {
-	return isEmpty(list) ? (
-		<div className={styles.promotion_cards_empty_state}>
-			<Image
-				src={GLOBAL_CONSTANTS.image_url.empty_promocode}
-				alt="promocode"
-				width={200}
-				height={200}
-			/>
-		</div>
-	) : (
+	if (isEmpty(list)) {
+		return (
+			<div className={styles.promotion_cards_empty_state}>
+				<Image
+					src={GLOBAL_CONSTANTS.image_url.empty_promocode}
+					alt="promocode"
+					width={200}
+					height={200}
+				/>
+			</div>
+		);
+	}
+
+	return (
 		<div className={styles.promotion_cards}>
 			<div className={styles.wrapper}>
 				<h3>
