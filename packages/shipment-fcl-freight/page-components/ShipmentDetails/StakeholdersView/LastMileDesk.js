19eb634577f5df067bc261cc6523ea79a901ba4d
Rajatuba-Cogoport <rajat.uba@cogoport.com>
2023-07-06 15:16:53 +0530

    removed last mile toggle

--- packages/shipment-fcl-freight/page-components/ShipmentDetails/StakeholdersView/LastMileDesk.js
+++ packages/shipment-fcl-freight/page-components/ShipmentDetails/StakeholdersView/LastMileDesk.js
@@ -1,13 +1,12 @@
-import { Tabs, TabPanel, Loader, Button, Toggle } from '@cogoport/components';
+import { Tabs, TabPanel, Loader, Button } from '@cogoport/components';
 import { ShipmentDetailContext } from '@cogoport/context';
 import { IcMRefresh } from '@cogoport/icons-react';
 import { Tracking } from '@cogoport/ocean-modules';
-import getNavigationFromUrl from '@cogoport/request/helpers/getNavigationFromUrl';
 import { ShipmentChat } from '@cogoport/shipment-chat';
 import { ShipmentMails } from '@cogoport/shipment-mails';
 import { isEmpty } from '@cogoport/utils';
 import { useRouter } from 'next/router';
-import React, { useMemo, useState, useEffect, useCallback } from 'react';
+import React, { useMemo, useState, useEffect } from 'react';
 
 import CancelDetails from '../../../common/CancelDetails';
 import DocumentHoldDetails from '../../../common/DocumentHoldDetails';
@@ -33,16 +32,6 @@ function LastMileDesk({ get = {}, activeStakeholder = '' }) {
 	const [activeTab, setActiveTab] = useState('overview');
 
 	const { shipment_data, isGettingShipment, getShipmentStatusCode, container_details } = get || {};
-
-	const handleVersionChange = useCallback(() => {
-		const navigation = getNavigationFromUrl();
-
-		const newHref = `${window.location.origin}/${router?.query?.partner_id}/shipments/${shipment_data?.id}
-		${navigation ? `?navigation=${navigation}` : ''}`;
-
-		window.location.replace(newHref);
-		window.sessionStorage.setItem('prev_nav', newHref);
-	}, [router?.query?.partner_id, shipment_data?.id]);
 
 	const rollover_containers = (container_details || []).filter(
 		(container) => container?.rollover_status === 'requested',
@@ -117,12 +106,6 @@ function LastMileDesk({ get = {}, activeStakeholder = '' }) {
 					<RolloveDetails />
 
 					<div className={styles.toggle_chat}>
-						<Toggle
-							size="md"
-							onLabel="Old"
-							offLabel="New"
-							onChange={handleVersionChange}
-						/>
 						<ShipmentChat />
 					</div>
 				</div>
