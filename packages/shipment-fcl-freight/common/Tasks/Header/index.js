57ab7d328c390d8493ea5478f04555c16967c051
shivani-gupta19 <shivani.gupta@cogoport.com>
2023-07-06 13:42:47 +0530

    fixes

--- packages/shipment-fcl-freight/common/Tasks/Header/index.js
+++ packages/shipment-fcl-freight/common/Tasks/Header/index.js
@@ -1,5 +1,6 @@
-import { Button, Toggle } from '@cogoport/components';
+import { Button, Toggle, Popover } from '@cogoport/components';
 import { ShipmentDetailContext } from '@cogoport/context';
+import GLOBAL_CONSTANTS from '@cogoport/globalization/constants/globals';
 import { useContext, useState } from 'react';
 
 import BookingRequirements from './BookingRequirements';
@@ -14,12 +15,16 @@ function Header({
 	setShowMyTasks = () => {},
 }) {
 	const [showBookingReq, setShowBookingReq] = useState(false);
+	const [visible, setVisible] = useState(false);
 
 	const contextValues = useContext(ShipmentDetailContext);
-	const { activeStakeholder, shipment_data } = contextValues || {};
+	const { activeStakeholder, shipment_data, primary_service } = contextValues || {};
 
 	const showBookingRequirementsCondition = ['superadmin', 'booking_desk', 'booking_desk_manager', 'so1_so2_ops']
 		.includes(activeStakeholder) && shipment_data?.state !== 'shipment_received';
+
+	const showSupplyRemarks = ['superadmin', 'admin', 'prod_process_owner', 'document_desk', 'document_desk_manager']
+		.includes(activeStakeholder);
 
 	return (
 		<div className={styles.container}>
@@ -30,6 +35,17 @@ function Header({
 				</div>
 
 				<div className={styles.right_content}>
+					{showSupplyRemarks ? (
+						<Popover
+							placement="bottom"
+							trigger="mouseenter"
+							caret={false}
+							render={`Supply Remarks: ${primary_service?.booking_preferences?.
+								[GLOBAL_CONSTANTS.zeroth_index].remarks}`}
+						>
+							<Button size="md" themeType="link" onClick={() => setVisible(!visible)}>Remarks</Button>
+						</Popover>
+					) : null }
 					<div className={styles.toggle_container}>
 						<div style={{ marginTop: '12px' }}>Hide completed tasks</div>
 						<Toggle
