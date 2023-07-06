a05a57c7e7e4ee48ce1cd32b0cd497d20f958d37
vineet-cogo <vineet.kumar@cogoport.com>
2023-07-06 17:22:51 +0530

    tab changes done

--- packages/shipment-fcl-freight/common/Overview/Services/ServiceDetails/Details/index.js
+++ packages/shipment-fcl-freight/common/Overview/Services/ServiceDetails/Details/index.js
@@ -1,22 +1,28 @@
 import { cl } from '@cogoport/components';
 import { getByKey, omit } from '@cogoport/utils';
 import React, { useState } from 'react';
+import { v4 as uuid } from 'uuid';
 
 import getConfigs from '../../configurations/get-configs';
 
 import Item from './Item';
 import styles from './styles.module.css';
 
+const MIN_LENGTH = 1;
+const START_ARRAY = 0;
+const FIRST_CHAR = 0;
+const SLICE_FROM = 1;
+
 function Details({ serviceData = [] }) {
 	const {
 		service_type, state, free_days_demurrage_destination,
 		free_days_demurrage_origin, free_days_detention_destination, free_days_detention_origin,
-	} = serviceData[0];
+	} = serviceData[START_ARRAY];
 
-	const serviceInitialKeys = {};
+	const SERVICE_INITIAL_KEYS = {};
 
 	(serviceData || []).forEach((data) => {
-		serviceInitialKeys[data?.container_size] = {
+		SERVICE_INITIAL_KEYS[data?.container_size] = {
 			container_size             : data?.container_size,
 			commodity                  : data?.commodity,
 			containers_count           : data?.containers_count,
@@ -26,7 +32,7 @@ function Details({ serviceData = [] }) {
 		};
 	});
 
-	const [multiServiceType, setMultiServiceType] = useState(Object.keys(serviceInitialKeys)?.[0]);
+	const [multiServiceType, setMultiServiceType] = useState(Object.keys(SERVICE_INITIAL_KEYS)?.[START_ARRAY]);
 
 	const service_items_key = getConfigs(service_type).details || {};
 
@@ -37,26 +43,37 @@ function Details({ serviceData = [] }) {
 		free_days_detention_origin,
 	};
 
-	const excludedKeys = [...Object.keys(serviceInitialKeys[(Object.keys(serviceInitialKeys)?.[0])]),
+	const excludedKeys = [...Object.keys(SERVICE_INITIAL_KEYS[(Object.keys(SERVICE_INITIAL_KEYS)?.[START_ARRAY])]),
 		...Object.keys(freeDays)];
 
-	const remainingServiceData = omit(serviceData?.[0], excludedKeys);
+	const remainingServiceData = omit(serviceData?.[START_ARRAY], excludedKeys);
 
 	return (
 		<div className={cl`${styles.container} ${styles[state]}`}>
 
-			{Object.keys(serviceInitialKeys).length > 1 ?	(
+			{Object.keys(SERVICE_INITIAL_KEYS).length > MIN_LENGTH ?	(
 				<div className={cl`${styles.multiservices_heading} ${styles[state]}`}>
-					{(Object.keys(serviceInitialKeys)).map((key) => (
+					{(Object.keys(SERVICE_INITIAL_KEYS)).map((val) => (
 						<div
-							className={`${styles.mainservice_tabs} 
-							${multiServiceType === key ? styles.active : null} 
+							key={uuid()}
+							className={cl`${styles.mainservice_tabs} 
+							${multiServiceType === val ? styles.active : null} 
 							${styles[state]}`}
 							role="button"
 							tabIndex={0}
-							onClick={() => setMultiServiceType(key)}
+							onClick={() => setMultiServiceType(val)}
 						>
-							{`${key} ft`}
+							{`${val} ft`}
+							{' '}
+							|
+							{' '}
+							{SERVICE_INITIAL_KEYS[val].container_type.charAt(FIRST_CHAR).toUpperCase()
+							+ SERVICE_INITIAL_KEYS[val].container_type.slice(SLICE_FROM)}
+							{' '}
+							|
+							{' '}
+							{SERVICE_INITIAL_KEYS[val].commodity.charAt(FIRST_CHAR).toUpperCase()
+							+ SERVICE_INITIAL_KEYS[val].commodity.slice(SLICE_FROM)}
 						</div>
 					))}
 				</div>
@@ -64,22 +81,22 @@ function Details({ serviceData = [] }) {
 
 			<div className={cl`${styles.multiservice} ${styles[state]}`}>
 				{(service_items_key || []).map((element) => (getByKey(
-					serviceInitialKeys[multiServiceType],
-					element?.key,
+					SERVICE_INITIAL_KEYS[multiServiceType],
+					element?.val,
 				) ? (
-					<Item state={state} label={element} detail={serviceInitialKeys[multiServiceType]} />
+					<Item key={uuid()} state={state} label={element} detail={SERVICE_INITIAL_KEYS[multiServiceType]} />
 					) : null))}
 			</div>
 
 			<div className={styles.remaining_keys}>
-				{(service_items_key || {}).map((element) => (getByKey(remainingServiceData, element.key) ? (
-					<Item state={state} label={element} detail={remainingServiceData} />
+				{(service_items_key || {}).map((element) => (getByKey(remainingServiceData, element.val) ? (
+					<Item key={uuid()} state={state} label={element} detail={remainingServiceData} />
 				) : null))}
 			</div>
 
 			<div className={styles.free_days}>
-				{(service_items_key || {}).map((element) => (getByKey(freeDays, element.key) ? (
-					<Item state={state} label={element} detail={freeDays} />
+				{(service_items_key || {}).map((element) => (getByKey(freeDays, element.val) ? (
+					<Item key={uuid()} state={state} label={element} detail={freeDays} />
 				) : null))}
 			</div>
 		</div>
