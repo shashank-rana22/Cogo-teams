f9f814b9f9bccde1de4459aad70423123cdc6c67
vineet-cogo <vineet.kumar@cogoport.com>
2023-07-07 15:59:31 +0530

    warnings fixed

--- packages/shipment-fcl-freight/common/Overview/Services/ServiceDetails/Details/index.js
+++ packages/shipment-fcl-freight/common/Overview/Services/ServiceDetails/Details/index.js
@@ -1,7 +1,7 @@
-import { cl } from '@cogoport/components';
+import { cl, Button } from '@cogoport/components';
+import GLOBAL_CONSTANTS from '@cogoport/globalization/constants/globals';
 import { getByKey, omit } from '@cogoport/utils';
-import React, { useState } from 'react';
-import { v4 as uuid } from 'uuid';
+import React, { useState, useMemo } from 'react';
 
 import getConfigs from '../../configurations/get-configs';
 
@@ -9,15 +9,23 @@ import Item from './Item';
 import styles from './styles.module.css';
 
 const MIN_LENGTH = 1;
-const START_ARRAY = 0;
 const FIRST_CHAR = 0;
 const SLICE_FROM = 1;
 
 function Details({ serviceData = [] }) {
+	const keysForPreference = useMemo(
+		() => Array(serviceData.length).fill(null).map(() => Math.random()),
+		[serviceData.length],
+	);
+
 	const {
-		service_type, state, free_days_demurrage_destination,
-		free_days_demurrage_origin, free_days_detention_destination, free_days_detention_origin,
-	} = serviceData[START_ARRAY];
+		service_type,
+		state,
+		free_days_demurrage_destination,
+		free_days_demurrage_origin,
+		free_days_detention_destination,
+		free_days_detention_origin,
+	} = serviceData[GLOBAL_CONSTANTS.zeroth_index];
 
 	const SERVICE_INITIAL_KEYS = {};
 
@@ -32,7 +40,8 @@ function Details({ serviceData = [] }) {
 		};
 	});
 
-	const [multiServiceType, setMultiServiceType] = useState(Object.keys(SERVICE_INITIAL_KEYS)?.[START_ARRAY]);
+	const [multiServiceType, setMultiServiceType] = useState(Object.keys(SERVICE_INITIAL_KEYS)
+		?.[GLOBAL_CONSTANTS.zeroth_index]);
 
 	const service_items_key = getConfigs(service_type).details || {};
 
@@ -43,23 +52,26 @@ function Details({ serviceData = [] }) {
 		free_days_detention_origin,
 	};
 
-	const excludedKeys = [...Object.keys(SERVICE_INITIAL_KEYS[(Object.keys(SERVICE_INITIAL_KEYS)?.[START_ARRAY])]),
-		...Object.keys(freeDays)];
+	const excludedKeys = [
+		...Object.keys(SERVICE_INITIAL_KEYS[Object.keys(SERVICE_INITIAL_KEYS)
+			?.[GLOBAL_CONSTANTS.zeroth_index]]),
+		...Object.keys(freeDays),
+	];
 
-	const remainingServiceData = omit(serviceData?.[START_ARRAY], excludedKeys);
+	const remainingServiceData = omit(serviceData?.[GLOBAL_CONSTANTS.zeroth_index], excludedKeys);
 
 	return (
 		<div className={cl`${styles.container} ${styles[state]}`}>
 
-			{Object.keys(SERVICE_INITIAL_KEYS).length > MIN_LENGTH ?	(
+			{Object.keys(SERVICE_INITIAL_KEYS).length > MIN_LENGTH ? (
 				<div className={cl`${styles.multiservices_heading} ${styles[state]}`}>
-					{(Object.keys(SERVICE_INITIAL_KEYS)).map((val) => (
-						<div
-							key={uuid()}
-							className={cl`${styles.mainservice_tabs} 
-							${multiServiceType === val ? styles.active : null} 
-							${styles[state]}`}
-							role="button"
+					{(Object.keys(SERVICE_INITIAL_KEYS)).map((val, i) => (
+						<Button
+							key={keysForPreference[i]}
+							themeType="none"
+							className={cl`${styles.mainservice_tabs}
+										${multiServiceType === val ? styles.active : null}
+										${styles[state]}`}
 							tabIndex={0}
 							onClick={() => setMultiServiceType(val)}
 						>
@@ -74,29 +86,34 @@ function Details({ serviceData = [] }) {
 							{' '}
 							{SERVICE_INITIAL_KEYS[val].commodity.charAt(FIRST_CHAR).toUpperCase()
 							+ SERVICE_INITIAL_KEYS[val].commodity.slice(SLICE_FROM)}
-						</div>
+						</Button>
 					))}
 				</div>
 			) : null}
 
 			<div className={cl`${styles.multiservice} ${styles[state]}`}>
-				{(service_items_key || []).map((element) => (getByKey(
+				{(service_items_key || []).map((element, i) => (getByKey(
 					SERVICE_INITIAL_KEYS[multiServiceType],
-					element?.val,
+					element?.key,
 				) ? (
-					<Item key={uuid()} state={state} label={element} detail={SERVICE_INITIAL_KEYS[multiServiceType]} />
+					<Item
+						key={keysForPreference[i]}
+						state={state}
+						label={element}
+						detail={SERVICE_INITIAL_KEYS[multiServiceType]}
+					/>
 					) : null))}
 			</div>
 
 			<div className={styles.remaining_keys}>
-				{(service_items_key || {}).map((element) => (getByKey(remainingServiceData, element.val) ? (
-					<Item key={uuid()} state={state} label={element} detail={remainingServiceData} />
+				{(service_items_key || {}).map((element, i) => (getByKey(remainingServiceData, element.key) ? (
+					<Item key={keysForPreference[i]} state={state} label={element} detail={remainingServiceData} />
 				) : null))}
 			</div>
 
 			<div className={styles.free_days}>
-				{(service_items_key || {}).map((element) => (getByKey(freeDays, element.val) ? (
-					<Item key={uuid()} state={state} label={element} detail={freeDays} />
+				{(service_items_key || {}).map((element, i) => (getByKey(freeDays, element.key) ? (
+					<Item key={keysForPreference[i]} state={state} label={element} detail={freeDays} />
 				) : null))}
 			</div>
 		</div>
