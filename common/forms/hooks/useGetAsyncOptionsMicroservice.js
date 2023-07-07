4f8dc4c35b27932bfb8e0a383e34b5d4b86516c5
amandeep2508 <amandeep.chauhan@cogoport.com>
2023-07-07 15:41:03 +0530

    fix

--- common/forms/hooks/useGetAsyncOptionsMicroservice.js
+++ common/forms/hooks/useGetAsyncOptionsMicroservice.js
@@ -92,8 +92,8 @@ function useGetAsyncOptionsMicroservice({
 			const res = await triggerSingle({
 				params: merge(params, (searchByq ? { q: value } : { filters: { [valueKey]: value } })),
 			});
-			return res?.data?.list?.[GLOBAL_CONSTANTS.zeroth_index] || res?.data?.[GLOBAL_CONSTANTS.zeroth_index]
-			|| null;
+			return res?.data?.list?.[GLOBAL_CONSTANTS.zeroth_index] || res?.data?.items[GLOBAL_CONSTANTS.zeroth_index]
+			|| res?.data?.[GLOBAL_CONSTANTS.zeroth_index] || null;
 		} catch (err) {
 			// console.log(err);
 			return {};
