ab939ecf7d6e0f94a0a2bcef78d66ad77dcd2c53
prachidewangan26 <prachi.dewangan@cogoport.com>
2023-07-09 23:44:57 +0530

    fixed

--- packages/new-employee-dashboard/page-components/TableView/useGetZipFile.js
+++ packages/new-employee-dashboard/page-components/TableView/useGetZipFile.js
@@ -5,22 +5,25 @@ import { useHarbourRequest } from '@cogoport/request';
 const onClickViewSampleFile = (url) => {
 	window.open(url, '_blank', 'noreferrer');
 };
-
 const useGetZipFile = () => {
-	const [{ data, loading }, trigger] = useHarbourRequest({
+	const [{ loading }, trigger] = useHarbourRequest({
 		url    : '/download_employee_documents_zip',
 		method : 'get',
 	}, { manual: true });
 
 	const downloadDocuments = async (id) => {
 		try {
-			await trigger({
+			const res = await trigger({
 				params: {
 					employee_detail_id: id,
 				},
 			});
 
-			onClickViewSampleFile(data?.zip_file_url);
+			const url = res?.data?.zip_file_url;
+
+			if (url) {
+				onClickViewSampleFile(url);
+			}
 		} catch (err) {
 			Toast.error(getApiErrorString(err?.response?.data) || 'Something went wrong');
 		}
