f1a6b721eb28718bad7ad71541c1a08507233a79
sushilr29 <sushil.rindhe@cogoport.com>
2023-07-10 16:58:57 +0700

    #deploy_on nexus1

--- packages/allocation/page-components/CRMFeedbackDashboard/Dashboard/constants/bulk-create-feedback-options.js
+++ packages/allocation/page-components/CRMFeedbackDashboard/Dashboard/constants/bulk-create-feedback-options.js
@@ -1,4 +1,6 @@
 export const OPTIONS = [
+	{ label: '2', value: '2' },
+	{ label: '10', value: '10' },
 	{ label: '25', value: '25' },
 	{ label: '50', value: '50' },
 	{ label: '100', value: '100' },
