7745c03a8905a93ca24512d4be7730ce73306f53
mayank-1546 <mayank.gangwar@cogoport.com>
2023-07-11 19:02:05 +0530

    fixes

--- packages/so2-surface/page-components/FTL/Filters/FilterBy/shipmentStates.js
+++ packages/so2-surface/page-components/FTL/Filters/FilterBy/shipmentStates.js
@@ -1,37 +1,31 @@
 const SHIPMENT_STATES = [
 	{
 		label : 'Awaiting Service Provider Confirmation',
-		value : 'Awaiting Service Provider Confirmation',
+		value : 'awaiting_service_provider_confirmation',
 	},
-
 	{
 		label : 'Confirmed by Service Provider',
-		value : 'Confirmed by Service Provider',
+		value : 'confirmed_by_service_provider',
 	},
-
 	{
 		label : 'Cargo Picked Up',
-		value : 'Cargo Picked Up',
+		value : 'cargo_picked_up',
 	},
-
 	{
 		label : 'Cargo dropped',
-		value : 'Cargo dropped',
+		value : 'cargo_dropped',
 	},
-
 	{
 		label : 'Completed',
-		value : 'Completed',
+		value : 'completed',
 	},
-
 	{
 		label : 'Aborted',
-		value : 'Aborted',
+		value : 'aborted',
 	},
-
 	{
 		label : 'Cancelled',
-		value : 'Cancelled',
+		value : 'cancelled',
 	},
 ];
 
