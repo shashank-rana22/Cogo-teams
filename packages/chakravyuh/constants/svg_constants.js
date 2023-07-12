13820b9cfa77e6cbc259acaced1d72836584ae5a
samir-cogoport <samir.dupare@cogoport.com>
2023-07-12 11:03:57 +0530

    setup

---
+++ packages/chakravyuh/constants/svg_constants.js
@@ -0,0 +1,17 @@
+/* eslint-disable max-len */
+export const PATH_STYLE = {
+	stroke           : '#F1CB0E',
+	strokeOpacity    : '1',
+	strokeWidth      : '3',
+	strokeDasharray  : 'none',
+	strokeLinecap    : 'butt',
+	strokeDashoffset : '0',
+	strokeLinejoin   : 'miter',
+	strokeMiterlimit : '4',
+	fill             : 'none',
+	fillRule         : 'nonzero',
+	opacity          : '1',
+};
+
+export const D_TOP = 'M 639.849 333.516 h 250.478 c 3.366 0 6.127 -2.743 6.127 -6.086 l 0.001 -53.977 v -198.352 c 0 -3.344 2.76 -6.086 6.126 -6.086 h 220.099';
+export const D_NEXT = 'M 639.849 333.516 h 250.478 c 3.366 0 6.127 -2.743 6.127 -6.086 v -76.418 c 0 -3.344 2.761 -6.086 6.127 -6.086 h 215.629';
