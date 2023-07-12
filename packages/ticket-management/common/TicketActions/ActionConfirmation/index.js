282c27b8a4bfdf3b4983a656948a95e811570b2f
amandeep2508 <amandeep.chauhan@cogoport.com>
2023-07-12 18:06:34 +0530

    fix

---
+++ packages/ticket-management/common/TicketActions/ActionConfirmation/index.js
@@ -0,0 +1,27 @@
+import { Modal, Button } from '@cogoport/components';
+
+import styles from './styles.module.css';
+
+function ActionConfirmation({
+	id = '', show = false, actionType = '', onSubmit = () => {},
+	setConfirmationConfig = () => {},
+}) {
+	const onClose = () => setConfirmationConfig({ show: false, actionType: '' });
+
+	return (
+		<Modal size="sm" show={show} onClose={onClose} placement="center">
+			<Modal.Body>
+				<div className={styles.modal_body}>
+					{`Are your sure want to ${actionType} Ticket #${id}?`}
+				</div>
+			</Modal.Body>
+
+			<Modal.Footer>
+				<Button className={styles.cancel_button} themeType="secondary" onClick={onClose}>Cancel</Button>
+				<Button onClick={(e) => onSubmit(e)}>Submit</Button>
+			</Modal.Footer>
+		</Modal>
+	);
+}
+
+export default ActionConfirmation;
