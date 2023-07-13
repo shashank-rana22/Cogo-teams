bd1d116e6709f3a53355a3a2c7a9a1a64a80ca75
Nagendra Singh <nagendra.singh@cogoport.com>
2023-07-13 16:25:48 +0530

    feat: error from toast api

--- packages/business-finance/page-components/AccountReceivables/hooks/useLedgerDownload.js
+++ packages/business-finance/page-components/AccountReceivables/hooks/useLedgerDownload.js
@@ -1,6 +1,7 @@
-import { Toast } from '@cogoport/components';
 import { useRequestBf } from '@cogoport/request';
 import { useSelector } from '@cogoport/store';
+
+import toastApiError from '../../commons/toastApiError.ts';
 
 const useLedgerDownload = ({ date, entities, item, setShowLedgerModal }) => {
 	const { profile } = useSelector((state) => state || {});
@@ -38,7 +39,7 @@ const useLedgerDownload = ({ date, entities, item, setShowLedgerModal }) => {
 			window.open(downloadUrl);
 			setShowLedgerModal(false);
 		} catch (err) {
-			Toast.error(err?.response?.data?.message || 'Something went wrong');
+			toastApiError(err);
 		}
 	};
 
