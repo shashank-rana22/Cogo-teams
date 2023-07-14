9f3bbe2e636a03639fc3bcd0bb3479a4be04b1c5
Rajatuba-Cogoport <rajat.uba@cogoport.com>
2023-07-14 21:43:01 +0530

    changes

--- common/navigation-configs/config/shipment-fcl-freight.js
+++ common/navigation-configs/config/shipment-fcl-freight.js
@@ -1,6 +1,7 @@
 const shipmentFclFreight = {
 	'/[partner_id]/booking/fcl/[shipment_id]': {
-		navigation: 'coe-shipments',
+		navigation          : 'coe-shipments',
+		alternateNavigation : ['coe-kam_desk', 'supply_crm'],
 	},
 };
 
