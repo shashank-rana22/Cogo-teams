50a42692246d6de2188c5d4f40f06699412d8d67
Bishal1184 <bishal.saha@cogoport.com>
2023-07-15 10:50:21 +0530

    ui-outgonig-call

--- common/core/components/AdminLayout/VideoCall/index.js
+++ common/core/components/AdminLayout/VideoCall/index.js
@@ -1,4 +1,5 @@
-import { Button } from '@cogoport/components';
+import { Button, Avatar } from '@cogoport/components';
+import { IcMCall, IcMScreenShare } from '@cogoport/icons-react';
 import { isEmpty } from '@cogoport/utils';
 import { getApp, getApps, initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
@@ -9,7 +10,7 @@ import { firebaseConfig } from './configurations/firebase-config';
 import useComingCall from './hooks/useComingCall';
 import useVideoCallFirebase from './hooks/useVideoCallFirebase';
 import styles from './styles.module.css';
-import VideoCallScreen from './VideoCallScreen';
+// import VideoCallScreen from './VideoCallScreen';
 
 function VideoCall() {
 	const app = isEmpty(getApps()) ? initializeApp(firebaseConfig) : getApp();
@@ -23,12 +24,12 @@ function VideoCall() {
 		peer_token : null,
 	});
 	const [callDetails, setCallDetails] = useState({
-		my_details           : null,
-		peer_details         : null,
-		calling_details      : null,
-		calling_room_id      : null,
+		my_details           : null, // agent_details
+		peer_details         : null, // video connected to the person
+		calling_details      : null, // room_details
+		calling_room_id      : null, // room_id
 		webrtc_token_room_id : null,
-		calling_type         : null,
+		calling_type         : null, // incoming or outgoing
 	});
 	const [streams, setStreams] = useState({
 		user_stream   : null,
@@ -41,7 +42,7 @@ function VideoCall() {
 		isScreenShareActive : false,
 		isMaximize          : false,
 	});
-
+	console.log(options, 'options');
 	const streamRef = useRef({
 		user : null,
 		peer : null,
@@ -50,6 +51,8 @@ function VideoCall() {
 		call_comming : null,
 		call_screen  : null,
 	});
+
+	// const { } = callDetails.calling_details || {};
 
 	const { callingTo, callUpdate, callEnd, stopStream, saveWebrtcToken } = useVideoCallFirebase({
 		firestore,
@@ -65,6 +68,9 @@ function VideoCall() {
 		streams,
 		peerRef,
 	});
+
+	console.log(callEnd, stopStream, 'reihgiergnkj');
+
 	const { rejectOfCall, answerOfCall } = useComingCall({
 		firestore,
 		setCallDetails,
@@ -131,30 +137,47 @@ function VideoCall() {
 				</div>
 			) : null}
 			{inACall ? (
-				<div draggable="false">
-					<div
-						className={styles.video_call_screen}
-						role="presentation"
-						draggable="true"
-						onDrag={(ev) => onDragHandler(ev, 'call_screen')}
-						ref={(e) => {
-							componentsRef.call_screen = e;
-						}}
-						onDragOver={(e) => e.preventDefault()}
-					>
-						{streams && (
-							<VideoCallScreen
-								setStreams={setStreams}
-								ref={streamRef}
-								setOptions={setOptions}
-								options={options}
-								callEnd={callEnd}
-								stopStream={stopStream}
-								callUpdate={callUpdate}
-							/>
-						)}
+				<div
+					className={styles.container}
+					draggable="true"
+					onDrag={(ev) => onDragHandler(ev, 'call_screen')}
+					ref={(e) => {
+						componentsRef.call_screen = e;
+					}}
+					onDragOver={(e) => e.preventDefault()}
+				>
+					<div className={styles.header}>Purnendu Shekhar</div>
+					<Avatar personName="Purnendu Shekhar" size="250px" className={styles.styled_avatar} />
+					<div className={styles.footer}>
+						<div className={styles.call_text}>
+							On Call
+							<span>00:44</span>
+						</div>
+						<div className={styles.image_container}>
+							<IcMScreenShare className={styles.share_icon} />
+							<div className={styles.hangup_icon}>
+								<IcMCall className={styles.end_call_icon} />
+							</div>
+						</div>
 					</div>
+					{/*
+
+								{streams && (
+									<VideoCallScreen
+										setStreams={setStreams}
+										ref={streamRef}
+										setOptions={setOptions}
+										options={options}
+										callEnd={callEnd}
+										stopStream={stopStream}
+										callUpdate={callUpdate}
+									/>
+								)}
+							</div>
+						</div>
+						*/}
 				</div>
+
 			) : null}
 		</div>
 	);
