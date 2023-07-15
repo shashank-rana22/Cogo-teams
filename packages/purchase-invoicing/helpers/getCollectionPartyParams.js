1cc0e778752d5a0085e08b5ddca84e5def4ea240
Rajatuba-Cogoport <rajat.uba@cogoport.com>
2023-07-15 17:36:32 +0530

    purchase based on stakeholder

---
+++ packages/purchase-invoicing/helpers/getCollectionPartyParams.js
@@ -0,0 +1,27 @@
+import getGeoConstants from '@cogoport/globalization/constants/geo';
+import getTradeTypeByIncoTerm from '@cogoport/globalization/utils/getTradeTypeByIncoTerm';
+
+const geo = getGeoConstants();
+
+const getCollectionPartyParams = ({ profile_data, servicesData = [] }) => {
+	const primary_service = servicesData?.find((i) => i.main_service_id === null);
+
+	const isFcl = primary_service?.service_type === 'fcl_freight_service';
+	const isImport = getTradeTypeByIncoTerm(primary_service?.inco_term);
+
+	const { partner:{ user_role_ids = [] } = {} } = profile_data || {};
+
+	if (isFcl && isImport) {
+		if (user_role_ids.includes(geo.uuid.document_control_manager)) {
+			return { required_bl_do_quotations: true };
+		}
+
+		if (user_role_ids.includes((i) => [...geo.uuid.service_ops2_role_id, geo.uuid.so_2_manager].includes(i))) {
+			return { required_document_quotations: true };
+		}
+	}
+
+	return {};
+};
+
+export default getCollectionPartyParams;
