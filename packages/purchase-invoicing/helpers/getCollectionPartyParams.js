1091ade4bd729f14fdace364c463742775bec64c
Rajatuba-Cogoport <rajat.uba@cogoport.com>
2023-07-15 18:06:12 +0530

    changes

--- packages/purchase-invoicing/helpers/getCollectionPartyParams.js
+++ packages/purchase-invoicing/helpers/getCollectionPartyParams.js
@@ -7,16 +7,17 @@ const getCollectionPartyParams = ({ profile_data, servicesData = [] }) => {
 	const primary_service = servicesData?.find((i) => i.main_service_id === null);
 
 	const isFcl = primary_service?.service_type === 'fcl_freight_service';
-	const isImport = getTradeTypeByIncoTerm(primary_service?.inco_term);
+	const isImport = getTradeTypeByIncoTerm(primary_service?.inco_term) === 'import';
 
+	console.log({ primary_service, profile_data, isFcl, isImport });
 	const { partner:{ user_role_ids = [] } = {} } = profile_data || {};
-
+	console.log({ user_role_ids });
 	if (isFcl && isImport) {
 		if (user_role_ids.includes(geo.uuid.document_control_manager)) {
 			return { required_bl_do_quotations: true };
 		}
 
-		if (user_role_ids.includes((i) => [...geo.uuid.service_ops2_role_id, geo.uuid.so_2_manager].includes(i))) {
+		if (user_role_ids.some((i) => [...geo.uuid.service_ops2_role_id, geo.uuid.so_2_manager].includes(i))) {
 			return { required_document_quotations: true };
 		}
 	}
