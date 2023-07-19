7e60e06e6ed6b882718490db253845da3d7343f8
danampallyrahul-cogo <danampally.rahul@cogoport.com>
2023-07-19 17:30:36 +0530

    change of key from firebase

--- packages/cogo-one/constants/viewTypeMapping.js
+++ packages/cogo-one/constants/viewTypeMapping.js
@@ -84,7 +84,7 @@ export const VIEW_TYPE_GLOBAL_MAPPING = {
 		teams_chats_base_query    : ({ agentId }) => [where('managers_ids', 'array-contains', agentId)],
 		group_chats_query         : ({ agentId }) => [where('group_members', 'array-contains', agentId)],
 		session_type_query        : getSalesSessionQuery,
-		kam_contacts_base_query   : ({ agentId }) => [where('user_details.sales_agent_ids', 'array-contains', agentId)],
+		kam_contacts_base_query   : ({ agentId }) => [where('user_details.agent_ids', 'array-contains', agentId)],
 		chat_sub_tabs_access      : ['all', 'observer', 'teams', 'kamContacts'],
 		default_side_nav          : 'profile',
 		accesible_filters         : {
@@ -117,7 +117,7 @@ export const VIEW_TYPE_GLOBAL_MAPPING = {
 		group_chats_query       : ({ agentId }) => [where('group_members', 'array-contains', agentId)],
 		teams_chats_base_query  : ({ agentId }) => [where('managers_ids', 'array-contains', agentId)],
 		session_type_query      : getSalesSessionQuery,
-		kam_contacts_base_query : ({ agentId }) => [where('user_details.sales_agent_ids', 'array-contains', agentId)],
+		kam_contacts_base_query : ({ agentId }) => [where('user_details.agent_ids', 'array-contains', agentId)],
 		chat_sub_tabs_access    : ['all', 'teams', 'kamContacts'],
 		default_side_nav        : 'profile',
 		accesible_filters       : {
