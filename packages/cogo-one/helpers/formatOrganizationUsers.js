d1ea3f62321a786a8696da177e51a95f3ec17e36
danampallyrahul-cogo <danampally.rahul@cogoport.com>
2023-07-19 12:03:02 +0530

    fixed payload

--- packages/cogo-one/helpers/formatOrganizationUsers.js
+++ packages/cogo-one/helpers/formatOrganizationUsers.js
@@ -7,18 +7,26 @@ function formatOrganizationUsers({ data }) {
 	const geo = getGeoConstants();
 
 	return list?.map((item) => {
-		const { organization_id, user_id, name, whatsapp_number_eformat, email, whatsapp_country_code } = item || {};
+		const {
+			organization_id,
+			user_id, name,
+			whatsapp_number,
+			mobile_number,
+			email,
+			whatsapp_country_code,
+		} = item || {};
 
 		const countryCode = whatsapp_country_code || geo.country.mobile_country_code;
 
 		return {
 			organization_id,
 			user_id,
-			userName  : name,
-			whatsapp_number_eformat,
+			userName                : name,
+			whatsapp_number_eformat : whatsapp_number || mobile_number,
 			email,
 			countryCode,
-			mobile_no : `${countryCode?.slice(REMOVE_PLUS_SIGN) || ''}${whatsapp_number_eformat}`,
+			mobile_no               : `${countryCode?.slice(REMOVE_PLUS_SIGN) || ''}
+			${whatsapp_number || mobile_number || ''}`,
 		};
 	});
 }
