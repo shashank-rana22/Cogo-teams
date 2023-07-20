b526c4c47786714eb94644b448200af6d4f55f87
Sandeep Nalabolu <sandeep.nalabolu@cogoport.com>
2023-07-20 13:04:32 +0530

    add date picker

--- packages/cogo-one/page-components/CogoOneChannel/HeaderBar/FlashRevertLogs/LogsTable/renderTableHeaders.js
+++ packages/cogo-one/page-components/CogoOneChannel/HeaderBar/FlashRevertLogs/LogsTable/renderTableHeaders.js
@@ -1,14 +1,25 @@
-import { Badge, Popover } from '@cogoport/components';
+import { Badge, Popover, DateRangepicker } from '@cogoport/components';
 import { IcMFilter } from '@cogoport/icons-react';
 import { isEmpty } from '@cogoport/utils';
 
 import styles from './styles.module.css';
 
-export function RenderFlashedAt({ filtersParams = {} }) {
+export function RenderFlashedAt({ setFilterParams = () => {}, filtersParams = {} }) {
 	return (
 		<div className={styles.header_container}>
 			Flashed At
-			<Popover placement="bottom">
+			<Popover
+				placement="bottom"
+				render={(
+					<DateRangepicker
+						name="date"
+						onChange={(val) => setFilterParams((prev) => ({ ...prev, flashed_at: val }))}
+						value={filtersParams?.flashed_at}
+						isPreviousDaysAllowed
+						maxDate={new Date()}
+					/>
+				)}
+			>
 				{isEmpty(filtersParams?.flashed_at) ? (
 					<Badge color="orange">
 						<IcMFilter className={styles.filter_icon} onClick={() => {}} />
