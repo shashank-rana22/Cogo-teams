26cca3b690cc00ab40ff7228ef57577659adedd8
likhilesh-cogoport <likhilesh.balpande@cogoport.com>
2023-07-21 17:30:16 +0530

    pie chart code optimization

--- packages/chakravyuh/constants/pie_chart_config.js
+++ packages/chakravyuh/constants/pie_chart_config.js
@@ -15,27 +15,35 @@ export const CUSTOM_THEME = {
 
 const CUMSTOM_DATA = [
 	{
-		key          : 'supply_rates',
-		id           : 'Supply Rates',
-		label        : 'Supply Rates',
+		key          : 'pie_chart_key_1',
+		id           : 'Backend Data 1',
+		label        : 'Backend Data 1',
 		value        : 1211,
 		color        : '#63BEC8',
 		cancellation : 90,
 	},
 	{
-		key          : 'predicted_rates',
-		id           : 'Predicted Rates',
-		label        : 'Predicted Rates',
+		key          : 'pie_chart_key_2',
+		id           : 'Backend Data 2',
+		label        : 'Backend Data 2',
 		value        : 1500,
 		color        : '#F9AE64',
 		cancellation : 70,
 	},
 	{
-		key          : 'extended_rates',
-		id           : 'Extended Rates',
-		label        : 'Extended Rates',
+		key          : 'pie_chart_key_3',
+		id           : 'Backend Data 3',
+		label        : 'Backend Data 3',
 		value        : 1312,
 		color        : '#9BA0CB',
+		cancellation : 65,
+	},
+	{
+		key          : 'pie_chart_key_4',
+		id           : 'Backend Data 4',
+		label        : 'Backend Data 4',
+		value        : 1312,
+		color        : '#FFD700',
 		cancellation : 65,
 	},
 ];
@@ -72,9 +80,10 @@ const EXPLORED_VIEW_DATA = [
 ];
 
 const ColorMappings = {
-	supply_rates    : ['#63BEC8', '#BCCFD2', '#3D747A', '#7EB2B8'],
-	predicted_rates : ['#F9AE64', '#FDD3AD', '#F9AE64', '#F58B33'],
-	extended_rates  : ['#9BA0CB', '#C2C6E3', '#686E9F', '#A59CBE'],
+	pie_chart_key_1 : ['#63BEC8', '#BCCFD2', '#3D747A', '#7EB2B8'],
+	pie_chart_key_2 : ['#F9AE64', '#FDD3AD', '#F9AE64', '#F58B33'],
+	pie_chart_key_3 : ['#9BA0CB', '#C2C6E3', '#686E9F', '#A59CBE'],
+	pie_chart_key_4 : ['#FFD700', '#FFFF99', '#FFF44F', '#FEBE10'],
 };
 
 export const usePieChartConfigs = (type) => {
