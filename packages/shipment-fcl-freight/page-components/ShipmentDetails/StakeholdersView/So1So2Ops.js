c0a910848bc26fca18e213cbf9bbc110dca46d2f
Sandeep Nalabolu <109941135+sandeepn1709@users.noreply.github.com>
2023-07-24 17:04:46 +0530

    revert these

--- packages/shipment-fcl-freight/page-components/ShipmentDetails/StakeholdersView/So1So2Ops.js
+++ packages/shipment-fcl-freight/page-components/ShipmentDetails/StakeholdersView/So1So2Ops.js
@@ -167,7 +167,7 @@ function So1So2Ops({ get = {}, activeStakeholder = '' }) {
 							<ShipmentMails
 								source="cogo_rpa"
 								filters={{ q: shipment_data?.serial_id }}
-								pre_subject_text={shipment_data?.serial_id}
+								pre_subject_text={`${shipment_data?.serial_id}`}
 							/>
 						</TabPanel>
 
