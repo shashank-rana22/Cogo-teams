88fcdbd20f38c6953ad1cd3c10620a03e6d5d54d
samir-cogoport <samir.dupare@cogoport.com>
2023-07-24 16:08:01 +0530

    side panel

--- packages/chakravyuh/page-components/AccuracyDashboard/MapView/SidePanel/GeoCoder/index.js
+++ packages/chakravyuh/page-components/AccuracyDashboard/MapView/SidePanel/GeoCoder/index.js
@@ -1,11 +1,29 @@
 import { cl } from '@cogoport/components';
 import { AsyncSelect } from '@cogoport/forms';
 import { IcALocationwhite, IcCSwitch, IcMOrigin } from '@cogoport/icons-react';
+import { isEmpty } from '@cogoport/utils';
 import React from 'react';
+
+import { getHigherHierarchy } from '../../../../../utils/hierarchy-utils';
 
 import styles from './styles.module.css';
 
-function GeoCoder({ locationFilters = {}, setLocationFilters = () => {} }) {
+function GeoCoder({ locationFilters = {}, setLocationFilters = () => {}, setHierarchy = () => {} }) {
+	const handleChange = (key, val, obj) => {
+		setLocationFilters((prev) => ({ ...prev, [key]: { id: val, ...obj } }));
+		if (key === 'destination' && !isEmpty(obj)) {
+			const currentKey = `${(obj?.type || '').includes('port') ? 'port' : obj?.type}_id`;
+			const parentKeys = getHigherHierarchy(currentKey);
+			const newHierarchy = parentKeys.reduce((acc, k) => {
+				if (obj[k]) {
+					acc[k] = obj[k];
+				}
+				return acc;
+			}, {});
+			setHierarchy({ ...newHierarchy, [currentKey]: val });
+		}
+	};
+
 	return (
 		<div className={styles.container}>
 			<div className={cl`${styles.col} ${styles.col_one}`}>
@@ -14,12 +32,14 @@ function GeoCoder({ locationFilters = {}, setLocationFilters = () => {} }) {
 				<IcALocationwhite />
 			</div>
 			<div className={cl`${styles.col} ${styles.main_col}`}>
-				{Object.keys(locationFilters).map((key) => (
+				{Object.keys(locationFilters).map((key, idx) => (
 					<AsyncSelect
 						key={key}
 						asyncKey="list_locations"
-						value={locationFilters?.[key]}
-						onChange={(val) => setLocationFilters((prev) => ({ ...prev, [key]: val }))}
+						value={locationFilters?.[key]?.id}
+						onChange={(val, obj) => handleChange(key, val, obj)}
+						isClearable={!!idx}
+						params={{ filters: { type: ['seaport', 'country', 'region'] } }}
 					/>
 				))}
 			</div>
--- packages/chakravyuh/page-components/AccuracyDashboard/MapView/SidePanel/index.js
+++ packages/chakravyuh/page-components/AccuracyDashboard/MapView/SidePanel/index.js
@@ -1,5 +1,7 @@
 import { Select, cl } from '@cogoport/components';
-import { IcMArrowLeft, IcMDescendingSort } from '@cogoport/icons-react';
+import { countriesHash } from '@cogoport/globalization/utils/getCountriesHash';
+import { IcMArrowLeft, IcMDescendingSort, IcMPortArrow } from '@cogoport/icons-react';
+import { isEmpty, startCase } from '@cogoport/utils';
 import React from 'react';
 
 import Heading from '../../../../common/Heading';
@@ -8,21 +10,44 @@ import { SORT_OPTIONS } from '../../../../constants/map_constants';
 import GeoCoder from './GeoCoder';
 import styles from './styles.module.css';
 
+const SCOPE_MAPPING = {
+	continents : 'country',
+	country    : 'region',
+	region     : 'ports',
+};
+
 function SidePanel({
-	setView = () => {}, backView = () => {},
-	isFull = false, setIsFull = () => {}, locationFilters = {}, setLocationFilters = () => {},
+	setView = () => {}, backView = () => {}, setHierarchy = () => {},
+	isFull = false, setIsFull = () => {}, locationFilters = {}, setLocationFilters = () => {}, activeList = [],
+	data = [],
 }) {
+	const originName = locationFilters.origin?.name || countriesHash?.[locationFilters?.origin?.id]?.name;
+	const destinationType = locationFilters?.destination?.type || '';
+	const destination = destinationType.includes('port')
+		? locationFilters?.destination?.name : SCOPE_MAPPING[destinationType];
+
+	const currentList = isEmpty(activeList) ? data : activeList;
+
 	return (
 		<>
 			<div className={cl`${styles.side_container} ${isFull && styles.hide}`}>
 				<div className={styles.heading}>
-					<Heading setView={setView} backView={backView} heading="Map View" />
+					<Heading setView={setView} backView={backView} heading="Map View" showFilterText={false} />
 				</div>
-				<GeoCoder locationFilters={locationFilters} setLocationFilters={setLocationFilters} />
-				<div className={styles.horizontal_line} />
+				<div className={styles.sticky_container}>
+					<GeoCoder
+						locationFilters={locationFilters}
+						setLocationFilters={setLocationFilters}
+						setHierarchy={setHierarchy}
+					/>
+					<div className={styles.horizontal_line} />
+				</div>
 				<div className={styles.list_container}>
 					<div className={styles.list_header}>
-						<h4>Nhava sheva ports</h4>
+						<h4>
+							{`${startCase(originName)} to 
+							${startCase(destination || 'Countries')}`}
+						</h4>
 						<Select
 							size="sm"
 							placeholder="sort by"
@@ -31,6 +56,21 @@ function SidePanel({
 							style={{ width: '160px' }}
 						/>
 					</div>
+					{currentList.map((item) => (
+						<div key={item?.id} className={styles.card}>
+							<div className={styles.left}>
+								<div>
+									<h4>{originName}</h4>
+									<IcMPortArrow />
+									<p>{item?.name}</p>
+
+								</div>
+							</div>
+							<div className={styles.right}>
+								<p>Deviation</p>
+							</div>
+						</div>
+					))}
 				</div>
 			</div>
 			<button
