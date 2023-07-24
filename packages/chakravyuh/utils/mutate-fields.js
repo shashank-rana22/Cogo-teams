cd7029e94cb87993d25964189862a736acf2c7d3
likhilesh-cogoport <likhilesh.balpande@cogoport.com>
2023-07-24 16:01:21 +0530

    bug fix: filters

--- packages/chakravyuh/utils/mutate-fields.js
+++ packages/chakravyuh/utils/mutate-fields.js
@@ -1,13 +1,16 @@
 import { FREIGHT_CONTAINER_COMMODITY_MAPPINGS } from '@cogoport/globalization/constants/commodities';
 import { startCase } from '@cogoport/utils';
 
-const mutateFields = ({ controls, containerType }) => {
+const mutateFields = ({ controls, containerType, setGlobalFilters }) => {
 	const newFields = controls.map((ctr) => {
 		const newCtr = { ...ctr };
 		if (ctr.name === 'commodity') {
 			newCtr.options = FREIGHT_CONTAINER_COMMODITY_MAPPINGS[containerType || 'standard']
 				.map((value) => ({ label: startCase(value), value }));
 			newCtr.disabled = !containerType;
+		}
+		if (ctr.name === 'mode') {
+			newCtr.onChange = (val) => setGlobalFilters((prev) => ({ ...prev, rate_type: val }));
 		}
 
 		return newCtr;
