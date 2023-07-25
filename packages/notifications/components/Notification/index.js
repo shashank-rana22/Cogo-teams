11050974010af11d02569d58298e536635310525
Rohit Patil <rohit.patil@cogoport.com>
2023-07-25 21:22:49 +0530

    adding Notif component

---
+++ packages/notifications/components/Notification/index.js
@@ -0,0 +1,54 @@
+import { IcMArrowRight } from '@cogoport/icons-react';
+import React from 'react';
+
+import formatDistanceToNow from '../../utils/formatDistanceToNow';
+
+import styles from './styles.module.css';
+
+function Notification({
+	item = {},
+	handleNotificationClick = () => {},
+	setShow = () => {},
+}) {
+	const getStatus = () => {
+		if (item?.is_clicked) {
+			return 'Read';
+		}
+		if (item?.is_seen) {
+			return 'Seen';
+		}
+		return 'Unread';
+	};
+	return (
+		<div
+			role="presentation"
+			className={styles.container}
+			onClick={() => {
+				handleNotificationClick(item);
+				setShow(false);
+			}}
+			onKeyDown={(e) => {
+				if (e.key === 'Enter') {
+					handleNotificationClick(item);
+				}
+			}}
+		>
+			<div className={styles.space_between}>
+				<div style={{ width: '95%' }}>
+					<div className={styles.row}>
+						{!item?.is_seen ? <div className={styles.new_notification} /> : null}
+						<div dangerouslySetInnerHTML={{ __html: item?.content?.body }} />
+					</div>
+					<div className={styles.row} style={{ marginTop: 8 }}>
+						<p className={styles.time_status} style={{ marginRight: 12 }}>
+							{formatDistanceToNow(item?.created_at, { addSuffix: true })}
+						</p>
+						{!item.is_rpa ? <p className={styles.time_status}>{getStatus()}</p> : null}
+					</div>
+				</div>
+				<IcMArrowRight size={1.5} />
+			</div>
+		</div>
+	);
+}
+export default Notification;
