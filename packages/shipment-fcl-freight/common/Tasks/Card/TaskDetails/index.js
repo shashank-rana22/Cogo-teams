f6c96cad881f4ed3647ed6aab7541a49f2ec043d
Kanchan Bhandari <kanchan.bhandari@cogoport.com>
2023-07-25 10:40:23 +0530

    trailer service display name fix

--- packages/shipment-fcl-freight/common/Tasks/Card/TaskDetails/index.js
+++ packages/shipment-fcl-freight/common/Tasks/Card/TaskDetails/index.js
@@ -27,7 +27,7 @@ function TaskDetails({
 		});
 	});
 
-	const taskName = getTaskDisplayName({ shipment_data, task, REQUIRED_SERVICE_ARR });
+	const taskName = getTaskDisplayName({ shipment_data, task, REQUIRED_SERVICE_ARR, servicesList });
 
 	return (
 		<div className={styles.container}>
