c317b1c34739b75e39b5ba78b86fca91db325c7d
AmaanAhmadCogo <amaan.ahmad@cogoport.com>
2023-07-25 11:53:36 +0530

    sync

--- packages/shipment-fcl-freight/page-components/ShipmentDetails/StakeholdersView/BookingDeskManager.js
+++ packages/shipment-fcl-freight/page-components/ShipmentDetails/StakeholdersView/BookingDeskManager.js
@@ -54,7 +54,6 @@ function BookingDeskManager({ get = {}, activeStakeholder = '' }) {
 		...servicesGet,
 		...getTimeline,
 		activeStakeholder,
-		stakeholderConfig,
 	}), [get, servicesGet, getTimeline, activeStakeholder]);
 
 	useEffect(() => {
@@ -155,7 +154,7 @@ function BookingDeskManager({ get = {}, activeStakeholder = '' }) {
 							<ShipmentMails
 								source="cogo_rpa"
 								filters={{ q: shipment_data?.serial_id }}
-								pre_subject_text={shipment_data?.serial_id}
+								pre_subject_text={`${shipment_data?.serial_id}`}
 							/>
 						</TabPanel>
 					</Tabs>
