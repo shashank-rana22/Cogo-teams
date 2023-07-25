7f4300640bcb84e554700be63066fb45a847f54e
Rohit Patil <rohit.patil@cogoport.com>
2023-07-25 21:48:53 +0530

    adding files

---
+++ packages/notifications/components/NotificationPopover/Header/index.js
@@ -0,0 +1,47 @@
+import { Button, Chips } from '@cogoport/components';
+import React from 'react';
+
+import styles from './styles.module.css';
+
+const ZERO_VALUE = 0;
+
+function Header({
+	onMarkAllAsRead,
+	onSeeAll,
+	setShow = () => {},
+	formattedData,
+	notificationType,
+	setNotificationType,
+}) {
+	return (
+		<div className={styles.container}>
+			<Chips
+				size="md"
+				items={[
+					{ label: 'Notifications', value: 'general' },
+					{ label: 'Mails', value: 'mails' },
+				]}
+				selectedItems={notificationType}
+				onItemChange={setNotificationType}
+			/>
+			{formattedData >= ZERO_VALUE && (
+				<div className={styles.row}>
+					<Button
+						className={styles.mark_read}
+						onClick={() => {
+							// eslint-disable-next-line no-unused-expressions
+							onMarkAllAsRead;
+							setShow(false);
+						}}
+					>
+						Mark all as read
+					</Button>
+					<Button className="primary sm uppercase" onClick={onSeeAll}>
+						SEE All
+					</Button>
+				</div>
+			)}
+		</div>
+	);
+}
+export default Header;
---
+++ packages/notifications/components/NotificationPopover/Loader/index.js
@@ -0,0 +1,16 @@
+import { IcMRadioLoader } from '@cogoport/icons-react';
+import React from 'react';
+
+import styles from './styles.module.css';
+
+function LoaderComp() {
+	return (
+		<div className={styles.container}>
+			<div className={styles.loader}>
+				<IcMRadioLoader size={1} />
+			</div>
+			<div className={styles.load_heading}>Loading...</div>
+		</div>
+	);
+}
+export default LoaderComp;
