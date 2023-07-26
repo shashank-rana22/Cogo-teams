e82c0d5602ebcd54706442f91660115ff0058d7c
samir-cogoport <samir.dupare@cogoport.com>
2023-07-26 11:36:00 +0530

    add point

---
+++ packages/chakravyuh/page-components/AccuracyDashboard/MapView/Map/AnimatedPoint/index.js
@@ -0,0 +1,33 @@
+import GLOBAL_CONSTANTS from '@cogoport/globalization/constants/globals';
+import { Marker, L } from '@cogoport/maps';
+import React from 'react';
+
+import styles from './styles.module.css';
+
+const ANCHOR = -15;
+const DEFAULT_SIZE = 12;
+const getIcon = ({ className, size }) => L.divIcon({
+	className,
+	iconSize    : L.point(...size),
+	popupAnchor : [GLOBAL_CONSTANTS.zeroth_index, ANCHOR],
+});
+
+const Point = React.forwardRef(({
+	position, showAnimation = false,
+	size = [DEFAULT_SIZE, DEFAULT_SIZE], children, ...rest
+}, ref) => (
+	<Marker
+		position={position}
+		icon={getIcon({
+			className: `${styles.point_animation}
+			${!showAnimation ? styles.hide_animation : ''}`,
+			size,
+		})}
+		{...rest}
+		ref={ref}
+	>
+		{children}
+	</Marker>
+));
+
+export default Point;
