c683a5586d2aa25eab6157eb2887942e0527f6ed
AbhiBarbiyal <abhi.barbiyal@cogoport.com>
2023-07-26 16:34:57 +0530

    eslint

--- common/shipment-chat/hooks/useFireBase.js
+++ common/shipment-chat/hooks/useFireBase.js
@@ -1,29 +1,29 @@
-import { initializeApp, getApps, getApp } from "firebase/app";
-import { getDatabase, ref, onValue } from "firebase/database";
-import { useState, useEffect } from "react";
+import GLOBAL_CONSTANTS from '@cogoport/globalization/constants/globals';
+import { initializeApp, getApps, getApp } from 'firebase/app';
+import { getDatabase, ref, onValue } from 'firebase/database';
+import { useState, useEffect } from 'react';
 
-import { FirebaseConfig } from "../FirebaseConfig";
+import { FirebaseConfig } from '../FirebaseConfig';
 
 const useFireBase = ({ id }) => {
-    const [msgContent, setMsgContent] = useState([]);
+	const [msgContent, setMsgContent] = useState([]);
 
-    const app =
-        getApps().length === 0 ? initializeApp(FirebaseConfig) : getApp();
-    const db = getDatabase(app);
+	const app = getApps().length === GLOBAL_CONSTANTS.zeroth_index ? initializeApp(FirebaseConfig) : getApp();
+	const db = getDatabase(app);
 
-    useEffect(() => {
-        const hit = ref(db, `Channels/${id}`);
+	useEffect(() => {
+		const hit = ref(db, `Channels/${id}`);
 
-        const unSubscribe = onValue(hit, (item) => {
-            setMsgContent(item.val());
-        });
+		const unSubscribe = onValue(hit, (item) => {
+			setMsgContent(item.val());
+		});
 
-        return () => unSubscribe();
-    }, [id, db]);
+		return () => unSubscribe();
+	}, [id, db]);
 
-    return {
-        msgContent,
-    };
+	return {
+		msgContent,
+	};
 };
 
 export default useFireBase;
