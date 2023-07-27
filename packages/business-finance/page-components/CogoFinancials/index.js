2fc05881da4340f6a2c7d8b4d266766d71559016
Nagendra Singh <nagendra.singh@cogoport.com>
2023-07-27 19:29:00 +0530

    closed shipment card

--- packages/business-finance/page-components/CogoFinancials/index.js
+++ packages/business-finance/page-components/CogoFinancials/index.js
@@ -1,3 +1,4 @@
+import { ResponsiveRadialBar } from '@cogoport/charts/radial-bar';
 import { Select, Toggle } from '@cogoport/components';
 import getGeoConstants from '@cogoport/globalization/constants/geo/index';
 import GLOBAL_CONSTANTS from '@cogoport/globalization/constants/globals';
@@ -29,10 +30,64 @@ const ENTITY_OPTIONS = Object.keys(GLOBAL_CONSTANTS.cogoport_entities)?.map((ite
 	label : `${item} - ${GLOBAL_CONSTANTS.cogoport_entities[item].name}`,
 }));
 
+const data = [
+	{
+		id   : 'Cost',
+		data : [
+			{
+				x : 'Estimated Cost',
+				y : 80,
+			},
+			{
+				x : 'Actual Cost',
+				y : 20,
+			},
+		],
+	},
+	{
+		id   : 'Revenue',
+		data : [
+			{
+				x : 'Estimated Revenue',
+				y : 60,
+			},
+			{
+				x : 'Actual Revenue',
+				y : 40,
+			},
+		],
+	},
+
+];
+
 function CogoFinancials() {
 	const [isPreTax, setIsPreTax] = useState(true);
 	const [timeRange, setTimeRange] = useState('1D');
 	const [entity, setEntity] = useState(DEFAULT_ENTITY);
+
+	const graphData = [
+		{
+			rowId    : 'first_row',
+			children : [
+				{ label: 'Estimated Revenue', value: 'Curr XXXXX', color: 'red' },
+				{ label: 'Estimated Cost', value: 'Curr XXXXX', color: 'red' },
+			],
+		},
+		{
+			rowId    : 'second_row',
+			children : [
+				{ label: 'Actual Revenue', value: 'Curr XXXXX', color: 'red' },
+				{ label: 'Actual Cost', value: 'Curr XXXXX', color: 'red' },
+			],
+		},
+		{
+			rowId    : 'third_row',
+			children : [
+				{ label: 'Deviation', value: 'Curr XXXXX', color: 'red' },
+				{ label: 'Deviation', value: 'Curr XXXXX', color: 'red' },
+			],
+		},
+	];
 
 	return (
 		<div>
@@ -71,6 +126,57 @@ function CogoFinancials() {
 					<div className={styles.info}><IcMInfo /></div>
 				</div>
 				<hr className={styles.bottom_line} />
+
+				<div style={{ margin: '20px', display: 'flex', alignItems: 'center' }}>
+					<div
+						className={styles.responsive_graph_circular}
+					>
+						<ResponsiveRadialBar
+							data={data}
+							valueFormat=">-.2f"
+							padding={0}
+							cornerRadius={2}
+							radialAxisStart={{ tickSize: 5, tickPadding: 5, tickRotation: 0 }}
+							circularAxisOuter={{ tickSize: 5, tickPadding: 12, tickRotation: 0 }}
+							endAngle="360"
+							innerRadius={0.6}
+							enableRadialGrid={false}
+							enableCircularGrid={false}
+							layers={['tracks', 'bars']}
+							colors={['#ee3425', '#f8aea8', '#6fa5ab', '#cfeaed']}
+						/>
+
+					</div>
+					<div className={styles.show_graph_data}>
+						{graphData.map((item) => (
+
+							<div
+								key={item?.id}
+								style={{
+									display        : 'flex',
+									justifyContent : 'space-between',
+									margin         : '20px 0px',
+									width          : '100%',
+								}}
+							>
+								{(item.children || []).map((child) => (
+									<div key={child.label}>
+										<div className={styles.graph_label}>
+											<span className={styles.label_circle} />
+											{child.label}
+										</div>
+										<div className={styles.graph_value}>
+											{child.value}
+										</div>
+									</div>
+								))}
+
+							</div>
+						))}
+
+					</div>
+				</div>
+
 			</div>
 		</div>
 	);
