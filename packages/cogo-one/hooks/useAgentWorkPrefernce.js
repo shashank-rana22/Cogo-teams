a61345ac5b6f81f84bfd4bf666891a12728a49ee
danampallyrahul-cogo <danampally.rahul@cogoport.com>
2023-07-27 17:13:30 +0530

    business_consultant

--- packages/cogo-one/hooks/useAgentWorkPrefernce.js
+++ packages/cogo-one/hooks/useAgentWorkPrefernce.js
@@ -8,6 +8,8 @@ const PERSONA_KEYS_MAPPING = ['sales', 'supply', 'support', 'shipment_specialist
 
 const DEFAULT_VIEW_TYPE = 'support';
 
+const SALES_VIEW_AGENT_TYPES = ['sales', 'business_consultant'];
+
 const getViewTypeFromWorkPreferences = ({ viewTypeFromRoleIds, agentType }) => {
 	if (viewTypeFromRoleIds === 'cogoone_admin') {
 		return viewTypeFromRoleIds;
@@ -19,6 +21,10 @@ const getViewTypeFromWorkPreferences = ({ viewTypeFromRoleIds, agentType }) => {
 
 	if (agentType.includes('admin')) {
 		return agentType;
+	}
+
+	if (SALES_VIEW_AGENT_TYPES.includes(agentType)) {
+		return 'sales';
 	}
 
 	return PERSONA_KEYS_MAPPING.find((eachPersona) => agentType.includes(eachPersona)) || '';
