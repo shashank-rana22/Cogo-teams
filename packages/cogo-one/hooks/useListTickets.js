e2e01445a452fbbbe54dce731b24c85781932aa7
Chandrakant26 <chandrakant.vangule@cogoport.com>
2023-07-29 15:57:10 +0530

    modication in dependency array

--- packages/cogo-one/hooks/useListTickets.js
+++ packages/cogo-one/hooks/useListTickets.js
@@ -18,35 +18,37 @@ const getParams = ({ filter, userId, pagination, performedById }) => ({
 const useListTickets = ({ userId = '', activeTab = '', fetchTicketsStats = () => {} }) => {
 	const { performedById } = useSelector(({ profile }) => ({ performedById: profile.user.id }));
 
+	const [pagination, setPagination] = useState(DEFAULT_PAGE_COUNT);
 	const [filter, setFilter] = useState('requested');
+
 	const [{ loading, data }, trigger] = useTicketsRequest({
 		url     : '/list',
 		method  : 'get',
 		authkey : 'get_tickets_list',
 	}, { manual: true });
 
-	const [pagination, setPagination] = useState(DEFAULT_PAGE_COUNT);
+	const fetchTickets = useCallback(() => {
+		if (activeTab === 'email' || !userId) {
+			return null;
+		}
+		try {
+			trigger({
+				params: getParams({ filter, userId, pagination, performedById }),
+			});
+		} catch (error) {
+			console.log('error:', error);
+		}
+		return null;
+	}, [activeTab, userId, trigger, filter, pagination, performedById]);
 
 	useEffect(() => {
 		setPagination(DEFAULT_PAGE_COUNT);
 		fetchTicketsStats();
 	}, [filter, fetchTicketsStats]);
 
-	const fetchTickets = useCallback(async () => {
-		try {
-			await trigger({
-				params: getParams({ filter, userId, pagination, performedById }),
-			});
-		} catch (error) {
-			console.log('error:', error);
-		}
-	}, [trigger, userId, filter, pagination, performedById]);
-
 	useEffect(() => {
-		if (activeTab !== 'email' && userId) {
-			fetchTickets();
-		}
-	}, [fetchTickets, userId, activeTab]);
+		fetchTickets();
+	}, [fetchTickets]);
 
 	return {
 		ticketData  : data,
