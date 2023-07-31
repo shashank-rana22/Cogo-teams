2dc8eebbb0a284c6ae91cf31a53d85f0425bad92
Rella Panduvardhan Reddy <rella.reddy@cogoport.com>
2023-07-31 20:16:06 +0530

    payload fixes

--- packages/service-discovery/common/OtherServices/AdditionalServices/configs/fcl/getSearchResultsPayload.js
+++ packages/service-discovery/common/OtherServices/AdditionalServices/configs/fcl/getSearchResultsPayload.js
@@ -25,7 +25,7 @@ const getSearchResultsPayload = ({
 					: 'ftl_freight',
 			...(origin_cargo_handling_type === 'stuffing_at_factory'
 				? {
-					trailer_freight_services: getServiceWisePayload({
+					trailer_freight_services_attributes: getServiceWisePayload({
 						additionalFormInfo,
 						detail,
 						service_name: 'trailer_freight',
@@ -33,7 +33,7 @@ const getSearchResultsPayload = ({
 					}),
 				}
 				: {
-					ftl_freight_services: getServiceWisePayload({
+					ftl_freight_services_attributes: getServiceWisePayload({
 						additionalFormInfo,
 						detail,
 						service_name: 'ftl_freight',
@@ -52,7 +52,7 @@ const getSearchResultsPayload = ({
 					: 'ftl_freight',
 			...(destination_cargo_handling_type !== 'destuffing_at_dock'
 				? {
-					trailer_freight_services: getServiceWisePayload({
+					trailer_freight_services_attributes: getServiceWisePayload({
 						additionalFormInfo,
 						detail,
 						service_name: 'trailer_freight',
@@ -60,7 +60,7 @@ const getSearchResultsPayload = ({
 					}),
 				}
 				: {
-					ftl_freight_services: getServiceWisePayload({
+					ftl_freight_services_attributes: getServiceWisePayload({
 						additionalFormInfo,
 						detail,
 						service_name: 'ftl_freight',
