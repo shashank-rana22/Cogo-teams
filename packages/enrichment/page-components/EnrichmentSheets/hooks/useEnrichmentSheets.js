e28d4b654a306f20c84ef3a8e4cfe17f7e3dc3a6
sushilr29 <sushil.rindhe@cogoport.com>
2023-08-02 12:47:55 +0700

    pr

--- packages/enrichment/page-components/EnrichmentSheets/hooks/useEnrichmentSheets.js
+++ packages/enrichment/page-components/EnrichmentSheets/hooks/useEnrichmentSheets.js
@@ -1,19 +1,9 @@
-import { Button, Pill } from '@cogoport/components';
 import { useDebounceQuery } from '@cogoport/forms';
-import GLOBAL_CONSTANTS from '@cogoport/globalization/constants/globals';
-import formatDate from '@cogoport/globalization/utils/formatDate';
 import { useAllocationRequest } from '@cogoport/request';
 import { useSelector } from '@cogoport/store';
-import { startCase } from '@cogoport/utils';
 import { useState, useEffect } from 'react';
 
-const UPLOAD_DOCUMENT_STATUS_MAPPING = {
-	success    : 'green',
-	inactive   : 'orange',
-	processing : 'yellow',
-	processed  : 'blue',
-	failed     : 'red',
-};
+import getEnrichmentSheetsColumns from '../utils/get-enrichment-sheets-columns';
 
 const useEnrichmentSheets = () => {
 	const { profile } = useSelector((state) => state || {});
@@ -72,86 +62,7 @@ const useEnrichmentSheets = () => {
 		}));
 	}, [partner_id, searchQuery, user_id]);
 
-	const columns = [
-		{
-			id       : 'file_id',
-			Header   : 'SERIAL ID',
-			accessor : ({ serial_id }) => (
-				<section>
-					<Pill>
-						#
-						{serial_id || '-'}
-					</Pill>
-				</section>
-			),
-		},
-		{
-			id       : 'file_name',
-			Header   : 'FILE NAME',
-			accessor : ({ file_name }) => (
-				<section>{startCase(file_name) || '-'}</section>
-			),
-		},
-		{
-			id       : 'upload_date',
-			Header   : 'UPLOAD DATE',
-			accessor : ({ created_at }) => (
-				<section>
-					{created_at ? formatDate({
-						date       : created_at,
-						dateFormat : GLOBAL_CONSTANTS.formats.date['dd MMM yyyy'],
-						formatType : 'date',
-					}) : '-'}
-				</section>
-			),
-		},
-		{
-			id       : 'sheet_url',
-			Header   : 'SHEET URL',
-			accessor : ({ sheet_url }) => (
-				<section>
-					<Button
-						themeType="secondary"
-						size="md"
-						type="button"
-						disabled={sheet_url === null}
-						onClick={() => window.open(sheet_url, '_blank')}
-					>
-						Download
-					</Button>
-				</section>
-			),
-		},
-		{
-			id       : 'error_sheet_url',
-			Header   : 'ERROR SHEET URL',
-			accessor : ({ error_sheet_url }) => (
-				<section>
-					<Button
-						themeType="secondary"
-						size="md"
-						type="button"
-						disabled={!error_sheet_url}
-						onClick={() => window.open(error_sheet_url, '_blank')}
-					>
-						Download
-					</Button>
-				</section>
-			),
-		},
-
-		{
-			id       : 'status',
-			Header   : 'STATUS',
-			accessor : ({ status }) => (
-				<seaction>
-					<Pill size="md" color={UPLOAD_DOCUMENT_STATUS_MAPPING[status]}>
-						{startCase(status) || '-'}
-					</Pill>
-				</seaction>
-			),
-		},
-	];
+	const columns = getEnrichmentSheetsColumns();
 
 	return {
 		refetch,
