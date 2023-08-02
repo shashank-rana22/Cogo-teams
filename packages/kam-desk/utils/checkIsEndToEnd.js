4c422bb3ac247143f4f86106e8dd63a87a911885
Bumblebee <rahulk@cogoport.com>
2023-08-02 19:15:11 +0530

    nominated tag for superadmin

--- packages/kam-desk/utils/checkIsEndToEnd.js
+++ packages/kam-desk/utils/checkIsEndToEnd.js
@@ -1,13 +1,19 @@
-export default function checkIsEndToEnd({ booking_agents = [], trade_type = '', userId = '' }) {
+import GLOBAL_CONSTANTS from '@cogoport/globalization/constants/globals';
+
+export default function checkIsEndToEnd({ booking_agents = [], trade_type = '', userData = {} }) {
 	const isExport = trade_type === 'export';
 	const isImport = trade_type === 'import';
+
+	const roleId = userData?.auth_role_data?.id;
+	const isSuperadmin = roleId === GLOBAL_CONSTANTS.uuid.superadmin_id;
+	const userId = userData?.user?.id;
 
 	return booking_agents?.some((item) => {
 		const isDestinationAgent = item?.stakeholder_type === 'destination_booking_agent';
 		const isOriginAgent = item?.stakeholder_type === 'origin_booking_agent';
 
-		if ((isDestinationAgent && isExport && userId === item?.id)
-        || (isOriginAgent && isImport && userId === item?.id)) {
+		if (((isDestinationAgent && isExport && userId === item?.id) || isSuperadmin)
+		|| 	(isOriginAgent && isImport && userId === item?.id) || isSuperadmin) {
 			return true;
 		}
 
