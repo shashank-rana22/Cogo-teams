b49ab3e57824bd6765b29594da889006a5c20173
Lachiramnaik1330 <malothu.lachiramnaik@cogoport.com>
2023-08-03 14:41:46 +0530

    added event info mapping

--- packages/cogo-one/common/MessageBody/UserActivityMessages/EmailClicked/index.js
+++ packages/cogo-one/common/MessageBody/UserActivityMessages/EmailClicked/index.js
@@ -7,28 +7,60 @@ import { getEventTitle } from '../../../../utils/getEventTitle';
 import PreviewModal from './PreviewModal';
 import styles from './styles.module.css';
 
-function EmailClicked({ serviceData = {}, name = '', formattedData = {} }) {
+const EVENT_INFORMATION_MAPPING = {
+	'System: Communication: Email Sent': {
+		title       : 'Customer Has Been Sent An Email',
+		information : `Please keep an eye out and see if the mail 
+		is opened or clicked in the near future. You may have to follow up 
+		with them then. The following is a preview of the mail - `,
+	},
+	'System: Communication: Email Opened': {
+		title       : 'Customer Has Opened A Cogoport Email',
+		information : `Please reach out to the customer if this is not their first time 
+		reading that mail asking if they have any doubts about 
+		Cogoport or shipments. The following is a preview of the mail - `,
+	},
+	'System: Communication: Email Clicked': {
+		title       : 'Customer Has Clicked A Cogoport Email',
+		information : `Please reach out to the customer letting them know that 
+		you will assist them with any doubts that they might have. 
+		The following is a preview of the mail - `,
+	},
+};
+
+function EmailClicked({ serviceData = {}, name = '', formattedData = {}, eventName = '' }) {
 	const [previewModal, setPreviewModal] = useState(false);
 
-	const { email = '', user_name = '' } = formattedData;
+	const { email = '', user_name = '', lead_user_details = {} } = formattedData;
 	const { content = {} } = serviceData || {};
 	const { subject = '', body } = content;
 	const eventTitle = getEventTitle({ name });
 
 	return (
 		<>
-			<div className={styles.title}>{eventTitle}</div>
-			<div className={styles.message}>
-				Following is a preview of the mail -
-			</div>
+			{(Object.keys(EVENT_INFORMATION_MAPPING) || {}).includes(eventName) ? (
+				<>
+					<div className={styles.title}>{EVENT_INFORMATION_MAPPING[eventName]?.title}</div>
+					<div className={styles.message}>
+						{EVENT_INFORMATION_MAPPING[eventName]?.information}
+					</div>
+				</>
+			) : (
+				<>
+					<div className={styles.title}>{eventTitle}</div>
+					<div className={styles.message}>
+						Following is a preview of the mail -
+					</div>
+				</>
+			)}
 			<div className={styles.banner}>
 				<div className={styles.user_name}>
-					{user_name}
+					{user_name || lead_user_details?.name}
 				</div>
 				<div className={styles.sender_email}>
 					To:
 					{' '}
-					{email}
+					{email || lead_user_details?.email}
 				</div>
 
 				<div className={styles.subject_name}>
--- packages/cogo-one/common/MessageBody/UserActivityMessages/KycEvent/index.js
+++ packages/cogo-one/common/MessageBody/UserActivityMessages/KycEvent/index.js
@@ -4,21 +4,55 @@ import { getEventTitle } from '../../../../utils/getEventTitle';
 
 import styles from './styles.module.css';
 
-function KycEvent({ data = {}, scope = '', name = '' }) {
+function KycEvent({ data = {}, scope = '', name = '', eventName = '' }) {
 	const eventTitle = getEventTitle({ name });
+	const eventPlatform = startCase(scope);
+
+	const EVENT_INFORMATION_MAPPING = {
+		'System: KYC: Requested': {
+			title       : `Customer Has Requested for KYC Verification on ${eventPlatform} platform`,
+			information : 'Please keep an eye out in the event that the customer\'s KYC is verified or rejected.',
+		},
+		'System: KYC: Verified': {
+			title       : `Customer Is Now KYC Verified on ${eventPlatform} platform`,
+			information : `You should send them a message encouraging them to perform a 
+			shipment if they are likely to do so.`,
+		},
+		'System: KYC: Rejected': {
+			title       : `Customer's KYC Verification Request Has Been Rejected on ${eventPlatform} platform`,
+			information : `Please contact them to understand their issue and help them 
+			to get it verified the next time.`,
+		},
+		'System: KYC: Pending Verification': {
+			title       : `Customer KYC Verification Is Still Pending After 3 Days on ${eventPlatform} platform`,
+			information : 'Please check why this might be happening and intimate the customer if there are any issues.',
+		},
+	};
 
 	return (
-		<>
-			<div className={styles.title}>
-				{`${startCase(eventTitle)} on ${startCase(scope)} platform`}
-			</div>
-			<div className={styles.message}>
-				{startCase(data?.error)}
-			</div>
-			<div className={styles.message}>
-				Information on user KYC verification is provided here.
-			</div>
-		</>
+
+		(Object.keys(EVENT_INFORMATION_MAPPING) || {}).includes(eventName) ? (
+			<>
+				<div className={styles.title}>
+					{EVENT_INFORMATION_MAPPING[eventName]?.title}
+				</div>
+				<div className={styles.message}>
+					{EVENT_INFORMATION_MAPPING[eventName]?.information}
+				</div>
+			</>
+		) : (
+			<>
+				<div className={styles.title}>
+					{`${startCase(eventTitle)} on ${startCase(scope)} platform`}
+				</div>
+				<div className={styles.message}>
+					{startCase(data?.error)}
+				</div>
+				<div className={styles.message}>
+					Information on user KYC verification is provided here.
+				</div>
+			</>
+		)
 	);
 }
 
