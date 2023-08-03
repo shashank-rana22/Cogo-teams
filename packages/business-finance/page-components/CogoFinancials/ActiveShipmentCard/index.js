0b7509a6a11087fe1ef71970cfd7d2c29fb30101
Nagendra Singh <nagendra.singh@cogoport.com>
2023-08-03 18:08:57 +0530

    fix: unwanted mounting

--- packages/business-finance/page-components/CogoFinancials/ActiveShipmentCard/index.js
+++ packages/business-finance/page-components/CogoFinancials/ActiveShipmentCard/index.js
@@ -5,57 +5,11 @@ import ParentServicewiseStats from '../Common/ParentServicewiseStats';
 
 import styles from './styles.module.css';
 
-const getCardMapping = ({
-	setActiveShipmentCard, mainCardData, taxType,
-	activeShipmentCard, entity, timeRange, filter, customDate,
-	setShowShipmentList, operationalData,
-	activeBar, setActiveBar, financialData,
-}) => ({
-	ongoing: <ParentServicewiseStats
-		setActiveShipmentCard={setActiveShipmentCard}
-		mainCardData={mainCardData}
-		taxType={taxType}
-		activeShipmentCard={activeShipmentCard}
-		entity={entity}
-		timeRange={timeRange}
-		filter={filter}
-		customDate={customDate}
-	/>,
-	operational: <ClosedGraphStats
-		title="Operationally Closed"
-		setActiveShipmentCard={setActiveShipmentCard}
-		setShowShipmentList={setShowShipmentList}
-		entity={entity}
-		timeRange={timeRange}
-		statsType="OPR_CLOSED"
-		filter={filter}
-		cardData={operationalData}
-		type="Operationally"
-		taxType={taxType}
-		customDate={customDate}
-		activeBar={activeBar}
-		setActiveBar={setActiveBar}
-		defaultWidth="360"
-	/>,
-	financial: <ClosedGraphStats
-		title="Financially Closed"
-		status="financial"
-		setActiveShipmentCard={setActiveShipmentCard}
-		setShowShipmentList={setShowShipmentList}
-		entity={entity}
-		timeRange={timeRange}
-		statsType="FINANCE_CLOSED"
-		filter={filter}
-		cardData={financialData}
-		type="Financially"
-		taxType={taxType}
-		customDate={customDate}
-		activeBar={activeBar}
-		setActiveBar={setActiveBar}
-		defaultWidth="360"
-	/>,
-
-});
+const COMPONENT_KEY_MAPPING = {
+	ongoing     : ParentServicewiseStats,
+	operational : ClosedGraphStats,
+	financial   : ClosedGraphStats,
+};
 
 function ActiveShipmentCard({
 	setActiveShipmentCard = () => {},
@@ -72,24 +26,60 @@ function ActiveShipmentCard({
 	activeBar = '',
 	setActiveBar = () => {},
 }) {
-	const CARD_MAPPINGS = getCardMapping({
-		setActiveShipmentCard,
-		mainCardData,
-		taxType,
-		activeShipmentCard,
-		entity,
-		timeRange,
-		filter,
-		customDate,
-		setShowShipmentList,
-		operationalData,
-		activeBar,
-		setActiveBar,
-		financialData,
-	});
+	const PROPS_KEY_MAPPING = {
+		ongoing: {
+			setActiveShipmentCard,
+			mainCardData,
+			taxType,
+			activeShipmentCard,
+			entity,
+			timeRange,
+			filter,
+			customDate,
+		},
+		operational: {
+			title        : 'Operationally Closed',
+			setActiveShipmentCard,
+			setShowShipmentList,
+			entity,
+			timeRange,
+			statsType    : 'OPR_CLOSED',
+			filter,
+			cardData     : operationalData,
+			type         : 'Operationally',
+			taxType,
+			customDate,
+			activeBar,
+			setActiveBar,
+			defaultWidth : '360',
+		},
+		financial: {
+			title        : 'Financially Closed',
+			status       : 'financial',
+			setActiveShipmentCard,
+			setShowShipmentList,
+			entity,
+			timeRange,
+			statsType    : 'FINANCE_CLOSED',
+			filter,
+			cardData     : financialData,
+			type         : 'Financially',
+			taxType,
+			customDate,
+			activeBar,
+			setActiveBar,
+			defaultWidth : '360',
+		},
+	};
+
+	const ActiveCard = COMPONENT_KEY_MAPPING[activeShipmentCard];
 	return (
 		<div className={styles.active_shipment_card}>
-			{CARD_MAPPINGS[activeShipmentCard]}
+			{ActiveCard && (
+				<ActiveCard
+					{...PROPS_KEY_MAPPING[activeShipmentCard]}
+				/>
+			)}
 		</div>
 	);
 }
