90eb464d63ffef58f25a362f460c18151f0d1b03
vineet-cogo <vineet.kumar@cogoport.com>
2023-08-04 20:17:10 +0530

    done

--- common/ocean-modules/components/Poc/index.js
+++ common/ocean-modules/components/Poc/index.js
@@ -18,7 +18,7 @@ import getServiceProviderData from './helpers/getServiceProviderData';
 import styles from './styles.module.css';
 
 function Poc({ shipment_data = {}, servicesList = [], activeStakeholder = '' }) {
-	const { id:shipment_id, importer_exporter_id, services } = shipment_data || {};
+	const { id:shipment_id, importer_exporter_id, services, is_rate_reverted, shipment_type } = shipment_data || {};
 
 	const [addCompany, setAddCompany] = useState(null);
 	const [addPoc, setAddPoc] = useState(null);
@@ -39,7 +39,13 @@ function Poc({ shipment_data = {}, servicesList = [], activeStakeholder = '' }) 
 		shipment_id,
 	});
 
+	const checkForStakeholders = activeStakeholder === 'booking_agent' || activeStakeholder === 'sales_agent';
+
 	const serviceProviders = getServiceProviderData(servicesList);
+
+	const isPocStakeholdersVisible = checkForStakeholders && shipment_type === 'fcl_freight'
+		? is_rate_reverted
+		: true;
 
 	return (
 		<div>
@@ -58,7 +64,7 @@ function Poc({ shipment_data = {}, servicesList = [], activeStakeholder = '' }) 
 							rolesPermission={rolesPermission}
 						/>
 
-						{rolesViewPermission?.includes('pocs') ? (
+						{rolesViewPermission?.includes('pocs') && isPocStakeholdersVisible ? (
 							<Pocs
 								tradePartnersData={data}
 								setAddPoc={setAddPoc}
