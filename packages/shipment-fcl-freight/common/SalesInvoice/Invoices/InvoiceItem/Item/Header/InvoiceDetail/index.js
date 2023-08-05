70f936d46e9c429b32a173cd1c48813c275b7a21
Bumblebee <rahulk@cogoport.com>
2023-08-05 13:08:46 +0530

    fix

--- packages/shipment-fcl-freight/common/SalesInvoice/Invoices/InvoiceItem/Item/Header/InvoiceDetail/index.js
+++ packages/shipment-fcl-freight/common/SalesInvoice/Invoices/InvoiceItem/Item/Header/InvoiceDetail/index.js
@@ -62,23 +62,7 @@ function InvoiceDetail({
 		});
 	};
 
-	const {
-		einvoicePdfUrl,
-		invoicePdfUrl,
-		proformaPdfUrl,
-		proformaNumber,
-		invoiceNumber,
-		einvoiceNumber,
-	} = bfInvoice || {};
-
 	const creditSource = invoice?.credit_option?.credit_source?.split('_');
-
-	const invoiceUrl = einvoicePdfUrl || invoicePdfUrl || proformaPdfUrl || '';
-
-	let invoiceNo = invoiceNumber || proformaNumber || live_invoice_number;
-	if (einvoicePdfUrl) {
-		invoiceNo = einvoiceNumber;
-	}
 
 	return (
 		<div className={styles.invoice_info}>
@@ -88,10 +72,14 @@ function InvoiceDetail({
 					role="button"
 					tabIndex={0}
 					onClick={() => (!isEmpty(bfInvoice)
-						? handleDownload(invoiceUrl)
+						? handleDownload(
+							bfInvoice?.invoicePdfUrl || bfInvoice?.proformaPdfUrl,
+						)
 						: null)}
 				>
-					{invoiceNo}
+					{bfInvoice?.invoiceNumber
+								|| bfInvoice?.proformaNumber
+								|| live_invoice_number}
 				</div>
 
 				<div className={styles.status_container}>
