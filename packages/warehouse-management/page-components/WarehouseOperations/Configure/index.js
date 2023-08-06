d18776e9e5f0f28e476a71204bf95bcdb0def918
tejoyasaswi-cogoport <Tejo.Uppuluri@cogoport.com>
2023-08-06 23:01:58 +0530

    changes

--- packages/warehouse-management/page-components/WarehouseOperations/Configure/index.js
+++ packages/warehouse-management/page-components/WarehouseOperations/Configure/index.js
@@ -1,9 +1,10 @@
 import { Button } from '@cogoport/components';
-import { IcMEdit } from '@cogoport/icons-react';
+import { IcMEdit, IcmDelete } from '@cogoport/icons-react';
+import { useState } from 'react';
 
 import List from '../../../commons/List';
 import configureFields from '../../../configurations/configure-fields';
-import useListConfigurations from '../../../hooks/useListConfigurations';
+import useListConfiguration from '../../../hooks/useListConfiguration';
 
 import AddNewZoneModal from './AddZone';
 import DeleteZoneModal from './DeleteZone';
@@ -11,18 +12,16 @@ import EditZoneModal from './EditZone';
 import styles from './styles.module.css';
 
 function Configure({
-	// data = {},
-	// handlePageChange = () => {},
-	// page = 1,
-	// setPage = () => {},
 	activeTab = 'configure',
-	// selectedWarehouse = 'delhi',
 	addNewZone = false,
 	setAddNewZone = () => {},
 }) {
 	const { fields } = configureFields;
+	const [item, setItem] = useState({});
+	const [editZone, setEditZone] = useState(false);
+	const [deleteZone, setDeleteZone] = useState(false);
 
-	const { data } = useListConfigurations({ activeTab: { activeTab } });
+	const { data, listAPI } = useListConfiguration();
 
 	const { loading, page, setPage, total_count } = data;
 	const handlePageChange = (pageVal) => {
@@ -30,59 +29,65 @@ function Configure({
 	};
 
 	const functions = {
-		handleEdit: (item) => (
+		handleEdit: () => (
 			<Button
 				themeType="linkUi"
 				disabled={loading}
 				style={{ fontSize: 12 }}
-				onClick={() => {
-					<EditZoneModal
-						item={item}
-					/>;
-				}}
+				onClick={() => setEditZone(true)}
 			>
 				<IcMEdit fill="#8B8B8B" />
 			</Button>
 		),
-		handleDelete: (item) => (
+		handleDelete: () => (
 			<Button
 				themeType="linkUi"
 				disabled={loading}
 				style={{ fontSize: 12 }}
-				onClick={() => {
-					// handleOnDelete(singleItem);
-					// console.log('singleItem---handleDelete', singleItem);
-					<DeleteZoneModal
-						item={item}
-					/>;
-				}}
+				onClick={() => setDeleteZone(true)}
 			>
-				<IcMEdit fill="#8B8B8B" />
+				<IcmDelete fill="#8B8B8B" />
 			</Button>
 		),
 	};
 
 	return (
 		<div className={styles.body_container}>
-			{loading ? <div>tejo</div> : (
-				<div className={styles.details_list}>
-					<List
-						fields={fields}
-						activeTab={activeTab}
-						data={data}
-						loading={loading}
-						total_count={total_count}
-						page={page}
-						setPage={setPage}
-						functions={functions}
-						handlePageChange={handlePageChange}
-					/>
-				</div>
-			)}
+			<div className={styles.details_list}>
+				<List
+					fields={fields}
+					activeTab={activeTab}
+					data={data}
+					loading={loading}
+					total_count={total_count}
+					page={page}
+					setPage={setPage}
+					listAPI={listAPI}
+					setItem={setItem}
+					functions={functions}
+					handlePageChange={handlePageChange}
+				/>
+			</div>
 			{addNewZone && (
 				<AddNewZoneModal
 					addNewZone={addNewZone}
 					setAddNewZone={setAddNewZone}
+					listAPI={listAPI}
+				/>
+			)}
+			{editZone && (
+				<EditZoneModal
+					item={item}
+					editZone={editZone}
+					setEditZone={setEditZone}
+					listAPI={listAPI}
+				/>
+			)}
+			{deleteZone && (
+				<DeleteZoneModal
+					id={item.id}
+					listAPI={listAPI}
+					setDeleteZone={setDeleteZone}
 				/>
 			)}
 		</div>
