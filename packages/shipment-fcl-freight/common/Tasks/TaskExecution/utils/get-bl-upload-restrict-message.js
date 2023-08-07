2c6b46a86c5b7f42f7cbd36c6f736304bc3eb4b6
piyushi-cogo <piyushi.garg@cogoport.com>
2023-08-07 20:41:15 +0530

    pr review

--- packages/shipment-fcl-freight/common/Tasks/TaskExecution/utils/get-bl-upload-restrict-message.js
+++ packages/shipment-fcl-freight/common/Tasks/TaskExecution/utils/get-bl-upload-restrict-message.js
@@ -1,5 +1,5 @@
-const getBlUploadRestrictMessage = ({ bl_state = [] }) => {
-	const isAmend = bl_state?.find((i) => i === 'document_amendment_requested');
+const getBlUploadRestrictMessage = ({ blState = [] }) => {
+	const isAmend = blState?.find((state) => state === 'document_amendment_requested');
 	if (isAmend) return 'Please amend Draft BL and then try to upload';
 
 	return 'Please ask your KAM to Approve Draft BL';
