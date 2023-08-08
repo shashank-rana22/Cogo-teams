ab794201a658010358b4ae1868b07e27b53c1969
piyushi-cogo <piyushi.garg@cogoport.com>
2023-08-08 15:30:01 +0530

    fix

--- common/globalization/constants/geo/IN.js
+++ common/globalization/constants/geo/IN.js
@@ -277,6 +277,7 @@ export default {
 		document_control_lead    : 'ce9831f7-5e5b-419a-8420-679e5ef9c9e9',
 		corporate_owner_demand   : '14fe1423-3c2d-4f66-bc33-89bed08b92ee',
 		finops_credit_controller : '8ab56d1b-b6c1-41e3-9c83-63278380aec7',
+		finance_head             : ['a8a175c5-fec2-4c08-8e6b-0fb5c7951c86', '635658c1-8d6b-4ab5-83a4-bd4989287193'],
 		so1_revenue_desk         : ['348bc262-64c3-4667-a23c-908ceca80233', 'f896df94-f77d-4e6d-b5dd-3a4b936f8401'],
 	},
 	options: {
