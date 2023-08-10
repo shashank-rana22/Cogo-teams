d6835abcdf7ba9a0e3e16178e5440750b54c9481
mona singh <mona.singh@cogoport.com>
2023-08-10 13:21:08 +0530

    changes

--- packages/business-finance/page-components/CogoBook/Accruals/ShipmentView/Card/MoreFilter/index.tsx
+++ packages/business-finance/page-components/CogoBook/Accruals/ShipmentView/Card/MoreFilter/index.tsx
@@ -11,7 +11,7 @@ import {
 } from '../../../../constants/accruals-more-filters';
 import { FilterInterface } from '../../../interface';
 
-import ProfitabilityContent from './profitabilityContent';
+import ProfitabilityContent from './ProfitabilityContent';
 import styles from './styles.module.css';
 
 interface MoreFilterInterface {
