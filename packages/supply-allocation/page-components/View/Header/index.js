f1645fbdf732abe5e4f153d23162eca46f3f9ee4
Niranjan Potnuru <potnuru.niranjan@cogoport.com>
2023-08-10 13:11:11 +0530

    lint and more locations

--- packages/supply-allocation/page-components/View/Header/index.js
+++ packages/supply-allocation/page-components/View/Header/index.js
@@ -1,4 +1,4 @@
-import { Tooltip } from '@cogoport/components';
+import { Button, Tooltip } from '@cogoport/components';
 import { AsyncSelectController, useForm } from '@cogoport/forms';
 import { IcMArrowBack, IcMPortArrow, IcMEdit } from '@cogoport/icons-react';
 import { useRouter } from '@cogoport/next';
@@ -11,11 +11,8 @@ import styles from './styles.module.css';
 const list = ['Persona Distribution', 'Weeky Distribution', 'Container Type'];
 
 const commonLocationProps = {
-	asyncKey : 'supply_fcl_searches',
-	params   : {
-		service_data_required: true,
-	},
-	getModifiedOptions: ({ options }) => (
+	asyncKey           : 'supply_fcl_searches',
+	getModifiedOptions : ({ options }) => (
 		options.map((option) => ({
 			...option,
 			origin_display_name      : option.origin_location.display_name,
@@ -38,114 +35,161 @@ function Graph({ graphData, count }) {
 	);
 }
 
-function RenderLocation({
-	name, editMode, control, type,	locationSearchId, setLocationSearchId,
-}) {
-	console.log('locationSearchId:', locationSearchId);
-	return (
-		<div style={{ width: '180px' }}>
-			{editMode ? (
-				<AsyncSelectController
-					name={`${name.toLowerCase()}_location_id`}
-					control={control}
-					isClearable
-					label={`Select ${name} SeaPort`}
-					labelKey={`${type}_display_name`}
-					valueKey={`${type}_location_id`}
-					{...commonLocationProps}
-					onChange={(_, item) => {
-						setLocationSearchId(item.id);
-					}}
-				/>
-			) : (
-				<Tooltip
-					content={<div className={styles.location_name_tooltip}>{name}</div>}
-					placement="bottom"
-					theme="light"
-					style={{ marginBottom: '24px' }}
-				>
-					<div className={styles.location_name}>{name}</div>
-				</Tooltip>
-			)}
-		</div>
-	);
-}
-
 function Header({ graphData, count, originName, destinationName }) {
 	const router = useRouter();
 	const [editMode, setEditMode] = useState(false);
 	const [locationSearchId, setLocationSearchId] = useState('');
-	const { control } = useForm();
-
+	const { control, watch, reset } = useForm();
+	const { origin_location_id, destination_location_id } = watch();
 	const onClickBack = () => {
 		router.push('/supply-allocation');
 	};
-
+	const onClickSearch = () => {
+		router.push(`/supply-allocation/view/${locationSearchId}`);
+		reset();
+	};
 	return (
 		<>
-			<div
-				style={{
-					display        : 'flex',
-					justifyContent : 'space-between',
-					alignItems     : 'center',
-					background     : '#fff',
-					padding        : '10px',
-					marginBottom   : '10px',
-				}}
-			>
-				<IcMArrowBack height={30} width={30} onClick={onClickBack} />
+			{!editMode
+				? (
+					<div
+						style={{
+							display        : 'flex',
+							justifyContent : 'space-between',
+							alignItems     : 'center',
+							background     : '#fff',
+							padding        : '10px',
+							marginBottom   : '10px',
+						}}
+					>
+						<IcMArrowBack height={30} width={30} onClick={onClickBack} />
 
-				<RenderLocation
-					name={originName}
-					editMode={editMode}
-					control={control}
-					type="origin"
-					locationSearchId={locationSearchId}
-					setLocationSearchId={setLocationSearchId}
+						<Tooltip
+							content={<div className={styles.location_name_tooltip}>{originName}</div>}
+							placement="bottom"
+							theme="light"
+							style={{ marginBottom: '24px' }}
+						>
+							<div className={styles.location_name}>{originName}</div>
+						</Tooltip>
 
-				/>
+						<div>
+							<IcMPortArrow className="port_arrow_icon" width={30} height={30} />
+						</div>
 
-				<div>
-					<IcMPortArrow className="port_arrow_icon" width={30} height={30} />
-				</div>
+						<Tooltip
+							content={(
+								<div className={styles.location_name_tooltip}>
+									{destinationName}
+								</div>
+							)}
+							placement="bottom"
+							theme="light"
+							style={{ marginBottom: '24px' }}
+						>
+							<div className={styles.location_name}>{destinationName}</div>
+						</Tooltip>
 
-				<RenderLocation
-					name={destinationName}
-					editMode={editMode}
-					control={control}
-					type="destination"
-					locationSearchId={locationSearchId}
-					setLocationSearchId={setLocationSearchId}
-				/>
+						<div style={{ display: 'flex', flexDirection: 'column' }}>
+							<div className={styles.sub_heading}>Avg Profitability</div>
+							{!editMode ? <div className={styles.bold}>73%</div> : '--'}
+						</div>
 
-				<div style={{ display: 'flex', flexDirection: 'column' }}>
-					<div className={styles.sub_heading}>Avg Profitability</div>
-					{!editMode ? <div className={styles.bold}>73%</div> : '--'}
-				</div>
+						<div style={{ display: 'flex', flexDirection: 'column' }}>
+							<div className={styles.sub_heading}>% Fulfillment (On best rate)</div>
+							{!editMode ? <div className={styles.bold}>67%</div> : '--'}
+						</div>
 
-				<div style={{ display: 'flex', flexDirection: 'column' }}>
-					<div className={styles.sub_heading}>% Fulfillment (On best rate)</div>
-					{!editMode ? <div className={styles.bold}>67%</div> : '--'}
-				</div>
+						<div style={{ display: 'flex', flexDirection: 'column' }}>
+							<div className={styles.sub_heading}>Forecasted Vol</div>
+							{!editMode ? <div className={styles.volume}>600 TEU</div> : '--'}
+						</div>
 
-				<div style={{ display: 'flex', flexDirection: 'column' }}>
-					<div className={styles.sub_heading}>Forecasted Vol</div>
-					{!editMode ? <div className={styles.volume}>600 TEU</div> : '--'}
-				</div>
+						<div
+							style={{ cursor: 'pointer' }}
+							role="presentation"
+							onClick={() => setEditMode((prev) => !prev)}
+						>
+							<IcMEdit
+								fill="#F68B21"
+								width={14}
+								height={14}
+								style={{ marginRight: 12 }}
+							/>
+						</div>
+					</div>
+				) : (
+					<div
+						style={{
+							display        : 'flex',
+							alignItems     : 'center',
+							background     : '#fff',
+							padding        : '10px',
+							marginBottom   : '10px',
+							justifyContent : 'space-between',
+						}}
+					>
+						<div style={{ display: 'flex', alignItems: 'center' }}>
 
-				<div
-					style={{ cursor: 'pointer' }}
-					role="presentation"
-					onClick={() => setEditMode((prev) => !prev)}
-				>
-					<IcMEdit
-						fill="#F68B21"
-						width={14}
-						height={14}
-						style={{ marginRight: 12 }}
-					/>
-				</div>
-			</div>
+							<IcMArrowBack height={30} width={30} onClick={onClickBack} />
+							<div style={{ width: '350px', marginRight: '20px', marginLeft: '20px' }}>
+								<AsyncSelectController
+									name="origin_location_id"
+									control={control}
+									isClearable
+									labelKey="origin_display_name"
+									valueKey="origin_location_id"
+									{...commonLocationProps}
+									onChange={(_, item) => {
+										setLocationSearchId(item.id);
+									}}
+									params={{
+										service_data_required: true,
+										...(origin_location_id ? { filters: { origin_location_id } } : {}),
+									}}
+									renderLabel={(item) => item.origin_location.display_name}
+								/>
+							</div>
+							<div style={{ marginRight: '20px' }}>
+								<IcMPortArrow className="port_arrow_icon" width={30} height={30} />
+							</div>
+							<div style={{ width: '350px' }}>
+								<AsyncSelectController
+									name="destination_location_id"
+									control={control}
+									isClearable
+									labelKey="destination_display_name"
+									valueKey="destination_location_id"
+									{...commonLocationProps}
+									onChange={(_, item) => {
+										setLocationSearchId(item.id);
+									}}
+									renderLabel={(item) => item.destination_location.display_name}
+									params={{
+										service_data_required: true,
+										...(destination_location_id ? { filters: { destination_location_id } } : {}),
+									}}
+								/>
+
+							</div>
+
+						</div>
+
+						<div style={{ display: 'flex' }}>
+
+							<Button
+								style={{ display: 'flex', marginRight: '20px' }}
+								onClick={() => setEditMode((prev) => !prev)}
+								themeType="secondary"
+							>
+								Cancel
+
+							</Button>
+							<Button themeType="accent" onClick={onClickSearch}>Search</Button>
+
+						</div>
+					</div>
+				)}
 
 			<Graph graphData={graphData} count={count} />
 		</>
