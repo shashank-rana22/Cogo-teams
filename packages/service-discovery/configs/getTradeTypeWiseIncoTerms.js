e299847f45aa583bc536f4a162044ae963d37714
rupesh-mutyala <rupesh.mutyala@cogoport.com>
2023-08-11 16:02:51 +0530

    fix: inco term in checkout

--- packages/service-discovery/configs/getTradeTypeWiseIncoTerms.js
+++ packages/service-discovery/configs/getTradeTypeWiseIncoTerms.js
@@ -1,14 +1,5 @@
 import incoterms from './incoterms.json';
 
-const MAPPING = {
-	export : ['cif', 'cfr', 'cpt', 'cip', 'dat', 'dap', 'ddp', 'exw'],
-	import : ['cpt', 'cip', 'dat', 'dap', 'ddp', 'fob', 'exw', 'fca', 'fas'],
-};
-
-const getTradeTypeWiseIncoTerms = (tradeType = 'export') => {
-	const possibleIncoTerms = MAPPING[tradeType] || MAPPING.export;
-
-	return possibleIncoTerms.map((key) => incoterms.find((incoTerm) => incoTerm.value === key));
-};
+const getTradeTypeWiseIncoTerms = (tradeType = 'export') => incoterms.filter((item) => item.tradeType === tradeType);
 
 export default getTradeTypeWiseIncoTerms;
