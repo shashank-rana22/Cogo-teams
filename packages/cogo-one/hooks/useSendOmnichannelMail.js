a29b3332764cfcb3c0ab8f4a213f237eb7ce19da
danampallyrahul-cogo <danampally.rahul@cogoport.com>
2023-08-11 12:10:55 +0530

    pr changes

--- packages/cogo-one/hooks/useSendOmnichannelMail.js
+++ packages/cogo-one/hooks/useSendOmnichannelMail.js
@@ -1,71 +1,10 @@
 import { Toast } from '@cogoport/components';
 import getApiErrorString from '@cogoport/forms/utils/getApiError';
-import GLOBAL_CONSTANTS from '@cogoport/globalization/constants/globals';
 import { useRequest } from '@cogoport/request';
 import { useSelector } from '@cogoport/store';
-import { isEmpty, startCase } from '@cogoport/utils';
+import { startCase } from '@cogoport/utils';
 
-const ENDPOINT_MAPPING = {
-	forward : '/cogolens/forward_mail',
-	reply   : '/cogolens/reply_mail',
-};
-
-const getCommunicationPayload = ({
-	userId = '',
-	formattedData = {},
-	draftMessage = '',
-	uploadedFiles = [],
-	emailState = {},
-	mailActions = {},
-	name = '',
-	source = '',
-}) => {
-	const { actionType = '', data = {} } = mailActions || {};
-
-	const { response } = data || {};
-
-	const {
-		message_id = '',
-	} = response || {};
-
-	const { conversation_id = '', user_id = '', lead_user_id = '' } = formattedData || {};
-
-	const { ccrecipients = [], bccrecipients = [], subject = '', toUserEmail = [] } = emailState || {};
-
-	const payload = {
-		sender      : source,
-		toUserEmail,
-		ccrecipients,
-		bccrecipients,
-		subject,
-		content     : draftMessage,
-		msgId       : message_id,
-		userId,
-		attachments : isEmpty(uploadedFiles) ? undefined : uploadedFiles,
-	};
-
-	return {
-		type             : 'rpa_email',
-		user_id,
-		lead_user_id,
-		recipient        : payload?.toUserEmail?.[GLOBAL_CONSTANTS.zeroth_index],
-		message_metadata : {
-			endpoint            : ENDPOINT_MAPPING[actionType] || ENDPOINT_MAPPING.send_mail,
-			body                : payload,
-			send_to_omnichannel : true,
-			conversation_id,
-			send_by             : name,
-			sender_user_id      : userId,
-		},
-		sender_user_id  : userId,
-		service         : 'user',
-		service_id      : userId,
-		sender          : payload?.sender,
-		cc_emails       : isEmpty(ccrecipients) ? undefined : ccrecipients,
-		bcc_emails      : isEmpty(bccrecipients) ? undefined : ccrecipients,
-		attachment_urls : isEmpty(uploadedFiles) ? undefined : uploadedFiles,
-	};
-};
+import { getCommunicationPayload } from '../helpers/communicationPayloadHelpers';
 
 const useSendOmnichannelMail = ({
 	scrollToBottom = () => {},
