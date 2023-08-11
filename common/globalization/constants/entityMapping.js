522be7793868c76232b76ef613ef2ba6adf36d48
mona singh <mona.singh@cogoport.com>
2023-08-11 12:54:00 +0530

    fix

--- common/globalization/constants/entityMapping.js
+++ common/globalization/constants/entityMapping.js
@@ -39,11 +39,9 @@ const ENTITY_MAPPING = {
 		icon                : IcCCountryIndia,
 		currency            : 'INR',
 		default_entity_code : '301',
-		feature_supported   : ['cogo_books', 'post_to_sage', 'cancel_irn', 'compliance'],
-		labels              : {
-			irn_label: 'IRN',
-		},
-		GSTIN: ['06AAICC8838P1ZV', '27AAICC8838P1ZR'],
+		feature_supported   : ['cogo_books', 'post_to_sage', 'cancel_irn', 'compliance', 'dunning'],
+		GSTIN               : ['06AAICC8838P1ZV', '07AAACF2136K1ZT',
+			'27AAACF2136K1ZR', '27AAICC8838P1ZR', 'MUMC26454B'],
 	},
 	401: {
 		country_code        : 'SG',
