8f1c7f924589fafc6893d100e85c4f8a533b1458
jatinChoubisaCp <jatin.choubisa@cogoport.com>
2023-08-17 15:46:28 +0530

    cp support new permission

--- packages/cogo-one/page-components/CogoOneChannel/ProfileDetails/OrganizationDetails/index.js
+++ packages/cogo-one/page-components/CogoOneChannel/ProfileDetails/OrganizationDetails/index.js
@@ -91,6 +91,8 @@ function OrganizationDetails({
 		window.open(`/${partnerId}/lead-organization/${leadOrganizationId}`, '_blank');
 	};
 
+	const isCpSupportTeam = VIEW_TYPE_GLOBAL_MAPPING[viewType]?.convert_account_to_cp;
+
 	if (isEmpty(organizationId || leadOrganizationId)) {
 		return (
 			<div className={styles.container}>
@@ -170,7 +172,7 @@ function OrganizationDetails({
 							</Pill>
 						) : null }
 
-						{!hideCpButton && !orgLoading && organizationId ? (
+						{!hideCpButton && !orgLoading && organizationId && isCpSupportTeam ? (
 							<Button
 								size="sm"
 								themeType="primary"
