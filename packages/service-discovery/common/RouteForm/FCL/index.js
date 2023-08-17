9d4b14d1f5d9fa15943f2b66f9c33f0feccd9616
Ritik Goyal <ritik.goyal@cogoport.com>
2023-08-17 11:41:11 +0530

    added: toggle location in route form

--- packages/service-discovery/common/RouteForm/FCL/index.js
+++ packages/service-discovery/common/RouteForm/FCL/index.js
@@ -1,48 +1,31 @@
-import { AsyncSelect } from '@cogoport/forms';
-import GLOBAL_CONSTANTS from '@cogoport/globalization/constants/globals';
-
+import LocationControl from '../common/LocationControl';
+import ToggleLocation from '../common/ToggleLocation';
 import getControls from '../getControls';
 
 import controls from './fcl-route-controls';
 import styles from './styles.module.css';
 
-const DEFAULT_SPAN = 12;
-const PERCENT_FACTOR = 100;
-const FLEX_OFFSET = 2;
-
 function FCLRouteForm({ mode = '', setFormValues = () => {}, formValues = {} }) {
-	const updatedControls = getControls(controls, mode);
+	const [originControls, destinationControls] = getControls(controls, mode);
 
 	return (
 		<div className={styles.container}>
-			{(updatedControls || []).map((controlItem) => {
-				const { label, span, name } = controlItem;
+			<LocationControl
+				formValues={formValues}
+				setFormValues={setFormValues}
+				controlItem={originControls}
+			/>
 
-				const flex = ((span || DEFAULT_SPAN) / DEFAULT_SPAN) * PERCENT_FACTOR - FLEX_OFFSET;
+			<ToggleLocation
+				setFormValues={setFormValues}
+				formValues={formValues}
+			/>
 
-				const key = name.split('_')?.[GLOBAL_CONSTANTS.zeroth_index];
-
-				return (
-					<div
-						key={name}
-						id={`${name}_input_control`}
-						className={styles.form_item}
-						style={{ width: `${flex}%` }}
-					>
-						<div className={styles.label}>
-							{label || ''}
-							{' '}
-							<div className={styles.required_mark}>*</div>
-						</div>
-
-						<AsyncSelect
-							{...controlItem}
-							value={formValues?.[key]?.id}
-							onChange={(val, obj) => setFormValues((prev) => ({ ...prev, [key]: obj }))}
-						/>
-					</div>
-				);
-			})}
+			<LocationControl
+				formValues={formValues}
+				setFormValues={setFormValues}
+				controlItem={destinationControls}
+			/>
 		</div>
 	);
 }
