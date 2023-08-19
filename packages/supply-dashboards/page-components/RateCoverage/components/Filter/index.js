692484c5b4267f306f195d10203f4f2c5b2e8dea
Namanagarwal015 <124123426+Namanagarwal015@users.noreply.github.com>
2023-08-19 14:44:10 +0530

    changes in filters

--- packages/supply-dashboards/page-components/RateCoverage/components/Filter/index.js
+++ packages/supply-dashboards/page-components/RateCoverage/components/Filter/index.js
@@ -1,4 +1,4 @@
-import { Button, Datepicker, Select } from '@cogoport/components';
+import { Button, Select } from '@cogoport/components';
 import { useEffect } from 'react';
 
 import Controls from '../../configurations/filter';
@@ -13,19 +13,30 @@ function Filter({ getStats, filter = {}, setFilter = () => {}, defaultFilterData
 	return (
 		<div className={styles.parent}>
 			<div className={styles.heading}>
-				<span style={{ fontWeight: '600' }}>Apply Filter</span>
+				<span style={{ fontWeight: '700', fontSize: '16px' }}>Apply Filter</span>
 				&nbsp;
 				(These filters will be applied throughout the page)
+				<Button
+					size="md"
+					themeType="tertiary"
+					onClick={() => {
+						setFilter(defaultFilterData);
+					}}
+					style={{ float: 'right', color: '#828282', fontWeight: '600', paddingBottom: '16px' }}
+				>
+					Clear All Filters
+				</Button>
 			</div>
+
 			<div className={styles.filter_container}>
 
 				{Controls().map((control) => (
-					<div key={control.name}>
+					<div key={control.name} style={{ width: control.width, marginLeft: '20px' }}>
 						<div style={{ marginBottom: '5px' }}>
 							{control.heading}
 						</div>
 						<Select
-							placeholder="Type here..."
+							placeholder={control.placeholder || 'Type Here...'}
 							value={filter[control.name]}
 							onChange={(val) => {
 								setFilter({ ...filter, [control.name]: val });
@@ -36,44 +47,6 @@ function Filter({ getStats, filter = {}, setFilter = () => {}, defaultFilterData
 
 					</div>
 				))}
-				<div>
-					<div style={{ marginBottom: '5px' }}>Validity Start Date</div>
-
-					<Datepicker
-						placeholder="Enter Date"
-						isPreviousDaysAllowed
-						dateFormat="MM/dd/yyyy HH:mm"
-						name="validity_end_less_than"
-						onChange={(val) => {
-							setFilter({ ...filter, validity_start_greater_than: val });
-						}}
-						value={filter.validity_start_greater_than}
-					/>
-				</div>
-				<div>
-					<div style={{ marginBottom: '5px' }}>Validity End Date</div>
-					<Datepicker
-						placeholder="Enter Date"
-						isPreviousDaysAllowed
-						dateFormat="MM/dd/yyyy HH:mm"
-						name="validity_end_less_than"
-						onChange={(val) => {
-							setFilter({ ...filter, validity_end_less_than: val });
-						}}
-						value={filter.validity_end_less_than}
-					/>
-				</div>
-
-				<Button
-					size="md"
-					themeType="tertiary"
-					onClick={() => {
-						setFilter(defaultFilterData);
-					}}
-					style={{ marginLeft: '-15px', float: 'right' }}
-				>
-					Clear All Filters
-				</Button>
 
 			</div>
 
