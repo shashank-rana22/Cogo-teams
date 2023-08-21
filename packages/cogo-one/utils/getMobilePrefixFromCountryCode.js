ad6229550b5130f4a6294e8cc63d6420f469ec3f
jatinChoubisaCp <jatin.choubisa@cogoport.com>
2023-08-21 18:23:59 +0530

    pr changes

--- packages/cogo-one/utils/getMobilePrefixFromCountryCode.js
+++ packages/cogo-one/utils/getMobilePrefixFromCountryCode.js
@@ -1,10 +1,8 @@
-import GLOBAL_CONSTANTS from '@cogoport/globalization/constants/globals';
-
 // eslint-disable-next-line
 import countries from '@/data-store/constants/countries.json';
 
 const IGNORE_PLUS = 1;
 export const getMobilePrefixFromCountryCode = ({ countryCode = 'IN' }) => {
-	const requiredCountry = countries.filter(({ country_code }) => country_code === countryCode);
-	return requiredCountry?.[GLOBAL_CONSTANTS.zeroth_index]?.mobile_country_code?.substr(IGNORE_PLUS) || '';
+	const requiredCountry = countries.find(({ country_code }) => country_code === countryCode);
+	return requiredCountry?.mobile_country_code?.substr(IGNORE_PLUS) || '';
 };
