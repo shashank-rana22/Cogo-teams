ab609dd7ae71b8f96cf0d43450f5519347b809f7
rajkamalCP <98801338+rajkamalCP@users.noreply.github.com>
2023-08-21 20:23:34 +0530

    Update index.js

--- scripts/pre-builds/countries/index.js
+++ scripts/pre-builds/countries/index.js
@@ -17,7 +17,7 @@ const setCountries = async () => {
 	await getCountries(
 		fs.writeFileSync,
 		countriesPath,
-		'https://api.stage.cogoport.io/',
+		'https://api.cogoport.com/',
 	);
 	// eslint-disable-next-line no-console
 	console.log('Successfully Built Countries ...');
