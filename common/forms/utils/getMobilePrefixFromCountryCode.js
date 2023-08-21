f08755a74d73869c4f155e6d484e54a2993b32f6
jatinChoubisaCp <jatin.choubisa@cogoport.com>
2023-08-21 19:57:34 +0530

    added 91

--- common/forms/utils/getMobilePrefixFromCountryCode.js
+++ common/forms/utils/getMobilePrefixFromCountryCode.js
@@ -4,5 +4,5 @@ import countries from '@/data-store/constants/countries.json';
 const IGNORE_PLUS = 1;
 export const getMobilePrefixFromCountryCode = ({ countryCode = 'IN' }) => {
 	const requiredCountry = countries.find(({ country_code }) => country_code === countryCode);
-	return requiredCountry?.mobile_country_code?.substr(IGNORE_PLUS) || '';
+	return requiredCountry?.mobile_country_code?.substr(IGNORE_PLUS) || '91';
 };
