6716bd250ac7bbd7322325ced53269b976cf5af5
Niranjan Potnuru <potnuru.niranjan@cogoport.com>
2023-08-23 17:18:39 +0530

    removed utils

--- packages/supply-allocation/page-components/View/SubBucketColumns/index.js
+++ packages/supply-allocation/page-components/View/SubBucketColumns/index.js
@@ -1,8 +1,25 @@
-import GetOrdinalNumber from '../../../utils/GetOrdinalNumber';
+import GLOBAL_CONSTANTS from '@cogoport/globalization/constants/globals';
 
 import Promised from './Promised';
 import ServiceProvider from './ServiceProvider';
 import styles from './styles.module.css';
+
+const C = 100;
+const DEF = GLOBAL_CONSTANTS.zeroth_index;
+const SUB = 20;
+const ANO_SUB = 10;
+
+function GetOrdinalNumber({ number = 0 }) {
+	const suffix = ['th', 'st', 'nd', 'rd'];
+	const quotient = number % C;
+	const ordinal =		suffix[(quotient - SUB) % ANO_SUB] || suffix[quotient] || suffix[DEF];
+	return (
+		<>
+			<span className="black">{number}</span>
+			<sup>{ordinal}</sup>
+		</>
+	);
+}
 
 const getSubBucketColumns = ({
 	control = {},
