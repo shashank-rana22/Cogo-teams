a129afb4b6950d59f6bdaf63b6b2a5e3fae80921
rugvedui <rugved.chilap@cogoport.com>
2023-08-25 16:57:32 +0530

    added back button

--- packages/revenue-desk/page-components/ControlPanel/index.js
+++ packages/revenue-desk/page-components/ControlPanel/index.js
@@ -1,3 +1,5 @@
+import { IcMArrowBack } from '@cogoport/icons-react';
+import { useRouter } from 'next/router';
 import { useState } from 'react';
 
 import FilterLayout from './FilterLayout';
@@ -6,12 +8,20 @@ import styles from './styles.module.css';
 import TableLayout from './TableLayout';
 
 function ControlPanel() {
+	const router = useRouter();
 	const [data, setData] = useState({});
 	const [filter, setFilter] = useState({ service_type: 'fcl_freight' });
 	const { apiTrigger } = useCreateRDAutomationParameters({ setData });
 	return (
 		<div className={styles.outerContainer}>
-			<div className={styles.headTitle}>Automation Desk</div>
+			<div
+				className={styles.back_button}
+				role="presentation"
+				onClick={() => router.push('/revenue-desk')}
+			>
+				<IcMArrowBack style={{ width: '1.5em', height: '1.5em' }} />
+				<div className={styles.headTitle}>Automation Desk</div>
+			</div>
 			<FilterLayout apiTrigger={apiTrigger} filter={filter} setFilter={setFilter} />
 			{data ? <TableLayout data={data} apiTrigger={apiTrigger} filter={filter} /> : null}
 
