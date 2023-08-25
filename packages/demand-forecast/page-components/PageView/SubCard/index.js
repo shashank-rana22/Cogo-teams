d9a2b94720b9e620f8f7749c9274b08013c48928
VedantKirveCogo <vedant.kirve@cogoport.com>
2023-08-25 17:45:49 +0530

    added popover

--- packages/demand-forecast/page-components/PageView/SubCard/index.js
+++ packages/demand-forecast/page-components/PageView/SubCard/index.js
@@ -1,3 +1,5 @@
+import { cl, Popover } from '@cogoport/components';
+import { IcMPortArrow } from '@cogoport/icons-react';
 import { isEmpty } from '@cogoport/utils';
 import { useEffect } from 'react';
 
@@ -12,6 +14,49 @@ const KEYS_MAPPING = {
 	high_demanding_port_pairs : 'High Demand Port Pairs',
 	remaining_clusters        : 'Remining Clusters',
 };
+
+function PopOverContent({ remaining_port_pairs = [] }) {
+	return (
+		<div className={styles.popover}>
+			<div className={styles.popover_header}>
+				<div className={cl`${styles.head} ${styles.origin_header}`}>Origin</div>
+				<div className={cl`${styles.head} ${styles.destination_header}`}>Destination</div>
+			</div>
+			{
+			remaining_port_pairs.map((port_pair) => {
+				const { origin_location = {}, destination_location = {} } = port_pair;
+				return (
+					<div key={port_pair} className={styles.popover_body}>
+						<div className={styles.name}>
+							<div className={styles.port_code}>
+								(
+								{origin_location?.port_code}
+								)
+							</div>
+							<div>
+								{origin_location?.name}
+							</div>
+						</div>
+						<div className={styles.arrow}>
+							<IcMPortArrow width={16} />
+						</div>
+						<div className={styles.name}>
+							<div className={styles.port_code}>
+								(
+								{destination_location?.port_code}
+								)
+							</div>
+							<div>
+								{destination_location?.name}
+							</div>
+						</div>
+					</div>
+				);
+			})
+			}
+		</div>
+	);
+}
 
 function SubCard({ showDetails = false, origin_cluster_id = '', destination_cluster_id = '' }) {
 	const { getRollingForecastPortPairs, data:portPairData, loading } = useGetRollingForecastPortPairs();
@@ -47,9 +92,31 @@ function SubCard({ showDetails = false, origin_cluster_id = '', destination_clus
 				!isEmpty(portPairData[key]) && (
 					<div key={key} className={styles.card}>
 						<div className={styles.title}>
-							{KEYS_MAPPING[key]}
-							{' '}
-							:
+							{
+								key === 'remaining_clusters' ? (
+									<Popover
+										placement="right"
+										content={(
+											<PopOverContent
+												remaining_port_pairs={portPairData?.remaining_port_pairs}
+											/>
+										)}
+										trigger="mouseenter"
+									>
+										<span className={styles.popover_txt}>
+											{KEYS_MAPPING[key]}
+											{' '}
+											:
+										</span>
+									</Popover>
+								) : (
+									<span>
+										{KEYS_MAPPING[key]}
+										{' '}
+										:
+									</span>
+								)
+							}
 						</div>
 						{key === 'high_demanding_port_pairs' && (
 							<div>
