88335f984048f6932e8b62f5ff56f013a8f12598
Swastik-Cogoport <swastik.sharma@cogoport.com>
2023-08-25 18:10:17 +0530

    additional pr changes done

--- packages/printing-desk/utils/getSelectAllCheckbox.js
+++ packages/printing-desk/utils/getSelectAllCheckbox.js
@@ -1,4 +1,5 @@
 import { Checkbox } from '@cogoport/components';
+import { useTranslation } from 'next-i18next';
 
 import { DOCUMENT_COPIES } from './documentCopies';
 import styles from './styles.module.css';
@@ -6,6 +7,7 @@ import styles from './styles.module.css';
 const TOTAL_AWB_COPIES = 12;
 
 export function GetSelectAllCheckbox({ copiesOnChange = () => {}, copiesValue = [] }) {
+	const { t } = useTranslation(['printingDesk']);
 	const onChangeTableHeaderCheckbox = (event) => {
 		copiesOnChange(event.currentTarget.checked ? DOCUMENT_COPIES : []);
 	};
@@ -13,7 +15,7 @@ export function GetSelectAllCheckbox({ copiesOnChange = () => {}, copiesValue = 
 
 	return (
 		<Checkbox
-			label="Select All"
+			label={t('printingDesk:utils_get_select_all_checkbox_label')}
 			value="select_all"
 			className={styles.select_checkbox}
 			checked={isAllRowsChecked}
