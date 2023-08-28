565e40de49178d05369abbaca89466e142d9c0c5
VedantKirveCogo <vedant.kirve@cogoport.com>
2023-08-28 16:57:52 +0530

    deleting unwanted config

--- packages/demand-forecast/configurations/icon-mapping.js
+++
@@ -1,9 +0,0 @@
-import { IcMFcl, IcMFlcl, IcMFairport } from '@cogoport/icons-react';
-
-const ICON_MAPPING = {
-	fcl_freight : <IcMFcl />,
-	lcl_freight : <IcMFlcl />,
-	air_freight : <IcMFairport />,
-};
-
-export default ICON_MAPPING;
--- packages/demand-forecast/configurations/short-form-service.js
+++
@@ -1,7 +0,0 @@
-const SHORT_FORM_MAPPING = {
-	fcl_freight : 'FCL',
-	lcl_freight : 'LCL',
-	air_freight : 'AIR',
-};
-
-export default SHORT_FORM_MAPPING;
