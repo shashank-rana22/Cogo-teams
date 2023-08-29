4ac09803e0015b903b1fc8b11e78566c20da14d0
Niranjan Potnuru <potnuru.niranjan@cogoport.com>
2023-08-29 15:40:33 +0530

    info

--- packages/demand-forecast/configurations/supplier-table-config.js
+++ packages/demand-forecast/configurations/supplier-table-config.js
@@ -1,3 +1,6 @@
+import { Tooltip } from '@cogoport/components';
+import { IcMInfo } from '@cogoport/icons-react';
+
 const getSupplierTableConfig = ({ t }) => {
 	const config = [
 		{
@@ -7,8 +10,22 @@ const getSupplierTableConfig = ({ t }) => {
 		},
 		{
 			key   : 'win_probability',
-			title : t('demandForecast:win_probability'),
-			width : '15%',
+			title : (
+				<div style={{ display: 'flex' }}>
+					{t('demandForecast:win_probability')}
+					<Tooltip
+						content={(
+							<div style={{ wordBreak: 'break-word' }}>
+								{t('demandForecast:win_probability_tooltip_content')}
+							</div>
+						)}
+						placement="top"
+					>
+						<IcMInfo style={{ alignSelf: 'center', height: '100%', marginLeft: '2px' }} />
+					</Tooltip>
+				</div>
+			),
+			width: '15%',
 		},
 		{
 			key   : 'capability',
