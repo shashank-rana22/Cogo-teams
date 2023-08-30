7377c30c907da3b5e1becbb7fb97fa48153f0bcc
Niranjan Potnuru <potnuru.niranjan@cogoport.com>
2023-08-30 19:22:26 +0530

    pr

--- packages/supply-allocation/commons/StyledTable/index.js
+++ packages/supply-allocation/commons/StyledTable/index.js
@@ -6,7 +6,6 @@ function StyledTable({ data = [], columns = [], loading = false }) {
 	return (
 		<div className={styles.table_container}>
 			<Table
-				className={styles.table_container}
 				data={data}
 				columns={columns}
 				loading={loading}
