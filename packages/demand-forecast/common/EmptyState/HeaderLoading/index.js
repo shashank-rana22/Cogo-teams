a0a615d611f53a2a88ea8beb4704d0778ea37fc8
VedantKirveCogo <vedant.kirve@cogoport.com>
2023-08-31 18:18:04 +0530

    loader corrected

--- packages/demand-forecast/common/EmptyState/HeaderLoading/index.js
+++ packages/demand-forecast/common/EmptyState/HeaderLoading/index.js
@@ -7,11 +7,11 @@ const PLACE_HOLDER_WIDTH = ['100px', '300px', '100px', '300px', '200px', '200px'
 function HeaderLoading() {
 	return (
 		<div className={styles.container}>
-			{PLACE_HOLDER_WIDTH.map((width) => (
+			{[...Array(PLACE_HOLDER_WIDTH.length).keys()].map((i) => (
 				<Placeholder
-					key={width}
+					key={i}
 					height="25px"
-					width={width}
+					width={PLACE_HOLDER_WIDTH[i]}
 					margin="10px 4px"
 				/>
 			))}
