4334f676e3a11cd0288aa1d1176afda37c8057db
Rajatuba-Cogoport <rajat.uba@cogoport.com>
2023-09-01 11:16:05 +0530

    changes

--- packages/supply-tools/page-components/CommodityClusters/CommodityForm/index.js
+++ packages/supply-tools/page-components/CommodityClusters/CommodityForm/index.js
@@ -1,5 +1,6 @@
 import { useForm } from '@cogoport/forms';
 import GLOBAL_CONSTANTS from '@cogoport/globalization/constants/globals';
+import { useImperativeHandle } from 'react';
 
 import Layout from '../../../common/Layout';
 
@@ -10,20 +11,30 @@ const OFFSET_LAST_INDEX = 1;
 
 const handleContainerTypeChange = ({ setValue = () => {}, name }) => {
 	const splitArr = name.split('.');
+
 	splitArr[splitArr.length - OFFSET_LAST_INDEX] = 'commodity';
+
 	setValue(splitArr.join('.'), []);
 };
 
-function CommodityForm({ item = {} }) {
+function CommodityForm({ item = {}, handleSubmitForm = () => {} }, ref) {
 	const controls = getControls({ item });
 
-	const { control, formState:{ errors = {} } = {}, watch = () => {}, setValue } = useForm();
+	const { control, formState:{ errors = {} } = {}, watch, setValue, handleSubmit } = useForm();
 
 	controls[COMMODITY_INDEX].controls[GLOBAL_CONSTANTS.zeroth_index].onChange = (
 		_o,
 		_v,
 		name,
 	) => handleContainerTypeChange({ name, setValue });
+
+	const onSubmit = (values) => handleSubmitForm({ data: values, item });
+
+	useImperativeHandle(ref, () => ({
+		formSubmit() {
+			handleSubmit(onSubmit)();
+		},
+	}));
 
 	return (
 		<Layout
--- packages/supply-tools/page-components/CommodityClusters/index.js
+++ packages/supply-tools/page-components/CommodityClusters/index.js
@@ -1,7 +1,46 @@
-import CommodityForm from './CommodityForm';
+import { Input, Table } from '@cogoport/components';
+import { IcMSearchlight } from '@cogoport/icons-react';
+
+import useListFclFreightCommodityClusters from '../../hooks/useListFclFreightCommodityClusters';
+
+import CreateCommodity from './CreateCommodity';
+import getTableColumns from './getTableColumns';
+import styles from './styles.module.css';
 
 function CommodityClusters() {
-	return <CommodityForm />;
+	const tableColumns = getTableColumns({});
+
+	const {
+		data = {}, loading = false, q = '',
+		setQ = () => {},
+	} = useListFclFreightCommodityClusters({ defaultFilters: { status: 'active' } });
+
+	return (
+		<div>
+			<div className={styles.header}>
+				<h1>COMMODITY CLUSTERS</h1>
+
+				<div className={styles.filters_add}>
+					<div className={styles.width_49}>
+						<Input
+							size="sm"
+							prefix={<IcMSearchlight />}
+							placeholder="Search By Cluster Name"
+							onChange={setQ}
+							value={q}
+						/>
+					</div>
+					<div className={styles.width_49}>
+						<CreateCommodity />
+					</div>
+				</div>
+			</div>
+
+			<div className={styles.table_container}>
+				<Table columns={tableColumns} data={data?.list || []} loading={loading} />
+			</div>
+		</div>
+	);
 }
 
 export default CommodityClusters;
