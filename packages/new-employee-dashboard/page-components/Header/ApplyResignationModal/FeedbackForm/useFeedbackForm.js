c2ea9a526d8f382dfc6eeac48dbaac5be363abaa
Shivam Singh <shivam.singh@cogoport.com>
2023-09-01 17:11:37 +0530

    null

--- packages/new-employee-dashboard/page-components/Header/ApplyResignationModal/FeedbackForm/useFeedbackForm.js
+++ packages/new-employee-dashboard/page-components/Header/ApplyResignationModal/FeedbackForm/useFeedbackForm.js
@@ -3,10 +3,7 @@ import { useForm } from '@cogoport/forms';
 import controls from './controls';
 
 const useFeedbackForm = () => {
-	const {
-		control, formState:
-		// { errors }, handleSubmit,
-	} = useForm();
+	const { control, formState:{ errors }, handleSubmit } = useForm();
 
 	return { control, controls };
 };
