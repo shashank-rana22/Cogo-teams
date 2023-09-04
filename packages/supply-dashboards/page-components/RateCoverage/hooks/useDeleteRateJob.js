1c7d5644d6fc6022aebf629898cda14115726c78
sreenivasulu-k-cogoport <sreenivasulu.kamalapuram@cogoport.com>
2023-09-04 13:48:48 +0530

    fixes

--- packages/supply-dashboards/page-components/RateCoverage/hooks/useDeleteRateJob.js
+++ packages/supply-dashboards/page-components/RateCoverage/hooks/useDeleteRateJob.js
@@ -7,9 +7,7 @@ const API_NAME = {
 	air_freight : 'delete_air_freight_rate_job',
 };
 
-const useDeleteRateJob = ({ service = 'fcl_freight', data = {} }) => {
-	const { id = '' } = data;
-
+const useDeleteRateJob = (service) => {
 	const endPoint = API_NAME[service];
 
 	const [checkboxValue, setCheckboxValue] = useState('');
@@ -19,19 +17,41 @@ const useDeleteRateJob = ({ service = 'fcl_freight', data = {} }) => {
 		method : 'POST',
 	}, { manual: true });
 
-	const deleteRateJob = useCallback(async () => {
+	const deleteRateJob = useCallback(async ({ id = '', data = {} }) => {
+		const weight_slabs = [
+			{
+				lower_limit  : data?.lower_limit,
+				upper_limit  : data?.upper_limit,
+				tariff_price : data?.price_per_unit,
+				currency     : 'INR',
+			},
+		];
 		try {
 			await trigger({
 				data: {
 					id,
-					closing_remarks: checkboxValue,
+					origin_airport_id      : data?.origin_airport,
+					destination_airport_id : data?.destination_airport,
+					commodity              : data?.commodity,
+					airline_id             : data?.air_line,
+					operation_type         : data?.flight_operation_type,
+					currency               : data?.currency,
+					price_type             : data?.price_type,
+					service_provider_id    : data?.service_provider,
+					procured_by_id         : data?.rate_procured_by_cogoport_agent,
+					sourced_by_id          : data?.rate_provided_by_lsp_user,
+					validity_start         : data?.startDateTime,
+					validity_end           : data?.endDateTime,
+					commodity_type         : 'all',
+					weight_slabs,
+					closing_remarks        : checkboxValue !== '' ? checkboxValue : undefined,
 				},
 			});
 		} catch (err) {
 			// console.log(err);
 			Toast.error('failed to cancel');
 		}
-	}, [trigger, id, checkboxValue]);
+	}, [trigger, checkboxValue]);
 
 	return {
 		loading,
