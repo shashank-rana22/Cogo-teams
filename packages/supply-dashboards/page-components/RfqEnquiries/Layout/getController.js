20803b68307803460272b19b01ed9975269e7bf4
samir-cogoport <samir.dupare@cogoport.com>
2023-09-05 16:18:27 +0530

    layout changes

--- packages/supply-dashboards/page-components/RfqEnquiries/Layout/getController.js
+++ packages/supply-dashboards/page-components/RfqEnquiries/Layout/getController.js
@@ -1,7 +1,20 @@
 import {
-	InputController, PillsController, SelectController, DatepickerController,
+	InputController,
+	PillsController,
+	SelectController,
+	DatepickerController,
+	SingleDateRangeController,
+	AsyncSelectController,
+	TextAreaController,
+	CheckboxController,
+	CountrySelectController,
+	UploadController,
+	CheckboxGroupController,
+	ToggleController,
+	InputGroupController,
+	CreatableSelectController,
+
 } from '@cogoport/forms';
-import UploadController from '@cogoport/forms/page-components/Controlled/UploadController';
 
 import DepartureDateSelectController from './DepartureDateSelect/index';
 
@@ -16,6 +29,9 @@ const getElementController = (type = 'text') => {
 		case 'select':
 			return SelectController;
 
+		case 'async_select':
+			return AsyncSelectController;
+
 		case 'file':
 			return UploadController;
 
@@ -25,8 +41,33 @@ const getElementController = (type = 'text') => {
 		case 'date_picker':
 			return DatepickerController;
 
+		case 'date_range_picker':
+			return SingleDateRangeController;
+
 		case 'departure_date':
 			return DepartureDateSelectController;
+
+		case 'textArea':
+		case 'textarea':
+			return TextAreaController;
+
+		case 'checkbox':
+			return CheckboxController;
+
+		case 'checkbox_group':
+			return CheckboxGroupController;
+
+		case 'country_select':
+			return CountrySelectController;
+
+		case 'toggle':
+			return ToggleController;
+
+		case 'input-group':
+			return InputGroupController;
+
+		case 'creatable-select':
+			return CreatableSelectController;
 
 		default:
 			return null;
