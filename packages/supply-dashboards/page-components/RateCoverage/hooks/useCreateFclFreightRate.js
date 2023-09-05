e5d89280807b87bbbd8a16c79c9bb141d37a80cd
sreenivasulu-k-cogoport <sreenivasulu.kamalapuram@cogoport.com>
2023-09-05 12:34:06 +0530

    fix

--- packages/supply-dashboards/page-components/RateCoverage/hooks/useCreateFclFreightRate.js
+++ packages/supply-dashboards/page-components/RateCoverage/hooks/useCreateFclFreightRate.js
@@ -17,18 +17,20 @@ const useCreateFclFreightRate = () => {
 		try {
 			const resp = await trigger({
 				data: {
-					origin_port_id      : dataa?.origin_location_id,
-					destination_port_id : dataa?.destination_location_id,
-					container_size      : dataa?.container_size,
-					container_type      : dataa?.container_type,
-					commodity           : dataa?.commodity,
-					validity_start      : dataa?.validity_start,
-					validity_end        : dataa?.validity_end,
-					service_provider_id : dataa?.service_provider_id,
-					shipping_line_id    : dataa?.shipping_line_id,
-					sourced_by_id       : dataa?.sourced_by_id,
-					line_items          : updatedLineItems,
-					procured_by_id      : dataa?.sourced_by_id,
+					origin_port_id           : dataa?.origin_location_id,
+					destination_port_id      : dataa?.destination_location_id,
+					container_size           : dataa?.container_size,
+					container_type           : dataa?.container_type,
+					commodity                : dataa?.commodity,
+					validity_start           : dataa?.validity_start,
+					validity_end             : dataa?.validity_end,
+					service_provider_id      : dataa?.service_provider_id,
+					shipping_line_id         : dataa?.shipping_line_id,
+					sourced_by_id            : dataa?.sourced_by_id,
+					line_items               : updatedLineItems,
+					procured_by_id           : dataa?.sourced_by_id,
+					origin_main_port_id      : dataa?.origin_main_port_id,
+					destination_main_port_id : dataa?.destination_main_port_id,
 				},
 			});
 			if (resp?.data) { return resp?.data?.id; }
