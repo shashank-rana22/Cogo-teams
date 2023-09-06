992982f8b8d20330c411c8cba1c319f242ea7d05
hrixtik <Hritik.Kumar@cogoport.com>
2023-09-06 13:54:37 +0530

    formValues

---
+++ packages/new-employee-dashboard/page-components/HRSeparation/FormComponent/hrbp-view/HRMeeting/NotesForManager/index.js
@@ -0,0 +1,38 @@
+import { Button } from '@cogoport/components';
+import { InputController } from '@cogoport/forms';
+import { IcMArrowDown, IcMArrowUp, IcMArrowRight } from '@cogoport/icons-react';
+import React, { useState } from 'react';
+
+import styles from './styles.module.css';
+
+function NotesForManager({ control }) {
+	const [show, setShow] = useState(false);
+
+	return (
+		<div className={styles.container}>
+			<div className={styles.heading} aria-hidden onClick={() => setShow(!show)}>
+				Notes for Manager
+				{show ? <IcMArrowUp /> : <IcMArrowDown />}
+			</div>
+
+			{show && (
+				<div className={styles.body}>
+					<InputController
+						control={control}
+						name="your_notes"
+						size="md"
+						style={{ marginRight: '8px', width: '100%' }}
+						placeholder="Type your notes here"
+						rules={{ required: 'this is required' }}
+					/>
+					<Button size="md" className={styles.button}>
+						<span style={{ fontSize: '16px' }}>Submit Notes</span>
+						<IcMArrowRight width={16} height={16} />
+					</Button>
+				</div>
+			) }
+		</div>
+	);
+}
+
+export default NotesForManager;
---
+++ packages/new-employee-dashboard/page-components/HRSeparation/FormComponent/hrbp-view/HRMeeting/index.js
@@ -0,0 +1,50 @@
+import { Button } from '@cogoport/components';
+import { useForm } from '@cogoport/forms';
+import { IcMTaskCompleted } from '@cogoport/icons-react';
+import React from 'react';
+
+import EmployeeDetail from '../../commons/EmployeeDetail';
+
+import DatePicker from './DatePicker';
+import InterviewQuestions from './InterviewQuestions';
+import JoiningBonus from './JoiningBonus';
+import NotesForManager from './NotesForManager';
+import styles from './styles.module.css';
+
+function HRMeeting() {
+	const {
+		control,
+		watch,
+		reset,
+		handleSubmit,
+		formState:{ errors = {} },
+	} = useForm();
+
+	const onSubmit = (values) => {
+		console.log(values, 'formValues');
+	};
+	return (
+		<>
+			<div className={styles.header}>
+				<div className={styles.left_header}>
+					<span className={styles.upper_text}>HR MEETING</span>
+					<span className={styles.lower_text}>Summary of application</span>
+				</div>
+				<div className={styles.logs_button}>
+					<Button size="md" themeType="accent" onClick={() => handleSubmit(onSubmit)()}>
+						<IcMTaskCompleted />
+						<span style={{ marginLeft: '4px' }}>Notes & Logs</span>
+					</Button>
+				</div>
+			</div>
+
+			<EmployeeDetail />
+			<DatePicker control={control} watch={watch} reset={reset} errors={errors} />
+			<JoiningBonus control={control} errors={errors} />
+			<InterviewQuestions control={control} errors={errors} />
+			<NotesForManager control={control} />
+		</>
+	);
+}
+
+export default HRMeeting;
