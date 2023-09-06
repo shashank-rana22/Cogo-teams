cb5997421ab0c9abcdfa55167bfea79fd61823c5
ankitraj-cogoport <ankit.rajpoot@cogoport.com>
2023-09-06 19:07:11 +0530

    fixes

--- common/shipment-chat/utils/getStaticPath.js
+++ common/shipment-chat/utils/getStaticPath.js
@@ -1,6 +1,6 @@
 import GLOBAL_CONSTANTS from '@cogoport/globalization/constants/globals';
 
-const getStaticPath = (path) => {
+const getStaticPath = ({ path = '' }) => {
 	const url = path
 		? `${process.env.STATIC_ASSETS_URL}/${path}`
 		: process.env.STATIC_ASSETS_URL || '';
