d60845c2ee4336358b8fbb6945f599090ff55961
Shivam Singh <shivam.singh@cogoport.com>
2023-09-07 19:19:45 +0530

    manager handover takeover

--- packages/new-employee-dashboard/page-components/HRSeparation/FormComponent/index.js
+++ packages/new-employee-dashboard/page-components/HRSeparation/FormComponent/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 import AdminClearance from './admin-view/admin-clearance';
 import AdminConfirmed from './admin-view/admin-confirmation';
+import HOTOClearanceConfirmation from './employee-view/HOTOClearanaceConfirmation';
+import HOTOClearance from './employee-view/HOTOClearance';
 import FinanceClearance from './hrbp-view/FinanceClearance';
 import HandoverTakeoverClearance from './hrbp-view/HandoverTakeoverClearance';
 import HRMeeting from './hrbp-view/HRMeeting';
 import ManagerClearance from './hrbp-view/ManagerClearance';
 import TechClearanceHrbp from './hrbp-view/TechClearanceHrbp';
-import HOTOClearanceConfirmation from './employee-view/HOTOClearanaceConfirmation';
-import HOTOClearance from './employee-view/HOTOClearance';
-import HRMeeting from './HRMeeting';
+import HandoverTakeover from './manager-view/HandoverTakeover';
 import ReviewRequest from './manager-view/ReviewRequest';
 import styles from './styles.module.css';
 import TechClearance from './tech-view/TechClearance';
@@ -25,9 +25,9 @@ const COMPONENT_MAPPING = {
 		tech_clearance              : TechClearanceHrbp,
 	},
 	manager: {
-		review_request: ReviewRequest,
+		review_request    : ReviewRequest,
+		handover_takeover : HandoverTakeover,
 	},
-<<<<<<< Updated upstream
 	tech: {
 		tech_clearance         : TechClearance,
 		tech_clearance_confirm : TechClearanceConfirm,
@@ -36,17 +36,15 @@ const COMPONENT_MAPPING = {
 		admin_clearance    : AdminClearance,
 		admin_confirmation : AdminConfirmed,
 	},
-=======
 	employee: {
 		HOTOClearance,
 		HOTOClearance_confirmation: HOTOClearanceConfirmation,
 	},
 
->>>>>>> Stashed changes
 };
 
 function FormComponent() {
-	const Render = COMPONENT_MAPPING.admin.admin_clearance;
+	const Render = COMPONENT_MAPPING.manager.handover_takeover;
 
 	return (
 		<div className={styles.container}>
