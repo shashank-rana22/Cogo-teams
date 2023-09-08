326940453949fb1995045f77f3626f2712e81c41
tejoyasaswi-cogoport <Tejo.Uppuluri@cogoport.com>
2023-09-08 16:07:31 +0530

    translations

--- packages/warehouse-management/index.js
+++ packages/warehouse-management/index.js
@@ -1,11 +1,1 @@
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-
 export { default as WarehouseManagement } from './page-components';
-
-export async function getServerSideProps({ locale }) {
-	return {
-		props: {
-			...(await serverSideTranslations(locale, ['common'])),
-		},
-	};
-}
