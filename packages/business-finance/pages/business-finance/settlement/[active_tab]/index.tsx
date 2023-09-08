b4285a4247c203a28925a4c9b06fe56ea0b2f97e
cogoport-ritik <ritik.piplani@cogoport.com>
2023-09-08 15:59:28 +0530

    settlement-translation

--- packages/business-finance/pages/business-finance/settlement/[active_tab]/index.tsx
+++ packages/business-finance/pages/business-finance/settlement/[active_tab]/index.tsx
@@ -5,7 +5,7 @@ export { default } from '@cogoport/business-finance/page-components/Settlement';
 export async function getServerSideProps({ locale }) {
 	return {
 		props: {
-			...(await serverSideTranslations(locale, ['common'])),
+			...(await serverSideTranslations(locale, ['common', 'settlement'])),
 		},
 	};
 }
