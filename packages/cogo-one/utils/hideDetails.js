6ba6dbf8c7b6de3b37528e1c72cacd9cfe78f6ae
Lachiramnaik1330 <malothu.lachiramnaik@cogoport.com>
2023-09-08 18:18:35 +0530

    minor changes

--- packages/cogo-one/utils/hideDetails.js
+++ packages/cogo-one/utils/hideDetails.js
@@ -2,7 +2,6 @@ import GLOBAL_CONSTANTS from '@cogoport/globalization/constants/globals';
 
 const CHECK_STRING_LENGTH = 2;
 const LAST_VARIABLE_IN_STRING = 1;
-const FIRST_STRING_VALUE = 0;
 const THIRD_STRING_VALUE = 3;
 
 function hideDetails({
@@ -12,7 +11,10 @@ function hideDetails({
 }) {
 	let finalString = data;
 	if (type === 'number' && data?.length > CHECK_STRING_LENGTH) {
-		finalString = `${countryCode}${data.substring(FIRST_STRING_VALUE, THIRD_STRING_VALUE)}****${data.substring(
+		finalString = `${countryCode}${data.substring(
+			GLOBAL_CONSTANTS.zeroth_index,
+			THIRD_STRING_VALUE,
+		)}****${data.substring(
 			data.length - CHECK_STRING_LENGTH,
 			data.length,
 		)}`;
@@ -21,7 +23,7 @@ function hideDetails({
 	if (type === 'mail') {
 		const strings = data?.split('@');
 		finalString = `${strings[GLOBAL_CONSTANTS.zeroth_index].substring(
-			FIRST_STRING_VALUE,
+			GLOBAL_CONSTANTS.zeroth_index,
 			THIRD_STRING_VALUE,
 		)}***@${
 			strings[strings.length - LAST_VARIABLE_IN_STRING]
