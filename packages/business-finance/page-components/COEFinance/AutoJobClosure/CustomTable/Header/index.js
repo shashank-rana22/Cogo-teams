90244b468cb66306a9fbbc27909260342c98159b
mayank <mayank.yadav@cogoport.com>
2023-09-08 19:49:16 +0530

    job-closure-config to json 2

--- packages/business-finance/page-components/COEFinance/AutoJobClosure/CustomTable/Header/index.js
+++ packages/business-finance/page-components/COEFinance/AutoJobClosure/CustomTable/Header/index.js
@@ -20,7 +20,7 @@ function Header({ config = {} }) {
 					className={cl`${styles.col} ${field.className}`}
 					key={field.key}
 					style={{
-						span  : field.span || DEFAULT_SPAN,
+						flex  : field.span || DEFAULT_SPAN,
 						width : `${((field.span || DEFAULT_SPAN) * (HUNDERED_PERCENT / TOTAL_SPAN))}px`,
 					}}
 				>
