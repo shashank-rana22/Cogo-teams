6c92b7c620661b1205c4bc2b35a9bb572871960c
Rohit Patil <rohit.patil@cogoport.com>
2023-09-10 18:44:21 +0530

    adding changes

--- packages/supply-tools/hooks/useCreateUpdateFclWeightSlabs.js
+++ packages/supply-tools/hooks/useCreateUpdateFclWeightSlabs.js
@@ -3,7 +3,7 @@ import { useRequest } from '@cogoport/request';
 import { useSelector } from '@cogoport/store';
 import { useState } from 'react';
 
-import getCreateFclRatePayload from '../helpers/getCreateFclRatePayload';
+import getCreateUpdateWeightSlabPayload from '../helpers/getCreateUpdateWeightSlabPayload';
 import toastApiError from '../utils/toastApiError';
 
 const useCreateUpdateFclWeightSlabs = ({
@@ -18,19 +18,24 @@ const useCreateUpdateFclWeightSlabs = ({
 
 	const [isCogoAssured, setIsCogoAssured] = useState(item?.is_cogo_assured);
 
+	let url = '/create_fcl_weight_slabs_configuration';
+	if (item?.isEdit) {
+		url = '/update_fcl_weight_slabs_configuration';
+	}
+
 	const handleCloseModal = () => {
 		setItem(false);
 	};
 
 	const [{ loading }, trigger] = useRequest({
-		url    : '/create_fcl_weight_slabs_configuration',
-		method : 'POST',
+		url,
+		method: 'POST',
 		scope,
-	});
+	}, { manual: true });
 
 	const apiTrigger = async (val) => {
 		try {
-			const payload = getCreateFclRatePayload({ data: val });
+			const payload = getCreateUpdateWeightSlabPayload({ data: val });
 
 			const res = await trigger({ data: payload });
 
