ab2e628d71fccb73c1b173a29c7c42d9f6cd2560
Nagendra Singh <nagendra.singh@cogoport.com>
2023-09-11 16:06:17 +0530

    fix: invoice tagging accordian ui

--- packages/business-finance/page-components/COEFinance/All_Invoices/ViewInvoices/Taggings/index.tsx
+++ packages/business-finance/page-components/COEFinance/All_Invoices/ViewInvoices/Taggings/index.tsx
@@ -1,4 +1,5 @@
-import { Accordion, Button } from '@cogoport/components';
+import { Button } from '@cogoport/components';
+import { IcMArrowRotateDown, IcMArrowRotateUp } from '@cogoport/icons-react';
 import React, { useState } from 'react';
 
 import ShowIcon from './ShowIcon';
@@ -14,42 +15,56 @@ function Tagging({ billId = '', setRemarksVal = () => {}, status = '', setCheckI
 		taggingRemark: string[];
 	}>> }) {
 	const [value, setValue] = useState({ approve: '', reject: '', undo: '', remark: '' });
+	const [showData, setShowData] = useState(false);
+
+	const onAccept = () => {
+		setShowData(false);
+		setCheckItem(
+			(prev: any) => ({ ...prev, taggingCheck: true }),
+		);
+	};
 
 	return (
-		<div>
-			<Accordion
-				type="text"
-				animate
-				title={(
-					<div className={styles.heading_data}>
-						<div className={styles.business_name}>
-							Invoice Tagging
-							{' '}
-							<ShowIcon value={value} />
-						</div>
-					</div>
-				)}
-				style={{ padding: '6px 0' }}
+		<div style={{ padding: '0 20px' }}>
+			<div
+				className={styles.heading_data}
+				role="presentation"
+				onClick={() => setShowData(!showData)}
 			>
-				<TagMap
-					billId={billId}
-					value={value}
-					setValue={setValue}
-					setRemarksVal={setRemarksVal}
-					status={status}
-				/>
-				<Button
-					size="md"
-					themeType="secondary"
-					style={{ marginRight: '8px' }}
-					onClick={() => setCheckItem(
-						(prev: any) => ({ ...prev, taggingCheck: true }),
-					)}
-					className={styles.approve_button}
-				>
-					Accept
-				</Button>
-			</Accordion>
+				<div className={styles.business_name}>
+					Invoice Tagging
+					{' '}
+					<ShowIcon value={value} />
+				</div>
+				<div>
+					{
+					showData
+						? <IcMArrowRotateUp height={16} width={16} />
+						: <IcMArrowRotateDown height={16} width={16} />
+}
+				</div>
+			</div>
+
+			{showData && (
+				<div>
+					<TagMap
+						billId={billId}
+						value={value}
+						setValue={setValue}
+						setRemarksVal={setRemarksVal}
+						status={status}
+					/>
+					<Button
+						size="md"
+						themeType="secondary"
+						style={{ marginRight: '8px' }}
+						onClick={onAccept}
+						className={styles.approve_button}
+					>
+						Accept
+					</Button>
+				</div>
+			)}
 		</div>
 	);
 }
