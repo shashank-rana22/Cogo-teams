56fbb3d05160df0e2edee75bd706ef5f28a41dcb
Rohit Patil <rohit.patil@cogoport.com>
2023-09-11 17:08:24 +0530

    adding changes

--- packages/saas/page-components/AirTracking/index.js
+++ packages/saas/page-components/AirTracking/index.js
@@ -1,8 +1,21 @@
+import { Button } from '@cogoport/components';
+import { IcMPlus } from '@cogoport/icons-react';
 import React from 'react';
+
+import styles from './styles.module.css';
 
 function AirTracking() {
 	return (
-		<div>AirTracking</div>
+		<div className={styles.container}>
+			<header className={styles.heading_container}>
+				<h1>Air Cargo Tracking</h1>
+
+				<Button size="lg">
+					<IcMPlus style={{ marginRight: 8 }} />
+					Create New Tracker
+				</Button>
+			</header>
+		</div>
 	);
 }
 
