7457ca768df48e12f52e47a89969488dcf7518cf
Akshay Kumar Adhana <akshay@Akshays-MacBook-Air.local>
2023-09-12 14:37:53 +0530

    first commit

---
+++ packages/locations/page-components/LocationClusters/Header/index.js
@@ -0,0 +1,10 @@
+import styles from './styles.module.css';
+
+function Header() {
+	return (
+		<div className={styles.container}>
+			<p className={styles.heading}>Location Clusters</p>
+		</div>
+	);
+}
+export default Header;
---
+++ packages/locations/page-components/LocationClusters/index.js
@@ -0,0 +1,11 @@
+import Header from './Header';
+import styles from './styles.module.css';
+
+function LocationClusters() {
+	return (
+		<div className={styles.container}>
+			<Header />
+		</div>
+	);
+}
+export default LocationClusters;
