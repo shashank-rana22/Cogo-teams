9f8f7f36091e6fbb995cce29fee8443a97733021
hrixtik <Hritik.Kumar@cogoport.com>
2023-09-13 21:59:52 +0530

    shifting

--- packages/attendance-leave-management/page-components/Separation/FormComponent/hrbp-view/HRMeeting/Heading/index.js
+++ packages/attendance-leave-management/page-components/Separation/FormComponent/hrbp-view/HRMeeting/Heading/index.js
@@ -1,6 +1,6 @@
-import { Button, Tags } from '@cogoport/components';
+import { Button, Pill } from '@cogoport/components';
 import { IcMTaskCompleted, IcCFtick, IcMClock } from '@cogoport/icons-react';
-import React, { useState } from 'react';
+import React from 'react';
 
 import styles from './styles.module.css';
 
@@ -8,17 +8,6 @@ function Heading({
 	title = 'HR MEETING', subTitle = 'Summary from manager interaction',
 	isComplete = false, name = '',
 }) {
-	const options = [
-		{
-			key      : '1',
-			disabled : false,
-			children : isComplete ? 'Completed' : 'Pending',
-			color    : isComplete ? 'green' : 'orange',
-			tooltip  : false,
-		},
-	];
-	const [items, setItems] = useState(options);
-
 	return (
 		<>
 			<div className={styles.header}>
@@ -27,7 +16,9 @@ function Heading({
 					<span className={styles.lower_text}>{subTitle}</span>
 				</div>
 				<div className={styles.logs_button}>
-					<Tags items={items} onItemsChange={setItems} size="xl" />
+					<Pill size="xl" color={isComplete ? 'green' : 'orange'} style={{ height: '32px' }}>
+						{isComplete ? 'Completed' : 'Pending'}
+					</Pill>
 					<Button size="md" themeType="accent">
 						<IcMTaskCompleted />
 						<span style={{ marginLeft: '4px' }}>Notes & Logs</span>
