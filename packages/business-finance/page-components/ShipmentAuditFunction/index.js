2b64e2533bc367bada1af832129d3e118c5d1d37
SahilMazumder-Cogoport <sahil.mazumder@cogoport.com>
2023-09-13 12:55:23 +0530

    Changes

---
+++ packages/business-finance/page-components/ShipmentAuditFunction/index.js
@@ -0,0 +1,11 @@
+import styles from './styles.module.css';
+
+function ShipmentAuditFunction() {
+	return (
+		<main>
+			<div className={styles.main_heading}>Shipment Audit Function</div>
+		</main>
+	);
+}
+
+export default ShipmentAuditFunction;
