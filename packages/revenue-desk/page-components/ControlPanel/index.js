2eb132fa31476183a360bba9100ac7f1069aa783
rugvedui <rugved.chilap@cogoport.com>
2023-09-14 16:07:53 +0530

    adedd chanhes

--- packages/revenue-desk/page-components/ControlPanel/index.js
+++ packages/revenue-desk/page-components/ControlPanel/index.js
@@ -1,5 +1,6 @@
 import { Tabs, TabPanel } from '@cogoport/components';
 import { IcMArrowBack } from '@cogoport/icons-react';
+import { useSelector } from '@cogoport/store';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 
@@ -9,6 +10,7 @@ import styles from './styles.module.css';
 
 function ControlPanel() {
 	const router = useRouter();
+	const partnerId = useSelector((s) => s?.profile?.partner?.id);
 	const [activeTab, setActiveTab] = useState('automation_desk');
 
 	return (
@@ -16,7 +18,7 @@ function ControlPanel() {
 			<div
 				className={styles.back_button}
 				role="presentation"
-				onClick={() => router.push('/revenue-desk')}
+				onClick={() => router.push(`/${partnerId}/revenue-desk`)}
 			>
 				<IcMArrowBack style={{ width: '1.5em', height: '1.5em' }} />
 				<div className={styles.headTitle}>Go Back</div>
