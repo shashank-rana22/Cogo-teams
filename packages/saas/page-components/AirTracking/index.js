06d14de03a73861cbc2daed9b5707a6b6960addd
Rohit Patil <rohit.patil@cogoport.com>
2023-09-14 15:11:47 +0530

    new component

--- packages/saas/page-components/AirTracking/index.js
+++ packages/saas/page-components/AirTracking/index.js
@@ -1,34 +1,10 @@
-import { Button } from '@cogoport/components';
-import { IcMPlus } from '@cogoport/icons-react';
-import React, { useState } from 'react';
-
 import styles from './styles.module.css';
 
 function AirTracking() {
-	const [airTrackers, setAirTrackers] = useState({});
-	const [isMobile, setIsMobile] = useState(false);
-	const [isMapView, setMapView] = useState(false);
-
-	console.log(airTrackers, isMobile, isMapView, setMapView, setAirTrackers, setIsMobile);
-
 	return (
 		<div className={styles.container}>
 			<header className={styles.heading_container}>
 				<h1>Air Cargo Tracking</h1>
-
-				<div
-					className={styles.button_container}
-					{...{ 'data-instructional-pre-overlay-step': '4' }}
-					{...{ 'data-instructional-overlay-step': '4' }}
-				>
-					<Button
-						size="lg"
-						id="air_trackers_create_new_tracker_btn"
-					>
-						<IcMPlus style={{ marginRight: 4 }} />
-						Create New Tracker
-					</Button>
-				</div>
 			</header>
 		</div>
 	);
