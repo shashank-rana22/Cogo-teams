b6199a2477d55d4513b1877395375d969822163a
hrixtik <Hritik.Kumar@cogoport.com>
2023-09-14 19:20:13 +0530

    finance api integrated

--- packages/attendance-leave-management/page-components/Separation/FormComponent/hrbp-view/FinanceClearance/columns.js
+++ packages/attendance-leave-management/page-components/Separation/FormComponent/hrbp-view/FinanceClearance/columns.js
@@ -1,22 +1,54 @@
-import { cl } from '@cogoport/components';
+import { cl, Popover } from '@cogoport/components';
+import { IcMEdit } from '@cogoport/icons-react';
 import { startCase } from '@cogoport/utils';
 import React from 'react';
 
 import styles from './styles.module.css';
 
-export const fnfColumns = [
+export const getFnfColumns = ({ onStatusChange = () => {} }) => [
 	{ Header: 'PARTICULARS', accessor: 'companyLoan' },
 	{ Header: 'RECOVERABLE AMOUNT', accessor: 'advanceAmount' },
 	{
 		Header   : 'STATUS',
 		accessor : (item) => (
-			<div className={cl`${styles.pending_flex}
-			${styles[item.status === 'pending' ? 'pending_color' : 'approved_color']}`}
+			<div
+				className={cl`${styles.pending_flex}
+				${styles[(item.status === null || item.status === 'not_recovered')
+					? 'pending_color' : 'approved_color']}`}
 			>
-				<div className={cl`${styles.pending_dot} 
-				${styles[item.status === 'pending' ? 'pending_color_bg' : 'approved_color_bg']}`}
+				<div className={cl`${styles.pending_dot}
+					${styles[(item.status === null || item.status === 'not_recovered')
+					? 'pending_color_bg' : 'approved_color_bg']}`}
 				/>
-				{startCase(item?.status) || '-'}
+				{startCase((item.status === null || item.status === 'not_recovered'))
+					? 'Not recovered' : 'Recovered' || '-'}
+
+				<Popover
+					placement="left"
+					render={(
+						<div>
+							<div
+								aria-hidden
+								onClick={() => onStatusChange({ id: item.id, status: 'not_recovered' })}
+								className={styles.popover_red}
+							>
+								Not Recovered
+							</div>
+
+							<div
+								aria-hidden
+								className={styles.popover_green}
+								onClick={() => onStatusChange({ id: item.id, status: 'recovered' })}
+							>
+								Recovered
+							</div>
+						</div>
+					)}
+				>
+					<div>
+						<IcMEdit style={{ marginLeft: '4px', cursor: 'pointer' }} />
+					</div>
+				</Popover>
 			</div>
 		),
 	},
@@ -32,10 +64,10 @@ export const outstandingColumns = [
 		Header   : 'STATUS',
 		accessor : (item) => (
 			<div className={cl`${styles.pending_flex}
-        ${styles[item.status === 'pending' ? 'pending_color' : 'approved_color']}`}
+        ${styles[item.status === 'Pending' ? 'pending_color' : 'approved_color']}`}
 			>
 				<div className={cl`${styles.pending_dot} 
-            ${styles[item.status === 'pending' ? 'pending_color_bg' : 'approved_color_bg']}`}
+            ${styles[item.status === 'Pending' ? 'pending_color_bg' : 'approved_color_bg']}`}
 				/>
 				{startCase(item?.status) || '-'}
 			</div>
