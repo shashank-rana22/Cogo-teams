54ff38228f7c3f4ae00d64cd2ccc27a89f11ac8f
RoshanOraonCogoport <roshan.oraon@cogoport.com>
2023-09-14 10:31:42 +0530

    changes

--- packages/business-finance/page-components/AccountPayables/index.js
+++ packages/business-finance/page-components/AccountPayables/index.js
@@ -39,7 +39,7 @@ function AccountPayables() {
 
 	const handleTabChange = (v) => {
 		if (
-			['payruns', 'outstanding', 'invoices'].includes(v)
+			['payruns', 'invoices'].includes(v)
 		) {
 			window.location.href = `/${partnerId}/business-finance/account-payables/${v}`;
 			return;
