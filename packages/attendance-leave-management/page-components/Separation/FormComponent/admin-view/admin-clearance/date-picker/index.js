b73618146d16312b5c14d14e77316ac84da03101
Shivam Singh <shivam.singh@cogoport.com>
2023-09-14 11:58:33 +0530

    PR

--- packages/attendance-leave-management/page-components/Separation/FormComponent/admin-view/admin-clearance/date-picker/index.js
+++ packages/attendance-leave-management/page-components/Separation/FormComponent/admin-view/admin-clearance/date-picker/index.js
@@ -9,14 +9,12 @@ const date = GLOBAL_CONSTANTS.formats.date['dd/MM/yyyy'];
 
 function DatePicker({ control, errors, is_complete = false }) {
 	return (
-
 		<div className={styles.container}>
-
 			<div className={styles.heading}>Last Working Day</div>
 
 			<div className={styles.dates}>
-
 				<IcMCalendar width={20} height={20} className={styles.icon} />
+
 				<DatepickerController
 					placeholder="Select Date"
 					control={control}
@@ -26,12 +24,8 @@ function DatePicker({ control, errors, is_complete = false }) {
 					rules={{ required: 'this is required' }}
 					disabled={is_complete}
 				/>
-				{errors.last_working_day && (
-					<span className={styles.error}>*required</span>
-				)}
-
+				{errors.last_working_day ? <span className={styles.error}>*required</span> : null}
 			</div>
-
 		</div>
 
 	);
