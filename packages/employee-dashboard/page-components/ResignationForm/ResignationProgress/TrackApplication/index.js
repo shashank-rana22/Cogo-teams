2f0b922b85fc9b14d7365a1304a07fdce749db06
Shashank Rana <shashank.rana@cogoport.com>
2023-09-14 16:34:53 +0530

    resignation-form-progress

--- packages/employee-dashboard/page-components/ResignationForm/ResignationProgress/TrackApplication/index.js
+++ packages/employee-dashboard/page-components/ResignationForm/ResignationProgress/TrackApplication/index.js
@@ -1,6 +1,5 @@
-/* eslint-disable max-len */
-import { Button, Stepper } from '@cogoport/components';
-import { IcMError, IcMArrowRight, IcMCross, IcMArrowDown, IcMEmail, IcMProfile, IcMClock } from '@cogoport/icons-react';
+import { Stepper } from '@cogoport/components';
+import { IcMArrowDown, IcMEmail, IcMProfile, IcMClock } from '@cogoport/icons-react';
 import React, { useState, useEffect, useCallback } from 'react';
 
 import ExitInterview from '../ExitInterview';
@@ -19,10 +18,13 @@ const STEPPER_ITEMS = [
 
 function TrackApplication({ data = {} }) {
 	const [show, setShow] = useState(true);
-	const [warningShow, warningSetShow] = useState(true);
+
+	// code for later requirements...
+	// const [warningShow, warningSetShow] = useState(true);
+
 	const [inProgressObject, setInProgressObject] = useState({});
 	const [authorityName, setAuthorityName] = useState('');
-	// const PERSON_NAME = 'Shivam Singh';
+
 	const [activeKey, setActiveKey] = useState('hr_meet');
 
 	const applicationStatusStepper = useCallback((dataset = {}) => {
@@ -35,7 +37,6 @@ function TrackApplication({ data = {} }) {
 		const arr = Object.values(dataset?.process_status);
 		const inFoundProgressObject = arr.find((obj) => obj.status === 'in_progress');
 		setInProgressObject(inFoundProgressObject);
-		// console.log('obj arr :: ',inFoundProgressObject);
 		Object.keys(dataset?.process_status).forEach((key) => {
 			const value = dataset.process_status[key];
 			if (value.status === 'in_progress') {
@@ -48,7 +49,8 @@ function TrackApplication({ data = {} }) {
 		// 		setAuthorityName(key);
 		// 	}
 		// }
-		// console.log('key set to find :: ', Object.entries(dataset?.process_status).find((element) => element[1].status !== 'completed'));
+		// console.log('key set to find :: ',
+		// Object.entries(dataset?.process_status).find((element) => element[1].status !== 'completed'));
 	}, []);
 
 	useEffect(() => {
@@ -75,7 +77,9 @@ function TrackApplication({ data = {} }) {
 			</div>
 
 			<div className={show ? styles.show_application : styles.hide_application}>
-				<div
+				{/* Code for later requirements.... */}
+				{/*
+				 <div
 					className={warningShow
 						? styles.completed_notification_container_show
 						: styles.completed_notification_container_hide}
@@ -100,7 +104,7 @@ function TrackApplication({ data = {} }) {
 							onClick={() => warningSetShow(false)}
 						/>
 					</div>
-				</div>
+				</div> */}
 
 				<div className={styles.stepper_container}>
 					<Stepper
@@ -131,10 +135,11 @@ function TrackApplication({ data = {} }) {
 					<div className={styles.waiting_notification_container}>
 						{authorityName === 'hr_meet' && (
 							<div className={styles.waiting_notification_text}>
-								You will be contacted by the HR for the meeting. Post meeting the separation process will formally start.
+								You will be contacted by the HR for the meeting.
+								Post meeting the separation process will formally start.
 							</div>
 						)}
-						{(authorityName !== 'hr_meet' && authorityName === 'exit_interview')
+						{(authorityName !== 'hr_meet' && authorityName !== 'exit_interview')
 						&& (
 							<>
 								<IcMClock height="22px" width="22px" color="#F68B21" />
@@ -149,10 +154,13 @@ function TrackApplication({ data = {} }) {
 					</div>
 
 				</div>
-				<ExitInterview
-					data={data}
-					inProgressObject={inProgressObject}
-				/>
+				{authorityName === 'exit_interview'
+				&& (
+					<ExitInterview
+						data={data}
+						inProgressObject={inProgressObject}
+					/>
+				) }
 
 			</div>
 
