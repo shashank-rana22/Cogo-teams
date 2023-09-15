9f519266a73b7b960b4b90abecfaad6c5ae8687b
Akshay Kumar Adhana <akshay@Akshays-MacBook-Air.local>
2023-09-15 19:17:37 +0530

    initial commit

---
+++ packages/locations/page-components/LocationClusters/hooks/useDeleteCluster.js
@@ -0,0 +1,31 @@
+import { Toast } from '@cogoport/components';
+import { useRequest } from '@cogoport/request';
+
+import toastApiError from '../utils/toastApiError';
+
+const useDeleteCluster = ({ refetch = () => {}, data = {} }) => {
+	const [{ loading }, trigger] = useRequest({
+		url    : '/update_location_cluster',
+		method : 'POST',
+	}, { manual: true });
+
+	const handleDeleteCluster = async () => {
+		try {
+			await trigger({
+				data: {
+					id     : data?.id,
+					status : 'inactive',
+				},
+			});
+			Toast.success('Cluster Deleted Successfully');
+			await refetch();
+		} catch (error) {
+			toastApiError(error);
+		}
+	};
+	return {
+		handleDeleteCluster,
+		loading,
+	};
+};
+export default useDeleteCluster;
