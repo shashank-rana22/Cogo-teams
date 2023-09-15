52c3e26f00684df149675a3cb84e12e90e950dd6
Mohit Nagar <mohit.nagar@cogoport.com>
2023-09-15 16:46:38 +0530

    left panel

---
+++ packages/performance-and-incentives/page-components/PublicLeaderboard/components/Body/LeftPanel/List/get-list-column-mapping.js
@@ -0,0 +1,45 @@
+import { isEmpty } from '@cogoport/utils';
+
+import styles from './styles.module.css';
+
+const getListColumnMapping = () => {
+	const LIST_COLUMN_MAPPING = [
+		{
+			id       : 'rank',
+			key      : 'rank',
+			flex     : 1,
+			Header   : <div className={styles.top_heading}>Rank</div>,
+			accessor : ({ rank }) => (isEmpty(rank) ? null : <p className={styles.rank}>{rank}</p>),
+		},
+		{
+			id       : 'name',
+			key      : 'name',
+			flex     : 4,
+			Header   : <div className={styles.top_heading}>Name</div>,
+			accessor : ({ name }) => (isEmpty(name) ? null : <p className={styles.name}>{name}</p>),
+		},
+		{
+			id       : 'score',
+			key      : 'score',
+			flex     : 1.5,
+			Header   : <div className={styles.top_heading}>Score</div>,
+			accessor : ({ score }) => (isEmpty(score) ? null : <p>{score}</p>),
+		},
+		{
+			id       : 'percentile',
+			key      : 'percentile',
+			flex     : 1.5,
+			Header   : <div className={styles.top_heading}>%ile</div>,
+			accessor : ({ percentile }) => (isEmpty(percentile) ? null : (
+				<p>
+					{percentile}
+					%
+				</p>
+			)),
+		},
+	];
+
+	return LIST_COLUMN_MAPPING;
+};
+
+export default getListColumnMapping;
---
+++ packages/performance-and-incentives/page-components/PublicLeaderboard/components/Body/LeftPanel/index.js
@@ -0,0 +1,24 @@
+import List from './List';
+import styles from './styles.module.css';
+import TopUsers from './TopUsers';
+import useGetLeaderbordList from './useGetLeaderbordList';
+
+function LeftPanel() {
+	const { list } = useGetLeaderbordList();
+
+	const [firstUser, secondUser, thirdUser, ...tableList] = list;
+
+	const topList = [secondUser, firstUser, thirdUser];
+
+	return (
+		<div className={styles.container}>
+			<p className={styles.heading}>LEADER BOARD</p>
+
+			<TopUsers top_list={topList} />
+
+			<List table_list={tableList} />
+		</div>
+	);
+}
+
+export default LeftPanel;
