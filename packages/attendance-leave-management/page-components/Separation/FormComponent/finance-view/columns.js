09a68f857002a7f14b1d983798c81bd48572b7c5
Udit Chavan <Udit.ChavanDattatray@cogoport.com>
2023-09-15 19:23:11 +0530

    bug fix

--- packages/attendance-leave-management/page-components/Separation/FormComponent/finance-view/columns.js
+++ packages/attendance-leave-management/page-components/Separation/FormComponent/finance-view/columns.js
@@ -12,9 +12,7 @@ const fnfColumns = ({
 		accessor : (item = {}) => (
 			<section className={styles.particular_container}>
 				<div>{item?.particular}</div>
-				{errors?.item?.particular && (
-					<div className={styles.error}>*Required</div>
-				)}
+				{errors?.item?.particular ? <div className={styles.error}>*Required</div> : null}
 			</section>
 		),
 	},
@@ -24,24 +22,21 @@ const fnfColumns = ({
 	},
 	{
 		Header   : 'RECOVERABLE AMOUNT',
-		accessor : (item) => {
-			console.log(item, 'item');
-			return (
-				<section>
-					<InputController
-						control={control}
-						placeholder="Type Amount here"
-						className={styles.name_input}
-						name={`${item.particular}RecoverableAmount`}
-						disabled
-						value={item?.recoverable_amount}
-						size="md"
-						type="number"
-						rules={{ required: { value: true, message: '*This Field is required' } }}
-					/>
-				</section>
-			);
-		},
+		accessor : (item) => (
+			<section>
+				<InputController
+					control={control}
+					placeholder="Type Amount here"
+					className={styles.name_input}
+					name={`${item.particular}RecoverableAmount`}
+					disabled
+					value={item?.recoverable_amount}
+					size="md"
+					type="number"
+					rules={{ required: { value: true, message: '*This Field is required' } }}
+				/>
+			</section>
+		),
 	},
 ]);
 export default fnfColumns;
