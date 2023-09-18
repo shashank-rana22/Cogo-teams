d34207c3434dfcc5a765d6a59c3b1af8e5e72b8e
whyaryanakhouri <aryan.akhouri@cogoport.com>
2023-09-18 19:35:18 +0530

    changes

--- packages/purchase-invoicing/page-components/InvoiceLowerContainer/styles.js
+++ packages/purchase-invoicing/page-components/InvoiceLowerContainer/styles.js
@@ -1,17 +1,17 @@
 const styles = {
 	fourth_container: {
-		border: '1px solid black',
+		border: '1px solid #000',
 	},
 	amount: {
 		width     : '70%',
-		border    : '1px solid black',
+		border    : '1px solid #000',
 		textAlign : 'left',
 		minHeight : '90px',
 		padding   : '10px',
 	},
 	tax: {
 		width     : '30%',
-		border    : '1px solid black',
+		border    : '1px solid #000',
 		textAlign : 'left',
 		minHeight : '90px',
 		padding   : '10px',
@@ -20,13 +20,13 @@ const styles = {
 		display        : 'flex',
 		justifyContent : 'flex-start',
 		padding        : '10px',
-		border         : '1px solid black',
+		border         : '1px solid #000',
 		minHeight      : '130px',
 		width          : '70%',
 	},
 	signature: {
 		width     : '30%',
-		border    : '1px solid black',
+		border    : '1px solid #000',
 		minHeight : '130px',
 		padding   : '10px',
 	},
