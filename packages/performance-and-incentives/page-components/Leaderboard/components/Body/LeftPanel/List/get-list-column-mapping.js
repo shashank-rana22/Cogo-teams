d018fd8a598e5dcb97721bab94171a8f4089c9b0
Madhesh Medasani <madhesh.medasani@cogoport.com>
2023-09-20 19:49:29 +0530

    sorting

--- packages/performance-and-incentives/page-components/Leaderboard/components/Body/LeftPanel/List/get-list-column-mapping.js
+++ packages/performance-and-incentives/page-components/Leaderboard/components/Body/LeftPanel/List/get-list-column-mapping.js
@@ -1,15 +1,46 @@
+import { IcMArrowRotateUp, IcMArrowRotateDown } from '@cogoport/icons-react';
 import { isEmpty } from '@cogoport/utils';
 
 import styles from './styles.module.css';
 
+const handleSort = ({ params = {}, setParams = () => {}, sortBy }) => {
+	if (params.sort_by === sortBy) {
+		if (params.sort_type === 'asc') {
+			setParams((prev) => ({ ...prev, sort_type: 'desc' }));
+		} else {
+			setParams((prev) => ({ ...prev, sort_type: 'asc' }));
+		}
+		return;
+	}
+	setParams((prev) => ({ ...prev, sort_by: sortBy, sort_type: 'desc' }));
+};
+
+function ArrowType({ sortBy = '', sortType = '', key = '' }) {
+	if (sortBy !== key) {
+		return <IcMArrowRotateUp className={styles.arrow} />;
+	}
+
+	if (sortType === 'asc') {
+		return <IcMArrowRotateUp className={styles.arrow} />;
+	}
+
+	return <IcMArrowRotateDown className={styles.arrow} />;
+}
+
 const getListColumnMapping = () => {
 	const LIST_COLUMN_MAPPING = [
 		{
-			id       : 'rank',
-			key      : 'rank',
-			flex     : 1,
-			Header   : <div className={styles.top_heading}>Rank</div>,
-			accessor : ({ rank }) => (isEmpty(rank) ? null : <p className={styles.rank}>{rank}</p>),
+			id   : 'rank',
+			key  : 'rank',
+			flex : 1,
+			Header:
+				(
+					<div role="presentation" className={styles.top_heading} onClick={() => handleSort({})}>
+						<div>Rank</div>
+						<ArrowType key="rank" />
+					</div>
+				),
+			accessor: ({ rank }) => (isEmpty(rank) ? null : <p className={styles.rank}>{rank}</p>),
 		},
 		{
 			id       : 'name',
@@ -19,18 +50,30 @@ const getListColumnMapping = () => {
 			accessor : ({ name }) => (isEmpty(name) ? null : <p className={styles.name}>{name}</p>),
 		},
 		{
-			id       : 'score',
-			key      : 'score',
-			flex     : 1.5,
-			Header   : <div className={styles.top_heading}>Score</div>,
-			accessor : ({ score }) => (isEmpty(score) ? null : <p>{score}</p>),
+			id   : 'score',
+			key  : 'score',
+			flex : 1.5,
+			Header:
+				(
+					<div role="presentation" className={styles.top_heading} onClick={() => handleSort({})}>
+						<div>Score</div>
+						<ArrowType key="score" />
+					</div>
+				),
+			accessor: ({ score }) => (isEmpty(score) ? null : <p>{score}</p>),
 		},
 		{
-			id       : 'percentile',
-			key      : 'percentile',
-			flex     : 1.5,
-			Header   : <div className={styles.top_heading}>%ile</div>,
-			accessor : ({ percentile }) => (isEmpty(percentile) ? null : (
+			id   : 'percentile',
+			key  : 'percentile',
+			flex : 1.5,
+			Header:
+				(
+					<div role="presentation" className={styles.top_heading} onClick={() => handleSort({})}>
+						<div>%ile</div>
+						<ArrowType key="percentile" />
+					</div>
+				),
+			accessor: ({ percentile }) => (isEmpty(percentile) ? null : (
 				<p>
 					{percentile}
 					%
