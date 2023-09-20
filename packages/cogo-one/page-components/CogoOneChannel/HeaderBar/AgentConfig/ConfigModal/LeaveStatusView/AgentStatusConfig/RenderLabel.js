d70f15296270ae4f7b5cc542f348fedd5515265f
Bishal1184 <bishal.saha@cogoport.com>
2023-09-20 11:34:42 +0530

    time-utils

--- packages/cogo-one/page-components/CogoOneChannel/HeaderBar/AgentConfig/ConfigModal/LeaveStatusView/AgentStatusConfig/RenderLabel.js
+++ packages/cogo-one/page-components/CogoOneChannel/HeaderBar/AgentConfig/ConfigModal/LeaveStatusView/AgentStatusConfig/RenderLabel.js
@@ -1,24 +1,49 @@
+import GLOBAL_CONSTANTS from '@cogoport/globalization/constants/globals';
+import formatDate from '@cogoport/globalization/utils/formatDate';
 import { startCase } from '@cogoport/utils';
 
 import styles from './styles.module.css';
 
-const TIME_FORMAT = 24;
-const ONE_MINUTE = 60;
-const CHECK_MERIDIEM = 12;
-const STRING_LENGTH = 2;
+const INDEX_OF_ZONE = 1;
+const TOTAL_HOURS = 24;
+const TOTAL_MINUTES = 60;
+const DEFAULT_TIME = 0;
+const HOURS_TO_CHANGE = 1;
 
-function formatTimeToIST({ timeString, localTime }) {
-	const [hours, minutes] = timeString.split(':').map(Number);
-	const [localHour, localMinute] = localTime.replace('+', '').split(':').map(Number);
+export const getDateFromTime = ({
+	timeStr = '00:00:00',
+	timeZone = '+00:00',
+}) => {
+	const [hour = 0, minute = 0, seconds = 0] = timeStr.split(':');
 
-	const ISTHours = (hours + localHour) % TIME_FORMAT;
-	const ISTMinutes = (minutes + localMinute) % ONE_MINUTE;
-	const period = ISTHours < CHECK_MERIDIEM ? 'AM' : 'PM';
-	const ISTHours12 = ISTHours % CHECK_MERIDIEM || CHECK_MERIDIEM;
+	const GMTZone = timeZone.slice(GLOBAL_CONSTANTS.zeroth_index, INDEX_OF_ZONE);
+	const timeDifference = timeZone.slice(INDEX_OF_ZONE);
+	const [hourDifference, minuteDifference] = timeDifference.split(':');
 
-	return `${ISTHours12.toString().padStart(STRING_LENGTH, '0')}:${ISTMinutes.toString().padStart(STRING_LENGTH, '0')} 
-	${period}`;
-}
+	const formattedMinute = GMTZone === '+'
+		? Number(minute) + Number(minuteDifference)
+		: Number(minute) - Number(minuteDifference);
+
+	const formattedHour = (
+		(GMTZone === '+'
+			? Number(hour) + Number(hourDifference)
+			: Number(hour) - Number(hourDifference))
+	+ Number(formattedMinute >= TOTAL_MINUTES ? HOURS_TO_CHANGE : DEFAULT_TIME)
+	- (formattedMinute < DEFAULT_TIME ? HOURS_TO_CHANGE : DEFAULT_TIME)
+	);
+
+	const currentTime = new Date();
+
+	currentTime.setHours(((formattedHour % TOTAL_HOURS) + TOTAL_HOURS) % TOTAL_HOURS);
+	currentTime.setMinutes(((formattedMinute % TOTAL_MINUTES) + TOTAL_MINUTES) % TOTAL_MINUTES);
+	currentTime.setSeconds(Number(seconds) || DEFAULT_TIME);
+
+	return formatDate({
+		date       : currentTime,
+		timeFormat : GLOBAL_CONSTANTS.formats.time['hh:mm aaa'],
+		formatType : 'time',
+	});
+};
 
 function RenderLabel({ item = {} }) {
 	const {
@@ -32,11 +57,11 @@ function RenderLabel({ item = {} }) {
 				Shift :
 				{' '}
 				<span>
-					{formatTimeToIST({ timeString: start_time_utc, localTime: local_time_zone })}
+					{getDateFromTime({ timeStr: start_time_utc, timeZone: local_time_zone })}
 					{' '}
 					-
 					{' '}
-					{formatTimeToIST({ timeString: end_time_utc, localTime: local_time_zone })}
+					{getDateFromTime({ timeStr: end_time_utc, timeZone: local_time_zone })}
 				</span>
 			</div>
 		</div>
