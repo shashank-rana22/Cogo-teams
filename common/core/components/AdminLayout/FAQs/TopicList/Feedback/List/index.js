0ea3f9edf5f7441af63b5d6e8c1291df58f0b7a7
amandeep2508 <amandeep.chauhan@cogoport.com>
2023-09-21 10:48:11 +0530

    fix

--- common/core/components/AdminLayout/FAQs/TopicList/Feedback/List/index.js
+++ common/core/components/AdminLayout/FAQs/TopicList/Feedback/List/index.js
@@ -1,29 +1,25 @@
 import { Pagination } from '@cogoport/components';
-import { useState } from 'react';
 
 import Card from './Card';
 import styles from './styles.module.css';
 
-const DEFAULT_PAGE = 1;
-
-function List() {
-	const [page, setPage] = useState(DEFAULT_PAGE);
-
+function List({
+	feedbacks = [],
+	page = 1,
+	setPage = () => {},
+	total = '',
+}) {
 	return (
 		<div className={styles.list}>
 			<div className={styles.scroll_view}>
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
+				{(feedbacks || []).map((item) => <Card key={item?.ID} {...item} />)}
 			</div>
 
 			<div className={styles.pagination}>
 				<Pagination
 					type="table"
 					currentPage={page}
-					totalItems={1000}
+					totalItems={total}
 					pageSize={10}
 					onPageChange={(val) => setPage(val)}
 				/>
