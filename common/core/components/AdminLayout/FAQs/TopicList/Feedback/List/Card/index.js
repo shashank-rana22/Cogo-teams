58eb736548ab5125905ab3d44a6f0e49c645f217
amandeep2508 <amandeep.chauhan@cogoport.com>
2023-09-21 10:42:59 +0530

    fix

--- common/core/components/AdminLayout/FAQs/TopicList/Feedback/List/Card/index.js
+++ common/core/components/AdminLayout/FAQs/TopicList/Feedback/List/Card/index.js
@@ -1,41 +1,61 @@
 import { Button } from '@cogoport/components';
-// import GLOBAL_CONSTANTS from '@cogoport/globalization/constants/globals';
-// import formatDate from '@cogoport/globalization/utils/formatDate';
+import GLOBAL_CONSTANTS from '@cogoport/globalization/constants/globals';
+import formatDate from '@cogoport/globalization/utils/formatDate';
+import { startCase } from '@cogoport/utils';
 
 import styles from './styles.module.css';
 
-function Card() {
+function Card({
+	Category: category = '',
+	ID: id = '',
+	CreatedAt: createdAt = '',
+	Status: status = '',
+	Description: description = '',
+	Data: data = {},
+	Type: issueType = '',
+}) {
+	const { Attachment: attachment = [] } = data || {};
+	const fileUrl = attachment[GLOBAL_CONSTANTS.zeroth_index];
+
 	return (
 		<div className={styles.card}>
 			<div className={styles.header}>
 				<div className={styles.basic_info}>
-					<div className={styles.feedback_number}>#123</div>
-					<Button
-						size="md"
-						themeType="linkUi"
-						id={styles.attachment}
-						// onClick={() => window.open()}
-					>
-						Attachment
-					</Button>
+					<div className={styles.feedback_number}>
+						#
+						{id}
+					</div>
+					<div className={styles.file_status}>
+						{fileUrl && (
+							<Button
+								size="md"
+								themeType="linkUi"
+								id={styles.attachment}
+								onClick={() => window.open(fileUrl)}
+							>
+								Attachment
+							</Button>
+						)}
+						<div className={styles.status}>{startCase(status)}</div>
+					</div>
 				</div>
 
-				<div className={styles.category}>category</div>
-				<div className={styles.issue_type}>issue_type</div>
+				<div className={styles.category}>{category}</div>
+				<div className={styles.issue_type}>{issueType}</div>
 			</div>
 
 			<div className={styles.desc_info}>
 				<div className={styles.date}>
-					{/* {formatDate({
+					{formatDate({
 						date       : createdAt,
 						dateFormat : GLOBAL_CONSTANTS.formats.date['dd/mm/yyyy'],
 						separator  : ', ',
 						timeFormat : GLOBAL_CONSTANTS.formats.time['HH:mm'],
 						formatType : 'dateTime',
-					})} */}
-					date
+					})}
+
 				</div>
-				<div className={styles.desc}>desc</div>
+				<div className={styles.description}>{description}</div>
 			</div>
 		</div>
 	);
