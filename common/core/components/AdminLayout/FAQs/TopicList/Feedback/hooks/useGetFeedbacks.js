2bc09acfde6fe83ee3384789869fc2129009d271
amandeep2508 <amandeep.chauhan@cogoport.com>
2023-09-21 10:46:51 +0530

    fix

---
+++ common/core/components/AdminLayout/FAQs/TopicList/Feedback/hooks/useGetFeedbacks.js
@@ -0,0 +1,56 @@
+import { useTicketsRequest } from '@cogoport/request';
+import { useSelector } from '@cogoport/store';
+import {
+	useEffect,
+	useCallback,
+} from 'react';
+
+const PAGE_DECREMENT = 1;
+
+const getPayload = ({
+	performerId,
+	page,
+}) => ({
+	PerformedByID : performerId,
+	size          : 10,
+	page          : page - PAGE_DECREMENT,
+	RequestType   : 'feedback',
+	Statuses      : '' || undefined,
+});
+
+const useGetFeedbacks = ({ page }) => {
+	const { id : performerId = '' } = useSelector((state) => state?.profile?.user);
+
+	const [{ data, loading }, trigger] = useTicketsRequest({
+		url     : '/list',
+		method  : 'get',
+		authkey : 'get_tickets_list',
+	}, { manual: true });
+
+	const getFeedbacks = useCallback(() => {
+		try {
+			trigger({
+				params: getPayload({
+					performerId,
+					page,
+				}),
+			});
+		} catch (error) {
+			console.error('error:', error);
+		}
+	}, [performerId, trigger, page]);
+
+	const { items, ...rest } = data || {};
+
+	useEffect(() => {
+		getFeedbacks();
+	}, [getFeedbacks]);
+
+	return {
+		loading,
+		feedbacks : items || [],
+		pageData  : rest,
+	};
+};
+
+export default useGetFeedbacks;
--- common/core/components/AdminLayout/FAQs/TopicList/Feedback/index.js
+++ common/core/components/AdminLayout/FAQs/TopicList/Feedback/index.js
@@ -3,11 +3,16 @@ import { IcMFeedback } from '@cogoport/icons-react';
 import { useState } from 'react';
 
 import FeedbackForm from './FeedbackForm';
+import useGetFeedbacks from './hooks/useGetFeedbacks';
 import List from './List';
 import styles from './styles.module.css';
 
 function Feedback() {
 	const [showAddFeedback, setShowAddFeedback] = useState(false);
+	const DEFAULT_PAGE = 1;
+	const [page, setPage] = useState(DEFAULT_PAGE);
+
+	const { feedbacks, pageData } = 	useGetFeedbacks({ page });
 
 	return (
 		<div className={styles.feedback_section}>
@@ -35,7 +40,12 @@ function Feedback() {
 						</div>
 					</div>
 				)}
-			<List />
+			<List
+				feedbacks={feedbacks}
+				setPage={setPage}
+				page={page}
+				{...pageData}
+			/>
 		</div>
 	);
 }
