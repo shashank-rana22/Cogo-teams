921e0c33acdc002685285aeb7cebfdf32de2d7d0
Mohit Nagar <mohit.nagar@cogoport.com>
2023-09-22 23:18:24 +0530

    dispatch to store

--- packages/performance-and-incentives/page-components/Leaderboard/hooks/useGetLeaderboardView.js
+++ packages/performance-and-incentives/page-components/Leaderboard/hooks/useGetLeaderboardView.js
@@ -1,16 +1,33 @@
 import { useAllocationRequest } from '@cogoport/request';
+import { useDispatch } from '@cogoport/store';
+import { setProfileState } from '@cogoport/store/reducers/profile';
+import { useEffect } from 'react';
 
 function useGetLeaderboardView() {
-	const [{ data, loading }] = useAllocationRequest({
+	const dispatch = useDispatch();
+
+	const [{ data, loading }, trigger] = useAllocationRequest({
 		url     : '/view',
 		method  : 'GET',
 		authkey : 'get_agent_scoring_view',
-	}, { manual: false });
+	}, { manual: true });
+
+	const getLeaderboardView = async () => {
+		try {
+			const res = await trigger();
+
+			dispatch(setProfileState({ incentive_leaderboard_viewtype: res?.data }));
+		} catch (err) {
+			console.log(err);
+		}
+	};
+
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	useEffect(() => { getLeaderboardView(); }, []);
 
 	return {
 		loading,
 		viewData: data,
-
 	};
 }
 
