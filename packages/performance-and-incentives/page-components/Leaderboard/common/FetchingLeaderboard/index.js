eec9f807da222bc5219ecae0fdd63a1581a434e7
Mohit Nagar <mohit.nagar@cogoport.com>
2023-09-22 20:56:16 +0530

    fetch view

---
+++ packages/performance-and-incentives/page-components/Leaderboard/common/FetchingLeaderboard/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function FetchingLeaderboard() {
+	return (
+		<div>FetchingLeaderboard</div>
+	);
+}
+
+export default FetchingLeaderboard;
---
+++ packages/performance-and-incentives/page-components/Leaderboard/hooks/useGetLeaderboardView.js
@@ -0,0 +1,17 @@
+import { useAllocationRequest } from '@cogoport/request';
+
+function useGetLeaderboardView() {
+	const [{ data, loading }] = useAllocationRequest({
+		url     : '/view',
+		method  : 'GET',
+		authkey : 'get_agent_scoring_view',
+	}, { manual: false });
+
+	return {
+		loading,
+		data,
+
+	};
+}
+
+export default useGetLeaderboardView;
