87a6f7ec353fcc41c279303a875730b65d6aec94
Nagendra Singh <nagendra.singh@cogoport.com>
2023-09-23 18:51:40 +0530

    functions formatting

--- packages/business-finance/page-components/COEFinance/All_Invoices/AdditionalRemarks/index.js
+++ packages/business-finance/page-components/COEFinance/All_Invoices/AdditionalRemarks/index.js
@@ -1,4 +1,3 @@
-/* eslint-disable max-lines-per-function */
 import { Checkbox, Radio, RadioGroup, Textarea } from '@cogoport/components';
 import { useState } from 'react';
 
@@ -9,18 +8,12 @@ import { formatReason } from './utilityFunctions';
 function AdditionalRemarks({ remarkData = {}, setRemarkData = () => {} }) {
 	const [active, setActive] = useState('');
 	const { notBilled = '', billed = '', draft = '' } = remarkData?.profitability || {};
-
 	const chosenProfitabilityReason = notBilled || billed || draft || remarkData?.profitability || '';
 	const chosenMismatchedReason = remarkData?.mismatched || '';
 	const chosenMiscReason = remarkData?.miscellaneous || '';
 
 	const handleCategoryChange = (fieldName) => {
-		setRemarkData((prev) => ({
-			...prev,
-			profitability: {
-				[fieldName]: true,
-			},
-		}));
+		setRemarkData((prev) => ({ ...prev, profitability: { [fieldName]: true } }));
 	};
 
 	return (
@@ -30,10 +23,7 @@ function AdditionalRemarks({ remarkData = {}, setRemarkData = () => {} }) {
 				label={`Profitability ${formatReason(chosenProfitabilityReason)}`}
 				checked={remarkData?.profitability}
 				onChange={() => {
-					setRemarkData((prev) => ({
-						...prev,
-						profitability: prev?.profitability ? undefined : {},
-					}));
+					setRemarkData((prev) => ({ ...prev, profitability: prev?.profitability ? undefined : {} }));
 					setActive('profitability');
 				}}
 			/>
@@ -48,23 +38,17 @@ function AdditionalRemarks({ remarkData = {}, setRemarkData = () => {} }) {
 					/>
 
 					<div className={styles.sub_category}>
-						{(remarkData?.profitability?.notBilled)
-							? (
-								<RadioGroup
-									options={notBilledOptions}
-									onChange={(val) => {
-										setRemarkData((prev) => ({
-											...prev,
-											profitability: {
-												notBilled: val,
-											},
-										}));
-										setActive('');
-									}}
-									value={typeof (remarkData?.profitability?.notBilled) === 'string'
-										? (remarkData?.profitability?.notBilled) : undefined}
-								/>
-							)
+						{(remarkData?.profitability?.notBilled) ? (
+							<RadioGroup
+								options={notBilledOptions}
+								onChange={(val) => {
+									setRemarkData((prev) => ({ ...prev, profitability: { notBilled: val } }));
+									setActive('');
+								}}
+								value={typeof (remarkData?.profitability?.notBilled) === 'string'
+									? (remarkData?.profitability?.notBilled) : undefined}
+							/>
+						)
 							: undefined}
 					</div>
 
@@ -76,23 +60,17 @@ function AdditionalRemarks({ remarkData = {}, setRemarkData = () => {} }) {
 					/>
 
 					<div className={styles.sub_category}>
-						{(remarkData?.profitability?.billed)
-							? (
-								<RadioGroup
-									options={billedOptions}
-									onChange={(val) => {
-										setRemarkData((prev) => ({
-											...prev,
-											profitability: {
-												billed: val,
-											},
-										}));
-										setActive('');
-									}}
-									value={typeof (remarkData?.profitability?.billed) === 'string'
-										? (remarkData?.profitability?.billed) : undefined}
-								/>
-							)
+						{(remarkData?.profitability?.billed) ? (
+							<RadioGroup
+								options={billedOptions}
+								onChange={(val) => {
+									setRemarkData((prev) => ({ ...prev, profitability: { billed: val } }));
+									setActive('');
+								}}
+								value={typeof (remarkData?.profitability?.billed) === 'string'
+									? (remarkData?.profitability?.billed) : undefined}
+							/>
+						)
 							: undefined}
 					</div>
 
@@ -104,23 +82,17 @@ function AdditionalRemarks({ remarkData = {}, setRemarkData = () => {} }) {
 					/>
 
 					<div className={styles.sub_category}>
-						{(remarkData?.profitability?.draft)
-							? (
-								<RadioGroup
-									options={draftOptions}
-									onChange={(val) => {
-										setRemarkData((prev) => ({
-											...prev,
-											profitability: {
-												draft: val,
-											},
-										}));
-										setActive('');
-									}}
-									value={typeof (remarkData?.profitability?.draft) === 'string'
-										? (remarkData?.profitability?.draft) : undefined}
-								/>
-							)
+						{(remarkData?.profitability?.draft) ?	(
+							<RadioGroup
+								options={draftOptions}
+								onChange={(val) => {
+									setRemarkData((prev) => ({ ...prev, profitability: {	draft: val }	}));
+									setActive('');
+								}}
+								value={typeof (remarkData?.profitability?.draft) === 'string'
+									? (remarkData?.profitability?.draft) : undefined}
+							/>
+						)
 							: undefined}
 					</div>
 
@@ -155,10 +127,7 @@ function AdditionalRemarks({ remarkData = {}, setRemarkData = () => {} }) {
 						label="Over due payments for customer"
 						checked={remarkData?.profitability === 'Over due payments for customer'}
 						onChange={() => {
-							setRemarkData((prev) => ({
-								...prev,
-								profitability: 'Over due payments for customer',
-							}));
+							setRemarkData((prev) => ({ ...prev, profitability: 'Over due payments for customer' }));
 							setActive('');
 						}}
 					/>
@@ -170,30 +139,23 @@ function AdditionalRemarks({ remarkData = {}, setRemarkData = () => {} }) {
 				label={`Document number mismatched ${formatReason(chosenMismatchedReason)}`}
 				checked={remarkData?.mismatched}
 				onChange={() => {
-					setRemarkData((prev) => ({
-						...prev,
-						mismatched: prev?.mismatched ? undefined : true,
-					}));
+					setRemarkData((prev) => ({ ...prev, mismatched: prev?.mismatched ? undefined : true }));
 					setActive('mismatched');
 				}}
 			/>
 
 			<div className={styles.category}>
-				{(remarkData?.mismatched && active === 'mismatched')
-					? (
-						<RadioGroup
-							options={mismatchedOptions}
-							onChange={(val) => {
-								setRemarkData((prev) => ({
-									...prev,
-									mismatched: val,
-								}));
-								setActive('');
-							}}
-							value={typeof (remarkData?.mismatched) === 'string'
-								? (remarkData?.mismatched) : undefined}
-						/>
-					)
+				{(remarkData?.mismatched && active === 'mismatched') ? 	(
+					<RadioGroup
+						options={mismatchedOptions}
+						onChange={(val) => {
+							setRemarkData((prev) => ({ ...prev, mismatched: val }));
+							setActive('');
+						}}
+						value={typeof (remarkData?.mismatched) === 'string'
+							? (remarkData?.mismatched) : undefined}
+					/>
+				)
 					: undefined}
 			</div>
 
@@ -201,30 +163,26 @@ function AdditionalRemarks({ remarkData = {}, setRemarkData = () => {} }) {
 				label={`Miscellaneous ${formatReason(chosenMiscReason)}`}
 				checked={remarkData?.miscellaneous}
 				onChange={() => {
-					setRemarkData((prev) => ({
-						...prev,
-						miscellaneous: prev?.miscellaneous ? undefined : true,
-					}));
+					setRemarkData((prev) => ({ ...prev, miscellaneous: prev?.miscellaneous ? undefined : true }));
 					setActive('misc');
 				}}
 			/>
 
 			<div className={styles.category}>
-				{(remarkData?.miscellaneous && active === 'misc')
-					? (
-						<RadioGroup
-							options={miscellaneousOptions}
-							onChange={(val) => {
-								setRemarkData((prev) => ({
-									...prev,
-									miscellaneous: val,
-								}));
-								setActive('');
-							}}
-							value={typeof (remarkData?.miscellaneous) === 'string'
-								? (remarkData?.miscellaneous) : undefined}
-						/>
-					)
+				{(remarkData?.miscellaneous && active === 'misc') ? 	(
+					<RadioGroup
+						options={miscellaneousOptions}
+						onChange={(val) => {
+							setRemarkData((prev) => ({
+								...prev,
+								miscellaneous: val,
+							}));
+							setActive('');
+						}}
+						value={typeof (remarkData?.miscellaneous) === 'string'
+							? (remarkData?.miscellaneous) : undefined}
+					/>
+				)
 					: undefined}
 			</div>
 
@@ -232,22 +190,15 @@ function AdditionalRemarks({ remarkData = {}, setRemarkData = () => {} }) {
 				label="Other"
 				checked={remarkData?.other}
 				onChange={() => {
-					setRemarkData((prev) => ({
-						...prev,
-						other: prev?.other ? undefined : true,
-					}));
+					setRemarkData((prev) => ({ ...prev, other: prev?.other ? undefined : true }));
 					setActive('');
 				}}
 			/>
 
 			{(remarkData?.other || typeof remarkData?.other === 'string') ? (
 				<Textarea
-					value={typeof (remarkData?.other) === 'string'
-						? (remarkData?.other) : undefined}
-					onChange={(e) => setRemarkData((prev) => ({
-						...prev,
-						other: e,
-					}))}
+					value={typeof (remarkData?.other) === 'string' ? (remarkData?.other) : undefined}
+					onChange={(e) => setRemarkData((prev) => ({ ...prev, other: e }))}
 					style={{ height: '100px', margin: '12px 0' }}
 				/>
 			) : undefined}
