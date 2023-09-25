22725e244aa3c75140648bcfb3db23b0d9602265
samir-cogoport <samir.dupare@cogoport.com>
2023-09-25 20:05:28 +0530

    point changes

--- packages/chakravyuh/page-components/AccuracyDashboard/MapView/Map/AnimatedPoint/index.js
+++ packages/chakravyuh/page-components/AccuracyDashboard/MapView/Map/AnimatedPoint/index.js
@@ -6,14 +6,18 @@ import styles from './styles.module.css';
 
 const ANCHOR = -15;
 const DEFAULT_SIZE = 12;
-const getIcon = ({ className, size }) => L.divIcon({
-	className,
+const getIcon = ({ className, backgroundColor, size }) => L.divIcon({
+	className   : styles.remove_bg,
 	iconSize    : L.point(...size),
 	popupAnchor : [GLOBAL_CONSTANTS.zeroth_index, ANCHOR],
+	html        : `<div class="${className}" 
+					style="background: ${backgroundColor}"
+					/>`,
+
 });
 
 const Point = React.forwardRef(({
-	position, animate = false, className = '',
+	position, animate = false, className = '', backgroundColor,
 	size = [DEFAULT_SIZE, DEFAULT_SIZE], children, ...rest
 }, ref) => (
 	<Marker
@@ -22,6 +26,7 @@ const Point = React.forwardRef(({
 			className: `${styles.point_animation}
 			${!animate ? styles.hide_animation : ''} ${className}`,
 			size,
+			backgroundColor,
 		})}
 		{...rest}
 		ref={ref}
