80fcb0c92a1581569a376f628d5cf60957707a6d
Mohit Nagar <mohit.nagar@cogoport.com>
2023-09-25 14:13:00 +0530

    changes

--- packages/performance-and-incentives/page-components/Leaderboard/components/Body/useScoringReports.js
+++ packages/performance-and-incentives/page-components/Leaderboard/components/Body/useScoringReports.js
@@ -2,20 +2,24 @@ import { useDebounceQuery } from '@cogoport/forms';
 import { useSelector } from '@cogoport/store';
 import { useState, useEffect } from 'react';
 
+import LEADERBOARD_VIEWTYPE_CONSTANTS from '../../../../constants/leaderboard-viewtype-constants';
+
+const { ADMIN } = LEADERBOARD_VIEWTYPE_CONSTANTS;
+
 const useScoringReports = (props) => {
 	const { dateRange, entity } = props;
 
-	const { incentive_leaderboard_viewtype: viewType, user } = useSelector(({ profile }) => profile);
+	const { incentive_leaderboard_viewtype, user } = useSelector(({ profile }) => profile);
 
 	const { debounceQuery, query: searchQuery } = useDebounceQuery();
 
-	const [view] = viewType.split('_');
+	const [view] = incentive_leaderboard_viewtype.split('_');
 
 	const [isChannel, setIsChannel] = useState(false);
 
 	let report_view_type;
 
-	if (view === 'admin') {
+	if (incentive_leaderboard_viewtype === ADMIN) {
 		report_view_type = isChannel ? 'channel_wise' : 'location_wise';
 	} else {
 		report_view_type = `${view}_wise`;
@@ -24,15 +28,16 @@ const useScoringReports = (props) => {
 	const [params, setParams] = useState({
 		user_data_required      : true,
 		role_data_required      : true,
-		add_current_user_report : view !== 'admin',
+		add_current_user_report : incentive_leaderboard_viewtype !== ADMIN,
 		filters                 : {
 			report_view_type,
-			...(view !== 'admin' ? { report_type: `${view}_report` } : {}),
+			...(incentive_leaderboard_viewtype !== ADMIN ? { report_type: `${view}_report` } : {}),
 			q                       : searchQuery || undefined,
 			created_at_greater_than : dateRange?.startDate || undefined,
 			created_at_less_than    : dateRange?.endDate || undefined,
 			partner_id              : entity || undefined,
-			current_user_id         : view !== 'admin' ? user.id : undefined,
+			current_user_id         : incentive_leaderboard_viewtype !== ADMIN ? user.id : undefined,
+			user_id                 : incentive_leaderboard_viewtype !== ADMIN ? user.id : undefined,
 		},
 	});
 
@@ -54,11 +59,12 @@ const useScoringReports = (props) => {
 			...prev,
 			filters: {
 				...prev.filters,
-				...(view === 'admin' ? { report_view_type: isChannel ? 'channel_wise' : 'location_wise' } : {}),
+				...(incentive_leaderboard_viewtype === ADMIN
+					? { report_view_type: isChannel ? 'channel_wise' : 'location_wise' } : {}),
 
 			},
 		}));
-	}, [isChannel, view]);
+	}, [isChannel, incentive_leaderboard_viewtype]);
 
 	return {
 		params,
