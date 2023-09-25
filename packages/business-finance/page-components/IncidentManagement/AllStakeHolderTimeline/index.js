c8101823a2751c4118f1372abc563b467964ab18
Swastik-Cogoport <swastik.sharma@cogoport.com>
2023-09-25 11:46:17 +0530

    icon size fix

--- packages/business-finance/page-components/IncidentManagement/AllStakeHolderTimeline/index.js
+++ packages/business-finance/page-components/IncidentManagement/AllStakeHolderTimeline/index.js
@@ -100,15 +100,15 @@ function AllStakeHolderTimeline({ timeline = [] }) {
 				})}
 				{showTimeline ? (
 					<IcMArrowRotateUp
-						width={18}
-						height={18}
+						width={15}
+						height={15}
 						onClick={() => setShowTimeline(!showTimeline)}
 						className={styles.arrow}
 					/>
 				) : (
 					<IcMArrowRotateDown
-						width={18}
-						height={18}
+						width={15}
+						height={15}
 						onClick={() => setShowTimeline(!showTimeline)}
 						className={styles.arrow}
 					/>
