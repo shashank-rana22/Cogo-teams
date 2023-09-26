55780d06033073ca04b502c6805252e776cd06d0
Mohit Nagar <mohit.nagar@cogoport.com>
2023-09-26 10:57:20 +0530

    changes

--- packages/performance-and-incentives/utils/get-entity-name-by-id.js
+++ packages/performance-and-incentives/utils/get-entity-name-by-id.js
@@ -3,10 +3,10 @@ import GLOBAL_CONSTANTS from '@cogoport/globalization/constants/globals';
 const getEntityNameById = (entity_id) => {
 	let cogoEntity = '';
 	Object.values(GLOBAL_CONSTANTS.cogoport_entities).forEach((entity) => {
-		const { id, name } = entity;
+		const { id, display_name } = entity;
 
 		if (id === entity_id) {
-			cogoEntity = name;
+			cogoEntity = display_name;
 		}
 	});
 
