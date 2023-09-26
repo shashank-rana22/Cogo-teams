452bcc815dc9d17134cee656ae49a959c5c8bfd6
Lachiramnaik1330 <malothu.lachiramnaik@cogoport.com>
2023-09-26 15:24:53 +0530

    added rm details

--- packages/cogo-one/constants/viewTypeMapping/viewTypes/shipmentSpecialist.js
+++ packages/cogo-one/constants/viewTypeMapping/viewTypes/shipmentSpecialist.js
@@ -20,7 +20,7 @@ const SHIPMENT_SPECIALIST = {
 	bulk_assign_features         : [],
 	stats_feedback_count         : [],
 	to_show_agent_activity_graph : false,
-	configurations_to_be_shown   : [],
+	configurations_to_be_shown   : ['agents_status'],
 	shift_view_default_type      : '',
 	group_agents_api_filter      : ['shipment_specialist',
 		'support', 'shipment_specialist_admin', ...COGOVERSE_AGENT_MAPPINGS.sales],
@@ -48,6 +48,7 @@ const SHIPMENT_SPECIALIST = {
 		show_lead_voice_calls                       : false,
 		show_shipments_stakeholders_contact_details : true,
 		hide_personal_mail                          : true,
+		show_rm_agent_details                       : true,
 	},
 };
 
