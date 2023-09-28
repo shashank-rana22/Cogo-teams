e36b9c44d4cf0cde82296fa3037606c19782186a
tejoyasaswi-cogoport <Tejo.Uppuluri@cogoport.com>
2023-09-28 18:39:08 +0530

    changes

--- packages/business-finance/page-components/COEFinance/Rejected/CommonListData/RenderTicket/index.js
+++ packages/business-finance/page-components/COEFinance/Rejected/CommonListData/RenderTicket/index.js
@@ -1,9 +1,37 @@
-import GLOBAL_CONSTANTS from '@cogoport/globalization/constants/globals';
+import { IcMArrowNext } from '@cogoport/icons-react';
+import { useRouter } from '@cogoport/next';
+import { isEmpty } from '@cogoport/utils';
+
+import styles from './styles.module.css';
+
+const START_INDEX = 0;
+const MAX_LENGTH = 5;
 
 function RenderTicket({ itemData = {} }) {
+	const ticketIds = itemData?.ticketIds;
+	const router = useRouter();
+
+	const routeHandler = () => {
+		router.push('/ticket-management/my-tickets');
+	};
+
 	return (
-		<div>
-			{itemData?.ticketIds?.[GLOBAL_CONSTANTS.zeroth_index] || '-'}
+		<div className={styles.ticket_ids}>
+			{ticketIds?.slice(START_INDEX, MAX_LENGTH)?.map((ticketId, index) => (
+				<div key={ticketId}>
+					<div>
+						{index > START_INDEX && ', '}
+						{ticketId }
+					</div>
+				</div>
+			))}
+			{!isEmpty(ticketIds)
+			&& (
+				<IcMArrowNext
+					onClick={routeHandler}
+					className={styles.redirect_button}
+				/>
+			)}
 		</div>
 	);
 }
