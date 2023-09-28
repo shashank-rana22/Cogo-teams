43c6c90f48895caf7a47f69f7093b9a3cd10d89b
AmaanAhmadCogo <amaan.ahmad@cogoport.com>
2023-09-28 13:00:09 +0530

    added scroll buttons for tabs in freight

--- packages/kam-desk/common/DeskTabs/index.js
+++ packages/kam-desk/common/DeskTabs/index.js
@@ -1,5 +1,6 @@
 import { Tabs, TabPanel } from '@cogoport/components';
-import { useContext } from 'react';
+import { IcMArrowRotateLeft, IcMArrowRotateRight } from '@cogoport/icons-react';
+import { useContext, useRef } from 'react';
 
 import shipmentTabMapping from '../../config/SHIPMENT_TAB_MAPPING';
 import { CRITICAL_TABS } from '../../config/SHIPMENTS_PAYLOAD';
@@ -7,7 +8,13 @@ import KamDeskContext from '../../context/KamDeskContext';
 
 import styles from './styles.module.css';
 
+const SCROLL_DIVISOR = 2;
+
 function DeskTabs() {
+	const scrollRef = useRef('');
+	const tabs_container = scrollRef.current;
+	const scrollWidth = window.innerWidth / SCROLL_DIVISOR;
+
 	const { stepperTab, shipmentType, activeTab, setActiveTab, setFilters, filters } = useContext(KamDeskContext);
 
 	const tabs = shipmentTabMapping[shipmentType]?.tabs || shipmentTabMapping[shipmentType]?.[stepperTab]?.tabs || [];
@@ -26,17 +33,43 @@ function DeskTabs() {
 		}
 	};
 
+	const slide = (shift) => {
+		tabs_container.scrollLeft += shift;
+	};
+
 	return (
-		<div className={styles.tabs_container}>
-			<Tabs themeType="secondary" activeTab={activeTab} onChange={handleChange}>
-				{tabs?.map((tab) => (
-					<TabPanel
-						title={tab.label}
-						key={tab.value}
-						name={tab.value}
-					/>
-				))}
-			</Tabs>
+		<div className={styles.container}>
+
+			<button
+				onClick={() => slide(-scrollWidth)}
+				className={styles.btn_left}
+			>
+				<IcMArrowRotateLeft width={16} height={16} />
+			</button>
+
+			<div className={styles.tabs_container} ref={scrollRef}>
+				<Tabs
+					themeType="secondary"
+					activeTab={activeTab}
+					onChange={handleChange}
+					className={styles.tabs}
+				>
+					{tabs?.map((tab) => (
+						<TabPanel
+							title={tab.label}
+							key={tab.value}
+							name={tab.value}
+						/>
+					))}
+				</Tabs>
+			</div>
+
+			<button
+				onClick={() => slide(+scrollWidth)}
+				className={styles.btn_right}
+			>
+				<IcMArrowRotateRight width={16} height={16} />
+			</button>
 		</div>
 	);
 }
