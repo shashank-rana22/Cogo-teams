fc9e6f448ea0a65e38da0c34d1ffdf3cf3acf141
anmolb-cogo <anmol.bansal@cogoport.com>
2023-09-29 18:24:20 +0530

    fix

--- common/ocean-modules/components/Poc/components/Pocs/Internal/Stakeholders/index.js
+++ common/ocean-modules/components/Poc/components/Pocs/Internal/Stakeholders/index.js
@@ -43,9 +43,9 @@ function Stakeholders({
 					id,
 				} = item || {};
 
-				if (rolesPermission?.hidden_poc?.includes(stakeholder_type)) return null;
-
-				if (!mapping[stakeholder_type]) return null;
+				if (rolesPermission?.hidden_poc?.includes(stakeholder_type) || !mapping[stakeholder_type]) {
+					return null;
+				}
 
 				const contact_number = `${mobile_country_code} ${mobile_number}`;
 
