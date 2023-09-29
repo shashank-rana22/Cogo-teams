d397610feac147d891b3e539a77bf2cbf3a7be6b
Manan Singhal <singhal.manan24@gmail.com>
2023-09-29 17:28:30 +0530

    Fixed API issue

--- packages/promotions/page-components/BudgetAllocation/RuleSettingTab/CustomConfig/CustomConfigForm/helpers/getOrganizationUpdateAgentRuleData.js
+++ packages/promotions/page-components/BudgetAllocation/RuleSettingTab/CustomConfig/CustomConfigForm/helpers/getOrganizationUpdateAgentRuleData.js
@@ -12,8 +12,10 @@ const getOrganizationUpdateAgentRuleData = (values) => {
 			discount_limit_currency,
 			discount_limit_unit,
 			discount_limit_value,
+			status: 'active',
 		},
 		id,
+		status: 'active',
 	};
 };
 
