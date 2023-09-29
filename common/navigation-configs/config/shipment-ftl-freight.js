854dd5a2b1728bdd4fbf05172b6ab5321e212c60
Aliarshadcogo <arshad.ali@cogoport.com>
2023-09-29 20:10:37 +0530

    fix

--- common/navigation-configs/config/shipment-ftl-freight.js
+++ common/navigation-configs/config/shipment-ftl-freight.js
@@ -1,11 +1,7 @@
 const shipmentFtlFreight = {
 	'/[partner_id]/booking/ftl/[shipment_id]': {
 		navigation          : 'coe-shipments',
-		alternateNavigation : ['coe-booking_note_desk',
-			'coe-last_mile',
-			'coe-kam_desk',
-			'coe-shipment_surface',
-			'coe-so2_surface'],
+		alternateNavigation : ['coe-booking_note_desk', 'coe-last_mile', 'coe-shipment_surface'],
 	},
 };
 
