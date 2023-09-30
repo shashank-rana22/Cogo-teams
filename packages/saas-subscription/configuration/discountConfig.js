30647cda5902115a7b4f46ce048d3eced92b52dd
ShubhamRam-cogoport <shubham.ram@cogoport.com>
2023-09-30 13:22:19 +0530

    transpile

--- packages/saas-subscription/configuration/discountConfig.js
+++ packages/saas-subscription/configuration/discountConfig.js
@@ -3,13 +3,13 @@ const getDiscountConfig = ({ t }) => [
 		key        : 'service_name',
 		label      : t('saasSubscription:discount_config_name'),
 		renderFunc : 'renderName',
-		width      : '22%',
+		width      : '20%',
 	},
 	{
 		key        : 'config_type',
 		label      : t('saasSubscription:discount_config_type'),
 		renderFunc : 'renderName',
-		width      : '22%',
+		width      : '18%',
 
 	},
 	{
@@ -24,6 +24,11 @@ const getDiscountConfig = ({ t }) => [
 	{
 		key   : 'usage_count',
 		label : t('saasSubscription:discount_config_count'),
+	},
+	{
+		key        : 'is_active',
+		label      : t('saasSubscription:discount_config_status'),
+		renderFunc : 'renderStatus',
 	},
 	{
 		key        : 'editor',
