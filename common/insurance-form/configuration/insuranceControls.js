5fa49b29656c5111c50c2a3721bfbf312a2728ae
ShubhamRam-cogoport <shubham.ram@cogoport.com>
2023-10-03 14:26:31 +0530

    added insurance form in common

---
+++ common/insurance-form/configuration/insuranceControls.js
@@ -0,0 +1,94 @@
+import currencyOptions from '@cogoport/forms/page-components/Business/PriceSelect/currencies';
+import { IcMLocation, IcMProductCodeMapping } from '@cogoport/icons-react';
+
+import ListLocationOpt from '../page-components/ListLocationOpt';
+
+const FILTER_TYPE_MAPPGING = {
+	ocean   : ['seaport', 'country'],
+	air     : ['airport', 'city', 'country'],
+	surface : ['pincode', 'seaport', 'airport', 'city'],
+};
+
+const getInsuranceControls = ({ activeTab = 'ocean' }) => [
+	{
+		name        : 'origin_point',
+		label       : 'Origin Point',
+		placeholder : 'Select Origin Point',
+		prefix      : <IcMLocation width={18} height={18} />,
+		type        : 'asyncSelect',
+		initialCall : true,
+		asyncKey    : 'list_locations',
+		params      : {
+			filters  : { status: 'active', type: FILTER_TYPE_MAPPGING[activeTab] },
+			includes : {
+				city                    : true,
+				country                 : true,
+				default_params_required : true,
+			},
+		},
+		rules       : { required: true },
+		renderLabel : (option) => <ListLocationOpt data={option} />,
+	},
+	{
+		name        : 'destination_point',
+		label       : 'Destination Point',
+		placeholder : 'Select Destination Point',
+		prefix      : <IcMLocation width={18} height={18} />,
+		type        : 'asyncSelect',
+		asyncKey    : 'list_locations',
+		params      : {
+			filters  : { status: 'active', type: FILTER_TYPE_MAPPGING[activeTab] },
+			includes : {
+				city                    : true,
+				country                 : true,
+				default_params_required : true,
+			},
+
+		},
+		initialCall : true,
+		rules       : { required: true },
+		renderLabel : (option) => <ListLocationOpt data={option} />,
+	},
+	{
+		name               : 'hsCode',
+		label              : 'HS Code',
+		placeholder        : 'Enter HS Code',
+		prefix             : <IcMProductCodeMapping width={14} height={14} />,
+		type               : 'asyncSelect',
+		asyncKey           : 'list_saas_hs_codes',
+		initialCall        : true,
+		rules              : { required: true },
+		params             : { codeType: 'FINAL' },
+		labelKey           : 'display_name',
+		valueKey           : 'hsCode',
+		getModifiedOptions : ({ options }) => options.map((opt) => ({
+			...opt,
+			display_name: (
+				<div>
+					{opt?.hsCode}
+					{' '}
+					-
+					{' '}
+					{opt?.description}
+
+				</div>),
+		})),
+	},
+	{
+		name        : 'currency',
+		label       : 'Invoice Currency',
+		placeholder : 'Select Invoice Currency',
+		type        : 'select',
+		options     : currencyOptions,
+		rules       : { required: true },
+	},
+	{
+		name        : 'value',
+		label       : 'Invoice Value',
+		placeholder : 'Enter Invoice Value',
+		type        : 'number',
+		rules       : { required: true },
+	},
+];
+
+export default getInsuranceControls;
---
+++ common/insurance-form/page-components/Form/index.js
@@ -0,0 +1,61 @@
+import { Button, Tabs, TabPanel } from '@cogoport/components';
+import { IcMFsea, IcMFairport, IcMFland } from '@cogoport/icons-react';
+import { useState } from 'react';
+
+import getInsuranceControls from '../../configuration/insuranceControls';
+import { getFieldController } from '../../helpers/getFieldController';
+import useInsurance from '../../hooks/useInsurance';
+
+import styles from './styles.module.css';
+
+function Insurance({ organization = {} }) {
+	const [activeTab, setActiveTab] = useState('ocean');
+
+	const { formHook, onSubmit, formValueRef } = useInsurance({ activeTab, organization });
+	const insuranceControls = getInsuranceControls({ activeTab });
+
+	const { control, handleSubmit, formState:{ errors } } = formHook;
+
+	return (
+		<div className={styles.container}>
+			<div className={styles.header}>
+				<h3>Select Service</h3>
+
+				<Tabs
+					activeTab={activeTab}
+					themeType="tertiary"
+					onChange={setActiveTab}
+				>
+					<TabPanel name="ocean" title="Ocean" icon={<IcMFsea />} />
+					<TabPanel name="air" title="Air" icon={<IcMFairport />} />
+					<TabPanel name="surface" title="Surface" icon={<IcMFland />} />
+				</Tabs>
+			</div>
+
+			<div className={styles.form_container}>
+				{insuranceControls.map((config) => {
+					const { name, type, label } = config || {};
+					const Element = getFieldController(type);
+
+					return (
+						<div key={name} className={styles.col}>
+							<p>{label}</p>
+							<Element
+								{...config}
+								control={control}
+								onChange={(e, data) => { formValueRef.current[name] = data || e; }}
+							/>
+							<p className={styles.error}>{errors?.[name]?.message || errors?.[name]?.type}</p>
+						</div>
+					);
+				})}
+			</div>
+
+			<div className={styles.footer}>
+				<Button size="lg" themeType="accent" onClick={handleSubmit(onSubmit)}>Search Rates</Button>
+			</div>
+		</div>
+	);
+}
+
+export default Insurance;
