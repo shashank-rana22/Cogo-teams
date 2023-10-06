7ccaa7aa20d082152e4e1ae62f71a391d6f8443f
Vishal <Vishal.Nagar@cogoport.com>
2023-10-06 19:08:40 +0530

    added

--- packages/business-finance/page-components/COEFinance/hook/useGetListStats.js
+++ packages/business-finance/page-components/COEFinance/hook/useGetListStats.js
@@ -1,7 +1,22 @@
 import { useRequestBf } from '@cogoport/request';
+// import { useSelector } from '@cogoport/store';
+// import { format } from '@cogoport/utils';
 import { useEffect } from 'react';
 
+// const BILL_TYPE = ['PURCHASE', 'PROFORMA', 'CONSOLIDATED'];
+
+// const formatDate = (date) => (
+// 	format(filters?.billDate?.startDate, "yyyy-MM-dd'T'HH:mm:sso", {}, false)
+// );
 const useGetListStats = () => {
+	// const profile = useSelector((state) => state);
+
+	// const entityCode = getEntityCode(profile?.profile?.partner?.id);
+
+	// const showbillType = filters?.billType === 'PURCHASE' ? 'false' : undefined;
+	// const showProforma = filters?.billType === 'PROFORMA' ? true : undefined;
+	// const showConsolidated = filters?.billType === 'CONSOLIDATED' ? 'CONSOLIDATED' : undefined;
+
 	const [{ data, loading }, trigger] = useRequestBf(
 		{
 			url     : '/purchase/bills/list-stats',
