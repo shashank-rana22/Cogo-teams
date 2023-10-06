a698f3cb7e0001c035f0ba8279b7cb9322ede889
satendra23 <satendra.patel@cogoport.com>
2023-10-06 18:45:06 +0530

    remarks

--- packages/business-finance/page-components/AccountPayables/Invoices/Constants/index.js
+++ packages/business-finance/page-components/AccountPayables/Invoices/Constants/index.js
@@ -75,5 +75,5 @@ export const SERVICE_TYPE = [
 
 export const REMARKS = [
 	{ name: 'MARK DISPUTED', value: 'DISPUTE' },
-	{ name: 'PUSH TO COE FINANCE', value: 'FINANCE REJECT' },
+	{ name: 'FINANCE REJECT', value: 'PUSH TO COE FINANCE' },
 ];
