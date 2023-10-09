b923f4bd8becefe1f65a083dc5733d163f8572df
likhilesh-cogoport <likhilesh.balpande@cogoport.com>
2023-10-09 17:05:18 +0530

    fixes

--- packages/chakravyuh/utils/formatBigNumbers.js
+++ packages/chakravyuh/utils/formatBigNumbers.js
@@ -3,7 +3,7 @@ import GLOBAL_CONSTANTS from '@cogoport/globalization/constants/globals';
 import { SECOND_IDX } from '../constants/map_constants';
 
 const DECIMALS = 1;
-const MAX_SAFE_LIMIT = 10;
+const MAX_SAFE_LIMIT = 14;
 const UNIT_MAPPING = {
 	T : 1000000000000,
 	B : 1000000000,
@@ -12,7 +12,9 @@ const UNIT_MAPPING = {
 };
 
 export const formatBigNumbers = (cnt) => {
-	if (`${cnt}`.length > MAX_SAFE_LIMIT) return GLOBAL_CONSTANTS.zeroth_index;
+	if (`${Math.round(cnt)}`.length > MAX_SAFE_LIMIT) {
+		return GLOBAL_CONSTANTS.zeroth_index;
+	}
 	if (Number.isNaN(+cnt)) return GLOBAL_CONSTANTS.zeroth_index;
 	const unit = Object.entries(UNIT_MAPPING).find(([, value]) => cnt >= value);
 	return unit
