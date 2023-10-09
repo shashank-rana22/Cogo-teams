2a6ef95ff1e4356ba0cbecd09dc2baee5f16f991
Sandeep Nalabolu <sandeep.nalabolu@cogoport.com>
2023-10-09 18:55:44 +0530

    update enriched leads

--- packages/cogo-one/hooks/useGetOrgUsers.js
+++ packages/cogo-one/hooks/useGetOrgUsers.js
@@ -6,27 +6,31 @@ import { useEffect, useCallback, useState } from 'react';
 const PAGE_LIMIT = 100;
 
 const API_MAPPING = {
-	organizations         : 'list_organization_users',
-	lead_organizations    : 'list_lead_users',
-	channel_partners      : 'get_channel_partner_users',
-	lead_channel_partners : 'list_lead_users',
+	organizations          : 'list_organization_users',
+	other_organizations    : 'list_lead_users',
+	channel_partners       : 'get_channel_partner_users',
+	other_channel_partners : 'list_lead_users',
 };
 
-const getPayload = ({ orgId = '', userId = '', searchQuery = '', orgType = '' }) => ({
+const getPayload = ({ orgId = '', userIds = [], searchQuery = '', orgType = '' }) => ({
 	filters: {
 		status               : orgType === 'organizations' ? 'active' : undefined,
-		user_id              : userId || undefined,
+		id                   : isEmpty(userIds) ? undefined : userIds,
 		q                    : searchQuery || undefined,
 		organization_id      : orgType === 'organizations' ? orgId || undefined : undefined,
-		lead_organization_id : orgType.includes('lead') ? orgId || undefined : undefined,
 		is_importer_exporter : orgType === 'channel_partners' ? true : undefined,
+		...(orgType.includes('other') ? {
+			is_channel_partner : orgType !== 'other_organizations',
+			lifecycle_stage    : ['enriched', 'marketing_qualified'],
+		} : {}),
 	},
 	...(orgType === 'channel_partners' ? { account_types: ['importer_exporter'] } : {}),
+
 	partner_id : orgType === 'channel_partners' ? orgId || undefined : undefined,
 	page_limit : PAGE_LIMIT,
 });
 
-const useGetOrgUsers = ({ orgId = '', userId = '', orgType = '' }) => {
+const useGetOrgUsers = ({ orgId = '', userIds = null, orgType = '' }) => {
 	const [query, setQuery] = useState('');
 	const [initialLoad, setInitialLoad] = useState(true);
 
@@ -37,25 +41,28 @@ const useGetOrgUsers = ({ orgId = '', userId = '', orgType = '' }) => {
 		method : 'get',
 	}, { manual: true });
 
-	const fetchUser = useCallback(async () => {
+	const fetchUser = useCallback(async ({ selectedUserIds }) => {
 		try {
 			if (!API_MAPPING[orgType]) {
 				return;
 			}
 			await trigger({
-				params: getPayload({ orgId, userId, searchQuery, orgType }),
+				params: getPayload({ orgId, userIds: selectedUserIds, searchQuery, orgType }),
 			});
 			setInitialLoad(false);
 		} catch (error) {
 			console.error(error);
 		}
-	}, [orgId, orgType, searchQuery, trigger, userId]);
+	}, [orgId, orgType, searchQuery, trigger]);
 
-	useEffect(() => {
-		if (orgId) {
-			fetchUser();
-		}
-	}, [fetchUser, orgId]);
+	useEffect(
+		() => {
+			if (orgId) {
+				fetchUser({ selectedUserIds: initialLoad ? userIds : undefined });
+			}
+		},
+		[fetchUser, initialLoad, orgId, userIds],
+	);
 
 	useEffect(() => {
 		debounceQuery(query?.trim());
@@ -64,11 +71,12 @@ const useGetOrgUsers = ({ orgId = '', userId = '', orgType = '' }) => {
 	const isOrgUserIdPresent = !isEmpty(data?.list);
 
 	return {
-		orgLoading : loading,
+		orgLoading   : loading,
 		isOrgUserIdPresent,
-		orgData    : data,
-		setQuery,
+		orgData      : (loading || !API_MAPPING[orgType] || !orgId) ? {} : data,
+		handleSearch : setQuery,
 		initialLoad,
+		searchQuery  : query,
 	};
 };
 
