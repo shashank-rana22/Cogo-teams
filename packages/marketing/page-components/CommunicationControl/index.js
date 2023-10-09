c0c6b6c7bb1fe51d1fde7b76148783c9764fdf5e
Akshay Kumar Adhana <akshay@Akshays-MacBook-Air.local>
2023-10-09 10:33:00 +0530

    toggle btn added

--- packages/marketing/page-components/CommunicationControl/index.js
+++ packages/marketing/page-components/CommunicationControl/index.js
@@ -1,13 +1,27 @@
-import { TabPanel, Tabs } from '@cogoport/components';
+import { TabPanel, Tabs, Toggle } from '@cogoport/components';
+import { useHandleVersionChangeToOld } from '@cogoport/next';
 import { useState } from 'react';
 
 import TAB_KEYS from './constants/TAB_KEYS';
 
 function Dashboard() {
 	const [activeTab, setActiveTab] = useState('channel_control');
+	const { handleRouteChange } = useHandleVersionChangeToOld({});
 	return (
 		<div>
-			<h1>Communication Control</h1>
+			<div style={{
+				display        : 'flex',
+				justifyContent : 'space-between',
+			}}
+			>
+				<h1>Communication Control</h1>
+				<Toggle
+					size="md"
+					onLabel="Old"
+					offLabel="New"
+					onChange={handleRouteChange}
+				/>
+			</div>
 			<Tabs
 				activeTab={activeTab}
 				themeType="primary"
