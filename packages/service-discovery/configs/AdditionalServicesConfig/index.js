830c7d3808aa97735b0fb77b3a4f9a9e36f5984e
rupesh-mutyala <rupesh.mutyala@cogoport.com>
2023-10-12 12:49:33 +0530

    removed unwanted code

--- packages/service-discovery/configs/AdditionalServicesConfig/index.js
+++ packages/service-discovery/configs/AdditionalServicesConfig/index.js
@@ -4,16 +4,6 @@ import fclIncotermsMapping from './FCL/fclIncotermsMapping.json';
 import fclAdditionalServiceControls from './FCL/serviceControls';
 import ftlInCoTersmMapping from './FTL/ftlIncotermsMapping.json';
 
-export const defaultAddedServices = {
-	ltl_freight     : ['ltl_freight'],
-	ftl_freight     : ['ftl_freight'],
-	trailer_freight : ['trailer_freight'],
-	haulage_freight : ['haulage_freight'],
-	fcl_customs     : ['fcl_customs'],
-	lcl_customs     : ['lcl_customs'],
-	air_customs     : ['air_customs'],
-};
-
 export const serviceMappings = ({ service:primaryService, origin_country_id = '', destination_country_id = '' }) => {
 	const serviceMap = {
 		fcl_freight: {
