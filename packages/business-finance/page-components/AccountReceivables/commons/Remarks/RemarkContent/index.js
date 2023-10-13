09afd023584afc108d1d513e354f64489ea74bda
prashantp01 <prashant.paddune@cogoport.com>
2023-10-13 15:07:19 +0530

    typescript: conflicts
    Signed-off-by: prashantp01 <prashant.paddune@cogoport.com>

--- packages/business-finance/page-components/AccountReceivables/commons/Remarks/RemarkContent/index.js
+++ packages/business-finance/page-components/AccountReceivables/commons/Remarks/RemarkContent/index.js
@@ -11,19 +11,11 @@ function RemarkContent({ itemData }) {
 					<div className={styles.heading}>Remarks</div>
 					<div className={styles.hr} />
 					<div className={styles.remarks_container}>
-<<<<<<< HEAD:packages/business-finance/page-components/AccountReceivables/commons/Remarks/RemarkContent/index.js
-						{getByKey(itemData, 'translationRemark')}
+						{getByKey(itemData, 'remarks')}
 					</div>
 				</>
 			)}
-			{isEmpty(getByKey(itemData, 'translationRemark')) && (
-=======
-						{getByKey(itemData, 'remarks') as string}
-					</div>
-				</>
-			)}
-			{isEmpty(getByKey(itemData, 'remarks') as string) && (
->>>>>>> 232ef11ced9cecabbd871a5e0d28195108cc8ddc:packages/business-finance/page-components/AccountReceivables/commons/Remarks/RemarkContent/index.tsx
+			{isEmpty(getByKey(itemData, 'remarks')) && (
 				<div className={styles.empty_container}>NO REMARKS FOUND</div>
 			)}
 		</div>
