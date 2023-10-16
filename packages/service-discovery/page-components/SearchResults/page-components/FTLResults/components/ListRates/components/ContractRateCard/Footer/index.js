dd624833ac9bbaf457fafc067dd3cce1639cb2e8
Ritik Goyal <ritik.goyal@cogoport.com>
2023-10-16 19:37:10 +0530

    contract card fixes

--- packages/service-discovery/page-components/SearchResults/page-components/FTLResults/components/ListRates/components/ContractRateCard/Footer/index.js
+++ packages/service-discovery/page-components/SearchResults/page-components/FTLResults/components/ListRates/components/ContractRateCard/Footer/index.js
@@ -37,15 +37,9 @@ function Footer({
 
 	const contractData = Object.values(service_rates).find((service) => service.source === 'contract');
 
-	console.log('contractData', contractData);
-
 	const { truck_type = '', trucks_count = 0, rate_type = '' } = contractData;
 
 	const showCogoPoints = rate?.earnable_cogopoints;
-
-	if (isSelectedCard) {
-		return null;
-	}
 
 	return (
 		<div className={styles.container}>
@@ -68,62 +62,65 @@ function Footer({
 					<strong>{startCase(rate_type)}</strong>
 				</div>
 			</div>
-			<div className={styles.wrapper}>
-				<div className={styles.left_section}>
-					<i>Incidentals, Surcharges, any additional charges will apply as per contract terms</i>
-				</div>
 
-				<div className={styles.right_section}>
-					<div className={cl`${styles.tabs} ${showCogoPoints ? styles.less_width : styles.more_width}`}>
-						{Object.keys(TABS).map((item) => (
-							<span
-								role="presentation"
-								key={item}
-								className={cl`${styles.other_details_tag} 
-								${activeTab === item ? styles.selected : {}}`}
-								onClick={() => {
-									if (activeTab === item) {
-										setActiveTab('');
-									} else setActiveTab(item);
-								}}
-							>
-								<div className={styles.tab_label_container}>
-									{TABS[item]?.label}
-
-									{countOfNoRates && item === 'price_break_up' ? (
-										<Tooltip
-											placement="top"
-											trigger="mouseenter"
-											interactive
-											content={(
-												<span className={styles.tooltip_content}>
-													Rates for
-													{' '}
-													{countOfNoRates}
-													{' '}
-													services
-													{' '}
-													are not available
-												</span>
-											)}
-										>
-											<IcMInfo className={styles.info_icon} />
-										</Tooltip>
-									) : null}
-								</div>
-							</span>
-						))}
+			{isSelectedCard ? null : (
+				<div className={styles.wrapper}>
+					<div className={styles.left_section}>
+						<i>Incidentals, Surcharges, any additional charges will apply as per contract terms</i>
 					</div>
 
-					{showCogoPoints ? (
-						<div className={styles.cogo_points}>
-							CogoPoints:
-							<IcCCogoCoin className={styles.cogo_points_logo} width={16} height={16} />
-							{rate?.earnable_cogopoints}
+					<div className={styles.right_section}>
+						<div className={cl`${styles.tabs} ${showCogoPoints ? styles.less_width : styles.more_width}`}>
+							{Object.keys(TABS).map((item) => (
+								<span
+									role="presentation"
+									key={item}
+									className={cl`${styles.other_details_tag} 
+								${activeTab === item ? styles.selected : {}}`}
+									onClick={() => {
+										if (activeTab === item) {
+											setActiveTab('');
+										} else setActiveTab(item);
+									}}
+								>
+									<div className={styles.tab_label_container}>
+										{TABS[item]?.label}
+
+										{countOfNoRates && item === 'price_break_up' ? (
+											<Tooltip
+												placement="top"
+												trigger="mouseenter"
+												interactive
+												content={(
+													<span className={styles.tooltip_content}>
+														Rates for
+														{' '}
+														{countOfNoRates}
+														{' '}
+														services
+														{' '}
+														are not available
+													</span>
+												)}
+											>
+												<IcMInfo className={styles.info_icon} />
+											</Tooltip>
+										) : null}
+									</div>
+								</span>
+							))}
 						</div>
-					) : null}
+
+						{showCogoPoints ? (
+							<div className={styles.cogo_points}>
+								CogoPoints:
+								<IcCCogoCoin className={styles.cogo_points_logo} width={16} height={16} />
+								{rate?.earnable_cogopoints}
+							</div>
+						) : null}
+					</div>
 				</div>
-			</div>
+			)}
 
 			{activeTab ? (
 				<BottomTabs
