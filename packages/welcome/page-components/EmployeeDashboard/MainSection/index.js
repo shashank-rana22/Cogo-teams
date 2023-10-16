70c32eaa7b1d5a3d660a6ef8c374f8f783fb76c0
Shashank Rana <shashank.rana@cogoport.com>
2023-10-16 11:15:48 +0530

    dashboard-company-performance

--- packages/welcome/page-components/EmployeeDashboard/MainSection/index.js
+++ packages/welcome/page-components/EmployeeDashboard/MainSection/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 
+import CompanyPerformance from './CompanyPerformance';
 import styles from './styles.module.css';
 import YourBoard from './YourBoard';
-import YourPerformance from './YourPerformance';
+// import YourPerformance from './YourPerformance';
 
 function MainSection() {
 	return (
 		<div className={styles.container}>
 			<div className={styles.left_section}>
-				<YourPerformance />
+				{/* <YourPerformance /> */}
+				<CompanyPerformance />
 			</div>
 			<div className={styles.right_section}>
 				<YourBoard />
