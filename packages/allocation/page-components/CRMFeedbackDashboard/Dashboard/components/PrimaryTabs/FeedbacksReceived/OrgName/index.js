ec13178c67a5aff4d35d5534f8f73f3c8ee5f311
Niranjan Potnuru <potnuru.niranjan@cogoport.com>
2023-10-17 15:46:26 +0530

    redirect

--- packages/allocation/page-components/CRMFeedbackDashboard/Dashboard/components/PrimaryTabs/FeedbacksReceived/OrgName/index.js
+++ packages/allocation/page-components/CRMFeedbackDashboard/Dashboard/components/PrimaryTabs/FeedbacksReceived/OrgName/index.js
@@ -22,10 +22,10 @@ function OrgName({ item = {} }) {
 	const urlMapping = {
 		user              : `${window.location.origin}/${partner_id}/details/demand/${organization_id}`,
 		organization      : `${window.location.origin}/${partner_id}/details/demand/${organization_id}`,
-		lead_organization : `${window.location.origin}/${partner_id}/lead_organization/${lead_organization_id}`,
+		lead_organization : `${window.location.origin}/${partner_id}/lead-organization/${lead_organization_id}`,
 		lead_user         : organization_id
 			? `${window.location.origin}/${partner_id}/details/demand/${organization_id}`
-			: `${window.location.origin}/${partner_id}/lead_user/${source_id}`,
+			: `${window.location.origin}/${partner_id}/lead-user/${source_id}`,
 	};
 
 	return (
