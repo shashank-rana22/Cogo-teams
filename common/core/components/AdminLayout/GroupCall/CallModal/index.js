d8c71b52d77c485d54116e2f6c7e36cc23b52b03
abhijitCogoport <abhijit.gayen@cogoport.com>
2023-10-18 19:30:56 +0530

    remove

--- common/core/components/AdminLayout/GroupCall/CallModal/index.js
+++ common/core/components/AdminLayout/GroupCall/CallModal/index.js
@@ -12,7 +12,6 @@ function CallModal({
 	if (!url) {
 		return null;
 	}
-	console.log('meeting_link', url, showDeleteModal);
 
 	// eslint-disable-next-line max-len
 	const cogo_admin_auth_token = getCookie(process.env.NEXT_PUBLIC_ADMIN_AUTH_TOKEN_NAME) || 'eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTc3NzgxNzIsImlhdCI6MTY5NzYwNTM3MiwiVXNlclNlc3Npb25JRCI6IjEwOWM3MWQxLTNlOGMtNDM5OS1hOGY5LWVjMzEwY2JhODk2YiJ9.R8Qb7n3gMO8EXnl-i-Sv2ZvlEBOsanmP_n1C5cvWfHoJV3-3aTtzDR0TOsDtC3RZdawxMesLU008R21TMGGL3Q';
@@ -24,10 +23,9 @@ function CallModal({
 				<iframe
 					allow="camera; microphone; display-capture; fullscreen; clipboard-read; clipboard-write; autoplay"
 					src={`${url}&auth=${cogo_admin_auth_token}`}
-					type="application/pdf"
 					width="100%"
 					height="100%"
-					title="Document"
+					title="Video Call"
 					style={{ border: 'none' }}
 				/>
 			</Modal.Body>
