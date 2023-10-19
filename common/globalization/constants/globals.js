281d7ee49c405595fe46deb86a9064eb3e8dad96
Sandeep Nalabolu <sandeep.nalabolu@cogoport.com>
2023-10-19 13:37:35 +0530

    add option to mail lead orgs

--- common/globalization/constants/globals.js
+++ common/globalization/constants/globals.js
@@ -842,6 +842,9 @@ const GLOBAL_CONSTANTS = {
 		internal_supply          : 'internal.supply@cogoport.com',
 		internal_operations      : 'internal.operations@cogoport.com',
 		internal_service         : 'internal.customer@cogoport.com',
+		customer_support         : 'customer.support@cogoport.com',
+		operations               : 'operations@cogoport.com',
+		cogo_supply              : 'cogosupply@cogoport.com',
 	},
 	mobile_number: {
 		cogoone_sales_contact_no  : '+918069195810',
