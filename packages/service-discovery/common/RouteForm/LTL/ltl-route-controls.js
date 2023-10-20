fee89fbac275f0af8751da627d208da39c3a5e7b
rupesh-mutyala <rupesh.mutyala@cogoport.com>
2023-10-20 16:10:54 +0530

    ltl form changes

--- packages/service-discovery/common/RouteForm/LTL/ltl-route-controls.js
+++ packages/service-discovery/common/RouteForm/LTL/ltl-route-controls.js
@@ -1,27 +1,45 @@
+import GLOBAL_CONSTANTS from '@cogoport/globalization/constants/globals';
+
+const SUPPORTED_COUNTRY_IDS =	GLOBAL_CONSTANTS.service_supported_countries.feature_supported_service.common
+	.services.ltl_freight.default_country_ids;
+
 const ltlRouteControls = [
 	{
-		name        : 'origin_location_id',
+		label       : 'Origin Location',
 		type        : 'async-select',
-		label       : 'Pickup Point',
-		placeholder : 'Search via port name/code or pin',
+		name        : 'origin_location_id',
+		placeholder : 'City/Port/Airport/Pincode',
 		asyncKey    : 'list_locations',
 		initialCall : false,
 		span        : 6,
 		isClearable : true,
-		rules       : { required: 'Pickup Point is required' },
+		params      : {
+			apply_sorting : false,
+			filters       : {
+				type       : ['pincode', 'seaport', 'airport', 'city', 'warehouse'],
+				country_id : SUPPORTED_COUNTRY_IDS,
+			},
+		},
+		rules: { required: 'Origin Location is required' },
 	},
 	{
+		label       : 'Destination Location',
 		name        : 'destination_location_id',
+		placeholder : 'City/Port/Airport/Pincode',
+		isClearable : true,
 		type        : 'async-select',
-		label       : 'Delivery Point',
-		placeholder : 'Search via port name/code or pin',
 		asyncKey    : 'list_locations',
 		initialCall : false,
 		span        : 6,
-		isClearable : true,
-		rules       : { required: 'Pickup Point is required' },
+		params      : {
+			apply_sorting : false,
+			filters       : {
+				type       : ['pincode', 'seaport', 'airport', 'city', 'warehouse'],
+				country_id : SUPPORTED_COUNTRY_IDS,
+			},
+		},
+		rules: { required: 'Destination Location is required' },
 	},
-
 ];
 
 export default ltlRouteControls;
