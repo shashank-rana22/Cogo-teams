e4782c126ea38aa79efd9f98be1d72704ca7b9bd
Ritik Goyal <ritik.goyal@cogoport.com>
2023-10-20 12:35:13 +0530

    route changes

--- packages/service-discovery/page-components/SearchResults/page-components/AirResults/components/ListRates/components/RateCard/Body/Route/JourneyLine/index.js
+++ packages/service-discovery/page-components/SearchResults/page-components/AirResults/components/ListRates/components/RateCard/Body/Route/JourneyLine/index.js
@@ -9,29 +9,24 @@ function JourneyLine({
 }) {
 	return (
 		<div className={styles.container}>
+			<span className={cl`${styles.tag} ${styles.transit_time}`}>
+				{convertMinutesToHoursOrDays(scheduleData?.transit_time)}
+			</span>
+
 			<span className={styles.journey_line}>
 				<span className={styles.circle} />
-
-				<span className={styles.dotted_line} />
-
-				<div className={styles.tags_container}>
-					<span className={cl`${styles.tag} ${styles.transit_time}`}>
-						{convertMinutesToHoursOrDays(scheduleData?.transit_time)}
-					</span>
-
-					<span className={cl`${styles.tag} ${styles.schedule}`}>
-						Schedule
-						{['fake', 'predicted'].includes(scheduleData?.schedule_source)
-							? ' - Estimated'
-							: null}
-					</span>
-				</div>
 
 				<span className={styles.dotted_line} />
 
 				<span className={styles.active_circle} />
 			</span>
 
+			<span className={cl`${styles.tag} ${styles.schedule}`}>
+				Schedule
+				{['fake', 'predicted'].includes(scheduleData?.schedule_source)
+					? ' - Estimated'
+					: null}
+			</span>
 		</div>
 	);
 }
