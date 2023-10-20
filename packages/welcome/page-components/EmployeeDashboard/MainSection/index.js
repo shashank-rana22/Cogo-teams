a7d78952e7afa383482a00004aabae60525c25e6
Akshay Sawant <akshay.sawant@cogoport.com>
2023-10-20 20:37:37 +0530

    fixes

--- packages/welcome/page-components/EmployeeDashboard/MainSection/index.js
+++ packages/welcome/page-components/EmployeeDashboard/MainSection/index.js
@@ -6,13 +6,13 @@ import styles from './styles.module.css';
 import YourBoard from './YourBoard';
 import YourPerformance from './YourPerformance';
 
-function MainSection({ data = {} }) {
+function MainSection() {
 	return (
 		<div className={styles.container}>
 			<div className={styles.left_section}>
 				{/* <CompanyPerformance /> */}
-				{/* <YourPerformance /> */}
-				<PhoenixPerformance data={data} />
+				<YourPerformance />
+				{/* <PhoenixPerformance data={data} /> */}
 			</div>
 			<div className={styles.right_section}>
 				<YourBoard />
