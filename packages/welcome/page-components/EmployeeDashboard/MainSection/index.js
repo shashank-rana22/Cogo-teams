05d2d39e41283b8173ceae553e3829c1dc5ae98d
Akshay Sawant <akshay.sawant@cogoport.com>
2023-10-20 16:53:44 +0530

    deploy #deploy_on phoenix1

--- packages/welcome/page-components/EmployeeDashboard/MainSection/index.js
+++ packages/welcome/page-components/EmployeeDashboard/MainSection/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 
 // import CompanyPerformance from './CompanyPerformance';
-import PhoenixPerformance from './PhoenixPerformance';
+// import PhoenixPerformance from './PhoenixPerformance';
 import styles from './styles.module.css';
 import YourBoard from './YourBoard';
-// import YourPerformance from './YourPerformance';
+import YourPerformance from './YourPerformance';
 
 function MainSection() {
 	return (
 		<div className={styles.container}>
 			<div className={styles.left_section}>
 				{/* <CompanyPerformance /> */}
-				{/* <YourPerformance /> */}
-				<PhoenixPerformance />
+				<YourPerformance />
+				{/* <PhoenixPerformance /> */}
 			</div>
 			<div className={styles.right_section}>
 				<YourBoard />
