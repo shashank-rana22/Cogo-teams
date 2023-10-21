9de6e8844b3942c6b60322491a043c3a3ac88f99
Prathmesh-cogoport <Prathmesh.Mankar@cogoport.com>
2023-10-21 13:21:32 +0530

    Fixes

--- packages/business-finance/page-components/AccountPayables/Invoices/OverSeasAgent/utils/GetInvoiceData.js
+++ packages/business-finance/page-components/AccountPayables/Invoices/OverSeasAgent/utils/GetInvoiceData.js
@@ -19,9 +19,7 @@ function GetInvoiceData({
 			)
 			:						(
 				<Button
-					onClick={() => {
-						setShowConfirmationModal(true);
-					}}
+					onClick={() => { setShowConfirmationModal(true); }}
 					disabled={!list?.length}
 				>
 					Merge Invoices
