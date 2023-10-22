09740e8fcbfd6b193396e537d0bb09f11a9d3544
samir-cogoport <samir.dupare@cogoport.com>
2023-10-22 12:52:15 +0530

    validation

--- packages/supply-dashboards/page-components/RateCoverage/components/ListData/ListCard/DetailsView/ServiceProviderModal/index.js
+++ packages/supply-dashboards/page-components/RateCoverage/components/ListData/ListCard/DetailsView/ServiceProviderModal/index.js
@@ -41,6 +41,28 @@ function NewServiceProviderModal({
 		return ctr;
 	});
 
+	const showElements = {
+		shipping_line_id:
+			filter?.service === 'fcl_freight'
+			|| (filter?.service === 'haulage_freight'
+				&& data?.origin_location?.port_code
+				&& data?.destination_location?.port_code),
+		airline_id   : filter?.service === 'air_freight',
+		validity_end : [
+			'fcl_freight',
+			'air_freight',
+			'lcl_freight',
+			'ltl_freight',
+			'ftl_freight',
+			'haulage_freight',
+			'trailer_freight',
+			'air_customs',
+			'fcl_customs',
+		].includes(filter?.service),
+		schedule_type : filter?.service === 'fcl_freight',
+		rate_type     : filter?.service === 'air_freight',
+	};
+
 	return (
 		<Modal size="lg" show={serviceModal} onClose={() => setServiceModal(false)} placement="top">
 			<Modal.Header title="Your Price" />
@@ -49,6 +71,7 @@ function NewServiceProviderModal({
 					fields={fields}
 					control={control}
 					errors={errors}
+					showElements={showElements}
 				/>
 			</Modal.Body>
 			<Modal.Footer>
