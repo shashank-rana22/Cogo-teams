07293504747f5aa2b8afd9151b3ef1b038f00733
samir-cogoport <samir.dupare@cogoport.com>
2023-10-22 13:29:14 +0530

    update fix

--- packages/supply-dashboards/page-components/RateCoverage/helpers/revertPriceHelpers.js
+++ packages/supply-dashboards/page-components/RateCoverage/helpers/revertPriceHelpers.js
@@ -1,51 +1,86 @@
 import GLOBAL_CONSTANTS from '@cogoport/globalization/constants/globals';
+import { isEmpty } from '@cogoport/utils';
 
 const MINIMUM_PRICE = 1;
 const PERCENTAGE = 100;
 const NUMBER_FALLBACK = 0;
+const BASIC_LINE_ITEMS = ['BAS', 'BASNO'];
 
-export const formatLineItems = ({ lineItems, values }) => {
-	const { chargeable_weight = 0, price = 0, currency = '', min_price = 0 } = values || {};
-	return (lineItems || []).filter(
-		(eachItem) => GLOBAL_CONSTANTS.flash_booking_charge_codes.includes(eachItem?.code || eachItem?.customs_code),
-	).map((eachitem) => {
-		const quantity = Number(chargeable_weight) || eachitem.quantity;
-		const tax_price = (Number(price) * (eachitem.tax_percent || NUMBER_FALLBACK)) / PERCENTAGE;
-		const code = eachitem.customs_code || eachitem.code || undefined;
+export const formatLineItems = ({
+	lineItems,
+	values,
+}) => {
+	let is_anycharge_taken = false;
+	let lineItemsParams = (lineItems || [])
+		.map((item) => {
+			if (
+				GLOBAL_CONSTANTS.flash_booking_charge_codes.includes(
+					item.code,
+				) && !is_anycharge_taken
+			) {
+				const quantity = BASIC_LINE_ITEMS.includes(item?.code) && item?.unit === 'per_kg'
+					? Number(values?.chargeable_weight) || item?.quantity
+					: item?.quantity;
+				const tax_price = (Number(values?.price
+					|| NUMBER_FALLBACK) * (item.tax_percent || NUMBER_FALLBACK)) / PERCENTAGE;
+				is_anycharge_taken = true;
+				return {
+					...item,
+					quantity,
+					price           : Number(values?.price || NUMBER_FALLBACK),
+					total_price     : Number(values?.price || NUMBER_FALLBACK) * (quantity || MINIMUM_PRICE),
+					tax_price,
+					tax_total_price : tax_price * (quantity || MINIMUM_PRICE),
+					currency        : values?.currency,
+					min_price       : Number(values?.min_price) || item?.min_price,
+				};
+			}
+			return null;
+		})
+		.filter((item) => !!item);
 
-		return {
-			code,
-			quantity,
-			price           : Number(price),
-			total_price     : Number(price) * (quantity || MINIMUM_PRICE),
-			tax_price,
-			tax_total_price : tax_price * (quantity || MINIMUM_PRICE),
-			currency        : eachitem?.currency || currency,
-			min_price       : Number(min_price) || eachitem?.min_price,
-		};
-	}) || [];
-};
-
-export const formatFirstLineItem = ({ lineItems, values }) => {
-	const [firstItem] = lineItems || [];
-	const quantity = Number(values?.chargeable_weight) || firstItem.quantity;
-	const tax_price = (Number(values?.price
-					|| NUMBER_FALLBACK) * (firstItem.tax_percent || NUMBER_FALLBACK)) / PERCENTAGE;
-	return (lineItems || []).filter(
-		(eachItem) => GLOBAL_CONSTANTS.flash_booking_charge_codes.includes(eachItem?.code || eachItem?.customs_code),
-	).map((eachitem) => {
-		const code = eachitem.customs_code || eachitem.code;
-		return [
+	if (isEmpty(lineItemsParams) && lineItems.length) {
+		const item = lineItems[GLOBAL_CONSTANTS.zeroth_index];
+		const quantity = item?.unit === 'per_kg'
+			? Number(values?.chargeable_weight) || item.quantity
+			: item.quantity;
+		const tax_price = (Number(values?.price || NUMBER_FALLBACK)
+		* (item.tax_percent || NUMBER_FALLBACK)) / PERCENTAGE;
+		lineItemsParams = [
 			{
-				...firstItem,
+				...item,
 				quantity,
-				code,
-				price           : Number(values.price) || NUMBER_FALLBACK,
-				total_price     : Number(values.price || NUMBER_FALLBACK) * (quantity || MINIMUM_PRICE),
+				price           : Number(values?.price || NUMBER_FALLBACK),
+				total_price     : Number(values?.price || NUMBER_FALLBACK) * (quantity || 1),
 				tax_price,
-				tax_total_price : tax_price * (quantity || MINIMUM_PRICE),
+				tax_total_price : tax_price * (quantity || 1),
 				currency        : values?.currency,
 			},
 		];
-	}) || [];
+	}
+
+	return lineItemsParams;
+};
+
+export const formatWeightSlabs = ({ values }) => {
+	const formattedWeightSlabs = values?.weight_slabs || [];
+
+	formattedWeightSlabs.forEach((weight_slab, index) => {
+		const { lower_limit, upper_limit } = weight_slab;
+		const floatLowerLimit = +lower_limit || 0;
+		const floatUpperLimit = +upper_limit || 0;
+		formattedWeightSlabs[index] = {
+			...formattedWeightSlabs[index],
+			lower_limit  : floatLowerLimit,
+			upper_limit  : floatUpperLimit,
+			tariff_price : +(values?.price || 0),
+			unit         : 'per_kg',
+			currency     : values?.currency,
+		};
+	});
+	(formattedWeightSlabs || []).sort(
+		(a, b) => a.lower_limit - b.lower_limit,
+	);
+
+	return formattedWeightSlabs;
 };
--- packages/supply-dashboards/page-components/RateCoverage/hooks/useUpdateFlashBookingRate.js
+++ packages/supply-dashboards/page-components/RateCoverage/hooks/useUpdateFlashBookingRate.js
@@ -2,86 +2,88 @@ import { Toast } from '@cogoport/components';
 import { useRequest } from '@cogoport/request';
 import { isEmpty } from '@cogoport/utils';
 
-import { formatLineItems, formatFirstLineItem } from '../helpers/revertPriceHelpers';
-import getPayload from '../payload/getRevertPricePayload';
+import { formatLineItems, formatWeightSlabs } from '../helpers/revertPriceHelpers';
+
+import useShipmentBuyQuotations from './useShipmentBuyQuotations';
 
 const LINE_ITEMS_NOT_REQUIRED_FOR = ['air_freight', 'ftl_freight', 'ltl_freight', 'ftl_freight'];
 
-const useUpdateFlashBookingRate = () => {
+const useUpdateFlashBookingRate = ({
+	data, filter,
+}) => {
 	const [{ loading }, trigger] = useRequest({
 		url    : 'update_shipment_flash_booking_rate',
 		method : 'post',
 	}, { manual: true });
 
+	const service = filter?.service;
+	const { shipment_id } = data || {};
+	const service_id = data?.shipment_service_id;
+	const { line_items: quotationLineItems } = useShipmentBuyQuotations({
+		shipment_id,
+		service_id,
+	});
+
 	const updateFlashBookingRate = async ({
-		data, formData,
-		shipment_data, filter, newServiceProviderData, falshBookingLineItems,
+		formData = {}, manualRevertData = {}, isManual = false,
 	}) => {
-		const service = filter?.service;
-		const { shipment_id, source_id, service_provider_id } = data || {};
-		const { line_items: new_line_items, shipment_id:new_shipment_id } = falshBookingLineItems || {};
-		const {
-			service_provider_id : new_service_provider_id, shipping_line_id,
-			currency: newCurrency, schedule_type: new_schedule_type, supplier_contract_no,
-		} = newServiceProviderData || {};
 		const {
 			is_shipper_specific = false,
-			weight_slabs, schedule_type,
-			line_items, currency,
-			chargeableWeight,
+			line_items = [],
 		} = formData || {};
-		const { summary } = shipment_data || {};
+		const values = { ...formData, ...manualRevertData };
 
-		const WEIGHT_SLABS = (weight_slabs || []).map((item) => ({
-			lower_limit  : item?.lower_limit,
-			upper_limit  : item?.upper_limit,
-			tariff_price : item?.price_per_unit || item?.price,
-			currency     : item?.currency || data?.currency,
-		}));
-
-		let lineItemsParams = formatLineItems({ lineItems: line_items || new_line_items, values: formData });
-		if (isEmpty(lineItemsParams) && !isEmpty(line_items)) {
-			lineItemsParams = formatFirstLineItem({ lineItems: line_items, values: formData });
-		}
+		const lineItemsParams = formatLineItems({
+			lineItems: isManual ? quotationLineItems : line_items,
+			values,
+			isManual,
+			quotationLineItems,
+		});
+		const formattedWeightSlabs = formatWeightSlabs({ values });
 		if (isEmpty(lineItemsParams) && !LINE_ITEMS_NOT_REQUIRED_FOR.includes(service)) {
 			Toast.error(
 				'You can not revert to this live booking - No LineItems!!',
 			);
 			return;
 		}
-		const formattedPayload = getPayload({
-			lineItemsParams,
-			values       : formData,
-			id           : source_id,
-			service_type : service,
-			chargeableWeight,
-		});
 
 		try {
 			const resp = await trigger({
 				data: {
-					service_type              : `${service}_service` || undefined,
-					is_rate_creation_required : false,
-					shipment_id               : shipment_id || new_shipment_id,
-					id                        : source_id,
-					sourced_by_id             : service_provider_id,
-					currency,
-					advance_amount_currency   : newCurrency || undefined,
-					schedule_type             : schedule_type || new_schedule_type,
-					is_shipper_specific       : is_shipper_specific || undefined,
-					importer_exporter_id      : is_shipper_specific === true ? summary?.importer_exporter_id
+					line_items           : lineItemsParams,
+					shipping_line_id     : values?.shipping_line_id || undefined,
+					airline_id           : values?.airline_id || undefined,
+					price_type           : values?.price_type || undefined,
+					operation_type       : values?.operation_type || undefined,
+					weight_slabs         : formattedWeightSlabs,
+					is_reverted          : true,
+					service_id           : isManual ? service_id : undefined,
+					service_type         : isManual ? `${service}_service` : undefined,
+					shipment_id          : isManual ? shipment_id : undefined,
+					supplier_contract_no : values.supplier_contract_no || undefined,
+					transit_time         : values?.transit_time || undefined,
+					detention_free_time  : values?.detention_free_time || undefined,
+					validity_end         : values?.validity_end || undefined,
+					sourced_by_id        : values?.sourced_by_id,
+					service_provider_id  : isManual
+						? values.service_provider_id
 						: undefined,
-					weight_slabs         : WEIGHT_SLABS,
-					formattedPayload     : formattedPayload || undefined,
-					service_provider_id  : new_service_provider_id || service_provider_id || undefined,
-					shipping_line_id     : shipping_line_id || undefined,
-					supplier_contract_no : supplier_contract_no || undefined,
+					remarks: values?.remarks || undefined,
+					advance_amount:
+					service === 'ftl_freight'
+						? Number(values?.advanced_amount || 0)
+						: undefined,
+					advance_amount_currency : values?.currency,
+					chargeable_weight       : Number(values?.chargeable_weight) || undefined,
+					is_shipper_specific     : is_shipper_specific || undefined,
+					schedule_type           : values.schedule_type || undefined,
+					rate_type               : values?.rate_type || undefined,
 				},
 			});
 			// eslint-disable-next-line consistent-return
-			if (resp) { return resp?.status; }
+			if (resp?.status) { return resp?.status; }
 		} catch (e) {
-			Toast.error('Failed To Cancel');
+			Toast.error(e);
 		}
 	};
 
