a77547176f407c76954dd1aee3c299c8ea75f67a
hrixtik <Hritik.Kumar@cogoport.com>
2023-10-25 19:27:05 +0530

    fixes

--- packages/profile/hooks/useUpdateEmployee.js
+++ packages/profile/hooks/useUpdateEmployee.js
@@ -2,6 +2,7 @@ import { Toast } from '@cogoport/components';
 import getApiErrorString from '@cogoport/forms/utils/getApiError';
 import { useHarbourRequest } from '@cogoport/request';
 import { useSelector } from '@cogoport/store';
+import { isEmpty } from '@cogoport/utils';
 import { useRouter } from 'next/router';
 
 const useUpdateEmployee = ({ handleModal = () => {}, getEmployeeDetails }) => {
@@ -15,9 +16,10 @@ const useUpdateEmployee = ({ handleModal = () => {}, getEmployeeDetails }) => {
 
 	let id_user = profile?.user?.id;
 
-	if ((query.employee_id) !== '') {
+	if (!isEmpty(query?.employee_id)) {
 		id_user = query.employee_id;
 	}
+	console.log('id:use', id_user, profile?.user?.id);
 
 	const updateEmployeeDetails = async (payload) => {
 		try {
