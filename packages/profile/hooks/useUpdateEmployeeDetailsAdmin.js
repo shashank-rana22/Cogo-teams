6b169988f8f3e549e8a119197fbce4ede939705d
hrixtik <Hritik.Kumar@cogoport.com>
2023-10-26 18:02:36 +0530

    fixes

--- packages/profile/hooks/useUpdateEmployeeDetailsAdmin.js
+++ packages/profile/hooks/useUpdateEmployeeDetailsAdmin.js
@@ -2,58 +2,29 @@ import { Toast } from '@cogoport/components';
 import getApiErrorString from '@cogoport/forms/utils/getApiError';
 import { useHarbourRequest } from '@cogoport/request';
 
-const getUpdatedKeysInPayload = (newValue, oldValue) => {
-	if (typeof oldValue === 'object' && JSON.stringify(oldValue) === JSON.stringify(newValue)) return undefined;
-	if (oldValue === newValue) return undefined;
-	return newValue;
-};
-
-// const getPersonalInfoPayload = (data, detail) => {
-// 	const NEW_DATA = {};
-// 	(Object.keys(data) || []).forEach((element) => {
-// 		NEW_DATA[element] = getUpdatedKeysInPayload(data[element], detail?.[element]);
-// 	});
-
-// 	const emergency_contact_details = [
-// 		{
-// 			mobile_number       : data?.emergency_contact_details?.number,
-// 			mobile_country_code : data?.emergency_contact_details?.country_code,
-// 		},
-// 	];
-
-// 	return {
-// 		...NEW_DATA,
-// 		passport_size_photo_url:
-// 				getUpdatedKeysInPayload(
-// 					data?.passport_size_photo_url?.finalUrl || data?.passport_size_photo_url,
-// 					detail?.passport_size_photo_url,
-// 				),
-// 		mobile_number: getUpdatedKeysInPayload(data?.mobile_number?.number, detail?.mobile_number),
-// 		mobile_country_code:
-// 				getUpdatedKeysInPayload(data?.mobile_number?.country_code, detail?.mobile_country_code),
-// 		date_of_birth: String(data?.date_of_birth),
-// 		emergency_contact_details:
-// 				getUpdatedKeysInPayload(emergency_contact_details, detail?.emergency_contact_details),
-// 	};
+// const getUpdatedKeysInPayload = (newValue, oldValue) => {
+// 	if (typeof oldValue === 'object' && JSON.stringify(oldValue) === JSON.stringify(newValue)) return undefined;
+// 	if (oldValue === newValue) return undefined;
+// 	return newValue;
 // };
 
-const getEducationalQualificationsPayload = (data, detail) => {
-	const employee_education_details = (data?.educational_qualification || []).map(
-		(item) => ({
-			...item,
-			degree_proof : item?.degree_proof?.finalUrl ? item?.degree_proof?.finalUrl : item?.degree_proof,
-			started_at   : item.started_at,
-			ended_at     : item.ended_at,
-		}),
-	);
+// const getEducationalQualificationsPayload = (data, detail) => {
+// 	const employee_education_details = (data?.educational_qualification || []).map(
+// 		(item) => ({
+// 			...item,
+// 			degree_proof : item?.degree_proof?.finalUrl ? item?.degree_proof?.finalUrl : item?.degree_proof,
+// 			started_at   : item.started_at,
+// 			ended_at     : item.ended_at,
+// 		}),
+// 	);
 
-	console.log('employee_education_details', employee_education_details);
+// 	console.log('employee_education_details', employee_education_details);
 
-	return {
-		employee_education_details:
-				getUpdatedKeysInPayload(employee_education_details, detail?.employee_education_details),
-	};
-};
+// 	return {
+// 		employee_education_details:
+// 				getUpdatedKeysInPayload(employee_education_details, detail?.employee_education_details),
+// 	};
+// };
 
 // const getEmploymentHistoryPayload = ({ data, offerLetter, paySlip, detail }) => {
 // 	const employee_experience_details = (data?.employment_history || []).map((item) => ({
@@ -72,9 +43,8 @@ const getEducationalQualificationsPayload = (data, detail) => {
 
 function useUpdateEmployeeDetails({
 	id,
+	TO_ADD,
 	getEmployeeDetails = () => {},
-	offerLetter = {},
-	paySlip = {},
 	setShow = () => {},
 }) {
 	const [{ loading }, trigger] = useHarbourRequest(
@@ -95,8 +65,9 @@ function useUpdateEmployeeDetails({
 			await trigger({
 				data: {
 					id,
+					to_add                     : TO_ADD,
 					// ...(getEducationalQualificationsPayload(data,) || []),
-					employee_education_details: [{
+					employee_education_details : [{
 						...values,
 						degree_proof: values?.degree_proof?.finalUrl || values?.degree_proof,
 					}],
--- packages/profile/page-components/TabsPanel/EducationDetails/DetailsUpdationModal/index.js
+++ packages/profile/page-components/TabsPanel/EducationDetails/DetailsUpdationModal/index.js
@@ -2,7 +2,7 @@ import { Button } from '@cogoport/components';
 import { useForm } from '@cogoport/forms';
 import { getCountryConstants } from '@cogoport/globalization/constants/geo';
 import GLOBAL_CONSTANTS from '@cogoport/globalization/constants/globals';
-import { useEffect } from 'react';
+// import { useEffect } from 'react';
 
 import useUpdateEmployeeDetails from '../../../../hooks/useUpdateEmployeeDetailsAdmin';
 import getElementController from '../../../../utils/configs/getElementController';
@@ -10,10 +10,10 @@ import getElementController from '../../../../utils/configs/getElementController
 import getControls from './controls';
 import styles from './styles.module.css';
 
-const FORM_TYPE_EDUCATIONAL_QUALIFICATION = 'educational_qualification';
+// const FORM_TYPE_EDUCATIONAL_QUALIFICATION = 'educational_qualification';
 
 function EducationalQualification({ getEmployeeDetails, data }) {
-	const { handleSubmit, control, setValue, watch, formState: { errors } } = useForm();
+	const { handleSubmit, control, watch, formState: { errors } } = useForm();
 
 	const controls = getControls();
 
@@ -22,7 +22,7 @@ function EducationalQualification({ getEmployeeDetails, data }) {
 	const { employee_detail } = data || {};
 	console.log('🚀 ~ file: index.js:21 ~ EducationalQualification ~ detail:', employee_detail);
 
-	const { id, employee_education_details } = employee_detail || {};
+	const { id } = employee_detail || {};
 	const country_id = GLOBAL_CONSTANTS?.country_ids?.IN;
 
 	const countrySpecificOptions = getCountryConstants({ country_id });
@@ -33,8 +33,8 @@ function EducationalQualification({ getEmployeeDetails, data }) {
 	console.log('options', options);
 
 	const { education_level } = watch() || {};
-
-	const { loading, updateEmployeeDetails } = useUpdateEmployeeDetails({ id, getEmployeeDetails });
+	const TO_ADD = true;
+	const { loading, updateEmployeeDetails } = useUpdateEmployeeDetails({ id, getEmployeeDetails, TO_ADD });
 
 	const onSubmit = (values) => {
 		// updateEmployeeDetails({
