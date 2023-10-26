c7726219072fb2f3e4691006c8b7da9fb039cec3
rupesh-mutyala <rupesh.mutyala@cogoport.com>
2023-10-26 11:29:33 +0530

    pill text

--- packages/service-discovery/common/Header/AIRHeader/Packages/PackageItem/index.js
+++ packages/service-discovery/common/Header/AIRHeader/Packages/PackageItem/index.js
@@ -1,4 +1,4 @@
-import { Popover, Tooltip } from '@cogoport/components';
+import { Popover } from '@cogoport/components';
 import GLOBAL_CONSTANTS from '@cogoport/globalization/constants/globals';
 import { IcMEdit } from '@cogoport/icons-react';
 import { startCase } from '@cogoport/utils';
@@ -15,79 +15,58 @@ function PackageItem({
 	popoverComponentData = {},
 	setShowModal = () => {},
 	setInfoBanner = () => {},
-	chargeable_weight = 0,
 }) {
-	const { commodity = '', commodity_details = [], total_volume = 1, total_weight = 1 } = loadItem;
+	const { commodity = '', commodity_details = [], total_volume = 1, total_weight = 1, total_quantity = 1 } = loadItem;
 
 	const { commodity_type = '' } = commodity_details[GLOBAL_CONSTANTS.zeroth_index] || {};
 
 	return (
-		<div className={styles.container}>
-			<div className={styles.load_item}>
-				<span className={styles.text}>
+		<div className={styles.load_item}>
+			<span className={styles.text}>
+				count:
+				{' '}
+				<strong>{total_quantity}</strong>
+				{', '}
+				volume:
+				{' '}
+				<strong>
 					{total_volume}
 					{' '}
 					CBM
-					{', '}
+				</strong>
+				{', '}
+				weight:
+				{' '}
+				<strong>
 					{total_weight}
 					{' '}
 					KG
-				</span>
+				</strong>
+				{', '}
+				<strong>{startCase(commodity === 'general' ? commodity : commodity_type)}</strong>
+			</span>
 
-				{isAllowedToEdit ? (
-					<Popover
-						placement="bottom"
-						caret
-						render={(
-							<InfoBannerContent
-								popoverComponentData={popoverComponentData}
-								totalBanners={totalBanners}
-								setInfoBanner={setInfoBanner}
-							/>
-						)}
-						visible={showPopover}
-					>
-						<IcMEdit
-							height={12}
-							width={12}
-							className={styles.edit}
-							onClick={() => setShowModal(true)}
+			{isAllowedToEdit ? (
+				<Popover
+					placement="bottom"
+					caret
+					render={(
+						<InfoBannerContent
+							popoverComponentData={popoverComponentData}
+							totalBanners={totalBanners}
+							setInfoBanner={setInfoBanner}
 						/>
-					</Popover>
-				) : null}
-			</div>
-
-			<div className={styles.load_item} style={{ marginLeft: 12 }}>
-				<Tooltip
-					placement="top"
-					content={(
-						<div className={styles.tooltip_content}>
-							Chargeable Weight:
-							{' '}
-							<strong>
-								{chargeable_weight}
-								{' '}
-								{' '}
-								Kgs
-							</strong>
-							<br />
-							( 1CBM = 166.67kgs )
-						</div>
 					)}
+					visible={showPopover}
 				>
-					<span className={styles.text}>
-						{chargeable_weight}
-						{' '}
-						Kgs
-					</span>
-				</Tooltip>
-			</div>
-
-			<div className={styles.load_item} style={{ margin: '0 12px' }}>
-				<span className={styles.text}>
-					{startCase(commodity === 'general' ? commodity : commodity_type)}
-				</span>
-			</div>
+					<IcMEdit
+						height={12}
+						width={12}
+						className={styles.edit}
+						onClick={() => setShowModal(true)}
+					/>
+				</Popover>
+			) : null}
 		</div>
 	);
 }
