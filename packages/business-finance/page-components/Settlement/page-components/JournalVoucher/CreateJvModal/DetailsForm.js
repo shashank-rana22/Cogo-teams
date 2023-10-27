53e74280947451cb1a1bc37fa6d8335b3fc2d656
Vejandla Chanukya <vejandlachanukya@Vejandlas-MacBook-Air.local>
2023-10-27 20:06:39 +0530

    exchange_fix

--- packages/business-finance/page-components/Settlement/page-components/JournalVoucher/CreateJvModal/DetailsForm.js
+++ packages/business-finance/page-components/Settlement/page-components/JournalVoucher/CreateJvModal/DetailsForm.js
@@ -11,7 +11,6 @@ import React from 'react';
 import styles from './styles.module.css';
 
 function DetailsForm({ errors, control, handleEntityChange }) {
-	console.log(errors, control, handleEntityChange, 'kkkkkkkk');
 	return (
 		<>
 			<div className={styles.flex}>
