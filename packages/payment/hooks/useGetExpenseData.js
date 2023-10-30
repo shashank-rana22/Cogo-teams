6553c54cc19b38ee4f85fe0491c8a6111d7f7713
venkateshpatnala <Venkatesh.Patnala@cogoport.com>
2023-10-30 19:27:22 +0530

    filters added to expense page

--- packages/payment/hooks/useGetExpenseData.js
+++ packages/payment/hooks/useGetExpenseData.js
@@ -3,7 +3,7 @@ import getApiErrorString from '@cogoport/forms/utils/getApiError';
 import { useHarbourRequest } from '@cogoport/request';
 import { useCallback, useEffect } from 'react';
 
-const useGetExpenseData = ({ toggleValue }) => {
+const useGetExpenseData = ({ toggleValue, value }) => {
 	const [{ loading, data }, trigger] = useHarbourRequest({
 		method : 'GET',
 		url    : '/get_expense_data',
@@ -13,12 +13,12 @@ const useGetExpenseData = ({ toggleValue }) => {
 		async () => {
 			await trigger({
 				params: {
-					employee_view: toggleValue,
-					// date_range    : value,
+					employee_view : toggleValue,
+					date_range    : value,
 				},
 			});
 		},
-		[toggleValue, trigger],
+		[toggleValue, trigger, value],
 	);
 
 	useEffect(() => {
