4d703e4bcfb73783cba7d000b23ffbcbe8898d1d
Sandeep Nalabolu <sandeep.nalabolu@cogoport.com>
2023-10-31 19:24:10 +0530

    add graph data

---
+++ packages/cogo-one-sme-dashboard/page-components/SmeComponents/Analytics/graphData.js
@@ -0,0 +1,272 @@
+export const DUMMY_DATA = [
+	{
+		id    : 'japan',
+		color : 'hsl(358, 70%, 50%)',
+		data  : [
+			{
+				x : 'plane',
+				y : 7,
+			},
+			{
+				x : 'helicopter',
+				y : 208,
+			},
+			{
+				x : 'boat',
+				y : 164,
+			},
+			{
+				x : 'train',
+				y : 85,
+			},
+			{
+				x : 'subway',
+				y : 288,
+			},
+			{
+				x : 'bus',
+				y : 133,
+			},
+			{
+				x : 'car',
+				y : 126,
+			},
+			{
+				x : 'moto',
+				y : 135,
+			},
+			{
+				x : 'bicycle',
+				y : 16,
+			},
+			{
+				x : 'horse',
+				y : 104,
+			},
+			{
+				x : 'skateboard',
+				y : 119,
+			},
+			{
+				x : 'others',
+				y : 138,
+			},
+		],
+	},
+	{
+		id    : 'france',
+		color : 'hsl(136, 70%, 50%)',
+		data  : [
+			{
+				x : 'plane',
+				y : 171,
+			},
+			{
+				x : 'helicopter',
+				y : 200,
+			},
+			{
+				x : 'boat',
+				y : 8,
+			},
+			{
+				x : 'train',
+				y : 8,
+			},
+			{
+				x : 'subway',
+				y : 250,
+			},
+			{
+				x : 'bus',
+				y : 30,
+			},
+			{
+				x : 'car',
+				y : 79,
+			},
+			{
+				x : 'moto',
+				y : 46,
+			},
+			{
+				x : 'bicycle',
+				y : 93,
+			},
+			{
+				x : 'horse',
+				y : 149,
+			},
+			{
+				x : 'skateboard',
+				y : 93,
+			},
+			{
+				x : 'others',
+				y : 255,
+			},
+		],
+	},
+	{
+		id    : 'us',
+		color : 'hsl(18, 70%, 50%)',
+		data  : [
+			{
+				x : 'plane',
+				y : 196,
+			},
+			{
+				x : 'helicopter',
+				y : 59,
+			},
+			{
+				x : 'boat',
+				y : 116,
+			},
+			{
+				x : 'train',
+				y : 34,
+			},
+			{
+				x : 'subway',
+				y : 33,
+			},
+			{
+				x : 'bus',
+				y : 249,
+			},
+			{
+				x : 'car',
+				y : 196,
+			},
+			{
+				x : 'moto',
+				y : 296,
+			},
+			{
+				x : 'bicycle',
+				y : 203,
+			},
+			{
+				x : 'horse',
+				y : 96,
+			},
+			{
+				x : 'skateboard',
+				y : 76,
+			},
+			{
+				x : 'others',
+				y : 80,
+			},
+		],
+	},
+	{
+		id    : 'germany',
+		color : 'hsl(290, 70%, 50%)',
+		data  : [
+			{
+				x : 'plane',
+				y : 249,
+			},
+			{
+				x : 'helicopter',
+				y : 92,
+			},
+			{
+				x : 'boat',
+				y : 236,
+			},
+			{
+				x : 'train',
+				y : 241,
+			},
+			{
+				x : 'subway',
+				y : 140,
+			},
+			{
+				x : 'bus',
+				y : 68,
+			},
+			{
+				x : 'car',
+				y : 100,
+			},
+			{
+				x : 'moto',
+				y : 256,
+			},
+			{
+				x : 'bicycle',
+				y : 54,
+			},
+			{
+				x : 'horse',
+				y : 265,
+			},
+			{
+				x : 'skateboard',
+				y : 198,
+			},
+			{
+				x : 'others',
+				y : 202,
+			},
+		],
+	},
+	{
+		id    : 'norway',
+		color : 'hsl(348, 70%, 50%)',
+		data  : [
+			{
+				x : 'plane',
+				y : 62,
+			},
+			{
+				x : 'helicopter',
+				y : 80,
+			},
+			{
+				x : 'boat',
+				y : 115,
+			},
+			{
+				x : 'train',
+				y : 132,
+			},
+			{
+				x : 'subway',
+				y : 289,
+			},
+			{
+				x : 'bus',
+				y : 99,
+			},
+			{
+				x : 'car',
+				y : 131,
+			},
+			{
+				x : 'moto',
+				y : 49,
+			},
+			{
+				x : 'bicycle',
+				y : 136,
+			},
+			{
+				x : 'horse',
+				y : 17,
+			},
+			{
+				x : 'skateboard',
+				y : 95,
+			},
+			{
+				x : 'others',
+				y : 177,
+			},
+		],
+	},
+];
--- packages/cogo-one-sme-dashboard/page-components/SmeComponents/Analytics/index.js
+++ packages/cogo-one-sme-dashboard/page-components/SmeComponents/Analytics/index.js
@@ -1,11 +1,81 @@
+import { ResponsiveLine } from '@cogoport/charts/line';
 import React from 'react';
 
+import { DUMMY_DATA } from './graphData';
 import styles from './styles.module.css';
 
 function Analytics() {
 	return (
 		<div className={styles.container}>
-			Analytics
+			<div className={styles.header}>
+				<div className={styles.label}>
+					Analytics
+				</div>
+			</div>
+			<div className={styles.graph_container}>
+				<ResponsiveLine
+					data={DUMMY_DATA}
+					margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+					xScale={{ type: 'point' }}
+					yScale={{
+						type    : 'linear',
+						min    	: 'auto',
+						max    	: 'auto',
+						stacked : true,
+						reverse : false,
+					}}
+					yFormat=" >-.2f"
+					axisTop={null}
+					axisRight={null}
+					axisBottom={{
+						tickSize    	  : 5,
+						tickPadding    : 5,
+						tickRotation   : 0,
+						legend    	    : '',
+						legendOffset   : 36,
+						legendPosition : 'middle',
+					}}
+					axisLeft={{
+						tickSize    	  : 5,
+						tickPadding    : 5,
+						tickRotation   : 0,
+						legend    	    : '',
+						legendOffset   : -40,
+						legendPosition : 'middle',
+					}}
+					enablePoints={false}
+					pointSize={2}
+					pointColor={{ theme: 'background' }}
+					pointBorderColor={{ from: 'serieColor' }}
+					pointLabelYOffset={-24}
+					useMesh
+					legends={[
+						{
+							anchor    	   : 'top-right',
+							direction    	: 'row',
+							justify    	  : false,
+							translateX    : 36,
+							translateY    : -37,
+							itemWidth    	: 66,
+							itemHeight    : 26,
+							itemsSpacing  : 8,
+							symbolSize    : 11,
+							symbolShape   : 'circle',
+							itemDirection : 'left-to-right',
+							itemTextColor : '#777',
+							effects    	  : [
+								{
+									on    : 'hover',
+									style : {
+										itemBackground : 'rgba(0, 0, 0, .03)',
+										itemOpacity    : 1,
+									},
+								},
+							],
+						},
+					]}
+				/>
+			</div>
 		</div>
 	);
 }
