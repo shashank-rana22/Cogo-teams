7f6b3e7b0aeea901b787e2d739e42b46efc3b47d
tejoyasaswi-cogoport <Tejo.Uppuluri@cogoport.com>
2023-11-01 13:41:24 +0530

    split components

--- packages/business-finance/page-components/AccountReceivables/configs/Completed_table.js
+++ packages/business-finance/page-components/AccountReceivables/configs/Completed_table.js
@@ -1,20 +1,13 @@
-/* eslint-disable max-lines-per-function */
 import { Pill, Tooltip } from '@cogoport/components';
 import formatAmount from '@cogoport/globalization/utils/formatAmount';
-import { IcMInfo, IcMOverview, IcMProvision } from '@cogoport/icons-react';
-import { format, getByKey, isEmpty, startCase } from '@cogoport/utils';
+import { getByKey, isEmpty, startCase } from '@cogoport/utils';
 import React from 'react';
 
-import InvoiceDetails from '../commons/invoiceDetails';
-import Remarks from '../commons/Remarks';
-import RenderIRNGenerated from '../commons/RenderIRNGenerated';
-import RibbonRender from '../commons/RibbonRender';
 import { getDocumentInfo } from '../Utils/getDocumentNumber';
-import getStatus from '../Utils/getStatus';
 
 import CheckboxItem from './CheckboxItem';
+import completedTableRestFields from './Completed_table_rest_fields';
 import HeaderCheckbox from './HeaderCheckbox';
-import SortHeaderInvoice from './SortHeaderInvoice';
 import styles from './styles.module.css';
 
 const STATUS = {
@@ -23,23 +16,7 @@ const STATUS = {
 	PAID             : '#CDF7D4',
 };
 
-const INVOICE_TYPE = {
-	REIMBURSEMENT : '#FEF1DF',
-	CREDIT_NOTE   : '#D9EAFD',
-	INVOICE       : '#CDF7D4',
-};
-
-const INVOICE_STATUS_MAPPING = {
-	DRAFT            : '#fcedbf',
-	POSTED           : '#a1f0ae',
-	FINANCE_ACCEPTED : '#CDF7D4',
-	CONSOLIDATED     : '#D9EAFD',
-	IRN_GENERATED    : '#b8debe',
-	IRN_FAILED       : '#F89880',
-	FAILED           : '#f9b498',
-	IRN_CANCELLED    : '#fbc5b0',
-	FINANCE_REJECTED : '#f9ac98',
-};
+const INVOICE_TYPE = { REIMBURSEMENT: '#FEF1DF', CREDIT_NOTE: '#D9EAFD', INVOICE: '#CDF7D4' };
 
 const IRN_GENERATEABLE_STATUSES = ['FINANCE_ACCEPTED', 'IRN_FAILED'];
 
@@ -68,7 +45,6 @@ function openLink({ event, partnerId, shipmentId, serviceTypeUpper }) {
 
 const MIN_NAME_STRING = 0;
 const MAX_NAME_STRING = 14;
-const NINE = 9;
 
 const completedColumn = ({
 	refetch,
@@ -136,11 +112,7 @@ const completedColumn = ({
 						</text>
 					</Tooltip>
 				)
-					: (
-						<div className={styles.cursor}>
-							{getByKey(row, 'organizationName')}
-						</div>
-					)
+					: (<div className={styles.cursor}>{getByKey(row, 'organizationName')}</div>)
 			)
 		),
 	},
@@ -309,236 +281,20 @@ const completedColumn = ({
 		),
 		id: 'invoice_amount',
 	},
-	{
-		Header: () => (
-			<div className={styles.flex}>
-				<div>
-					Ledger Amount
-				</div>
-				<SortHeaderInvoice
-					invoiceFilter={invoiceFilters}
-					setInvoiceFilter={setinvoiceFilters}
-					setOrderBy={setSort}
-					sortStyleDesc={sortStyleLedgerTotalDesc}
-					sortStyleAsc={sortStyleLedgerTotalAsc}
-					type="ledgerTotal"
-				/>
-			</div>
-		),
-		accessor: (row) => (
-			<div>
-				<div>
-					{
-				formatAmount({
-					amount   : getByKey(row, 'ledgerAmount'),
-					currency : getByKey(row, 'ledgerCurrency'),
-					options  : {
-						style           : 'currency',
-						currencyDisplay : 'code',
-					},
-				})
-			}
-				</div>
-			</div>
-		),
-		id: 'ledger_amount',
-	},
-	{
-		Header   : 'Balance Amount',
-		accessor : (row) => (
-			<div>
-				<div>
-					{
-				formatAmount({
-					amount   : getByKey(row, 'balanceAmount'),
-					currency : getByKey(row, 'invoiceCurrency'),
-					options  : {
-						style           : 'currency',
-						currencyDisplay : 'code',
-					},
-				})
-			}
-
-				</div>
-			</div>
-		),
-	},
-	{
-		Header: () => (
-			<div className={styles.flex}>
-				<div>
-					Invoice Date
-				</div>
-				<SortHeaderInvoice
-					invoiceFilter={invoiceFilters}
-					setInvoiceFilter={setinvoiceFilters}
-					setOrderBy={setSort}
-					sortStyleDesc={sortStyleInvoiceDateDesc}
-					sortStyleAsc={sortStyleInvoiceDateAsc}
-					type="invoiceDate"
-				/>
-			</div>
-		),
-		accessor: (row) => (
-			<div>
-				<div>{format(getByKey(row, 'invoiceDate'), 'dd MMM yy', {}, false)}</div>
-
-			</div>
-		),
-		id: 'invoice_date',
-
-	},
-	{
-		Header: () => (
-			<div className={styles.flex}>
-				<div>
-					Due Date
-				</div>
-				<SortHeaderInvoice
-					invoiceFilter={invoiceFilters}
-					setInvoiceFilter={setinvoiceFilters}
-					setOrderBy={setSort}
-					sortStyleDesc={sortStyleDueDateDesc}
-					sortStyleAsc={sortStyleDueDateAsc}
-					type="dueDate"
-				/>
-			</div>
-		),
-		accessor: (row) => (
-			<div>
-				<div>{format(getByKey(row, 'dueDate'), 'dd MMM yy', {}, false)}</div>
-
-			</div>
-		),
-		id: 'due_date',
-
-	},
-
-	{
-		Header   : 'Overdue',
-		accessor : (row) => (
-			<div>
-				{getByKey(row, 'overDueDays')}
-			</div>
-		),
-	},
-
-	{
-		Header   : 'Proforma Status',
-		accessor : (row) => (
-
-			<div
-				className={styles.styled_pills}
-				style={{
-					'--color': INVOICE_STATUS_MAPPING[(getByKey(row, 'invoiceStatus'))],
-				}}
-			>
-				{row?.isFinalPosted ? <text className={styles.style_text}>FINAL POSTED</text> : (
-					<div>
-						{(startCase(row?.invoiceStatus)).length > NINE ? (
-							<Tooltip
-								interactive
-								placement="top"
-								content={(
-									<div
-										className={styles.tool_tip}
-									>
-										{startCase(getStatus({
-											entityCode    : row?.entityCode,
-											invoiceStatus : row?.invoiceStatus,
-										}))}
-									</div>
-								)}
-							>
-								<text className={styles.style_text}>
-									{`${startCase(getStatus({
-										entityCode    : row?.entityCode,
-										invoiceStatus : row?.invoiceStatus,
-									})).substring(
-										MIN_NAME_STRING,
-										NINE,
-									)}...`}
-
-								</text>
-							</Tooltip>
-						)
-							: (
-								<div className={styles.style_text}>
-									{startCase(getStatus({
-										entityCode,
-										invoiceStatus: row?.invoiceStatus,
-									}))}
-								</div>
-							)}
-					</div>
-				)}
-			</div>
-
-		),
-	},
-
-	{
-		Header:
-	<div className={styles.action_div}>
-		<span>
-			Actions
-		</span>
-		{' '}
-		<Tooltip
-			placement="top"
-			content={(
-				<div>
-					<div className={styles.div_flex}>
-						<IcMProvision
-							height={24}
-							width={24}
-							color="#F68B21"
-						/>
-						<span className={styles.margin_span}>
-							Remarks
-						</span>
-					</div>
-					<div className={styles.div_flex}>
-						<IcMOverview width={24} height={24} color="#F68B21" />
-						<span className={styles.margin_span}>
-							Invoice TimeLine
-						</span>
-					</div>
-				</div>
-
-			)}
-		>
-			<IcMInfo className={styles.icon_style} />
-		</Tooltip>
-	</div>,
-		id       : 'remarks',
-		accessor : (row) => (
-			<div style={{ display: 'flex', alignItems: 'center' }}>
-				<Remarks itemData={row} />
-				{showFilters ? (
-					<InvoiceDetails
-						item={row}
-						entityCode={entityCode}
-					/>
-				) : null}
-				<RenderIRNGenerated
-					itemData={row}
-					refetch={refetch}
-					entityCode={entityCode}
-				/>
-			</div>
-		),
-	},
-	{
-		Header   : '',
-		id       : 'ribbon',
-		accessor : (row) => (
-			<div>
-				<RibbonRender row={row} />
-			</div>
-		),
-	},
-
+	...completedTableRestFields({
+		setSort,
+		sortStyleLedgerTotalAsc,
+		sortStyleLedgerTotalDesc,
+		sortStyleInvoiceDateAsc,
+		sortStyleInvoiceDateDesc,
+		sortStyleDueDateAsc,
+		sortStyleDueDateDesc,
+		invoiceFilters,
+		setinvoiceFilters,
+		refetch,
+		entityCode,
+		showFilters,
+	}),
 ];
 
 export default completedColumn;
