3ebae506d997693221f857d4e8e020f4da69d28f
Ankit Kaushal <ankit.kaushal@cogoport.com>
2023-11-01 15:35:24 +0530

    exchange-rate-condition

--- packages/shipment-fcl-freight/common/SalesInvoice/Invoices/Header/index.js
+++ packages/shipment-fcl-freight/common/SalesInvoice/Invoices/Header/index.js
@@ -1,7 +1,5 @@
 import { ShipmentDetailContext } from '@cogoport/context';
-import GLOBAL_CONSTANTS from '@cogoport/globalization/constants/globals';
 import formatAmount from '@cogoport/globalization/utils/formatAmount';
-import { useSelector } from '@cogoport/store';
 import { useContext } from 'react';
 
 import EditInvoicePreference from './EditInvoicePreference';
@@ -21,11 +19,9 @@ function Header({
 		invoicing_parties,
 		reviewed_invoices,
 	} = invoiceData;
-	const user_data = useSelector(({ profile }) => profile || {});
-	const { shipment_data } = useContext(ShipmentDetailContext) || {};
 
-	const showExchangeRate = [GLOBAL_CONSTANTS.uuid.ajeet_singh_user_id,
-		GLOBAL_CONSTANTS.uuid.santram_gurjar_user_id].includes(user_data?.user?.id);
+	const { shipment_data } = useContext(ShipmentDetailContext) || {};
+	const showExchangeRate = !!shipment_data?.end_to_end_shipment?.is_possible;
 
 	const refetch = () => {
 		bfInvoiceRefetch();
