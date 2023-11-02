76f9c9aeeeb31990756106df4fa7feb83bc75c7e
Arya <arya.avinashtathe@cogoport.com>
2023-11-02 13:08:00 +0530

    ui improved

--- packages/rfq-dashboard/page-components/Dashboard/Content/RfqDetails/List/ListLoading/index.js
+++ packages/rfq-dashboard/page-components/Dashboard/Content/RfqDetails/List/ListLoading/index.js
@@ -4,16 +4,14 @@ import styles from './styles.module.css';
 
 function ListLoading() {
 	return (
-		<div>
+		<div className={styles.wrapper}>
 			{
 				[...Array(10).keys()].map((key) => (
 					<div className={styles.container} key={key}>
-						<div>
-							<Placeholder height="25px" width="200px" margin="10px 4px 10px 4px" />
-						</div>
-						<div><Placeholder height="25px" width="200px" margin="10px 4px 10px 4px" /></div>
-						<div><Placeholder height="25px" width="200px" margin="10px 4px 10px 4px" /></div>
-						<div><Placeholder height="25px" width="200px" margin="10px 4px 10px 4px" /></div>
+						<Placeholder height="25px" width="25%" margin="10px 4px 10px 4px" />
+						<Placeholder height="25px" width="25%" margin="10px 4px 10px 4px" />
+						<Placeholder height="25px" width="25%" margin="10px 4px 10px 4px" />
+						<Placeholder height="25px" width="25%" margin="10px 4px 10px 4px" />
 					</div>
 				))
 			}
