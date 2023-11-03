11e0864ef6a077d684275f8f55778100625b861d
sagar-paliwal <sagar.paliwal@cogoport.com>
2023-11-03 12:25:38 +0530

    create quest

--- packages/performance-and-incentives/page-components/Plans/components/QuestPlans/CreateQuest/index.js
+++ packages/performance-and-incentives/page-components/Plans/components/QuestPlans/CreateQuest/index.js
@@ -1,8 +1,23 @@
-function CreateQuests() {
+import { IcMArrowBack } from '@cogoport/icons-react';
+
+import MODE_KEYS_MAPPING from '../configurations/active-mode-key-mapping';
+
+import styles from './styles.module.css';
+
+const { LIST } = MODE_KEYS_MAPPING;
+
+function CreateQuests({ setMode = () => {} }) {
 	return (
 		<div>
-			<div>
-				CreateQuests
+			<div className={styles.header}>
+				<IcMArrowBack
+					className={styles.back_icon}
+					width={20}
+					height={20}
+					onClick={() => setMode(LIST)}
+				/>
+
+				<div className={styles.title}>Create Quest</div>
 			</div>
 		</div>
 	);
