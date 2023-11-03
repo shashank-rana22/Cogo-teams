fafe7521a1abf9ad894b69a3807d7d872d3ca8af
danampallyrahul-cogo <danampally.rahul@cogoport.com>
2023-11-03 17:18:45 +0530

    change payload

--- packages/cogo-one/page-components/CogoOneChannel/HeaderBar/ShowMoreStats/AgentStats/Stats/index.js
+++ packages/cogo-one/page-components/CogoOneChannel/HeaderBar/ShowMoreStats/AgentStats/Stats/index.js
@@ -34,9 +34,8 @@ function Stats({
 
 	const {
 		sent_count = 0,
-		read_count = 0,
-		unread_count = 0,
 		received_count = 0,
+		replied_count = 0,
 	} = mail_stats || {};
 
 	const { data, escalateLoading } = useGetAgentTimelineEscalate({ viewType, timePeriodValue });
@@ -77,10 +76,9 @@ function Stats({
 		avg_response_time           : `${(avgResponseTime || MIN_COUNT)?.toFixed(MIN_ROUND_UP_DIGIT)} min`,
 		emails_send_count           : getFormattedNumber(emailCount || MIN_COUNT),
 		escalate_chats_count        : getFormattedNumber(escalateCount || MIN_COUNT),
-		mail_sent                   : getFormattedNumber(sent_count || MIN_COUNT),
-		mail_received               : getFormattedNumber(received_count || MIN_COUNT),
-		mail_read                   : getFormattedNumber(read_count || MIN_COUNT),
-		mail_unread                 : getFormattedNumber(unread_count || MIN_COUNT),
+		total_mails_sent            : getFormattedNumber(sent_count || MIN_COUNT),
+		total_mails_received        : getFormattedNumber(received_count || MIN_COUNT),
+		total_mail_replies          : getFormattedNumber(replied_count || MIN_COUNT),
 	};
 
 	const STATS_FEEDBACK_MAPPING = VIEW_TYPE_GLOBAL_MAPPING[viewType]?.stats_feedback_count;
