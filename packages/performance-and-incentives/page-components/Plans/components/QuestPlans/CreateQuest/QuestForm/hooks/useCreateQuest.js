d81ee33fb21ae0d79359f70af74f6629d4a715fc
sagar-paliwal <sagar.paliwal@cogoport.com>
2023-11-08 19:30:05 +0530

    code files fixed

--- packages/performance-and-incentives/page-components/Plans/components/QuestPlans/CreateQuest/QuestForm/hooks/useCreateQuest.js
+++ packages/performance-and-incentives/page-components/Plans/components/QuestPlans/CreateQuest/QuestForm/hooks/useCreateQuest.js
@@ -44,13 +44,12 @@ const useCreateQuest = ({
 
 	const handleClick = async (formValues) => {
 		try {
-			const { name, date_range, agent_scoring_config_id, quest_string } = formValues;
+			const { name, date_range, agent_scoring_config_id } = formValues;
 
 			const { data: res } = await trigger({
 				data: {
 					name,
 					agent_scoring_config_id,
-					quest_string,
 					start_date : date_range?.startDate,
 					end_date   : getFormattedDate({ currentDate: date_range?.endDate }),
 				},
