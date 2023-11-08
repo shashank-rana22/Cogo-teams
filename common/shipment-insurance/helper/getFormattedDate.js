3844d8d77ce3c605098c4664b744573b43244387
ShubhamRam-cogoport <shubham.ram@cogoport.com>
2023-11-08 13:37:56 +0530

    added cmt

--- common/shipment-insurance/helper/getFormattedDate.js
+++ common/shipment-insurance/helper/getFormattedDate.js
@@ -1,11 +1,12 @@
-const sixHrInMilli = 6 * 60 * 60 * 1000;
+// adding 5:30 hr to match UTC time to IST
+const fiveHrInMilli = 5.5 * 60 * 60 * 1000;
 
 const getFormattedDate = ({ currentDate = '' }) => {
 	const date = (typeof currentDate === 'string' ? new Date() : currentDate);
 
 	const originalDate = new Date(date);
 
-	originalDate.setTime(originalDate.getTime() + sixHrInMilli);
+	originalDate.setTime(originalDate.getTime() + fiveHrInMilli);
 
 	return originalDate;
 };
