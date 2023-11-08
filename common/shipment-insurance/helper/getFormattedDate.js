08b1d79f6e732d375c8f88799f198e54f8f58530
ShubhamRam-cogoport <shubham.ram@cogoport.com>
2023-11-08 13:56:58 +0530

    fix timezone issue

--- common/shipment-insurance/helper/getFormattedDate.js
+++ common/shipment-insurance/helper/getFormattedDate.js
@@ -1,9 +1,12 @@
+// adding 5:30 hr to match UTC time to IST
+const fiveHrInMilli = 5.5 * 60 * 60 * 1000;
+
 const getFormattedDate = ({ currentDate = '' }) => {
 	const date = (typeof currentDate === 'string' ? new Date() : currentDate);
 
 	const originalDate = new Date(date);
 
-	originalDate.toLocaleString('en-IN', { timeZone: 'Asia/Kolkata' });
+	originalDate.setTime(originalDate.getTime() + fiveHrInMilli);
 
 	return originalDate;
 };
