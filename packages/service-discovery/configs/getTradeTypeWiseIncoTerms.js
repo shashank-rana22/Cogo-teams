a03744f022dc0c0a44574a09420690b554956b9e
rupesh-mutyala <rupesh.mutyala@cogoport.com>
2023-11-08 14:01:15 +0530

    fixes

--- packages/service-discovery/configs/getTradeTypeWiseIncoTerms.js
+++ packages/service-discovery/configs/getTradeTypeWiseIncoTerms.js
@@ -3,5 +3,5 @@ import incoterms from './incoterms.json';
 const getTradeTypeWiseIncoTerms = ({
 	tradeType = 'export',
 	isCrossLocationSearch,
-}) => incoterms.filter((item) => (item.tradeType === tradeType || isCrossLocationSearch));
+}) => incoterms.filter((item) => ((item.tradeType === tradeType) || isCrossLocationSearch));
 export default getTradeTypeWiseIncoTerms;
