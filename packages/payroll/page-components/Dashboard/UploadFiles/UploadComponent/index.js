dfd60de237b7c76aca589f2b31a3a7f71586e991
venkateshpatnala <Venkatesh.Patnala@cogoport.com>
2023-11-17 23:54:26 +0530

    file uploader

--- packages/payroll/page-components/Dashboard/UploadFiles/UploadComponent/index.js
+++ packages/payroll/page-components/Dashboard/UploadFiles/UploadComponent/index.js
@@ -1,6 +1,14 @@
+import { Button } from '@cogoport/components';
 import { InputController } from '@cogoport/forms';
+import { IcMDownload } from '@cogoport/icons-react';
+import { saveAs } from 'file-saver';
+import { useState } from 'react';
 import { useDropzone } from 'react-dropzone';
 import * as XLSX from 'xlsx';
+
+// import DownloadPDF from '../Base64topdf';
+import useUploadPayrollData from '../../../../hooks/useUploadPayrollData';
+import { STRING111 } from '../string';
 
 import styles from './styles.module.css';
 
@@ -12,6 +20,8 @@ const DROPZONESTYLES = {
 	cursor       : 'pointer',
 };
 function FileUploader({ control = {}, name = '', setFileArray = () => {}, filearray = {} }) {
+	const { updatePayrollData } = useUploadPayrollData();
+	const [isError, setIsError] = useState(null);
 	const onDrop = (acceptedFiles) => {
 		const file = acceptedFiles[0];
 
@@ -20,20 +30,33 @@ function FileUploader({ control = {}, name = '', setFileArray = () => {}, filear
 		reader.onload = (e) => {
 			const binaryString = e.target.result;
 			const workbook = XLSX.read(binaryString, { type: 'binary' });
-			console.log('first', workbook);
 			const firstSheetName = workbook.SheetNames[0];
 			const worksheet = workbook.Sheets[firstSheetName];
 			const data = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
 
 			// Convert data to base64
 			const base64 = btoa(unescape(encodeURIComponent(JSON.stringify(data))));
+			const res = updatePayrollData({
+				payload: base64,
+			});
+			setIsError(res);
 			setFileArray((prev) => ({ ...prev, [name]: base64 }));
 		};
 
 		reader.readAsBinaryString(file);
 	};
 
-	const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+	const downloadPDF = (base64String, fileName) => {
+		const file = `data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,${base64String}`;
+
+		saveAs(file, fileName);
+	};
+	const { getRootProps, getInputProps, isDragActive } = useDropzone({
+		onDrop,
+		accept: {
+			'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet': ['.xlsx'],
+		},
+	});
 
 	return (
 		<div className={styles.upload_container}>
@@ -53,6 +76,17 @@ function FileUploader({ control = {}, name = '', setFileArray = () => {}, filear
 					{' '}
 				</span>
 			)}
+			{
+				isError && (
+					<Button size="md" themeType="secondary" onClick={() => downloadPDF(STRING111, 'BASE64FILE')}>
+						<div className={styles.button_container}>
+							<IcMDownload width={14} height={14} />
+							<span className={styles.error_button_text}>Get Error Report</span>
+						</div>
+					</Button>
+				)
+			}
+
 		</div>
 	);
 }
