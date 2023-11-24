736e8f21cba1066b8feca36d514815fc7b0537a5
rupesh-mutyala <rupesh.mutyala@cogoport.com>
2023-11-24 14:22:26 +0530

    removed span

--- packages/service-discovery/page-components/SearchResults/common/LikeDislike/NewDislikeModal/components/ModalBody/components/DislikeRate/configs/main-service/has-additional-line-items-controls.js
+++ packages/service-discovery/page-components/SearchResults/common/LikeDislike/NewDislikeModal/components/ModalBody/components/DislikeRate/configs/main-service/has-additional-line-items-controls.js
@@ -3,7 +3,6 @@ const hasAdditionalLineItemsControls = () => [
 		label    : 'Remarks',
 		type     : 'textarea',
 		name     : 'has_additional_line_items.remarks',
-		span     : 12,
 		rules    : { required: 'Remarks are required' },
 		divWidth : '100%',
 	},
