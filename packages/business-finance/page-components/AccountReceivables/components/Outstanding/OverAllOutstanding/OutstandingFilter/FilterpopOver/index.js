8f292cc00f40931ab0208ed7f8dc5c2b88f64b2f
tejoyasaswi-cogoport <Tejo.Uppuluri@cogoport.com>
2023-11-26 19:32:37 +0530

    changes

--- packages/business-finance/page-components/AccountReceivables/components/Outstanding/OverAllOutstanding/OutstandingFilter/FilterpopOver/index.js
+++ packages/business-finance/page-components/AccountReceivables/components/Outstanding/OverAllOutstanding/OutstandingFilter/FilterpopOver/index.js
@@ -9,7 +9,7 @@ import AsyncSelect from '@cogoport/forms/page-components/Business/AsyncSelect';
 import { IcMFilter } from '@cogoport/icons-react';
 import React, { useState } from 'react';
 
-import { companyType } from '../../../../../constants/index';
+import { companyType, taggedState } from '../../../../../constants/index';
 
 import styles from './styles.module.css';
 
@@ -122,6 +122,16 @@ function FilterpopOver({
 									options={companyType}
 									value={filters?.companyType}
 									onChange={(val) => onChange(val, 'companyType')}
+									className={styles.style_radio}
+								/>
+							</div>
+						</TabPanel>
+						<TabPanel name="taggedState" title="Tagged State">
+							<div>
+								<RadioGroup
+									options={taggedState}
+									value={filters?.taggedState}
+									onChange={(val) => onChange(val, 'taggedState')}
 									className={styles.style_radio}
 								/>
 							</div>
