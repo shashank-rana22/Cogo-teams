a8c0b9b9adf99735f8a0b8a675dbabc058d1991b
sagar-paliwal <sagar.paliwal@cogoport.com>
2023-11-27 16:08:46 +0530

    added date in quests options

--- packages/performance-and-incentives/page-components/PublicLeaderboard/components/Body/RightPanel/QuestLeaderBoard/index.js
+++ packages/performance-and-incentives/page-components/PublicLeaderboard/components/Body/RightPanel/QuestLeaderBoard/index.js
@@ -1,10 +1,14 @@
 import { AsyncSelect } from '@cogoport/forms';
 
+import CustomSelectOption from '../../../../../../common/CustomSelectOption';
+
 import List from './List';
 import styles from './styles.module.css';
 
 function QuestLeaderBoard(props) {
 	const { questId, setQuestId, officeLocation } = props;
+
+	const renderQuestLabel = (data) => CustomSelectOption({ data, key: 'quests' });
 
 	return (
 		<div>
@@ -19,6 +23,7 @@ function QuestLeaderBoard(props) {
 					placeholder="Select quest"
 					size={window.innerWidth >= 2560 ? 'md' : 'sm'}
 					className={styles.quest_selector}
+					renderLabel={renderQuestLabel}
 				/>
 			</div>
 			<List questId={questId} officeLocation={officeLocation} />
