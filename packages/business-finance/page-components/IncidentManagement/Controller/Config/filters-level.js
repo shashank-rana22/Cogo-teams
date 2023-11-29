48e58cfe4b5f9ff65effd46cd363afbdc86dca08
KunalSinghCogoport <Kunal.Singh@cogoport.com>
2023-11-29 16:16:38 +0530

    approval changes

--- packages/business-finance/page-components/IncidentManagement/Controller/Config/filters-level.js
+++ packages/business-finance/page-components/IncidentManagement/Controller/Config/filters-level.js
@@ -53,6 +53,11 @@ const getControls = (t = () => {}) => [
 				value : 'JOB_OPEN',
 				label : t('incidentManagement:job_open_label'),
 			},
+			{
+				value : 'JOB_OPEN_FINANCIALLY',
+				label : 'JOB OPEN FINANCIALLY',
+			},
+			// todo :: add to translatin
 		],
 		rules: { required: t('incidentManagement:incident_type_required_message') },
 	},
