cb88ece8194435eddf045754bee5399e3b8b8ab4
tejoyasaswi-cogoport <Tejo.Uppuluri@cogoport.com>
2023-11-29 19:40:43 +0530

    changes

--- packages/business-finance/page-components/AccountReceivables/configs/get-service-wise-columns.js
+++ packages/business-finance/page-components/AccountReceivables/configs/get-service-wise-columns.js
@@ -1,82 +1,63 @@
 import formatAmount from '@cogoport/globalization/utils/formatAmount';
 
+function ShowAmount({ item = {}, amountKey = '', currencyKey = '' }) {
+	return (
+		<div>
+			{formatAmount({
+				amount   : item?.[amountKey],
+				currency : item?.[currencyKey],
+				options  : {
+					style                 : 'currency',
+					currencyDisplay       : 'code',
+					maximumFractionDigits : 0,
+				},
+			})}
+		</div>
+	);
+}
+
 const serviceWiseColumns = [
 	{
 		Header   : 'OPEN INVOICES',
 		accessor : (row) => (
-			<div>
-				{formatAmount({
-					amount   : row?.totalOpenInvoiceAmount,
-					currency : row?.ledCurrency,
-				})}
-			</div>
+			<ShowAmount item={row} amountKey="totalOpenInvoiceAmount" currencyKey="ledCurrency" />
 		),
 	},
 	{
 		Header   : 'NOT DUE',
 		accessor : (row) => (
-			<div>
-				{formatAmount({
-					amount   : row?.invoiceNotDueAmount,
-					currency : row?.ledCurrency,
-				})}
-			</div>
+			<ShowAmount item={row} amountKey="invoiceNotDueAmount" currencyKey="ledCurrency" />
 		),
 	},
 	{
 		Header   : '1-30 DAYS',
 		accessor : (row) => (
-			<div>
-				{formatAmount({
-					amount   : row?.invoiceThirtyAmount,
-					currency : row?.ledCurrency,
-				})}
-			</div>
+			<ShowAmount item={row} amountKey="invoiceThirtyAmount" currencyKey="ledCurrency" />
 		),
 	},
 	{
 		Header   : '31-60 DAYS',
 		accessor : (row) => (
-			<div>
-				{formatAmount({
-					amount   : row?.invoiceSixtyAmount,
-					currency : row?.ledCurrency,
-				})}
-			</div>
+			<ShowAmount item={row} amountKey="invoiceSixtyAmount" currencyKey="ledCurrency" />
 		),
 	},
 	{
 		Header   : '61-90 DAYS',
 		accessor : (row) => (
-			<div>
-				{formatAmount({
-					amount   : row?.invoiceNinetyAmount,
-					currency : row?.ledCurrency,
-				})}
-			</div>
+			<ShowAmount item={row} amountKey="invoiceNinetyAmount" currencyKey="ledCurrency" />
 		),
 
 	},
 	{
 		Header   : '91-180 DAYS',
 		accessor : (row) => (
-			<div>
-				{formatAmount({
-					amount   : row?.invoiceOneEightyAmount,
-					currency : row?.ledCurrency,
-				})}
-			</div>
+			<ShowAmount item={row} amountKey="invoiceOneEightyAmount" currencyKey="ledCurrency" />
 		),
 	},
 	{
 		Header   : '181-365 DAYS',
 		accessor : (row) => (
-			<div>
-				{formatAmount({
-					amount   : row?.invoiceThreeSixtyFiveAmount,
-					currency : row?.ledCurrency,
-				})}
-			</div>
+			<ShowAmount item={row} amountKey="invoiceThreeSixtyFiveAmount" currencyKey="ledCurrency" />
 		),
 	},
 ];
